97fdec5a124e654ad7779bf2f2a7333c439dbf0b
Lars Yencken <lars@yencken.org>
2021-09-23 03:29:46 +0200

    Get make to auto-install dependencies

--- Makefile
+++ Makefile
@@ -12,15 +12,25 @@ SRC = etl tests
 # watch:
 # 	poetry run watchmedo shell-command -c 'clear; make unittest' --recursive --drop .
 
-check-typing:
+.submodule-init:
+	@echo '==> Initialising submodules'
+	git submodule update --init
+	touch $@
+
+.venv: pyproject.toml poetry.toml poetry.lock vendor/*/* .submodule-init
+	@echo '==> Installing packages'
+	poetry install
+	touch $@
+
+check-typing: .venv
 	@echo '==> Checking types'
 	poetry run mypy --strict $(SRC)
 
-coverage:
+coverage: .venv
 	@echo '==> Unit testing with coverage'
 	poetry run pytest --cov=etl --cov-report=term-missing tests
 
-etl:
+etl: .venv
 	@echo '==> Running full etl'
 	poetry run python -m etl.command
 
--- default.mk
+++ default.mk
@@ -14,27 +14,33 @@ default:
 # check formatting before lint, since an autoformat might fix linting issues
 test-default: check-formatting lint check-typing unittest
 
-lint-default:
+.venv-default:
+	@echo '==> Installing packages'
+	git submodule update --init
+	poetry install
+	touch $@
+
+lint-default: .venv
 	@echo '==> Linting'
 	@poetry run flake8 $(SRC)
 
-check-formatting-default:
+check-formatting-default: .venv
 	@echo '==> Checking formatting'
 	@poetry run black --check $(SRC)
 
-check-typing-default:
+check-typing-default: .venv
 	@echo '==> Checking types'
 	poetry run mypy $(SRC)
 
-unittest-default:
+unittest-default: .venv
 	@echo '==> Running unit tests'
 	poetry run pytest $(SRC)
 
-format-default:
+format-default: .venv
 	@echo '==> Reformatting files'
 	@poetry run black $(SRC)
 
-watch-default:
+watch-default: .venv
 	poetry run watchmedo shell-command -c 'clear; make test' --recursive --drop .
 
 # allow you to override a command, e.g. "watch", but if you do not, then use
