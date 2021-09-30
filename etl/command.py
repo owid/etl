31c36fa81c37d3fcdbdbcc93f16c21b51ad4f6d5
Lars Yencken <lars@yencken.org>
2021-09-30 14:35:43 +0200

    feat: Only rebuild datasets that have changed
    This change refactors the command module into Step classes which are
    able to determine if they are "dirty", i.e. if they are missing or if
    their ingredients have changed since they were last built.
    
    The dirty checks are all based on checksums, rather than timestamps, so
    they only care about consistency.
    
    The checksums for data steps include the Python or Jupyter notebook
    files containing the code to generate the step, so the step will rebuild
    if those files change.
    
    Skipped steps are still listed in the ETL, but they are marked as
    "cached".

--- etl/command.py
+++ etl/command.py
@@ -3,13 +3,16 @@
 #  etl.py
 #
 
-from os import path
-import os
-from importlib import import_module
-from typing import Callable, List, Dict, Set, Iterable, Tuple, Any
 from collections import defaultdict
-import graphlib
+from dataclasses import dataclass
+from importlib import import_module
+from os import path
+from pathlib import Path
+from typing import Callable, List, Dict, Protocol, Set, Iterable, Tuple, Any
 from urllib.parse import urlparse
+import graphlib
+import hashlib
+import os
 import tempfile
 import time
 import warnings
@@ -22,44 +25,71 @@ with warnings.catch_warnings():
     warnings.simplefilter("ignore")
     import papermill as pm
 
-from owid import walden
+from owid import catalog, walden  # type: ignore
 
-BASE_DIR = path.normpath(path.join(path.dirname(__file__), ".."))
-DAG_FILE = path.join(BASE_DIR, "dag.yml")
-DATA_DIR = path.join(BASE_DIR, "data")
-STEP_DIR = path.join(BASE_DIR, "etl", "steps")
+BASE_DIR = Path(__file__).parent.parent
+DAG_FILE = BASE_DIR / "dag.yml"
+DATA_DIR = BASE_DIR / "data"
+STEP_DIR = BASE_DIR / "etl" / "steps"
 
 Graph = Dict[str, Set[str]]
 
 
 @click.command()
-@click.option("--dry-run", is_flag=True)
+@click.option("--dry-run", is_flag=True, help="Only print the steps that would be run")
+@click.option(
+    "--force", is_flag=True, help="Redo a step even if it appears done and up-to-date"
+)
 @click.argument("steps", nargs=-1)
-def main(steps: List[str], dry_run: bool = False) -> None:
+def main(steps: List[str], dry_run: bool = False, force: bool = False) -> None:
     """
     Execute all ETL steps listed in dag.yaml
     """
-    dag = load_yaml(DAG_FILE)
-    run_dag(dag, steps, dry_run)
+    # Load our graph of steps and the things they depend on
+    dag = load_yaml(DAG_FILE.as_posix())
+
+    # Run the steps we have selected, and everything downstream of them
+    run_dag(dag, steps, dry_run=dry_run, force=force)
 
 
-def run_dag(dag: Dict[str, Any], selection: List[str], dry_run: bool = False) -> None:
+def run_dag(
+    dag: Dict[str, Any], selection: List[str], dry_run: bool = False, force=False
+) -> None:
+    """
+    Run the selected steps, and anything that needs updating based on them. An empty
+    selection means "run all steps".
+
+    By default, data steps do not re-run if they appear to be up-to-date already by
+    looking at their checksum.
+    """
     step_names = select_steps(dag, selection)
+    steps = [_parse_step(name, dag) for name in step_names]
 
     print(f"Running {len(step_names)} steps:")
-    for i, step_name in enumerate(step_names, 1):
+    for i, step_name in enumerate(step_names, 0):
+        step = steps[i]
+
         print(f"  {i}. {step_name}... ", end="", flush=True)
         if not dry_run:
-            time_taken = timed_run(lambda: run_step(step_name))
-            print(f"({time_taken:.0f}s)")
+            if force or step.is_dirty():
+                time_taken = timed_run(lambda: step.run())
+                print(f"({time_taken:.0f}s)")
+            else:
+                print(f"(cached)")
         else:
             print()
 
 
 def select_steps(dag: Dict[str, Any], selection: List[str]) -> List[str]:
+    """
+    Return the list of steps which, if executed in order, mean that every
+    step has its dependencies ready for it.
+    """
     graph = reverse_graph(dag["steps"])
 
     if selection:
+        # cut the graph to just the listed steps and the things that
+        # then depend on them (transitive closure)
         subgraph = filter_to_subgraph(graph, selection)
     else:
         subgraph = graph
@@ -112,92 +142,197 @@ def topological_sort(graph: Graph) -> List[str]:
     return list(reversed(list(graphlib.TopologicalSorter(graph).static_order())))
 
 
-def run_step(step_name: str) -> None:
-    step_type, name = parse_step(step_name)
+def _parse_step(step_name: str, dag: Dict[str, Any]) -> "Step":
+    parts = urlparse(step_name)
+    step_type = parts.scheme
+    path = parts.netloc + parts.path
 
     if step_type == "data":
-        run_data_step(name)
+        dependencies = dag["steps"][step_name]
+        step = DataStep(path, [_parse_step(s, dag) for s in dependencies])
 
     elif step_type == "walden":
-        run_walden_step(name)
+        step = WaldenStep(path)
 
     else:
         raise Exception(f"no recipe for executing step: {step_name}")
 
-
-def run_data_step(step_name: str) -> None:
-    # data steps are expected to create new datasets in data/
-    dest_dir = path.join(DATA_DIR, step_name.lstrip("/"))
-    os.makedirs(path.dirname(dest_dir), exist_ok=True)
-
-    module_name = path.join(STEP_DIR, "data", step_name)
-    if path.exists(module_name + ".py"):
-        run_data_step_py(step_name, dest_dir)
-
-    elif path.exists(module_name + ".ipynb"):
-        run_data_step_notebook(module_name + ".ipynb", dest_dir)
-
-    else:
-        raise Exception(f"have no idea how to run step: {step_name}")
+    return step
 
 
-def run_data_step_py(step_name: str, dest_dir: str) -> None:
-    step_module = import_module(f"etl.steps.data.{step_name}")
-    if not hasattr(step_module, "run"):
-        raise Exception(f'no run() method defined for step "{step_name}"')
+class Step(Protocol):
+    def run(self) -> None:
+        ...
 
-    # data steps
-    step_module.run(dest_dir)  # type: ignore
+    def is_dirty(self) -> bool:
+        ...
 
-
-def run_data_step_notebook(notebook_path: str, dest_dir: str) -> None:
-    "Run a parameterised Jupyter notebook."
-    with tempfile.TemporaryDirectory() as tmp_dir:
-        notebook_out = path.join(tmp_dir, "notebook.ipynb")
-        with open(path.join(tmp_dir, "output.log"), "w") as ostream:
-            pm.execute_notebook(
-                notebook_path,
-                notebook_out,
-                parameters={"dest_dir": path.abspath(dest_dir)},
-                progress_bar=False,
-                stdout_file=ostream,
-                stderr_file=ostream,
-            )
+    def checksum_output(self) -> str:
+        ...
 
 
-def run_walden_step(walden_path: str) -> None:
-    "Ensure the dataset we're looking for is there."
-    walden_dataset = _find_walden_dataset(walden_path)
-    walden_dataset.ensure_downloaded()
+class DataStep(Step):
+    """
+    A step which creates a local Dataset on disk in the data/ folder. You specify it
+    by making a Python module or a Jupyter notebook with a matching path in the
+    etl/steps/data folder.
+    """
 
+    path: str
+    dependencies: List[Step]
 
-def _find_walden_dataset(walden_path: str) -> walden.Dataset:
-    if walden_path.count("/") != 2:
-        raise ValueError(f"malformed walden path: {walden_path}")
+    def __init__(self, path: str, dependencies: List[Step]) -> None:
+        self.path = path
+        self.dependencies = dependencies
 
-    namespace, version, short_name = walden_path.split("/")
-    catalog = walden.Catalog()
+    def run(self) -> None:
+        # make sure the encosing folder is there
+        self._dest_dir.parent.mkdir(parents=True, exist_ok=True)
 
-    # normally version is a year or date, but we also accept "latest"
-    if version == "latest":
-        dataset = catalog.find_latest(namespace=namespace, short_name=short_name)
-    else:
-        dataset = catalog.find_one(
-            namespace=namespace, version=version, short_name=short_name
+        if self._search_path.with_suffix(".py").exists():
+            self._run_py()
+
+        elif self._search_path.with_suffix(".ipynb").exists():
+            self._run_notebook()
+
+        else:
+            raise Exception(f"have no idea how to run step: {self.path}")
+
+        # modify the dataset to remember what inputs were used to build it
+        dataset = self._output_dataset
+        dataset.metadata.source_checksum = self.checksum_input()
+        dataset.save()
+
+    def is_dirty(self) -> bool:
+        return (
+            # dirty if we haven't built this dataset yet
+            not self._dest_dir.is_dir()
+            # or it was built off of different inputs
+            or catalog.Dataset(self._dest_dir.as_posix()).metadata.source_checksum
+            != self.checksum_input()
         )
 
-    return dataset
+    def checksum_input(self) -> str:
+        "Return the MD5 of all ingredients for making this step."
+        inputs = [d.checksum_output() for d in self.dependencies]
+        inputs.extend(_checksum_file(f) for f in sorted(self._step_files()))
+        return hashlib.md5(",".join(inputs).encode("utf8")).hexdigest()
+
+    @property
+    def _output_dataset(self) -> catalog.Dataset:
+        "If this step is completed, return the MD5 of the output."
+        if not self._dest_dir.is_dir():
+            raise Exception("dataset has not been created yet")
+
+        return catalog.Dataset(self._dest_dir.as_posix())
+
+    def checksum_output(self) -> str:
+        return self._output_dataset.checksum()
+
+    def _step_files(self) -> List[str]:
+        known_suffixes = [".py", ".ipynb"]
+        files = []
+        for suffix in known_suffixes:
+            p = self._search_path.with_suffix(suffix)
+            if p.exists():
+                files.append(p.as_posix())
+
+        return files
+
+    @property
+    def _search_path(self) -> Path:
+        return Path(STEP_DIR) / "data" / self.path
+
+    @property
+    def _dest_dir(self) -> Path:
+        return DATA_DIR / self.path.lstrip("/")
+
+    def _run_py(self) -> None:
+        """
+        Import the Python module for this step and call run() on it.
+        """
+        module_path = self.path.lstrip("/").replace("/", ".")
+        step_module = import_module(f"etl.steps.data.{module_path}")
+        if not hasattr(step_module, "run"):
+            raise Exception(f'no run() method defined for module "{step_module}"')
+
+        # data steps
+        step_module.run(self._dest_dir.as_posix())  # type: ignore
+
+    def _run_notebook(self) -> None:
+        "Run a parameterised Jupyter notebook."
+        notebook_path = self._search_path.with_suffix(".ipynb").as_posix()
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            notebook_out = path.join(tmp_dir, "notebook.ipynb")
+            with open(path.join(tmp_dir, "output.log"), "w") as ostream:
+                pm.execute_notebook(
+                    notebook_path,
+                    notebook_out,
+                    parameters={"dest_dir": self._dest_dir.as_posix()},
+                    progress_bar=False,
+                    stdout_file=ostream,
+                    stderr_file=ostream,
+                )
 
 
-def parse_step(step_name: str) -> Tuple[str, str]:
-    parts = urlparse(step_name)
-    return parts.scheme, parts.netloc + parts.path
+@dataclass
+class WaldenStep(Step):
+    path: str
+
+    def __init__(self, path: str) -> None:
+        self.path = path
+
+    def run(self) -> None:
+        "Ensure the dataset we're looking for is there."
+        self._walden_dataset.ensure_downloaded(quiet=True)
+
+    def is_dirty(self) -> bool:
+        return not Path(self._walden_dataset.local_path).exists()
+
+    def checksum_output(self) -> str:
+        checksum = self._walden_dataset.md5
+        if not checksum:
+            raise Exception(
+                f"no md5 checksum available for walden dataset: {self.path}"
+            )
+        return checksum
+
+    @property
+    def _walden_dataset(self) -> walden.Dataset:
+        if self.path.count("/") != 2:
+            raise ValueError(f"malformed walden path: {self.path}")
+
+        namespace, version, short_name = self.path.split("/")
+        catalog = walden.Catalog()
+
+        # normally version is a year or date, but we also accept "latest"
+        if version == "latest":
+            dataset = catalog.find_latest(namespace=namespace, short_name=short_name)
+        else:
+            dataset = catalog.find_one(
+                namespace=namespace, version=version, short_name=short_name
+            )
+
+        return dataset
 
 
 def timed_run(f: Callable[[], Any]) -> float:
     start_time = time.time()
     f()
     return time.time() - start_time
+
+
+def _checksum_file(filename: str) -> str:
+    "Return the md5 hex digest of the file contents."
+    chunk_size = 2 ** 20
+    _hash = hashlib.md5()
+    with open(filename, "rb") as istream:
+        chunk = istream.read(chunk_size)
+        while chunk:
+            _hash.update(chunk)
+            chunk = istream.read(chunk_size)
+
+    return _hash.hexdigest()
 
 
 if __name__ == "__main__":
