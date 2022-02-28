99ca772d4a80a1f0ee310ae2d3a38be2b62c884e
Marigold <mojmir.vinkler@gmail.com>
2022-02-28 15:56:38 +0100

    data(countries_regions): accurate continents

---
+++ etl/scripts/countries-regions/init.py
@@ -0,0 +1,13 @@
+import os
+import pandas as pd  # noqa
+from IPython import get_ipython
+from sqlalchemy import create_engine
+
+engine = create_engine(os.environ["MYSQL_URL"])
+
+ipython = get_ipython()
+ipython.magic("load_ext rich")
+ipython.magic("load_ext autoreload")
+ipython.magic("autoreload 2")
+ipython.magic("pylab inline")
+ipython.magic("config InlineBackend.figure_format = 'svg'")
