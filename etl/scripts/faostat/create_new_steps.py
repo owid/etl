c8e20d5f5a9d07f7ca683de15b93a6e6cd7b2cb7
Pablo Rosado <pabloarosado@gmail.com>
2022-05-03 17:28:00 +0200

    enhance(faostat): improve script to create new steps, and let it update dag file

--- etl/scripts/faostat/create_new_steps.py
+++ etl/scripts/faostat/create_new_steps.py
@@ -1,16 +1,38 @@
 """Create new version of FAOSTAT steps in meadow or garden.
 
+Workflow to create a new version of the FAOSTAT dataset (with version YYYY-MM-DD):
+1. Execute this script for the meadow workspace.
+  > python etl/scripts/faostat/create_new_steps.py -w meadow
+2. Run the new etl meadow steps.
+  > etl meadow/faostat/YYYY-MM-DD
+3. Run this script again for the garden workspace.
+4. Run the new etl garden steps.
+  > etl garden/faostat/YYYY-MM-DD
+5. TODO: Decide what to do with grapher steps.
+
+This script will, for a given workspace:
+* Create a new folder in the workspace (with today's date) and copy all step files from the latest version into the folder.
+  * This means that every time it is executed, it will create a new step file for each domain, even if the
+    original data (in walden) did not change.
+    TODO: Consider improving this, so that step files are created only if there was an update.
+* Add all new steps to the dag file.
+
+TODO: Implement the same logic for garden steps.
+
 """
 
 import argparse
 import datetime
 import shutil
 from pathlib import Path
+from typing import Dict, List, Optional
 
 import pandas as pd
+import yaml
 from owid import catalog
+from owid.walden import Catalog
 
-from etl.paths import DATA_DIR, STEP_DIR
+from etl.paths import DAG_FILE, DATA_DIR, STEP_DIR
 
 NAMESPACE = "faostat"
 # Base name of additional metadata file.
@@ -18,10 +40,22 @@ ADDITIONAL_METADATA_FILE_NAME = f"{NAMESPACE}_metadata"
 RUN_FILE_NAME = "shared"
 
 
+def list_dataset_codes(data_files_dir: Path) -> List[Path]:
+    # Path to additional metadata file name (without extension).
+    metadata_file_name = data_files_dir / ADDITIONAL_METADATA_FILE_NAME
+    # Get list of dataset codes (or domains) from additional metadata.
+    additional_metadata = catalog.Dataset(metadata_file_name)
+    domains = pd.unique(
+        [table_name.split("_")[1] for table_name in additional_metadata.table_names]
+    ).tolist()
+
+    return domains
+
+
 def create_steps_for_new_version(
     new_version_dir: Path,
     latest_version_dir: Path,
-    latest_metadata_file: Path,
+    domains: List[Path],
     create_all_files_from_scratch: bool = False,
 ) -> None:
     if new_version_dir.is_dir():
@@ -33,15 +67,6 @@ def create_steps_for_new_version(
         new_version_dir.mkdir()
 
         if create_all_files_from_scratch:
-            # Get list of domains from additional metadata generated in latest version.
-            additional_metadata = catalog.Dataset(latest_metadata_file)
-            domains = pd.unique(
-                [
-                    table_name.split("_")[1]
-                    for table_name in additional_metadata.table_names
-                ]
-            ).tolist()
-
             # Copy run file from latest step to new step.
             latest_run_file = latest_version_dir / (RUN_FILE_NAME + ".py")
             new_run_file = new_version_dir / (RUN_FILE_NAME + ".py")
@@ -69,6 +94,83 @@ def create_steps_for_new_version(
                 shutil.copy(old_file_path, new_file_path)
 
 
+def list_all_steps(data_files_dir: Path) -> List[str]:
+    domains = list_dataset_codes(data_files_dir=data_files_dir)
+    step_names = [f"{NAMESPACE}_{domain}" for domain in domains]
+    # Add metadata step to the list.
+    step_names += [ADDITIONAL_METADATA_FILE_NAME]
+    step_names = sorted(step_names)
+
+    return step_names
+
+
+def create_dependency_graph(
+    workspace: str, new_version: str, domains: List[Path]
+) -> Dict[str, str]:
+    # Gather all step names.
+    step_names = [f"{NAMESPACE}_{domain}" for domain in domains]
+    # Add metadata step to the list.
+    step_names += [ADDITIONAL_METADATA_FILE_NAME]
+    step_names = sorted(step_names)
+
+    dag_steps = {}
+    for step_name in step_names:
+        # Define new step in dag.
+        dag_step = f"data://{workspace}/{NAMESPACE}/{new_version}/{step_name}"
+
+        if workspace == "meadow":
+            # Find latest walden version for current step.
+            walden_version = (
+                Catalog().find_latest(namespace=NAMESPACE, short_name=step_name).version
+            )
+            # Define dependencies for new step.
+            dependency_step = f"walden://{NAMESPACE}/{walden_version}/{step_name}"
+        elif workspace == "garden":
+            # TODO: add dependencies from meadow. Also, create a dictionary for manual dependencies (like fbsc).
+            dependency_step = ""
+            raise NotImplemented
+        else:
+            raise ValueError("workspace must be meadow or garden")
+
+        dag_steps[dag_step] = dependency_step
+
+    return dag_steps
+
+
+def write_steps_to_dag_file(
+    dag_steps: Dict[str, str], header_line: Optional[str]
+) -> None:
+    # Indentation of steps in the dag file.
+    step_indent = "  "
+    dependency_indent = "    - "
+    new_step_lines = ""
+    if header_line:
+        new_step_lines += f"{step_indent}#\n"
+        new_step_lines += f"{step_indent}{header_line}\n"
+        new_step_lines += f"{step_indent}#\n"
+
+    # Load dag.
+    with open(DAG_FILE) as _dag_file:
+        dag = yaml.safe_load(_dag_file)
+
+    # Initialise a flag that is True only if at least one step has to be written to dag.
+    any_step_updated = False
+    for dag_step in dag_steps:
+        # Add new step to dag if not already there.
+        if dag_step in dag["steps"]:
+            print(f"Dag step {dag_step} already in dag. Skipping.")
+        else:
+            any_step_updated = True
+            new_step_lines += f"{step_indent}{dag_step}:\n"
+            new_step_lines += f"{dependency_indent}{dag_steps[dag_step]}\n"
+
+    if any_step_updated:
+        print("Writing new steps to dag file.")
+        # Add new lines to dag file.
+        with open(DAG_FILE, "a") as _dag_file:
+            _dag_file.write(new_step_lines)
+
+
 def main(workspace: str, create_all_files_from_scratch: bool = False) -> None:
     # Path to folder containing steps in this workspace.
     versions_dir = STEP_DIR / "data" / workspace / NAMESPACE
@@ -77,20 +179,34 @@ def main(workspace: str, create_all_files_from_scratch: bool = False) -> None:
     # New version of steps to create.
     new_version = datetime.datetime.today().strftime("%Y-%m-%d")
     # Path to folder containing data files produced in the latest version.
-    files_dir = DATA_DIR / workspace / NAMESPACE / latest_version
-    # Path to additional metadata file from latest version.
-    latest_metadata_file = files_dir / ADDITIONAL_METADATA_FILE_NAME
+    latest_files_dir = DATA_DIR / workspace / NAMESPACE / latest_version
     # Path to folder containing code for steps in the latest version.
     latest_version_dir = versions_dir / latest_version
     # Path to folder to be created with new steps.
     new_version_dir = versions_dir / new_version
 
-    create_steps_for_new_version(
-        new_version_dir=new_version_dir,
-        latest_version_dir=latest_version_dir,
-        latest_metadata_file=latest_metadata_file,
-        create_all_files_from_scratch=create_all_files_from_scratch,
-    )
+    if latest_version == new_version:
+        print(f"Dataset is already up-to-date with version: {new_version}")
+    else:
+        # List all FAOSTAT dataset codes (domains) that are relevant.
+        domains = list_dataset_codes(data_files_dir=latest_files_dir)
+
+        # Create folder for new version and add a file for each step.
+        create_steps_for_new_version(
+            new_version_dir=new_version_dir,
+            latest_version_dir=latest_version_dir,
+            domains=domains,
+            create_all_files_from_scratch=create_all_files_from_scratch,
+        )
+
+        # Generate dictionary of step dependencies.
+        dag_steps = create_dependency_graph(
+            workspace=workspace, new_version=new_version, domains=domains
+        )
+
+        # Update dag file.
+        header_line = f"# FAOSTAT {workspace} steps for version {new_version}"
+        write_steps_to_dag_file(dag_steps=dag_steps, header_line=header_line)
 
 
 if __name__ == "__main__":
@@ -99,6 +215,7 @@ if __name__ == "__main__":
         "-w",
         "--workspace",
         help=f"Name of workspace where new step will be created (either meadow or garden).",
+        required=True,
     )
     parser.add_argument(
         "-s",
