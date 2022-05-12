5acbb4b622cbd2d48e8491fc1bac1684de17db83
William Cox <williamcox@college.harvard.edu>
2022-05-12 15:24:45 -0500

    Add parquet benchmarking, use bigger test file

--- benchmarking_tmp.py
+++ benchmarking_tmp.py
@@ -6,9 +6,8 @@ import timeit
 
 from tempfile import TemporaryDirectory as TempDir
 
-ITERATIONS = 100
-TEST_DF = pd.read_feather(
-    'https://owid-catalog.nyc3.digitaloceanspaces.com/garden/ggdc/2020-10-01/ggdc_maddison/maddison_gdp.feather')
+ITERATIONS = 20
+TEST_DF = pd.read_feather('data/garden/owid/latest/covid/covid.feather')
 
 
 def read_feather_gz(feather_gz_file: str) -> pd.DataFrame:
@@ -19,7 +18,7 @@ def read_feather_gz(feather_gz_file: str) -> pd.DataFrame:
 
 def benchmark():
     """Compare the compressed size + read performance for zstd and gzip compression of feather files"""
-    print('Dataframe len: {}'.format(len(TEST_DF.index)))
+    print('Dataframe len: {}\n'.format(len(TEST_DF.index)))
 
     with TempDir() as tempdir:
         # Write feather with native compression
@@ -34,15 +33,21 @@ def benchmark():
             with gzip.open(raw_feather_gz, 'wb') as f_out:
                 shutil.copyfileobj(f_in, f_out)
 
+        # Write parquet
+        parquet_snappy = os.path.join(tempdir, 'df.parquet')
+        TEST_DF.to_parquet(parquet_snappy)  # has default 'snappy' compression
+
         # Print file sizes to compare
         print('Size of zstd feather: {:,} bytes'.format(os.path.getsize(zstd_feather)))
         print('Size of gzipped raw feather: {:,} bytes'.format(os.path.getsize(raw_feather_gz)))
+        print('Size of parquet (snappy compression): {:,} bytes\n'.format(os.path.getsize(parquet_snappy)))
 
         # Profile read time for each type
+        setup = 'import pandas as pd'
         zstd_timer = timeit.Timer(
             '_ = pd.read_feather(zstd_feather)',
             globals={'zstd_feather': zstd_feather},
-            setup='import pandas as pd')
+            setup=setup)
         zstd_time = zstd_timer.timeit(ITERATIONS) / ITERATIONS
 
         gz_timer = timeit.Timer(
@@ -50,8 +55,15 @@ def benchmark():
             globals={'read_feather_gz': read_feather_gz, 'raw_feather_gz': raw_feather_gz})
         gz_time = gz_timer.timeit(ITERATIONS) / ITERATIONS
 
+        parquet_timer = timeit.Timer(
+            '_ = pd.read_parquet(parquet_snappy)',
+            globals={'parquet_snappy': parquet_snappy},
+            setup=setup)
+        parquet_time = parquet_timer.timeit(ITERATIONS) / ITERATIONS
+
         print('Average read time (zstd): {:.5f}s'.format(zstd_time))
         print('Average read time (uncompressed + gz): {:.5f}s'.format(gz_time))
+        print('Average read time (parquet + snappy): {:.5f}s'.format(parquet_time))
 
 
 if __name__ == "__main__":
