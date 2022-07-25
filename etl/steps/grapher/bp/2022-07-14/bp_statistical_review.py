fb1df5f5e650587dd186f05ec6c6734500590bc3
Pablo Rosado <pabloarosado@gmail.com>
2022-07-25 13:41:49 +0200

    fix(bp): Load dataset descriptions properly in BP statistical review grapher step

--- etl/steps/grapher/bp/2022-07-14/bp_statistical_review.py
+++ etl/steps/grapher/bp/2022-07-14/bp_statistical_review.py
@@ -13,9 +13,9 @@ def get_grapher_dataset() -> catalog.Dataset:
     dataset = catalog.Dataset(DATA_DIR / "garden" / NAMESPACE / VERSION / "bp_statistical_review")
     assert len(dataset.metadata.sources) == 1
 
-    # move description to source as that is what is shown in grapher
-    # (dataset.description would be displayed under `Internal notes` in the admin UI otherwise)
+    # Copy the dataset description to the source's description, since this is what is shown in grapher.
     dataset.metadata.sources[0].description = dataset.metadata.description
+    # Empty dataset description (otherwise it will appear in `Internal notes` in the admin UI).
     dataset.metadata.description = ""
 
     return dataset
@@ -24,6 +24,11 @@ def get_grapher_dataset() -> catalog.Dataset:
 def get_grapher_tables(dataset: catalog.Dataset) -> Iterable[catalog.Table]:
     # There is only one table in the dataset, with the same name as the dataset.
     table = dataset[dataset.table_names[0]].reset_index()
+
+    # Copy the new dataset description (which is now stored in sources) to the variable sources' description.
+    data_columns = [col for col in table.columns if col not in ["year", "country", "country_code"]]
+    for col in data_columns:
+        table[col].metadata.sources[0].description = dataset.metadata.sources[0].description
 
     # Grapher needs a column entity id, that is constructed based on the unique entity names in the database.
     table["entity_id"] = gh.country_to_entity_id(table["country"])
