25659d1552c67bee1810792eb7b9f6d5fe93275f
Pablo Rosado <pabloarosado@gmail.com>
2022-07-27 11:07:55 +0200

    enhance: Simplify BP and energy grapher steps

--- etl/steps/grapher/energy/2022-07-20/fossil_fuel_production.py
+++ etl/steps/grapher/energy/2022-07-20/fossil_fuel_production.py
@@ -7,14 +7,13 @@ from typing import Iterable
 from etl import grapher_helpers as gh
 from etl.paths import DATA_DIR
 from owid import catalog
-from .shared import adapt_dataset_metadata_for_grapher, adapt_table_for_grapher
 
 DATASET_PATH = DATA_DIR / "garden" / "energy" / "2022-07-20" / "fossil_fuel_production"
 
 
 def get_grapher_dataset() -> catalog.Dataset:
     dataset = catalog.Dataset(DATASET_PATH)
-    dataset.metadata = adapt_dataset_metadata_for_grapher(dataset.metadata)
+    dataset.metadata = gh.adapt_dataset_metadata_for_grapher(dataset.metadata)
 
     return dataset
 
@@ -22,6 +21,7 @@ def get_grapher_dataset() -> catalog.Dataset:
 def get_grapher_tables(dataset: catalog.Dataset) -> Iterable[catalog.Table]:
     # There is only one table in the dataset, with the same name as the dataset.
     table = dataset[dataset.table_names[0]].reset_index()
-    table = adapt_table_for_grapher(table)
+    table = gh.adapt_table_for_grapher(table)
 
     yield from gh.yield_wide_table(table, na_action="drop")
+
