0947512768ebae41a8581840522a533d6f731cb8
Marigold <mojmir.vinkler@gmail.com>
2022-07-28 22:01:27 +0200

    fix(isolated_env): keep certain modules imported

--- etl/helpers.py
+++ etl/helpers.py
@@ -3,6 +3,7 @@
 #  etl
 #
 
+import re
 import sys
 import tempfile
 from collections.abc import Generator
@@ -56,12 +57,16 @@ def _get_github_branches(org: str, repo: str) -> List[Any]:
 
 
 @contextmanager
-def isolated_env(working_dir: Path) -> Generator[None, None, None]:
+def isolated_env(
+    working_dir: Path, keep_modules: str = r"openpyxl|pyarrow|lxml|PIL"
+) -> Generator[None, None, None]:
     """Add given directory to pythonpath, run code in context, and
     then remove from pythonpath and unimport modules imported in context.
 
     Note that unimporting modules means they'll have to be imported again, but
     it has minimal impact on performance (ms).
+
+    :param keep_modules: regex of modules to keep imported
     """
     # add module dir to pythonpath
     sys.path.append(working_dir.as_posix())
@@ -71,9 +76,10 @@ def isolated_env(working_dir: Path) -> Generator[None, None, None]:
 
     yield
 
-    # unimport modules imported during execution
+    # unimport modules imported during execution unless they match `keep_modules`
     for module_name in set(sys.modules.keys()) - imported_modules:
-        sys.modules.pop(module_name)
+        if not re.search(keep_modules, module_name):
+            sys.modules.pop(module_name)
 
     # remove module dir from pythonpath
     sys.path.remove(working_dir.as_posix())
