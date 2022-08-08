506d8b96976a23625a8312cbfeae022ee2cb9e07
Pablo Rosado <pabloarosado@gmail.com>
2022-08-08 17:59:49 +0200

    feat(energy): Complete garden step for OWID Energy dataset

--- etl/steps/data/garden/energy/2022-08-05/owid_energy.py
+++ etl/steps/data/garden/energy/2022-08-05/owid_energy.py
@@ -1,7 +1,6 @@
 """Garden step that combines various datasets related to energy and produces the OWID Energy dataset (2022).
 
 Datasets combined:
-* Statistical Review of World Energy - BP (2022).
 * Energy mix from BP.
 * Fossil fuel production (BP & Shift, 2022).
 * Primary energy consumption (BP & EIA, 2022).
@@ -9,55 +8,26 @@ Datasets combined:
 
 """
 
+from typing import Dict, cast
+
 import pandas as pd
 from owid import catalog
+from owid.datautils import dataframes
 
 from etl.paths import DATA_DIR
-from shared import CURRENT_DIR
+from shared import CURRENT_DIR, gather_sources_from_tables
 
 # Details for dataset to export.
 DATASET_SHORT_NAME = "owid_energy"
+DATASET_TITLE = "Energy dataset (OWID, 2022)"
 METADATA_PATH = CURRENT_DIR / f"{DATASET_SHORT_NAME}.meta.yml"
 # Details for datasets to import.
-STATISTICAL_REVIEW_DATASET_PATH = DATA_DIR / "garden/bp/2022-07-14/statistical_review"
 ENERGY_MIX_DATASET_PATH = DATA_DIR / "garden/bp/2022-07-14/energy_mix"
 FOSSIL_FUEL_PRODUCTION_DATASET_PATH = DATA_DIR / "garden/energy/2022-07-20/fossil_fuel_production"
 PRIMARY_ENERGY_CONSUMPTION_DATASET_PATH = DATA_DIR / "garden/energy/2022-07-29/primary_energy_consumption"
 ELECTRICITY_MIX_DATASET_PATH = DATA_DIR / "garden/energy/2022-08-03/electricity_mix"
-
-
-def process_bp_data(table_bp: catalog.Table) -> pd.DataFrame:
-    """Load necessary columns from BP's Statistical Review dataset.
-
-    Parameters
-    ----------
-    table_bp : catalog.Table
-        BP's Statistical Review (already processed, with harmonized countries and region aggregates).
-
-    Returns
-    -------
-    df_bp : pd.DataFrame
-        Processed BP data.
-
-    """
-    # Columns to load from BP dataset.
-    columns = {
-        "electricity_generation": "total_generation__twh",
-        # TODO: Add required columns.
-    }
-    table_bp = table_bp[list(columns)].rename(columns=columns, errors="raise")
-
-    # Prepare data in a dataframe with a dummy index.
-    df_bp = pd.DataFrame(table_bp).reset_index()
-
-    return df_bp
-
-
-def combine_all_data(df_statistical_review: pd.DataFrame, df_energy_mix: pd.DataFrame, df_fossil_fuels: pd.DataFrame,
-                     df_primary_energy: pd.DataFrame, df_electricity_mix: pd.DataFrame) -> pd.DataFrame:
-    combined = pd.DataFrame()
-
-    return combined
+# Path to file with mapping of variable names from one of the datasets to the final energy dataset.
+VARIABLE_MAPPING_FILE = CURRENT_DIR / "owid_energy_variable_mapping.csv"
 
 
 def prepare_output_table(combined: pd.DataFrame) -> catalog.Table:
@@ -76,9 +46,7 @@ def prepare_output_table(combined: pd.DataFrame) -> catalog.Table:
     """
     # Sort rows and columns conveniently and set an index.
     combined = combined[sorted(combined.columns)]
-    combined = combined.set_index(
-        ["country", "year"], verify_integrity=True
-    ).sort_index()
+    combined = combined.set_index(["country", "year"], verify_integrity=True).sort_index()
 
     # Convert dataframe into a table (with no metadata).
     table = catalog.Table(combined)
@@ -89,43 +57,106 @@ def prepare_output_table(combined: pd.DataFrame) -> catalog.Table:
     return table
 
 
+def combine_tables_data_and_metadata(tables: Dict[str, catalog.Table],
+                                     countries_regions: catalog.Table,
+                                     variable_mapping: pd.DataFrame) -> catalog.Table:
+    # Merge all dataframes.
+    dfs = [pd.DataFrame(table) for table in tables.values()]
+    df_combined = dataframes.multi_merge(dfs, on=["country", "year"], how="outer")
+
+    # Add ISO codes for countries (regions that are not in countries-regions dataset will have nan iso_code).
+    df_combined = pd.merge(df_combined, countries_regions, left_on="country", right_on="name", how="left")
+
+    # Check that there were no repetition in column names.
+    error = "Repeated columns in combined data."
+    assert len([column for column in set(df_combined.columns) if "_x" in column]) == 0, error
+
+    # Create a table with combined data with no metadata.
+    tb_combined = catalog.Table(df_combined)
+
+    # Select only variables described in the variable mapping file.
+    source_variables = variable_mapping.index.get_level_values(0).tolist()    
+
+    # Gather original metadata for each variable, add the descriptions and sources from the variable mapping file.
+    for source_variable in source_variables:
+        variable_metadata = variable_mapping.loc[source_variable]
+        source_dataset = variable_metadata["source_dataset"]
+        # Check that the variable indeed exists in the original dataset that the variable mapping says.
+        # Ignore columns "country", "year" (assigned to a dummy dataset 'various_datasets'),
+        # and "iso_alpha3" (that comes from countries_regions dataset).
+        if source_dataset not in ["various_datasets", "countries_regions"]:
+            error = f"Variable {source_variable} not found in any of the original datasets."
+            assert source_variable in tables[source_dataset].columns, error
+            tb_combined[source_variable].metadata = tables[source_dataset][source_variable].metadata
+
+        # Update metadata with the content of the variable mapping file.
+        tb_combined[source_variable].metadata.description = variable_metadata["description"]
+        tb_combined[source_variable].metadata.sources = [catalog.meta.Source(name=variable_metadata["source"])]
+
+    # Select only variables in the mapping file, and rename variables according to the mapping.
+    tb_combined = tb_combined[source_variables].rename(columns=variable_mapping.to_dict()["variable"])
+
+    # Set index and sort conveniently.
+    tb_combined = tb_combined.set_index(["country", "year"], verify_integrity=True).sort_index()
+
+    return cast(catalog.Table, tb_combined)
+
+
 def run(dest_dir: str) -> None:
     #
     # Load data.
     #
     # Read all required datasets.
-    ds_statistical_review = catalog.Dataset(STATISTICAL_REVIEW_DATASET_PATH)
     ds_energy_mix = catalog.Dataset(ENERGY_MIX_DATASET_PATH)
     ds_fossil_fuels = catalog.Dataset(FOSSIL_FUEL_PRODUCTION_DATASET_PATH)
     ds_primary_energy = catalog.Dataset(PRIMARY_ENERGY_CONSUMPTION_DATASET_PATH)
     ds_electricity_mix = catalog.Dataset(ELECTRICITY_MIX_DATASET_PATH)
 
-    # Gather all data from all required tables in datasets.
-    df_statistical_review = pd.DataFrame(ds_statistical_review[ds_statistical_review.table_names[0]]).reset_index()
-    df_energy_mix = pd.DataFrame(ds_energy_mix[ds_electricity_mix.table_names[0]]).reset_index()
-    df_fossil_fuels = pd.DataFrame(ds_fossil_fuels[ds_fossil_fuels.table_names[0]]).reset_index()
-    df_primary_energy = pd.DataFrame(ds_primary_energy[ds_primary_energy.table_names[0]]).reset_index()
-    df_electricity_mix = pd.DataFrame(ds_electricity_mix[ds_electricity_mix.table_names[0]]).reset_index()
+    # Gather all required tables from all datasets.
+    tb_energy_mix = ds_energy_mix[ds_energy_mix.table_names[0]].reset_index()
+    tb_fossil_fuels = ds_fossil_fuels[ds_fossil_fuels.table_names[0]].reset_index()
+    tb_primary_energy = ds_primary_energy[ds_primary_energy.table_names[0]].reset_index()
+    tb_electricity_mix = ds_electricity_mix[ds_electricity_mix.table_names[0]].reset_index()
+    
+    # Load countries-regions dataset (required to get ISO codes).
+    countries_regions = catalog.Dataset(DATA_DIR / "garden/reference/")["countries_regions"].\
+        reset_index()[["name", "iso_alpha3"]]
+    
+    # Load mapping from variable names in the component dataset to the final variable name in the output dataset.
+    variable_mapping = pd.read_csv(VARIABLE_MAPPING_FILE).set_index(["source_variable"])
 
     #
     # Process data.
     #
     # Combine all tables.
-    combined = combine_all_data(
-        df_statistical_review=df_statistical_review, df_energy_mix=df_energy_mix, df_fossil_fuels=df_fossil_fuels,
-        df_primary_energy=df_primary_energy, df_electricity_mix=df_electricity_mix)
-
-    # Prepare output table.
-    table = prepare_output_table(combined=combined)
+    tables = {
+        "energy_mix": tb_energy_mix.drop(columns=["country_code"], errors="ignore"),
+        "fossil_fuel_production": tb_fossil_fuels,
+        "primary_energy_consumption": tb_primary_energy.drop(columns=["population", "source"], errors="ignore"),
+        "electricity_mix": tb_electricity_mix.drop(
+            columns=["population", "primary_energy_consumption__twh"], errors="ignore"),
+    }
+    tb_combined = combine_tables_data_and_metadata(
+        tables=tables,
+        countries_regions=countries_regions,
+        variable_mapping=variable_mapping,
+    )
 
     #
     # Save outputs.
     #
     ds_garden = catalog.Dataset.create_empty(dest_dir)
-    # Import metadata from the metadata yaml file.
+    # Gather metadata sources from all tables' original dataset sources.
+    ds_garden.metadata.sources = gather_sources_from_tables(tables=list(tables.values()))
+    # Get the rest of the metadata from the yaml file.
     ds_garden.metadata.update_from_yaml(METADATA_PATH)
     # Create dataset.
     ds_garden.save()
 
+    # Add other metadata fields to table.
+    tb_combined.metadata.short_name = DATASET_SHORT_NAME
+    tb_combined.metadata.title = DATASET_TITLE
+    tb_combined.metadata.dataset = ds_garden.metadata
+
     # Add combined tables to the new dataset.
-    ds_garden.add(table)
+    ds_garden.add(tb_combined)
--- etl/steps/data/garden/energy/2022-08-05/shared.py
+++ etl/steps/data/garden/energy/2022-08-05/shared.py
@@ -1,4 +1,22 @@
 from pathlib import Path
+from typing import List
+
+from owid import catalog
 
 CURRENT_DIR = Path(__file__).parent
 VERSION = CURRENT_DIR.name
+
+
+def gather_sources_from_tables(tables: List[catalog.Table]) -> List[catalog.meta.Source]:
+    # Initialise list that will gather all unique metadata sources from the tables.
+    known_sources = []
+    for table in tables:
+        # Get list of sources of the dataset of current table.
+        table_sources = table.metadata.dataset.sources
+        for source in table_sources:
+            # Go source by source of current table, and check if its name is not already in the list of known sources.
+            if all([source.name != known_source.name for known_source in known_sources]):
+                # Add the new source to the list.
+                known_sources.append(source)
+
+    return known_sources
