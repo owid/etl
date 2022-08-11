ce72e9cbbb4ff85d4da545bf4c521a9fe76a406b
Pablo Rosado <pabloarosado@gmail.com>
2022-08-11 10:54:10 +0200

    refactor(cait): Rename meadow step

--- etl/steps/data/meadow/cait/2022-08-10/ghg_emissions_by_sector.py
+++ etl/steps/data/meadow/cait/2022-08-10/ghg_emissions_by_sector.py
@@ -9,7 +9,7 @@ from owid.walden import Catalog as WaldenCatalog
 from etl.steps.data.converters import convert_walden_metadata
 from shared import NAMESPACE, VERSION
 
-DATASET_SHORT_NAME = "ghg_emissions"
+DATASET_SHORT_NAME = "ghg_emissions_by_sector"
 DATASET_TITLE = "Greenhouse gas emissions by sector"
 WALDEN_SHORT_NAME = "cait_ghg_emissions"
 WALDEN_VERSION = "2022-08-10"
