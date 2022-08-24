382aabdd52b076573adaa9192903e826dfdf6bde
Marigold <mojmir.vinkler@gmail.com>
2022-08-24 13:49:27 +0200

    enhance(grapher_helpers): improve performance of yield_wide_table

--- etl/grapher_helpers.py
+++ etl/grapher_helpers.py
@@ -149,12 +149,11 @@ def yield_wide_table(
                 table_to_yield[column].metadata.unit is not None
             ), f"Unit for column {column} should not be None here!"
 
+            # Select only one column and dimensions for performance
+            tab = table_to_yield[[column]]
+
             # Drop NA values
-            tab = (
-                table_to_yield.dropna(subset=[column])
-                if na_action == "drop"
-                else table_to_yield
-            )
+            tab = tab.dropna() if na_action == "drop" else table_to_yield
 
             # Create underscored name of a new column from the combination of column and dimensions
             short_name = _slugify_column_and_dimensions(column, dims, dim_names)
@@ -179,6 +178,7 @@ def yield_wide_table(
                 title=title_with_dims,
             )
 
+            # Keep only entity_id and year in index
             yield tab.reset_index().set_index(["entity_id", "year"])[[short_name]]
 
 
