f1d6025602d20574bc010d120a7d189ec885dbdd
Marigold <mojmir.vinkler@gmail.com>
2022-09-05 11:37:45 +0200

    fix(makefile): typecheck grapher steps too

--- scripts/typecheck_steps.sh
+++ scripts/typecheck_steps.sh
@@ -7,8 +7,12 @@ all_step_dirs() {
     echo etl/steps/*/*/*/*/
 }
 
+all_grapher_dirs() {
+    echo etl/steps/grapher/*/*/
+}
+
 only_dirs_with_python_files() {
-    for dir in $(all_step_dirs); do
+    for dir in $(all_step_dirs) $(all_grapher_dirs); do
         if find $dir -name '*.py' -print -quit | grep -q . ; then
             echo $dir
         fi
