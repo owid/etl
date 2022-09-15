582934afd0792a69bece9b8f18f69c26ca0c6b64
Pablo Rosado <pabloarosado@gmail.com>
2022-09-15 11:20:21 +0200

    feat(rff): Add sector names using mapping of IPCC codes

--- etl/steps/data/garden/rff/2022-09-14/world_carbon_pricing.py
+++ etl/steps/data/garden/rff/2022-09-14/world_carbon_pricing.py
@@ -17,6 +17,7 @@ COLUMNS = {
     "year": "year",
     "ipcc_code": "ipcc_code",
     "product": "product",
+    "sector_name": "sector_name",
     "tax": "tax",
     "ets": "ets",
     "tax_rate_excl_ex_clcu": "tax_rate_gross",
@@ -87,10 +88,11 @@ def prepare_data(df: pd.DataFrame) -> pd.DataFrame:
     df["tax"] = df["tax"].replace(0, np.nan)
     df["ets"] = df["ets"].replace(0, np.nan)
 
-    # Remove rows where all data columns are nan.
-    df = df.dropna(subset=[column for column in df.columns if column not in INDEX_COLUMNS], how="all").reset_index(
-        drop=True
-    )
+    # Remove rows where all data columns are nan (ignore index columns and sector names).
+    columns_that_must_have_data = [column for column in df.columns
+                                   if column not in INDEX_COLUMNS if column != "sector_name"]
+    assert set(columns_that_must_have_data) < set(df.columns)
+    df = df.dropna(subset=columns_that_must_have_data, how="all").reset_index(drop=True)
 
     # Columns 'tax' and 'ets' were converted to float (because we introduced nans).
     # Now that nans have been removed, make them integer again.
--- etl/steps/data/meadow/rff/2022-09-14/world_carbon_pricing.py
+++ etl/steps/data/meadow/rff/2022-09-14/world_carbon_pricing.py
@@ -10,6 +10,12 @@ VERSION = "2022-09-14"
 # Version of dataset in walden.
 WALDEN_VERSION = "2022-09-14"
 
+# Columns to select and rename in ipcc column names.
+IPCC_COLUMNS = {
+    "IPCC_CODE": "ipcc_code",
+    "FULLNAME": "sector_name",
+}
+
 
 def run(dest_dir: str) -> None:
     #
@@ -17,8 +23,21 @@ def run(dest_dir: str) -> None:
     #
     # Load data from walden.
     walden_ds = WaldenCatalog().find_one(namespace="rff", short_name="world_carbon_pricing", version=WALDEN_VERSION)
-    local_file = walden_ds.ensure_downloaded()
-    df = pd.read_csv(local_file, dtype=object)
+    df = pd.read_csv(walden_ds.ensure_downloaded(), dtype=object)
+    # Load IPCC codes from walden.
+    walden_ipcc_ds = WaldenCatalog().find_one(namespace="rff", short_name="ipcc_codes", version=WALDEN_VERSION)
+    ipcc_codes = pd.read_csv(walden_ipcc_ds.ensure_downloaded(), dtype=object)
+
+    #
+    # Process data.
+    #
+    # Prepare IPCC codes dataframe.
+    ipcc_codes = ipcc_codes[list(IPCC_COLUMNS)].rename(columns=IPCC_COLUMNS)
+    # Sanity check.
+    error = "IPCC codes found in data that are missing in IPCC codes file."
+    assert set(df["ipcc_code"]) <= set(ipcc_codes["ipcc_code"]), error
+    # Add sector names to data, mapping IPCC codes.
+    df = pd.merge(df, ipcc_codes, on="ipcc_code", how="left")
 
     #
     # Save outputs.
@@ -27,14 +46,11 @@ def run(dest_dir: str) -> None:
     ds = Dataset.create_empty(dest_dir)
     ds.metadata = convert_walden_metadata(walden_ds)
     ds.metadata.version = VERSION
-
     # Create new table with metadata.
     table_metadata = TableMeta(short_name=walden_ds.short_name, title=walden_ds.name, description=walden_ds.description)
     tb = Table(df, metadata=table_metadata)
-
     # Prepare table and add it to the dataset.
     tb = underscore_table(tb)
     ds.add(tb)
-
     # Save the dataset.
     ds.save()
