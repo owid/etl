dd34f688cda947a8129e534dfb9540391cf377fe
Fiona Spooner <fiona@ourworldindata.org>
2022-09-16 17:01:05 +0100

    Update gbd_cause.py

--- etl/steps/data/meadow/ihme_gbd/2019/gbd_cause.py
+++ etl/steps/data/meadow/ihme_gbd/2019/gbd_cause.py
@@ -8,8 +8,6 @@ log = get_logger()
 
 # naming conventions
 N = Names(__file__)
-N = Names('/Users/fionaspooner/Documents/OWID/repos/etl/etl/steps/data/meadow/ihme_gbd/2019/gbd_cause.py')
-
 def run(dest_dir: str) -> None:
     dataset = N.short_name
     metadata_path = N.metadata_path
