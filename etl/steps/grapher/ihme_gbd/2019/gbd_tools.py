550082b9fd71bdf08675f86e92b5faf0d8373479
spoonerf <spoonerfiona@gmail.com>
2022-09-16 10:26:08 +0100

    refactor(gbd): function to grab variables used in charts

--- etl/steps/grapher/ihme_gbd/2019/gbd_tools.py
+++ etl/steps/grapher/ihme_gbd/2019/gbd_tools.py
@@ -89,28 +89,29 @@ def run_wrapper(garden_dataset: Dataset, dataset: Dataset, old_dataset_name: str
         df = garden_dataset[table]
         df = create_var_name(df)
         # Get variables used in existing charts
-        df = df[df['variable'].isin(variables_in_charts)]
-        if df.shape[0] >0:
-        df["source"] = "Institute for Health Metrics and Evaluation - Global Burden of Disease (2019)"
+        df = df[df["variable"].isin(variables_in_charts)]
 
-        var_gr = df.groupby("variable")
+        if df.shape[0] > 0:
+            df["source"] = "Institute for Health Metrics and Evaluation - Global Burden of Disease (2019)"
 
-        for var_name, df_var in var_gr:
-            df_tab = add_metadata_and_prepare_for_grapher(df_var, garden_dataset)
-            df_tab.metadata.dataset = dataset.metadata
+            var_gr = df.groupby("variable")
 
-            # NOTE: long format is quite inefficient, we're creating a table for every variable
-            # converting it to wide format would be too sparse, but we could move dimensions from
-            # variable names to proper dimensions
-            # currently we generate ~10000 files with total size 73MB (grapher step runs in 692s
-            # and both reindex and publishing is fast, so this is not a real bottleneck besides
-            # polluting `grapher` channel in our catalog)
-            # see https://github.com/owid/etl/issues/447
-            for wide_table in gh.long_to_wide_tables(df_tab):
-                # table is generated for every column, use it as a table name
-                # shorten it under 255 characteres as this is the limit for file name
-                wide_table.metadata.short_name = wide_table.columns[0]
-                dataset.add(wide_table)
+            for var_name, df_var in var_gr:
+                df_tab = add_metadata_and_prepare_for_grapher(df_var, garden_dataset)
+                df_tab.metadata.dataset = dataset.metadata
+
+                # NOTE: long format is quite inefficient, we're creating a table for every variable
+                # converting it to wide format would be too sparse, but we could move dimensions from
+                # variable names to proper dimensions
+                # currently we generate ~10000 files with total size 73MB (grapher step runs in 692s
+                # and both reindex and publishing is fast, so this is not a real bottleneck besides
+                # polluting `grapher` channel in our catalog)
+                # see https://github.com/owid/etl/issues/447
+                for wide_table in gh.long_to_wide_tables(df_tab):
+                    # table is generated for every column, use it as a table name
+                    # shorten it under 255 characteres as this is the limit for file name
+                    wide_table.metadata.short_name = wide_table.columns[0]
+                    dataset.add(wide_table)
 
 
 def get_variables_used_in_charts(old_dataset_name: str) -> List:
