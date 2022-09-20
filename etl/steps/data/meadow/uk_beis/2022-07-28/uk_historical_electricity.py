fa2920aee5d0bd7b6558549921946b98040890b6
Pablo Rosado <pabloarosado@gmail.com>
2022-09-20 14:42:35 +0200

    feat(uk_beis): Add meadow step on historical UK electricity

---
+++ etl/steps/data/meadow/uk_beis/2022-07-28/uk_historical_electricity.py
@@ -0,0 +1,132 @@
+from typing import Dict, List
+
+import pandas as pd
+from owid.catalog import Dataset, Table, TableMeta
+from owid.catalog.utils import underscore_table
+from owid.walden import Catalog as WaldenCatalog
+
+from etl.helpers import Names
+from etl.steps.data.converters import convert_walden_metadata
+
+# Naming conventions.
+N = Names(__file__)
+
+
+def prepare_data(df: pd.DataFrame, expected_content: Dict[int, List[str]], columns: Dict[int, str]) -> pd.DataFrame:
+    df = df.copy()
+
+    # Check that certain words are contained in specific columns, to ensure that they contain the data we expect.
+    for column in expected_content:
+        expected_elements = expected_content[column]
+        for element in expected_elements:
+            error = f"Excel file may have changed structure (expected {element} in column {column})."
+            assert df[df.columns[column]].str.contains(element, regex=False).any(), error
+
+    # Select columns and how to rename them.
+    df = df[df.columns[list(columns)]].rename(columns={df.columns[i]: columns[i] for i in columns})
+
+    # Remove all rows for which the year column does not start with an integer of 4 digits.
+    df = df[df["year"].astype(str).str.contains(r"^\d{4}", regex=True, na=False)].reset_index(drop=True)
+    # Remove annotations from years (e.g. replace "1987 (5)" by 1987).
+    df["year"] = df["year"].astype(str).str[0:4].astype(int)
+
+    # Make all columns float (except year column).
+    df.astype({column: float for column in df.columns if column != "year"})
+
+    return df
+
+
+def run(dest_dir: str) -> None:
+    #
+    # Load data.
+    #
+    # Retrieve raw data from walden.
+    walden_ds = WaldenCatalog().find_one(
+        namespace="uk_beis", short_name="uk_historical_electricity", version="2022-07-28"
+    )
+    local_file = walden_ds.ensure_downloaded()
+
+    # Load data from the two relevant sheets of the excel file.
+    # The original excel file is poorly formatted and will be hard to parse automatically.
+    fuel_input = pd.read_excel(local_file, sheet_name="Fuel input")
+    supply = pd.read_excel(local_file, sheet_name="Supply, availability & consump")
+
+    #
+    # Process data.
+    #
+    # Process data from the fuels input sheet.
+    fuel_input = prepare_data(
+        df=fuel_input,
+        expected_content={
+            1: ["Total", "all", "fuels"],
+            2: ["Coal"],
+            3: ["Oil"],
+            4: ["Natural", "gas"],
+            5: ["Nuclear"],
+            6: ["Natural", "flow hydro"],
+            7: ["Wind", "and solar"],
+            9: ["Other", "fuels"],
+        },
+        columns={
+            0: "year",
+            1: "all_sources",
+            2: "coal",
+            3: "oil",
+            4: "gas",
+            5: "nuclear",
+            6: "hydro",
+            7: "wind_and_solar",
+            9: "other",
+        },
+    )
+
+    # Prepare data from the supply sheet.
+    supply = prepare_data(
+        df=supply,
+        expected_content={
+            1: ["Electricity", "supplied"],
+            3: ["Net", "Imports"],
+        },
+        columns={
+            0: "year",
+            1: "electricity_generation",
+            3: "net_imports",
+        },
+    )
+
+    #
+    # Save outputs.
+    #
+    # Create new dataset and reuse walden metadata.
+    ds = Dataset.create_empty(dest_dir)
+    ds.metadata = convert_walden_metadata(walden_ds)
+    ds.metadata.version = "2022-07-28"
+
+    # Create tables using metadata from walden.
+    tb_fuel_input = Table(
+        fuel_input,
+        metadata=TableMeta(
+            short_name="fuel_input",
+            title="Fuel input for electricity generation",
+            description=walden_ds.description,
+        ),
+    )
+    tb_supply = Table(
+        supply,
+        metadata=TableMeta(
+            short_name="supply",
+            title="Electricity supply, availability and consumption",
+            description=walden_ds.description,
+        ),
+    )
+
+    # Underscore all table columns.
+    tb_fuel_input = underscore_table(tb_fuel_input)
+    tb_supply = underscore_table(tb_supply)
+
+    # Add tables to a dataset.
+    ds.add(tb_fuel_input)
+    ds.add(tb_supply)
+
+    # Save the dataset.
+    ds.save()
