e7741347dfb9a72de41df780b6795fc7383946dd
Pablo Rosado <pabloarosado@gmail.com>
2022-09-28 15:14:56 +0200

    enhance(andrew): Rename country column in grapher step

--- etl/steps/grapher/andrew/2019-12-03/co2_mitigation_curves.py
+++ etl/steps/grapher/andrew/2019-12-03/co2_mitigation_curves.py
@@ -13,10 +13,10 @@ def run(dest_dir: str) -> None:
     # Load tables from dataset and change the "origin" column to act as if it was the country name.
     # This is a workaround to be able to visualize all curves of origin together in a line chart.
     table_1p5celsius = (
-        N.garden_dataset["co2_mitigation_curves_1p5celsius"].reset_index().rename(columns={"origin": "country"})
+        N.garden_dataset["co2_mitigation_curves_1p5celsius"].reset_index().rename(columns={"origin": "entity"})
     )
     table_2celsius = (
-        N.garden_dataset["co2_mitigation_curves_2celsius"].reset_index().rename(columns={"origin": "country"})
+        N.garden_dataset["co2_mitigation_curves_2celsius"].reset_index().rename(columns={"origin": "entity"})
     )
 
     # Add tables to grapher dataset and save it.
