ef5b2395b6f028dd3e54e570709652a9eb5109d8
Lars Yencken <lars@yencken.org>
2022-09-30 13:49:19 +0200

    fix: make --ipdb work again for processing steps

--- etl/steps/__init__.py
+++ etl/steps/__init__.py
@@ -377,15 +377,14 @@ class DataStep(Step):
         """
         # use a subprocess to isolate each step from the others, and avoid state bleeding
         # between them
-        args = [
-            "poetry",
-            "run",
-            "run_python_step",
+        args = ["poetry", "run", "run_python_step"]
+        if config.IPDB_ENABLED:
+            args.append("--ipdb")
+
+        args.extend([
             str(self),
             self._dest_dir.as_posix(),
-        ]
-        if config.IPDB_ENABLED:
-            args[1:1] = ["--ipdb"]
+        ])
 
         subprocess.check_call(args)
 
