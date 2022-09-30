cb88825bbf05839d75ad0b7aad32ec6deef0b8f1
Pablo Rosado <pabloarosado@gmail.com>
2022-09-30 12:55:41 +0200

    feat(gcp): Add more variables to the GCB dataset

--- etl/steps/data/meadow/gcp/2022-09-29/global_carbon_budget_additional.py
+++ etl/steps/data/meadow/gcp/2022-09-29/global_carbon_budget_additional.py
@@ -18,7 +18,8 @@ from etl.steps.data.converters import convert_walden_metadata
 CARBON_TO_CO2 = 3.664
 
 # Details of dataset(s) to be imported.
-WALDEN_DATASET_NAME = "global_carbon_budget_global"
+WALDEN_GLOBAL_DATASET_NAME = "global_carbon_budget_global"
+WALDEN_NATIONAL_DATASET_NAME = "global_carbon_budget_national"
 WALDEN_VERSION = "2022-09-29"
 # Details of dataset to be exported.
 MEADOW_VERSION = "2022-09-29"
@@ -26,49 +27,96 @@ MEADOW_DATASET_NAME = "global_carbon_budget_additional"
 MEADOW_TITLE = "Global Carbon Budget - Additional variables"
 
 
-def run(dest_dir: str) -> None:
-    #
-    # Load data.
-    #
-    # Load raw data from walden.
-    walden_ds = WaldenCatalog().find_one(namespace="gcp", short_name=WALDEN_DATASET_NAME, version=WALDEN_VERSION)
-    local_file = walden_ds.ensure_downloaded()
-    
-    # For now, load only the historical budget sheet.
-    df = pd.read_excel(local_file, sheet_name="Historical Budget", skiprows=15)
-    assert df.columns[0] == "Year", "The structure of the file has changed (consider changing skiprows)."
-
-    #
-    # Process data.
-    #
-    # Columns to select and how to rename them.
+def prepare_historical_budget(df: pd.DataFrame) -> pd.DataFrame:
+    # Columns to select in historical budget and how to rename them.
     columns = {
         "Year": "year",
-        "fossil emissions excluding carbonation": 'fossil emissions excluding carbonation',
-        "land-use change emissions": "land-use change emissions",
+        "fossil emissions excluding carbonation": 'global_fossil_emissions',
+        "land-use change emissions": "global_land_use_change_emissions",
     }
     df = df[list(columns)].rename(columns=columns)
     # Convert units from gigatonnes of carbon per year emissions to gigatonnes of CO2 per year.
     for column in df.drop(columns="year").columns:
         df[column] *= CARBON_TO_CO2
+    # Add column for country (to be able to combine this with the national data).
+    df["country"] = "World"
+
+    # Set an index and sort row and columns conveniently.
+    df = df.set_index(["country", "year"], verify_integrity=True).sort_index().sort_index(axis=1)
+
+    return df
+
+
+def prepare_emissions(df: pd.DataFrame, column_name: str) -> pd.DataFrame:
+    df = df.copy()
+    # The zeroth column is expected to be year.
+    df = df.rename(columns={df.columns[0]: "year"})
+    # Each column represents a country; then the final columns are regions, "Bunkers", and "Statistical Difference".
+    # Keep "Bunkers" as a separate column, but remove "Statistical Difference" (which is almost completely empty).
+    df = df.drop(columns="Statistical Difference")
+    df = df.melt(id_vars=["year", "Bunkers"]).rename(
+        columns={"variable": "country", "value": column_name, "Bunkers": "bunker_emissions"})
+
+    # Set an index and sort row and columns conveniently.
+    df = df.set_index(["country", "year"], verify_integrity=True).sort_index().sort_index(axis=1)
+
+    return df
+
+
+def run(dest_dir: str) -> None:
+    #
+    # Load data.
+    #
+    # Load historical budget data from the global data file in walden.
+    global_ds = WaldenCatalog().find_one(namespace="gcp", short_name=WALDEN_GLOBAL_DATASET_NAME, version=WALDEN_VERSION)
+    historical_budget_df = pd.read_excel(global_ds.ensure_downloaded(), sheet_name="Historical Budget", skiprows=15)
+    error = "Structure of 'Historical Budget' sheet in global data file has changed (consider changing 'skiprows')."
+    assert historical_budget_df.columns[0] == "Year", error
+
+    # Load national data file from walden.
+    national_ds = WaldenCatalog().find_one(namespace="gcp", short_name=WALDEN_NATIONAL_DATASET_NAME, version=WALDEN_VERSION)
+    # Load production-based emissions from the national data file.
+    production_emissions_df = pd.read_excel(
+        national_ds.ensure_downloaded(), sheet_name="Territorial Emissions", skiprows=11)
+    error = "Structure of 'Territorial Emissions' sheet in national data file has changed (consider changing 'skiprows')."
+    assert production_emissions_df.columns[1] == "Afghanistan", error
+    # Load consumption-based emissions from the national data file.
+    consumption_emissions_df = pd.read_excel(
+        national_ds.ensure_downloaded(), sheet_name="Consumption Emissions", skiprows=8)
+    error = "Structure of 'Consumption Emissions' sheet in national data file has changed (consider changing 'skiprows')."
+    assert consumption_emissions_df.columns[1] == "Afghanistan", error
+
+    #
+    # Process data.
+    #
+    # Prepare historical budget data.
+    historical_budget_df = prepare_historical_budget(df=historical_budget_df)
+
+    # Prepare production and consumption based emissions data.
+    production_emissions_df = prepare_emissions(df=production_emissions_df, column_name="production_emissions")
+    consumption_emissions_df = prepare_emissions(df=consumption_emissions_df, column_name="consumption_emissions")
 
     #
     # Save outputs.
     #
-    # Create new dataset and reuse walden metadata.
+    # Create new dataset and reuse walden metadata (from any of the raw files).
     ds = Dataset.create_empty(dest_dir)
-    ds.metadata = convert_walden_metadata(walden_ds)
+    ds.metadata = convert_walden_metadata(global_ds)
     ds.metadata.version = MEADOW_VERSION
-    # Create table with metadata from dataframe.
-    table_metadata = TableMeta(
-        short_name=MEADOW_DATASET_NAME,
-        title=MEADOW_TITLE,
-        description=walden_ds.description,
-    )
-    tb = Table(df, metadata=table_metadata)
+    # Create tables with metadata.
+    consumption_emissions_tb = Table(consumption_emissions_df, metadata=TableMeta(
+        short_name="consumption_emissions", title="Consumption-based emissions"))
+    production_emissions_tb = Table(production_emissions_df, metadata=TableMeta(
+        short_name="production_emissions", title="Production-based emissions"))
+    historical_budget_tb = Table(historical_budget_df, metadata=TableMeta(
+        short_name="historical_emissions", title="Historical emissions"))
     # Ensure all columns are lower snake case.
-    tb = underscore_table(tb)
-    # Add table to new dataset.
-    ds.add(tb)
+    consumption_emissions_tb = underscore_table(consumption_emissions_tb)
+    production_emissions_tb = underscore_table(production_emissions_tb)
+    historical_budget_tb = underscore_table(historical_budget_tb)
+    # Add tables to new dataset.
+    ds.add(consumption_emissions_tb)
+    ds.add(production_emissions_tb)
+    ds.add(historical_budget_tb)
     # Save dataset.
     ds.save()
