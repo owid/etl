7ba99ed16bc11e61159e0d74e58092e4ec65a6de
Pablo Rosado <pabloarosado@gmail.com>
2022-10-12 15:52:33 +0200

    style(aviation_safety_network): Improve format

--- etl/steps/data/meadow/aviation_safety_network/2022-10-12/aviation_statistics.py
+++ etl/steps/data/meadow/aviation_safety_network/2022-10-12/aviation_statistics.py
@@ -1,11 +1,12 @@
+"""Generate a dataset of aviation statistics by combining the statistics "by period" and "by nature" of the Aviation
+Safety Network.
+
+"""
 import pandas as pd
 from owid.catalog import Dataset, Table, TableMeta
 from owid.catalog.utils import underscore_table
 from owid.walden import Catalog as WaldenCatalog
-from structlog import get_logger
 
-from etl.helpers import Names
-from etl.paths import DATA_DIR, REFERENCE_DATASET
 from etl.steps.data.converters import convert_walden_metadata
 
 NAMESPACE = "aviation_safety_network"
@@ -21,10 +22,12 @@ MEADOW_DATASET_TITLE = "Aviation statistics"
 
 def run(dest_dir: str) -> None:
     # Get data (statistics by period and by nature) from Walden.
-    walden_ds_by_period = WaldenCatalog().find_one(namespace=NAMESPACE, short_name=WALDEN_DATASET_NAME_BY_PERIOD,
-                                         version=WALDEN_VERSION)
-    walden_ds_by_nature = WaldenCatalog().find_one(namespace=NAMESPACE, short_name=WALDEN_DATASET_NAME_BY_NATURE,
-                                     version=WALDEN_VERSION)
+    walden_ds_by_period = WaldenCatalog().find_one(
+        namespace=NAMESPACE, short_name=WALDEN_DATASET_NAME_BY_PERIOD, version=WALDEN_VERSION
+    )
+    walden_ds_by_nature = WaldenCatalog().find_one(
+        namespace=NAMESPACE, short_name=WALDEN_DATASET_NAME_BY_NATURE, version=WALDEN_VERSION
+    )
     local_file_by_period = walden_ds_by_period.ensure_downloaded()
     local_file_by_nature = walden_ds_by_nature.ensure_downloaded()
 
@@ -49,7 +52,7 @@ def run(dest_dir: str) -> None:
     tb = Table(df_combined, metadata=table_metadata)
 
     # Ensure all columns are snake-case and underscore.
-    tb = underscore_table(tb)    
+    tb = underscore_table(tb)
 
     # Add table to new Meadow dataset.
     ds.add(tb)
