52ae28ea44a767a646842c2454c1f254898d676e
spoonerf <spoonerfiona@gmail.com>
2022-10-18 15:59:59 +0100

    fix(malnu): typo in file name

--- etl/steps/data/garden/malnutrition/2022-10-18/malnutrition.py
+++ etl/steps/data/garden/malnutrition/2022-10-18/malnutrition.py
@@ -1,10 +1,5 @@
-"""Combine WDI's prevalence of stunting, wasting and underweight in children under five. 
-Convert this into absolute numbers using UNWPP population data. 
-
+"""Combine WDI's prevalence of stunting, wasting and underweight in children under five. Convert this into absolute numbers using UNWPP population data.
 """
-
-import numpy as np
-import pandas as pd
 from owid import catalog
 from shared import CURRENT_DIR
 
@@ -26,10 +21,11 @@ def run(dest_dir: str) -> None:
     ds_unwpp = catalog.Dataset(UNWPP_DATASET_PATH)
     ds_wdi = catalog.Dataset(WDI_DATASET_PATH)
 
-    # columns of required indicators - stunting, underweight and wasting prevalence
+    # Columns of required indicators - stunting, underweight and wasting prevalence
     cols = ["sh_sta_stnt_me_zs", "sh_sta_maln_zs", "sh_sta_wast_zs"]
     new_cols = ["number_of_stunted_children", "number_of_underweight_children", "number_of_wasted_children"]
     base_cols = ["country", "year"]
+
     df_wdi = ds_wdi["wdi"][cols].dropna().reset_index()
     df_wdi[cols] = (df_wdi[cols].astype(float).round(2)) / 100
 
@@ -48,9 +44,15 @@ def run(dest_dir: str) -> None:
     df_both[new_cols] = df_both[cols].multiply(df_both["value"], axis="index").astype(int).round(0)
     base_cols.extend(new_cols)
     df_num = df_both[base_cols]
+    tb_num = catalog.Table(df_num)
+    # Add other metadata fields to table.
+    tb_num.metadata.short_name = DATASET_SHORT_NAME
+    tb_num.metadata.title = DATASET_TITLE
+    tb_num.update_metadata_from_yaml(METADATA_PATH, DATASET_SHORT_NAME)
 
     ds_garden = catalog.Dataset.create_empty(dest_dir)
     # Get the rest of the metadata from the yaml file.
-    ds_garden.metadata.update_from_yaml(METADATA_PATH, if_source_exists="replace")
+    ds_garden.metadata.update_from_yaml(METADATA_PATH)
     # Create dataset.
     ds_garden.save()
+    ds_garden.add(tb_num)
