8f6d1936e280864f485abc58d99347b2fba68b8a
Pablo Rosado <pabloarosado@gmail.com>
2022-10-18 13:56:59 +0200

    feat(aviation_safety_network): Add grapher step

---
+++ etl/steps/grapher/aviation_safety_network/2022-10-14/aviation_statistics.py
@@ -0,0 +1,12 @@
+from owid import catalog
+
+from etl.helpers import Names
+
+N = Names(__file__)
+
+
+def run(dest_dir: str) -> None:
+    dataset = catalog.Dataset.create_empty(dest_dir, N.garden_dataset.metadata)
+    table = N.garden_dataset["aviation_statistics"]
+    dataset.add(table)
+    dataset.save()
