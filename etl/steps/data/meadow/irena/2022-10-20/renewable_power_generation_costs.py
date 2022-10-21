f365f9da791433c9cdf383b58c74fe3c11aa7d32
Pablo Rosado <pabloarosado@gmail.com>
2022-10-21 16:13:07 +0200

    feat(irena): Extract LCOE cost from all energy sources

--- etl/steps/data/meadow/irena/2022-10-20/renewable_power_generation_costs.py
+++ etl/steps/data/meadow/irena/2022-10-20/renewable_power_generation_costs.py
@@ -1,8 +1,7 @@
-"""Extract data from IRENA's Renewable Power Generation Costs 2022 dataset.
-
-For the moment, we only extract PV costs.
+"""Extract weighted average LCOE for all energy sources from IRENA's Renewable Power Generation Costs 2022 dataset.
 
 """
+
 from typing import cast
 
 import pandas as pd
@@ -21,58 +20,115 @@ VERSION = "2022-10-20"
 # Get naming conventions.
 N = Names(str(CURRENT_DIR / "renewable_power_generation_costs"))
 
-# Photovoltaic technologies to choose for average monthly prices.
-PV_TECHNOLOGIES = ["Thin film a-Si/u-Si or Global Index (from Q4 2013)"]
 
+# +
+# It's unclear if this data will be used. If so, it could be a separate table.
+# def prepare_pv_data(data_file: str) -> pd.DataFrame:
+#     """Prepare yearly data on solar photovoltaic costs.
 
-def prepare_pv_data(data_file: str) -> pd.DataFrame:
-    """Prepare yearly data on solar photovoltaic costs.
+#     Monthly data will be averaged, and only complete years (with 12 informed months) will be considered.
 
-    Monthly data will be averaged, and only complete years (with 12 informed months) will be considered.
+#     Parameters
+#     ----------
+#     data_file : str
+#         Path to raw data (IRENA's excel file on renewable power generation costs).
 
-    Parameters
-    ----------
-    data_file : str
-        Path to raw data (IRENA's excel file on renewable power generation costs).
+#     Returns
+#     -------
+#     pv_prices : pd.DataFrame
+#         PV prices.
 
-    Returns
-    -------
-    pv_prices : pd.DataFrame
-        PV prices.
+#     """
+#     # Photovoltaic technologies to choose for average monthly prices.
+#     pv_technologies = ["Thin film a-Si/u-Si or Global Index (from Q4 2013)"]
+#     # Load upper table in sheet from Figure 3.2, which is:
+#     # Average monthly solar PV module prices by technology and manufacturing country sold in Europe, 2010 to 2021.
+#     pv_prices = pd.read_excel(
+#         data_file, sheet_name="Fig 3.2", skiprows=4, skipfooter=18, usecols=lambda column: "Unnamed" not in column
+#     )
 
-    """
-    # Load upper table in sheet from Figure 3.2, which is:
-    # Average monthly solar PV module prices by technology and manufacturing country sold in Europe, 2010 to 2021.
-    pv_prices = pd.read_excel(
-        data_file, sheet_name="Fig 3.2", skiprows=4, skipfooter=18, usecols=lambda column: "Unnamed" not in column
-    )
+#     # Transpose dataframe so that each row corresponds to a month.
+#     pv_prices = pv_prices.rename(columns={"2021 USD/W": "technology"}).melt(
+#         id_vars="technology", var_name="month", value_name="cost"
+#     )
 
-    # Transpose dataframe so that each row corresponds to a month.
-    pv_prices = pv_prices.rename(columns={"2021 USD/W": "technology"}).melt(
-        id_vars="technology", var_name="month", value_name="cost"
-    )
+#     # Select PV technologies.
+#     pv_prices = pv_prices[pv_prices["technology"].isin(pv_technologies)].reset_index(drop=True)
 
-    # Select PV technologies.
-    pv_prices = pv_prices[pv_prices["technology"].isin(PV_TECHNOLOGIES)].reset_index(drop=True)
+#     # Get year from dates.
+#     pv_prices["year"] = pd.to_datetime(pv_prices["month"], format="%b %y").dt.year
 
-    # Get year from dates.
-    pv_prices["year"] = pd.to_datetime(pv_prices["month"], format="%b %y").dt.year
+#     # For each year get the average cost over all months.
+#     pv_prices = (
+#         pv_prices.groupby(["technology", "year"])
+#         .agg({"cost": "mean", "year": "count"})
+#         .rename(columns={"year": "n_months"})
+#         .reset_index()
+#     )
 
-    # For each year get the average cost over all months.
-    pv_prices = (
-        pv_prices.groupby(["technology", "year"])
-        .agg({"cost": "mean", "year": "count"})
-        .rename(columns={"year": "n_months"})
-        .reset_index()
-    )
+#     # Ignore years for which we don't have 12 months.
+#     pv_prices = pv_prices[pv_prices["n_months"] == 12].drop(columns=["n_months"]).reset_index(drop=True)
 
-    # Ignore years for which we don't have 12 months.
-    pv_prices = pv_prices[pv_prices["n_months"] == 12].drop(columns=["n_months"]).reset_index(drop=True)
+#     # Set an appropriate index and sort conveniently.
+#     pv_prices = pv_prices.set_index(["technology", "year"], verify_integrity=True).sort_index().sort_index(axis=1)
+
+#     return cast(pd.DataFrame, pv_prices)
+# -
+
+def extract_cost_for_all_sources_from_excel_file(local_file: str) -> pd.DataFrame:
+    # Load file as an excel object.
+    excel_object = pd.ExcelFile(local_file)
+
+    # Extract weighted average LCOE for different sources:
+
+    # Solar photovoltaic.
+    solar_pv = excel_object.parse("Fig 3.1", skiprows=22).dropna(how="all", axis=1).\
+        rename(columns={"Unnamed: 1": "temp"})
+    solar_pv = solar_pv[solar_pv["temp"] == "Weighted average"].melt(id_vars="temp", var_name="year", value_name="cost")[["year", "cost"]]
+    solar_pv["technology"] = "Solar photovoltaic"
+
+    # Onshore wind.
+    onshore_wind = excel_object.\
+        parse("Fig 2.12", skiprows=3, usecols=lambda column: "Unnamed" not in column).\
+        rename(columns={"Year": "year", "Weighted average": "cost"})
+    onshore_wind["technology"] = "Onshore wind"
+
+    # Concentrated solar power.
+    csp = excel_object.parse("Fig 5.7", skiprows=4).dropna(how="all", axis=1)
+    csp = csp[csp["2021 USD/kWh"]=="Weighted average"].melt(id_vars="2021 USD/kWh", var_name="year", value_name="cost")[["year", "cost"]].reset_index(drop=True)
+    csp["technology"] = "Concentrated solar power"
+
+    # Offshore wind.
+    offshore_wind = excel_object.parse("Fig 4.13", skiprows=3).\
+        rename(columns={"Year": "year", "Weighted average": "cost"})[["year", "cost"]]
+    offshore_wind["technology"] = "Offshore wind"
+
+    # Geothermal.
+    geothermal = excel_object.parse("Fig 7.4", skiprows=5).\
+        rename(columns={"Year": "year", "Weighted average": "cost"})[["year", "cost"]]
+    geothermal["technology"] = "Geothermal"
+
+    # Bioenergy.
+    bioenergy = excel_object.parse("Fig 8.1", skiprows=20).dropna(axis=1, how="all").\
+        rename(columns={"Unnamed: 1": "temp"})
+    bioenergy = bioenergy[bioenergy["temp"] =="Weighted average"].\
+        melt(id_vars="temp", var_name="year", value_name="cost")[["year", "cost"]]
+    bioenergy["technology"] = "Bioenergy"
+
+    # Hydropower.
+    hydropower = excel_object.parse("Fig 6.1", skiprows=20).dropna(how="all", axis=1).\
+        rename(columns={"Unnamed: 1": "temp"})
+    hydropower = hydropower[hydropower["temp"] == "Weighted average"].\
+        melt(id_vars="temp", var_name="year", value_name="cost")[["year", "cost"]]
+    hydropower["technology"] = "Hydropower"
+
+    # Concatenate all sources into one dataframe.
+    df = pd.concat([solar_pv, onshore_wind, csp, offshore_wind, geothermal, bioenergy, hydropower], ignore_index=True)
 
     # Set an appropriate index and sort conveniently.
-    pv_prices = pv_prices.set_index(["technology", "year"], verify_integrity=True).sort_index().sort_index(axis=1)
+    df = df.set_index(["technology", "year"], verify_integrity=True).sort_index().sort_index(axis=1)
 
-    return cast(pd.DataFrame, pv_prices)
+    return df
 
 
 def run(dest_dir: str) -> None:
@@ -81,9 +137,9 @@ def run(dest_dir: str) -> None:
         namespace="irena", short_name="renewable_power_generation_costs", version=WALDEN_VERSION
     )
     local_file = walden_ds.ensure_downloaded()
-
-    # Load and prepare data on photovoltaic costs.
-    df = prepare_pv_data(data_file=local_file)
+    
+    # Extract weighted average LCOE cost for all energy sources.
+    df = extract_cost_for_all_sources_from_excel_file(local_file=local_file)
 
     # Create a new Meadow dataset and reuse walden metadata.
     ds = Dataset.create_empty(dest_dir)
