fae44e49a403fd4806b67b9cf1f6739adae5c3a4
Pablo Rosado <pabloarosado@gmail.com>
2022-10-26 11:14:49 +0200

    refactor(irena): Adapt grapher step to new changes

--- etl/steps/grapher/irena/2022-10-25/renewable_energy_patents.py
+++ etl/steps/grapher/irena/2022-10-25/renewable_energy_patents.py
@@ -8,8 +8,8 @@ N = Names(__file__)
 def run(dest_dir: str) -> None:
     # Create new Grapher dataset using metadata from garden.
     dataset = catalog.Dataset.create_empty(dest_dir, N.garden_dataset.metadata)
-    # Load main table from Garden dataset.
-    table = N.garden_dataset[N.garden_dataset.table_names[0]]
+    # Load simplified table from Garden dataset.
+    table = N.garden_dataset["renewable_energy_patents_by_technology"]
     # Add table to dataset and save dataset.
     dataset.add(table)
     dataset.save()
