a82a9c1690e6d8a60fd42c3ad34614f86d30c2c3
Marigold <mojmir.vinkler@gmail.com>
2022-11-25 14:23:47 +0100

    wip

--- snapshots/ggdc/2020-10-01/ggdc_maddison.py
+++ snapshots/ggdc/2020-10-01/ggdc_maddison.py
@@ -16,7 +16,6 @@ from etl.snapshot import Snapshot
 )
 def main(upload: bool) -> None:
     snap = Snapshot("ggdc/2020-10-01/ggdc_maddison.xlsx")
-    __import__("ipdb").set_trace()
     snap.download_from_source()
     snap.dvc_add(upload=upload)
 
