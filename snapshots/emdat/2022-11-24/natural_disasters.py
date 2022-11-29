4c9dbf66b847248331468b534f23569d350e811f
Pablo Rosado <pabloarosado@gmail.com>
2022-11-29 16:19:02 +0100

    refactor(emdat): Adapt ingest scripts to new structure

--- snapshots/emdat/2022-11-24/natural_disasters.py
+++ snapshots/emdat/2022-11-24/natural_disasters.py
@@ -7,30 +7,36 @@ Before running this script, the data needs to be downloaded:
   * All regions in "Location".
   * All years.
 * Then click on "Download".
-* Move the file to the current directory (where this file is located) and rename it as "natural_disasters.xlsx".
-
-TODO: It might be better to download the file in data/snapshots/emdat/2022-11-24/natural_disasters.xlsx instead of here.
+* Run this script using the argument --path-to-file followed by the path to the downloaded file.
 
 """
 
-import pathlib
+from pathlib import Path
 
 import click
 
+from etl.paths import DATA_DIR
 from etl.snapshot import Snapshot
 
-CURRENT_DIR = pathlib.Path(__file__).parent
-
+SNAPSHOT_VERSION = "2022-11-24"
 
 @click.command()
+@click.option("--path-to-file", prompt=True, type=str, help="Path to local data file.")
 @click.option(
     "--upload/--skip-upload",
     default=True,
     type=bool,
     help="Upload snapshot",
 )
-def main(upload: bool) -> None:
-    snap = Snapshot(CURRENT_DIR / "natural_disasters.xlsx")
+def main(path_to_file: str, upload: bool) -> None:
+    # Path to destination file in snapshots data folder.
+    destination_file = Path(DATA_DIR / f"snapshots/emdat/{SNAPSHOT_VERSION}/natural_disasters.xlsx")
+    # Ensure destination folder exists.
+    destination_file.parent.mkdir(exist_ok=True, parents=True)
+    # Copy local data file to snapshots data folder.
+    destination_file.write_bytes(Path(path_to_file).read_bytes())
+    # Create new snapshot.
+    snap = Snapshot(f"emdat/{SNAPSHOT_VERSION}/natural_disasters.xlsx")
     snap.dvc_add(upload=upload)
 
 
