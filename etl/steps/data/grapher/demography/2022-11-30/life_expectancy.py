1568a084ebc573743d3bcc8c393460e604be651f
lucasrodes <lucasrodes@users.noreply.github.com>
2022-12-01 00:12:24 +0100

    clean: remove comments

--- etl/steps/data/grapher/demography/2022-11-30/life_expectancy.py
+++ etl/steps/data/grapher/demography/2022-11-30/life_expectancy.py
@@ -6,14 +6,14 @@ N = Names(__file__)
 
 
 def run(dest_dir: str) -> None:
+    # get dataset
     dataset = catalog.Dataset.create_empty(dest_dir, N.garden_dataset.metadata)
 
+    # get table
     table = N.garden_dataset["life_expectancy"]
 
-    # optionally set additional dimensions
-    # table = table.set_index(["sex", "income_group"], append=True)
-
-    # if you data is in long format, check gh.long_to_wide_tables
+    # add table
     dataset.add(table)
 
+    # save table
     dataset.save()
