8a07cbebf760133be63cb7412d6767f0168046d2
Lucas Rodés-Guirao <lucasrodes@users.noreply.github.com>
2022-11-30 19:14:50 +0100

    data(undp_hdr): garden and grapher datasets (#648)
    * data(undp_hdr): add snapshots
    
    * docs(undp_hdr): add access_notes
    
    * enhance(undp_hdr): meadow step
    
    * enhance(undp_hdr): meadow step
    
    * enhance(undp_hdr): add metadata table
    
    * data(undp): garden dataset
    
    * enhance(undp): snap dataset
    
    * enhance(undp_hdr): add metadata
    
    * enhance(undp_hdr): garden step in dag
    
    * data(undp): grapher dataset
    
    * enhance(undp_hdr): docs
    
    * enhance(undp_hdr): feedback by @pabloarosado

---
+++ etl/steps/data/grapher/un/2022-11-29/undp_hdr.py
@@ -0,0 +1,19 @@
+from owid import catalog
+
+from etl.helpers import Names
+
+N = Names(__file__)
+
+
+def run(dest_dir: str) -> None:
+    # Initiate grapher dataset
+    dataset = catalog.Dataset.create_empty(dest_dir, N.garden_dataset.metadata)
+
+    # get table from dataset
+    table = N.garden_dataset["undp_hdr"]
+
+    # add table to dataset
+    dataset.add(table)
+
+    # save and exit
+    dataset.save()
