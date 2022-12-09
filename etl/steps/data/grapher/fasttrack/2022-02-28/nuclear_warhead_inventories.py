2ead0e8319fa38c610b2ec1a4746dae496aaed45
Mojmir Vinkler <mojmir.vinkler@gmail.com>
2022-12-09 11:22:42 +0100

    Add fast-track to add datasets from Google Sheets (#600)
    * feature(fasttrack): fasttrack CLI for adding datasets from Google Sheets

---
+++ etl/steps/data/grapher/fasttrack/2022-02-28/nuclear_warhead_inventories.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from owid import catalog
+
+from etl.helpers import Names
+from etl.snapshot import Snapshot
+
+N = Names(__file__)
+
+
+def run(dest_dir: str) -> None:
+    # load snapshot
+    data = pd.read_csv(Snapshot("fasttrack/2022-02-28/nuclear_warhead_inventories.csv").path)
+
+    # create empty dataframe and table
+    ds = catalog.Dataset.create_empty(dest_dir)
+    tb = catalog.Table(data)
+
+    # update metadata from *.meta.yml
+    ds.metadata.update_from_yaml(N.metadata_path)
+    tb.update_metadata_from_yaml(N.metadata_path, N.short_name)
+
+    # add table to dataset and save
+    ds.add(tb)
+    ds.save()
