91c025ae104e2e21944344c6c790bfe223360871
lucasrodes <lucasrodes@users.noreply.github.com>
2022-12-16 01:41:55 +0100

    enhance(chart-suggester): fix api usage

--- etl/steps/data/grapher/un_sdg/2022-07-07/match_vars_crs.py
+++ etl/steps/data/grapher/un_sdg/2022-07-07/match_vars_crs.py
@@ -12,12 +12,15 @@ NEW_DATASET_NAME = f"{NAMESPACE}__{VERSION.replace('-', '_')}"
 OLD_DATASET_NAME = "United Nations Sustainable Development Goals - United Nations (2022-04)"
 
 
+mapping_file = (Path(__file__).parent.resolve() / "output/variable_replacements.json").as_posix()
+
+
 match_variables_from_two_versions_of_a_dataset.main(
     old_dataset_name=OLD_DATASET_NAME,
     new_dataset_name=NEW_DATASET_NAME,
-    output_file=(Path(__file__).parent.resolve() / "output/variable_replacements.json").as_posix(),
+    output_file=mapping_file,
 )
 
 
-suggester = ChartRevisionSuggester(Path(__file__).parent.resolve().as_posix(), VERSION, NEW_DATASET_NAME)
+suggester = ChartRevisionSuggester.from_dict(mapping_file)
 suggester.suggest()
