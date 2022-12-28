0a2741ff6d7bb69fe0e8a8076b6566897ce76d81
lucasrodes <lucasrodes@users.noreply.github.com>
2022-12-28 22:31:10 +0100

    enhance(karlinsky): grapher

--- etl/steps/data/grapher/health/2022-12-28/karlinsky.py
+++ etl/steps/data/grapher/health/2022-12-28/karlinsky.py
@@ -8,10 +8,13 @@ N = Names(__file__)
 def run(dest_dir: str) -> None:
     dataset = catalog.Dataset.create_empty(dest_dir, N.garden_dataset.metadata)
 
-    table = N.garden_dataset["death_registration"]
+    table = N.garden_dataset["deaths_karlinsky"]
 
     # optionally set additional dimensions
     # table = table.set_index(["sex", "income_group"], append=True)
+
+    # patch
+    table.metadata.short_name = "death_registration"
 
     # if your data is in long format, you can use `grapher_helpers.long_to_wide_tables`
     # to get into wide format
