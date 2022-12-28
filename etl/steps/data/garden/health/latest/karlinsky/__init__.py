a6f3f8690cb74b01b5ae783e8defbd6bc78c2e3b
lucasrodes <lucasrodes@users.noreply.github.com>
2022-12-28 13:08:49 +0100

    enhance(karlinsky): add sanity checks

--- etl/steps/data/garden/health/latest/karlinsky/__init__.py
+++ etl/steps/data/garden/health/latest/karlinsky/__init__.py
@@ -10,9 +10,9 @@ import os
 
 import pandas as pd
 from owid.catalog import Dataset, Table
-from owid.datautils import geo
 from structlog import get_logger
 
+from etl.data_helpers import geo
 from etl.helpers import Names as N
 from etl.helpers import downloaded
 
@@ -25,28 +25,32 @@ log = get_logger()
 
 def run(dest_dir: str) -> None:
     # load dataframe
-    log.info("karlinsky: loading dataframe")
+    log.info("karlinsky: loading dataframe...")
     df = load_dataframe()
 
+    # sanity checks
+    log.info("karlinsky: sanity checking...")
+    sanity_check(df)
+
     # build table
-    log.info("karlinsky: converting dataframe to table")
+    log.info("karlinsky: converting dataframe to table...")
     tb = Table(df, short_name="death_registration")
 
     # build dataset
-    log.info("karlinsky: creating dataset")
+    log.info("karlinsky: creating dataset...")
     ds = Dataset.create_empty(dest_dir)
 
     # add table
-    log.info("karlinsky: adding table to dataset")
+    log.info("karlinsky: adding table to dataset...")
     ds.add(tb)
 
     # update metadata
-    log.info("karlinsky: adding metadata")
+    log.info("karlinsky: adding metadata...")
     ds.update_metadata(PATH_METADATA)
     ds.metadata.date_accessed = dt.date.today().strftime("%Y-%m-%d")
 
     # save
-    log.info("karlinsky: saving dataset")
+    log.info("karlinsky: saving dataset...")
     ds.save()
 
 
@@ -66,9 +70,14 @@ def load_dataframe() -> pd.DataFrame:
 
 def harmonize_countries(df: pd.DataFrame) -> pd.DataFrame:
     unharmonized_countries = df["country"]
-    df = geo.harmonize_countries(df=df, countries_file=str(PATH_COUNTRIES))
+    df = geo.harmonize_countries(
+        df=df,
+        countries_file=str(PATH_COUNTRIES),
+        warn_on_missing_countries=True,
+        make_missing_countries_nan=True,
+    )
 
-    missing_countries = set(unharmonized_countries[df.country.isnull()])
+    missing_countries = set(unharmonized_countries[df["country"].isnull()])
     if any(missing_countries):
         raise RuntimeError(
             "The following raw country names have not been harmonized. "
@@ -78,3 +87,34 @@ def harmonize_countries(df: pd.DataFrame) -> pd.DataFrame:
         )
 
     return df
+
+
+def sanity_check(df: pd.DataFrame) -> None:
+    # check columns
+    columns_expected = {
+        "death_comp",
+        "expected_deaths",
+        "expected_gbd",
+        "expected_ghe",
+        "expected_wpp",
+        "reg_deaths",
+    }
+    columns_new = set(df.columns).difference(columns_expected)
+    if columns_new:
+        raise ValueError(f"Unexpected columns {columns_new}")
+
+    # ensure percentages make sense (within range [0, 100])
+    columns_perc = ["death_comp"]
+    for col in columns_perc:
+        assert all(df[col] <= 100), f"{col} has values larger than 100%"
+        assert all(df[col] >= 0), f"{col} has values lower than 0%"
+
+    # ensure absolute values make sense (positive, lower than population)
+    columns_absolute = [col for col in df.columns if col not in columns_perc]
+    df_ = df.reset_index()
+    df_ = geo.add_population_to_dataframe(df_)
+    for col in columns_absolute:
+        x = df_.dropna(subset=[col])
+        assert all(
+            x[col] < 0.2 * x["population"]
+        ), f"{col} contains values that might be too large (comapred to population values)!"
