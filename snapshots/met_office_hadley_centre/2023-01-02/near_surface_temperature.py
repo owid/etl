42290bf5a864fe3a80d71a438d438e40a2333752
Pablo Rosado <pabloarosado@gmail.com>
2023-01-02 13:18:08 -0600

    feat(met_office_hadley_centre): Add snapshot for near surface temperature dataset

---
+++ snapshots/met_office_hadley_centre/2023-01-02/near_surface_temperature.py
@@ -0,0 +1,33 @@
+"""Snapshot of the HadCRUT5 near surface temperature dataset (temperature anomaly).
+
+The HadCRUT5 near surface temperature data set is produced by blending data from the CRUTEM5 surface air temperature
+dataset and the HadSST4 sea-surface temperature dataset.
+"""
+
+import pathlib
+
+import click
+
+from etl.snapshot import Snapshot
+
+CURRENT_DIR = pathlib.Path(__file__).parent
+SNAPSHOT_VERSION = "2023-01-02"
+
+
+@click.command()
+@click.option(
+    "--upload/--skip-upload",
+    default=True,
+    type=bool,
+    help="Upload dataset to Snapshot",
+)
+def main(upload: bool) -> None:
+    snap = Snapshot(
+        f"met_office_hadley_centre/{SNAPSHOT_VERSION}/near_surface_temperature.csv"
+    )
+    snap.download_from_source()
+    snap.dvc_add(upload=upload)
+
+
+if __name__ == "__main__":
+    main()
