a0d11961920f86cbf6f98ed3f8ed59859a44fe7e
Pablo Rosado <pabloarosado@gmail.com>
2023-01-02 15:22:17 -0600

    feat(met_office_hadley_centre): Add snapshots for northern and southern hemisphre

--- etl/steps/data/meadow/met_office_hadley_centre/2023-01-02/near_surface_temperature.py
+++ etl/steps/data/meadow/met_office_hadley_centre/2023-01-02/near_surface_temperature.py
@@ -1,16 +1,14 @@
-"""Load near surface temperature by Met Office Hadley Centre and prepare a table.
+"""Load near surface temperature dataset (northern hemisphere, southern hemisphere, and global) by Met Office Hadley
+Centre and create a single table.
 
 """
 
 import pandas as pd
 from owid.catalog import Dataset, Table
-from structlog import get_logger
 
 from etl.helpers import Names
 from etl.snapshot import Snapshot
 from etl.steps.data.converters import convert_snapshot_metadata
-
-log = get_logger()
 
 # Get naming conventions.
 N = Names(__file__)
@@ -18,35 +16,55 @@ N = Names(__file__)
 # Snapshot and Meadow dataset versions.
 SNAPSHOT_VERSION = "2023-01-02"
 MEADOW_VERSION = SNAPSHOT_VERSION
+MEADOW_SHORT_NAME = "near_surface_temperature"
 
 # Columns to select and how to name them.
 COLUMNS = {
+    # Additional column.
+    "region": "region",
+    # Original column names and new names.
     "Time": "year",
     "Anomaly (deg C)": "temperature_anomaly",
     "Lower confidence limit (2.5%)": "lower_limit",
     "Upper confidence limit (97.5%)": "upper_limit",
 }
 
+# Names of snapshot files.
+REGION_FILE_NAMES = {
+    "Global": "near_surface_temperature_global.csv",
+    "Northern hemisphere": "near_surface_temperature_northern_hemisphere.csv",
+    "Southern hemisphere": "near_surface_temperature_southern_hemisphere.csv",
+}
 
 def run(dest_dir: str) -> None:
-    log.info("near_surface_temperature.start")
+    #
+    # Load data.
+    #
+    # Load snapshots.
+    snapshots = {region: Snapshot(f"met_office_hadley_centre/{SNAPSHOT_VERSION}/{file_name}")
+                 for region, file_name in REGION_FILE_NAMES.items()}
+    df = pd.concat([pd.read_csv(snapshot.path).assign(**{"region": region})
+                    for region, snapshot in snapshots.items()], ignore_index=True)
 
-    # Load snapshot.
-    snap = Snapshot("met_office_hadley_centre/2023-01-02/near_surface_temperature.csv")
-    df = pd.read_csv(snap.path)
-
+    #
+    # Prepare data.
+    #
     # Select and rename required columns.
     df = df.rename(columns=COLUMNS, errors="raise")[COLUMNS.values()]
 
-    # Create new meadow dataset.
-    ds = Dataset.create_empty(dest_dir, metadata=convert_snapshot_metadata(snap.metadata))
+    # Set an appropriate index and sort conveniently.
+    df = df.set_index(["region", "year"], verify_integrity=True).sort_index()
+
+    #
+    # Save outputs.
+    #
+    # Create new meadow dataset, using metadata from one of the snapshots.
+    ds = Dataset.create_empty(dest_dir, metadata=convert_snapshot_metadata(snapshots["Global"].metadata))
     ds.metadata.version = MEADOW_VERSION
 
     # Create new table with metadata and underscore all columns.
-    tb = Table(df, short_name=snap.metadata.short_name, underscore=True)
+    tb = Table(df, short_name=MEADOW_SHORT_NAME, underscore=True)
 
     # Add table to new meadow dataset and save dataset.
     ds.add(tb)
     ds.save()
-
-    log.info("near_surface_temperature.end")
--- snapshots/met_office_hadley_centre/2023-01-02/near_surface_temperature.py
+++ snapshots/met_office_hadley_centre/2023-01-02/near_surface_temperature.py
@@ -12,7 +12,11 @@ from etl.snapshot import Snapshot
 
 CURRENT_DIR = pathlib.Path(__file__).parent
 SNAPSHOT_VERSION = "2023-01-02"
-
+FILE_NAMES = [
+    "near_surface_temperature_global.csv",
+    "near_surface_temperature_northern_hemisphere.csv",
+    "near_surface_temperature_southern_hemisphere.csv",
+]
 
 @click.command()
 @click.option(
@@ -22,9 +26,10 @@ SNAPSHOT_VERSION = "2023-01-02"
     help="Upload dataset to Snapshot",
 )
 def main(upload: bool) -> None:
-    snap = Snapshot(f"met_office_hadley_centre/{SNAPSHOT_VERSION}/near_surface_temperature.csv")
-    snap.download_from_source()
-    snap.dvc_add(upload=upload)
+    for file_name in FILE_NAMES:
+        snap = Snapshot(f"met_office_hadley_centre/{SNAPSHOT_VERSION}/{file_name}")
+        snap.download_from_source()
+        snap.dvc_add(upload=upload)
 
 
 if __name__ == "__main__":
