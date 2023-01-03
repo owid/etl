21cf39ba84d701dbef7001f77fea98f78aa13945
Pablo Rosado <pabloarosado@gmail.com>
2023-01-03 16:09:01 -0600

    style(unep): Improve format

--- etl/steps/data/garden/unep/2023-01-03/renewable_energy_investments.py
+++ etl/steps/data/garden/unep/2023-01-03/renewable_energy_investments.py
@@ -2,16 +2,9 @@
 
 """
 
-import json
-from typing import List, cast
+from owid import catalog
 
-import pandas as pd
-from owid.catalog import Dataset, Table
-from structlog import get_logger
-
-from etl.data_helpers import geo
 from etl.helpers import PathFinder
-from etl.paths import DATA_DIR
 
 # Get paths and naming conventions.
 paths = PathFinder(__file__)
@@ -22,14 +15,14 @@ def run(dest_dir: str) -> None:
     # Load data.
     #
     # Load dataset from meadow.
-    ds_meadow = paths.load_dependency("renewable_energy_investments")
+    ds_meadow: catalog.Dataset = paths.load_dependency("renewable_energy_investments")
     tb_meadow = ds_meadow["renewable_energy_investments"]
 
     #
     # Save outputs.
     #
     # Create a new dataset with the same metadata as meadow
-    ds_garden = Dataset.create_empty(dest_dir, metadata=ds_meadow.metadata)
+    ds_garden = catalog.Dataset.create_empty(dest_dir, metadata=ds_meadow.metadata)
 
     # Create new table with metadata and add it to the new dataset.
     tb_garden = tb_meadow
