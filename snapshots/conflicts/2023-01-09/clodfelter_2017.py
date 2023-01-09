00b1d3f51244c494bda2383507ecce95fa34a20e
lucasrodes <lucasrodes@users.noreply.github.com>
2023-01-09 19:24:05 +0100

    data(snapshot): add conflict datasets from spreadsheets

---
+++ snapshots/conflicts/2023-01-09/clodfelter_2017.py
@@ -0,0 +1,36 @@
+"""Get our manually curated dataset.
+
+This dataset was obtained by examining the data provided by the source, and transcribing it into a Google Spreadsheet. It can be found
+at https://docs.google.com/spreadsheets/d/1Evc5VP2Ra3Lb2_GoV8LjMaJcFoCzlUwslY-TFtC_fB4/edit?usp=sharing."""
+
+
+from pathlib import Path
+
+import click
+
+from etl.snapshot import Snapshot
+
+SNAPSHOT_VERSION = "2023-01-09"
+
+
+@click.command()
+@click.option("--path-to-file", prompt=True, type=str, help="Path to local data file.")
+@click.option(
+    "--upload/--skip-upload",
+    default=True,
+    type=bool,
+    help="Upload dataset to Snapshot",
+)
+def main(path_to_file: str, upload: bool) -> None:
+    # Create new snapshot.
+    snap = Snapshot(f"conflicts/{SNAPSHOT_VERSION}/clodfelter_2017.csv")
+    # Ensure destination folder exists.
+    snap.path.parent.mkdir(exist_ok=True, parents=True)
+    # Copy local data file to snapshots data folder.
+    snap.path.write_bytes(Path(path_to_file).read_bytes())
+    # Add file to DVC and upload to S3.
+    snap.dvc_add(upload=upload)
+
+
+if __name__ == "__main__":
+    main()
---
+++ snapshots/conflicts/2023-01-09/eckhardt_1991.py
@@ -0,0 +1,35 @@
+"""Get our manually curated dataset.
+
+This dataset was obtained by examining the data provided by the source, and transcribing it into a Google Spreadsheet. It can be found
+at https://docs.google.com/spreadsheets/d/1vI5zTU5r7LNjG8AcH0ZIoCNEjMIo68Rn6pqwarVAQ-Q/edit?usp=sharing."""
+
+from pathlib import Path
+
+import click
+
+from etl.snapshot import Snapshot
+
+SNAPSHOT_VERSION = "2023-01-09"
+
+
+@click.command()
+@click.option("--path-to-file", prompt=True, type=str, help="Path to local data file.")
+@click.option(
+    "--upload/--skip-upload",
+    default=True,
+    type=bool,
+    help="Upload dataset to Snapshot",
+)
+def main(path_to_file: str, upload: bool) -> None:
+    # Create new snapshot.
+    snap = Snapshot(f"conflicts/{SNAPSHOT_VERSION}/eckhardt_1991.csv")
+    # Ensure destination folder exists.
+    snap.path.parent.mkdir(exist_ok=True, parents=True)
+    # Copy local data file to snapshots data folder.
+    snap.path.write_bytes(Path(path_to_file).read_bytes())
+    # Add file to DVC and upload to S3.
+    snap.dvc_add(upload=upload)
+
+
+if __name__ == "__main__":
+    main()
