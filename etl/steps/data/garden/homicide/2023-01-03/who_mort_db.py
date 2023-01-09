b2aa66624e1718c7a3424b85fcd63faff6db80bb
Fiona Spooner <fiona@ourworldindata.org>
2023-01-09 13:36:24 +0000

    Homicide datasets used to create long-run homicide OMM (#741)
    * feat(homicide): adding snapshot of who mortality homicide data
    
    * feat(homicide): adding homicide data from WHO mort db with metadata
    
    * feat(homicide): adding who mortalty database data
    
    * feat(homicide): adding unodc data and moving who mort db data to snapshot folder
    
    * feat(homicide): adding unodc to meadow
    
    * feat(homicide): cleaning up unodc data
    
    * refactor(homicide): adding description to unodc
    
    * feat(homicide): dag and metadata
    
    * refactor(dag): adding some comments to dag
    
    * refactor(dag): updating dag
    
    * refactor(homicide): removing redundant code
    
    * refactor(homicide): combining eisner with who and unodc and getting grapher ready
    
    * fix(homicide): make format
    
    * format(homicide): fixing some formatting issues
    
    * refactor(homicide): fix to oceania entity
    
    * refactor(homicide): removing index columns and large snapshot file
    
    * fix(homicide): dropping index
    
    * refactor(homicide): adding eisner standalone variable and associated metadata
    
    * refactor(homicide): adding to eisner metadata
    
    * refactor(homicide): moving steps from walkthrough to main dag
    
    * fix(homicide): adding in mojmirs suggestion
    
    * fix(riley): adding fix to riley as it was failing due to deprecatied functions in PyPDF2

---
+++ etl/steps/data/garden/homicide/2023-01-03/who_mort_db.py
@@ -0,0 +1,73 @@
+import json
+from typing import List, cast
+
+import pandas as pd
+from owid.catalog import Dataset, Table
+from structlog import get_logger
+
+from etl.data_helpers import geo
+from etl.helpers import Names
+from etl.paths import DATA_DIR
+
+log = get_logger()
+
+# naming conventions
+N = Names(__file__)
+
+
+def run(dest_dir: str) -> None:
+    log.info("who_mort_db.start")
+
+    # read dataset from meadow
+    ds_meadow = Dataset(DATA_DIR / "meadow/homicide/2023-01-03/who_mort_db")
+    tb_meadow = ds_meadow["who_mort_db"]
+
+    df = pd.DataFrame(tb_meadow)
+
+    log.info("who_mort_db.exclude_countries")
+    df = exclude_countries(df)
+
+    log.info("who_mort_db.harmonize_countries")
+    df = harmonize_countries(df)
+
+    # create new dataset with the same metadata as meadow
+    ds_garden = Dataset.create_empty(dest_dir, metadata=ds_meadow.metadata)
+
+    # create new table with the same metadata as meadow and add it to dataset
+    tb_garden = Table(df, like=tb_meadow)
+    ds_garden.add(tb_garden)
+
+    # update metadata from yaml file
+    ds_garden.update_metadata(N.metadata_path)
+
+    ds_garden.save()
+
+    log.info("who_mort_db.end")
+
+
+def load_excluded_countries() -> List[str]:
+    with open(N.excluded_countries_path, "r") as f:
+        data = json.load(f)
+        assert isinstance(data, list)
+    return data
+
+
+def exclude_countries(df: pd.DataFrame) -> pd.DataFrame:
+    excluded_countries = load_excluded_countries()
+    return cast(pd.DataFrame, df.loc[~df.country.isin(excluded_countries)])
+
+
+def harmonize_countries(df: pd.DataFrame) -> pd.DataFrame:
+    unharmonized_countries = df["country"]
+    df = geo.harmonize_countries(df=df, countries_file=str(N.country_mapping_path))
+
+    missing_countries = set(unharmonized_countries[df.country.isnull()])
+    if any(missing_countries):
+        raise RuntimeError(
+            "The following raw country names have not been harmonized. "
+            f"Please: (a) edit {N.country_mapping_path} to include these country "
+            f"names; or (b) add them to {N.excluded_countries_path}."
+            f"Raw country names: {missing_countries}"
+        )
+
+    return df
