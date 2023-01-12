696544099dcbbda53bcb963e0aeb98fd7fcfc704
spoonerf <spoonerfiona@gmail.com>
2023-01-12 14:05:54 +0000

    refactor(cherry_blossom): adding in multi-year averages

--- etl/steps/data/garden/biodiversity/2023-01-11/cherry_blossom.py
+++ etl/steps/data/garden/biodiversity/2023-01-11/cherry_blossom.py
@@ -1,11 +1,7 @@
-import json
-from typing import List, cast
-
 import pandas as pd
-from owid import catalog
+from owid.catalog import Dataset, Table
 from structlog import get_logger
 
-from etl.data_helpers import geo
 from etl.helpers import Names
 from etl.paths import DATA_DIR
 
@@ -19,25 +15,20 @@ def run(dest_dir: str) -> None:
     log.info("cherry_blossom.start")
 
     # read dataset from meadow
-    ds_meadow = catalog.Dataset(DATA_DIR / "meadow/biodiversity/2023-01-11/cherry_blossom")
+    ds_meadow = Dataset(DATA_DIR / "meadow/biodiversity/2023-01-11/cherry_blossom")
     tb_meadow = ds_meadow["cherry_blossom"]
 
     df = pd.DataFrame(tb_meadow)
 
-    log.info("cherry_blossom.exclude_countries")
-    df = exclude_countries(df)
-
-    log.info("cherry_blossom.harmonize_countries")
-    df = harmonize_countries(df)
-
-    # Calculate a 20 year average
+    # Calculate a 20,40 and 50 year average
     df = calculate_multiple_year_average(df)
 
     # create new dataset with the same metadata as meadow
-    ds_garden = catalog.Dataset.create_empty(dest_dir, metadata=ds_meadow.metadata)
+    ds_garden = Dataset.create_empty(dest_dir, metadata=ds_meadow.metadata)
 
-    # create new table with the same metadata as meadow and add it to dataset
-    tb_garden = catalog.Table(df, metadata=N.metadata_path)
+    # create new table with metadata from meta.yml
+    df = df.reset_index(drop=True)
+    tb_garden = Table(df, short_name=tb_meadow.metadata.short_name)
     ds_garden.add(tb_garden)
 
     # update metadata from yaml file
@@ -46,34 +37,6 @@ def run(dest_dir: str) -> None:
     ds_garden.save()
 
     log.info("cherry_blossom.end")
-
-
-def load_excluded_countries() -> List[str]:
-    with open(N.excluded_countries_path, "r") as f:
-        data = json.load(f)
-        assert isinstance(data, list)
-    return data
-
-
-def exclude_countries(df: pd.DataFrame) -> pd.DataFrame:
-    excluded_countries = load_excluded_countries()
-    return cast(pd.DataFrame, df.loc[~df.country.isin(excluded_countries)])
-
-
-def harmonize_countries(df: pd.DataFrame) -> pd.DataFrame:
-    unharmonized_countries = df["country"]
-    df = geo.harmonize_countries(df=df, countries_file=str(N.country_mapping_path))
-
-    missing_countries = set(unharmonized_countries[df.country.isnull()])
-    if any(missing_countries):
-        raise RuntimeError(
-            "The following raw country names have not been harmonized. "
-            f"Please: (a) edit {N.country_mapping_path} to include these country "
-            f"names; or (b) add them to {N.excluded_countries_path}."
-            f"Raw country names: {missing_countries}"
-        )
-
-    return df
 
 
 def calculate_multiple_year_average(df: pd.DataFrame) -> pd.DataFrame:
@@ -88,5 +51,16 @@ def calculate_multiple_year_average(df: pd.DataFrame) -> pd.DataFrame:
     df_comb = df_comb.sort_values("year")
 
     df_comb["average_20_years"] = df_comb["full_flowering_date"].rolling(20, min_periods=1).mean()
+
+    df_comb["average_40_years"] = df_comb["full_flowering_date"].rolling(40, min_periods=1).mean()
+
+    df_comb["average_50_years"] = df_comb["full_flowering_date"].rolling(50, min_periods=1).mean()
+
+    df_comb["average_100_years"] = df_comb["full_flowering_date"].rolling(100, min_periods=1).mean()
+
+    # experimenting with splines
+    # df_nona = df_comb.dropna(subset="full_flowering_date")
+    # spl = UnivariateSpline(x=df_nona["year"], y=df_nona["full_flowering_date"], s=200, k=1)
+    # df_comb["uni_spline"] = spl(df_comb["year"])
 
     return df_comb
