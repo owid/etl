1c51fa8db96247fe9528b0f03648d5841c356225
lucasrodes <lucasrodes@users.noreply.github.com>
2023-01-18 20:32:50 +0100

    enhance(conflicts): meadow checks

--- etl/steps/data/meadow/conflicts/2023-01-10/shared.py
+++ etl/steps/data/meadow/conflicts/2023-01-10/shared.py
@@ -44,23 +44,42 @@ COLUMN_RENAME = {
     "Update": "update",
 }
 
+COLUMNS_REQUIRED = [
+    "bulk_id",
+    "conflict_name",
+    "conflict_participants",
+    "start_year",
+    "end_year",
+    "continent",
+    "total_deaths",
+]
+
 
 def clean_data(df: pd.DataFrame, column_rename: Optional[Dict[str, str]]) -> pd.DataFrame:
     if not column_rename:
         column_rename = COLUMN_RENAME
 
-    # sanity check: check if all columns are expected
+    # sanity checks
     columns_expected = set(column_rename)
-    columns_unexpected = set(df.columns).difference(columns_expected)
+    columns_given = set(df.columns)
+    # check if all columns are expected
+    columns_unexpected = columns_given.difference(columns_expected)
     if columns_unexpected:
         raise ValueError(f"Unexpected columns found! {columns_unexpected}")
+    # sanity check: check if there is any missing column
+    columns_missing = set(columns_expected).difference(columns_given)
+    if columns_missing:
+        raise ValueError(f"There are missing columns! {columns_missing}")
 
     # rename columns
     df = df.rename(columns=column_rename, errors="raise")
 
-    # table might contain some columns with only NaNs. These should be dropped, as they don't contain any data
+    # table might contain some columns with only NaNs. These should be dropped, as they don't contain any data.
+    # Note that *not* all should be dropped, some are required for correct functioning!
     df = df.dropna(how="all", axis=1)
-
+    for col in COLUMNS_REQUIRED:
+        if col not in df.columns:
+            raise ValueError(f"Missing required column{col}")
     return df
 
 
@@ -69,7 +88,6 @@ def run_pipeline(dest_dir: str, paths: PathFinder, column_rename: Optional[Dict[
     # read snapshot dataset
     snap = paths.load_dependency(paths.short_name)
     df = pd.read_csv(snap.path, header=4)
-
     # clean and transform data
     df = clean_data(df, column_rename)
 
