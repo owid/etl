3318e1086ed550005215b07212265e885602c552
Pablo Rosado <pabloarosado@gmail.com>
2023-01-27 14:26:34 +0100

    refactor: Create a dag/archive folder

--- etl/paths.py
+++ etl/paths.py
@@ -4,7 +4,7 @@ from pathlib import Path
 BASE_DIR = Path(os.environ.get("BASE_DIR", Path(__file__).parent.parent))
 DAG_DIR = BASE_DIR / "dag"
 DAG_FILE = DAG_DIR / "main.yml"
-DAG_ARCHIVE_FILE = DAG_DIR / "main_archive.yml"
+DAG_ARCHIVE_FILE = DAG_DIR / "archive" / "main.yml"
 DATA_DIR = BASE_DIR / "data"
 SNAPSHOTS_DIR = BASE_DIR / "snapshots"
 STEP_DIR = BASE_DIR / "etl" / "steps"
