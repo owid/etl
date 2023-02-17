6363f6e6bc73be153c327a3d27b479534cdcadbb
spoonerf <spoonerfiona@gmail.com>
2023-02-17 12:03:16 +0000

    feat(garden steps for flunet and fluid)

--- etl/steps/data/garden/who/2023-02-16/fluid.py
+++ etl/steps/data/garden/who/2023-02-16/fluid.py
@@ -36,9 +36,12 @@ def run(dest_dir: str) -> None:
         df=df, countries_file=paths.country_mapping_path, excluded_countries_file=paths.excluded_countries_path
     )
 
+    # Subset the data
+    df = subset_and_clean_data(df)
+    df = df.reset_index(drop=True)
     # Create a new table with the processed data.
-    tb_garden = Table(df, like=tb_meadow)
-
+    tb_garden = Table(df, short_name=paths.short_name)
+    tb_garden.update_metadata_from_yaml(paths.metadata_path, paths.short_name)
     #
     # Save outputs.
     #
@@ -49,3 +52,107 @@ def run(dest_dir: str) -> None:
     ds_garden.save()
 
     log.info("fluid.end")
+
+
+def subset_and_clean_data(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    * Select out the 'All' age groups
+    * Select the case info types we are interested in: SARI, ILI, ARI, SARI_DEATHS
+    * Format the date
+    * Drop unwanted columns
+    * Drop rows where reported cases = 0
+    """
+
+    df = df.query("agegroup_code == 'All' & case_info.isin(['ILI', 'SARI', 'ARI','SARI_ICU' ,'SARI_DEATHS'])")
+    df["date"] = pd.to_datetime(df["iso_weekstartdate"], format="%Y-%m-%d", utc=True).dt.date.astype(str)
+
+    # At time of creation the source type was not used in FluID, but it is used in FluNET to identify Sentinel sites
+    assert df.origin_source.unique() == ["NOTDEFINED"]
+
+    df = df.drop(
+        columns=[
+            "whoregion",
+            "fluseason",
+            "hemisphere",
+            "itz",
+            "country_code",
+            "iso_weekstartdate",
+            "iso_year",
+            "iso_week",
+            "mmwr_weekstartdate",
+            "mmwr_year",
+            "mmwr_week",
+            "agegroup_code",
+            "outpatients",
+            "inpatients",
+            "pop_cov",
+            "inf_tested",
+            "inf_pos",
+            "inf_neg",
+            "inf_a",
+            "inf_b",
+            "inf_mixed",
+            "ah1",
+            "ah1n12009",
+            "ah3",
+            "ah5",
+            "ah7",
+            "anotsubtyped",
+            "aothertypes",
+            "byamagata",
+            "bvictoria",
+            "bnotdetermined",
+            "other",
+            "rsv",
+            "adeno",
+            "parainfluenza",
+            "inf_risk",
+            "geospread",
+            "impact",
+            "intensity",
+            "trend",
+            "nb_sites",
+            "mortality_all",
+            "mortality_pni",
+            "iso2",
+            "isoyw",
+            "mmwryw",
+            "iso2",
+            "isoyw",
+            "mmwryw",
+            "origin_source",
+        ]
+    )
+
+    df = df.dropna(subset=["reported_cases"])
+    return df
+
+
+def pivot_fluid(df: pd.DataFrame) -> pd.DataFrame:
+
+    df_piv = df.pivot(
+        index=[
+            "country",
+            "date",
+        ],
+        columns="case_info",
+        values=[
+            "reported_cases",
+        ],
+    ).reset_index()
+
+    df_piv.columns = list(map("".join, df_piv.columns))
+
+    df_piv = df_piv.dropna(axis=1, how="all")
+
+    df_piv = df_piv.rename(
+        columns={
+            "reported_casesSARI": "reported_sari_cases",
+            "reported_casesARI": "ari_cases",
+            "reported_casesILI": "ili_cases",
+            "reported_casesSARI_DEATHS": "reported_sari_deaths",
+            "reported_casesSARI_ICU": "reported_sari_icu",
+        }
+    )
+
+    return df_piv
--- etl/steps/data/garden/who/2023-02-16/flunet.py
+++ etl/steps/data/garden/who/2023-02-16/flunet.py
@@ -36,16 +36,66 @@ def run(dest_dir: str) -> None:
         df=df, countries_file=paths.country_mapping_path, excluded_countries_file=paths.excluded_countries_path
     )
 
+    df = clean_and_format_data(df)
+    df = aggregate_surveillance_type(df)
     # Create a new table with the processed data.
-    tb_garden = Table(df, like=tb_meadow)
-
+    # tb_garden = Table(df, like=tb_meadow)
+    tb_garden = Table(df, short_name=paths.short_name)
+    tb_garden.update_metadata_from_yaml(paths.metadata_path, paths.short_name)
     #
     # Save outputs.
     #
     # Create a new garden dataset with the same metadata as the meadow dataset.
     ds_garden = create_dataset(dest_dir, tables=[tb_garden], default_metadata=ds_meadow.metadata)
-
     # Save changes in the new garden dataset.
     ds_garden.save()
 
     log.info("flunet.end")
+
+
+def clean_and_format_data(df: pd.DataFrame) -> pd.DataFrame:
+    df["date"] = df["iso_weekstartdate"]
+    return df
+
+
+def create_date_from_iso_week(date_iso: pd.Series) -> pd.Series:
+    date = pd.to_datetime(date_iso, format="%Y-%m-%d", utc=True).dt.date.astype(str)
+    return date
+
+
+def aggregate_surveillance_type(combined_df: pd.DataFrame) -> pd.DataFrame:
+    sel_cols = [
+        "country",
+        "hemisphere",
+        "date",
+        "origin_source",
+        "ah1n12009",
+        "ah1",
+        "ah3",
+        "ah5",
+        "ah7n9",
+        "anotsubtyped",
+        "anotsubtypable",
+        "aother_subtype",
+        "aother_subtype_details",
+        "inf_a",
+        "bvic_2del",
+        "bvic_3del",
+        "bvic_nodel",
+        "bvic_delunk",
+        "byam",
+        "bnotdetermined",
+        "inf_b",
+        "inf_all",
+        "inf_negative",
+        "ili_activity",
+        "spec_processed_nb",
+        "spec_received_nb",
+    ]
+    df = combined_df[sel_cols]
+    df = df.copy(deep=True)
+    # Summing all cases by country, hemisphere and date
+    df_agg = df.groupby(["country", "hemisphere", "date"]).sum().reset_index()
+    # Check we haven't lost any cases along the way
+    assert combined_df["inf_all"].sum() == df_agg["inf_all"].sum()
+    return df_agg
