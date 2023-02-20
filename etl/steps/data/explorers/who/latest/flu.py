bf8d741c77f02ec5bfe80c282f75a2a9385ec618
spoonerf <spoonerfiona@gmail.com>
2023-02-20 08:58:58 +0000

    fix(flu): adding short name to table'

--- etl/steps/data/explorers/who/latest/flu.py
+++ etl/steps/data/explorers/who/latest/flu.py
@@ -1,7 +1,7 @@
 """Load a garden dataset and create an explorers dataset."""
 
 import pandas as pd
-from owid.catalog import Dataset
+from owid.catalog import Dataset, Table
 
 from etl.helpers import PathFinder, create_dataset
 
@@ -22,6 +22,7 @@ def run(dest_dir: str) -> None:
     tb_fluid = fluid_garden["fluid"]
 
     tb_flu = pd.merge(tb_fluid, tb_flunet, on=["country", "date"], how="outer")
+    tb_flu = Table(tb_flu, short_name="flu")
 
     # Create explorer dataset, with garden table and metadata in csv format
     ds_explorer = create_dataset(dest_dir, tables=[tb_flu], default_metadata=flunet_garden.metadata, formats=["csv"])
