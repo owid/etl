806290aea0c8cc81fa112281e914074c0ddca737
Pablo Rosado <pabloarosado@gmail.com>
2023-02-24 17:31:35 +0100

    enhance(ember): Remove unnecessary code from shared module

--- etl/steps/data/garden/ember/2023-02-20/shared.py
+++ etl/steps/data/garden/ember/2023-02-20/shared.py
@@ -3,7 +3,6 @@ from typing import Any, Dict, List, Optional, Union
 
 import numpy as np
 import pandas as pd
-from owid import catalog
 from structlog import get_logger
 
 from etl.data_helpers import geo
@@ -143,49 +142,12 @@ HISTORIC_TO_CURRENT_REGION: Dict[str, Dict[str, Union[str, List[str]]]] = {
     },
 }
 
-# Historical countries for which we don't have population.
-EXPECTED_COUNTRIES_WITHOUT_POPULATION = []
-
 # Overlaps found between historical regions and successor countries, that we accept in the data.
 # We accept them either because they happened close to the transition, or to avoid needing to introduce new
 # countries for which we do not have data (like the Russian Empire).
 ACCEPTED_OVERLAPS = {
     # 1991: {"Georgia", "USSR"},
 }
-
-
-def gather_sources_from_tables(
-    tables: List[catalog.Table],
-) -> List[catalog.meta.Source]:
-    """Gather unique sources from the metadata.dataset of each table in a list of tables.
-
-    Note: To check if a source is already listed, only the name of the source is considered (not the description or any
-    other field in the source).
-
-    Parameters
-    ----------
-    tables : list
-        List of tables with metadata.
-
-    Returns
-    -------
-    known_sources : list
-        List of unique sources from all tables.
-
-    """
-    # Initialise list that will gather all unique metadata sources from the tables.
-    known_sources: List[catalog.meta.Source] = []
-    for table in tables:
-        # Get list of sources of the dataset of current table.
-        table_sources = table.metadata.dataset.sources
-        # Go source by source of current table, and check if its name is not already in the list of known_sources.
-        for source in table_sources:
-            # Check if this source's name is different to all known_sources.
-            if all([source.name != known_source.name for known_source in known_sources]):
-                # Add the new source to the list.
-                known_sources.append(source)
-
-    return known_sources
 
 
 def get_countries_in_region(
