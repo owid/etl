350907aeddb91a1bce3a7dd933479855fc6dc2a2
Pablo Rosado <pabloarosado@gmail.com>
2023-02-28 22:14:16 +1100

    feat(energy): Add new garden and grapher steps for the energy mix dataset

--- etl/steps/data/grapher/bp/2023-02-20/energy_mix.py
+++ etl/steps/data/grapher/bp/2023-02-20/energy_mix.py
@@ -1,5 +1,6 @@
-"""Grapher step for the global primary energy dataset.
+"""Grapher step for BP's energy mix dataset.
 """
+
 from owid.catalog import Dataset
 
 from etl.helpers import PathFinder, create_dataset
@@ -13,19 +14,20 @@ def run(dest_dir: str) -> None:
     # Load data.
     #
     # Load garden dataset.
-    ds_garden: Dataset = paths.load_dependency("global_primary_energy")
+    ds_garden: Dataset = paths.load_dependency("energy_mix")
 
-    # Read main table from dataset.
-    tb_garden = ds_garden["global_primary_energy"]
+    # Read table from garden dataset.
+    tb_garden = ds_garden["energy_mix"].reset_index()
 
     #
     # Process data.
     #
-    # Drop unnecessary columns from table.
-    tb_garden = tb_garden.reset_index().drop(columns=["data_source"])
+    # Remove unnecessary columns.
+    tb_garden = tb_garden.drop(columns=["country_code"])
 
     #
     # Save outputs.
     #
+    # Create new grapher dataset.
     ds_grapher = create_dataset(dest_dir=dest_dir, tables=[tb_garden], default_metadata=ds_garden.metadata)
     ds_grapher.save()
