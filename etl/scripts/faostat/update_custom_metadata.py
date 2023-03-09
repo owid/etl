729a316589d42b283cbf17773422e28095150475
Pablo Rosado <pabloarosado@gmail.com>
2023-03-09 20:41:52 +1100

    feat(faostat): Create process to detect changes in elements and units

--- etl/scripts/faostat/update_custom_metadata.py
+++ etl/scripts/faostat/update_custom_metadata.py
@@ -14,6 +14,16 @@ from shared import INCLUDED_DATASETS_CODES, VERSION
 from etl.paths import DATA_DIR, STEP_DIR
 
 
+def _display_differences_and_wait(old: str, new: str, message: str) -> None:
+    print("\n" + "------------" * 10)
+    print(message)
+    print("------------" * 10)
+    print(f"\n{old}")
+    print("\n->")
+    print(f"\n{new}")
+    input("\nEnter to move on.")
+
+
 def update_custom_datasets_file(interactive=False, version=VERSION, read_only=False):
     """Update custom_datasets.csv file of a specific version of the garden steps.
 
@@ -22,7 +32,7 @@ def update_custom_datasets_file(interactive=False, version=VERSION, read_only=Fa
     interactive : bool, optional
         True to print changes one by one (and to confirm before overwriting file). False to do all changes silently.
     version : _type_, optional
-        Version of the garden steps to consider (where the custom_datasets.csv file to be updated is).
+        Version of the garden steps to consider (where the custom_*.csv file to be updated is).
     read_only : bool, optional
         True to find changes without actually overwriting existing file.
 
@@ -32,6 +42,9 @@ def update_custom_datasets_file(interactive=False, version=VERSION, read_only=Fa
         Updated dataframe of custom datasets.
 
     """
+    if interactive:
+        print(f"\nChecking for changes in FAO dataset titles and descriptions.")
+
     # Path to custom datasets file in garden.
     custom_datasets_file = STEP_DIR / "data/garden/faostat" / version / "custom_datasets.csv"
 
@@ -68,13 +81,9 @@ def update_custom_datasets_file(interactive=False, version=VERSION, read_only=Fa
             # If old and new are not identical (or if they are not both nan) update custom_datasets.
             if (old != new) and not (pd.isna(new) and pd.isna(old)):
                 if interactive:
-                    print("\n" + "------------" * 10)
-                    print(f"Old and new FAO dataset {field} for {dataset_short_name}:")
-                    print("------------" * 10)
-                    print(f"\n{old}")
-                    print("\n->")
-                    print(f"\n{new}")
-                    input("\nEnter to move on.")
+                    _display_differences_and_wait(
+                        old=old, new=new, message=f"Old and new FAO dataset {field} for {dataset_short_name}:"
+                    )
 
                 # Update FAO field.
                 custom_datasets_updated.loc[dataset_short_name, f"fao_dataset_{field}"] = new
@@ -86,7 +95,7 @@ def update_custom_datasets_file(interactive=False, version=VERSION, read_only=Fa
     custom_datasets_updated = custom_datasets_updated.sort_index()
 
     if interactive and not CHANGES_FOUND:
-        print("No changes found.")
+        print("\nNo changes found.")
 
     if CHANGES_FOUND and not read_only:
         if interactive:
@@ -96,6 +105,85 @@ def update_custom_datasets_file(interactive=False, version=VERSION, read_only=Fa
         custom_datasets_updated.sort_index().to_csv(custom_datasets_file)
 
     return custom_datasets_updated
+
+
+def update_custom_elements_and_units_file(interactive=False, version=VERSION, read_only=False):
+    """Update custom_elements_and_units.csv file of a specific version of the garden steps.
+
+    Parameters
+    ----------
+    interactive : bool, optional
+        True to print changes one by one (and to confirm before overwriting file). False to do all changes silently.
+    version : _type_, optional
+        Version of the garden steps to consider (where the custom_*.csv file to be updated is).
+    read_only : bool, optional
+        True to find changes without actually overwriting existing file.
+
+    Returns
+    -------
+    custom_elements_updated : pd.DataFrame
+        Updated dataframe of custom elements and units.
+
+    """
+    if interactive:
+        print(f"\nChecking for changes in FAO element names and descriptions, as well as units and short units.")
+
+    # Path to custom elements and units file in garden.
+    custom_elements_file = STEP_DIR / "data/garden/faostat" / version / "custom_elements_and_units.csv"
+
+    error = f"File custom_elements_and_units.csv not found. Ensure garden steps for version {version} exist."
+    assert custom_elements_file.is_file(), error
+
+    # Load custom elements and units file.
+    custom_elements = pd.read_csv(custom_elements_file, dtype=str).set_index(["dataset", "element_code"])
+
+    # Initialize boolean that is True if there were changes in any field.
+    CHANGES_FOUND = False
+
+    # Initialize a new custom elements and units dataframe.
+    custom_elements_updated = custom_elements.copy()
+
+    # Load metadata from new garden dataset.
+    fao_new_metadata = Dataset(DATA_DIR / "garden/faostat" / version / "faostat_metadata")
+
+    # Go one by one on the datasets for which at least one custom element or unit was defined.
+    for dataset_short_name in custom_elements.index.get_level_values(0).unique():
+        for element_code in custom_elements.loc[dataset_short_name].index.get_level_values(0).unique():
+            new_metadata = fao_new_metadata["elements"].loc[dataset_short_name, element_code].fillna("")
+            old_metadata = custom_elements.loc[dataset_short_name, element_code].fillna("")
+            for field in ["fao_element", "fao_element_description", "fao_unit", "fao_unit_short_name"]:
+                new = new_metadata[field]
+                old = old_metadata[field]
+
+                # If old and new are not identical (or if they are not both nan) update custom_datasets.
+                if (old != new) and not (pd.isna(new) and pd.isna(old)):
+                    if interactive:
+                        _display_differences_and_wait(
+                            old=old,
+                            new=new,
+                            message=f"Old and new {field} for {dataset_short_name} with element code {element_code}:",
+                        )
+
+                # Update FAO field.
+                custom_elements_updated.loc[dataset_short_name, element_code][field] = new
+
+                # There was at least one change.
+                CHANGES_FOUND = True
+
+    # Sort custom element conveniently.
+    custom_elements_updated = custom_elements_updated.sort_values(["fao_element"])
+
+    if interactive and not CHANGES_FOUND:
+        print("\nNo changes found.")
+
+    if CHANGES_FOUND and not read_only:
+        if interactive:
+            input(f"Press enter to overwrite file: {custom_elements_file}")
+
+        # Update custom elements file.
+        custom_elements_updated.to_csv(custom_elements_file)
+
+    return custom_elements_updated
 
 
 if __name__ == "__main__":
@@ -122,3 +210,6 @@ if __name__ == "__main__":
     )
     args = argument_parser.parse_args()
     _ = update_custom_datasets_file(interactive=args.interactive, version=args.version, read_only=args.read_only)
+    _ = update_custom_elements_and_units_file(
+        interactive=args.interactive, version=args.version, read_only=args.read_only
+    )
