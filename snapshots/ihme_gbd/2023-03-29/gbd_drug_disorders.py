b1d1cf43523972d203fcde3598cfce8b77bef02f
spoonerf <spoonerfiona@gmail.com>
2023-03-29 13:31:12 +0100

    refactor(gbd): rolling back the name change

--- snapshots/ihme_gbd/2023-03-29/gbd_drug_disorders.py
+++ snapshots/ihme_gbd/2023-03-29/gbd_drug_disorders.py
@@ -20,7 +20,7 @@ SNAPSHOT_VERSION = Path(__file__).parent.name
 @click.option("--path-to-file", prompt=True, type=str, help="Path to local data file.")
 def main(path_to_file: str, upload: bool) -> None:
     # Create a new snapshot.
-    snap = Snapshot(f"ihme/{SNAPSHOT_VERSION}/gbd_drug_disorders.csv")
+    snap = Snapshot(f"ihme_gbd/{SNAPSHOT_VERSION}/gbd_drug_disorders.csv")
 
     # Ensure destination folder exists.
     snap.path.parent.mkdir(exist_ok=True, parents=True)
