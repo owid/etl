8ff74e49dc041419e0b467a2f4cbaaa763789d01
Pablo Rosado <pabloarosado@gmail.com>
2023-04-12 11:40:28 +0200

    feat(faostat): Add table for percentage of sustainable and overexploited fish to grapher dataset

--- etl/steps/data/grapher/faostat/2023-02-22/additional_variables.py
+++ etl/steps/data/grapher/faostat/2023-02-22/additional_variables.py
@@ -17,6 +17,7 @@ def run(dest_dir: str) -> None:
 
     # Read tables from garden dataset.
     tb_arable_land_per_crop_output = ds_garden["arable_land_per_crop_output"]
+    tb_sustainable_and_overexploited_fish = ds_garden["share_of_sustainable_and_overexploited_fish"]
     # To insert table into grapher DB, change "item" column to "country" (which will be changed back in the admin).
     tb_area_used_per_crop_type = ds_garden["area_used_per_crop_type"].reset_index().rename(columns={"item": "country"})
 
@@ -30,7 +31,7 @@ def run(dest_dir: str) -> None:
     # Create a new grapher dataset with the same metadata as the garden dataset.
     ds_grapher = create_dataset(
         dest_dir,
-        tables=[tb_arable_land_per_crop_output, tb_area_used_per_crop_type],
+        tables=[tb_arable_land_per_crop_output, tb_area_used_per_crop_type, tb_sustainable_and_overexploited_fish],
         default_metadata=ds_garden.metadata,
     )
 
