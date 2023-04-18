548dcbf9944e68e04323646fd8c28c96190db614
Mojmir Vinkler <mojmir.vinkler@gmail.com>
2023-04-18 12:07:07 +0200

    fix(data): exclude outs when saving snapshot & fix excess mortality (#1010)
    * fix(data): exclude outs when saving snapshot & fix excess mortality
    
    * fix(xm): paths to snapshot file
    
    * fix(xm): snapshot metadata
    
    * fix(xm): snapshot
    
    ---------
    
    Co-authored-by: lucasrodes <lucasrodes@users.noreply.github.com>

--- snapshots/excess_mortality/latest/xm_karlinsky_kobak.py
+++ snapshots/excess_mortality/latest/xm_karlinsky_kobak.py
@@ -1,5 +1,5 @@
 """Script to create a snapshot of dataset 'Excess mortality during the COVID-19 pandemic'."""
-from datetime import datetime
+from datetime import date
 from pathlib import Path
 
 import click
@@ -21,14 +21,14 @@ def main(upload: bool) -> None:
     # Data aggregated on all ages
     add_snapshot(f"excess_mortality/{SNAPSHOT_VERSION}/xm_karlinsky_kobak.csv", upload)
     # Data broken down by ages
-    add_snapshot(f"excess_mortality/{SNAPSHOT_VERSION}/xm_karlinsky_kobak.ages.csv", upload)
+    add_snapshot(f"excess_mortality/{SNAPSHOT_VERSION}/xm_karlinsky_kobak_ages.csv", upload)
 
 
 def add_snapshot(uri: str, upload: bool):
     # Load snapshot
     snap = Snapshot(uri)
     # Add date_accessed
-    snap.metadata.date_accessed = datetime.today()
+    snap.metadata.date_accessed = date.today()
     snap.metadata.save()
     # Download file
     snap.download_from_source()
