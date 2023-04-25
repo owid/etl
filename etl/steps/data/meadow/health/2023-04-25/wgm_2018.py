3f6ccef3edb05da01696b420519bb7ab2a62612a
lucasrodes <lucasrodes@users.noreply.github.com>
2023-04-26 00:34:29 +0200

    data(wgm_2018): add meadow

--- etl/steps/data/meadow/health/2023-04-25/wgm_2018.py
+++ etl/steps/data/meadow/health/2023-04-25/wgm_2018.py
@@ -1,4 +1,8 @@
-"""Load a snapshot and create a meadow dataset."""
+"""Load a snapshot and create a meadow dataset.
+
+Formatting of the data file. We also map COUNTRY IDs to COUNTRY NAMES. This facilitates the country harmonization proces in Garden."""
+
+from typing import Dict
 
 import pandas as pd
 from owid.catalog import Table
@@ -23,23 +27,23 @@ def run(dest_dir: str) -> None:
     # Retrieve snapshot.
     snap: Snapshot = paths.load_dependency("wgm_2018.xlsx")
 
-    # Load data from snapshot.
+    # Load all data from snapshot.
     log.info("wgm_2018: load data")
     dfs = pd.read_excel(snap.path, sheet_name=None)
-
-    # Data
+    # Load data
     df = dfs["Full dataset"]
-    # Set dtype for all questions to str
-    columns = [col for col in df.columns if col not in ["wgt", "projwt"]]
-    df[columns] = df[columns].astype(str)
-    # Metadata
+    df = df.astype({column: str for column in df.columns if column not in ["wgt", "projwt"]})
+    # Load metadata
     df_metadata = dfs["Data dictionary"]
+
     #
     # Process data.
     #
-    # Create a new table and ensure all columns are snake-case.
     log.info("wgm_2018: create tables")
+    # Data table
+    df = add_country_column(df, df_metadata)
     tb = Table(df, short_name=paths.short_name, underscore=True)
+    # Metadata table
     tb_meta = Table(df_metadata, short_name=f"{paths.short_name}_metadata", underscore=True)
     #
     # Save outputs.
@@ -52,3 +56,26 @@ def run(dest_dir: str) -> None:
     ds_meadow.save()
 
     log.info("wgm_2018: end")
+
+
+def add_country_column(df: pd.DataFrame, df_meta: pd.DataFrame) -> pd.DataFrame:
+    log.info("wgm_2018: mapping COUNTRY IDs to COUNTRY NAMEs")
+    # Get COUNTRY ID to COUNTRY NAME mapping
+    mapping = _build_mapping_country_id_to_country_name(df_meta)
+    # Add country column
+    df["country"] = df["WP5"].map(mapping)
+    assert df["country"].isna().sum() == 0, "Some country IDs were not mapped to a name!"
+    return df
+
+
+def _build_mapping_country_id_to_country_name(df: pd.DataFrame) -> Dict[str, str]:
+    print(df.head())
+    # Build mapping COUNTRY ID to COUNTRY NAME
+    MAPPING_COUNTRY_VALUES = {}
+    mapping_str = df.loc[(df["Variable Name"] == "WP5"), "Variable Type & Codes*"].item()
+    mapping_list = mapping_str.split(", ")
+    for mapping in mapping_list:
+        if mapping:
+            country_id, country_name = mapping.split("=")
+            MAPPING_COUNTRY_VALUES[country_id] = country_name
+    return MAPPING_COUNTRY_VALUES
