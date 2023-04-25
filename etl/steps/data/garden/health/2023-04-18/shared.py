0d301aae9f39ec149989f1a7eb06c1f165b28e55
lucasrodes <lucasrodes@users.noreply.github.com>
2023-04-25 12:45:42 +0200

    enhance(wgm): typos

--- etl/steps/data/garden/health/2023-04-18/shared.py
+++ etl/steps/data/garden/health/2023-04-18/shared.py
@@ -103,7 +103,7 @@ question_mapping = {
         },
     },
     "mh4a": {
-        "title": "Important for national govt to fund research on cancer",
+        "title": "Important for national government to fund research on cancer",
         "answers": {
             "1": "Extremely important",
             "2": "Somewhat important",
@@ -113,7 +113,7 @@ question_mapping = {
         },
     },
     "mh4b": {
-        "title": "Important for national govt to fund research on anxiety/depression",
+        "title": "Important for national government to fund research on anxiety/depression",
         "answers": {
             "1": "Extremely important",
             "2": "Somewhat important",
@@ -235,7 +235,7 @@ question_mapping = {
         },
     },
     "mh9a": {
-        "title": "How helpful was talking to mental health professional when anxious/depressed",
+        "title": "How helpful was talking to mental health professional when anxious/depressed?",
         "answers": {
             "1": "Very helpful",
             "2": "Somewhat helpful",
@@ -244,7 +244,7 @@ question_mapping = {
         },
     },
     "mh9b": {
-        "title": "How helpful was engaging in religious or spiritual activities when anxious/depressed",
+        "title": "How helpful was engaging in religious or spiritual activities when anxious/depressed?",
         "answers": {
             "1": "Very helpful",
             "2": "Somewhat helpful",
@@ -253,7 +253,7 @@ question_mapping = {
         },
     },
     "mh9c": {
-        "title": "How helpful was talking to friends or family when anxious/depressed",
+        "title": "How helpful was talking to friends or family when anxious/depressed?",
         "answers": {
             "1": "Very helpful",
             "2": "Somewhat helpful",
@@ -396,7 +396,7 @@ question_mapping = {
         },
     },
     "w5f": {
-        "title": "Trust in people working for NGOs/charitable orgs in your country",
+        "title": "Trust in people working for NGOs/charitable organizations in your country",
         "answers": {
             "1": "A lot",
             "2": "Some",
@@ -446,7 +446,7 @@ question_mapping = {
         },
     },
     "w7c": {
-        "title": "Trust leaders in the national government to value opinmions and expertise of scientists",
+        "title": "Trust leaders in the national government to value opinions and expertise of scientists",
         "answers": {
             "1": "A lot",
             "2": "Some",
@@ -456,7 +456,7 @@ question_mapping = {
         },
     },
     "w8": {
-        "title": "Work from scientists in your country benefits most, some of very few people",
+        "title": "Work from scientists in your country benefits most, some, or very few people",
         "answers": {
             "1": "Most",
             "2": "Some",
@@ -531,7 +531,7 @@ question_mapping = {
         },
     },
     "w15_1a": {
-        "title": "Based decisions about coronavirus on scientific advice: National government",
+        "title": "Based decisions about coronavirus on scientific advice from the National government",
         "answers": {
             "1": "A lot",
             "2": "Some",
@@ -541,7 +541,7 @@ question_mapping = {
         },
     },
     "w15_1b": {
-        "title": "Based decisions about coronavirus on scientific advice: Friends and family",
+        "title": "Based decisions about coronavirus on scientific advice from friends and family",
         "answers": {
             "1": "A lot",
             "2": "Some",
@@ -551,7 +551,7 @@ question_mapping = {
         },
     },
     "w15_1c": {
-        "title": "Based decisions about coronavirus on scientific advice: WHO",
+        "title": "Based decisions about coronavirus on scientific advice from the WHO",
         "answers": {
             "1": "A lot",
             "2": "Some",
@@ -561,7 +561,7 @@ question_mapping = {
         },
     },
     "w15_1d": {
-        "title": "Based decisions about coronavirus on scientific advice: Doctors and nurses in your country",
+        "title": "Based decisions about coronavirus on scientific advice from doctors and nurses in your country",
         "answers": {
             "1": "A lot",
             "2": "Some",
@@ -571,7 +571,7 @@ question_mapping = {
         },
     },
     "w15_1e": {
-        "title": "Based decisions about coronavirus on scientific advice: Religious leaders",
+        "title": "Based decisions about coronavirus on scientific advice from religious leaders",
         "answers": {
             "1": "A lot",
             "2": "Some",
@@ -591,7 +591,7 @@ question_mapping = {
         },
     },
     "w15_2b": {
-        "title": "After the COVID crisis ends, your government should spend money on preventing and curing dieases ONLY if they pose a risk to poeple in your country",
+        "title": "After the COVID crisis ends, your government should spend money on preventing and curing diseases ONLY if they pose a risk to people in your country",
         "answers": {
             "1": "Strongly agree",
             "2": "Somewhat agree",
