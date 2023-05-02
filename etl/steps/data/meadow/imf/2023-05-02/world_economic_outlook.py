bcc3cc6ca191963d87b1636cba6814a1dc4ed53f
Edouard Mathieu <edmat@pm.me>
2023-05-02 14:35:24 +0200

    feat(world_economic_outlook): add meadow

---
+++ etl/steps/data/meadow/imf/2023-05-02/world_economic_outlook.py
@@ -0,0 +1,81 @@
+"""Load a snapshot and create a meadow dataset."""
+
+import pandas as pd
+from owid.catalog import Table
+from structlog import get_logger
+
+from etl.helpers import PathFinder, create_dataset
+from etl.snapshot import Snapshot
+
+# Initialize logger.
+log = get_logger()
+
+# Get paths and naming conventions for current step.
+paths = PathFinder(__file__)
+
+VARIABLE_LIST = ["Gross domestic product, constant prices - Percent change"]
+
+
+def read(path: str) -> pd.DataFrame:
+    df = (
+        pd.read_csv(path, delimiter="\t", encoding="utf-16-le")
+        .drop(
+            columns=[
+                "WEO Country Code",
+                "WEO Subject Code",
+                "Estimates Start After",
+                "ISO",
+                "Country/Series-specific Notes",
+                "Subject Notes",
+                "Scale",
+            ]
+        )
+        .dropna(subset=["Country"])
+    )
+    df = df.loc[:, ~df.columns.str.contains("Unnamed")]
+    return df
+
+
+def make_variable_names(df: pd.DataFrame) -> pd.DataFrame:
+    df["variable"] = df["Subject Descriptor"] + " - " + df["Units"]
+    df = df.drop(columns=["Subject Descriptor", "Units"])
+    return df
+
+
+def pick_variables(df: pd.DataFrame) -> pd.DataFrame:
+    return df[df.variable.isin(VARIABLE_LIST)]
+
+
+def reshape(df: pd.DataFrame) -> pd.DataFrame:
+    return (
+        df.melt(id_vars=["Country", "variable"], var_name="year")
+        .pivot(index=["Country", "year"], columns="variable", values="value")
+        .reset_index()
+    )
+
+
+def run(dest_dir: str) -> None:
+    log.info("world_economic_outlook.start")
+
+    #
+    # Load inputs.
+    #
+    # Retrieve snapshot.
+    snap: Snapshot = paths.load_dependency("world_economic_outlook.xls")
+
+    # Load data from snapshot, then process.
+    df = read(snap.path).pipe(make_variable_names).pipe(pick_variables).pipe(reshape)
+
+    # Create a new table and ensure all columns are snake-case.
+    tb = Table(df, short_name=paths.short_name, underscore=True)
+
+    #
+    # Save outputs.
+    #
+    # Create a new meadow dataset with the same metadata as the snapshot.
+    ds_meadow = create_dataset(dest_dir, tables=[tb], default_metadata=snap.metadata)
+
+    # Save changes in the new garden dataset.
+    ds_meadow.save()
+
+    log.info("world_economic_outlook.end")
