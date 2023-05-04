6fa2ffe9268b97517510ab1fdd3beb76d1f671ef
lucasrodes <lucasrodes@users.noreply.github.com>
2023-05-04 11:13:06 +0200

    fix(wgm): failed to estimate new categories

--- etl/steps/data/garden/health/2023-04-25/wgm_2018.py
+++ etl/steps/data/garden/health/2023-04-25/wgm_2018.py
@@ -203,7 +203,8 @@ def map_ids_to_labels(df: pd.DataFrame) -> pd.DataFrame:
 
     # Map IDs to Labels (Question, Answer, Gender, Age group)
     log.info("wgm_2018: mapping ids to labels}")
-    df["question"] = df["question"].replace(question_id_to_label)
+    df["question_code"] = df["question"]
+    df["question"] = df["question_code"].replace(question_id_to_label)
     df["answer"] = df["question__answer"].map(question_answer_id_to_label).fillna("Unknown")
     df["gender"] = df["gender"].replace(MAPPING_GENDER_VALUES)
     return df
@@ -238,7 +239,7 @@ def create_agree_and_disagree_categories(df: pd.DataFrame) -> pd.DataFrame:
     }
     for q in questions_merge_agree_disagree:
         # Keep only rows of affected questions
-        df_ = df[df["question"] == MAPPING_QUESTION_VALUES[q]["title"]].copy()
+        df_ = df[df["question_code"] == q].copy()
         # Keep only affected answers
         df_ = df_[df_["answer"].isin(list(mapping.keys()))]
         # Map categories to new categories
