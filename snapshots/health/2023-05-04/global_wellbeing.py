67f277b3c58ef690b2a0449483438faf83e8822d
lucasrodes <lucasrodes@users.noreply.github.com>
2023-05-04 22:02:39 +0200

    data(gwi): snapshot

--- snapshots/health/2023-05-04/global_wellbeing.py
+++ snapshots/health/2023-05-04/global_wellbeing.py
@@ -28,7 +28,7 @@ SNAPSHOT_VERSION = Path(__file__).parent.name
 @click.option("--path-to-file", prompt=True, type=str, help="Path to local data file.")
 def main(path_to_file: str, upload: bool) -> None:
     # Create a new snapshot.
-    snap = Snapshot(f"health/{SNAPSHOT_VERSION}/gallup.")
+    snap = Snapshot(f"health/{SNAPSHOT_VERSION}/global_wellbeing.xlsx")
 
     # Ensure destination folder exists.
     snap.path.parent.mkdir(exist_ok=True, parents=True)
