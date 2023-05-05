e3f61220b943edab8a761e0b992420a36cc034a7
lucasrodes <lucasrodes@users.noreply.github.com>
2023-05-05 14:02:03 +0200

    fix(gwi): add index table to grapher

--- etl/steps/data/grapher/health/2023-05-04/global_wellbeing.py
+++ etl/steps/data/grapher/health/2023-05-04/global_wellbeing.py
@@ -17,7 +17,7 @@ def run(dest_dir: str) -> None:
 
     # Read table from garden dataset.
     tb_questions = ds_garden["global_wellbeing"]
-    # tb_index = ds_garden["global_wellbeing_index"]
+    tb_index = ds_garden["global_wellbeing_index"]
     #
     # Process data.
     #
@@ -28,7 +28,7 @@ def run(dest_dir: str) -> None:
     # Create a new grapher dataset with the same metadata as the garden dataset.
     tables = [
         tb_questions,
-        # tb_index,  # I think this leads to an error in Grapher, due to duplicate entries for the same country and year.
+        tb_index,  # I think this leads to an error in Grapher, due to duplicate entries for the same country and year.
     ]
     ds_grapher = create_dataset(dest_dir, tables=tables, default_metadata=ds_garden.metadata)
 
