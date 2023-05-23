eba46c1952fe0f1ef0ce3debfbee7389b38f865a
lucasrodes <lucasrodes@users.noreply.github.com>
2023-05-23 11:29:52 +0200

    ⏪ geo: population dataset downgrade

--- etl/data_helpers/geo.py
+++ etl/data_helpers/geo.py
@@ -31,8 +31,9 @@ NUM_ALLOWED_NANS_PER_YEAR = None
 
 # Paths to datasets used in this module. Feel free to update the versions or paths whenever there is a
 # new version of the datasets.
-# Path to Key Indicators dataset
-DATASET_POPULATION = DATA_DIR / "garden" / "demography" / "2023-03-31" / "population"
+# Path to Key Indicators dataset (TODO: should change it)
+# DATASET_POPULATION = DATA_DIR / "garden" / "demography" / "2023-03-31" / "population"
+DATASET_POPULATION = DATA_DIR / "garden" / "owid" / "latest" / "key_indicators"
 TNAME_KEY_INDICATORS = "population"
 # Path to Key Indicators dataset
 # TODO: we should update it to latest garden/wb/*/income_groups dataset
