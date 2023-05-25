eb29abd131880d867da2f59ffd9923c441f481ac
veronikasamborska1994 <veronika@ourworldindata.org>
2023-05-25 11:19:49 +0100

    set to inherit garden metadata from meadow

--- etl/steps/data/garden/itopf/2023-05-18/oil_spills.py
+++ etl/steps/data/garden/itopf/2023-05-18/oil_spills.py
@@ -60,7 +60,7 @@ def run(dest_dir: str) -> None:
     # Save outputs.
     #
     # Create a new garden dataset with the same metadata as the meadow dataset.
-    ds_garden = create_dataset(dest_dir, tables=[tb_garden], default_metadata=None)
+    ds_garden = create_dataset(dest_dir, tables=[tb_garden], default_metadata=ds_meadow.metadata)
 
     # Save changes in the new garden dataset.
     ds_garden.save()
