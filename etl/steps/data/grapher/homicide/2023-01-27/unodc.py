1d867ccbf1a7bebff5809bd29de7274d53978e2a
Fiona Spooner <fiona@ourworldindata.org>
2023-05-25 13:54:58 +0100

    📊 (UNODC): Adding further tables and data disaggregations (#1153)
    * refactor(unodc): creating separate tables
    
    * refactor(homicide): adding in table for homicides by perpatrator
    
    * tidy
    
    * feat(homicide): adding situational context
    
    * refactor(homicide): adding description of what another weapon is
    
    * fix(homicide): fixing dependencies
    
    * fix(homicide): reset index in long run homicide omm

--- etl/steps/data/grapher/homicide/2023-01-27/unodc.py
+++ etl/steps/data/grapher/homicide/2023-01-27/unodc.py
@@ -6,15 +6,12 @@ paths = PathFinder(__file__)
 
 
 def run(dest_dir: str) -> None:
-    dataset = catalog.Dataset.create_empty(dest_dir, paths.garden_dataset.metadata)
+    dataset = catalog.Dataset.create_empty(dest_dir, paths.meadow_dataset.metadata)
 
-    table = paths.garden_dataset["unodc"]
-
-    # optionally set additional dimensions
-    # table = table.set_index(["sex", "income_group"], append=True)
-
+    table_names = paths.garden_dataset.table_names
     # if your data is in long format, you can use `grapher_helpers.long_to_wide_tables`
     # to get into wide format
-    dataset.add(table)
-
-    dataset.save()
+    for table_name in table_names:
+        table = paths.garden_dataset[table_name]
+        dataset.add(table)
+        dataset.save()
