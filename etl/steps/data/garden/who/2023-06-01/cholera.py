bb719bd812f1626491e23b64dddf164314d03b2e
spoonerf <spoonerfiona@gmail.com>
2023-06-01 11:49:27 +0100

    chore(cholera): tidy

--- etl/steps/data/garden/who/2023-06-01/cholera.py
+++ etl/steps/data/garden/who/2023-06-01/cholera.py
@@ -24,8 +24,8 @@ def run(dest_dir: str) -> None:
     who_gh_dataset: Dataset = paths.load_dependency(short_name)
     who_gh = who_gh_dataset[short_name].reset_index()
 
+    # Process backport dataset
     cholera_cols = who_gh.columns[who_gh.columns.str.contains("cholera")].to_list()
-
     cholera_bp = who_gh[["year", "entity_name"] + cholera_cols]
     cholera_bp[cholera_cols] = cholera_bp[cholera_cols].apply(pd.to_numeric, errors="coerce")
     cholera_bp = cholera_bp.dropna(how="all", axis=0, subset=cholera_cols).rename(
@@ -40,13 +40,14 @@ def run(dest_dir: str) -> None:
         df=cholera_bp, countries_file=paths.country_mapping_path, excluded_countries_file=paths.excluded_countries_path
     )
 
+    # Add global aggregate
     cholera_bp = add_global_total(cholera_bp)
 
     # Load fast track dataset
     snap: Dataset = paths.load_dependency("cholera.csv")
     cholera_ft = pd.read_csv(snap.path)
 
-    # Combine backport and fast track
+    # Combine backport and fast track datasets
     cholera_combined = pd.concat([cholera_bp, cholera_ft]).set_index(["country", "year"])
     tb_garden = Table(cholera_combined, short_name="cholera")
 
@@ -54,8 +55,6 @@ def run(dest_dir: str) -> None:
     #
     # Create a new garden dataset with the same metadata as the meadow dataset.
     ds_garden = create_dataset(dest_dir, tables=[tb_garden])
-
-    # ds_garden.update_metadata(paths.metadata_path)
 
     # Save changes in the new garden dataset.
     ds_garden.save()
