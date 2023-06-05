48b93d265514d272ef040669b15e42b18efa5903
spoonerf <spoonerfiona@gmail.com>
2023-06-05 11:35:15 +0100

    update

--- etl/steps/data/garden/who/2023-06-01/cholera.py
+++ etl/steps/data/garden/who/2023-06-01/cholera.py
@@ -24,6 +24,14 @@ def run(dest_dir: str) -> None:
     who_gh_dataset: Dataset = paths.load_dependency(short_name)
     who_gh = who_gh_dataset[short_name].reset_index()
 
+    # Load fast track dataset
+    snap: Dataset = paths.load_dependency("cholera.csv")
+    cholera_ft = pd.read_csv(snap.path)
+
+    # Load countries regions
+    regions_dataset: Dataset = paths.load_dependency("regions")
+    regions = regions_dataset["regions"]
+
     # Process backport dataset
     cholera_cols = who_gh.columns[who_gh.columns.str.contains("cholera")].to_list()
     cholera_bp = who_gh[["year", "entity_name"] + cholera_cols]
@@ -39,17 +47,14 @@ def run(dest_dir: str) -> None:
     cholera_bp = geo.harmonize_countries(
         df=cholera_bp, countries_file=paths.country_mapping_path, excluded_countries_file=paths.excluded_countries_path
     )
-
     # Add global aggregate
-    cholera_bp = add_global_total(cholera_bp)
-
-    # Load fast track dataset
-    snap: Dataset = paths.load_dependency("cholera.csv")
-    cholera_ft = pd.read_csv(snap.path)
-
+    cholera_bp = add_global_total(cholera_bp, regions)
     # Combine backport and fast track datasets
-    cholera_combined = pd.concat([cholera_bp, cholera_ft]).set_index(["country", "year"])
-    tb_garden = Table(cholera_combined, short_name="cholera")
+    cholera_combined = pd.concat([cholera_bp, cholera_ft])
+
+    cholera_combined = add_regions(cholera_combined, regions)
+
+    tb_garden = Table(cholera_combined.set_index(["country", "year"], verify_integrity=True), short_name="cholera")
 
     # Save outputs.
     #
@@ -62,13 +67,13 @@ def run(dest_dir: str) -> None:
     log.info("cholera.end")
 
 
-def add_global_total(df: pd.DataFrame) -> pd.DataFrame:
+def add_global_total(df: pd.DataFrame, regions: Table) -> pd.DataFrame:
     """
     Calculate global total of cholera cases and add it to the existing dataset
     """
-    countries_regions = geo._load_countries_regions()
-    countries = countries_regions[countries_regions["region_type"] == "country"]["name"].to_list()
-    manual_countries_to_allow = ["The former state union Serbia and Montenegro"]
+
+    countries = regions[regions["region_type"] == "country"]["name"].to_list()
+    manual_countries_to_allow = ["Serbia and Montenegro (former)"]
     countries = countries + manual_countries_to_allow
     assert all(
         df["country"].isin(countries)
@@ -77,4 +82,39 @@ def add_global_total(df: pd.DataFrame) -> pd.DataFrame:
     df_glob["country"] = "World"
     df_glob["cholera_case_fatality_rate"] = (df_glob["cholera_deaths"] / df_glob["cholera_reported_cases"]) * 100
     df = pd.concat([df, df_glob])
+
+    return df
+
+
+def add_regions(df: pd.DataFrame, regions: Table) -> pd.DataFrame:
+    continents = regions[regions["region_type"] == "continent"]["name"].to_list()
+
+    countries_in_regions = {
+        region: sorted(set(geo.list_countries_in_region(region)) & set(df["country"])) for region in continents
+    }
+    df_cont = df
+    df_out = pd.DataFrame()
+    for continent in continents:
+        if continent == "Europe":
+            df_cont = geo.add_region_aggregates(
+                df=df[["year", "country", "cholera_reported_cases", "cholera_deaths"]],
+                region=continent,
+                countries_in_region=countries_in_regions[continent] + ["Serbia and Montenegro (former)"],
+                countries_that_must_have_data=[],
+                num_allowed_nans_per_year=None,
+            )
+        else:
+            df_cont = geo.add_region_aggregates(
+                df=df[["year", "country", "cholera_reported_cases", "cholera_deaths"]],
+                region=continent,
+                countries_in_region=countries_in_regions[continent],
+                countries_that_must_have_data=[],
+                num_allowed_nans_per_year=None,
+            )
+        df_cont = df_cont[df_cont["country"].isin(continents)]
+        df_out = pd.concat([df_out, df_cont])
+    df_out["cholera_case_fatality_rate"] = (df_out["cholera_deaths"] / df_out["cholera_reported_cases"]) * 100
+
+    df = pd.concat([df, df_out])
+
     return df
