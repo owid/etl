470ab3e2b7c51ed9dd7430b96ddb905571e51980
spoonerf <spoonerfiona@gmail.com>
2023-06-08 11:17:32 +0100

    refactor(ghe): adding regional and global totals to ghe and removing junk code from cholera

--- etl/steps/data/garden/who/2022-09-30/ghe.py
+++ etl/steps/data/garden/who/2022-09-30/ghe.py
@@ -7,6 +7,7 @@ from owid.catalog import Dataset, Table
 from owid.datautils import dataframes
 from structlog import get_logger
 
+from etl.data_helpers import geo
 from etl.data_helpers.population import add_population
 from etl.helpers import PathFinder, create_dataset
 
@@ -45,6 +46,10 @@ def run(dest_dir: str) -> None:
     ds_meadow = N.meadow_dataset
     df = pd.DataFrame(ds_meadow["ghe"])
 
+    # Load countries regions
+    regions_dataset: Dataset = N.load_dependency("regions")
+    regions = regions_dataset["regions"]
+
     # convert codes to country names
     code_to_country = cast(Dataset, N.load_dependency("regions"))["regions"]["name"].to_dict()
     df["country"] = dataframes.map_series(df["country"], code_to_country, warn_on_missing_mappings=True)
@@ -64,6 +69,9 @@ def clean_data(df: pd.DataFrame) -> pd.DataFrame:
     df = combine_drug_and_alcohol(df)
     # Add broader age groups
     df = add_age_groups(df)
+    # Add global and regional values
+    df = add_global_total(df)
+    df = add_regions(df)
     # Set indices
     df = df.astype(
         {
@@ -218,94 +226,50 @@ def add_age_groups(df: pd.DataFrame) -> pd.DataFrame:
     return df_combined
 
 
-def build_custom_self_harm_age_groups(df: pd.DataFrame) -> pd.DataFrame:
-    """Estimate all the metrics for a broader age group."""
-    df = pd.DataFrame(df)
+def add_global_total(df: pd.DataFrame, regions: Table) -> pd.DataFrame:
+    """
+    Calculate global total of cholera cases and add it to the existing dataset
+    """
 
-    # Get two dfs: one with cause=self-harm, the other with the rest
-    log.info("ghe: separate self-harm from other causes")
-    msk = (df["cause"].isin(["Self-harm"])) & (
-        # The following age groups are passed as they are, without assigning them to broader age groups.
-        ~df["age_group"].isin(
-            [
-                "ALLAges",
-                "YEARS85PLUS",
-                "YEARS15-19",
-                "YEARS20-24",
-            ]
+    countries = regions[regions["region_type"] == "country"]["name"].to_list()
+    assert all(
+        df["country"].isin(countries)
+    ), f"{df['country'][~df['country'].isin(countries)].drop_duplicates()}, is not a country"
+    df_glob = (
+        df.groupby(["year", "age_group", "sex", "cause", "flag_level"])
+        .agg({"daly_count": "sum", "death_count": "sum"})
+        .reset_index()
+    )
+    df_glob["country"] = "World"
+    df_glob = calculate_rates(df_glob)
+    df = pd.concat([df, df_glob])
+
+    return df
+
+
+def add_regions(df: pd.DataFrame, regions: Table) -> pd.DataFrame:
+    continents = regions[regions["region_type"] == "continent"]["name"].to_list()
+
+    countries_in_regions = {
+        region: sorted(set(geo.list_countries_in_region(region)) & set(df["country"])) for region in continents
+    }
+    df_cont = df
+    df_out = pd.DataFrame()
+    for continent in continents:
+        df_cont = geo.add_region_aggregates(
+            df=df[["year", "age_group", "sex", "cause", "flag_level"]],
+            region=continent,
+            countries_in_region=countries_in_regions[continent],
+            countries_that_must_have_data=[],
+            num_allowed_nans_per_year=None,
+            country_col="country",
+            year_col="year",
+            frac_allowed_nans_per_year=1,
         )
-    )
-    df_sh = df[msk].reset_index(drop=True).copy()
-    df = df[~msk].reset_index(drop=True).copy()
+        df_cont = df_cont[df_cont["country"].isin(continents)]
+        df_out = pd.concat([df_out, df_cont])
+    df_out = calculate_rates(df_out)
 
-    # Add population values for each dimension
-    log.info("ghe: add population values")
-    AGE_GROUPS_RANGES = {
-        "YEARS0-1": [0, 1],
-        "YEARS1-4": [1, 4],
-        "YEARS5-9": [5, 9],
-        "YEARS10-14": [10, 14],
-        # "YEARS15-19": [15, 19],
-        # "YEARS20-24": [20, 24],
-        "YEARS25-29": [25, 29],
-        "YEARS30-34": [30, 34],
-        "YEARS35-39": [35, 39],
-        "YEARS40-44": [40, 44],
-        "YEARS45-49": [45, 49],
-        "YEARS50-54": [50, 54],
-        "YEARS55-59": [55, 59],
-        "YEARS60-64": [60, 64],
-        "YEARS65-69": [65, 69],
-        "YEARS70-74": [70, 74],
-        "YEARS75-79": [75, 79],
-        "YEARS80-84": [80, 84],
-        # "YEARS85PLUS": [85, None],
-    }
-    df_sh = add_population(
-        df=df_sh,
-        country_col="country",
-        year_col="year",
-        sex_col="sex",
-        sex_group_all="Both sexes",
-        sex_group_female="Female",
-        sex_group_male="Male",
-        age_col="age_group",
-        age_group_mapping=AGE_GROUPS_RANGES,
-    )
-    # Map age groups to broader age groups. Missing age groups in the list are passed as they are (no need to assign to broad group)
-    log.info("ghe: create broader age groups")
-    AGE_GROUPS = {
-        "YEARS0-1": "YEARS0-14",
-        "YEARS1-4": "YEARS0-14",
-        "YEARS5-9": "YEARS0-14",
-        "YEARS10-14": "YEARS0-14",
-        # "YEARS15-19": "YEARS15-24",
-        # "YEARS20-24": "YEARS15-24",
-        "YEARS25-29": "YEARS25-34",
-        "YEARS30-34": "YEARS25-34",
-        "YEARS35-39": "YEARS35-44",
-        "YEARS40-44": "YEARS35-44",
-        "YEARS45-49": "YEARS45-54",
-        "YEARS50-54": "YEARS45-54",
-        "YEARS55-59": "YEARS55-64",
-        "YEARS60-64": "YEARS55-64",
-        "YEARS65-69": "YEARS65-74",
-        "YEARS70-74": "YEARS65-74",
-        "YEARS75-79": "YEARS75-84",
-        "YEARS80-84": "YEARS75-84",
-        # "YEARS85PLUS": "YEARS85PLUS",
-    }
-    df_sh["age_group"] = df_sh["age_group"].map(AGE_GROUPS)
+    df = pd.concat([df, df_out])
 
-    # Sum
-    log.info("ghe: re-estimate metrics")
-    df_sh = df_sh.groupby(["country", "year", "age_group", "sex", "cause"], as_index=False, observed=True).sum()
-    # Fix column values (rates + flag)
-    df_sh["daly_rate100k"] = 100000 * df_sh["daly_count"] / df_sh["population"]
-    df_sh["death_rate100k"] = 100000 * df_sh["death_count"] / df_sh["population"]
-    df_sh["flag_level"] = 3
-    df_sh = df_sh.drop(columns=["population"])
-
-    log.info("ghe: concatenate dfs")
-    df = pd.concat([df, df_sh], axis=0, ignore_index=True, sort=False)
     return df
