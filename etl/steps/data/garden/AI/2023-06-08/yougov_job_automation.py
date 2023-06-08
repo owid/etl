00e9096f2b5aee07770bce65df8e344713d9165b
veronikasamborska1994 <veronika@ourworldindata.org>
2023-06-08 12:06:37 +0100

    added yougov surveys

---
+++ etl/steps/data/garden/AI/2023-06-08/yougov_job_automation.py
@@ -0,0 +1,51 @@
+"""Load a meadow dataset and create a garden dataset."""
+
+from typing import cast
+
+import pandas as pd
+from owid.catalog import Dataset, Table
+from structlog import get_logger
+
+from etl.helpers import PathFinder, create_dataset
+
+log = get_logger()
+
+# Get paths and naming conventions for current step.
+paths = PathFinder(__file__)
+
+
+def run(dest_dir: str) -> None:
+    log.info("yougov_job_automation.start")
+    #
+    # Load inputs.
+    #
+    # Load meadow dataset.
+    ds_meadow = cast(Dataset, paths.load_dependency("yougov_job_automation"))
+
+    # Read table from meadow dataset.
+    tb = ds_meadow["yougov_job_automation"]
+    df = pd.DataFrame(tb)
+
+    # Create a date column (counting days since 2021-01-01)
+    df["days_since_2021"] = (
+        pd.to_datetime(df["year"].astype(str), format="%Y-%m-%d") - pd.to_datetime("2021-01-01")
+    ).dt.days
+    df = df.drop("year", axis=1)
+    df.rename(columns={"days_since_2021": "year"}, inplace=True)
+
+    # Create a pivot table for each demographic group
+    pivot_df = df.pivot_table(index=[df.columns[0], "year"], columns="group", values="value").reset_index()
+    pivot_df = pivot_df.rename_axis(None, axis=1)
+    pivot_df.rename(columns={pivot_df.columns[0]: "country"}, inplace=True)
+
+    tb = Table(pivot_df, short_name="yougov_job_automation", underscore=True)
+    #
+    # Save outputs.
+    #
+    # Create a new garden dataset with the same metadata as the meadow dataset.
+    ds_garden = create_dataset(dest_dir, tables=[tb], default_metadata=ds_meadow.metadata)
+
+    # Save changes in the new garden dataset.
+    ds_garden.save()
+
+    log.info("yougov_job_automation.end")
---
+++ etl/steps/data/meadow/AI/2023-06-08/yougov_job_automation.py
@@ -0,0 +1,47 @@
+"""Load a snapshot and create a meadow dataset."""
+
+from typing import cast
+
+import shared as shrd
+from owid.catalog import Table
+from structlog import get_logger
+
+from etl.helpers import PathFinder, create_dataset
+from etl.snapshot import Snapshot
+
+# Initialize logger.
+log = get_logger()
+
+# Get paths and naming conventions for current step.
+paths = PathFinder(__file__)
+
+
+def run(dest_dir: str) -> None:
+    log.info("yougov_job_automation.start")
+
+    #
+    # Load inputs.
+    #
+    # Retrieve snapshot.
+    snap = cast(Snapshot, paths.load_dependency("yougov_job_automation.xlsx"))
+    excel_object = shrd.load_data(snap)
+    df_all_sheets = shrd.process_data(excel_object)
+
+    # Get the list of sheet names in the Excel file
+
+    #
+    # Process data.
+    #
+    # Create a new table and ensure all columns are snake-case.
+    tb = Table(df_all_sheets, short_name=paths.short_name, underscore=True)
+
+    #
+    # Save outputs.
+    #
+    # Create a new meadow dataset with the same metadata as the snapshot.
+    ds_meadow = create_dataset(dest_dir, tables=[tb], default_metadata=snap.metadata)
+
+    # Save changes in the new garden dataset.
+    ds_meadow.save()
+
+    log.info("yougov_job_automation.end")
