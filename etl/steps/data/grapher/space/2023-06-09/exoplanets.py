809a85a129d90befad24f34aadedaeebc0198567
spoonerf <spoonerfiona@gmail.com>
2023-06-09 11:52:43 +0100

    fix(space): converting column name entity -> country

--- etl/steps/data/grapher/space/2023-06-09/exoplanets.py
+++ etl/steps/data/grapher/space/2023-06-09/exoplanets.py
@@ -19,6 +19,7 @@ def run(dest_dir: str) -> None:
 
     # Read table from garden dataset.
     tb = ds_garden["exoplanets"]
+    tb = tb.rename(columns={"entity": "country"})
 
     #
     # Process data.
