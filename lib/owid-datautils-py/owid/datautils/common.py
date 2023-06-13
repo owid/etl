45717572a71e3f903e68062b1e6a7ef05f1a85b8
Lars Yencken <lars@yencken.org>
2023-06-13 17:47:43 +0100

    :honeybee: add snapshot of owid-datautils-py

---
+++ lib/owid-datautils-py/owid/datautils/common.py
@@ -0,0 +1,32 @@
+"""Common objects shared by other modules."""
+
+import warnings
+from typing import Any, List, Set, Union
+
+
+class ExceptionFromDocstring(Exception):
+    """Exception that returns its own docstring, if no message is explicitly given."""
+
+    def __init__(self, exception_message: Union[str, None] = None, *args: Any):
+        super().__init__(exception_message or self.__doc__, *args)
+
+
+def warn_on_list_of_entities(
+    list_of_entities: Union[List[Any], Set[Any]], warning_message: str, show_list: bool
+) -> None:
+    """Raise a warning with a custom message, and optionally print a list of affected elements.
+
+    Parameters
+    ----------
+    list_of_entities : list or set
+        Elements to optionally print one by one (only relevant if show_list is True).
+    warning_message : str
+        Warning message.
+    show_list : bool
+        True to print a list of affected entities.
+
+    """
+    warnings.warn(warning_message)
+    if show_list:
+        print(warning_message)
+        print("\n".join(["* " + str(entity) for entity in list_of_entities]))
---
+++ lib/owid-datautils-py/owid/datautils/io/__init__.py
@@ -0,0 +1,13 @@
+"""Input/Output methods."""
+from owid.datautils.io.archive import decompress_file
+from owid.datautils.io.df import from_file as df_from_file, to_file as df_to_file
+from owid.datautils.io.json import load_json, save_json
+
+
+__all__ = [
+    "decompress_file",
+    "load_json",
+    "save_json",
+    "df_from_file",
+    "df_to_file",
+]
