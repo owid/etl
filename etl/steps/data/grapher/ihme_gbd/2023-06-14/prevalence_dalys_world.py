b39563857bdb7c7305f3db2f9431e5f1a9667dbe
Lucas Rodés-Guirao <lucasrodes@users.noreply.github.com>
2023-06-14 18:29:15 +0200

    📊 ihme: custom dataset for a chart (using causes as entities) (#1226)
    * 📊 ihme: custom dataset for a chart (using causes as entities)
    
    * 💄 ihme: spacing in metadata dataset description

---
+++ etl/steps/data/grapher/ihme_gbd/2023-06-14/prevalence_dalys_world.py
@@ -0,0 +1,40 @@
+"""Load a garden dataset and create a grapher dataset."""
+
+from typing import cast
+
+from owid.catalog import Dataset
+
+from etl.helpers import PathFinder, create_dataset, grapher_checks
+
+# Get paths and naming conventions for current step.
+paths = PathFinder(__file__)
+
+
+def run(dest_dir: str) -> None:
+    #
+    # Load inputs.
+    #
+    # Load garden dataset.
+    ds_garden = cast(Dataset, paths.load_dependency("prevalence_dalys_world"))
+
+    # Read table from garden dataset.
+    tb = ds_garden["prevalence_dalys_world"]
+
+    #
+    # Process data.
+    #
+    tb = tb.reset_index().rename(columns={"cause": "country"}).set_index(["country", "year"])
+
+    #
+    # Save outputs.
+    #
+    # Create a new grapher dataset with the same metadata as the garden dataset.
+    ds_grapher = create_dataset(dest_dir, tables=[tb], default_metadata=ds_garden.metadata)
+
+    #
+    # Checks.
+    #
+    grapher_checks(ds_grapher)
+
+    # Save changes in the new grapher dataset.
+    ds_grapher.save()
