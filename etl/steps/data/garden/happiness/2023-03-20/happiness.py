12ee0dcf0a8d9fc25b421de72c93e87bd447ebcb
spoonerf <spoonerfiona@gmail.com>
2023-06-21 13:38:10 +0100

    fix(happiness): fixing CI fails

--- etl/steps/data/garden/happiness/2023-03-20/happiness.py
+++ etl/steps/data/garden/happiness/2023-03-20/happiness.py
@@ -59,7 +59,7 @@ def run(dest_dir: str) -> None:
     # Save outputs.
     #
     # Create a new garden dataset with the same metadata as the meadow dataset.
-    ds_garden = create_dataset(dest_dir, tables=[tb_garden], default_metadata=ds_meadow.metadata)
+    ds_garden = create_dataset(dest_dir, tables=[tb_garden])
 
     # Save changes in the new garden dataset.
     ds_garden.save()
