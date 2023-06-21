e6458f44d732546e6ce61f0c23b266a7aef368f9
spoonerf <spoonerfiona@gmail.com>
2023-06-21 13:34:18 +0100

    refactor(happiness): adding garden steps

--- etl/steps/data/garden/happiness/2023-03-20/happiness.py
+++ etl/steps/data/garden/happiness/2023-03-20/happiness.py
@@ -1,7 +1,4 @@
 """Load a meadow dataset and create a garden dataset."""
-
-from datetime import datetime
-
 import pandas as pd
 from owid.catalog import Dataset, Table
 from structlog import get_logger
@@ -35,33 +32,18 @@ def run(dest_dir: str) -> None:
         "2012-03-20",
     ]
 
-    tb_meadow_concat = pd.DataFrame()
+    df = pd.DataFrame()
     for version in versions:
         ds_meadow: Dataset = paths.load_dependency(short_name="happiness", version=version)
         # Read table from meadow dataset.
         tb_meadow = ds_meadow["happiness"]
-        print(tb_meadow.head())
-        tb_meadow_concat = pd.concat([tb_meadow_concat, tb_meadow])
+        df = pd.concat([df, tb_meadow])
 
-    dt = datetime.strptime(paths.version, "%Y-%m-%d")
-    # The report give values the average of the three previous years, which we report as the middle year - consistently 2 years prior to the publication year
-    tb_meadow["year"] = dt.year - 2
-
+    # The report give values from survey data of the three previous years, which we report as the last survey year - consistently 1 year prior to the publication year
+    df["year"] = df["report_year"] - 1
+    df = df.drop(columns=["report_year"])
     # tb_backport = backport["dataset_235_world_happiness_report__2022"].reset_index()
     # Create a dataframe with data from the table.
-    df = pd.DataFrame(tb_meadow)
-    df = df[["year", "country", "ladder_score", "standard_error_of_ladder_score", "upperwhisker", "lowerwhisker"]]
-
-    df_backport = pd.DataFrame(tb_backport)
-    df_backport = df_backport[
-        ["year", "entity_name", "life_satisfaction_in_cantril_ladder__world_happiness_report_2022"]
-    ]
-    df_backport = df_backport.rename(
-        columns={
-            "entity_name": "country",
-            "life_satisfaction_in_cantril_ladder__world_happiness_report_2022": "ladder_score",
-        }
-    )
     #
     # Process data.
     #
@@ -69,13 +51,9 @@ def run(dest_dir: str) -> None:
     df = geo.harmonize_countries(
         df=df, countries_file=paths.country_mapping_path, excluded_countries_file=paths.excluded_countries_path
     )
-
-    df_combined = pd.concat([df, df_backport])
-    df_combined["year"] = df_combined["year"].astype("uint32")
-    df_combined = df_combined.sort_values("year")
-    df_combined = df_combined.reset_index(drop=True)
+    df = df.reset_index(drop=True)
     # Create a new table with the processed data.
-    tb_garden = Table(df_combined, like=tb_meadow)
+    tb_garden = Table(df, short_name="happiness")
 
     #
     # Save outputs.
