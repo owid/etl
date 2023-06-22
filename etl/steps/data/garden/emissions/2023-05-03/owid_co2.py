37f0529e1b23afaedfd57b48684985372713673c
Pablo Rosado <pabloarosado@gmail.com>
2023-06-22 19:05:30 +0200

    fix(emissions): Fix metadata handling

--- etl/steps/data/garden/emissions/2023-05-03/owid_co2.py
+++ etl/steps/data/garden/emissions/2023-05-03/owid_co2.py
@@ -200,11 +200,12 @@ def convert_units(table: catalog.Table) -> catalog.Table:
     # Check units and convert to more convenient ones.
     for column in table.columns:
         unit = table[column].metadata.unit
+        title = table[column].metadata.title
+        description = table[column].metadata.description
         if unit in list(UNITS):
             table[column] *= UNITS[unit]["conversion"]
-            table[column].metadata.description = table[column].metadata.description.replace(
-                unit, UNITS[unit]["new_unit"]
-            )
+            table[column].metadata.title = title
+            table[column].metadata.description = description.replace(unit, UNITS[unit]["new_unit"])
 
     return table
 
