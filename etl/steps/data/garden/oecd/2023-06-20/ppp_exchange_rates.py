24ed5b477e845906afa0bbff8b11d7a152d0535c
veronikasamborska1994 <veronika@ourworldindata.org>
2023-06-23 15:19:34 +0200

    garden step will be done in untwo etl

--- etl/steps/data/garden/oecd/2023-06-20/ppp_exchange_rates.py
+++ etl/steps/data/garden/oecd/2023-06-20/ppp_exchange_rates.py
@@ -2,7 +2,6 @@
 
 from typing import cast
 
-import pandas as pd
 from owid.catalog import Dataset, Table
 from structlog import get_logger
 
@@ -26,35 +25,17 @@ def run(dest_dir: str) -> None:
 
     # Read table from meadow dataset.
     tb = ds_meadow["ppp_exchange_rates"]
-
     #
     # Process data.
     #
     log.info("ppp_exchange_rates.harmonize_countries")
     tb: Table = geo.harmonize_countries(df=tb, countries_file=paths.country_mapping_path)
+
     pivot_table = tb.pivot(
         index=["measure", "country", "year", "unit"], columns="transaction", values="value"
     ).reset_index()
 
-    # Load WDI
-    ds_wdi = cast(Dataset, paths.load_dependency("wdi"))
-    tb_wdi = ds_wdi["wdi"]
-
-    # Assume country and year are multi-index
-    df_wdi_cpi = tb_wdi[["fp_cpi_totl"]]
-    df_wdi_cpi.reset_index(inplace=True)
-
-    # Load WDI
-    ds_tourism = cast(Dataset, paths.load_dependency("unwto"))
-    tb_tourism = ds_tourism["unwto"]
-
-    # Assume country and year are multi-index
-    inboud_exp = tb_tourism[["country", "year", "in_to_ex_tr"]]
-
-    merg_inbound_exp = pd.merge(inboud_exp, df_wdi_cpi, on=["country", "year"], how="inner")
-    merg_all = pd.merge(merg_inbound_exp, pivot_table, on=["country", "year"], how="inner")
-
-    tb_garden = Table(merg_all, short_name="ppp_exchange_rates")
+    tb_garden = Table(pivot_table, short_name="ppp_exchange_rates")
 
     #
     # Save outputs.
