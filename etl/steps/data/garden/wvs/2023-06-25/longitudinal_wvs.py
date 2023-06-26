939c138373438dd00926f8a2f878ae3d2873ccd9
veronikasamborska1994 <veronika@ourworldindata.org>
2023-06-26 12:33:20 +0200

    added garden and grapher steps

--- etl/steps/data/garden/wvs/2023-06-25/longitudinal_wvs.py
+++ etl/steps/data/garden/wvs/2023-06-25/longitudinal_wvs.py
@@ -2,6 +2,7 @@
 
 from typing import cast
 
+import pandas as pd
 from owid.catalog import Dataset, Table
 from structlog import get_logger
 
@@ -25,14 +26,17 @@ def run(dest_dir: str) -> None:
 
     # Read table from meadow dataset.
     tb = ds_meadow["longitudinal_wvs"]
-
     #
     # Process data.
     #
     log.info("longitudinal_wvs.harmonize_countries")
-    tb: Table = geo.harmonize_countries(
-        df=tb, countries_file=paths.country_mapping_path, excluded_countries_file=paths.excluded_countries_path
-    )
+    tb: Table = geo.harmonize_countries(df=tb, countries_file=paths.country_mapping_path)
+    q1 = question_1(tb)
+    q2 = question_2(tb)
+    q3 = question_3(tb)
+    merge_q1_q2 = pd.merge(q1, q2, on=["country", "year"], how="outer")
+    merge_q1_q2_q3 = pd.merge(merge_q1_q2, q3, on=["country", "year"], how="outer")
+    tb = Table(merge_q1_q2_q3, short_name=paths.short_name, underscore=True)
 
     #
     # Save outputs.
@@ -44,3 +48,67 @@ def run(dest_dir: str) -> None:
     ds_garden.save()
 
     log.info("longitudinal_wvs.end")
+
+
+def calculate_percentage(df, column, valid_responses_dict):
+    valid_responses = df[column].isin(valid_responses_dict.keys())
+    df_filtered = df[["country", "year", column]][valid_responses].reset_index(drop=True)
+    grouped = df_filtered.groupby(["country", "year"])
+    counts = grouped[column].value_counts().reset_index(name="count")
+    total_counts = counts.groupby(["country", "year"])["count"].transform("sum")
+    counts["percentage"] = (counts["count"] / total_counts) * 100
+    counts[column] = counts[column].map(valid_responses_dict)
+    return counts
+
+
+def question_1(df):
+    dict_q1 = {1: "Very much", 2: "A great deal", 3: "Not much", 4: "Not at all", -1: "Don ́t know", -2: "No answer"}
+    counts_q1 = calculate_percentage(df, "worries__a_terrorist_attack", dict_q1)
+    select_df = counts_q1[["country", "year", "percentage", "worries__a_terrorist_attack"]]
+    pivoted_df = select_df.pivot(index=["country", "year"], columns="worries__a_terrorist_attack", values="percentage")
+    pivoted_df.reset_index(inplace=True)
+    pivoted_df.columns.name = None
+    pivoted_df["great_deal_or_very_much"] = pivoted_df["A great deal"] + pivoted_df["Very much"]
+    pivoted_df["not_at_all_or_not_much"] = pivoted_df["Not at all"] + pivoted_df["Not much"]
+    pivoted_df["great_deal_or_very_much_not_at_all_or_not_much_ratio"] = (
+        pivoted_df["great_deal_or_very_much"] / pivoted_df["not_at_all_or_not_much"]
+    )
+    return pivoted_df[["country", "year", "great_deal_or_very_much_not_at_all_or_not_much_ratio"]]
+
+
+def question_2(df):
+    dict_q2 = {0: "Disagree", 1: "Hard to say", 2: "Agree", -1: "Don ́t know", -2: "No answer"}
+    counts_q2 = calculate_percentage(
+        df, "effects_of_immigrants_on_the_development_of__your_country__increase_the_risks_of_terrorism", dict_q2
+    )
+    select_df = counts_q2[
+        [
+            "country",
+            "year",
+            "percentage",
+            "effects_of_immigrants_on_the_development_of__your_country__increase_the_risks_of_terrorism",
+        ]
+    ]
+    pivoted_df = select_df.pivot(
+        index=["country", "year"],
+        columns="effects_of_immigrants_on_the_development_of__your_country__increase_the_risks_of_terrorism",
+        values="percentage",
+    )
+    pivoted_df.reset_index(inplace=True)
+    pivoted_df.columns.name = None
+    pivoted_df["agree_disagree"] = pivoted_df["Agree"] / pivoted_df["Disagree"]
+    return pivoted_df[["country", "year", "agree_disagree"]]
+
+
+def question_3(df):
+    # Quesiton 3
+    valid_resp_q3 = list(range(11))
+    valid_responses_q3 = df["justifiable__terrorism_as_a_political__ideological_or_religious_mean"].isin(valid_resp_q3)
+    df_q3 = df[valid_responses_q3]
+    grouped_q3 = df_q3.groupby(["country", "year"])
+    df_q3 = (
+        grouped_q3["justifiable__terrorism_as_a_political__ideological_or_religious_mean"]
+        .mean()
+        .reset_index(name="average_from_never_1_always_10")
+    )
+    return df_q3
--- etl/steps/data/meadow/wvs/2023-06-25/longitudinal_wvs.py
+++ etl/steps/data/meadow/wvs/2023-06-25/longitudinal_wvs.py
@@ -4,7 +4,7 @@ from typing import cast
 
 import numpy as np
 import pandas as pd
-from owid.catalog import Table
+from owid.catalog import Dataset, Table
 from structlog import get_logger
 
 from etl.helpers import PathFinder, create_dataset
@@ -28,27 +28,33 @@ def run(dest_dir: str) -> None:
 
     # Load data from snapshot.
     df = pd.read_csv(snap.path)
-    terrorism_columns = ["COUNTRY_ALPHA", "S020", "H006_04", "G057", "E206", "F114E"]
+    terrorism_columns = ["COUNTRY_ALPHA", "S020", "H006_04", "G057", "F114E"]
     df = df[terrorism_columns]
-    # replace keys where question was not asked with nan (1,2,3,4 are the only valid answers)
-    cols_to_update = ["H006_04", "G057", "E206", "F114E"]
-    df[cols_to_update] = df[cols_to_update].applymap(lambda x: x if x in [1, 2, 3, 4] else np.nan)
-
+    # replace keys where question was not asked with nan
+    cols_to_update = ["H006_04", "G057", "F114E"]
+    valid_resp = list(range(11)) + [-1, -2]
+    df[cols_to_update] = df[cols_to_update].applymap(lambda x: x if x in valid_resp else np.nan)
     dictionary_keys = {
-        "COUNTRY_ALPHA": "country",
+        "COUNTRY_ALPHA": "code",
         "S020": "year",
         "H006_04": "Worries: a terrorist attack",
         "G057": "Effects of immigrants on the development of [your country]: Increase the risks of terrorism",
-        "E206": "Free and fair elections will reduce terrorism",
         "F114E": "Justifiable: Terrorism as a political, ideological or religious mean",
     }
     df.rename(columns=dictionary_keys, inplace=True)
+
+    countries_regions = cast(Dataset, paths.load_dependency("regions"))["regions"]
+    iso_match = countries_regions.reset_index()[["code", "name"]]
+
+    match_code_df = pd.merge(iso_match, df, on="code", how="right")
+    match_code_df.drop("code", axis=1, inplace=True)
+    match_code_df.rename(columns={"name": "country"}, inplace=True)
 
     #
     # Process data.
     #
     # Create a new table and ensure all columns are snake-case.
-    tb = Table(df, short_name=paths.short_name, underscore=True)
+    tb = Table(match_code_df, short_name=paths.short_name, underscore=True)
 
     #
     # Save outputs.
