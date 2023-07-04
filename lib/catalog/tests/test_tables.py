682f217bd82597d1301c69385317d10e8a245b73
Marigold <mojmir.vinkler@gmail.com>
2023-07-04 12:57:10 +0200

    fix black

--- lib/catalog/tests/test_tables.py
+++ lib/catalog/tests/test_tables.py
@@ -10,6 +10,7 @@ import jsonschema
 import numpy as np
 import pandas as pd
 import pytest
+
 from owid.catalog import tables
 from owid.catalog.datasets import FileFormat
 from owid.catalog.meta import TableMeta, VariableMeta
