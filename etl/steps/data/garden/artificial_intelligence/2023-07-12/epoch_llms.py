915983c53bedcf17037c122a7f97c90f8f24b8ae
veronikasamborska1994 <veronika@ourworldindata.org>
2023-07-12 16:01:52 +0200

    adding AI PhDs and another EPOCH dataset

---
+++ etl/steps/data/garden/artificial_intelligence/2023-07-12/epoch_llms.py
@@ -0,0 +1,33 @@
+"""Load a meadow dataset and create a garden dataset."""
+
+from typing import cast
+
+import pandas as pd
+from owid.catalog import Table
+
+from etl.helpers import PathFinder, create_dataset
+from etl.snapshot import Snapshot
+
+# Get paths and naming conventions for current step.
+paths = PathFinder(__file__)
+
+
+def run(dest_dir: str) -> None:
+    #
+    # Load inputs.
+    #
+    # Load meadow dataset.
+    snap = cast(Snapshot, paths.load_dependency("epoch_llms.csv"))
+    df = pd.read_csv(snap.path)
+
+    tb = Table(df, short_name=paths.short_name, underscore=True)
+    tb.set_index("architecture", inplace=True)
+
+    #
+    # Save outputs.
+    #
+    # Create a new garden dataset with the same metadata as the meadow dataset.
+    ds_garden = create_dataset(dest_dir, tables=[tb], default_metadata=snap.metadata)
+
+    # Save changes in the new garden dataset.
+    ds_garden.save()
