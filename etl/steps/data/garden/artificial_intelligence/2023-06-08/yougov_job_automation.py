a4c7dc418f95db985fac0fa5f58df1ef0f49d239
veronikasamborska1994 <veronika@ourworldindata.org>
2023-07-14 19:38:10 +0200

    Revert "rehshape yougov_job_automation survey"
    This reverts commit ae1e19d6b76fc0f11729560f8f48db8b6eb1b432.

--- etl/steps/data/garden/artificial_intelligence/2023-06-08/yougov_job_automation.py
+++ etl/steps/data/garden/artificial_intelligence/2023-06-08/yougov_job_automation.py
@@ -2,7 +2,6 @@
 
 from typing import cast
 
-import numpy as np
 import pandas as pd
 from owid.catalog import Dataset, Table
 from structlog import get_logger
@@ -26,25 +25,6 @@ def run(dest_dir: str) -> None:
     # Read table from meadow dataset.
     tb = ds_meadow["yougov_job_automation"]
     df = pd.DataFrame(tb)
-    # First, create a mapping of the worry responses to the new categories.
-    worry_mapping = {
-        "Very worried": "Expressed worry",
-        "Fairly worried": "Expressed worry",
-        "Not very worried": "Not expressed worry",
-        "Not worried at all": "Not expressed worry",
-    }
-
-    # Apply the mapping to create a new column for the combined categories.
-    df["worry_category"] = df[
-        "how_worried__if_it_all__are_you_that_your_type_of_work_could_be_automated_within_your_lifetime"
-    ].map(worry_mapping)
-
-    # Now we can drop the rows where 'worry_category' is NaN (i.e., the 'Don't know' rows).
-    df = df.dropna(subset=["worry_category"])
-
-    # Group by the 'date', 'group', and new 'worry_category' columns, and sum the 'value' column within each group.
-    df = df.groupby(["date", "group", "worry_category"]).value.sum().reset_index()
-
     #
     # Process data.
     #
@@ -54,76 +34,29 @@ def run(dest_dir: str) -> None:
         pd.to_datetime(df["date"].astype(str), format="%Y-%m-%d") - pd.to_datetime("2021-01-01")
     ).dt.days
     df = df.drop("date", axis=1)
-    df = assign_indicator(df, "group")
 
-    df = reshape_dataframe(df, "indicator_group", "value")
+    # Create a pivot table for each demographic group
+    pivot_df = df.pivot(index=[df.columns[0], "days_since_2021"], columns="group", values="value").reset_index()
+    pivot_df = pivot_df.rename_axis(None, axis=1)
+    pivot_df.rename(columns={pivot_df.columns[0]: "question"}, inplace=True)
 
-    tb = Table(df, short_name="yougov_job_automation", underscore=True)
-    tb.set_index(["days_since_2021", "group"], inplace=True)
+    tb = Table(pivot_df, short_name="yougov_job_automation", underscore=True)
+
+    for column in tb.columns:
+        if column.startswith("_") and column not in ["_2_year", "_4_year"]:
+            new_column = "answers_age_" + column[1:]
+            tb.rename(columns={column: new_column}, inplace=True)
+        elif column == "_2_year" or column == "_4_year":
+            new_column = "education_" + column[1:]
+            tb.rename(columns={column: new_column}, inplace=True)
 
     #
     # Save outputs.
     #
     # Create a new garden dataset with the same metadata as the meadow dataset.
-    ds_garden = create_dataset(dest_dir, tables=[tb], default_metadata=None)
+    ds_garden = create_dataset(dest_dir, tables=[tb], default_metadata=ds_meadow.metadata)
 
     # Save changes in the new garden dataset.
     ds_garden.save()
 
     log.info("yougov_job_automation.end")
-
-
-# Adjusted function to assign indicator
-def assign_indicator(df, column_name):
-    conditions = []
-    categories = []
-
-    for method, mapping in condition_category_mapping.items():
-        for category, values in mapping.items():
-            if method == "isin":
-                conditions.append(df[column_name].isin(values))
-            elif method == "contains":
-                conditions.append(df[column_name].str.contains("|".join(values)))
-            categories.append(category)
-
-    df["indicator_group"] = np.select(conditions, categories, default="All adults")
-    return df
-
-
-condition_category_mapping = {
-    "isin": {
-        "Age": ["18-29", "30-44", "45-64", "65+"],
-        "Race": [
-            "White",
-            "Black",
-            "Hispanic",
-            "Asian",
-            "Native American",
-            "Middle Eastern",
-            "Two or more races",
-            "Other",
-        ],
-        "Gender": ["Male", "Female"],
-        "Education": ["No HS", "High school graduate", "Some college", "2-year", "4-year", "Post-grad"],
-        "Political Affiliation": ["Democrat", "Republican", "Independent"],
-    }
-}
-
-
-def reshape_dataframe(df, col_categories, col_values):
-    reshaped_df = df.pivot(
-        index=[
-            "days_since_2021",
-            "group",
-        ],
-        columns=[
-            col_categories,
-            "worry_category",
-        ],
-        values=col_values,
-    )
-    reshaped_df.reset_index(inplace=True)
-
-    reshaped_df.columns = [" ".join(col).strip() for col in reshaped_df.columns.values]
-
-    return reshaped_df
