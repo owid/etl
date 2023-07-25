541091a12254cb455c00a7955750b2ba7c62eb9d
spoonerf <spoonerfiona@gmail.com>
2023-07-25 15:10:47 +0100

    fix missing MH step

---
+++ etl/steps/data/grapher/ihme_gbd/2019/gbd_mental_health.py
@@ -0,0 +1,13 @@
+from owid import catalog
+
+from etl.helpers import PathFinder
+
+from .gbd_tools import run_wrapper
+
+N = PathFinder(__file__)
+
+
+def run(dest_dir: str) -> None:
+    garden_dataset = N.garden_dataset
+    dataset = catalog.Dataset.create_empty(dest_dir, garden_dataset.metadata)
+    dataset = run_wrapper(garden_dataset=garden_dataset, dataset=dataset, dims=["sex", "age", "cause"])
