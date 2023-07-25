71fa6014811607178aed06a49305f2154c43e7c3
spoonerf <spoonerfiona@gmail.com>
2023-07-25 13:59:08 +0100

    update

--- etl/steps/data/grapher/ihme_gbd/2019/gbd_cause.py
+++ etl/steps/data/grapher/ihme_gbd/2019/gbd_cause.py
@@ -10,5 +10,5 @@ N = PathFinder(__file__)
 def run(dest_dir: str) -> None:
     garden_dataset = N.garden_dataset
     dataset = catalog.Dataset.create_empty(dest_dir, garden_dataset.metadata)
-    run_wrapper(garden_dataset=garden_dataset, dataset=dataset, dims=["sex", "age", "cause"])
+    dataset = run_wrapper(garden_dataset=garden_dataset, dataset=dataset, dims=["sex", "age", "cause"])
     dataset.save()
