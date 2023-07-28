58cd1d55e8cbc0d05c7ae466e839bc3f0fbf620b
veronikasamborska1994 <veronika@ourworldindata.org>
2023-07-28 13:10:27 +0100

    split the educational attainment and projections

--- etl/steps/data/garden/education/2023-07-17/education_barro_lee_projections.py
+++ etl/steps/data/garden/education/2023-07-17/education_barro_lee_projections.py
@@ -3,7 +3,6 @@
 from typing import cast
 
 import education_lee_lee
-import pandas as pd
 from owid.catalog import Dataset, Table
 
 from etl.data_helpers import geo
@@ -31,25 +30,15 @@ def run(dest_dir: str) -> None:
     tb["age_group"] = tb["age_group"].replace(
         {"15-64": "Youth and Adults (15-64 years)", "15-24": "Youth (15-24 years)", "25-64": "Adults (25-64 years)"}
     )
-    df_attainment = education_lee_lee.prepare_attainment_data(tb)
-    tb_future = Table(df_attainment, short_name=paths.short_name, underscore=True)
-    tb_future.set_index(["country", "year"], inplace=True)
-
-    ds_past = cast(Dataset, paths.load_dependency("education_lee_lee"))
-    tb_past = ds_past["education_lee_lee"]
-
-    cols_to_drop = [col for col in tb_past.columns if "enrollment_rates" in col]
-    tb_past = tb_past.drop(columns=cols_to_drop)
-
-    stiched = pd.concat([tb_future, tb_past])
-
-    tb_stiched = Table(stiched, short_name=paths.short_name, underscore=True)
+    df_projections = education_lee_lee.prepare_attainment_data(tb)
+    tb_projections = Table(df_projections, short_name=paths.short_name, underscore=True)
+    tb_projections.set_index(["country", "year"], inplace=True)
 
     #
     # Save outputs.
     #
     # Create a new garden dataset with the same metadata as the meadow dataset.
-    ds_garden = create_dataset(dest_dir, tables=[tb_stiched], default_metadata=ds_meadow.metadata)
+    ds_garden = create_dataset(dest_dir, tables=[tb_projections], default_metadata=ds_meadow.metadata)
 
     # Save changes in the new garden dataset.
     ds_garden.save()
