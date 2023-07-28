616b016579c8ecdbcad77ef8593b51ff89715137
Fiona Spooner <fiona@ourworldindata.org>
2023-07-28 17:55:13 +0100

    📊 Improving GBD metadata (#1380)
    * fixing the child mortality percent data
    
    * tidying metadata
    
    * improving metadata
    
    * prepping for improving metadata
    
    * refactor(gbd): trying to improve metadata
    
    * remove accidental index
    
    * trying to sort metadata
    
    * update
    
    * fixing mental health issue
    
    * fix missing MH step
    
    * fix issue with prevalence_dalys_world
    
    * removing metadata table
    
    * trying to fix metadata
    
    * fix metadata
    
    * adding in lucas' comments
    
    * sorting out variable names
    
    * incorporating risk factors into metadata
    
    * typing fix
    
    * small adjustment
    
    * experiment with removing empty variables
    
    * test
    
    * removing excess dataset.save()
    
    * fixing error in add_metadata function
    
    * remove print
    
    * fixing prevalence_dalys_world
    
    * format

--- etl/steps/data/grapher/ihme_gbd/2019/gbd_child_mortality.py
+++ etl/steps/data/grapher/ihme_gbd/2019/gbd_child_mortality.py
@@ -4,12 +4,12 @@ from etl.helpers import PathFinder
 
 from .gbd_tools import run_wrapper
 
-N = PathFinder(__file__)
+paths = PathFinder(__file__)
 
 
 def run(dest_dir: str) -> None:
-    garden_dataset = N.garden_dataset
+    garden_dataset = paths.garden_dataset
     dataset = catalog.Dataset.create_empty(dest_dir, garden_dataset.metadata)
-    dataset.save()
+    # dataset.save()
 
-    run_wrapper(garden_dataset=garden_dataset, dataset=dataset, dims=["sex", "age", "cause"])
+    run_wrapper(garden_dataset=garden_dataset, dataset=dataset)
