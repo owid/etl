d99c9689100f647ea07dda792d24ad62b781f339
spoonerf <spoonerfiona@gmail.com>
2023-07-28 13:27:39 +0100

    removing excess dataset.save()

--- etl/steps/data/grapher/ihme_gbd/2019/gbd_risk.py
+++ etl/steps/data/grapher/ihme_gbd/2019/gbd_risk.py
@@ -10,6 +10,6 @@ paths = PathFinder(__file__)
 def run(dest_dir: str) -> None:
     garden_dataset = paths.garden_dataset
     dataset = catalog.Dataset.create_empty(dest_dir, garden_dataset.metadata)
-    dataset.save()
+    # dataset.save()
 
     run_wrapper(garden_dataset=garden_dataset, dataset=dataset)
