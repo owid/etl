2452c3078c5a7115e07ddae70e38f26d8f172de9
veronikasamborska1994 <veronika@ourworldindata.org>
2023-08-01 22:28:18 +0100

    updating EPOCH dataset
    changed to direct download, deleted some variables that no longer exist

--- snapshots/artificial_intelligence/2023-06-21/epoch.py
+++ snapshots/artificial_intelligence/2023-06-21/epoch.py
@@ -1,5 +1,6 @@
 """Script to create a snapshot of dataset 'Parameter, Compute and Data Trends in Machine Learning (Epoch, 2023)'."""
 
+
 from pathlib import Path
 
 import click
@@ -17,20 +18,12 @@ SNAPSHOT_VERSION = Path(__file__).parent.name
     type=bool,
     help="Upload dataset to Snapshot",
 )
-@click.option("--path-to-file", prompt=True, type=str, help="Path to local data file.")
-def main(path_to_file: str, upload: bool) -> None:
-    """Script to create a snapshot of dataset Parameter, Compute and Data Trends in Machine Learning (Epoch, 2023)'.
-    - Visit https://epochai.org/mlinputs/visualization
-    - In top right corner click on download
-    - Click on "CSV" """
+def main(upload: bool) -> None:
     # Create a new snapshot.
     snap = Snapshot(f"artificial_intelligence/{SNAPSHOT_VERSION}/epoch.csv")
 
-    # Ensure destination folder exists.
-    snap.path.parent.mkdir(exist_ok=True, parents=True)
-
-    # Copy local data file to snapshots data folder.
-    snap.path.write_bytes(Path(path_to_file).read_bytes())
+    # Download data from source.
+    snap.download_from_source()
 
     # Add file to DVC and upload to S3.
     snap.dvc_add(upload=upload)
