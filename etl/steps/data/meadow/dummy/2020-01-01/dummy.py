93361150ba3cf8b6eb09dfa568da456078350f3c
Marigold <mojmir.vinkler@gmail.com>
2023-08-07 09:27:06 +0200

    update dummy dataset

--- etl/steps/data/meadow/dummy/2020-01-01/dummy.py
+++ etl/steps/data/meadow/dummy/2020-01-01/dummy.py
@@ -27,12 +27,13 @@ def run(dest_dir: str) -> None:
     #
     # Create a new table and ensure all columns are snake-case.
     tb = Table(df, short_name=paths.short_name, underscore=True)
+    tb = tb.set_index(["country", "year"], verify_integrity=True)
 
     #
     # Save outputs.
     #
     # Create a new meadow dataset with the same metadata as the snapshot.
-    ds_meadow = create_dataset(dest_dir, tables=[tb.set_index(["country", "year"])], default_metadata=snap.metadata)
+    ds_meadow = create_dataset(dest_dir, tables=[tb], default_metadata=snap.metadata)
 
     # Save changes in the new garden dataset.
     ds_meadow.save()
