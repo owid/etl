a2438a8f0c425124252113fc71971885c420c6b1
spoonerf <spoonerfiona@gmail.com>
2023-08-08 18:00:22 +0100

    fixing map

--- etl/steps/data/garden/who/2023-08-04/icd_codes.py
+++ etl/steps/data/garden/who/2023-08-04/icd_codes.py
@@ -21,14 +21,16 @@ def run(dest_dir: str) -> None:
     # Read table from meadow dataset.
     tb = ds_meadow["icd_codes"]
     tb = tb.reset_index()
+    icd_codes_sum = {"Icd10": "ICD-10", "Icd9": "ICD-9", "Icd8": "ICD-8", "Icd7": "01. ICD-7"}
     icd_codes_map = {"Icd10": "04. ICD-10", "Icd9": "03. ICD-9", "Icd8": "02. ICD-8", "Icd7": "01. ICD-7"}
-    tb["icd"] = tb["icd"].replace(icd_codes_map)
 
     # Calculate sum of number of countries using each ICD code type each year
     tb_sum = tb.groupby(["year", "icd"]).count().reset_index()
     tb_sum = tb_sum.rename(columns={"icd": "country", "country": "countries_using_icd_code"})
+    tb_sum["country"] = tb["country"].replace(icd_codes_sum)
 
     tb = geo.harmonize_countries(df=tb, countries_file=paths.country_mapping_path)
+    tb["icd"] = tb["icd"].replace(icd_codes_map)
 
     # Combine the datasets
     tb = tb.set_index(["country", "year"], verify_integrity=True)
