55de3428a141a48cfefe71fb6edd92925a887aa6
Pablo Rosado <pabloarosado@gmail.com>
2023-08-08 17:01:20 +0200

    feat(catalog): Add read_from_records function that propagates metadata

--- lib/catalog/owid/catalog/processing.py
+++ lib/catalog/owid/catalog/processing.py
@@ -1,6 +1,15 @@
 """Common operations performed on tables and variables.
 
 """
-from .tables import ExcelFile, concat, melt, merge, pivot, read_csv, read_excel
+from .tables import (
+    ExcelFile,
+    concat,
+    melt,
+    merge,
+    pivot,
+    read_csv,
+    read_excel,
+    read_from_records,
+)
 
-__all__ = ["ExcelFile", "concat", "melt", "merge", "pivot", "read_csv", "read_excel"]
+__all__ = ["ExcelFile", "concat", "melt", "merge", "pivot", "read_csv", "read_excel", "read_from_records"]
