69b66dba4be9b74974b939b30ed8a953ebe90ab7
Pablo Arriagada <paarriagadap@gmail.com>
2023-08-08 18:20:31 +0100

    fix: html tags in metadata

--- etl/steps/data/garden/eth/2023-03-15/ethnic_power_relations.py
+++ etl/steps/data/garden/eth/2023-03-15/ethnic_power_relations.py
@@ -276,13 +276,6 @@ def run(dest_dir: str) -> None:
     # Create a new garden dataset with the same metadata as the meadow dataset.
     ds_garden = create_dataset(dest_dir, tables=[tb_garden])
 
-    # For now the variable descriptions are stored as a list of strings, this transforms them into a single string
-    tb_garden = ds_garden["ethnic_power_relations"]
-    for col in tb_garden.columns:
-        if isinstance(tb_garden[col].metadata.description, list):
-            tb_garden[col].metadata.description = "\n".join(tb_garden[col].metadata.description)
-    ds_garden.add(tb_garden)
-
     # Save changes in the new garden dataset.
     ds_garden.save()
 
