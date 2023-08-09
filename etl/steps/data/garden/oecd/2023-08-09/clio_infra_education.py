dbd6903f644cd0e098edf770cf30ac28f92d1ea5
veronikasamborska1994 <veronika@ourworldindata.org>
2023-08-09 19:56:53 +0100

    garden step

--- etl/steps/data/garden/oecd/2023-08-09/clio_infra_education.py
+++ etl/steps/data/garden/oecd/2023-08-09/clio_infra_education.py
@@ -16,10 +16,10 @@ def run(dest_dir: str) -> None:
     # Load inputs.
     #
     # Load meadow dataset.
-    ds_meadow = cast(Dataset, paths.load_dependency("years_of_education"))
+    ds_meadow = cast(Dataset, paths.load_dependency("clio_infra_education"))
 
     # Read table from meadow dataset.
-    tb = ds_meadow["years_of_education"].reset_index()
+    tb = ds_meadow["clio_infra_education"].reset_index()
 
     #
     # Process data.
