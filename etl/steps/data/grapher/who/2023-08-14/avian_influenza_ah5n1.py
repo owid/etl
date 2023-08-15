85e47a3003fae53cde85aa3fa291c26ddaa30409
Lucas Rodés-Guirao <lucasrodes@users.noreply.github.com>
2023-08-16 01:02:13 +0200

    📊 pandemics: add avian influenza cases from who (#1476)
    * h5n1: who snapshot
    
    * avian dataet
    
    * improve metadata
    
    * lint
    
    * lint
    
    * wip
    
    * snapshot kucharski
    
    * avian influenza a/h5n1 improvements
    
    * fix estimates for world

---
+++ etl/steps/data/grapher/who/2023-08-14/avian_influenza_ah5n1.py
@@ -0,0 +1,66 @@
+"""Load a garden dataset and create a grapher dataset."""
+
+from typing import cast
+
+from owid.catalog import Dataset, Table
+
+from etl.helpers import PathFinder, create_dataset, grapher_checks
+
+# Get paths and naming conventions for current step.
+paths = PathFinder(__file__)
+
+
+def run(dest_dir: str) -> None:
+    #
+    # Load inputs.
+    #
+    # Load garden dataset.
+    ds_garden = cast(Dataset, paths.load_dependency("avian_influenza_ah5n1"))
+
+    # Read table from garden dataset.
+    tb = ds_garden["avian_influenza_ah5n1"].reset_index()
+
+    #
+    # Process data.
+    #
+    # Get zeroDay as the minimum date in the dataset and set it to zeroDay
+    tb = add_num_days(tb)
+
+    tb = tb.set_index(["country", "year"], verify_integrity=True)
+
+    #
+    # Save outputs.
+    #
+    # Create a new grapher dataset with the same metadata as the garden dataset.
+    ds_grapher = create_dataset(dest_dir, tables=[tb], default_metadata=ds_garden.metadata)
+
+    #
+    # Checks.
+    #
+    grapher_checks(ds_grapher)
+
+    # Save changes in the new grapher dataset.
+    ds_grapher.save()
+
+
+def add_num_days(tb: Table) -> Table:
+    """Add column with number of days after zero_day.
+
+    Also, drop `date` column.
+    """
+    column_indicator = "avian_cases"
+
+    if tb[column_indicator].metadata.display is None:
+        tb[column_indicator].metadata.display = {}
+
+    zero_day = tb["date"].min()
+    tb[column_indicator].metadata.display["yearIsDay"] = True
+    tb[column_indicator].metadata.display["zeroDay"] = zero_day.strftime("%Y-%m-%d")
+
+    # Add column with number of days after zero_day
+    tb["year"] = (tb["date"] - zero_day).dt.days
+
+    # Drop date column
+    tb = tb.drop(columns=["date"])
+
+    return tb
