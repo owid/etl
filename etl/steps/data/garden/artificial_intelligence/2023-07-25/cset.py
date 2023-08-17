c67f946e919c13f3ecaa6c4f37e3560227a65c26
veronikasamborska1994 <veronika@ourworldindata.org>
2023-08-17 21:46:55 +0100

    CSET World bug fix, assertion error fix in barro lee education dataset

--- etl/steps/data/garden/artificial_intelligence/2023-07-25/cset.py
+++ etl/steps/data/garden/artificial_intelligence/2023-07-25/cset.py
@@ -31,11 +31,24 @@ def add_world(tb: Table, ds_regions: Dataset) -> Table:
     # Create a deep copy of the input table to avoid modifying the original data
     tb_with_regions = tb.copy()
 
-    # List of members representing different regions (CSET)
-    members = ["North America", "Europe", "Asia Pacific", "Africa", "Latin America and the Caribbean", "Oceania"]
+    # List of members representing different regions CSET
+    members = [
+        "Quad (Australia, India, Japan and the US)",
+        "Five Eyes (Australia, Canada, New Zealand, UK, and the US)",
+        "Global Partnership on Artificial Intelligence",
+        "European Union (27)",
+        "ASEAN (Association of Southeast Asian Nations)",
+        "North America",
+        "Europe",
+        "Asia Pacific",
+        "Africa",
+        "Latin America and the Caribbean",
+        "Oceania",
+        "Nato",
+    ]
 
-    # Filter the table to only include rows corresponding to the specified regions
-    df_regions = tb_with_regions[tb_with_regions["country"].isin(members)]
+    # Filter the table to only include rows corresponding to the countries and not regions
+    df_regions = tb_with_regions[~tb_with_regions["country"].isin(members)]
 
     # Reset the index of the filtered data
     df_regions.reset_index(inplace=True, drop=True)
