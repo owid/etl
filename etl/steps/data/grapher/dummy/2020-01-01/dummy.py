82953b4b34d90c858e86f905866f105bd0c4da09
Marigold <mojmir.vinkler@gmail.com>
2023-08-21 09:55:23 +0200

    🔧 chore(helpers.py): add support for running grapher checks when creating a dataset with metadata channel as "grapher"
    🔧 chore(dummy.py): remove redundant import of grapher_checks from helpers.py
    
    🔧 chore({{cookiecutter.short_name}}.py): remove redundant import of grapher_checks from helpers.py

--- etl/steps/data/grapher/dummy/2020-01-01/dummy.py
+++ etl/steps/data/grapher/dummy/2020-01-01/dummy.py
@@ -1,6 +1,6 @@
 """Load a garden dataset and create a grapher dataset."""
 
-from etl.helpers import PathFinder, create_dataset, grapher_checks
+from etl.helpers import PathFinder, create_dataset
 
 # Get paths and naming conventions for current step.
 paths = PathFinder(__file__)
@@ -24,14 +24,7 @@ def run(dest_dir: str) -> None:
     # Save outputs.
     #
     # Create a new grapher dataset with the same metadata as the garden dataset.
-    ds_grapher = create_dataset(
-        dest_dir, tables=[tb], default_metadata=ds_garden.metadata, check_variables_metadata=True
-    )
-
-    #
-    # Checks.
-    #
-    grapher_checks(ds_grapher)
+    ds_grapher = create_dataset(dest_dir, tables=[tb], check_variables_metadata=True)
 
     # Save changes in the new grapher dataset.
     ds_grapher.save()
