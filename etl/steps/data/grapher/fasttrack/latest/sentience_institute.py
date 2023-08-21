fe024675d56c099e5b9e448609ec631f4e63671c
owidbot <tech@ourworldindata.org>
2023-08-21 15:38:35 +0000

    fasttrack: fasttrack/latest/sentience_institute.csv

---
+++ etl/steps/data/grapher/fasttrack/latest/sentience_institute.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from owid import catalog
+
+from etl.helpers import PathFinder, create_dataset
+from etl.snapshot import Snapshot
+
+P = PathFinder(__file__)
+
+
+def run(dest_dir: str) -> None:
+    # load snapshot
+    snap = Snapshot("fasttrack/latest/sentience_institute.csv")
+
+    # load data
+    data = pd.read_csv(snap.path)
+
+    # create empty dataframe and table
+    tb = catalog.Table(data, short_name=P.short_name)
+
+    # add table, update metadata from *.meta.yml and save
+    ds = create_dataset(dest_dir, tables=[tb.set_index(["country", "year"])], default_metadata=snap.metadata)
+    ds.save()
