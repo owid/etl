7a5456d2c6d940c6d33683ad22aa39a5a8c3f00c
lucasrodes <lucasrodes@users.noreply.github.com>
2023-08-22 17:57:28 +0200

    add auxiliary tables

--- etl/steps/data/meadow/health/2023-08-22/unaids_gap_art.py
+++ etl/steps/data/meadow/health/2023-08-22/unaids_gap_art.py
@@ -14,9 +14,7 @@ from etl.snapshot import Snapshot
 paths = PathFinder(__file__)
 # Mapping long dimension names to short names.
 INDICATOR_MAPPING = {
-    "N- Deaths averted by ART Male+Female": "estimate",
-    "N- Deaths averted by ART Male+Female; Lower bound": "lower estimate",
-    "N- Deaths averted by ART Male+Female; Upper bound": "upper estimate",
+    "N-2nd 90% - Gap to reaching the target of people receiving ART (90% of PLHIV)": "All ages",
 }
 # Map column names
 INDEX_MAPPING = {
@@ -35,36 +33,40 @@ def run(dest_dir: str) -> None:
     # Load inputs.
     #
     # Retrieve snapshot.
-    snap = cast(Snapshot, paths.load_dependency("unaids_deaths_averted_art.xlsx"))
+    snap = cast(Snapshot, paths.load_dependency("unaids_gap_art.xlsx"))
 
     # Load data from snapshot.
-    df = pd.read_excel(snap.path, sheet_name="ResultGrid", header=1)
+    df = pd.read_excel(snap.path, sheet_name="ResultGrid (1)", header=1)
 
     #
     # Process data.
     #
-    log.info("unaids_deaths_averted_art: handle NaNs")
+    log.info("unaids_gap_art: handle NaNs")
     df = handle_nans(df)
 
     # Rename columns
-    log.info("unaids_deaths_averted_art: rename columns and keep relevant ones")
+    log.info("unaids_gap_art: rename columns and keep relevant ones")
     # Rename columns & keep relevant columns
     df = df.rename(columns=INDICATOR_MAPPING | INDEX_MAPPING)[COLUMNS]
 
     # Format dataframe (wide to long format)
-    log.info("unaids_deaths_averted_art: format table with indices and values (wide to long format)")
-    df = df.melt(id_vars=["country", "year"], var_name="subgroup_description", value_name="deaths_averted_art")
+    log.info("unaids_gap_art: format table with indices and values (wide to long format)")
+    df = df.melt(id_vars=["country", "year"], var_name="subgroup_description", value_name="gap_on_art")
 
     # Replace dots
-    log.info("unaids_deaths_averted_art: replace '...' with NaNs and assign float type")
-    df["deaths_averted_art"] = df["deaths_averted_art"].replace("...", np.nan).astype(float)
+    log.info("unaids_gap_art: replace '...' with NaNs and assign float type")
+    df["gap_on_art"] = df["gap_on_art"].replace("...", np.nan).astype(float)
+
+    # Drop NaNs
+    log.info("unaids_gap_art: drop NaNs")
+    df = df.dropna(subset=["gap_on_art"])
 
     # Strip empty characters from country names
-    log.info("unaids_deaths_averted_art: strip empty characters from country names")
+    log.info("unaids_gap_art: strip empty characters from country names")
     df["country"] = df["country"].str.strip()
 
     # Create a new table and ensure all columns are snake-case.
-    log.info("unaids_deaths_averted_art: create table")
+    log.info("unaids_gap_art: create table")
     tb = Table(df, short_name=paths.short_name, underscore=True)
     tb = tb.set_index(["country", "year", "subgroup_description"], verify_integrity=True)
 
