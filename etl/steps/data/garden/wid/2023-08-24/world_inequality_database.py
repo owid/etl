40769040638c9ffef4b8d2049a2dc01f4fe1be23
Pablo Arriagada <paarriagadap@gmail.com>
2023-08-24 16:32:12 +0100

    feat: new WID version + distribution

---
+++ etl/steps/data/garden/wid/2023-08-24/world_inequality_database.py
@@ -0,0 +1,73 @@
+"""Load World Inequality Database meadow dataset and create a garden dataset."""
+
+import pandas as pd
+from owid.catalog import Dataset, Table
+from shared import add_metadata_vars
+from structlog import get_logger
+
+from etl.helpers import PathFinder, create_dataset
+
+log = get_logger()
+
+# Get paths and naming conventions for current step.
+paths = PathFinder(__file__)
+
+
+# Data processing function (cleaning and small transformations)
+def data_processing(df: pd.DataFrame) -> pd.DataFrame:
+    # Multiply shares by 100
+    df[list(df.filter(like="share"))] *= 100
+
+    # Delete age and pop, two one-value variables
+    df = df.drop(columns=["age", "pop", "age_extrapolated", "pop_extrapolated"])
+
+    # Delete some share ratios we are not using, and also the p0p40 (share) variable only available for pretax
+    drop_list = ["s90_s10_ratio", "s90_s50_ratio", "p0p40"]
+
+    for var in drop_list:
+        df = df[df.columns.drop(list(df.filter(like=var)))]
+
+    # Verify index and sort
+    df = df.set_index(["country", "year"], verify_integrity=True).sort_index()
+
+    return df
+
+
+def run(dest_dir: str) -> None:
+    log.info("world_inequality_database.start")
+
+    #
+    # Load inputs.
+    #
+    # Load meadow dataset.
+    ds_meadow: Dataset = paths.load_dependency("world_inequality_database")
+
+    # Read table from meadow dataset.
+    tb_meadow = ds_meadow["world_inequality_database"]
+
+    # Create a dataframe with data from the table.
+    df = pd.DataFrame(tb_meadow).reset_index()
+
+    #
+    # Process data.
+    # Change units and drop unnecessary columns
+    df = data_processing(df)
+
+    # Create a new table with the processed data.
+    tb_garden = Table(df, short_name="world_inequality_database")
+
+    tb_garden = add_metadata_vars(tb_garden)
+
+    #
+    # Save outputs.
+    #
+    # Create a new garden dataset with the same metadata as the meadow dataset and add the garden table.
+    ds_garden = create_dataset(dest_dir, tables=[tb_garden], default_metadata=ds_meadow.metadata)
+
+    # Update dataset and table metadata using the adjacent yaml file.
+    ds_garden.update_metadata(paths.metadata_path)
+
+    # Save changes in the new garden dataset.
+    ds_garden.save()
+
+    log.info("world_inequality_database.end")
---
+++ etl/steps/data/meadow/wid/2023-08-24/world_inequality_database_distribution.py
@@ -0,0 +1,212 @@
+"""Load a snapshot and create a meadow dataset."""
+
+from typing import cast
+
+import numpy as np
+import pandas as pd
+from owid.catalog import Dataset, Table
+from structlog import get_logger
+
+from etl.helpers import PathFinder, create_dataset
+from etl.snapshot import Snapshot
+
+# Get paths and naming conventions for current step.
+paths = PathFinder(__file__)
+
+# Initialize logger.
+log = get_logger()
+
+# List of countries/regions not included in the ISO2 standard, but added by WID
+iso2_missing = {
+    "CN-RU": "China (rural)",
+    "CN-UR": "China (urban)",
+    "DD": "East Germany",
+    "KS": "Kosovo",
+    "OA": "Other Russia and Central Asia (WID)",
+    "OB": "Other East Asia (WID)",
+    "OC": "Other Western Europe (WID)",
+    "OD": "Other Latin America (WID)",
+    "OE": "Other MENA (WID)",
+    "OH": "Other North America and Oceania (WID)",
+    "OI": "Other South & South-East Asia (WID)",
+    "OJ": "Other Sub-Saharan Africa (WID)",
+    "QB": "Africa (WID)",
+    "QD": "Asia (WID)",
+    "QE": "Europe (WID)",
+    "QF": "Oceania (WID)",
+    "QJ": "Central Asia (WID)",
+    "QK": "East Africa (WID)",
+    "QL": "East Asia (WID)",
+    "QM": "Eastern Europe (WID)",
+    "QN": "Middle Africa (WID)",
+    "QO": "North Africa (WID)",
+    "QP": "North America (WID)",
+    "QS": "South-East Asia (WID)",
+    "QT": "South Africa region (WID)",
+    "QU": "South Asia (WID)",
+    "QV": "West Africa (WID)",
+    "QW": "West Asia (WID)",
+    "QX": "Western Europe (WID)",
+    "QY": "European Union (WID)",
+    "WO": "World",
+    "XA": "Asia (excluding Middle East) (WID)",
+    "XB": "North America and Oceania (WID)",
+    "XF": "Sub-Saharan Africa (WID)",
+    "XL": "Latin America (WID)",
+    "XM": "Middle East (WID)",
+    "XN": "MENA (WID)",
+    "XR": "Russia and Central Asia (WID)",
+    "XS": "South & South-East Asia (WID)",
+    "ZZ": "Zanzibar",
+}
+
+# We are not using these entities
+# iso2_missing_mer = {
+#     "OA-MER": "Other Russia and Central Asia (at market exchange rate) (WID)",
+#     "OB-MER": "Other East Asia (at market exchange rate) (WID)",
+#     "OC-MER": "Other Western Europe (at market exchange rate) (WID)",
+#     "OD-MER": "Other Latin America (at market exchange rate) (WID)",
+#     "OE-MER": "Other MENA (at market exchange rate) (WID)",
+#     "OH-MER": "Other North America and Oceania (at market exchange rate) (WID)",
+#     "OI-MER": "Other South & South-East Asia (at market exchange rate) (WID)",
+#     "OJ-MER": "Other Sub-Saharan Africa (at market exchange rate) (WID)",
+#     "QB-MER": "Africa (at market exchange rate) (WID)",
+#     "QD-MER": "Asia (at market exchange rate) (WID)",
+#     "QE-MER": "Europe (at market exchange rate) (WID)",
+#     "QF-MER": "Oceania (at market exchange rate) (WID)",
+#     "QJ-MER": "Central Asia (at market exchange rate) (WID)",
+#     "QK-MER": "East Africa (at market exchange rate) (WID)",
+#     "QL-MER": "East Asia (at market exchange rate) (WID)",
+#     "QM-MER": "Eastern Europe (at market exchange rate) (WID)",
+#     "QN-MER": "Middle Africa (at market exchange rate) (WID)",
+#     "QO-MER": "North Africa (at market exchange rate) (WID)",
+#     "QP-MER": "North America (at market exchange rate) (WID)",
+#     "QS-MER": "South-East Asia (at market exchange rate) (WID)",
+#     "QT-MER": "South Africa region (at market exchange rate) (WID)",
+#     "QU-MER": "South Asia (at market exchange rate) (WID)",
+#     "QV-MER": "West Africa (at market exchange rate) (WID)",
+#     "QW-MER": "West Asia (at market exchange rate) (WID)",
+#     "QX-MER": "Western Europe (at market exchange rate) (WID)",
+#     "QY-MER": "European Union (at market exchange rate) (WID)",
+#     "WO-MER": "World (at market exchange rate) (WID)",
+#     "XA-MER": "Asia (excluding Middle East) (at market exchange rate) (WID)",
+#     "XB-MER": "North America and Oceania (at market exchange rate) (WID)",
+#     "XF-MER": "Sub-Saharan Africa (at market exchange rate) (WID)",
+#     "XL-MER": "Latin America (at market exchange rate) (WID)",
+#     "XM-MER": "Middle East (at market exchange rate) (WID)",
+#     "XN-MER": "MENA (at market exchange rate) (WID)",
+#     "XR-MER": "Russia and Central Asia (at market exchange rate) (WID)",
+#     "XS-MER": "South & South-East Asia (at market exchange rate) (WID)",
+# }
+
+
+# Country harmonization function, using both the reference country/regional OWID dataset and WID's `iso2_missing` list
+def harmonize_countries(df: pd.DataFrame, iso2_missing: dict) -> pd.DataFrame:
+    # Load reference file with country names in OWID standard
+    df_countries_regions = cast(Dataset, paths.load_dependency("regions"))["regions"]
+
+    # Merge dataset and country dictionary to get the name of the country
+    df = pd.merge(
+        df, df_countries_regions[["name", "iso_alpha2"]], left_on="country", right_on="iso_alpha2", how="left"
+    )
+
+    # Several countries are not matched, because WID amends the ISO-2 list with additional countries and regions
+    # See https://wid.world/codes-dictionary/#country-code
+
+    # Replace missing items
+    for x, y in iso2_missing.items():
+        # NOTE: See if I can do this without numpy
+        df["name"] = np.where(df["country"] == x, y, df["name"])
+
+    missing_list = list(df[df["name"].isnull()]["country"].unique())
+    missing_count = len(missing_list)
+
+    # Warns if there are still entities missing
+    if missing_count > 0:
+        log.warning(
+            f"There are still {missing_count} WID countries/regions without a name and will be deleted! Take a look at this list:\n {missing_list}"
+        )
+
+    # Drop rows without match
+    df = df[~df["name"].isnull()].reset_index(drop=True)
+    # Drop old country and ISO alpha 2 variable. Rename the newly built variable as `country`
+    df = df.drop(columns=["country", "iso_alpha2"])
+    df = df.rename(columns={"name": "country"})
+
+    # Move country and year to the beginning
+    cols_to_move = ["country", "year"]
+    df = df[cols_to_move + [col for col in df.columns if col not in cols_to_move]]
+
+    return df
+
+
+def run(dest_dir: str) -> None:
+    #
+    # Load inputs.
+    #
+
+    # List of datasets with their snapshot
+    # Retrieve snapshot.
+    snap = cast(Snapshot, paths.load_dependency("world_inequality_database_distribution.csv"))
+
+    # Load data from snapshot.
+    # `keep_default_na` and `na_values` are included because there is a country labeled NA, Namibia, which becomes null without the parameters
+    na_values = [
+        "-1.#IND",
+        "1.#QNAN",
+        "1.#IND",
+        "-1.#QNAN",
+        "#N/A N/A",
+        "#N/A",
+        "N/A",
+        "n/a",
+        "",
+        "#NA",
+        "NULL",
+        "null",
+        "NaN",
+        "-NaN",
+        "nan",
+        "-nan",
+        "",
+    ]
+    df = pd.read_csv(
+        snap.path,
+        keep_default_na=False,
+        na_values=na_values,
+    )
+
+    # Retrieve snapshot with extrapolations
+    snap: Snapshot = paths.load_dependency("world_inequality_database_distribution_with_extrapolations.csv")
+    # Load data from snapshot.
+    # `keep_default_na` and `na_values` are included because there is a country labeled NA, Namibia, which becomes null without the parameters
+    df_extrapolations = pd.read_csv(
+        snap.path,
+        keep_default_na=False,
+        na_values=na_values,
+    )
+
+    # Combine both datasets
+    df = pd.merge(
+        df,
+        df_extrapolations[["country", "year", "welfare", "percentile", "thr", "avg", "share"]],
+        on=["country", "year", "welfare", "percentile"],
+        how="outer",
+        suffixes=("", "_extrapolated"),
+    )
+
+    # Harmonize countries
+    df = harmonize_countries(df, iso2_missing)
+
+    # Create a new table and ensure all columns are snake-case.
+    tb = Table(df, short_name=paths.short_name, underscore=True)
+    tb = tb.set_index(["country", "year", "welfare", "percentile", "p"], verify_integrity=True).sort_index()
+
+    #
+    # Save outputs.
+    #
+    # Create a new meadow dataset with the same metadata as the snapshot.
+    ds_meadow = create_dataset(dest_dir, tables=[tb], default_metadata=snap.metadata)
+
+    # Save changes in the new garden dataset.
+    ds_meadow.save()
