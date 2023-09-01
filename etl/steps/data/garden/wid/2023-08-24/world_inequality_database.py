4add578079f0eb7c8cb56f6a8555936d9b48fcfb
Pablo Arriagada <paarriagadap@gmail.com>
2023-09-01 17:29:32 +0100

    refactor: drop the use of pandas

--- etl/steps/data/garden/wid/2023-08-24/world_inequality_database.py
+++ etl/steps/data/garden/wid/2023-08-24/world_inequality_database.py
@@ -1,7 +1,6 @@
 """Load World Inequality Database meadow dataset and create a garden dataset."""
 
 
-import pandas as pd
 from owid.catalog import Dataset, Table
 from shared import add_metadata_vars, add_metadata_vars_distribution
 from structlog import get_logger
@@ -15,23 +14,20 @@ paths = PathFinder(__file__)
 
 
 # Data processing function (cleaning and small transformations)
-def data_processing(df: pd.DataFrame) -> pd.DataFrame:
+def data_processing(tb: Table) -> Table:
     # Multiply shares by 100
-    df[list(df.filter(like="share"))] *= 100
+    tb[list(tb.filter(like="share"))] *= 100
 
     # Delete age and pop, two one-value variables
-    df = df.drop(columns=["age", "pop", "age_extrapolated", "pop_extrapolated"])
+    tb = tb.drop(columns=["age", "pop", "age_extrapolated", "pop_extrapolated"])
 
     # Delete some share ratios we are not using, and also the p0p40 (share) variable only available for pretax
     drop_list = ["s90_s10_ratio", "s90_s50_ratio", "p0p40"]
 
     for var in drop_list:
-        df = df[df.columns.drop(list(df.filter(like=var)))]
+        tb = tb[tb.columns.drop(list(tb.filter(like=var)))]
 
-    # Verify index and sort
-    df = df.set_index(["country", "year"], verify_integrity=True).sort_index()
-
-    return df
+    return tb
 
 
 def run(dest_dir: str) -> None:
@@ -44,20 +40,15 @@ def run(dest_dir: str) -> None:
     ds_meadow: Dataset = paths.load_dependency("world_inequality_database")
 
     # Read table from meadow dataset.
-    tb_meadow = ds_meadow["world_inequality_database"]
-
-    # Create a dataframe with data from the table.
-    df = pd.DataFrame(tb_meadow).reset_index()
+    tb = ds_meadow["world_inequality_database"]
 
     #
     # Process data.
     # Change units and drop unnecessary columns
-    df = data_processing(df)
+    tb = data_processing(tb)
 
-    # Create a new table with the processed data.
-    tb_garden = Table(df, short_name="world_inequality_database")
-
-    tb_garden = add_metadata_vars(tb_garden)
+    # Add metadata by code
+    tb = add_metadata_vars(tb)
 
     ########################################
     # Percentile data
@@ -78,10 +69,7 @@ def run(dest_dir: str) -> None:
     # Save outputs.
     #
     # Create a new garden dataset with the same metadata as the meadow dataset and add the garden table.
-    ds_garden = create_dataset(dest_dir, tables=[tb_garden, tb_percentiles], default_metadata=ds_meadow.metadata)
-
-    # Update dataset and table metadata using the adjacent yaml file.
-    ds_garden.update_metadata(paths.metadata_path)
+    ds_garden = create_dataset(dest_dir, tables=[tb, tb_percentiles], default_metadata=ds_meadow.metadata)
 
     # Save changes in the new garden dataset.
     ds_garden.save()
