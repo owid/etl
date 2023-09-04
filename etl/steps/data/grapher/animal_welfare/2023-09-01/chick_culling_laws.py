6f52480d6122b205311e9b9161a86bef162921cf
Pablo Rosado <pabloarosado@gmail.com>
2023-09-04 11:40:34 +0200

    enhance(animal_welfare): Improve metadata and adapt table for grapher

--- etl/steps/data/grapher/animal_welfare/2023-09-01/chick_culling_laws.py
+++ etl/steps/data/grapher/animal_welfare/2023-09-01/chick_culling_laws.py
@@ -1,9 +1,48 @@
 """Load a garden dataset and create a grapher dataset."""
 
+from owid.catalog import Table
+
 from etl.helpers import PathFinder, create_dataset
 
 # Get paths and naming conventions for current step.
 paths = PathFinder(__file__)
+
+# Current year.
+CURRENT_YEAR = int(paths.version.split("-")[0])
+
+
+def generate_annotations_for_each_country(tb: Table):
+    tb_updated = tb.copy()
+
+    # Add annotations for each country.
+    annotations = {}
+    # Select countries that have some laws regarding chick culling.
+    countries_with_ban = tb[tb["status"] != "No laws"]["country"].unique()
+    for country in countries_with_ban:
+        year_effective = tb[tb["country"] == country]["year_effective"].item()
+        if year_effective <= CURRENT_YEAR:
+            annotations[country] = f"{country}: Banned since {year_effective}."
+        else:
+            annotations[country] = f"{country}: Ban starts in {year_effective}."
+
+    # Add any other annotation.
+    countries_with_annotations = tb[~tb["annotation"].isnull()]["country"].unique()
+    for country in countries_with_annotations:
+        additional_annotation = tb[tb["country"] == country]["annotation"].item()
+        if country in annotations:
+            annotations[country] += " " + additional_annotation
+        else:
+            annotations[country] = f"{country}: {additional_annotation}"
+
+    # Convert to list.
+    annotations = list(annotations.values())
+
+    # Update display metadata.
+    if tb_updated["status"].metadata.display is None:
+        tb_updated["status"].metadata.display = {}
+    tb_updated["status"].metadata.display["annotations"] = annotations
+
+    return tb_updated
 
 
 def run(dest_dir: str) -> None:
@@ -14,7 +53,23 @@ def run(dest_dir: str) -> None:
     ds_garden = paths.load_dataset("chick_culling_laws")
 
     # Read table from garden dataset.
-    tb = ds_garden["chick_culling_laws"]
+    tb = ds_garden["chick_culling_laws"].reset_index()
+
+    #
+    # Process data.
+    #
+    # Add annotations for each country to metadata.
+    # NOTE: For now annotations are not shown in map tabs. Possibly in the future they will appear in map tab tooltips.
+    tb = generate_annotations_for_each_country(tb=tb)
+
+    # Select relevant columns.
+    tb = tb[["country", "status"]]
+
+    # Add current year (there should be no timeline in the chart).
+    tb["year"] = CURRENT_YEAR
+
+    # Set an appropriate index.
+    tb = tb.set_index(["country", "year"], verify_integrity=True).sort_index()
 
     #
     # Save outputs.
--- snapshots/animal_welfare/2023-09-01/chick_culling_laws.py
+++ snapshots/animal_welfare/2023-09-01/chick_culling_laws.py
@@ -18,58 +18,64 @@ def main(upload: bool) -> None:
     snap = Snapshot(f"animal_welfare/{SNAPSHOT_VERSION}/chick_culling_laws.csv")
 
     # Create a table of data manually extracted from different websites.
-    columns = ["country", "status", "enforcement_year", "comments", "evidence", "url"]
+    columns = ["country", "status", "year_effective", "comments", "evidence", "url", "annotation"]
     data = [
-        # Add countries where chick culling is fully or partially banned by law (or planned to be so).
+        # Add countries where chick culling is fully or partially banned (or planned to be so).
         (
             "Austria",
-            "Banned by law",
+            "Banned",
             2023,
-            "Enforcement date: 2022-07-18. The prohibition was adopted in July 2022 through a law amending the Animal Welfare Act (130. Bundesgesetz, mit dem das Tierschutzgesetz-TSchG und das Tiertransportgesetz geändert werden). Scope excludes male chicks used as feed in zoos or for birds of prey. Destruction of non-hatched is allowed up until 14 day of incubation.",
-            "Section 6(2), Animal Welfare Act [Original: Tierschutzgesetz-TSch, Section 6(2)]",
+            "Date effective: 2022-07-18. The prohibition was adopted in July 2022 through a law amending the Animal Welfare Act (130. Bundesgesetz, mit dem das Tierschutzgesetz-TSchG und das Tiertransportgesetz geändert werden). Scope excludes male chicks used as feed in zoos or for birds of prey. Destruction of non-hatched is allowed up until 14 day of incubation.",
+            "Section 6(2), Animal Welfare Act",
             "https://www.ris.bka.gv.at/GeltendeFassung.wxe?Abfrage=Bundesnormen&Gesetzesnummer=20003541",
+            "Excludes male chicks used as feed in zoos or for birds of prey.",
         ),
         (
             "France",
-            "Banned by law",
+            "Banned",
             2023,
-            "Enforcement date: 2022-12-31. The prohibition was adopted on January 2022, through a regulation (Décret n° 2022-137 du 5 février 2022 relatif à l'interdiction de mise à mort des poussins des lignées de l'espèce Gallus gallus destinées à la production d'oeufs de consommation et à la protection des animaux dans le cadre de leur mise à mort en dehors des établissements d'abattage). In ovo sexing technologies benefit from a five-year nonobsolescence clause. Male chicks for animal food production benefit from an exemption. Destruction of non-hatched is allowed up until 15 day of incubation.",
-            "R 214-17 of the Rural Code [Original: R.214-17, Code rural et la pêche maritime]",
+            "Date effective: 2022-12-31. The prohibition was adopted on January 2022, through a regulation (Décret n° 2022-137 du 5 février 2022 relatif à l'interdiction de mise à mort des poussins des lignées de l'espèce Gallus gallus destinées à la production d'oeufs de consommation et à la protection des animaux dans le cadre de leur mise à mort en dehors des établissements d'abattage). In ovo sexing technologies benefit from a five-year nonobsolescence clause. Male chicks for animal food production benefit from an exemption. Destruction of non-hatched is allowed up until 15 day of incubation.",
+            "R 214-17 of the Rural Code",
             "https://www.legifrance.gouv.fr/codes/article_lc/LEGIARTI000028969470",
+            "Male chicks for animal food production benefit from an exemption.",
         ),
         (
             "Germany",
-            "Banned by law",
+            "Banned",
             2022,
-            "Enforcement date: 2022-01-01. The prohibiton was adopted on January 2022, through a regulation which prohibits the culling of one-day old chicks by 2022, and the culling of fertilized eggs passed the 6th day of incubation. Note: No derogation.",
-            "Section 3 (4c), Animal Welfare Act [Original: Tierschutzgesetz, Dritter Abschnitt Töten von Tieren, 4c]",
+            "Date effective: 2022-01-01. The prohibition was adopted on January 2022, through a regulation which prohibits the culling of one-day old chicks by 2022, and the culling of fertilized eggs passed the 6th day of incubation. Note: No derogation.",
+            "Section 3 (4c), Animal Welfare Act",
             "https://www.gesetze-im-internet.de/tierschg/BJNR012770972.html",
+            "",
         ),
         (
             "Italy",
-            "Planned to be banned by law",
+            "Banned but not yet effective",
             2027,
-            "Enforcement date: 2026-12-31. The law prohibits the selective killing of male chicks by December 31st, 2026 and provides exemptions for animal protection purposes only. A decree will later specify the ways in which the law should be implemented. The law does not provide a rule regarding the destruction of non-hatched eggs nor exemptions, other than exemptions for animal health and protection purposes. A decree will likely specify these two aspects.",
-            "Article 18, European Delegation Law (22G00136) [Original: Articolo 18, Delega al Governo per il recepimento delle direttive europee e l'attuazione di altri atti normativi dell'Unione europea - Legge di delegazione europea 2021 (22G00136)]",
+            "Date effective: 2026-12-31. The law prohibits the selective killing of male chicks by December 31st, 2026 and provides exemptions for animal protection purposes only. A decree will later specify the ways in which the law should be implemented. The law does not provide a rule regarding the destruction of non-hatched eggs nor exemptions, other than exemptions for animal health and protection purposes. A decree will likely specify these two aspects.",
+            "Article 18, European Delegation Law (22G00136)",
             "https://www.normattiva.it/uri-res/N2Ls?urn:nir:stato:legge:2022-08-04;127",
+            "Provides exemptions for animal protection purposes only.",
         ),
         (
             "Luxembourg",
-            "Banned by law",
+            "Banned",
             2018,
             "",
-            "Enforcement date: 2018-06-18. Animal protection law on 6 June 2018, The Luxembourg Government.",
+            "Date effective: 2018-06-18. Animal protection law on 6 June 2018, The Luxembourg Government.",
             "https://gouvernement.lu/en/dossiers/2018/tierschutz.html",
+            "",
         ),
         (
             "Switzerland",
-            "Partially banned by law",
-            np.nan,
+            "Partially banned",
+            2019,
             "Grinding chicks was banned in 2019, but gassing is still legal.",
             "Swissinfo (2019-09-20).",
             "https://www.swissinfo.ch/eng/society/animal-protection_-switzerland-bans-shredding-of-male-chicks-/45240798",
+            "Grinding is banned, but gassing is still legal.",
         ),
-        # Add countries for which there is evidence of chick culling with no ban enforced or planned.
+        # Add countries for which there is evidence of chick culling with no ban.
         (
             "Australia",
             "No laws",
@@ -77,6 +83,7 @@ def main(upload: bool) -> None:
             "",
             "Royal Society for the Prevention of Cruelty to Animals (RSPCA) Australia (2021-09-22).",
             "https://kb.rspca.org.au/knowledge-base/what-happens-with-male-chicks-in-the-egg-industry/",
+            "",
         ),
         (
             "Canada",
@@ -85,6 +92,7 @@ def main(upload: bool) -> None:
             "",
             "Canadian Poultry Magazine (2016-12-19)",
             "https://www.canadianpoultrymag.com/hypereye-a-game-changer-30033/",
+            "",
         ),
         (
             "New Zealand",
@@ -93,6 +101,7 @@ def main(upload: bool) -> None:
             "",
             "Save Animals From Exploitation (SAFE) New Zealand (2023).",
             "https://safe.org.nz/our-work/animals-in-aotearoa/male-chicks/",
+            "",
         ),
         (
             "United Kingdom",
@@ -101,6 +110,7 @@ def main(upload: bool) -> None:
             "",
             "The Humane League (2021-07-29).",
             "https://thehumaneleague.org.uk/article/what-happens-to-male-chicks-in-the-egg-industry",
+            "",
         ),
         (
             "United States",
@@ -109,6 +119,7 @@ def main(upload: bool) -> None:
             "",
             "Vox (2021-04-12).",
             "https://www.vox.com/future-perfect/22374193/eggs-chickens-animal-welfare-culling",
+            "",
         ),
     ]
     # Countries in the European Union for which there is no law against chick culling.
@@ -145,6 +156,7 @@ def main(upload: bool) -> None:
                 "",
                 "European Parliamentary Research Service (2022-12).",
                 "https://www.europarl.europa.eu/RegData/etudes/ATAG/2022/739246/EPRS_ATA(2022)739246_EN.pdf",
+                "",
             ),
         )
     tb = snap.read_from_records(data=data, columns=columns)
@@ -152,7 +164,7 @@ def main(upload: bool) -> None:
     # Add all individual sources to the full citation in the metadata.
     sources_text = """Evidence of laws banning chick culling, and evidence of chick culling being practiced without any ban, has been gathered from various sources for different countries.\n Some of those sources were extracted from [a report by the European Institute for Animal Law & Policy](https://animallaweurope.com/wp-content/uploads/2023/01/Animal-Law-Europe-%E2%80%93-Chick-Killing-Report-2023.pdf): "Chick and Duckling Killing: Achieving an EU-Wide Prohibition" (White paper, January 2023) by Alice Di Concetto, Olivier Morice, Matthias Corion, Simão Santos.\n"""
     for i, row in tb.iterrows():
-        sources_text += f"* {row['country']}: {row['status']}. Source: [{row['evidence']}]({row['url']})."
+        sources_text += f"- {row['country']}: {row['status']}. Source: [{row['evidence']}]({row['url']})."
         if len(row["comments"]) > 0:
             sources_text += f" {row['comments']}"
         sources_text += "\n"
