e33eda49fe0fb205eea00d66f22cf0e257fd442e
lucasrodes <lucasrodes@users.noreply.github.com>
2023-09-13 15:45:20 +0200

    add brecke regions

--- etl/steps/data/garden/war/2023-07-20/brecke.py
+++ etl/steps/data/garden/war/2023-07-20/brecke.py
@@ -19,26 +19,26 @@ On regions:
 
     We have manually inferred the mappings using COW codes. Our current estimate for the original regions is as follows:
 
-    - North America, Central America, and the Caribbean: 2-95 (US-Panama)
-    - South America: 100-165 (Colombia-Uruguay)
-    - Western Europe: 200-280 (UK-Mecklenburg Schwerin), 305 (Autria), 325-338 (Italy-Malta), 380-395 (Sweden-Iceland)
-    - Eastern Europe: 290-300 (Poland-Austria-Hungary), 310-317 (Hungary-Slovakia), 339-375 (Albania-Finland), 640 (Turkey)
-    - Middle East: 630-645 (Iran-Iraq), 652-698
-    - North Africa: 600-626 (Morocco-South Sudan) 432 (Mali), 435-436 (Mauritania-Niger), 483 (Niger), 651 (Egypt)
-    - West & Central Africa: 402-420 (Cape Verde, Gambia), 433 (Senegal-)
-    - East & South Africa:
-    - Central Asia:
-    - South Asia:
-    - Southeast Asia:
-    - East Asia:
+    1. North America, Central America, and the Caribbean: 2-95 (US-Panama)
+    2. South America: 100-165 (Colombia-Uruguay)
+    3. Western Europe: 200-280 (UK-Mecklenburg Schwerin), 305 (Autria), 325-338 (Italy-Malta), 380-395 (Sweden-Iceland)
+    4. Eastern Europe: 290-300 (Poland-Austria-Hungary), 310-317 (Hungary-Slovakia), 339-375 (Albania-Finland), 640 (Turkey)
+    5. Middle East: 630 (Iran), 645-698 (Iraq-Oman)
+    6. North Africa: 600-626 (Morocco-South Sudan) 432 (Mali), 435-436 (Mauritania-Niger), 483 (Niger), 651 (Egypt)
+    7. West & Central Africa: 402-420 (Cape Verde, Gambia), 433-434 (Senegal-Benin), 437-482 (Ivory Coast-Central African Republic), 484-490 (Congo-Democratic Republic of the Congo)
+    8. East & South Africa: 500-591 (Uganda-Seychelles)
+    9. Central Asia: 700-705 (Afghanistan-Kazakhstan)
+    10. South Asia: 750-771 (India-Bangladesh), 780-790 (Sri Lanka-Nepal)
+    11. Southeast Asia: 800-990 (Thailand-Samoa)
+    12. East Asia: 710-740 (Taiwan-Japan)
 
     With the mapping done with `REGIONS_RENAME`, we have:
 
-    - Americas:
-    - Europe:
-    - Middle East:
-    - Africa:
-    - Asia and Oceania:
+    - Americas (1, 2): 2-165
+    - Europe (3, 4): 200-395 (UK-Iceland), 640 (Turkey)
+    - Middle East (5): 630 (Iran), 645-698 (Iraq-Oman)
+    - Africa (6, 7, 8): 402-626 (Cape Verde-South Sudan),
+    - Asia and Oceania (9, 10, 11, 12): 700-990 (Afghanistan-Samoa)
 """
 
 from typing import cast
