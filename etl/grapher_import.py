c9246c7cf40c976bfcfb64c4ad0a2ca86ae7026e
Marigold <mojmir.vinkler@gmail.com>
2023-09-14 13:11:54 +0200

    :bug: fix MySQL deadlocks

--- etl/grapher_import.py
+++ etl/grapher_import.py
@@ -39,6 +39,7 @@ log = structlog.get_logger()
 
 source_table_lock = Lock()
 origins_table_lock = Lock()
+links_lock = Lock()
 
 
 CURRENT_DIR = os.path.dirname(__file__)
@@ -275,21 +276,24 @@ def upsert_table(
         # less than 10ms per variable
         df = add_entity_code_and_name(session, df)
 
-        # delete all previous relationships
-        db_variable.delete_links(session)
+        # We're sometimes getting deadlock from inserts into origins_variables table
+        # It is safer to add threadlock, even though it might be slowing things down a bit
+        with links_lock:
+            # delete all previous relationships
+            db_variable.delete_links(session)
 
-        # create links, we need to do it after we commit deleted relationships above
-        db_variable.create_links(
-            session,
-            db_origins,
-            faqs=variable_meta.presentation.faqs if variable_meta.presentation else [],
-            tag_names=variable_meta.presentation.topic_tags if variable_meta.presentation else [],
-        )
-        session.add(db_variable)
+            # create links, we need to do it after we commit deleted relationships above
+            db_variable.create_links(
+                session,
+                db_origins,
+                faqs=variable_meta.presentation.faqs if variable_meta.presentation else [],
+                tag_names=variable_meta.presentation.topic_tags if variable_meta.presentation else [],
+            )
+            session.add(db_variable)
 
-        # we need to commit changes because we use SQL command in `variable_metadata`. We wouldn't
-        # have to if we used ORM instead
-        session.commit()
+            # we need to commit changes because we use SQL command in `variable_metadata`. We wouldn't
+            # have to if we used ORM instead
+            session.commit()
 
         # process and upload data to S3
         var_data = variable_data(df)
