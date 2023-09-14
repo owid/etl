a323d1bc0278ecf1aa675de4c4fd32bf3c9534f7
Mojmir Vinkler <mojmir.vinkler@gmail.com>
2023-09-14 13:19:08 +0200

    :tada: ETL --watch mode (#1595)
    * 🎉 add watch mode to ETL

--- etl/config.py
+++ etl/config.py
@@ -90,6 +90,10 @@ IPDB_ENABLED = False
 # number of workers for grapher inserts
 GRAPHER_INSERT_WORKERS = int(env.get("GRAPHER_WORKERS", 40))
 
+# only upsert indicators matching this filter, this is useful for fast development
+# of data pages for a single indicator
+GRAPHER_FILTER = env.get("GRAPHER_FILTER", None)
+
 # forbid any individual step from consuming more than this much memory
 # (only enforced on Linux)
 MAX_VIRTUAL_MEMORY_LINUX = 32 * 2**30  # 32 GB
