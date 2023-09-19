1b1c260c8bf50cab09ab1424277d3adfb481e725
spoonerf <spoonerfiona@gmail.com>
2023-09-19 12:07:22 +0100

    grapher steps

--- etl/steps/data/garden/un/2023-08-29/long_run_child_mortality.py
+++ etl/steps/data/garden/un/2023-08-29/long_run_child_mortality.py
@@ -1,18 +1,11 @@
 """Load a meadow dataset and create a garden dataset."""
 import owid.catalog.processing as pr
-from owid.catalog import Dataset, Table
+from owid.catalog import Table
 
-from etl.data_helpers import geo
 from etl.helpers import PathFinder, create_dataset
-from etl.paths import DATA_DIR
 
 # Get paths and naming conventions for current step.
 paths = PathFinder(__file__)
-
-# Gapminder dataset location - it doesn't seem to load through regular dependencies as open_numbers isn't included as a channel
-DATASET_GAPMINDER_CHILD_MORTALITY = DATA_DIR / "open_numbers" / "open_numbers" / "latest" / "gapminder__child_mortality"
-
-GAPMINDER_SOURCE_NAME = "gapminder"
 
 
 def run(dest_dir: str) -> None:
@@ -21,9 +14,9 @@ def run(dest_dir: str) -> None:
     #
     # Load meadow dataset.
     ds_igme = paths.load_dataset("igme")
-
+    ds_gapminder = paths.load_dataset("under_five_mortality")
     # Read table from meadow dataset.
-    tb = ds_igme["igme"].reset_index()
+    tb_igme = ds_igme["igme"].reset_index()
 
     # Select out columns of interest.
     cols = [
@@ -31,72 +24,46 @@ def run(dest_dir: str) -> None:
         "year",
         "observation_value__deaths_per_1_000_live_births__under_five_mortality_rate__both_sexes__all_wealth_quintiles",
     ]
-    tb = tb[cols]
-    tb = tb.rename(
+    tb_igme = tb_igme[cols]
+    tb_igme = tb_igme.rename(
         columns={
-            "observation_value__deaths_per_1_000_live_births__under_five_mortality_rate__both_sexes__all_wealth_quintiles": "under_five_mortality_rate"
+            "observation_value__deaths_per_1_000_live_births__under_five_mortality_rate__both_sexes__all_wealth_quintiles": "under_five_mortality"
         }
     )
 
-    tb["source"] = "igme"
-    # Load Gapminder data for years before 2016 - after this predictions are used from UN WPP
-    ds_gapminder, tb_gapminder = load_gapminder_data(maximum_year=2016)
+    tb_igme["source"] = "igme"
+    # Load full Gapminder data
+    tb_gap = ds_gapminder["under_five_mortality"].reset_index()
+    tb_gap["source"] = "gapminder"
     #
-    # Process Gapminder data.
-    #
-    tb_gapminder = geo.harmonize_countries(
-        df=tb_gapminder,
-        countries_file=paths.country_mapping_path,
-    )
+    tb_gap_sel = ds_gapminder["under_five_mortality_selected"].reset_index()
+    tb_gap_sel["source"] = "gapminder"
 
     # Combine IGME and Gapminder data
-    tb_combined = pr.concat([tb, tb_gapminder]).sort_values(["country", "year", "source"])
-    tb_combined.metadata.short_name = "long_run_child_mortality"
-    # For overlapping years, prefer IGME data
-    tb_combined = remove_duplicates(tb_combined, preferred_source="igme")
+    tb_combined = combine_datasets(tb_igme, tb_gap, "long_run_child_mortality")
+    tb_combined_sel = combine_datasets(tb_igme, tb_gap_sel, "long_run_child_mortality_selected")
+
     #
     # Save outputs.
     tb_combined = tb_combined.drop(columns=["source"]).set_index(["country", "year"], verify_integrity=True)
-
+    tb_combined_sel = tb_combined_sel.drop(columns=["source"]).set_index(["country", "year"], verify_integrity=True)
     #
     # Create a new garden dataset with the same metadata as the meadow dataset.
-    ds_garden = create_dataset(
-        dest_dir, tables=[tb_combined], check_variables_metadata=True, default_metadata=ds_gapminder.metadata
-    )
+    ds_garden = create_dataset(dest_dir, tables=[tb_combined, tb_combined_sel], check_variables_metadata=True)
     ds_garden.update_metadata(metadata_path=paths.metadata_path)
     # Save changes in the new garden dataset.
     ds_garden.save()
 
 
-def load_gapminder_data(maximum_year: int) -> Table:
+def combine_datasets(tb_igme: Table, tb_gap: Table, table_name: str) -> Table:
     """
-    Load gapminder dataset and subset it to years before `maximum_year`.
-    See: https://www.gapminder.org/data/documentation/gd005/
+    Combine IGME and Gapminder data.
     """
-    ds = Dataset(DATASET_GAPMINDER_CHILD_MORTALITY)
-    tb = ds["child_mortality_0_5_year_olds_dying_per_1000_born"]
+    tb_combined = pr.concat([tb_igme, tb_gap]).sort_values(["country", "year", "source"])
+    tb_combined.metadata.short_name = table_name
+    tb_combined = remove_duplicates(tb_combined, preferred_source="igme")
 
-    # reset index
-    tb = tb.reset_index()
-
-    # add source
-    tb["source"] = GAPMINDER_SOURCE_NAME
-    # columns
-    tb = tb.rename(
-        columns={
-            "time": "year",
-            "geo": "country",
-            "child_mortality_0_5_year_olds_dying_per_1000_born": "under_five_mortality_rate",
-        }
-    )
-    # tb = tb.copy(deep=True)
-    msk = tb["year"] <= maximum_year
-    tb = tb[msk]
-
-    # output columns
-    tb = tb[["country", "year", "under_five_mortality_rate", "source"]]
-
-    return ds, tb
+    return tb_combined
 
 
 def remove_duplicates(tb: Table, preferred_source: str) -> Table:
