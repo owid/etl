a243b5fac2dbb2e6b0852dc376e2105e8ace5e6f
lucasrodes <lucasrodes@users.noreply.github.com>
2023-09-26 16:25:46 +0200

    fix metadata propagation

--- etl/steps/data/meadow/war/2023-09-25/peace_diehl.py
+++ etl/steps/data/meadow/war/2023-09-25/peace_diehl.py
@@ -17,9 +17,18 @@ def run(dest_dir: str) -> None:
     snap = paths.load_snapshot("peace_diehl.csv")
 
     # Load data from snapshot.
-    tb = pr.read_fwf(snap.path, header=None, metadata=snap.to_table_metadata())
-    tb = tb[0].str.split(",", expand=True)
-    # tb = Table(df, metadata=snap.to_table_metadata(), short_name=paths.short_name)
+    tb = snap.read_fwf(header=None, names=["data"])
+    meta_origins = tb["data"].metadata.origins
+
+    # Split into multiple columns (returned columns have numeric names, i.e. metadata is lost)
+    tb = tb["data"].str.split(",", expand=True)
+
+    # Rename columns
+    tb = tb.rename(columns={col: str(col) for col in tb.columns})
+
+    # Ensure metadata in indicators is preserved
+    for column in tb.all_columns:
+        tb[column].metadata.origins = meta_origins
 
     #
     # Process data.
@@ -28,7 +37,9 @@ def run(dest_dir: str) -> None:
     # Concatenate related columns; go from (code, years_1, peace_scale_level_1, years_2, peace_scale_level_2, ...) -> (code, years, peace_scale_level)
     tbs = []
     for i in range(1, tb.shape[1], 2):
-        tb_ = tb[[0, i, i + 1]].rename(columns={0: "code", i: "years", i + 1: "peace_scale_level"})
+        tb_ = tb[["0", str(i), str(i + 1)]].rename(
+            columns={"0": "code", str(i): "years", str(i + 1): "peace_scale_level"}
+        )
         tbs.append(tb_)
     tb = pr.concat(tbs, ignore_index=True, short_name=paths.short_name)
 
@@ -69,4 +80,5 @@ def run(dest_dir: str) -> None:
     ds_meadow = create_dataset(dest_dir, tables=[tb], check_variables_metadata=True, default_metadata=snap.metadata)
 
     # Save changes in the new meadow dataset.
+    ds_meadow.save()
     ds_meadow.save()
