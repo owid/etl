b7883b720998574e44de75a8f368aaac5d3fd216
lucasrodes <lucasrodes@users.noreply.github.com>
2023-09-29 12:32:49 +0200

    lint

--- etl/steps/data/meadow/countries/2023-09-25/isd.py
+++ etl/steps/data/meadow/countries/2023-09-25/isd.py
@@ -28,10 +28,12 @@ def run(dest_dir: str) -> None:
     tb = tb.astype({"othername": str, "decdate": str, "latitude": str, "longitude": str})
 
     # Replace -9 -> NaN
-    tb = tb.replace({
-        -9: np.nan,
-        "-9": np.nan,
-    })
+    tb = tb.replace(
+        {
+            -9: np.nan,
+            "-9": np.nan,
+        }
+    )
 
     # Remove -9 from categorical columns
     columns_cat = tb.dtypes[tb.dtypes.isin(["category", "object"])].index
