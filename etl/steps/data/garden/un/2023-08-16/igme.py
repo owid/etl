7d0679fd086e4b358be9cfe8e0a1a0c1fc979a83
Fiona Spooner <fiona@ourworldindata.org>
2023-10-03 13:17:15 +0100

    📊 Under-fifteen mortality rate - two versions of UN IGME (#1669)
    * adding under fifteen deaths
    
    * u15 deaths
    
    * update
    
    * adding missing dimensions
    
    * adding in the u15 mort rate for just the observed values - both sexes
    
    * dropping intermediate column
    
    * update table name
    
    * adding 'Dataset' so paths.load_dependency() works correctly
    
    * updating table name
    
    * fixing issue with pivot - not sure but the join_column_levels_with argument seemed to cause issues with values going missing
    
    * typo
    
    * matching up the indicators
    
    * adding more metadata
    
    * changing snapshot version to better reflect data version
    
    * updating version
    
    * adding in lucas's suggestions :)
    
    * fix

--- etl/steps/data/garden/un/2023-08-16/igme.py
+++ etl/steps/data/garden/un/2023-08-16/igme.py
@@ -1,8 +1,10 @@
 """Load a meadow dataset and create a garden dataset."""
 
 from math import trunc
+from typing import List
 
-from owid.catalog import Table
+import owid.catalog.processing as pr
+from owid.catalog import Dataset, Table
 
 from etl.data_helpers import geo
 from etl.helpers import PathFinder, create_dataset
@@ -16,11 +18,13 @@ def run(dest_dir: str) -> None:
     # Load inputs.
     #
     # Load meadow dataset.
-    ds_meadow = paths.load_dataset("igme")
-
+    ds_meadow: Dataset = paths.load_dependency("igme", version="2023-08-16")
+    # Load vintage dataset which has older data.
+    ds_vintage: Dataset = paths.load_dependency("igme", version="2018")
     # Read table from meadow dataset.
     tb = ds_meadow["igme"].reset_index()
-
+    tb_vintage = ds_vintage["igme"].reset_index()
+    tb_youth = tb_vintage[tb_vintage["indicator_name"].isin(["Deaths age 5 to 14", "Mortality rate age 5 to 14"])]
     #
     # Process data.
     #
@@ -32,12 +36,38 @@ def run(dest_dir: str) -> None:
     tb = tb.rename(
         columns={"obs_value": "Observation value", "lower_bound": "Lower bound", "upper_bound": "Upper bound"}
     )
+    tb["source"] = "igme (current)"
+
+    # Process 5-14 mortality data
+    tb_youth = tb_youth.rename(
+        columns={
+            "indicator_name": "indicator",
+            "sex_name": "sex",
+            "unit_measure_name": "unit_of_measure",
+            "obs_value": "Observation value",
+            "lower_bound": "Lower bound",
+            "upper_bound": "Upper bound",
+        }
+    ).drop(columns=["series_name_name"])
+    tb_youth = clean_values(tb_youth)
+    tb_youth["wealth_quintile"] = "All wealth quintiles"
+    tb_youth["source"] = "igme (2018)"
+    tb_youth["indicator"] = tb_youth["indicator"].replace({"Mortality rate age 5 to 14": "Mortality rate age 5-14"})
+
+    # Combine datasets with a preference for the current data when there is a conflict.
+
+    tb = combine_datasets(tb_a=tb, tb_b=tb_youth, table_name="igme_combined", preferred_source="igme (current)")
+
+    tb = calculate_under_fifteen_deaths(tb)
+
     tb = tb.pivot(
         index=["country", "year"],
         values=["Observation value", "Lower bound", "Upper bound"],
         columns=["unit_of_measure", "indicator", "sex", "wealth_quintile"],
-        join_column_levels_with="-",
     )
+    tb.columns = ["-".join(col).strip() for col in tb.columns.values]
+    tb = tb.reset_index()
+    tb = calculate_under_fifteen_mortality_rates(tb)
     # Add some metadata to the variables. Getting the unit from the column name and inferring the number of decimal places from the unit.
     # If it contains " per " we know it is a rate and should have 1 d.p., otherwise it should be an integer.
     for col in tb.columns[2:]:
@@ -56,6 +86,126 @@ def run(dest_dir: str) -> None:
 
     # Save changes in the new garden dataset.
     ds_garden.save()
+
+
+def calculate_under_fifteen_mortality_rates(tb: Table) -> Table:
+    """
+    First of all we must adjust the mortality rates, so that we can combine age groups together.
+
+    For example, if we want to calculate the mortality rate of under-fifteens then we need to combine the under-five mortality rate and the 5-14 year old age group.
+
+    If there are 100 deaths per 1000 under fives, then we need to adjust the denominator of the 5-14 age group to take account of this.
+    """
+    tb["adjusted_5_14_mortality_rate"] = (
+        (
+            1000
+            - tb[
+                "Observation value-Deaths per 1,000 live births-Under-five mortality rate-Both sexes-All wealth quintiles"
+            ]
+        )
+        / 1000
+    ) * tb["Observation value-Deaths per 1000 children aged 5-Mortality rate age 5-14-Both sexes-All wealth quintiles"]
+
+    tb[
+        "Observation value-Deaths per 1,000 live births-Under-fifteen mortality rate-Both sexes-All wealth quintiles"
+    ] = (
+        tb["Observation value-Deaths per 1,000 live births-Under-five mortality rate-Both sexes-All wealth quintiles"]
+        + tb["adjusted_5_14_mortality_rate"]
+    )
+    tb = tb.drop(columns="adjusted_5_14_mortality_rate")
+    return tb
+
+
+def combine_datasets(tb_a: Table, tb_b: Table, table_name: str, preferred_source: str) -> Table:
+    """
+    Combine two tables with a preference for one source of data.
+    """
+    tb_combined = pr.concat([tb_a, tb_b], short_name=table_name).sort_values(["country", "year", "source"])
+    assert any(tb_combined["source"] == preferred_source), "Preferred source not in table!"
+    tb_combined = remove_duplicates(
+        tb_combined,
+        preferred_source=preferred_source,
+        dimensions=["country", "year", "sex", "wealth_quintile", "indicator", "unit_of_measure"],
+    )
+
+    return tb_combined
+
+
+def remove_duplicates(tb: Table, preferred_source: str, dimensions: List[str]) -> Table:
+    """
+    Removing rows where there are overlapping years with a preference for IGME data.
+
+    """
+    assert any(tb["source"] == preferred_source)
+
+    duplicate_rows = tb.duplicated(subset=dimensions, keep=False)
+
+    tb_no_duplicates = tb[~duplicate_rows]
+
+    tb_duplicates = tb[duplicate_rows]
+
+    tb_duplicates_removed = tb_duplicates[tb_duplicates["source"] == preferred_source]
+
+    tb = pr.concat([tb_no_duplicates, tb_duplicates_removed])
+
+    assert len(tb[tb.duplicated(subset=dimensions, keep=False)]) == 0, "Duplicates still in table!"
+
+    return tb
+
+
+def calculate_under_fifteen_deaths(tb: Table) -> Table:
+    """
+    Calculate the under fifteen mortality total deaths.
+    """
+    tb_u5 = (
+        tb[(tb["indicator"] == "Under-five deaths") & (tb["unit_of_measure"] == "Number of deaths")]
+        .drop(columns="indicator")
+        .rename(
+            columns={
+                "Observation value": "under_five_mortality",
+                "Lower bound": "under_five_mortality_lb",
+                "Upper bound": "under_five_mortality_ub",
+            }
+        )
+    )
+    tb_5_14 = (
+        tb[(tb["indicator"] == "Deaths age 5 to 14") & (tb["unit_of_measure"] == "Number of deaths")]
+        .drop(columns="indicator")
+        .rename(
+            columns={
+                "Observation value": "five_to_fourteen_mortality",
+                "Lower bound": "five_to_fourteen_mortality_lb",
+                "Upper bound": "five_to_fourteen_mortality_ub",
+            }
+        )
+    )
+    tb_u15 = pr.merge(tb_u5, tb_5_14)
+
+    for suffix in ["", "_lb", "_ub"]:
+        tb_u15[f"under_fifteen_mortality{suffix}"] = (
+            tb_u15[f"under_five_mortality{suffix}"] + tb_u15[f"five_to_fourteen_mortality{suffix}"]
+        )
+
+    tb_u15 = tb_u15.drop(
+        columns=[
+            "under_five_mortality",
+            "under_five_mortality_lb",
+            "under_five_mortality_ub",
+            "five_to_fourteen_mortality",
+            "five_to_fourteen_mortality_lb",
+            "five_to_fourteen_mortality_ub",
+        ]
+    ).rename(
+        columns={
+            "under_fifteen_mortality": "Observation value",
+            "under_fifteen_mortality_lb": "Lower bound",
+            "under_fifteen_mortality_ub": "Upper bound",
+        }
+    )
+    tb_u15["indicator"] = "Under-fifteen deaths"
+
+    tb = pr.concat([tb, tb_u15])
+    return tb
 
 
 def filter_data(tb: Table) -> Table:
@@ -97,10 +247,10 @@ def clean_values(tb: Table) -> Table:
         "Second": "Second poorest quintile",
         "Fourth": "Fourth poorest quintile",
     }
-
-    tb["sex"] = tb["sex"].replace(sex_dict)
-
-    tb["wealth_quintile"] = tb["wealth_quintile"].replace(wealth_dict)
+    if "sex" in tb.columns:
+        tb["sex"] = tb["sex"].replace(sex_dict)
+    if "wealth_quintile" in tb.columns:
+        tb["wealth_quintile"] = tb["wealth_quintile"].replace(wealth_dict)
 
     return tb
 
