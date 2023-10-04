c12cdf429de3e78e9462cfd003e0b7debf3c14fc
spoonerf <spoonerfiona@gmail.com>
2023-10-04 15:28:04 +0100

    Adding commits

--- etl/steps/data/garden/ihme_gbd/2023-10-04/leading_causes_child_deaths.py
+++ etl/steps/data/garden/ihme_gbd/2023-10-04/leading_causes_child_deaths.py
@@ -13,20 +13,29 @@ log = get_logger()
 
 
 def run(dest_dir: str) -> None:
+    # Load in the cause of death data and hierarchy of causes data
     tb_cause = paths.load_dataset("gbd_cause")
     tb_hierarchy = paths.load_dataset("cause_hierarchy")
     tb_hierarchy = tb_hierarchy["cause_hierarchy"]
 
-    # Select out the level 2 and level 3 causes separately, level 2 are broader than level 3
+    # Underschore the hierachy cause names to match tb_cause
     tb_hierarchy["cause_name_underscore"] = tb_hierarchy["cause_name"].apply(underscore)
 
+    # We'll iterate through each level of the hierachy to find the leading cause of death in under-fives in each country-year
     levels = [1, 2, 3, 4]
 
     tb_out = []
     for level in levels:
+        # Get the causes at this level
         level_causes = tb_hierarchy[tb_hierarchy["level"] == level]["cause_name_underscore"].to_list()
-        level_causes = list(map(lambda item: underscore(item), level_causes))
-        tb_level = create_hierarchy_table(tb_cause, level_causes, short_name=f"leading_cause_level_{level}")
+        # Create table with leading cause of death at this level for each country-year
+        tb_level = create_hierarchy_table(
+            age_group="__under_5",
+            tb_cause=tb_cause,
+            level_causes=level_causes,
+            short_name=f"leading_cause_level_{level}",
+        )
+        # Make the disease names more readable
         tb_level = clean_disease_names(tb=tb_level, tb_hierarchy=tb_hierarchy)
         tb_level = tb_level.set_index(["country", "year"], verify_integrity=True)
         tb_out.append(tb_level)
@@ -49,14 +58,17 @@ def run(dest_dir: str) -> None:
     ds_garden.save()
 
 
-def create_hierarchy_table(tb_cause: Table, level_causes: List[str], short_name: str) -> Table:
-    u5_causes = [item for item in tb_cause.table_names if "__under_5" in item]
-    # u5_causes = list(map(lambda cause: cause.split("__")[0],  u5_causes))
+def create_hierarchy_table(age_group: str, tb_cause: Table, level_causes: List[str], short_name: str) -> Table:
+    u5_causes = [item for item in tb_cause.table_names if age_group in item]
+    assert u5_causes.shape[0] > 0, f"No causes found for {age_group}, check spelling"
+
     tb_out = Table()
     for cause in u5_causes:
         tb = tb_cause[cause].reset_index()
+        # Get cause name from table name
         cause_name = cause.split("__")[0]
         if cause_name in level_causes:
+            # Get deaths column from cause name
             death_col = f"deaths_that_are_from_{cause_name}__in_both_sexes_aged_under_5"
             if death_col in tb.columns:
                 cols = ["country", "year", death_col]
@@ -64,10 +76,11 @@ def create_hierarchy_table(tb_cause: Table, level_causes: List[str], short_name:
                 tb = tb.rename(columns={death_col: cause_name})
                 tb_out = pr.concat([tb_out, tb])
 
+    # Melt the table from wide to long to make it easier to groupby
     long_tb = pr.melt(
         tb_out, id_vars=["country", "year"], var_name=f"disease_{short_name}", value_name=f"deaths_{short_name}"
     )
-
+    # Find the cause of death with the highest number of deaths in each country-year
     leading_causes_idx = long_tb.groupby(["country", "year"], observed=True)[f"deaths_{short_name}"].idxmax()
     leading_causes_tb = long_tb.loc[leading_causes_idx]
 
