9b9c12e55359d45bcdb395b5e641c313c6a41426
veronikasamborska1994 <32176660+veronikasamborska1994@users.noreply.github.com>
2023-10-09 11:58:06 +0100

    Update bmj_2022.py

--- etl/steps/data/meadow/statins/2023-10-05/bmj_2022.py
+++ etl/steps/data/meadow/statins/2023-10-05/bmj_2022.py
@@ -23,34 +23,33 @@ def run(dest_dir: str) -> None:
     # Process data.
     #
 
-    # Melting the statin_utilization columns
+    # Melting the 'statin_utilization' columns and extracting the year
     melted_statin_utilization = pd.melt(
         tb,
-        id_vars=[
-            "country",
-        ],
+        id_vars=["country"],
         value_vars=["statin_utilization_october_2019", "statin_utilization_september_2020"],
         var_name="indicator",
         value_name="value",
     )
-
-    # Extracting year and renaming columns
     melted_statin_utilization["year"] = melted_statin_utilization["indicator"].str.extract(r"(\d+)")
     melted_statin_utilization["indicator"] = "Statin use"
     melted_statin_utilization["value"] = melted_statin_utilization["value"].astype(float)
 
+    # Processing 'health_expenditure_per_capita_2018' and renaming columns
     health_exp_tb = tb[["country", "health_expenditure_per_capita_2018"]]
     health_exp_tb["year"] = 2018
-    health_exp_tb["indicator"] = "Health expenditure per capita in 2018 "
+    health_exp_tb["indicator"] = "Health expenditure per capita in 2018"
     health_exp_tb = health_exp_tb.rename(columns={"health_expenditure_per_capita_2018": "value"})
     health_exp_tb["value"] = health_exp_tb["value"].replace(r"[\$,e]", "", regex=True).astype(float)
 
+    # Processing 'ihd_mortality_rate_2019' and renaming columns
     ihd_mort_tb = tb[["country", "ihd_mortality_rate_2019"]]
     ihd_mort_tb["year"] = 2019
-    ihd_mort_tb["indicator"] = "IHD mortality rate in 2019 "
+    ihd_mort_tb["indicator"] = "IHD mortality rate in 2019"
     ihd_mort_tb = ihd_mort_tb.rename(columns={"ihd_mortality_rate_2019": "value"})
     ihd_mort_tb["value"] = ihd_mort_tb["value"].astype(float)
 
+    # Processing 'statins_essential_medicine_2017' and renaming columns
     essential_med_tb = tb[["country", "statins_essential_medicine_2017"]]
     essential_med_tb["year"] = 2017
     essential_med_tb["indicator"] = "Essential medicine list"
@@ -60,9 +59,10 @@ def run(dest_dir: str) -> None:
     essential_med_tb["value"].replace("Yes", 1, inplace=True)
     essential_med_tb["value"] = essential_med_tb["value"].astype(float)
 
+    # Concatenating processed DataFrames to form a single DataFrame
     merged_df = pd.concat([essential_med_tb, ihd_mort_tb, health_exp_tb, melted_statin_utilization])
 
-    # Ensure all columns are snake-case, set an appropriate index, and sort conveniently.
+    # Harmonizing column names, setting index, and sorting
     merged_df = merged_df.underscore().set_index(["country", "year", "indicator"], verify_integrity=True).sort_index()
     tb = Table(merged_df, short_name=paths.short_name, metadata=snap.to_table_metadata())
     tb["value"].m.origins = [snap.m.origin]
