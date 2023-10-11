3df12ad71772b32a648d15c97ff78c4765e03045
veronikasamborska1994 <32176660+veronikasamborska1994@users.noreply.github.com>
2023-10-11 17:28:26 +0100

    snapshot change -> now extracts all ESC datasets based on provided links

--- etl/steps/data/meadow/cardiology/2023-10-10/esc.py
+++ etl/steps/data/meadow/cardiology/2023-10-10/esc.py
@@ -11,7 +11,7 @@ def run(dest_dir: str) -> None:
     # Load inputs.
     #
     # Retrieve snapshot.
-    snap = paths.load_snapshot("cardiologists.csv")
+    snap = paths.load_snapshot("esc.csv")
 
     # Load data from snapshot.
     tb = snap.read_csv()
