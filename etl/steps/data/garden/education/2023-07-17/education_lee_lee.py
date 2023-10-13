dd410325183a9137e06cb28c17b067773dabbcc0
Marigold <mojmir.vinkler@gmail.com>
2023-10-13 10:44:29 +0200

    :bug: remove breakpoint

--- etl/steps/data/garden/education/2023-07-17/education_lee_lee.py
+++ etl/steps/data/garden/education/2023-07-17/education_lee_lee.py
@@ -125,8 +125,6 @@ def run(dest_dir: str) -> None:
     tb = Table(df_merged_wb, short_name=paths.short_name, underscore=True)
     tb.set_index(["country", "year"], inplace=True)
 
-    __import__("ipdb").set_trace()
-
     #
     # Save outputs
     #
