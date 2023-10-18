baec4be211da760fdcc28efc941dff144ffd39a4
Pablo Rosado <pabloarosado@gmail.com>
2023-10-18 10:10:12 +0200

    Add snapshot

--- snapshots/met_office_hadley_centre/2023-10-18/near_surface_temperature.py
+++ snapshots/met_office_hadley_centre/2023-10-18/near_surface_temperature.py
@@ -4,18 +4,25 @@ The HadCRUT5 near surface temperature data set is produced by blending data from
 dataset and the HadSST4 sea-surface temperature dataset.
 """
 
-import pathlib
+from pathlib import Path
 
 import click
 
 from etl.snapshot import Snapshot
 
-SNAPSHOT_VERSION = pathlib.Path(__file__).parent.name
-FILE_NAMES = [
+# Version for current snapshot dataset.
+SNAPSHOT_VERSION = Path(__file__).parent.name
+
+# Names of input data files to create snapshots for.
+DATA_FILES = [
     "near_surface_temperature_global.csv",
     "near_surface_temperature_northern_hemisphere.csv",
     "near_surface_temperature_southern_hemisphere.csv",
 ]
+
+# Define common metadata fields (to be written to dvc files).
+CITATION_FULL = """Morice, C.P., J.J. Kennedy, N.A. Rayner, J.P. Winn, E. Hogan, R.E. Killick, R.J.H. Dunn, T.J. Osborn, P.D. Jones and I.R. Simpson (in press) An updated assessment of near-surface temperature change from 1850: the HadCRUT5 dataset. Journal of Geophysical Research (Atmospheres) doi:10.1029/2019JD032361 (supporting information)."""
+DESCRIPTION = """The HadCRUT5 near surface temperature data set is produced by blending data from the CRUTEM5 surface air temperature dataset and the HadSST4 sea-surface temperature dataset.\n\nTemperature anomalies are based on the HadCRUT5 near-surface temperature dataset as published by the Met Office Hadley Centre. Temperature anomalies are given in degrees celsius relative to the average temperature over the period 1961-1990.\n\nThese are available for the Northern Hemisphere and the Southern Hemisphere. The global mean has been calculated by averaging anomalies for northern and southern hemispheres."""
 
 
 @click.command()
@@ -26,10 +33,19 @@ FILE_NAMES = [
     help="Upload dataset to Snapshot",
 )
 def main(upload: bool) -> None:
-    for file_name in FILE_NAMES:
-        snap = Snapshot(f"met_office_hadley_centre/{SNAPSHOT_VERSION}/{file_name}")
-        snap.download_from_source()
-        snap.dvc_add(upload=upload)
+    # Create a new snapshot for each dataset.
+    for data_file in DATA_FILES:
+        snap = Snapshot(f"met_office_hadley_centre/{SNAPSHOT_VERSION}/{data_file}")
+
+        # Replace the full citation and description in the metadata.
+        snap.metadata.origin.citation_full = CITATION_FULL  # type: ignore
+        snap.metadata.origin.description = DESCRIPTION  # type: ignore
+
+        # Rewrite metadata to dvc file.
+        snap.metadata_path.write_text(snap.metadata.to_yaml())
+
+        # Download data from source, add file to DVC and upload to S3.
+        snap.create_snapshot(upload=upload)
 
 
 if __name__ == "__main__":
