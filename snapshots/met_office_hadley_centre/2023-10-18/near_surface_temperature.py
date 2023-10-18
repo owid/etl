e37bb5173d48658a8faf040e7fdd2513382df432
Pablo Rosado <pabloarosado@gmail.com>
2023-10-18 16:57:37 +0200

    Add links to papers, as suggested by Fiona

--- snapshots/met_office_hadley_centre/2023-10-18/near_surface_temperature.py
+++ snapshots/met_office_hadley_centre/2023-10-18/near_surface_temperature.py
@@ -21,7 +21,7 @@ DATA_FILES = [
 ]
 
 # Define common metadata fields (to be written to dvc files).
-CITATION_FULL = """Morice, C.P., J.J. Kennedy, N.A. Rayner, J.P. Winn, E. Hogan, R.E. Killick, R.J.H. Dunn, T.J. Osborn, P.D. Jones and I.R. Simpson (in press) An updated assessment of near-surface temperature change from 1850: the HadCRUT5 dataset. Journal of Geophysical Research (Atmospheres) doi:10.1029/2019JD032361 (supporting information)."""
+CITATION_FULL = """Morice, C.P., J.J. Kennedy, N.A. Rayner, J.P. Winn, E. Hogan, R.E. Killick, R.J.H. Dunn, T.J. Osborn, P.D. Jones and I.R. Simpson (in press) An updated assessment of near-surface temperature change from 1850: the HadCRUT5 dataset. Journal of Geophysical Research (Atmospheres) [doi:10.1029/2019JD032361](https://www.metoffice.gov.uk/hadobs/hadcrut5/HadCRUT5_accepted.pdf) ([supporting information](https://www.metoffice.gov.uk/hadobs/hadcrut5/HadCRUT5_supporting_information_accepted.pdf))."""
 DESCRIPTION = """The HadCRUT5 near surface temperature data set is produced by blending data from the CRUTEM5 surface air temperature dataset and the HadSST4 sea-surface temperature dataset.\n\nTemperature anomalies are based on the HadCRUT5 near-surface temperature dataset as published by the Met Office Hadley Centre."""
 
 
