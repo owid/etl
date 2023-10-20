b83e9530726cda289205e6be0246b0a63c82d8bf
spoonerf <spoonerfiona@gmail.com>
2023-10-20 16:38:35 +0100

    opening shp file

---
+++ etl/steps/data/meadow/papers/2023-10-20/anthromes_input.py
@@ -0,0 +1,60 @@
+"""Load a snapshot and create a meadow dataset."""
+
+import os
+import tempfile
+import zipfile
+
+import geopandas as gpd
+from owid.catalog import Table
+
+from etl.helpers import PathFinder, create_dataset
+from etl.snapshot import Snapshot
+
+# Get paths and naming conventions for current step.
+paths = PathFinder(__file__)
+
+
+def run(dest_dir: str) -> None:
+    #
+    # Load inputs.
+    #
+    # Retrieve snapshot.
+    snap = paths.load_snapshot("anthromes_input.zip")
+    # Get the table from the shapefile - metadata currently gets lost along the way
+    tb = get_table_from_shp_file(snap)
+
+    # Process data.
+    #
+    # Ensure all columns are snake-case, set an appropriate index, and sort conveniently.
+    tb = tb.underscore().set_index(["regn_nm"], verify_integrity=True).sort_index()
+
+    #
+    # Save outputs.
+    #
+    # Create a new meadow dataset with the same metadata as the snapshot.
+    ds_meadow = create_dataset(dest_dir, tables=[tb], check_variables_metadata=True, default_metadata=snap.metadata)
+
+    # Save changes in the new meadow dataset.
+    ds_meadow.save()
+
+
+def get_table_from_shp_file(snap: Snapshot):
+    with tempfile.TemporaryDirectory() as temp_dir:
+        with zipfile.ZipFile(snap.path, "r") as zip_ref:
+            zip_ref.extractall(temp_dir)
+
+        # List all files in the temporary directory
+        extracted_dir = os.listdir(temp_dir)
+        extracted_files = os.listdir(os.path.join(temp_dir, extracted_dir[0]))
+
+        # Keep only the DBF file (assuming it has a .dbf extension)
+        shp_file = None
+        for file_name in extracted_files:
+            if file_name.endswith(".shp"):
+                shp_file = os.path.join(temp_dir, extracted_dir[0], file_name)
+        gdf = gpd.read_file(shp_file)
+        gdf = gdf.drop(columns=["geometry"])
+
+        gdf = Table(gdf)
+        gdf.metadata = snap.to_table_metadata()
+        return gdf
