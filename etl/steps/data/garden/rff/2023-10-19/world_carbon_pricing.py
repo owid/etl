6197dc10afbea1281d3a2985703207027a2e324b
Pablo Rosado <pabloarosado@gmail.com>
2023-10-20 11:47:35 +0200

    Improve all steps code and metadata

--- etl/steps/data/garden/rff/2023-10-19/world_carbon_pricing.py
+++ etl/steps/data/garden/rff/2023-10-19/world_carbon_pricing.py
@@ -1,25 +1,23 @@
+"""Create a garden dataset from the meadow dataset."""
 from typing import Dict, List, cast
 
-import pandas as pd
-from owid.catalog import Dataset, Table
-from owid.catalog.utils import underscore_table
+from owid.catalog import Table, Variable
 from owid.datautils import dataframes, io
-from shared import CURRENT_DIR, LAST_INFORMED_YEAR, MEADOW_VERSION, VERSION
+from structlog import get_logger
 
 from etl.data_helpers import geo
-from etl.helpers import PathFinder
-from etl.paths import DATA_DIR, STEP_DIR
+from etl.helpers import PathFinder, create_dataset
 
-# Details of the input dataset.
-MEADOW_DATASET_NAME = "world_carbon_pricing"
-MEADOW_MAIN_DATASET_PATH = DATA_DIR / f"meadow/rff/{MEADOW_VERSION}/{MEADOW_DATASET_NAME}"
-MEADOW_SUBNATIONAL_DATASET_PATH = DATA_DIR / f"meadow/rff/{MEADOW_VERSION}/world_carbon_pricing__subnational"
-# Details of the output tables.
-GARDEN_MAIN_TABLE_NAME = MEADOW_DATASET_NAME
-GARDEN_VERSION = VERSION
-GARDEN_ANY_SECTOR_TABLE_NAME = "world_carbon_pricing_any_sector"
-# Get naming convention.
-N = PathFinder(str(CURRENT_DIR / MEADOW_DATASET_NAME))
+# Initialize logger.
+log = get_logger()
+
+# Get paths and naming conventions for current step.
+paths = PathFinder(__file__)
+
+# It may happen that the data for the most recent year is incomplete.
+# If so, define the following to be last year fully informed.
+# LAST_INFORMED_YEAR = 2021
+LAST_INFORMED_YEAR = None
 
 # Labels for the variables showing whether any sector is covered by an ETS or a carbon tax at the national or only
 # sub-national level.
@@ -29,7 +27,7 @@ LABEL_ETS_COVERED_ONLY_SUBNATIONAL = "Has an ETS only at a sub-national level"
 LABEL_TAX_NOT_COVERED = "No carbon tax"
 LABEL_TAX_COVERED = "Has a carbon tax"
 LABEL_TAX_COVERED_ONLY_SUBNATIONAL = "Has a carbon tax only at a sub-national level"
-# If a country-years has both national and subnational coverage, mention only the national and ignore subnational.
+# If a country-years has both national and sub-national coverage, mention only the national and ignore subnational.
 LABEL_ETS_COVERED_NATIONAL_AND_SUBNATIONAL = "Has an ETS"
 LABEL_TAX_COVERED_NATIONAL_AND_SUBNATIONAL = "Has a carbon tax"
 
@@ -47,28 +45,23 @@ COLUMNS = {
     "ets_price": "ets_price",
 }
 
-# Columns to use as index in main table.
-INDEX_COLUMNS = ["country", "year", "ipcc_code", "product"]
-# Columns to use as index in table simplified to show whether there is coverage for any sector.
-INDEX_COLUMNS_ANY_SECTOR = ["country", "year"]
-
 # Mapping of countries and the regions of the country included in the sub-national dataset.
 # In the future, it would be good to load this mapping as additional data (however, the mapping is hardcoded in the
 # original repository, so it's not trivial to get this mapping automatically).
-COUNTRY_MEMBERS_FILE = STEP_DIR / f"data/garden/rff/{GARDEN_VERSION}/sub_national_jurisdictions.json"
+COUNTRY_MEMBERS_FILE = paths.directory / "world_carbon_pricing.country_members.json"
 
 
-def sanity_checks(df: pd.DataFrame) -> None:
+def sanity_checks(tb: Table) -> None:
     """Sanity checks on the raw data.
 
     Parameters
     ----------
-    df : pd.DataFrame
+    tb : Table
         Raw data from meadow.
 
     """
     column_checks = (
-        df.groupby("jurisdiction")
+        tb.groupby("jurisdiction")
         .agg(
             {
                 # Columns 'tax' and 'ets' must contain only 0 and/or 1.
@@ -88,105 +81,112 @@ def sanity_checks(df: pd.DataFrame) -> None:
     error = f"Unexpected content in columns {column_checks[~column_checks].index.tolist()}."
     assert column_checks.all(), error
 
+    # If the last year in the data is the current year, or if the data for the last year is missing, raise a warning.
+    if (
+        tb["year"].max() == int(paths.version.split("-")[0])
+        or tb[["year", "ets_price"]].groupby(["year"], observed=True).sum(min_count=1)["ets_price"].isnull().iloc[-1]
+    ):
+        log.warning("The last year in the data may be incomplete. Define LAST_INFORMED_YEAR.")
 
-def prepare_data(df: pd.DataFrame) -> pd.DataFrame:
+
+def prepare_data(tb_national: Table) -> Table:
     """Prepare data.
 
     Parameters
     ----------
-    df : pd.DataFrame
+    tb_national : Table
         Raw data.
 
     Returns
     -------
-    df : pd.DataFrame
+    tb_national : Table
         Clean data.
 
     """
-    df = df.copy()
+    tb_national = tb_national.copy()
 
     # Select and rename columns.
-    df = df[list(COLUMNS)].rename(columns=COLUMNS, errors="raise")
+    tb_national = tb_national[list(COLUMNS)].rename(columns=COLUMNS, errors="raise")
 
     # Column 'product' has many nans. Convert them into empty strings.
-    df["product"] = df["product"].cat.add_categories("").fillna("")
+    tb_national["product"] = Variable(tb_national["product"].cat.add_categories("").fillna("")).copy_metadata(
+        tb_national["product"]
+    )
 
-    # Given that the most recent data is incomplete, keep only data points prior to (or at) a certain year
-    # (given by global variable LAST_INFORMED_YEAR).
-    df = df[df["year"] <= LAST_INFORMED_YEAR].reset_index(drop=True)
+    if LAST_INFORMED_YEAR is not None:
+        # Keep only data points prior to (or at) a certain year.
+        tb_national = tb_national[tb_national["year"] <= LAST_INFORMED_YEAR].reset_index(drop=True)
 
-    return df
+    return tb_national
 
 
-def get_coverage_for_any_sector(df: pd.DataFrame) -> pd.DataFrame:
-    """Create a dataframe showing whether a country has any sector covered by an ets/carbon tax.
+def get_coverage_for_any_sector(tb: Table) -> Table:
+    """Create a table showing whether a country has any sector covered by an ets/carbon tax.
 
     Parameters
     ----------
-    df : pd.DataFrame
+    tb : Table
         Original national or sub-national data, disaggregated by sector.
 
     Returns
     -------
-    df_any_sector : pd.DataFrame
+    tb_any_sector : tb
         Coverage for any sector.
 
     """
-    # Create a simplified dataframe that gives, for each country and year, whether the country has any sector(-fuel)
+    # Create a simplified table that gives, for each country and year, whether the country has any sector(-fuel)
     # that is covered by at least one tax instrument. And idem for ets.
-    df_any_sector = (
-        df.reset_index()
+    tb_any_sector = (
+        tb.reset_index()
         .groupby(["country", "year"], observed=True)
         .agg({"ets": lambda x: min(x.sum(), 1), "tax": lambda x: min(x.sum(), 1)})
         .astype(int)
         .reset_index()
     )
 
-    return df_any_sector
+    return tb_any_sector
 
 
-def prepare_subnational_data(df_subnational: pd.DataFrame, country_members: Dict[str, List[str]]) -> pd.DataFrame:
-    """Create a dataframe showing whether a country has any sub-national jurisdiction for which any sector is covered by
+def prepare_subnational_data(tb_subnational: Table, country_members: Dict[str, List[str]]) -> Table:
+    """Create a table showing whether a country has any sub-national jurisdiction for which any sector is covered by
     an ets/carbon tax.
 
-    The 'country' column of this dataframe does not need to be harmonized, since we are mapping the original
+    The 'country' column of this table does not need to be harmonized, since we are mapping the original
     sub-national jurisdiction names to the harmonized name of the country.
 
     Parameters
     ----------
-    df_subnational : pd.DataFrame
+    tb_subnational : Table
         Sub-national data, disaggregated by sector.
 
     Returns
     -------
-    pd.DataFrame
+    tb_subnational : Table
         Processed sub-national data.
 
     """
     # Prepare subnational data.
-    df_subnational = prepare_data(df_subnational)
+    tb_subnational = prepare_data(tb_subnational)
     # Map subnational regions to their corresponding country.
     subregions_to_country = {
         subregion: country for country in list(country_members) for subregion in country_members[country]
     }
-    df_subnational["country"] = dataframes.map_series(
-        series=df_subnational["country"],
+    tb_subnational["country"] = dataframes.map_series(
+        series=tb_subnational["country"],
         mapping=subregions_to_country,
         warn_on_missing_mappings=True,
         warn_on_unused_mappings=True,
     )
     # Get coverage of "any sector", where we only care about having at least one sector covered by carbon tax/ets.
-    df_subnational = get_coverage_for_any_sector(df=df_subnational)
+    tb_subnational = get_coverage_for_any_sector(tb=tb_subnational)
 
-    return df_subnational
+    return tb_subnational
 
 
-def combine_national_and_subnational_data(
-    df_any_sector_national: pd.DataFrame, df_any_sector_subnational: pd.DataFrame
-) -> pd.DataFrame:
+def combine_national_and_subnational_data(tb_any_sector_national: Table, tb_any_sector_subnational: Table) -> Table:
     """Combine national and sub-national data on whether countries have any sector covered by a tax instrument.
 
-    The returned dataframe will have the following labels:
+    The returned table will have the following labels:
     * Whether a country-year has no sector covered.
     * Whether a country-year has at least one sector covered at the national level.
     * Whether a country-year has at least one sector in one sub-national jurisdiction covered, but no sector covered at
@@ -196,25 +196,21 @@ def combine_national_and_subnational_data(
 
     Parameters
     ----------
-    df_any_sector_national : pd.DataFrame
+    tb_any_sector_national : Table
         National data on whether countries have any sector covered by a tax instrument.
-    df_any_sector_subnational : pd.DataFrame
+    tb_any_sector_subnational : Table
         Sub-national data on whether countries have any sector covered by a tax instrument.
 
     Returns
     -------
-    df_any_sector : pd.DataFrame
-        Combined dataframe showing whether a country has at least one sector covered by a tax instrument at a national
+    tb_any_sector : Table
+        Combined table showing whether a country has at least one sector covered by a tax instrument at a national
         level, or only at the sub-national level, or not at all.
 
     """
     # Combine national and subnational data.
-    df_any_sector = pd.merge(
-        df_any_sector_national,
-        df_any_sector_subnational,
-        on=["country", "year"],
-        how="left",
-        suffixes=("_national", "_subnational"),
+    tb_any_sector = tb_any_sector_national.merge(
+        tb_any_sector_subnational, on=["country", "year"], how="left", suffixes=("_national", "_subnational")
     ).fillna(0)
 
     # Create two new columns ets and tax, that are:
@@ -222,10 +218,10 @@ def combine_national_and_subnational_data(
     # * 1 if there is a national ets/tax and not a subnational ets/tax.
     # * 2 if there is a subnational ets/tax and not a national ets/tax.
     # * 3 if there are both a national and a subnational ets/tax.
-    df_any_sector = df_any_sector.assign(
+    tb_any_sector = tb_any_sector.assign(
         **{
-            "ets": df_any_sector["ets_national"] + 2 * df_any_sector["ets_subnational"],
-            "tax": df_any_sector["tax_national"] + 2 * df_any_sector["tax_subnational"],
+            "ets": tb_any_sector["ets_national"] + 2 * tb_any_sector["ets_subnational"],
+            "tax": tb_any_sector["tax_national"] + 2 * tb_any_sector["tax_subnational"],
         }
     )[["country", "year", "ets", "tax"]]
 
@@ -242,33 +238,20 @@ def combine_national_and_subnational_data(
         2: LABEL_TAX_COVERED_ONLY_SUBNATIONAL,
         3: LABEL_TAX_COVERED_NATIONAL_AND_SUBNATIONAL,
     }
-    df_any_sector["ets"] = dataframes.map_series(series=df_any_sector["ets"], mapping=ets_mapping)
-    df_any_sector["tax"] = dataframes.map_series(series=df_any_sector["tax"], mapping=tax_mapping)
+    tb_any_sector["ets"] = dataframes.map_series(series=tb_any_sector["ets"], mapping=ets_mapping)
+    tb_any_sector["tax"] = dataframes.map_series(series=tb_any_sector["tax"], mapping=tax_mapping)
 
-    return cast(pd.DataFrame, df_any_sector)
+    return cast(Table, tb_any_sector)
 
 
 def run(dest_dir: str) -> None:
     #
     # Load data.
     #
-    # Read main dataset from meadow.
-    ds_meadow = Dataset(MEADOW_MAIN_DATASET_PATH)
-
-    # Read subnational dataset from meadow.
-    ds_meadow_subnational = Dataset(MEADOW_SUBNATIONAL_DATASET_PATH)
-
-    # Get main table from dataset.
-    tb_meadow = ds_meadow[ds_meadow.table_names[0]]
-
-    # Get table for subnational data from dataset.
-    tb_meadow_subnational = ds_meadow_subnational[ds_meadow_subnational.table_names[0]]
-
-    # Construct a dataframe from the main table.
-    df = pd.DataFrame(tb_meadow)
-
-    # Construct a dataframe for subnational data.
-    df_subnational = pd.DataFrame(tb_meadow_subnational)
+    # Load main dataset from meadow, and read its main tables.
+    ds_meadow = paths.load_dataset("world_carbon_pricing")
+    tb_national = ds_meadow["world_carbon_pricing_national_level"].reset_index()
+    tb_subnational = ds_meadow["world_carbon_pricing_subnational_level"].reset_index()
 
     # Load dictionary mapping sub-national jurisdictions to their countries.
     country_members = io.load_json(COUNTRY_MEMBERS_FILE)
@@ -277,56 +260,42 @@ def run(dest_dir: str) -> None:
     # Process data.
     #
     # Sanity checks on raw data.
-    sanity_checks(df=df)
-    sanity_checks(df=df_subnational)
+    sanity_checks(tb=tb_national)
+    sanity_checks(tb=tb_subnational)
 
     # Prepare data.
-    df = prepare_data(df=df)
+    tb = prepare_data(tb_national=tb_national)
 
     # Harmonize country names.
-    df = geo.harmonize_countries(df=df, countries_file=str(N.country_mapping_path), warn_on_unused_countries=False)
+    tb = geo.harmonize_countries(df=tb, countries_file=paths.country_mapping_path)
 
     # Create a simplified table for "any sector" of national data.
-    df_any_sector_national = get_coverage_for_any_sector(df=df)
+    tb_any_sector_national = get_coverage_for_any_sector(tb=tb)
 
-    # Create a simplified dataframe with the coverage for "any sector" of subnational data.
-    df_any_sector_subnational = prepare_subnational_data(df_subnational=df_subnational, country_members=country_members)
+    # Create a simplified table with the coverage for "any sector" of subnational data.
+    tb_any_sector_subnational = prepare_subnational_data(tb_subnational=tb_subnational, country_members=country_members)
 
     # Combine national and subnational data.
-    df_any_sector = combine_national_and_subnational_data(
-        df_any_sector_national=df_any_sector_national, df_any_sector_subnational=df_any_sector_subnational
+    tb_any_sector = combine_national_and_subnational_data(
+        tb_any_sector_national=tb_any_sector_national, tb_any_sector_subnational=tb_any_sector_subnational
     )
 
-    # Prepare output tables.
-    tb = underscore_table(Table(df)).set_index(INDEX_COLUMNS, verify_integrity=True).sort_index().sort_index(axis=1)
-    tb_any_sector = (
-        underscore_table(Table(df_any_sector))
-        .set_index(INDEX_COLUMNS_ANY_SECTOR, verify_integrity=True)
-        .sort_index()
-        .sort_index(axis=1)
-    )
+    # Rename tables.
+    tb.metadata.short_name = "world_carbon_pricing"
+    tb_any_sector.metadata.short_name = "world_carbon_pricing_any_sector"
+
+    # Set an appropriate index and sort conveniently.
+    tb = tb.set_index(["country", "year", "ipcc_code", "product"], verify_integrity=True).sort_index().sort_index(axis=1)
+    tb_any_sector = tb_any_sector.set_index(["country", "year"], verify_integrity=True).sort_index().sort_index(axis=1)
 
     #
     # Save outputs.
     #
     # Create a new garden dataset.
-    ds_garden = Dataset.create_empty(dest_dir)
-
-    # Fetch metadata from meadow step (if any).
-    ds_garden.metadata = ds_meadow.metadata
-
-    # Update dataset metadata using metadata yaml file.
-    ds_garden.metadata.update_from_yaml(N.metadata_path, if_source_exists="replace")
-
-    # Update main table metadata using metadata yaml file.
-    tb.update_metadata_from_yaml(N.metadata_path, GARDEN_MAIN_TABLE_NAME)
-
-    # Update simplified table metadata using metadata yaml file.
-    tb_any_sector.update_metadata_from_yaml(N.metadata_path, GARDEN_ANY_SECTOR_TABLE_NAME)
-
-    # Add tables to dataset.
-    ds_garden.add(tb)
-    ds_garden.add(tb_any_sector)
-
-    # Save dataset.
+    ds_garden = create_dataset(
+        dest_dir=dest_dir,
+        tables=[tb, tb_any_sector],
+        default_metadata=ds_meadow.metadata,
+        check_variables_metadata=True,
+    )
     ds_garden.save()
--- etl/steps/data/meadow/rff/2023-10-19/world_carbon_pricing.py
+++ etl/steps/data/meadow/rff/2023-10-19/world_carbon_pricing.py
@@ -1,4 +1,9 @@
-"""Load snapshot and create a meadow dataset"""
+"""Load snapshot and create a meadow dataset.
+
+The IPCC codes file used to be given in the World Carbon Pricing (WCP) repos, but that is no longer the case.
+For that reason, we load those codes from the Emissions-Weighted Carbon Price (ECP) repos.
+
+"""
 
 import zipfile
 
@@ -8,6 +13,12 @@ from etl.helpers import PathFinder, create_dataset
 
 # Get paths and naming conventions for current step.
 paths = PathFinder(__file__)
+
+# Common path within the WCP compressed folder to the national/subnational data files.
+DATA_PATH = "WorldCarbonPricingDatabase-master/_dataset/data/CO2/"
+
+# Path within the ECP compressed folder to the IPCC codes file.
+IPCC_CODES_PATH = "ECP-master/_raw/_aux_files/ipcc2006_iea_category_codes.csv"
 
 # Columns to select and rename in ipcc column names.
 IPCC_COLUMNS = {
@@ -23,85 +34,66 @@ def run(dest_dir: str) -> None:
     # Load snapshot.
     snap = paths.load_snapshot("world_carbon_pricing.zip")
 
+    # Just to extract the IPCC codes, load the ECP snapshot.
+    snap_ecp = paths.load_snapshot("emissions_weighted_carbon_price.zip")
+
     # Read necessary data from compressed folder.
     with zipfile.ZipFile(snap.path) as z:
-        # Read data files corresponding to national data (within the zipped folder).
-        national_files = sorted(
-            [
-                path
-                for path in z.namelist()
-                if path.startswith("WorldCarbonPricingDatabase-master/_dataset/data/CO2/national/")
-                if path.endswith(".csv")
-            ]
-        )
-        tb_national = pr.concat(
-            [
-                pr.read_csv(
-                    z.open(file_name), metadata=snap.to_table_metadata(), origin=snap.metadata.origin, underscore=True
-                )
-                for file_name in national_files
-            ],
-            ignore_index=True,
-            short_name="carbon_pricing_at_national_level",
-        )
+        # Extract metadata and origin from snapshot.
+        metadata = snap.to_table_metadata()
+        origin = snap.metadata.origin
 
-        # Read data files corresponding to sub-national data (within the zipped folder).
-        subnational_files = sorted(
-            [
-                path
-                for path in z.namelist()
-                if path.startswith("WorldCarbonPricingDatabase-master/_dataset/data/CO2/subnational/")
-                if path.endswith(".csv")
-            ]
-        )
+        # Read data files corresponding to national data (within the zipped folder), and create a table with them.
+        data_national = [
+            pr.read_csv(z.open(path), metadata=metadata, origin=origin, underscore=True)
+            for path in sorted(z.namelist())
+            if path.startswith(f"{DATA_PATH}national/") and path.endswith(".csv")
+        ]
+        tb_national = pr.concat(data_national, ignore_index=True, short_name="world_carbon_pricing_national_level")
+
+        # Read data files corresponding to sub-national data (within the zipped folder) and create a table with them.
+        data_subnational = [
+            pr.read_csv(z.open(path), metadata=metadata, origin=origin, underscore=True)
+            for path in sorted(z.namelist())
+            if path.startswith(f"{DATA_PATH}subnational/") and path.endswith(".csv")
+        ]
         tb_subnational = pr.concat(
-            [
-                pr.read_csv(
-                    z.open(file_name), metadata=snap.to_table_metadata(), origin=snap.metadata.origin, underscore=True
-                )
-                for file_name in subnational_files
-            ],
-            ignore_index=True,
-            short_name="carbon_pricing_at_subnational_level",
+            data_subnational, ignore_index=True, short_name="world_carbon_pricing_subnational_level"
         )
 
-        # TODO: It seems that the IPCC file is no longer where it used to.
-        #  Instead, I think we can take it from the ECP snapshot:
-        #  https://github.com/g-dolphin/ECP/blob/master/_raw/_aux_files/ipcc2006_iea_category_codes.csv
-        # Therefore, consider if this step should load both snapshots and create all tables in the same step.
-
-        # # Read IPCC codes from file.
-        # ipcc_codes_file = [path for path in z.namelist() if path == "WorldCarbonPricingDatabase-master/_raw/_aux_files/misc/ipcc2006_category_codes_links.csv"][0]
-        # tb_ipcc_codes = pr.read_csv(z.open(ipcc_codes_file), metadata=snap.to_table_metadata(), origin=snap.metadata.origin, dtype=object)
-        # tb_ipcc_codes.metadata.short_name="ipcc_codes"
+    # Read IPCC codes from the ECP snapshot (since, for some reason, it is no longer available in the WCP repos).
+    # NOTE: To avoid mixing metadata and origins from ECP and WCP, use the origins from WCP.
+    with zipfile.ZipFile(snap_ecp.path) as z:
+        tb_ipcc_codes = pr.read_csv(z.open(IPCC_CODES_PATH), metadata=metadata, origin=origin, dtype=object)
+        tb_ipcc_codes.metadata.short_name = "ipcc_codes"
 
     #
     # Process data.
     #
-    # Prepare IPCC codes dataframe.
-    # tb_ipcc_codes = tb_ipcc_codes[list(IPCC_COLUMNS)].rename(columns=IPCC_COLUMNS, errors="raise")
-    # # Sanity check.
-    # error = "IPCC codes found in data that are missing in IPCC codes file."
-    # assert set(df["ipcc_code"]) <= set(ipcc_codes["ipcc_code"]), error
-    # # Add sector names to data, mapping IPCC codes.
-    # tb_national = tb_national.merge(ipcc_codes, on="ipcc_code", how="left")
-    # tb_subnational = tb_subnational.merge(ipcc_codes, on="ipcc_code", how="left")
+    # Prepare IPCC codes.
+    tb_ipcc_codes = tb_ipcc_codes[list(IPCC_COLUMNS)].rename(columns=IPCC_COLUMNS, errors="raise")
+
+    # Sanity check.
+    error = "IPCC codes found in data that are missing in IPCC codes file."
+    assert set(tb_ipcc_codes["ipcc_code"]) <= set(tb_ipcc_codes["ipcc_code"]), error
+
+    # Add sector names to data, mapping IPCC codes.
+    tb_national = tb_national.merge(tb_ipcc_codes, on="ipcc_code", how="left")
+    tb_subnational = tb_subnational.merge(tb_ipcc_codes, on="ipcc_code", how="left")
 
     # Set an appropriate index and sort conveniently.
-    tb_national = (
-        tb_national.set_index(["jurisdiction", "year", "ipcc_code", "product"], verify_integrity=True)
-        .sort_index()
-        .sort_index(axis=1)
-    )
-    tb_subnational = (
-        tb_subnational.set_index(["jurisdiction", "year", "ipcc_code", "product"], verify_integrity=True)
-        .sort_index()
-        .sort_index(axis=1)
-    )
+    index_columns = ["jurisdiction", "year", "ipcc_code", "product"]
+    tb_national = tb_national.set_index(index_columns, verify_integrity=True).sort_index().sort_index(axis=1)
+    tb_subnational = tb_subnational.set_index(index_columns, verify_integrity=True).sort_index().sort_index(axis=1)
 
     #
     # Save outputs.
     #
     # Create new dataset with metadata from walden.
-    ds_meadow = create_dataset(dest_dir=dest_dir, tables=[tb_national, tb_subnational], check_variables_metadata=True)
+    ds_meadow = create_dataset(
+        dest_dir=dest_dir,
+        tables=[tb_national, tb_subnational],
+        default_metadata=snap.metadata,
+        check_variables_metadata=True,
+    )
     ds_meadow.save()
