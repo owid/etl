a2ebd05a8c337d52ead19d080f7f65e42d8487df
lucasrodes <lucasrodes@users.noreply.github.com>
2023-10-31 21:28:15 +0100

    prio

--- etl/steps/data/garden/war/2023-09-21/prio_v31.py
+++ etl/steps/data/garden/war/2023-09-21/prio_v31.py
@@ -26,11 +26,10 @@ import numpy as np
 import owid.catalog.processing as pr
 import pandas as pd
 from owid.catalog import Table
+from shared import add_indicators_extra
 from structlog import get_logger
 
 from etl.helpers import PathFinder, create_dataset
-
-from .shared import add_indicators_extra
 
 # Get paths and naming conventions for current step.
 paths = PathFinder(__file__)
@@ -61,16 +60,38 @@ def run(dest_dir: str) -> None:
     # Read table from meadow dataset.
     tb = ds_meadow["prio_v31"].reset_index()
 
-    # Read table from COW codes
-    ds_cow_ssm = paths.load_dataset("gleditsch")
-    tb_regions = ds_cow_ssm["gleditsch_regions"].reset_index()
+    # Read table from GW codes
+    ds_gw = paths.load_dataset("gleditsch")
+    tb_regions = ds_gw["gleditsch_regions"].reset_index()
+    tb_gw_countries = ds_gw["gleditsch_countries"]
 
     #
     # Process data.
     #
+    paths.log.info("rename columns")
+    tb = tb.rename(
+        columns={
+            "type": "conflict_type",
+        }
+    )
+
+    # Country-level stuff
+    paths.log.info("getting country-level indicators")
+    tb_country = estimate_metrics_country_level(tb, tb_gw_countries)
+
     # Relevant rows
     paths.log.info("keep relevant columns")
-    COLUMNS_RELEVANT = ["id", "year", "region", "type", "startdate", "ependdate", "bdeadlow", "bdeadhig", "bdeadbes"]
+    COLUMNS_RELEVANT = [
+        "id",
+        "year",
+        "region",
+        "conflict_type",
+        "startdate",
+        "ependdate",
+        "bdeadlow",
+        "bdeadhig",
+        "bdeadbes",
+    ]
     tb = tb[COLUMNS_RELEVANT]
 
     paths.log.info("sanity checks")
@@ -81,13 +102,6 @@ def run(dest_dir: str) -> None:
 
     paths.log.info("estimate metrics")
     tb = estimate_metrics(tb)
-
-    paths.log.info("rename columns")
-    tb = tb.rename(
-        columns={
-            "type": "conflict_type",
-        }
-    )
 
     paths.log.info("replace NaNs with zeroes")
     tb = replace_missing_data_with_zeros(tb)
@@ -118,20 +132,28 @@ def run(dest_dir: str) -> None:
     paths.log.info("rename regions")
     tb["conflict_type"] = tb["conflict_type"].map(CONFTYPES_RENAME | {"all": "all", "intrastate": "intrastate"})
     assert tb["conflict_type"].isna().sum() == 0, "Unmapped conflict_type!"
+    tb_country["conflict_type"] = tb_country["conflict_type"].map(CONFTYPES_RENAME)
+    assert tb_country["conflict_type"].isna().sum() == 0, "Unmapped conflict_type!"
 
     # sanity check: summing number of ongoing and new conflicts of all types is equivalent to conflict_type="all"
     paths.log.info("sanity checking number of conflicts")
     _sanity_check_final(tb)
 
+    # Set index
     paths.log.info("set index")
     tb = tb.set_index(["year", "region", "conflict_type"], verify_integrity=True)
+    tb_country = tb_country.set_index(["year", "country", "conflict_type"], verify_integrity=True).sort_index()
 
     #
     # Save outputs.
     #
+    tables = [
+        tb,
+        tb_country,
+    ]
     # Create a new garden dataset with the same metadata as the meadow dataset.
     ds_garden = create_dataset(
-        dest_dir, tables=[tb], check_variables_metadata=True, default_metadata=ds_meadow.metadata
+        dest_dir, tables=tables, check_variables_metadata=True, default_metadata=ds_meadow.metadata
     )
 
     # Save changes in the new garden dataset.
@@ -161,7 +183,7 @@ def _sanity_checks(tb: Table) -> None:
         tb.groupby("id")["region"].nunique() == 1
     ).all(), "Some conflicts occurs in multiple regions! That was not expected."
     assert (
-        tb.groupby(["id", "year"])["type"].nunique() == 1
+        tb.groupby(["id", "year"])["conflict_type"].nunique() == 1
     ).all(), "Some conflicts has different values for `type` in the same year! That was not expected."
 
 
@@ -170,7 +192,7 @@ def estimate_metrics(tb: Table) -> Table:
     tb_new = _add_new_metrics(tb)
 
     # Combine
-    tb = tb_ongoing.merge(tb_new, on=["year", "region", "type"], how="outer")
+    tb = tb_ongoing.merge(tb_new, on=["year", "region", "conflict_type"], how="outer")
 
     return tb
 
@@ -184,25 +206,25 @@ def _add_ongoing_metrics(tb: Table) -> Table:
 
     ops = {"id": "nunique", "bdeadlow": sum_nan, "bdeadbes": sum_nan, "bdeadhig": sum_nan}
     ## By region and type
-    tb_ongoing = tb.groupby(["year", "type", "region"], as_index=False).agg(ops)
+    tb_ongoing = tb.groupby(["year", "conflict_type", "region"], as_index=False).agg(ops)
     ## Type='all'
     tb_ongoing_alltype = tb.groupby(["year", "region"], as_index=False).agg(ops)
-    tb_ongoing_alltype["type"] = "all"
+    tb_ongoing_alltype["conflict_type"] = "all"
     ## Type='intrastate'
-    tb_intra_ = tb[tb["type"].isin([3, 4])].copy()
+    tb_intra_ = tb[tb["conflict_type"].isin([3, 4])].copy()
     tb_ongoing_intratype = tb_intra_.groupby(["year", "region"], as_index=False).agg(ops)
-    tb_ongoing_intratype["type"] = "intrastate"
+    tb_ongoing_intratype["conflict_type"] = "intrastate"
     ## World (by type)
-    tb_ongoing_world = tb.groupby(["year", "type"], as_index=False).agg(ops)
+    tb_ongoing_world = tb.groupby(["year", "conflict_type"], as_index=False).agg(ops)
     tb_ongoing_world["region"] = "World"
     ## World (type 'all')
     tb_ongoing_world_alltype = tb.groupby(["year"], as_index=False).agg(ops)
     tb_ongoing_world_alltype["region"] = "World"
-    tb_ongoing_world_alltype["type"] = "all"
+    tb_ongoing_world_alltype["conflict_type"] = "all"
     ## World (type 'intrastate')
     tb_ongoing_world_intratype = tb_intra_.groupby(["year"], as_index=False).agg(ops)
     tb_ongoing_world_intratype["region"] = "World"
-    tb_ongoing_world_intratype["type"] = "intrastate"
+    tb_ongoing_world_intratype["conflict_type"] = "intrastate"
 
     ## Combine
     tb_ongoing = pr.concat(
@@ -216,7 +238,7 @@ def _add_ongoing_metrics(tb: Table) -> Table:
         ],
         ignore_index=True,
     )
-    tb_ongoing = tb_ongoing.sort_values(["year", "region", "type"])  # type: ignore
+    tb_ongoing = tb_ongoing.sort_values(["year", "region", "conflict_type"])  # type: ignore
 
     ## Rename
     tb_ongoing = tb_ongoing.rename(  # type: ignore
@@ -234,28 +256,28 @@ def _add_ongoing_metrics(tb: Table) -> Table:
 def _add_new_metrics(tb: Table) -> Table:
     # Reduce table
     tb_new = tb.sort_values("year").drop_duplicates(subset=["id", "region"], keep="first")[
-        ["year", "region", "type", "id"]
+        ["year", "region", "conflict_type", "id"]
     ]
     assert (
         tb_new["id"].value_counts().max() == 1
     ), "There are multiple instances of a conflict with the same ID. Maybe same conflict in different regions or with different types? This is assumed not to happen"
 
     # Estimate metric for regions and types
-    tb_new_regions = tb_new.groupby(["year", "region", "type"], as_index=False)["id"].nunique()
+    tb_new_regions = tb_new.groupby(["year", "region", "conflict_type"], as_index=False)["id"].nunique()
 
     # Estimate metric for new type='all'
     tb_new_alltype = tb_new_regions.groupby(["year", "region"], as_index=False)["id"].sum()
-    tb_new_alltype["type"] = "all"
+    tb_new_alltype["conflict_type"] = "all"
 
     # Estimate metric for new type='intrastate'
-    tb_new_regions_intra_ = tb_new_regions[tb_new_regions["type"].isin([3, 4])]
+    tb_new_regions_intra_ = tb_new_regions[tb_new_regions["conflict_type"].isin([3, 4])]
     tb_new_intratype = tb_new_regions_intra_.groupby(["year", "region"], as_index=False)["id"].sum()
-    tb_new_intratype["type"] = "intrastate"
+    tb_new_intratype["conflict_type"] = "intrastate"
 
     tb_new = pr.concat([tb_new_regions, tb_new_alltype, tb_new_intratype], ignore_index=True)
 
     # Estimate metric for new region='World'
-    tb_new_world = tb_new.groupby(["year", "type"], as_index=False)["id"].sum()
+    tb_new_world = tb_new.groupby(["year", "conflict_type"], as_index=False)["id"].sum()
     tb_new_world["region"] = "World"
 
     # Combine
@@ -325,3 +347,97 @@ def _sanity_check_final(tb: Table) -> Table:
     # 2)
     msk = tb["number_deaths_ongoing_conflicts_battle_low"] > tb["number_deaths_ongoing_conflicts_battle_high"]
     assert not msk.any(), f"Low estimates higher than high estimates. This can't be correct! {tb[msk]}"
+
+
+def estimate_metrics_country_level(tb: Table, tb_gw: Table) -> Table:
+    """Add country-level indicators."""
+    ###################
+    # Participated in #
+    ###################
+
+    # Get table with [year, conflict_type, code]
+    tb_country = pr.concat(
+        [
+            tb[["year", "conflict_type", "gwnoa"]].rename(columns={"gwnoa": "code"}),
+            tb[["year", "conflict_type", "gwnob"]].rename(columns={"gwnob": "code"}),
+        ],
+    )
+    # Drop rows with code = NaN
+    tb_country = tb_country.dropna(subset=["code"])
+    tb_country = tb_country[~tb_country["code"].isin(["nan", "-99"])]
+    # Drop duplicates
+    tb_country = tb_country.drop_duplicates()
+
+    # Explode where multiple codes
+    tb_country["code"] = tb_country["code"].astype(str).str.split(",")
+    tb_country = tb_country.explode("code")
+    # Drop duplicates (may appear duplicates after exploding)
+    tb_country = tb_country.drop_duplicates()
+    # Ensure numeric type
+    tb_country["code"] = tb_country["code"].str.strip().astype(int)
+
+    # Sanity check
+    assert not tb_country.isna().any(axis=None), "There are some NaNs!"
+
+    # Add country name
+    tb_country["country"] = tb_country.apply(lambda x: _get_country_name(tb_gw, x["code"], x["year"]), axis=1)
+    assert tb_country["country"].notna().all(), "Some countries were not found! NaN was set"
+
+    # Add flag
+    tb_country["participated_in_conflict"] = 1
+    tb_country["participated_in_conflict"].m.origins = tb["gwnoa"].m.origins
+
+    # Prepare GW table
+    tb_alltypes = Table(pd.DataFrame({"conflict_type": tb_country["conflict_type"].unique()}))
+    tb_gw = tb_gw.reset_index().merge(tb_alltypes, how="cross")
+    tb_gw["country"] = tb_gw["country"].astype(str)
+
+    # Combine all GW entries with UCDP
+    tb_country = tb_gw.merge(tb_country, on=["year", "country", "conflict_type"], how="left")
+    tb_country["participated_in_conflict"] = tb_country["participated_in_conflict"].fillna(0)
+    tb_country = tb_country[["year", "country", "conflict_type", "participated_in_conflict"]]
+
+    # Add intrastate (all)
+    tb_country = add_conflict_country_all_intrastate(tb_country)
+    # Add state-based
+    tb_country = add_conflict_country_all_statebased(tb_country)
+
+    # Only preserve years that make sense
+    # tb_country = tb_country[tb_country["year"] >= tb["year"].min()]
+
+    # Set short name
+    tb_country.metadata.short_name = "prio_v31_country"
+
+    return tb_country
+
+
+def _get_country_name(tb_gw: Table, code: int, year: int) -> str:
+    try:
+        country_name = tb_gw.loc[(code, year)]
+    except KeyError:
+        country_name = tb_gw.loc[(code, year - 1)]
+    return country_name
+
+
+def add_conflict_country_all_intrastate(tb: Table) -> Table:
+    """Add metrics for conflict_type = 'intrastate'."""
+    tb_intra = tb[
+        tb["conflict_type"].isin(["intrastate (non-internationalized)", "intrastate (internationalized)"])
+    ].copy()
+    tb_intra = tb_intra.groupby(["year", "country"], as_index=False).agg(
+        {"participated_in_conflict": lambda x: min(x.sum(), 1)}
+    )
+    tb_intra["conflict_type"] = "intrastate"
+    tb = pr.concat([tb, tb_intra], ignore_index=True)
+    return tb
+
+
+def add_conflict_country_all_statebased(tb: Table) -> Table:
+    """Add metrics for conflict_type = 'state-based'."""
+    tb_state = tb[tb["conflict_type"].isin(CONFTYPES_RENAME.values())].copy()
+    tb_state = tb_state.groupby(["year", "country"], as_index=False).agg(
+        {"participated_in_conflict": lambda x: min(x.sum(), 1)}
+    )
+    tb_state["conflict_type"] = "state-based"
+    tb = pr.concat([tb, tb_state], ignore_index=True)
+    return tb
