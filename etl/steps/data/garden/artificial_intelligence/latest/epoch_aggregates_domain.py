1340cdebc40385da2e782c6dfb074580f6aac442
veronikasamborska1994 <32176660+veronikasamborska1994@users.noreply.github.com>
2023-11-01 12:34:43 +0000

    restructuring to try out a datapages trick

--- etl/steps/data/garden/artificial_intelligence/latest/epoch_aggregates_domain.py
+++ etl/steps/data/garden/artificial_intelligence/latest/epoch_aggregates_domain.py
@@ -31,46 +31,58 @@ def run(dest_dir: str) -> None:
 
     # Pivot the table to get the counts for each domain in a separate column
     df_pivot_domain = domain_counts.pivot(index="year", columns="domain", values="count").reset_index()
-    # Get organization categorisation counts
-    organization_categorization_counts = (
-        tb.groupby(["year", "organization_categorization"]).size().reset_index(name="count")
-    )
-    # Pivot the table to get the counts for each organization type in a separate column
-    df_pivot_org = organization_categorization_counts.pivot(
-        index="year", columns="organization_categorization", values="count"
-    ).reset_index()
-
-    # Get approach counts
-    approach_counts = tb.groupby(["year", "approach"]).size().reset_index(name="count")
-
-    # Pivot the table to get the counts for each approach type in a separate column
-    df_pivot_approach = approach_counts.pivot(index="year", columns="approach", values="count").reset_index()
 
     # Rename columns to avoid confusion with the original columns
     df_pivot_domain = df_pivot_domain.rename(columns={"Not specified": "not_specified_domain", "Other": "other_domain"})
-    df_pivot_org = df_pivot_org.rename(
-        columns={
-            "Not specified": "not_specified_organization_categorization",
-            "Other": "other_organization_categorization",
-        }
+
+    # Melting the dataframe
+    melted_df = df_pivot_domain.melt(
+        id_vars=["year"],
+        value_vars=[
+            "Drawing",
+            "Games",
+            "Language",
+            "Multimodal",
+            "not_specified_domain",
+            "other_domain",
+            "Recommendation",
+            "Speech",
+            "Vision",
+        ],
+        var_name="domain",
+        value_name="yearly_count",
     )
 
-    df_pivot_approach = df_pivot_approach.rename(columns={"Not specified": "not_specified_approach"})
-
-    # Merge the dataframes
-    merged_df = pr.merge(df_pivot_domain, df_pivot_org, on="year").copy_metadata(from_table=tb)
-    merged_df = pr.merge(df_pivot_approach, merged_df, on="year").copy_metadata(from_table=tb)
-    # Create cumulative columns
-    for column in merged_df.columns:
+    # Replace with cumulative columns
+    for column in df_pivot_domain.columns:
         if column not in ["year"]:
-            merged_df[f"{column}_cumsum"] = merged_df[column].cumsum()
+            df_pivot_domain[f"{column}"] = df_pivot_domain[column].cumsum()
+    # Melting the dataframe
+    melted_df_cumulative = df_pivot_domain.melt(
+        id_vars=["year"],
+        value_vars=[
+            "Drawing",
+            "Games",
+            "Language",
+            "Multimodal",
+            "not_specified_domain",
+            "other_domain",
+            "Recommendation",
+            "Speech",
+            "Vision",
+        ],
+        var_name="domain",
+        value_name="cumulative_count",
+    )
 
+    df_merged = pr.merge(melted_df_cumulative, melted_df, on=["year", "domain"]).copy_metadata(from_table=tb)
     # Create table
-    tb_agg = merged_df.underscore().set_index("year", verify_integrity=True)
+    tb_agg = df_merged.underscore().set_index(["year", "domain"], verify_integrity=True)
 
     # Add origins metadata to the aggregated table
     for column in tb_agg:
         tb_agg[column].metadata.origins = tb["domain"].metadata.origins
+
     #
     # Save outputs.
     #
