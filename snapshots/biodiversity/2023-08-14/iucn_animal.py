0c865338d029e86fb5de0f5857a64b917eae2806
spoonerf <spoonerfiona@gmail.com>
2023-11-03 14:15:33 +0000

    fix

--- snapshots/biodiversity/2023-08-14/iucn_animal.py
+++ snapshots/biodiversity/2023-08-14/iucn_animal.py
@@ -8,7 +8,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.wait import WebDriverWait
 
 from etl.snapshot import Snapshot
 
