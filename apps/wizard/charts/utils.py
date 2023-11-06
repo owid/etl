648571835939cf4837ae610b3ce6dc50f320cfff
Marigold <mojmir.vinkler@gmail.com>
2023-11-06 13:28:43 +0100

    :sparkles: don't map onto same variables in wizard charts

--- apps/wizard/charts/utils.py
+++ apps/wizard/charts/utils.py
@@ -49,6 +49,10 @@ def get_variables_from_datasets(dataset_id_1: int, dataset_id_2: int) -> Tuple[p
         old_variables = get_variables_in_dataset(db_conn=db_conn, dataset_id=dataset_id_1, only_used_in_charts=True)
         # Get all variables from new dataset.
         new_variables = get_variables_in_dataset(db_conn=db_conn, dataset_id=dataset_id_2, only_used_in_charts=False)
+        # Remove new variables that are in the old dataset. This can happen if we're matching a dataset to itself
+        # in case of renaming variables.
+        new_variables = new_variables[~new_variables["id"].isin(old_variables["id"])]
+
     return old_variables, new_variables
 
 
