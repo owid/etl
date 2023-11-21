0bd836dafa8ad93bd0485d0dd8bf8a51c42f5923
Pablo Arriagada <paarriagadap@gmail.com>
2023-11-21 11:49:59 +0000

    :lipstick: linting

--- etl/steps/data/garden/state_capacity/2023-11-10/information_capacity_dataset.py
+++ etl/steps/data/garden/state_capacity/2023-11-10/information_capacity_dataset.py
@@ -1,8 +1,6 @@
 """Load a meadow dataset and create a garden dataset."""
 
-import numpy as np
 import owid.catalog.processing as pr
-import pandas as pd
 from owid.catalog import Table
 
 from etl.data_helpers import geo
