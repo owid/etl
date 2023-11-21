c2a15031323831fbfe5ee66dd366cdc2a2c42552
Pablo Arriagada <paarriagadap@gmail.com>
2023-11-21 16:19:47 +0000

    :construction: add missing data

--- etl/steps/data/garden/state_capacity/2023-11-10/information_capacity_dataset.py
+++ etl/steps/data/garden/state_capacity/2023-11-10/information_capacity_dataset.py
@@ -23,6 +23,13 @@ def run(dest_dir: str) -> None:
     # Load population data.
     ds_pop = paths.load_dataset("population")
 
+    # Load regional data
+    ds_regions = paths.load_dataset("regions")
+    tb_country_list = ds_regions["regions"].reset_index()
+    tb_country_list = tb_country_list[
+        (tb_country_list["region_type"] == "country") & ~(tb_country_list["is_historical"])
+    ].reset_index(drop=True)
+
     #
     # Process data.
     #
@@ -39,7 +46,7 @@ def run(dest_dir: str) -> None:
     tb = tb.reset_index(drop=True)
 
     # Add regional aggregations
-    tb = regional_aggregations(tb, ds_pop)
+    tb = regional_aggregations(tb, ds_pop, tb_country_list)
 
     tb = tb.set_index(["country", "year"], verify_integrity=True)
 
@@ -55,13 +62,17 @@ def run(dest_dir: str) -> None:
     ds_garden.save()
 
 
-def regional_aggregations(tb: Table, ds_pop: Dataset) -> Table:
+def regional_aggregations(tb: Table, ds_pop: Dataset, tb_country_list: Table) -> Table:
     """
     Add regional aggregations for some of the indicators
     """
 
     tb_regions = tb.copy()
 
+    # Add missing countries to tb_regions
+    tb_regions = add_missing_countries(tb_regions, tb_country_list)
+
+    # Add population data
     tb_regions = geo.add_population_to_table(tb_regions, ds_pop)
 
     # List of index columns
@@ -122,4 +133,35 @@ def regional_aggregations(tb: Table, ds_pop: Dataset) -> Table:
     # Concatenate tb and tb_regions
     tb = pr.concat([tb, tb_regions], ignore_index=True)
 
+    # Delete all the rows with only nan values (except for country and year)
+    tb = tb.dropna(how="all", subset=[col for col in tb.columns if col not in ["country", "year"]])
+
     return tb
+
+
+def add_missing_countries(tb_regions: Table, tb_country_list: Table) -> Table:
+    """
+    Add countries not in the dataset to generate regional aggregates
+    """
+
+    # Add all the countries available in tb_country_list to tb_regions
+    countries_tb = tb_regions["country"].unique().tolist()
+    countries_regions_dataset = tb_country_list["name"].unique().tolist()
+
+    # Obtain the list of countries in countries_regions_dataset that are not in countries_available
+    countries_to_add = list(set(countries_regions_dataset) - set(countries_tb))
+
+    # Make year integer
+    tb_regions["year"] = tb_regions["year"].astype(int)
+
+    # Create two tables, one for countries_to_add and one with the range between the minimum and maximum years
+    tb_countries = Table({"country": countries_to_add})
+    tb_years = Table({"year": range(tb_regions["year"].min(), tb_regions["year"].max() + 1)})
+
+    # Create a new table with all the countries in country_list and all the years in year_list
+    tb_country_year = pr.merge(tb_countries, tb_years, how="cross")
+
+    # Add the new table to tb_regions
+    tb_regions = pr.concat([tb_regions, tb_country_year], ignore_index=True)
+
+    return tb_regions
