adbee25ef1dbdb002f6ba19d9363869d14bab63f
Lucas Rodés-Guirao <lucasrodes@users.noreply.github.com>
2023-11-23 12:59:03 +0100

    🐛 state_capacity: minor fixes (#1969)
    * tag is required
    
    * improve layout
    
    * change indicator name
    
    * add saudi
    
    * NaN -> zero

--- etl/steps/data/grapher/countries/2023-10-01/gleditsch.py
+++ etl/steps/data/grapher/countries/2023-10-01/gleditsch.py
@@ -1,5 +1,7 @@
 """Load a garden dataset and create a grapher dataset."""
 
+from shared import fill_gaps_with_zeroes
+
 from etl.helpers import PathFinder, create_dataset
 
 # Get paths and naming conventions for current step.
@@ -21,13 +23,14 @@ def run(dest_dir: str) -> None:
     # Process data.
     #
     tb_regions = tb_regions.rename_index_names({"region": "country"})
-    tb_countries = (
-        tb_countries.reset_index()
-        .set_index(["year", "country"], verify_integrity=True)
-        .rename(columns={"id": "is_present"})
-    )
+    tb_countries = tb_countries.reset_index().rename(columns={"id": "is_present"})
     tb_countries["is_present"] = 1
     tb_countries["is_present"].m.origins = tb_regions["number_countries"].m.origins
+
+    # Fill zeroes
+    column_index = ["year", "country"]
+    tb_countries = fill_gaps_with_zeroes(tb_countries, columns=column_index)
+    tb_countries = tb_countries.set_index(column_index, verify_integrity=True)
 
     #
     # Save outputs.
