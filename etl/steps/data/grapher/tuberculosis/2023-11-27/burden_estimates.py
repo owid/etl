baeef4444d3fe4c9c3b579fd4f1aa3f3c3bba082
spoonerf <spoonerfiona@gmail.com>
2023-11-28 14:15:22 +0000

    tidy

--- etl/steps/data/grapher/tuberculosis/2023-11-27/burden_estimates.py
+++ etl/steps/data/grapher/tuberculosis/2023-11-27/burden_estimates.py
@@ -16,8 +16,6 @@ def run(dest_dir: str) -> None:
     # Read table from garden dataset.
     tb = ds_garden["burden_estimates"]
     #
-    # Save outputs.
-    #
     # Create a new grapher dataset with the same metadata as the garden dataset.
     ds_grapher = create_dataset(
         dest_dir, tables=[tb], check_variables_metadata=True, default_metadata=ds_garden.metadata
