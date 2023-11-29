129c06f46fc14c0414599430acffed791a05cd7e
Pablo Arriagada <63430031+paarriagadap@users.noreply.github.com>
2023-11-29 18:23:57 +0000

    📊 survey: Create consolidated surveys from barometers (#2003)
    * Create trust dataset from barometers
    
    * :sparkles: garden metadata
    
    * :sparkles: metadata for concatenated table
    
    * :bug: migrate code to new metadata
    
    * :sparkles: name of dataset
    
    * :lipstick: add instructions in do file and py file
    
    * :construction: remove arab barometer steps
    
    * :lipstick: indentations

---
+++ etl/steps/data/garden/survey/2023-08-04/latinobarometro_trust.py
@@ -0,0 +1,38 @@
+"""Load a meadow dataset and create a garden dataset."""
+
+
+from owid.catalog import Table
+
+from etl.data_helpers import geo
+from etl.helpers import PathFinder, create_dataset
+
+# Get paths and naming conventions for current step.
+paths = PathFinder(__file__)
+
+
+def run(dest_dir: str) -> None:
+    #
+    # Load inputs.
+    #
+    # Load meadow dataset.
+    ds_meadow = paths.load_dataset("latinobarometro_trust")
+
+    # Read table from meadow dataset.
+    tb = ds_meadow["latinobarometro_trust"].reset_index()
+
+    #
+    # Process data.
+    #
+    tb: Table = geo.harmonize_countries(df=tb, countries_file=paths.country_mapping_path)
+
+    # Create index, verify columns, and sort.
+    tb = tb.set_index(["country", "year"], verify_integrity=True).sort_index()
+
+    #
+    # Save outputs.
+    #
+    # Create a new garden dataset with the same metadata as the meadow dataset.
+    ds_garden = create_dataset(dest_dir, tables=[tb], default_metadata=ds_meadow.metadata)
+
+    # Save changes in the new garden dataset.
+    ds_garden.save()
