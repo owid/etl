cfd50f72f205e5dcfe7de9b230fd04dd9a66683d
spoonerf <spoonerfiona@gmail.com>
2023-12-04 15:01:14 +0000

    adding both tables to grapher steps

--- etl/steps/data/grapher/tuberculosis/2023-11-27/burden_disaggregated.py
+++ etl/steps/data/grapher/tuberculosis/2023-11-27/burden_disaggregated.py
@@ -15,13 +15,14 @@ def run(dest_dir: str) -> None:
 
     # Read table from garden dataset.
     tb = ds_garden["burden_disaggregated"]
+    tb_rate = ds_garden["burden_disaggregated_rate"]
 
     #
     # Save outputs.
     #
     # Create a new grapher dataset with the same metadata as the garden dataset.
     ds_grapher = create_dataset(
-        dest_dir, tables=[tb], check_variables_metadata=True, default_metadata=ds_garden.metadata
+        dest_dir, tables=[tb, tb_rate], check_variables_metadata=True, default_metadata=ds_garden.metadata
     )
 
     # Save changes in the new grapher dataset.
