98f5b61742fca787415cd8841e051314760f2a1b
spoonerf <spoonerfiona@gmail.com>
2023-12-04 11:52:48 +0000

    adding grapher step and jinja metadata

--- etl/steps/data/garden/tuberculosis/2023-11-27/burden_disaggregated.py
+++ etl/steps/data/garden/tuberculosis/2023-11-27/burden_disaggregated.py
@@ -23,7 +23,8 @@ def run(dest_dir: str) -> None:
     tb = geo.harmonize_countries(
         df=tb, countries_file=paths.country_mapping_path, excluded_countries_file=paths.excluded_countries_path
     )
-    tb = tb.set_index(["country", "year"], verify_integrity=True)
+    tb = tb.drop(columns=["measure", "unit"])
+    tb = tb.set_index(["country", "year", "age_group", "sex", "risk_factor"], verify_integrity=True)
 
     #
     # Save outputs.
