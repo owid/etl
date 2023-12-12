d6d51f907559036637592fe552a119c0d1e8b5da
spoonerf <spoonerfiona@gmail.com>
2023-12-12 09:37:24 +0000

    adding snapshot -> grapher for notifications dataset

--- etl/steps/data/garden/tuberculosis/2023-11-27/notifications.py
+++ etl/steps/data/garden/tuberculosis/2023-11-27/notifications.py
@@ -1,4 +1,5 @@
 """Load a meadow dataset and create a garden dataset."""
+from owid.catalog import Table
 from shared import add_variable_description_from_producer, removing_old_variables
 
 from etl.data_helpers import geo
@@ -13,21 +14,21 @@ def run(dest_dir: str) -> None:
     # Load inputs.
     #
     # Load meadow dataset.
-    ds_meadow = paths.load_dataset("outcomes")
+    ds_meadow = paths.load_dataset("notifications")
     snap = paths.load_snapshot("data_dictionary.csv")
     # Load data dictionary from snapshot.
     dd = snap.read()
     # Read table from meadow dataset.
-    tb = ds_meadow["outcomes"].reset_index()
-
+    tb = ds_meadow["notifications"].reset_index()
     #
     # Process data.
     #
     tb = geo.harmonize_countries(
         df=tb, countries_file=paths.country_mapping_path, excluded_countries_file=paths.excluded_countries_path
     )
-    tb = removing_old_variables(tb, dd, dataset_name="Outcomse")
+    tb = removing_old_variables(tb, dd, dataset_name="Notification")
     tb = add_variable_description_from_producer(tb, dd)
+    tb = removing_unclear_variables(tb)
     tb = tb.set_index(["country", "year"], verify_integrity=True)
 
     #
@@ -40,3 +41,31 @@ def run(dest_dir: str) -> None:
 
     # Save changes in the new garden dataset.
     ds_garden.save()
+
+
+def removing_unclear_variables(tb: Table) -> Table:
+    """
+    For some of the variables in the dataset it's not very clear what they mean, or their meaning is contingent on another variable.
+    As they are somewhat ancillary variables we have decided to remove them from the dataset.
+    """
+
+    cols_to_drop = [
+        "newrel_art",
+        "newrel_hivpos",
+        "newrel_hivtest",
+        "mdr_alloral_short_used",
+        "mdr_shortreg_used",
+        "mdrxdr_dlm_used",
+        "mdrxdr_alloral_used",
+        "mdrxdr_bdq_used",
+        "mdrxdr_bpalm_used",
+        "nrr_hpmz_used",
+        "rdxsurvey_newinc_rdx",
+        "rdxsurvey_newinc",
+        "newinc_ep_rdx",
+        "newinc_pulm_clindx_rdx",
+        "newinc_pulm_labconf_rdx",
+        "newinc_rdx",
+    ]
+
+    return tb.drop(columns=cols_to_drop)
