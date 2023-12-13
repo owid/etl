8633740f617df6e4400cdc7a5eda568baefffc10
spoonerf <spoonerfiona@gmail.com>
2023-12-13 10:55:38 +0000

    adding lab rates and metadata

--- etl/steps/data/garden/tuberculosis/2023-11-27/laboratories.py
+++ etl/steps/data/garden/tuberculosis/2023-11-27/laboratories.py
@@ -1,5 +1,7 @@
 """Load a meadow dataset and create a garden dataset."""
 
+from owid.catalog import Table
+from owid.catalog import processing as pr
 from shared import add_variable_description_from_producer
 
 from etl.data_helpers import geo
@@ -16,6 +18,9 @@ def run(dest_dir: str) -> None:
     # Load meadow dataset.
     ds_meadow = paths.load_dataset("laboratories")
     snap = paths.load_snapshot("data_dictionary.csv")
+    ds_un_wpp = paths.load_dataset("un_wpp")
+
+    ds_pop = ds_un_wpp["population"].reset_index()
     # Load data dictionary from snapshot.
     dd = snap.read()
     # Read table from meadow dataset.
@@ -28,6 +33,7 @@ def run(dest_dir: str) -> None:
     tb = tb[["country", "year", "culture", "m_wrd"]]
     tb = add_variable_description_from_producer(tb, dd)
     tb = tb.dropna(subset=["culture", "m_wrd"], how="all")
+    tb = add_population_and_rates(tb, ds_pop)
     tb = tb.set_index(["country", "year"], verify_integrity=True)
 
     #
@@ -40,3 +46,25 @@ def run(dest_dir: str) -> None:
 
     # Save changes in the new garden dataset.
     ds_garden.save()
+
+
+def add_population_and_rates(tb: Table, ds_pop: Table) -> Table:
+    """
+    Adding the total population of each country-year to the table and then calculating the rates per 100,000 people.
+
+    """
+    ds_pop = ds_pop[
+        (ds_pop["variant"] == "estimates")
+        & (ds_pop["age"] == "all")
+        & (ds_pop["sex"] == "all")
+        & (ds_pop["metric"] == "population")
+    ]
+    ds_pop = ds_pop.rename(columns={"location": "country", "value": "population"})
+
+    tb_pop = pr.merge(tb, ds_pop, on=["country", "year"], how="left")
+    tb_pop["culture_rate"] = tb_pop["culture"] / tb_pop["population"] * 100000
+    tb_pop["m_wrd_rate"] = tb_pop["m_wrd"] / tb_pop["population"] * 100000
+
+    tb_pop = tb_pop[["country", "year", "culture", "culture_rate", "m_wrd", "m_wrd_rate"]]
+
+    return tb_pop
