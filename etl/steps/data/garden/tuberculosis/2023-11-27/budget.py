ad692a18f4dbd9b4a2999f39b23b82044b799ae1
spoonerf <spoonerfiona@gmail.com>
2023-12-13 13:50:29 +0000

    fixing budget gap

--- etl/steps/data/garden/tuberculosis/2023-11-27/budget.py
+++ etl/steps/data/garden/tuberculosis/2023-11-27/budget.py
@@ -16,18 +16,18 @@ def run(dest_dir: str) -> None:
     #
     # Load meadow dataset.
     ds_meadow = paths.load_dataset("budget")
-    ds_expense = paths.load_dataset("expenditure")
+
     snap = paths.load_snapshot("data_dictionary.csv")
     # Read table from meadow dataset.
     tb = ds_meadow["budget"].reset_index()
-    tb_exp = ds_expense["expenditure"].reset_index()
+
     dd = snap.read()
     #
     # Process data.
     #
     tb = add_variable_description_from_producer(tb, dd)
     tb = geo.harmonize_countries(df=tb, countries_file=paths.country_mapping_path)
-    tb = calculate_budget_gap(tb, tb_exp)
+    tb = calculate_budget_gap(tb)
     tb = tb.set_index(["country", "year"], verify_integrity=True)
 
     #
@@ -42,15 +42,13 @@ def run(dest_dir: str) -> None:
     ds_garden.save()
 
 
-def calculate_budget_gap(tb_budget: Table, tb_exp: Table) -> Table:
+def calculate_budget_gap(tb: Table) -> Table:
     """
     Calculating the budget gap for each country-year.
 
-    We substract the total funding received from all sources (rcvd_tot_sources) from the total budget required (budget_tot).
+    We substract the total expected funding received from all sources (cf_tot_sources) from the total budget required (budget_tot).
     """
-    tb_exp = tb_exp[["country", "year", "rcvd_tot_sources"]]
-    tb_combined = tb_budget.merge(tb_exp, on=["country", "year"], how="left")
-    tb_combined["budget_gap"] = tb_combined["budget_tot"] - tb_combined["rcvd_tot_sources"]
-    tb_combined = tb_combined.drop(columns=["rcvd_tot_sources"])
 
-    return tb_combined
+    tb["budget_gap"] = tb["budget_tot"].astype("Int64") - tb["cf_tot_sources"].astype("Int64")
+
+    return tb
