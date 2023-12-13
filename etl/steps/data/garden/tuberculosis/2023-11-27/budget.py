748c07035671cac84323c95841829ce11bce6077
spoonerf <spoonerfiona@gmail.com>
2023-12-13 14:03:23 +0000

    adding regional aggs

--- etl/steps/data/garden/tuberculosis/2023-11-27/budget.py
+++ etl/steps/data/garden/tuberculosis/2023-11-27/budget.py
@@ -1,13 +1,28 @@
 """Load a meadow dataset and create a garden dataset."""
 
 from owid.catalog import Table
+from owid.catalog import processing as pr
 from shared import add_variable_description_from_producer
 
-from etl.data_helpers import geo
+from etl.data_helpers.geo import add_regions_to_table, harmonize_countries
 from etl.helpers import PathFinder, create_dataset
 
 # Get paths and naming conventions for current step.
 paths = PathFinder(__file__)
+
+REGIONS_TO_ADD = [
+    "North America",
+    "South America",
+    "Europe",
+    "Africa",
+    "Asia",
+    "Oceania",
+    "Low-income countries",
+    "Upper-middle-income countries",
+    "Lower-middle-income countries",
+    "High-income countries",
+    "World",
+]
 
 
 def run(dest_dir: str) -> None:
@@ -23,11 +38,15 @@ def run(dest_dir: str) -> None:
 
     dd = snap.read()
     #
+    ds_regions = paths.load_dependency("regions")
+    # Load income groups dataset.
+    ds_income_groups = paths.load_dependency("income_groups")
     # Process data.
     #
     tb = add_variable_description_from_producer(tb, dd)
-    tb = geo.harmonize_countries(df=tb, countries_file=paths.country_mapping_path)
+    tb = harmonize_countries(df=tb, countries_file=paths.country_mapping_path)
     tb = calculate_budget_gap(tb)
+    tb = add_regional_aggregates(tb, ds_regions, ds_income_groups)
     tb = tb.set_index(["country", "year"], verify_integrity=True)
 
     #
@@ -50,5 +69,21 @@ def calculate_budget_gap(tb: Table) -> Table:
     """
 
     tb["budget_gap"] = tb["budget_tot"].astype("Int64") - tb["cf_tot_sources"].astype("Int64")
+
+    return tb
+
+
+def add_regional_aggregates(tb: Table, ds_regions: Table, ds_income_groups: Table) -> Table:
+    """
+    Add regional sum aggregates for columns that aren't rates.
+    """
+    cols_to_drop = ["budget_cpp_dstb", "budget_cpp_mdr", "budget_cpp_xdr", "budget_cpp_tpt"]
+
+    tb_no_agg = tb[["country", "year"] + cols_to_drop]
+    tb_agg = tb.drop(columns=cols_to_drop)
+
+    tb_agg = add_regions_to_table(tb_agg, ds_regions, ds_income_groups, REGIONS_TO_ADD, min_num_values_per_year=1)
+
+    tb = pr.merge(tb_agg, tb_no_agg, on=["country", "year"], how="left")
 
     return tb
