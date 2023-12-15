b59bc5174261e419aeb13417fcb8e00cd6df824a
Pablo Rosado <pabloarosado@gmail.com>
2023-12-15 14:44:07 +0100

    Adapt meadow and garden steps of UK historical electricity

--- etl/steps/data/garden/uk_beis/2023-12-12/uk_historical_electricity.py
+++ etl/steps/data/garden/uk_beis/2023-12-12/uk_historical_electricity.py
@@ -1,7 +1,5 @@
-# TODO: This file is a duplicate of the previous step. It is not yet used in the dag and should be updated soon.
-
 import owid.catalog.processing as pr
-from owid.catalog import Dataset, Table
+from owid.catalog import Table
 
 from etl.helpers import PathFinder, create_dataset
 
@@ -61,7 +59,7 @@ def run(dest_dir: str) -> None:
     # Load data.
     #
     # Load meadow dataset and read its tables.
-    ds_meadow: Dataset = paths.load_dependency("uk_historical_electricity")
+    ds_meadow = paths.load_dataset("uk_historical_electricity")
     tb_fuel_input = ds_meadow["fuel_input"].reset_index()
     tb_supply = ds_meadow["supply"].reset_index()
     tb_efficiency = ds_meadow["efficiency"].reset_index()
@@ -76,7 +74,5 @@ def run(dest_dir: str) -> None:
     # Save outputs.
     #
     # Create new dataset.
-    ds_garden = create_dataset(
-        dest_dir=dest_dir, tables=[tb], default_metadata=ds_meadow.metadata, check_variables_metadata=True
-    )
+    ds_garden = create_dataset(dest_dir=dest_dir, tables=[tb], check_variables_metadata=True)
     ds_garden.save()
