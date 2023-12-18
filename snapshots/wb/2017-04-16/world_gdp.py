f2d13126f0fea7ab118f71ee0c8d9136cfcb1dda
Mojmir Vinkler <mojmir.vinkler@gmail.com>
2023-12-18 09:59:43 +0100

    :bar_chart: migrate fertility rate and GDP to ETL (#2111)

---
+++ snapshots/wb/2017-04-16/world_gdp.py
@@ -0,0 +1,54 @@
+from pathlib import Path
+
+import click
+import pandas as pd
+
+from etl.backport_helpers import long_to_wide
+from etl.snapshot import Snapshot, SnapshotMeta
+
+SNAPSHOT_NAMESPACE = Path(__file__).parent.parent.name
+SNAPSHOT_VERSION = Path(__file__).parent.name
+
+
+@click.command()
+@click.option(
+    "--upload/--skip-upload",
+    default=True,
+    type=bool,
+    help="Upload dataset to Snapshot",
+)
+def main(upload: bool) -> None:
+    # Load backported snapshot.
+    snap_values = Snapshot(
+        "backport/latest/dataset_393_world_gdp_in_2011_int_dollar__owid_based_on_world_bank_plus_maddison__2017_values.feather"
+    )
+    snap_values.pull()
+    snap_config = Snapshot(
+        "backport/latest/dataset_393_world_gdp_in_2011_int_dollar__owid_based_on_world_bank_plus_maddison__2017_config.json"
+    )
+    snap_config.pull()
+
+    # Create snapshot metadata for the new file
+    meta = SnapshotMeta(**snap_values.metadata.to_dict())
+    meta.namespace = SNAPSHOT_NAMESPACE
+    meta.version = SNAPSHOT_VERSION
+    meta.short_name = "world_gdp"
+    meta.fill_from_backport_snapshot(snap_config.path)
+    meta.save()
+
+    # Create a new snapshot.
+    snap = Snapshot(meta.uri)
+
+    # Convert from long to wide format.
+    df = long_to_wide(pd.read_feather(snap_values.path))
+
+    # Copy file to the new snapshot.
+    snap.path.parent.mkdir(parents=True, exist_ok=True)
+    df.reset_index().to_feather(snap.path)
+
+    # Add file to DVC and upload to S3.
+    snap.dvc_add(upload=upload)
+
+
+if __name__ == "__main__":
+    main()
