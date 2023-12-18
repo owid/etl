d2c60c235396b8f462bf90aab24228a94c2b6d72
Pablo Rosado <pabloarosado@gmail.com>
2023-12-18 11:33:15 +0100

    Adapt remaining meadow, garden and grapher IRENA steps

--- etl/steps/data/garden/irena/2023-12-12/renewable_electricity_capacity.py
+++ etl/steps/data/garden/irena/2023-12-12/renewable_electricity_capacity.py
@@ -1,13 +1,6 @@
-# TODO: This file is a duplicate of the previous step. It is not yet used in the dag and should be updated soon.
-
 """Create a dataset of renewable electricity capacity using IRENA's Renewable Electricity Capacity and Generation.
 
 """
-
-from typing import List
-
-from owid.catalog import Dataset, Table
-
 from etl.data_helpers import geo
 from etl.helpers import PathFinder, create_dataset
 
@@ -31,41 +24,19 @@ REGIONS = [
 ]
 
 
-def add_data_for_regions(tb: Table, regions: List[str], ds_regions: Dataset, ds_income_groups: Dataset) -> Table:
-    tb_with_regions = tb.copy()
-    for region in REGIONS:
-        # Find members of current region.
-        members = geo.list_members_of_region(
-            region=region,
-            ds_regions=ds_regions,
-            ds_income_groups=ds_income_groups,
-        )
-        tb_with_regions = geo.add_region_aggregates(
-            df=tb_with_regions,
-            region=region,
-            countries_in_region=members,
-            countries_that_must_have_data=[],
-            num_allowed_nans_per_year=None,
-            frac_allowed_nans_per_year=0.99999,
-        )
-    tb_with_regions = tb_with_regions.copy_metadata(from_table=tb)
-
-    return tb_with_regions
-
-
 def run(dest_dir: str) -> None:
     #
     # Load data.
     #
     # Load dataset from Meadow and read its main table.
-    ds_meadow: Dataset = paths.load_dependency("renewable_electricity_capacity")
+    ds_meadow = paths.load_dataset("renewable_electricity_capacity")
     tb = ds_meadow["renewable_electricity_capacity"].reset_index()
 
     # Load regions dataset.
-    ds_regions: Dataset = paths.load_dependency("regions")
+    ds_regions = paths.load_dataset("regions")
 
     # Load income groups dataset.
-    ds_income_groups: Dataset = paths.load_dependency("income_groups")
+    ds_income_groups = paths.load_dataset("income_groups")
 
     #
     # Process data.
@@ -77,10 +48,10 @@ def run(dest_dir: str) -> None:
     tb = tb.pivot(index=["country", "year"], columns=["technology"], values="capacity", join_column_levels_with="")
 
     # For convenience, remove parentheses from column names.
-    tb = tb.rename(columns={column: column.replace("(", "").replace(")", "") for column in tb.columns})
+    tb = tb.rename(columns={column: column.replace("(", "").replace(")", "") for column in tb.columns}, errors="raise")
 
     # Add region aggregates.
-    tb = add_data_for_regions(tb=tb, regions=REGIONS, ds_regions=ds_regions, ds_income_groups=ds_income_groups)
+    tb = geo.add_regions_to_table(tb, regions=REGIONS, ds_regions=ds_regions, ds_income_groups=ds_income_groups, min_num_values_per_year=1)
 
     # Set an appropriate index and sort conveniently.
     tb = tb.set_index(["country", "year"], verify_integrity=True).sort_index().sort_index(axis=1)
@@ -89,5 +60,5 @@ def run(dest_dir: str) -> None:
     # Save outputs.
     #
     # Create a new garden dataset.
-    ds_garden = create_dataset(dest_dir=dest_dir, tables=[tb], default_metadata=ds_meadow.metadata)
+    ds_garden = create_dataset(dest_dir=dest_dir, tables=[tb], check_variables_metadata=True)
     ds_garden.save()
