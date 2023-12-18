651845cc06e19b23338bffc5e6fc77ef364734ff
Pablo Rosado <pabloarosado@gmail.com>
2023-12-18 09:37:42 +0100

    Adapt renewable energy patents steps (#2110)
    * Adapt snapshots, meadow, garden and grapher steps for renewable energy patents
    
    * Improve metadata
    
    * Improve format

--- etl/steps/data/grapher/irena/2023-12-12/renewable_energy_patents.py
+++ etl/steps/data/grapher/irena/2023-12-12/renewable_energy_patents.py
@@ -1,17 +1,22 @@
-# TODO: This file is a duplicate of the previous step. It is not yet used in the dag and should be updated soon.
+"""Load a garden dataset and create a grapher dataset."""
 
-from owid import catalog
+from etl.helpers import PathFinder, create_dataset
 
-from etl.helpers import PathFinder
-
-N = PathFinder(__file__)
+# Get paths and naming conventions for current step.
+paths = PathFinder(__file__)
 
 
 def run(dest_dir: str) -> None:
-    # Create new Grapher dataset using metadata from garden.
-    dataset = catalog.Dataset.create_empty(dest_dir, N.garden_dataset.metadata)
-    # Load simplified table from Garden dataset.
-    table = N.garden_dataset["renewable_energy_patents_by_technology"]
-    # Add table to dataset and save dataset.
-    dataset.add(table)
-    dataset.save()
+    #
+    # Load inputs.
+    #
+    # Load garden dataset and read its main table.
+    ds_garden = paths.load_dataset("renewable_energy_patents")
+    tb_garden = ds_garden["renewable_energy_patents_by_technology"]
+
+    #
+    # Save outputs.
+    #
+    # Create a new grapher dataset with the same metadata as the garden dataset.
+    ds_grapher = create_dataset(dest_dir=dest_dir, tables=[tb_garden], check_variables_metadata=True)
+    ds_grapher.save()
--- etl/steps/data/meadow/irena/2023-12-12/renewable_energy_patents.py
+++ etl/steps/data/meadow/irena/2023-12-12/renewable_energy_patents.py
@@ -1,58 +1,42 @@
-# TODO: This file is a duplicate of the previous step. It is not yet used in the dag and should be updated soon.
+"""Load snapshot of IRENA's Renewable Energy Patents and create a raw data table.
 
-import pandas as pd
-from owid.catalog import Dataset, Table, TableMeta
-from owid.walden import Catalog as WaldenCatalog
+"""
+from etl.helpers import PathFinder, create_dataset
 
-from etl.steps.data.converters import convert_walden_metadata
-
-# Details for input datasets.
-WALDEN_VERSION = "2022-10-25"
-WALDEN_DATASET_NAME = "renewable_energy_patents"
-# Details for output dataset.
-VERSION = WALDEN_VERSION
+# Get naming conventions.
+paths = PathFinder(__file__)
 
 # Columns to use from raw data and how to rename them.
 COLUMNS = {
-    "Country/area": "country",
+    "Country": "country",
     "Year": "year",
     "Sector": "sector",
     "Technology": "technology",
-    "Sub Technology": "sub_technology",
-    "Number of Patents": "patents",
+    "Subtechnology": "sub_technology",
+    "Filed Patents": "patents",
 }
 
 
 def run(dest_dir: str) -> None:
     #
-    # Load data.
+    # Load inputs.
     #
-    # Load raw data from Walden.
-    walden_ds = WaldenCatalog().find_one(namespace="irena", short_name=WALDEN_DATASET_NAME, version=WALDEN_VERSION)
-    local_file = walden_ds.ensure_downloaded()
-    df = pd.read_csv(local_file)
+    # Retrieve snapshot.
+    snap = paths.load_snapshot("renewable_energy_patents.xlsx")
+    tb = snap.read(sheet_name="INSPIRE_data")
 
     #
     # Process data.
     #
     # Select and rename columns conveniently.
-    df = df[list(COLUMNS)].rename(columns=COLUMNS)
+    tb = tb[list(COLUMNS)].rename(columns=COLUMNS, errors="raise")
 
     # Set an appropriate index and sort conveniently.
-    df = df.set_index(["country", "year", "sector", "technology", "sub_technology"], verify_integrity=True).sort_index()
+    tb = tb.set_index(["country", "year", "sector", "technology", "sub_technology"], verify_integrity=True).sort_index()
 
     #
     # Save outputs.
     #
-    # Create new dataset and reuse metadata form Walden.
-    ds = Dataset.create_empty(dest_dir)
-    ds.metadata = convert_walden_metadata(walden_ds)
-    ds.metadata.version = VERSION
-
-    # Create table with metadata from dataframe
-    table_metadata = TableMeta(short_name=walden_ds.short_name, title=walden_ds.name, description=walden_ds.description)
-    tb = Table(df, metadata=table_metadata)
-
-    # Add table to new dataset and save dataset.
-    ds.add(tb)
-    ds.save()
+    # Create a new meadow dataset with the same metadata as the snapshot.
+    ds_meadow = create_dataset(dest_dir, tables=[tb], check_variables_metadata=True)
+    ds_meadow.save()
