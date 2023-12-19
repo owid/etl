93264b5f48ded444ccc8fc561685f53b726325a0
lucasrodes <lucasrodes@users.noreply.github.com>
2023-12-19 22:26:01 +0100

    add to wizard

--- apps/wizard/app.py
+++ apps/wizard/app.py
@@ -26,6 +26,7 @@ show_pages(
         Page(str(CURRENT_DIR / "templating/grapher.py"), "Grapher", icon="4️⃣"),
         Section("Other tools"),
         Page(str(CURRENT_DIR / "charts/__main__.py"), "Charts", icon="📊"),
+        Page(str(CURRENT_DIR / "metagpt.py"), "MetaGPT", icon="🤖"),
     ]
 )
 
