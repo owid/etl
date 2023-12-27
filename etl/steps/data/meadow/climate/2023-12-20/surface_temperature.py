d469e4948765cc852e425ef1d8e66cda571bc1cf
veronikasamborska1994 <32176660+veronikasamborska1994@users.noreply.github.com>
2023-12-27 11:10:31 +0000

    add a World Bank shapefile with country polygons

--- etl/steps/data/meadow/climate/2023-12-20/surface_temperature.py
+++ etl/steps/data/meadow/climate/2023-12-20/surface_temperature.py
@@ -1,13 +1,23 @@
 """Load a snapshot and create a meadow dataset."""
+import datetime
+import zipfile
+
+import geopandas as gpd
 import numpy as np
+import pandas as pd
+import rasterio
+import rioxarray
 import xarray as xr
-from owid import catalog
 from owid.catalog import Table
+from shapely.geometry import mapping
+from structlog import get_logger
 
 from etl.helpers import PathFinder, create_dataset
 
 # Get paths and naming conventions for current step.
 paths = PathFinder(__file__)
+# Initialize logger.
+log = get_logger()
 
 
 def run(dest_dir: str) -> None:
@@ -32,45 +42,57 @@ def run(dest_dir: str) -> None:
     # Convert the temperature values from Kelvin to Celsius by subtracting 273.15.
     da_degc = da - 273.15
 
-    # import geopandas as gpd
-    # import rasterio
-    # import rioxarray
-    # from shapely.geometry import mapping
-    # # read shapefile
+    # Read the shapefile to extract country informaiton
 
-    # shapefile = gpd.read_file(
-    #     "/Users/veronikasamborska/Downloads/world-administrative-boundaries/world-administrative-boundaries.shp"
-    # )
+    snap_geo = paths.load_snapshot("world_bank.zip")
+    shapefile_name = "WB_countries_Admin0_10m/WB_countries_Admin0_10m.shp"
 
-    # temp_country = {}
-    # small_countries = []
-    # for i in range(shapefile.shape[0]):
-    #     data = shapefile[shapefile.index == i]
-    #     da_degc.rio.write_crs("epsg:4326", inplace=True)
-    #     try:
-    #         clip = da_degc.rio.clip(data.geometry.apply(mapping), data.crs)
-    #         weights = np.cos(np.deg2rad(clip.latitude))
-    #         weights.name = "weights"
-    #         clim_month_weighted = clip.weighted(weights)
-    #         country_weighted_mean = clim_month_weighted.mean(dim=["longitude", "latitude"]).values
-    #         temp_country[shapefile.iloc[i]["name"]] = country_weighted_mean
-    #     except:
-    #         small_countries.append(shapefile.iloc[i]["name"])
-    # import datetime
+    # Check if the shapefile exists in the ZIP archive
+    with zipfile.ZipFile(snap_geo.path, "r") as z:
+        if shapefile_name in z.namelist():
+            # Construct the correct path for Geopandas
+            file_path = f"zip://{snap_geo.path}!/{shapefile_name}"
 
-    # import pandas as pd
+            # Read the shapefile directly from the ZIP archive
+            shapefile = gpd.read_file(file_path)
+            shapefile = shapefile[["geometry", "WB_NAME"]]
+            # Continue processing the data as needed
+        else:
+            log.info(
+                f"Shapefile using World Bank coordinates with '{shapefile_name}' name not found in the ZIP archive."
+            )
 
-    # start_time = datetime.datetime(1950, 1, 1, 0, 0, 0)
-    # end_time = datetime.datetime(2023, 12, 1, 0, 0, 0)
-    # idx = pd.date_range(start_time, end_time, freq="1M")
+    temp_country = {}
+    small_countries = []
+    for i in range(shapefile.shape[0]):
+        data = shapefile[shapefile.index == i]
+        da_degc.rio.write_crs("epsg:4326", inplace=True)
+        try:
+            clip = da_degc.rio.clip(data.geometry.apply(mapping), data.crs)
+            weights = np.cos(np.deg2rad(clip.latitude))
+            weights.name = "weights"
+            clim_month_weighted = clip.weighted(weights)
+            country_weighted_mean = clim_month_weighted.mean(dim=["longitude", "latitude"]).values
+            temp_country[shapefile.iloc[i]["WB_NAME"]] = country_weighted_mean
+        except:
+            small_countries.append(shapefile.iloc[i]["WB_NAME"])
+
+    log.info(f"It wasn't possible to extract data for {len(small_countries)} small countries")
+    start_time = datetime.datetime(1950, 1, 1, 0, 0, 0)
+    end_time = datetime.datetime(2023, 12, 1, 0, 0, 0)
+    idx = pd.date_range(start_time, end_time, freq="1M")
+
+    # df of temperatures for each country
+    df_temp = pd.DataFrame(temp_country)
+    df_temp["time"] = idx
 
     # # df of temperatures for each country
-    # df_temp = pd.DataFrame(temp_country)
-    # df_temp.index = idx
+    df_temp = pd.DataFrame(temp_country)
+    melted_df = df_temp.melt(id_vars=["time"], var_name="country", value_name="2m_temperature")
 
     # Create a new table and ensure all columns are snake-case and add relevant metadata.
-    tb = Table(df, short_name=paths.short_name, underscore=True)
-    tb["t2m"].metadata.origins = [snap.metadata.origin]
+    tb = Table(melted_df, short_name=paths.short_name, underscore=True)
+    tb["2m_temperature"].metadata.origins = [snap.metadata.origin]
     #
     # Save outputs.
     #
