e2d028d7ea116beff17198634e9dbe7364b86c83
lucasrodes <lucasrodes@users.noreply.github.com>
2024-01-04 18:40:23 +0100

    re-structure hyde steps

--- etl/steps/data/garden/hyde/2024-01-03/all_indicators.py
+++ etl/steps/data/garden/hyde/2024-01-03/all_indicators.py
@@ -1,5 +1,5 @@
 """Load a meadow dataset and create a garden dataset."""
-
+from etl.data_helpers import geo
 from etl.helpers import PathFinder, create_dataset
 
 # Get paths and naming conventions for current step.
@@ -7,6 +7,7 @@ paths = PathFinder(__file__)
 
 
 def run(dest_dir: str) -> None:
+    """Run step."""
     #
     # Load inputs.
     #
@@ -16,13 +17,18 @@ def run(dest_dir: str) -> None:
     tb = ds_meadow["all_indicators"].reset_index()
 
     # Load meadow dataset.
-    ds_codes = paths.load_dataset("general_files")
-    tb_codes = ds_codes["general_files"].reset_index()
+    tb_codes = ds_meadow["general_files"].reset_index()
 
     #
     # Process data.
     #
-    # Add country name
+    # Standardise country codes in codes table
+    tb_codes = geo.harmonize_countries(
+        df=tb_codes,
+        countries_file=paths.country_mapping_path,
+    )
+
+    # Add country name to main table
     tb = tb.rename(columns={"country": "iso_code"}).astype({"iso_code": "str"})
     tb_codes = tb_codes.astype("str")
     tb = tb.merge(tb_codes, on="iso_code", how="left")
@@ -32,30 +38,32 @@ def run(dest_dir: str) -> None:
 
     # Scale indicators
     ## Population indicators are given in 1,000
-    columns_1000 = [
-        "popc_c",
-        "urbc_c",
-        "rurc_c",
-    ]
-    tb[columns_1000] *= 1000
+    tb[
+        [
+            "popc_c",
+            "urbc_c",
+            "rurc_c",
+        ]
+    ] *= 1000
     ## Land use indicators are given in km2, but we want ha: 1km2 = 100ha
-    columns_100 = [
-        "uopp_c",
-        "cropland_c",
-        "tot_rice_c",
-        "tot_rainfed_c",
-        "rf_rice_c",
-        "rf_norice_c",
-        "tot_irri_c",
-        "ir_rice_c",
-        "ir_norice_c",
-        "grazing_c",
-        "pasture_c",
-        "rangeland_c",
-        "conv_rangeland_c",
-        "shifting_c",
-    ]
-    tb[columns_100] *= 100
+    tb[
+        [
+            "uopp_c",
+            "cropland_c",
+            "tot_rice_c",
+            "tot_rainfed_c",
+            "rf_rice_c",
+            "rf_norice_c",
+            "tot_irri_c",
+            "ir_rice_c",
+            "ir_norice_c",
+            "grazing_c",
+            "pasture_c",
+            "rangeland_c",
+            "conv_rangeland_c",
+            "shifting_c",
+        ]
+    ] *= 100
 
     # Set index
     tb = tb.set_index(["country", "year"], verify_integrity=True).sort_index()
--- etl/steps/data/meadow/hyde/2024-01-02/all_indicators.py
+++ etl/steps/data/meadow/hyde/2024-01-02/all_indicators.py
@@ -4,21 +4,78 @@ import os
 from pathlib import Path
 
 import owid.catalog.processing as pr
+from owid.catalog import Table
 
 from etl.helpers import PathFinder, create_dataset
+from etl.snapshot import Snapshot
 
 # Get paths and naming conventions for current step.
 paths = PathFinder(__file__)
 
 
 def run(dest_dir: str) -> None:
+    """Run step."""
     #
     # Load inputs.
     #
-    # Retrieve snapshot.
+    paths.log.info("Loading snapshots...")
+    ## All indicators
     snap = paths.load_snapshot("all_indicators.zip")
+    ## General files (country codes)
+    snap_codes = paths.load_snapshot("general_files.zip")
 
-    # Load data from snapshot.
+    # Load data from main snapshot
+    tb = load_main_table(snap)
+    # Load code -> country mapping table
+    tb_codes = load_codes_table(snap_codes)
+
+    #
+    # Process main table
+    #
+    paths.log.info("Processing main table...")
+    # Rename
+    tb = tb.rename({"region": "country"}, axis=1)
+    # Ensure all columns are snake-case, set an appropriate index, and sort conveniently.
+    tb = tb.underscore().set_index(["country", "year"], verify_integrity=True).sort_index()
+
+    #
+    # Process aux table
+    #
+    paths.log.info("Processing country codes table...")
+    # Keep good columns
+    tb_codes = tb_codes[["ISO-CODE", "Country"]]
+    # Filter spurious entries
+    mask = (tb_codes["ISO-CODE"] == 826) & (tb_codes["Country"] != "United Kingdom  ")
+    tb_codes = tb_codes[~mask]
+    # Strip country names
+    tb_codes["Country"] = tb_codes["Country"].str.strip()
+    # Ensure all columns are snake-case, set an appropriate index, and sort conveniently.
+    tb_codes = tb_codes.underscore().set_index(["iso_code"], verify_integrity=True).sort_index()
+
+    #
+    # Save outputs.
+    #
+    paths.log.info("Saving outputs...")
+    # Create list of tables
+    tables = [
+        tb,
+        tb_codes,
+    ]
+
+    # Create a new meadow dataset with the same metadata as the snapshot.
+    ds_meadow = create_dataset(
+        dest_dir,
+        tables=tables,
+        check_variables_metadata=True,
+        default_metadata=snap.metadata,
+    )
+
+    # Save changes in the new meadow dataset.
+    ds_meadow.save()
+
+
+def load_main_table(snap: Snapshot) -> Table:
+    """Load main table from snapshot."""
     with snap.extract_to_tempdir() as tmpdir:
         tbs = []
         files = os.listdir(tmpdir)
@@ -44,20 +101,24 @@ def run(dest_dir: str) -> None:
     for tb_ in tbs[1:]:
         tb = pr.merge(tb, tb_, how="outer", on=["region", "year"])
 
-    #
-    # Process data.
-    #
-    # Rename
-    tb = tb.rename({"region": "country"}, axis=1)
+    return tb
 
-    # Ensure all columns are snake-case, set an appropriate index, and sort conveniently.
-    tb = tb.underscore().set_index(["country", "year"], verify_integrity=True).sort_index()
 
-    #
-    # Save outputs.
-    #
-    # Create a new meadow dataset with the same metadata as the snapshot.
-    ds_meadow = create_dataset(dest_dir, tables=[tb], check_variables_metadata=True, default_metadata=snap.metadata)
+def load_codes_table(snap: Snapshot) -> Table:
+    """Load country codes table from snapshot.
 
-    # Save changes in the new meadow dataset.
-    ds_meadow.save()
+    This table is later used to map country codes to country names.
+    """
+    with snap.extract_to_tempdir() as tmpdir:
+        # Get all files in the directory
+        files = os.listdir(tmpdir)
+        # Sanity check
+        fname_expected = "HYDE_country_codes.xlsx"
+        assert fname_expected in files, f"The directory should contain a file named {fname_expected} (empty string)!"
+        # Read file
+        tb = pr.read_excel(
+            Path(tmpdir) / fname_expected,
+            metadata=snap.to_table_metadata(),
+            origin=snap.metadata.origin,
+        )
+    return tb
