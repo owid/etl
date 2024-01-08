b25c9fd143af0969d89a42129358d47e7acfafb8
spoonerf <spoonerfiona@gmail.com>
2024-01-08 15:56:27 +0000

    adding grapher step

---
+++ etl/steps/data/grapher/wash/2024-01-06/who.py
@@ -0,0 +1,35 @@
+"""Load a garden dataset and create a grapher dataset."""
+
+from etl.helpers import PathFinder, create_dataset
+
+# Get paths and naming conventions for current step.
+paths = PathFinder(__file__)
+
+
+def run(dest_dir: str) -> None:
+    #
+    # Load inputs.
+    #
+    # Load garden dataset.
+    ds_garden = paths.load_dataset("who")
+    table_names = ds_garden.table_names
+
+    # Read table from garden dataset.
+    tables = []
+    for table_name in table_names:
+        tb = ds_garden[table_name]
+        tables.append(tb)
+    #
+    # Process data.
+    #
+
+    #
+    # Save outputs.
+    #
+    # Create a new grapher dataset with the same metadata as the garden dataset.
+    ds_grapher = create_dataset(
+        dest_dir, tables=tables, check_variables_metadata=True, default_metadata=ds_garden.metadata
+    )
+
+    # Save changes in the new grapher dataset.
+    ds_grapher.save()
