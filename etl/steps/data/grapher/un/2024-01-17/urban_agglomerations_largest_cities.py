048085192e12301a55b2e4f126e5518109da3f53
veronikasamborska1994 <32176660+veronikasamborska1994@users.noreply.github.com>
2024-02-02 13:53:09 +0000

    small edits to urbanization metadata

--- etl/steps/data/grapher/un/2024-01-17/urban_agglomerations_largest_cities.py
+++ etl/steps/data/grapher/un/2024-01-17/urban_agglomerations_largest_cities.py
@@ -1,5 +1,7 @@
 """Load a garden dataset and create a grapher dataset."""
 
+import owid.catalog.processing as pr
+
 from etl.helpers import PathFinder, create_dataset
 
 # Get paths and naming conventions for current step.
@@ -20,18 +22,36 @@ def run(dest_dir: str) -> None:
     #
     tb = tb.reset_index()
     # Rename urban_agglomeration to country for the grapher.
-    tb = tb.rename(columns={"urban_agglomeration": "country", "country": "country_code"})
-    tb = tb.drop(columns=["rank_order", "population_capital"])
-    tb["country_code"].metadata.unit = ""
-    tb["country_code"].metadata.title = "Country color code"
+    tb = tb.rename(
+        columns={
+            "urban_agglomeration": "country",
+            "country": "country_code",
+            "time_series_of_the_population_of_the_30_largest_urban_agglomerations_in_2018_ranked_by_population_size": "population_30_largest_cities",
+        }
+    )
+    tb = tb.drop(columns=["rank_order", "population_capital", "country_code"])
 
-    tb = tb.set_index(["country", "year"], verify_integrity=True)
+    # Create two new dataframes to separate data into estimates and projections (pre-2019 and post-2019)
+    past_estimates = tb[tb["year"] < 2019].copy()
+    future_projections = tb[tb["year"] >= 2019].copy()
+
+    # Now, for each column in the original dataframe, split it into two
+    for col in tb.columns:
+        if col not in ["country", "year"]:
+            past_estimates[f"{col}_estimates"] = tb.loc[tb["year"] < 2019, col]
+            future_projections[f"{col}_projections"] = tb.loc[tb["year"] >= 2019, col]
+            past_estimates = past_estimates.drop(columns=[col])
+            future_projections = future_projections.drop(columns=[col])
+
+    tb_merged = pr.merge(past_estimates, future_projections, on=["country", "year"], how="outer")
+    tb_merged = tb_merged.set_index(["country", "year"], verify_integrity=True)
+
     #
     # Save outputs.
     #
     # Create a new grapher dataset with the same metadata as the garden dataset.
     ds_grapher = create_dataset(
-        dest_dir, tables=[tb], check_variables_metadata=True, default_metadata=ds_garden.metadata
+        dest_dir, tables=[tb_merged], check_variables_metadata=True, default_metadata=ds_garden.metadata
     )
     ds_grapher.metadata.title = "World Urbanization Prospects Dataset - Population of the 30 largest cities"
 
