fccefcfba056628575b232f12e679515f3fb4b90
lucasrodes <lucasrodes@users.noreply.github.com>
2024-02-05 13:38:07 +0100

    wizard: wip fasttrack integration

--- apps/wizard/etl_steps/fasttrack.py
+++ apps/wizard/etl_steps/fasttrack.py
@@ -1,3 +1,6 @@
+"""Fast-track import."""
+from pathlib import Path
+
 import streamlit as st
 from st_pages import add_indentation
 
@@ -5,6 +8,9 @@ from st_pages import add_indentation
 st.set_page_config(page_title="Wizard (meadow)", page_icon="🪄")
 add_indentation()
 
+
+# CONFIG
+CURRENT_DIR = Path(__file__).parent
 
 #########################################################
 # MAIN ##################################################
@@ -16,101 +22,92 @@ st.markdown(
             Fast-track is a tool for importing datasets from Google Sheets. The idea is to keep all data and metadata there, and use this interface to import or update the data in grapher database where it can be used to create charts. Fast-track also commits your work to [ETL repository](https://github.com/owid/etl) where you can further process your data with Python.
 """
 )
+# Warning (if needed)
 # st.warning("This tool is still in beta. Please report any issues to @Mojmir.")
 
-# How will the import be?
+# IMPORT method: there are three different ways to import a dataset via Fast-Track:
+# 1. Import from a Google sheet
+# 2. Update from an existing Google sheet
+# 3. Import from a local CSV
+IMPORT_GSHEET = "import_gsheet"
+UPDATE_GSHEET = "update_gsheet"
+LOCAL_CSV = "local_csv"
+IMPORT_OPTIONS = {
+    IMPORT_GSHEET: {
+        "title": "New Google sheet",
+        "description": "Import data from a Google sheet",
+        "guidelines": {
+            "heading": "❓ **How to import from a Google sheet**",
+            "file_path": CURRENT_DIR / "markdown" / "fasttrack_gsheet_import.md",
+        },
+    },
+    UPDATE_GSHEET: {
+        "title": "Existing Google sheet",
+        "description": "Update from a Google sheet (already imported in the database)",
+        "guidelines": {
+            "heading": "❓ **How to update from an existing sheet**",
+            "file_path": CURRENT_DIR / "markdown" / "fasttrack_gsheet_update.md",
+        },
+    },
+    LOCAL_CSV: {
+        "title": "Local CSV",
+        "description": "Import from a local CSV",
+        "guidelines": {
+            "heading": "❓ **How to import a local CSV**",
+            "file_path": CURRENT_DIR / "markdown" / "fasttrack_csv.md",
+        },
+    },
+}
+# Choose import method
 import_method = st.radio(
     label="How do you want to import the dataset?",
-    options=[
-        "New Google sheet",
-        "Existing Google sheet",
-        "Local CSV",
-    ],
-    captions=[
-        "Import data from a Google sheet",
-        "Update from a Google sheet (already imported in the database)",
-        "Import from a local CSV",
-    ],
+    options=IMPORT_OPTIONS.keys(),
+    captions=[IMPORT_OPTIONS[x]["description"] for x in IMPORT_OPTIONS],
+    format_func=lambda x: IMPORT_OPTIONS[x]["title"],
     help="Select the source of your data.",
     horizontal=True,
 )
+# Show brief guidelines for each import method
+for option, option_params in IMPORT_OPTIONS.items():
+    if import_method == option:
+        with st.expander(option_params["guidelines"]["heading"], expanded=False):
+            with open(option_params["guidelines"]["file_path"], "r") as f:
+                st.markdown(f.read())
+st.divider()
 
-if import_method == "New Google sheet":
-    with st.expander("❓ **How to import from a Google sheet**", expanded=False):
-        st.markdown("Placeholder")
-    st.divider()
-    with st.form("Some form"):
+# Actually create form
+with st.form("fasttrack-form"):
+    # Import field
+    if import_method == IMPORT_GSHEET:
         st.text_input(
             label="New Google Sheets URL",
             help="In the Google spreadsheet, click on `File -> Share -> Publish to Web` and share the entire document as csv.",
         )
-
-        st.checkbox(
-            label="Infer missing metadata (instead of raising an error)",
-            value=True,
-        )
-        st.checkbox(
-            label="Make dataset private (your metadata will be still public!)",
-            value=False,
-        )
-
-        submitted = st.form_submit_button(
-            "Submit",
-            type="primary",
-            use_container_width=True,
-            # on_click=update_state,
-        )
-elif import_method == "Existing Google sheet":
-    with st.expander("❓ **How to update from an existing sheet**", expanded=False):
-        st.markdown("Placeholder")
-    st.divider()
-    with st.form("Some form"):
+    elif import_method == UPDATE_GSHEET:
         st.selectbox(
             label="Existing Google Sheets",
             options=["Sheet 1", "Sheet 2", "Sheet 3"],
             help="Selected sheet will be used if you don't specify Google Sheets URL.",
         )
-
-        st.checkbox(
-            label="Infer missing metadata (instead of raising an error)",
-            value=True,
-        )
-        st.checkbox(
-            label="Make dataset private (your metadata will be still public!)",
-            value=False,
-        )
-
-        submitted = st.form_submit_button(
-            "Submit",
-            type="primary",
-            use_container_width=True,
-            # on_click=update_state,
-        )
-
-elif import_method == "Local CSV":
-    with st.expander("❓ **How to import a local CSV**", expanded=False):
-        st.markdown("Placeholder")
-    st.divider()
-
-    with st.form("Some form"):
+    else:
         st.file_uploader(
             label="Upload Local CSV",
             type=["csv"],
             help="Upload a local CSV file to import data.",
         )
+    # Other parameters
+    st.checkbox(
+        label="Infer missing metadata (instead of raising an error)",
+        value=True,
+    )
+    st.checkbox(
+        label="Make dataset private (your metadata will be still public!)",
+        value=False,
+    )
 
-        st.checkbox(
-            label="Infer missing metadata (instead of raising an error)",
-            value=True,
-        )
-        st.checkbox(
-            label="Make dataset private (your metadata will be still public!)",
-            value=False,
-        )
-
-        submitted = st.form_submit_button(
-            "Submit",
-            type="primary",
-            use_container_width=True,
-            # on_click=update_state,
-        )
+    submitted = st.form_submit_button(
+        "Submit",
+        type="primary",
+        use_container_width=True,
+        # on_click=update_state,
+    )
---
+++ apps/wizard/etl_steps/markdown/fasttrack_gsheet_update.md
@@ -0,0 +1,3 @@
+## Updating from an existing Google Sheet
+
+If you have previously imported a file from Google Sheets, this file is already being tracked by the system. Therefore, the dataset should appear in the list of datasets in the dropdown menu below.
