0bea0fd5e070319182af9960f84d80e507b13977
Lucas Rodés-Guirao <lucasrodes@users.noreply.github.com>
2024-02-09 12:37:00 +0100

    add snapshot script (#2273)

---
+++ snapshots/ggdc/2024-01-19/maddison_federico_paper.py
@@ -0,0 +1,28 @@
+"""Script to create a snapshot of dataset.
+
+This file was received via a private mail from Giovanni Federico. It's a work in progress and the final and polished version should be published soon.
+"""
+
+from pathlib import Path
+
+import click
+
+from etl.snapshot import Snapshot
+
+# Version for current snapshot dataset.
+SNAPSHOT_VERSION = Path(__file__).parent.name
+
+
+@click.command()
+@click.option("--upload/--skip-upload", default=True, type=bool, help="Upload dataset to Snapshot")
+@click.option("--path-to-file", prompt=True, type=str, help="Path to local data file.")
+def main(path_to_file: str, upload: bool) -> None:
+    # Create a new snapshot.
+    snap = Snapshot(f"ggdc/{SNAPSHOT_VERSION}/maddison_federico_paper.xlsx")
+
+    # Copy local data file to snapshots data folder, add file to DVC and upload to S3.
+    snap.create_snapshot(filename=path_to_file, upload=upload)
+
+
+if __name__ == "__main__":
+    main()
