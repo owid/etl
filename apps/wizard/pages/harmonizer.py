ea97d1208adaa098940ac9f64f865733f7179752
lucasrodes <lucasrodes@users.noreply.github.com>
2024-02-13 19:17:45 +0100

    keyerror fix

--- apps/wizard/pages/harmonizer.py
+++ apps/wizard/pages/harmonizer.py
@@ -16,7 +16,8 @@ from etl.steps import load_from_uri
 path = None
 if ("steps" in st.session_state) and ("garden" in st.session_state.steps):
     garden_vars = st.session_state["steps"]["garden"]
-    path = f"data://meadow/{garden_vars['namespace']}/{garden_vars['meadow_version']}/{garden_vars['short_name']}"
+    if ("namespace" in garden_vars) and ("meadow_version" in garden_vars) and ("short_name" in garden_vars):
+        path = f"data://meadow/{garden_vars['namespace']}/{garden_vars['meadow_version']}/{garden_vars['short_name']}"
 
 
 ####################################################################################################
