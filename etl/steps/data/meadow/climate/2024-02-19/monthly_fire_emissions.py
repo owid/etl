f58bb6b8720e0238139c3cbcaffcd13d3b5320f0
veronikasamborska1994 <32176660+veronikasamborska1994@users.noreply.github.com>
2024-02-20 09:51:28 +0000

    fire emissions

---
+++ etl/steps/data/meadow/climate/2024-02-19/monthly_fire_emissions.py
@@ -0,0 +1,55 @@
+"""Load a snapshot and create a meadow dataset."""
+
+import zipfile
+
+from owid.catalog import processing as pr
+
+from etl.helpers import PathFinder, create_dataset
+
+# Get paths and naming conventions for current step.
+paths = PathFinder(__file__)
+
+
+def run(dest_dir: str) -> None:
+    #
+    # Load inputs.
+    #
+    # Retrieve snapshot.
+    snap = paths.load_snapshot("monthly_fire_emissions.zip")
+    file_name = "emission_gfed_full_2002_2022 2.csv"
+
+    # Create a temporary directory to extract the file to
+    with zipfile.ZipFile(snap.path) as z:
+        # open the csv file in the dataset
+        with z.open(file_name) as f:
+            # Extract the file
+            tb = pr.read_csv(f, metadata=snap.to_table_metadata(), origin=snap.m.origin)
+
+    tb.metadata = snap.to_table_metadata()
+    print(tb)
+    columns_to_keep = [
+        "year",
+        "month",
+        "country",
+        "region",
+        "forest",
+        "savannas",
+        "shrublands_grasslands",
+        "croplands",
+        "other",
+    ]
+    tb = tb[columns_to_keep]
+    #
+    # Process data.
+    #
+    # Ensure all columns are snake-case, set an appropriate index, and sort conveniently.
+    tb = tb.underscore().set_index(["country", "year", "month", "region"], verify_integrity=True).sort_index()
+
+    #
+    # Save outputs.
+    #
+    # Create a new meadow dataset with the same metadata as the snapshot.
+    ds_meadow = create_dataset(dest_dir, tables=[tb], check_variables_metadata=True, default_metadata=snap.metadata)
+
+    # Save changes in the new meadow dataset.
+    ds_meadow.save()
