996c62db6183f07cd327f7e8ef25ef1241a37508
Pablo Rosado <pabloarosado@gmail.com>
2024-02-21 11:24:26 +0100

    Let temporary step create a dataset to avoid issues (#2319)

--- etl/steps/data/meadow/temp/latest/step.py
+++ etl/steps/data/meadow/temp/latest/step.py
@@ -1,1 +1,8 @@
-# Dummy file created for consistency.
+"""Dummy file created for consistency."""
+from etl.helpers import create_dataset
+
+
+def run(dest_dir: str):
+    # Create a new garden dataset with the same metadata as the meadow dataset.
+    ds_garden = create_dataset(dest_dir, tables=[], check_variables_metadata=True)
+    ds_garden.save()
