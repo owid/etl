f7d4dcd6405915f0866e4e50925e5d4a30645026
veronikasamborska1994 <32176660+veronikasamborska1994@users.noreply.github.com>
2024-02-23 22:42:48 +0000

    moving meadow processing to snapshot

--- etl/steps/data/meadow/climate/2023-12-20/surface_temperature.py
+++ etl/steps/data/meadow/climate/2023-12-20/surface_temperature.py
@@ -1,16 +1,6 @@
 """Load a snapshot and create a meadow dataset."""
-import gzip
-import zipfile
 
-import geopandas as gpd
-import numpy as np
-import pandas as pd
-import xarray as xr
-from owid.catalog import Table
-from rioxarray.exceptions import NoDataInBounds
-from shapely.geometry import mapping
 from structlog import get_logger
-from tqdm import tqdm
 
 from etl.helpers import PathFinder, create_dataset
 
@@ -25,106 +15,9 @@ def run(dest_dir: str) -> None:
     # Load inputs.
     #
     # Retrieve snapshot.
-    snap = paths.load_snapshot("surface_temperature.gz")
-    # Load data from snapshot.
-    with gzip.open(snap.path, "r") as _file:
-        ds = xr.open_dataset(_file)
-
-    # The latest 3 months in this dataset are made available through ERA5T, which is slightly different to ERA5. In the downloaded file, an extra dimenions ‘expver’ indicates which data is ERA5 (expver = 1) and which is ERA5T (expver = 5).
-    # If a value is missing in the first dataset, it is filled with the value from the second dataset.
-    ERA5_combine = ds.sel(expver=1).combine_first(ds.sel(expver=5))
-
-    # Select the 't2m' variable from the combined dataset and assign it to 'da'.
-    da = ERA5_combine["t2m"]
-
-    # Convert the temperature values from Kelvin to Celsius by subtracting 273.15.
-    da = da - 273.15
-
-    # Read the shapefile to extract country informaiton
-    snap_geo = paths.load_snapshot("world_bank.zip")
-    shapefile_name = "WB_countries_Admin0_10m/WB_countries_Admin0_10m.shp"
-
-    # Check if the shapefile exists in the ZIP archive
-    with zipfile.ZipFile(snap_geo.path, "r"):
-        # Construct the correct path for Geopandas
-        file_path = f"zip://{snap_geo.path}!/{shapefile_name}"
-
-        # Read the shapefile directly from the ZIP archive
-        shapefile = gpd.read_file(file_path)
-        shapefile = shapefile[["geometry", "WB_NAME"]]
-
-    #
-    # Process data.
-    #
-
-    # Initialize an empty dictionary to store the country-wise average temperature.
-    temp_country = {}
-
-    # Initialize a list to keep track of small countries where temperature data extraction fails.
-    small_countries = []
-
-    # Iterate over each row in the shapefile data.
-    for i in tqdm(range(shapefile.shape[0])):
-        # Extract the data for the current row.
-        data = shapefile[shapefile.index == i]
-        country_name = shapefile.iloc[i]["WB_NAME"]
-
-        # Set the coordinate reference system for the temperature data to EPSG 4326.
-        da.rio.write_crs("epsg:4326", inplace=True)
-
-        try:
-            # Clip the temperature data to the current country's shape.
-            clip = da.rio.clip(data.geometry.apply(mapping), data.crs)
-
-            # Calculate weights based on latitude to account for area distortion in latitude-longitude grids.
-            weights = np.cos(np.deg2rad(clip.latitude))
-            weights.name = "weights"
-
-            # Apply the weights to the clipped temperature data.
-            clim_month_weighted = clip.weighted(weights)
-
-            # Calculate the weighted mean temperature for the country.
-            country_weighted_mean = clim_month_weighted.mean(dim=["longitude", "latitude"]).values
-
-            # Store the calculated mean temperature in the dictionary with the country's name as the key.
-            temp_country[country_name] = country_weighted_mean
-
-        except NoDataInBounds:
-            log.info(
-                f"No data was found in the specified bounds for {country_name}."
-            )  # If an error occurs (usually due to small size of the country), add the country's name to the small_countries list.  # If an error occurs (usually due to small size of the country), add the country's name to the small_countries list.
-            small_countries.append(shapefile.iloc[i]["WB_NAME"])
-
-    # Log information about countries for which temperature data could not be extracted.
-    log.info(
-        f"It wasn't possible to extract temperature data for {len(small_countries)} small countries as they are too small for the resolution of the Copernicus data."
-    )
-
-    # Add Global mean temperature
-    weights = np.cos(np.deg2rad(da.latitude))
-    weights.name = "weights"
-    clim_month_weighted = da.weighted(weights)
-    global_mean = clim_month_weighted.mean(["longitude", "latitude"])
-    temp_country["World"] = global_mean
-
-    # Define the start and end dates
-    start_time = da["time"].min().dt.date.astype(str).item()
-    end_time = da["time"].max().dt.date.astype(str).item()
-
-    # Generate a date range from start_time to end_time with monthly frequency
-    month_starts = pd.date_range(start=start_time, end=end_time, freq="MS")
-
-    # month_starts is a DateTimeIndex object; you can convert it to a list if needed
-    month_starts_list = month_starts.tolist()
-
-    # df of temperatures for each country
-    df_temp = pd.DataFrame(temp_country)
-    df_temp["time"] = month_starts_list
-
-    melted_df = df_temp.melt(id_vars=["time"], var_name="country", value_name="temperature_2m")
-
-    # Create a new table and ensure all columns are snake-case and add relevant metadata.
-    tb = Table(melted_df, short_name=paths.short_name, underscore=True)
+    snap = paths.load_snapshot("surface_temperature.csv")
+    # Load snapshot data.
+    tb = snap.read()
     tb = tb.set_index(["time", "country"], verify_integrity=True)
 
     tb["temperature_2m"].metadata.origins = [snap.metadata.origin]
--- snapshots/climate/2024-02-14/surface_temperature.py
+++ snapshots/climate/2024-02-14/surface_temperature.py
@@ -8,16 +8,26 @@
    The data is downloaded as a NetCDF file. Tutorials for using the Copernicus API are here and work with the NETCDF format are here: https://ecmwf-projects.github.io/copernicus-training-c3s/cds-tutorial.html
    """
 
-import gzip
-import shutil
 import tempfile
+import zipfile
 from pathlib import Path
 
 # CDS API
 import cdsapi
 import click
+import geopandas as gpd
+import numpy as np
+import pandas as pd
+import xarray as xr
+from owid.datautils.io import df_to_file
+from rioxarray.exceptions import NoDataInBounds
+from shapely.geometry import mapping
+from structlog import get_logger
+from tqdm import tqdm
 
 from etl.snapshot import Snapshot
+
+log = get_logger()
 
 # Version for current snapshot dataset.
 SNAPSHOT_VERSION = Path(__file__).parent.name
@@ -27,7 +37,7 @@ SNAPSHOT_VERSION = Path(__file__).parent.name
 @click.option("--upload/--skip-upload", default=True, type=bool, help="Upload dataset to Snapshot")
 def main(upload: bool) -> None:
     # Create a new snapshot.
-    snap = Snapshot(f"climate/{SNAPSHOT_VERSION}/surface_temperature.gz")
+    snap = Snapshot(f"climate/{SNAPSHOT_VERSION}/surface_temperature.csv")
     # Save data as a compressed temporary file.
     with tempfile.TemporaryDirectory() as temp_dir:
         c = cdsapi.Client()
@@ -38,7 +48,7 @@ def main(upload: bool) -> None:
             {
                 "product_type": "monthly_averaged_reanalysis",
                 "variable": "2m_temperature",
-                "year": [str(year) for year in range(1940, 2025)],
+                "year": [str(year) for year in range(1950, 2024)],
                 "month": ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"],
                 "time": "00:00",
                 "area": [90, -180, -90, 180],
@@ -46,14 +56,103 @@ def main(upload: bool) -> None:
             },
             output_file,
         )
-        # Compress the file
-        with open(output_file, "rb") as f_in:
-            with gzip.open(str(output_file) + ".gz", "wb") as f_out:
-                shutil.copyfileobj(f_in, f_out)
+        ds = xr.open_dataset(output_file)
+    #
+    # Process data. This step is done here to avoid memory issues caused by geographical cropping in meadow step.
+    #
 
-        gzip_file = str(output_file) + ".gz"
-        # Upload snapshot.
-        snap.create_snapshot(filename=gzip_file, upload=upload)
+    # The latest 3 months in this dataset are made available through ERA5T, which is slightly different to ERA5. In the downloaded file, an extra dimenions ‘expver’ indicates which data is ERA5 (expver = 1) and which is ERA5T (expver = 5).
+    # If a value is missing in the first dataset, it is filled with the value from the second dataset.
+    ERA5_combine = ds.sel(expver=1).combine_first(ds.sel(expver=5))
+
+    # Select the 't2m' variable from the combined dataset and assign it to 'da'.
+    da = ERA5_combine["t2m"]
+
+    # Convert the temperature values from Kelvin to Celsius by subtracting 273.15.
+    da = da - 273.15
+
+    # Read the shapefile to extract country informaiton
+    snap_geo_path = Snapshot("countries/2023-12-27/world_bank.zip").path
+    shapefile_name = "WB_countries_Admin0_10m/WB_countries_Admin0_10m.shp"
+
+    # Check if the shapefile exists in the ZIP archive
+    with zipfile.ZipFile(snap_geo_path, "r"):
+        # Construct the correct path for Geopandas
+        file_path = f"zip://{snap_geo_path}!/{shapefile_name}"
+
+        # Read the shapefile directly from the ZIP archive
+        shapefile = gpd.read_file(file_path)
+        shapefile = shapefile[["geometry", "WB_NAME"]]
+
+    # Initialize an empty dictionary to store the country-wise average temperature.
+    temp_country = {}
+
+    # Initialize a list to keep track of small countries where temperature data extraction fails.
+    small_countries = []
+
+    # Iterate over each row in the shapefile data.
+    for i in tqdm(range(shapefile.shape[0])):
+        # Extract the data for the current row.
+        data = shapefile[shapefile.index == i]
+        country_name = shapefile.iloc[i]["WB_NAME"]
+
+        # Set the coordinate reference system for the temperature data to EPSG 4326.
+        da.rio.write_crs("epsg:4326", inplace=True)
+
+        try:
+            # Clip the temperature data to the current country's shape.
+            clip = da.rio.clip(data.geometry.apply(mapping), data.crs)
+
+            # Calculate weights based on latitude to account for area distortion in latitude-longitude grids.
+            weights = np.cos(np.deg2rad(clip.latitude))
+            weights.name = "weights"
+
+            # Apply the weights to the clipped temperature data.
+            clim_month_weighted = clip.weighted(weights)
+
+            # Calculate the weighted mean temperature for the country.
+            country_weighted_mean = clim_month_weighted.mean(dim=["longitude", "latitude"]).values
+
+            # Store the calculated mean temperature in the dictionary with the country's name as the key.
+            temp_country[country_name] = country_weighted_mean
+
+        except NoDataInBounds:
+            log.info(
+                f"No data was found in the specified bounds for {country_name}."
+            )  # If an error occurs (usually due to small size of the country), add the country's name to the small_countries list.  # If an error occurs (usually due to small size of the country), add the country's name to the small_countries list.
+            small_countries.append(shapefile.iloc[i]["WB_NAME"])
+
+    # Log information about countries for which temperature data could not be extracted.
+    log.info(
+        f"It wasn't possible to extract temperature data for {len(small_countries)} small countries as they are too small for the resolution of the Copernicus data."
+    )
+
+    # Add Global mean temperature
+    weights = np.cos(np.deg2rad(da.latitude))
+    weights.name = "weights"
+    clim_month_weighted = da.weighted(weights)
+    global_mean = clim_month_weighted.mean(["longitude", "latitude"])
+    temp_country["World"] = global_mean
+
+    # Define the start and end dates
+    start_time = da["time"].min().dt.date.astype(str).item()
+    end_time = da["time"].max().dt.date.astype(str).item()
+
+    # Generate a date range from start_time to end_time with monthly frequency
+    month_starts = pd.date_range(start=start_time, end=end_time, freq="MS")
+
+    # month_starts is a DateTimeIndex object; you can convert it to a list if needed
+    month_starts_list = month_starts.tolist()
+
+    # df of temperatures for each country
+    df_temp = pd.DataFrame(temp_country)
+    df_temp["time"] = month_starts_list
+
+    melted_df = df_temp.melt(id_vars=["time"], var_name="country", value_name="temperature_2m")
+    df_to_file(melted_df, file_path=snap.path)
+
+    # Add file to DVC and upload to S3.
+    snap.dvc_add(upload=upload)
 
 
 if __name__ == "__main__":
