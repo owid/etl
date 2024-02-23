eee30b35ce283d0c2cd19fc14535973f7be9bf75
veronikasamborska1994 <32176660+veronikasamborska1994@users.noreply.github.com>
2024-02-23 23:57:30 +0000

    fixed wrong year extraction

--- snapshots/climate/2024-02-14/surface_temperature.py
+++ snapshots/climate/2024-02-14/surface_temperature.py
@@ -48,7 +48,7 @@ def main(upload: bool) -> None:
             {
                 "product_type": "monthly_averaged_reanalysis",
                 "variable": "2m_temperature",
-                "year": [str(year) for year in range(1950, 2024)],
+                "year": [str(year) for year in range(1940, 2025)],
                 "month": ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"],
                 "time": "00:00",
                 "area": [90, -180, -90, 180],
