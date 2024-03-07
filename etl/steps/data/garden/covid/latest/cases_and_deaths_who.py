d96c4ab04627c288f97f59345f20567d53213e12
Lucas Rodés-Guirao <lucasrodes@users.noreply.github.com>
2024-03-07 15:08:46 +0100

    🐛 fix covid (#2384)
    * minor fix in bearing estimation
    
    * trigger build

--- etl/steps/data/garden/covid/latest/cases_and_deaths_who.py
+++ etl/steps/data/garden/covid/latest/cases_and_deaths_who.py
@@ -1,6 +1,6 @@
-from etl.helpers import PathFinder
+from shared import run as shared_run
 
-from .shared import run as shared_run
+from etl.helpers import PathFinder
 
 # Get paths and naming conventions for current step.
 paths = PathFinder(__file__)
