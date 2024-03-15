a05ec616724fac15e8821a4c545d4fd3ffcfb21c
lucasrodes <lucasrodes@users.noreply.github.com>
2024-03-15 14:59:24 +0100

    move NaN assignment to Grapher

--- etl/steps/data/grapher/democracy/2024-03-07/bmr.py
+++ etl/steps/data/grapher/democracy/2024-03-07/bmr.py
@@ -1,5 +1,7 @@
 """Load a garden dataset and create a grapher dataset."""
 
+import numpy as np
+
 from etl.helpers import PathFinder, create_dataset
 
 # Get paths and naming conventions for current step.
@@ -15,11 +17,17 @@ def run(dest_dir: str) -> None:
 
     # Read table from garden dataset.
     tb = ds_garden["bmr"]
+    tb_num_countries = ds_garden["num_countries_regime"]
+    tb_num_countries_years = ds_garden["num_countries_regime_years"]
+    tb_population = ds_garden["population_regime"]
+    tb_population_years = ds_garden["population_regime_years"]
 
     #
     # Process data.
     #
-    # Special indicator values renamings
+
+    # Main table
+    ## Special indicator values renamings
     cols = [
         # "num_years_in_democracy",
         # "num_years_in_democracy_ws",
@@ -29,7 +37,7 @@ def run(dest_dir: str) -> None:
     for col in cols:
         tb[col] = tb[col].astype("string").replace({"0": "non-democracy"})
 
-    # Drop indicators (only useful in Garden)
+    ## Drop indicators (only useful in Garden)
     columns_drop = [
         "regime_imputed_country",
         "regime_imputed",
@@ -38,15 +46,22 @@ def run(dest_dir: str) -> None:
     ]
     tb = tb.drop(columns=columns_drop)
 
+    # Region table
+    ## Set to NaN so that indicator is not imported to Grapher
+    ## WHY? These indicators count the number of countries (and people) living in countries for which we do not have regime (with WS) data (i.e. can't tell if these countries are democracies with women's suffrage or not). These indicators are equivalent to the counts of countries (and people) living in countries with no regime data (i.e. without necessarily having WS). Hence, this is to avoid having duplicate indicators.
+    mask = (slice(None), slice(None), -1)
+    tb_num_countries.loc[mask, "num_countries_regime_ws"] = np.nan
+    tb_population.loc[mask, "population_regime_ws"] = np.nan
+
     #
     # Save outputs.
     #
     tables = [
         tb,
-        ds_garden["num_countries_regime"],
-        ds_garden["num_countries_regime_years"],
-        ds_garden["population_regime"],
-        ds_garden["population_regime_years"],
+        tb_num_countries,
+        tb_num_countries_years,
+        tb_population,
+        tb_population_years,
     ]
     # Create a new grapher dataset with the same metadata as the garden dataset.
     ds_grapher = create_dataset(
