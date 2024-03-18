24df2206342ea73b8c9d3f70c33f6c4d25e49ae3
lucasrodes <lucasrodes@users.noreply.github.com>
2024-03-19 00:17:13 +0100

    meadow

---
+++ etl/steps/data/meadow/democracy/2024-03-07/vdem.py
@@ -0,0 +1,32 @@
+"""Load a snapshot and create a meadow dataset."""
+
+from etl.helpers import PathFinder, create_dataset
+
+# Get paths and naming conventions for current step.
+paths = PathFinder(__file__)
+
+
+def run(dest_dir: str) -> None:
+    #
+    # Load inputs.
+    #
+    # Retrieve snapshot.
+    snap = paths.load_snapshot("vdem.zip")
+
+    # Load data from snapshot.
+    tb = snap.read_in_archive("V-Dem-CY-Full+Others-v14.csv")
+
+    #
+    # Process data.
+    #
+    # Ensure all columns are snake-case, set an appropriate index, and sort conveniently.
+    tb = tb.underscore().set_index(["country_name", "year"], verify_integrity=True).sort_index()
+
+    #
+    # Save outputs.
+    #
+    # Create a new meadow dataset with the same metadata as the snapshot.
+    ds_meadow = create_dataset(dest_dir, tables=[tb], check_variables_metadata=True, default_metadata=snap.metadata)
+
+    # Save changes in the new meadow dataset.
+    ds_meadow.save()
