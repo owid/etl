cb4a45a885dc0dfba79875d40744cdff2190687c
Lucas Rodés-Guirao <lucasrodes@users.noreply.github.com>
2024-03-19 18:36:09 +0100

    enhance: generalise fillna table method

--- lib/catalog/owid/catalog/tables.py
+++ lib/catalog/owid/catalog/tables.py
@@ -899,18 +899,21 @@ class Table(pd.DataFrame):
         tb = tb.set_index(column_idx_new)
         return tb
 
-    def fillna(self, value, **kwargs) -> "Table":
+    def fillna(self, **kwargs) -> "Table":
         """Usual fillna, but, if the object given to fill values with is a table, transfer its metadata to the filled
         table."""
-        tb = super().fillna(value, **kwargs)
+        if "value" in kwargs:
+            value = kwargs.pop("value")
+            tb = super().fillna(value, **kwargs)
 
-        if type(value) == type(self):
-            for column in tb.columns:
-                if column in value.columns:
-                    tb._fields[column] = variables.combine_variables_metadata(
-                        variables=[tb[column], value[column]], operation="fillna", name=column
-                    )
-
+            if type(value) == type(self):
+                for column in tb.columns:
+                    if column in value.columns:
+                        tb._fields[column] = variables.combine_variables_metadata(
+                            variables=[tb[column], value[column]], operation="fillna", name=column
+                        )
+        else:
+            tb = super().fillna(**kwargs)
         return tb
 
 
