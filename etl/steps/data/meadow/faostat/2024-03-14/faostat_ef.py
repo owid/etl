3b0c8e4a4b4906636fdfc324f81570e9f670c659
Pablo Rosado <pabloarosado@gmail.com>
2024-03-20 11:37:23 +0100

    Remove steps that do not exist anymore in FAOSTAT

--- etl/steps/data/meadow/faostat/2024-03-14/faostat_ef.py
+++
@@ -1,2 +0,0 @@
-"""FAOSTAT meadow step for faostat_ef dataset."""
-from .shared import run  # noqa:F401
--- etl/steps/data/meadow/faostat/2024-03-14/faostat_ep.py
+++
@@ -1,2 +0,0 @@
-"""FAOSTAT meadow step for faostat_ep dataset."""
-from .shared import run  # noqa:F401
