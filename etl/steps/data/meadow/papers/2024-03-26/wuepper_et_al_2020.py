e2aaaadf95156250afc84484758aab5ee917af57
Pablo Rosado <pabloarosado@gmail.com>
2024-03-26 18:21:56 +0100

    Add meadow steps for old agriculture papers

---
+++ etl/steps/data/meadow/papers/2024-03-26/wuepper_et_al_2020.py
@@ -0,0 +1,27 @@
+"""Load a snapshot and create a meadow dataset."""
+
+from etl.helpers import PathFinder, create_dataset
+
+# Get paths and naming conventions for current step.
+paths = PathFinder(__file__)
+
+
+def run(dest_dir: str) -> None:
+    #
+    # Load inputs.
+    #
+    # Retrieve snapshot and read its data.
+    snap = paths.load_snapshot("wuepper_et_al_2020.xlsx")
+    tb = snap.read()
+
+    #
+    # Process data.
+    #
+    tb = tb.format(["country"], verify_integrity=False)
+
+    #
+    # Save outputs.
+    #
+    # Create a new meadow dataset.
+    ds_meadow = create_dataset(dest_dir, tables=[tb], check_variables_metadata=True)
+    ds_meadow.save()
