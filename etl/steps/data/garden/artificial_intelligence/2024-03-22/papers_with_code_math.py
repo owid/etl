e9dc20d2f663fabb22f226a1d032ae0540bcdff6
veronikasamborska1994 <32176660+veronikasamborska1994@users.noreply.github.com>
2024-03-26 09:32:38 +0000

    refactor code as per Mojmir's suggestion :)

--- etl/steps/data/garden/artificial_intelligence/2024-03-22/papers_with_code_math.py
+++ etl/steps/data/garden/artificial_intelligence/2024-03-22/papers_with_code_math.py
@@ -1,52 +1,15 @@
-"""Load a meadow dataset and create a garden dataset."""
-
 import owid.catalog.processing as pr
-import pandas as pd
+from shared import load_and_process_dataset
 
-from etl.helpers import PathFinder, create_dataset
+from etl.helpers import PathFinder
 
 # Get paths and naming conventions for current step.
 paths = PathFinder(__file__)
 
 
 def run(dest_dir: str) -> None:
-    # Load inputs.
-    # Load meadow dataset.
-    ds_meadow = paths.load_dataset("papers_with_code_math")
+    file_indicator = "math"  # Change this based on your dataset: math, language, coding
+    column_to_process = ["performance_math"]  # Change or expand this list based on your dataset
+    perform_merge = False  # Set to True if you need to merge two processed tables
 
-    # Read table from meadow dataset.
-    tb = ds_meadow["papers_with_code_math"].reset_index()
-    # Calculate 'days_since' column.
-    tb["days_since"] = (
-        pd.to_datetime(tb["date"].astype(str), format="%Y-%m-%d") - pd.to_datetime("2019-01-01")
-    ).dt.days
-    tb = tb.drop("date", axis=1)
-
-    tb = group_sort_filter(tb, "performance_math")
-    tb = tb.set_index(["days_since", "name"], verify_integrity=True).sort_index()
-
-    # Save outputs.
-    # Create a new garden dataset with the same metadata as the meadow dataset.
-    ds_garden = create_dataset(dest_dir, tables=[tb], default_metadata=ds_meadow.metadata)
-
-    # Save changes in the new garden dataset.
-    ds_garden.save()
-
-
-def group_sort_filter(tb, column):
-    # Group the DataFrame by 'days_since', 'name', and find the maximum value for the performance
-    grouped = tb.groupby(["days_since", "name"]).agg({column: "max"})
-    grouped = grouped.dropna(subset=[column]).reset_index()
-    # Ensure the tables are sorted
-    grouped = grouped.sort_values(by="days_since")
-    # Find the maximum performing model on each day
-    max_performing = grouped.groupby("days_since")[column].idxmax()
-    best_daily_models = grouped.loc[max_performing]
-    # Sort the DataFrame by 'days_since'
-    best_daily_models = best_daily_models.sort_values(by="days_since")
-    # Use cummax to compare current row performance with the previous highest one
-    best_daily_models["is_improved"] = best_daily_models[column].gt(
-        best_daily_models[column].cummax().shift(fill_value=-float("inf"))
-    )
-    # Filter rows where 'is_improved' is True
-    return best_daily_models[best_daily_models["is_improved"]].drop(columns="is_improved")
+    load_and_process_dataset(file_indicator, column_to_process, dest_dir, paths, perform_merge)
