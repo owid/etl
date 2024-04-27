240085a2f7037f03d23c41f53278814ecd3d6aca
lucasrodes <lucasrodes@users.noreply.github.com>
2024-04-27 18:18:04 +0200

    wip

---
+++ apps/wizard/pages/charts_v2/search_config.py
@@ -0,0 +1,119 @@
+"""Search configuration."""
+from typing import Any, Dict
+
+import pandas as pd
+import streamlit as st
+from pydantic import BaseModel
+from structlog import get_logger
+
+from apps.wizard.pages.charts.variable_config import reset_variable_form
+from apps.wizard.utils import set_states
+
+log = get_logger()
+
+# Set to True to select good initial default dataset selections
+DEBUG = False
+dataset_old_debug = "Population (various sources, 2023.1)"
+dataset_new_debug = "Population (Experimental)"
+
+
+def build_dataset_form(df: pd.DataFrame, similarity_names: Dict[str, Any]) -> "SearchConfigForm":
+    """Form to input dataset 1 and dataset 2."""
+    # Build dataset display name
+    df["display_name"] = "[" + df["id"].astype(str) + "] " + df["name"]
+    display_name_to_id_mapping = df.set_index("display_name")["id"].to_dict()
+
+    # Header
+    st.header(
+        "Dataset update",
+        help="Variable mapping will be done from the old to the new dataset. The idea is that the variables in the new dataset will replace those from the old dataset in our charts.",
+    )
+
+    # For debugging
+    if DEBUG:
+        index_old = list(df["name"]).index(dataset_old_debug)
+        index_new = list(df["name"]).index(dataset_new_debug)
+
+    # Dataset selectboxes
+    col1, col2 = st.columns(2)
+    with col1:
+        ## Old dataset
+        dataset_old = st.selectbox(
+            label="Old dataset",
+            options=df["display_name"],
+            help="Dataset containing variables to be replaced in our charts.",
+            index=index_old if DEBUG else 0,  # type: ignore , Debugging
+        )
+    with col2:
+        ## New dataset
+        dataset_new = st.selectbox(
+            label="New dataset",
+            options=df["display_name"],
+            help="Dataset contianinng the new variables. These will replace the old variables in our charts.",
+            index=index_new if DEBUG else 0,  # type: ignore , Debugging
+        )
+
+    # Parameters
+    col0, _ = st.columns([1, 2])
+    with col0:
+        with st.expander("Parameters"):
+            map_identical = st.toggle(
+                "Map identically named variables",
+                value=True,
+                help="Map variables with the same name in the old and new datasets. \n\n**NOTE:** This is option is DISABLED when working with the same dataset (i.e. old dataset and new dataset are the same) and can't be changed via this checkbox.",
+            )
+            enable_explore = st.toggle(
+                "Explore variable mappings (Experimental)",
+                help="Compare the variable mappings with tables and charts. This might take some time initially, as we need to download data values from S3",
+                value=False,
+            )
+            similarity_name = st.selectbox(
+                label="Similarity matching function",
+                options=similarity_names,
+                help="Select the prefered function for matching variables. Find more details at https://www.analyticsvidhya.com/blog/2021/07/fuzzy-string-matching-a-hands-on-guide/",
+            )
+
+    # Submit button
+    submitted_datasets = st.form_submit_button(
+        "Next (1/3)",
+        type="primary",
+        use_container_width=True,
+    )
+
+    # If user clicks on next, proceed
+    if submitted_datasets:
+        set_states(
+            {
+                "submitted_datasets": True,  # This means that the user has submitted the datasets
+                "submitted_variables": False,
+                "submitted_revisions": False,
+            },
+            logging=True,
+        )
+        reset_variable_form()
+
+    # Get IDs of datasets
+    dataset_old_id = display_name_to_id_mapping[dataset_old]
+    dataset_new_id = display_name_to_id_mapping[dataset_new]
+
+    return SearchConfigForm(
+        dataset_old_id=dataset_old_id,
+        dataset_new_id=dataset_new_id,
+        map_identical_variables=map_identical,
+        similarity_function_name=similarity_name,
+        enable_explore_mode=enable_explore,
+    )
+
+
+class SearchConfigForm(BaseModel):
+    """Form 1."""
+
+    dataset_old_id: str
+    dataset_new_id: str
+    map_identical_variables: bool
+    similarity_function_name: str
+    enable_explore_mode: bool
+
+    def __init__(self, **data: Any) -> None:
+        """Constructor."""
+        super().__init__(**data)
---
+++ apps/wizard/pages/charts_v2/utils.py
@@ -0,0 +1,108 @@
+"""Utils for chart revision tool."""
+from typing import Any, Dict, Tuple
+
+import pandas as pd
+import streamlit as st
+from MySQLdb import OperationalError
+from structlog import get_logger
+
+from etl.chart_revision.v2.schema import get_schema_chart_config
+from etl.db import config, get_all_datasets, get_connection, get_variables_in_dataset
+
+# Logger
+log = get_logger()
+
+
+@st.cache_data(show_spinner=False)
+def get_datasets() -> pd.DataFrame:
+    """Load datasets."""
+    with st.spinner("Retrieving datasets..."):
+        try:
+            datasets = get_all_datasets(archived=False)
+        except OperationalError as e:
+            raise OperationalError(
+                f"Could not retrieve datasets. Try reloading the page. If the error persists, please report an issue. Error: {e}"
+            )
+        else:
+            return datasets.sort_values("name")
+
+
+@st.cache_data(show_spinner=False)
+def get_schema() -> Dict[str, Any]:
+    """Load datasets."""
+    with st.spinner("Retrieving chart config schema..."):
+        try:
+            schema = get_schema_chart_config()
+        except OperationalError as e:
+            raise OperationalError(
+                f"Could not retrieve the schema. Try reloading the page. If the error persists, please report an issue. Error: {e.__traceback__}"
+            )
+        else:
+            return schema
+
+
+def get_variables_from_datasets(
+    dataset_id_1: int, dataset_id_2: int, show_new_not_in_old: int = False
+) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    """Get variables from two datasets."""
+    with get_connection() as db_conn:
+        # Get variables from old dataset that have been used in at least one chart.
+        old_variables = get_variables_in_dataset(db_conn=db_conn, dataset_id=dataset_id_1, only_used_in_charts=True)
+        # Get all variables from new dataset.
+        new_variables = get_variables_in_dataset(db_conn=db_conn, dataset_id=dataset_id_2, only_used_in_charts=False)
+        if show_new_not_in_old:
+            # Unsure why this was done, but it seems to be wrong.
+            # Remove variables in the new dataset that are not in the old dataset.
+            # This can happen if we are matching a dataset to itself in case of renaming variables.
+            new_variables = new_variables[~new_variables["id"].isin(old_variables["id"])]
+    return old_variables, new_variables
+
+
+def _check_env() -> bool:
+    """Check if environment variables are set correctly."""
+    ok = True
+    for env_name in ("GRAPHER_USER_ID", "DB_USER", "DB_NAME", "DB_HOST"):
+        if getattr(config, env_name) is None:
+            ok = False
+            st.warning(st.markdown(f"Environment variable `{env_name}` not found, do you have it in your `.env` file?"))
+
+    if ok:
+        st.success("`.env` configured correctly")
+    return ok
+
+
+def _show_environment() -> None:
+    """Show environment variables (streamlit)."""
+    # show variables (from .env)
+    st.info(
+        f"""
+    * **GRAPHER_USER_ID**: `{config.GRAPHER_USER_ID}`
+    * **DB_USER**: `{config.DB_USER}`
+    * **DB_NAME**: `{config.DB_NAME}`
+    * **DB_HOST**: `{config.DB_HOST}`
+    """
+    )
+
+
+@st.cache_resource
+def _check_env_and_environment() -> None:
+    """Check if environment variables are set correctly."""
+    ok = _check_env()
+    if ok:
+        # check that you can connect to DB
+        try:
+            with st.spinner():
+                _ = get_connection()
+        except OperationalError as e:
+            st.error(
+                "We could not connect to the database. If connecting to a remote database, remember to"
+                f" ssh-tunel into it using the appropriate ports and then try again.\n\nError:\n{e}"
+            )
+            ok = False
+        except Exception as e:
+            raise e
+        else:
+            msg = "Connection to the Grapher database was successfull!"
+            st.success(msg)
+            st.subheader("Environment")
+            _show_environment()
