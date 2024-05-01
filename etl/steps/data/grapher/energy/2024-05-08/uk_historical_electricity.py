bcdd678f1fc71fecff7e393c1bd331c272d1f1be
Pablo Rosado <pabloarosado@gmail.com>
2024-05-01 09:46:12 +0200

    Fix missing data for other renewables in the UK

--- etl/steps/data/grapher/energy/2024-05-08/uk_historical_electricity.py
+++ etl/steps/data/grapher/energy/2024-05-08/uk_historical_electricity.py
@@ -15,6 +15,19 @@ def run(dest_dir: str) -> None:
     tb_garden = ds_garden["uk_historical_electricity"]
 
     #
+    # Prepare data.
+    #
+    ####################################################################################################################
+    # Temporary solution: Fill forward other renewables, which is the only missing indicator in 2023, and makes stacked
+    # area chart show no data for 2023.
+    import pandas as pd
+
+    error = "Other renewables is no longer missing in 2023. Remove this temporary solution."
+    assert pd.isna(tb_garden.loc["United Kingdom", 2023]["other_renewables_generation"]), error
+    tb_garden["other_renewables_generation"] = tb_garden["other_renewables_generation"].ffill()
+    ####################################################################################################################
+
+    #
     # Save outputs.
     #
     ds_grapher = create_dataset(dest_dir=dest_dir, tables=[tb_garden], check_variables_metadata=True)
