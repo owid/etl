d1e9b45ad6144d715068733d98aa1cf5b7963910
lucasrodes <lucasrodes@users.noreply.github.com>
2024-05-01 18:12:25 +0200

    wip

--- etl/steps/data/grapher/democracy/2024-05-01/ert.py
+++ etl/steps/data/grapher/democracy/2024-05-01/ert.py
@@ -13,19 +13,19 @@ def run(dest_dir: str) -> None:
     # Load garden dataset.
     ds_garden = paths.load_dataset("ert")
 
-    # Read table from garden dataset.
-    tb = ds_garden["ert"]
-
     #
     # Process data.
     #
-
+    tables = [
+        ds_garden["ert"],
+        ds_garden["region_aggregates"],
+    ]
     #
     # Save outputs.
     #
     # Create a new grapher dataset with the same metadata as the garden dataset.
     ds_grapher = create_dataset(
-        dest_dir, tables=[tb], check_variables_metadata=True, default_metadata=ds_garden.metadata
+        dest_dir, tables=tables, check_variables_metadata=True, default_metadata=ds_garden.metadata
     )
 
     # Save changes in the new grapher dataset.
