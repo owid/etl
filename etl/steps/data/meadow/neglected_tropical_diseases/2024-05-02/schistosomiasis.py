e7c61acbb280d07baf5e9bc3fef231d478ad3198
spoonerf <spoonerfiona@gmail.com>
2024-05-02 12:14:45 +0100

    adding schist and soil helminthiases

--- etl/steps/data/meadow/neglected_tropical_diseases/2024-05-02/schistosomiasis.py
+++ etl/steps/data/meadow/neglected_tropical_diseases/2024-05-02/schistosomiasis.py
@@ -1,6 +1,7 @@
 """Load a snapshot and create a meadow dataset."""
 
 import numpy as np
+from owid.catalog import processing as pr
 
 from etl.helpers import PathFinder, create_dataset
 
@@ -13,7 +14,7 @@ def run(dest_dir: str) -> None:
     # Load inputs.
     #
     # Retrieve snapshot.
-    snap = paths.load_snapshot("lymphatic_filariasis.xlsx")
+    snap = paths.load_snapshot("schistosomiasis.xlsx")
 
     # Load data from snapshot.
     tb = snap.read()
@@ -21,10 +22,26 @@ def run(dest_dir: str) -> None:
     #
     # Process data.
     #
-    tb = tb.rename(columns={"Country": "country", "Year": "year", "Type of MDA": "type_of_mda"}, errors="raise")
-    tb = tb.replace("No data", np.nan)
+    # Values to replace with NaN
+    values_to_replace = [
+        "To be defined",
+        "No PC required",
+        "Surveillance",
+        " ",
+    ]  # List of values you want to replace with NaN
+
+    # Columns in which to replace the values
+    columns_to_check = [
+        "Population requiring PC for SCH annually",
+        "SAC population requiring PC for SCH annually",
+        "Programme coverage (%)",
+    ]
+
+    # Replacing the values
+    tb[columns_to_check] = tb[columns_to_check].replace(values_to_replace, np.nan)
+    tb["Programme coverage (%)"] = tb["Programme coverage (%)"].astype(float)
     # Ensure all columns are snake-case, set an appropriate index, and sort conveniently.
-    tb = tb.format(["country", "year", "type_of_mda"])
+    tb = tb.format(["country", "year"])
 
     #
     # Save outputs.
---
+++ etl/steps/data/meadow/neglected_tropical_diseases/2024-05-02/soil_transmitted_helminthiases.py
@@ -0,0 +1,56 @@
+"""Load a snapshot and create a meadow dataset."""
+
+
+import numpy as np
+
+from etl.helpers import PathFinder, create_dataset
+
+# Get paths and naming conventions for current step.
+paths = PathFinder(__file__)
+
+
+def run(dest_dir: str) -> None:
+    #
+    # Load inputs.
+    #
+    # Retrieve snapshot.
+    snap = paths.load_snapshot("soil_transmitted_helminthiases.xlsx")
+
+    # Load data from snapshot.
+    tb = snap.read()
+
+    #
+    # Process data.
+    #
+    values_to_replace = [
+        "To be defined",
+        "No PC required",
+        "No data",
+        "No data available",
+        " ",
+    ]  # List of values you want to replace with NaN
+
+    # Columns in which to replace the values - so that they can be floats rather than objects
+    columns_to_check = [
+        "Population requiring PC for STH, Pre-SAC",
+        "Programme coverage, Pre-SAC (%)",
+        "Population requiring PC for STH, SAC",
+        "Programme coverage, SAC (%)",
+    ]
+
+    # Replacing the values
+    tb[columns_to_check] = tb[columns_to_check].replace(values_to_replace, np.nan)
+    # Ensure all columns are snake-case, set an appropriate index, and sort conveniently.
+    cols = ["country", "year", "Drug combination, SAC"]
+    # For some reason format doesn't work here
+    # tb = tb.format(cols)
+    tb = tb.set_index(cols)
+
+    #
+    # Save outputs.
+    #
+    # Create a new meadow dataset with the same metadata as the snapshot.
+    ds_meadow = create_dataset(dest_dir, tables=[tb], check_variables_metadata=True, default_metadata=snap.metadata)
+
+    # Save changes in the new meadow dataset.
+    ds_meadow.save()
