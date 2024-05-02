7e0cbc9acc69d1391b40f61c31cfd6f9fab93c8d
spoonerf <spoonerfiona@gmail.com>
2024-05-02 09:53:12 +0100

    lymphatic filiarisis snap -> garden

---
+++ etl/steps/data/meadow/neglected_tropical_diseases/2024-05-02/lymphatic_filariasis.py
@@ -0,0 +1,36 @@
+"""Load a snapshot and create a meadow dataset."""
+
+import numpy as np
+
+from etl.helpers import PathFinder, create_dataset
+
+# Get paths and naming conventions for current step.
+paths = PathFinder(__file__)
+
+
+def run(dest_dir: str) -> None:
+    #
+    # Load inputs.
+    #
+    # Retrieve snapshot.
+    snap = paths.load_snapshot("lymphatic_filariasis.xlsx")
+
+    # Load data from snapshot.
+    tb = snap.read()
+
+    #
+    # Process data.
+    #
+    tb = tb.rename(columns={"Country": "country", "Year": "year", "Type of MDA": "type_of_mda"})
+    tb = tb.replace("No data", np.nan)
+    # Ensure all columns are snake-case, set an appropriate index, and sort conveniently.
+    tb = tb.format(["country", "year", "type_of_mda"])
+
+    #
+    # Save outputs.
+    #
+    # Create a new meadow dataset with the same metadata as the snapshot.
+    ds_meadow = create_dataset(dest_dir, tables=[tb], check_variables_metadata=True, default_metadata=snap.metadata)
+
+    # Save changes in the new meadow dataset.
+    ds_meadow.save()
