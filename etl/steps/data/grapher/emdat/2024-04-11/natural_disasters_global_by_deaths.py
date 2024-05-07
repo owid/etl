38e27381382c54edb7c95920ca29705267a4fa49
Pablo Rosado <pabloarosado@gmail.com>
2024-05-07 16:56:25 +0200

    Simplify code

--- etl/steps/data/grapher/emdat/2024-04-11/natural_disasters_global_by_deaths.py
+++ etl/steps/data/grapher/emdat/2024-04-11/natural_disasters_global_by_deaths.py
@@ -1,30 +1,6 @@
-from owid.catalog import Table
-
 from etl.helpers import PathFinder, create_dataset
 
 paths = PathFinder(__file__)
-
-
-def prepare_tables(table: Table, is_decade: bool) -> Table:
-    # Create wide tables.
-    table_wide = table.reset_index()
-
-    if is_decade:
-        variable_name_suffix = "_decadal"
-        variable_title_suffix = " (decadal)"
-    else:
-        variable_name_suffix = "_yearly"
-        variable_title_suffix = ""
-
-    # Improve variable names and titles.
-    for column in table_wide.drop(columns=["country", "year"], errors="raise").columns:
-        table_wide[column].metadata.title += variable_title_suffix
-        table_wide = table_wide.rename(columns={column: column + variable_name_suffix}, errors="raise")
-
-    # Set an appropriate index and sort conveniently.
-    table_wide = table_wide.format()
-
-    return table_wide
 
 
 def run(dest_dir: str) -> None:
@@ -69,9 +45,5 @@ def run(dest_dir: str) -> None:
     # Save outputs.
     #
     # Create new grapher dataset.
-    ds_grapher = create_dataset(
-        dest_dir,
-        tables=[tb_global],
-        check_variables_metadata=True,
-    )
+    ds_grapher = create_dataset(dest_dir, tables=[tb_global], check_variables_metadata=True)
     ds_grapher.save()
