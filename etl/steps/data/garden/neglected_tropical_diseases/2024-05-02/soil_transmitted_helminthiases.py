d7d75e9aef67354ac47f40d8b915b0143fa91059
spoonerf <spoonerfiona@gmail.com>
2024-05-14 16:04:34 +0100

    separating out the national coverage indicators for STH

--- etl/steps/data/garden/neglected_tropical_diseases/2024-05-02/soil_transmitted_helminthiases.py
+++ etl/steps/data/garden/neglected_tropical_diseases/2024-05-02/soil_transmitted_helminthiases.py
@@ -21,6 +21,21 @@ def run(dest_dir: str) -> None:
     # Process data.
     #
     tb = geo.harmonize_countries(df=tb, countries_file=paths.country_mapping_path)
+    # Split out the national coverage variables into separate tables
+    tb_nat_sac = (
+        tb[["country", "year", "national_coverage__sac__pct"]]
+        .copy()
+        .drop_duplicates()
+        .dropna(subset=["national_coverage__sac__pct"])
+        .drop_duplicates(subset=["country", "year"])
+    )
+    tb_nat_pre_sac = (
+        tb[["country", "year", "national_coverage__pre_sac__pct"]]
+        .copy()
+        .drop_duplicates()
+        .dropna(subset=["national_coverage__pre_sac__pct"])
+        .drop_duplicates(subset=["country", "year"])
+    )
     age_groups = ["pre_sac", "sac"]
     tbs = {}
     for age_group in age_groups:
@@ -32,7 +47,6 @@ def run(dest_dir: str) -> None:
             f"number_of_{age_group}_targeted",
             f"reported_number_of_{age_group}_treated",
             f"programme_coverage__{age_group}__pct",
-            f"national_coverage__{age_group}__pct",
         ]
         tbs[f"tb_{age_group}"] = tb[cols].copy()
         tbs[f"tb_{age_group}"].columns = [
@@ -43,7 +57,6 @@ def run(dest_dir: str) -> None:
             "number_targeted",
             "reported_number_treated",
             "programme_coverage__pct",
-            "national_coverage__pct",
         ]
         tbs[f"tb_{age_group}"] = tbs[f"tb_{age_group}"].dropna(
             subset=[
@@ -52,7 +65,6 @@ def run(dest_dir: str) -> None:
                 "number_targeted",
                 "reported_number_treated",
                 "programme_coverage__pct",
-                "national_coverage__pct",
             ],
         )
         # There are some rows which seem to be erroneous duplicates, we will drop these e.g. Burundi 2015 for sac
@@ -60,18 +72,24 @@ def run(dest_dir: str) -> None:
 
     tb_pre_sac = tbs["tb_pre_sac"]
     tb_sac = tbs["tb_sac"]
-
+    # Give the tables relevant short names
     tb_pre_sac.metadata.short_name = "soil_transmitted_helminthiases_pre_sac"
     tb_sac.metadata.short_name = "soil_transmitted_helminthiases_sac"
+    tb_nat_sac.metadata.short_name = "soil_transmitted_helminthiases_national_sac"
+    tb_nat_pre_sac.metadata.short_name = "soil_transmitted_helminthiases_national_pre_sac"
 
     tb_sac = tb_sac.format(["country", "year", "drug_combination"])
     tb_pre_sac = tb_pre_sac.format(["country", "year", "drug_combination"])
-
+    tb_nat_sac = tb_nat_sac.format(["country", "year"])
+    tb_nat_pre_sac = tb_nat_pre_sac.format(["country", "year"])
     # Save outputs.
     #
     # Create a new garden dataset with the same metadata as the meadow dataset.
     ds_garden = create_dataset(
-        dest_dir, tables=[tb_sac, tb_pre_sac], check_variables_metadata=True, default_metadata=ds_meadow.metadata
+        dest_dir,
+        tables=[tb_sac, tb_pre_sac, tb_nat_sac, tb_nat_pre_sac],
+        check_variables_metadata=True,
+        default_metadata=ds_meadow.metadata,
     )
 
     # Save changes in the new garden dataset.
