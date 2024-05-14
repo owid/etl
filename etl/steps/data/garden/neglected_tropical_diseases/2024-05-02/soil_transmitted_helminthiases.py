ee267d06bdd41c4ab935e070a2e82717645d1bd0
spoonerf <spoonerfiona@gmail.com>
2024-05-14 13:44:44 +0100

    fixing sth

--- etl/steps/data/garden/neglected_tropical_diseases/2024-05-02/soil_transmitted_helminthiases.py
+++ etl/steps/data/garden/neglected_tropical_diseases/2024-05-02/soil_transmitted_helminthiases.py
@@ -21,14 +21,57 @@ def run(dest_dir: str) -> None:
     # Process data.
     #
     tb = geo.harmonize_countries(df=tb, countries_file=paths.country_mapping_path)
-    tb = tb.format(["country", "year"])
+    age_groups = ["pre_sac", "sac"]
+    tbs = {}
+    for age_group in age_groups:
+        cols = [
+            "country",
+            "year",
+            f"drug_combination__{age_group}",
+            f"population_requiring_pc_for_sth__{age_group}",
+            f"number_of_{age_group}_targeted",
+            f"reported_number_of_{age_group}_treated",
+            f"programme_coverage__{age_group}__pct",
+            f"national_coverage__{age_group}__pct",
+        ]
+        tbs[f"tb_{age_group}"] = tb[cols].copy()
+        tbs[f"tb_{age_group}"].columns = [
+            "country",
+            "year",
+            "drug_combination",
+            "population_requiring_pc_for_sth",
+            "number_targeted",
+            "reported_number_treated",
+            "programme_coverage__pct",
+            "national_coverage__pct",
+        ]
+        tbs[f"tb_{age_group}"] = tbs[f"tb_{age_group}"].dropna(
+            subset=[
+                "drug_combination",
+                "population_requiring_pc_for_sth",
+                "number_targeted",
+                "reported_number_treated",
+                "programme_coverage__pct",
+                "national_coverage__pct",
+            ],
+        )
+        # There are some rows which seem to be erroneous duplicates, we will drop these e.g. Burundi 2015 for sac
+        tbs[f"tb_{age_group}"] = tbs[f"tb_{age_group}"].drop_duplicates(subset=["country", "year", "drug_combination"])
 
-    #
+    tb_pre_sac = tbs["tb_pre_sac"]
+    tb_sac = tbs["tb_sac"]
+
+    tb_pre_sac.metadata.short_name = "soil_transmitted_helminthiases_pre_sac"
+    tb_sac.metadata.short_name = "soil_transmitted_helminthiases_sac"
+
+    tb_sac = tb_sac.format(["country", "year", "drug_combination"])
+    tb_pre_sac = tb_pre_sac.format(["country", "year", "drug_combination"])
+
     # Save outputs.
     #
     # Create a new garden dataset with the same metadata as the meadow dataset.
     ds_garden = create_dataset(
-        dest_dir, tables=[tb], check_variables_metadata=True, default_metadata=ds_meadow.metadata
+        dest_dir, tables=[tb_sac, tb_pre_sac], check_variables_metadata=True, default_metadata=ds_meadow.metadata
     )
 
     # Save changes in the new garden dataset.
--- etl/steps/data/grapher/neglected_tropical_diseases/2024-05-02/soil_transmitted_helminthiases.py
+++ etl/steps/data/grapher/neglected_tropical_diseases/2024-05-02/soil_transmitted_helminthiases.py
@@ -14,18 +14,14 @@ def run(dest_dir: str) -> None:
     ds_garden = paths.load_dataset("soil_transmitted_helminthiases")
 
     # Read table from garden dataset.
-    tb = ds_garden["soil_transmitted_helminthiases"]
-
-    #
-    # Process data.
-    #
-
+    tb_pre_sac = ds_garden["soil_transmitted_helminthiases_pre_sac"]
+    tb_sac = ds_garden["soil_transmitted_helminthiases_sac"]
     #
     # Save outputs.
     #
     # Create a new grapher dataset with the same metadata as the garden dataset.
     ds_grapher = create_dataset(
-        dest_dir, tables=[tb], check_variables_metadata=True, default_metadata=ds_garden.metadata
+        dest_dir, tables=[tb_pre_sac, tb_sac], check_variables_metadata=True, default_metadata=ds_garden.metadata
     )
 
     # Save changes in the new grapher dataset.
