f187705c1f5f93e8dd95c23c58ec60a4ec3d053d
spoonerf <spoonerfiona@gmail.com>
2024-05-15 13:56:09 +0100

    fixing aggregates in LF

--- etl/steps/data/garden/neglected_tropical_diseases/2024-05-02/lymphatic_filariasis.py
+++ etl/steps/data/garden/neglected_tropical_diseases/2024-05-02/lymphatic_filariasis.py
@@ -1,6 +1,9 @@
 """Load a meadow dataset and create a garden dataset."""
 
+from typing import List
+
 import numpy as np
+from owid.catalog import Dataset, Table
 from owid.catalog import processing as pr
 
 from etl.data_helpers import geo
@@ -29,7 +32,12 @@ def run(dest_dir: str) -> None:
     # There are separate rows for each combination of drugs used, but this is duplicate for `national_coverage__pct`, so we will extract this column and create a separate table for it
 
     # In many cases the are two identical values for 'national_coverage__pct', for each country year, this de-duplicates them
-    tb_nat = tb[["country", "year", "national_coverage__pct"]].copy().drop_duplicates()
+    tb_nat = (
+        tb[["country", "year", "national_coverage__pct", "population_requiring_pc_for_lf"]].copy().drop_duplicates()
+    )
+    tb_nat = add_regions_to_selected_vars(
+        tb_nat, cols=["country", "year", "population_requiring_pc_for_lf"], ds_regions=ds_regions
+    )
     # There are a few cases with two values for some country-year combos, here we drop them because we are not sure which is the correct value
     tb_nat = tb_nat.drop_duplicates(subset=["country", "year"])
     tb_nat.metadata.short_name = "lymphatic_filariasis_national"
@@ -38,14 +46,9 @@ def run(dest_dir: str) -> None:
     # Replace "No data" with NaN
     tb = tb.replace("No data", np.nan)
     # Add regions to the table - for selected variables
-    tb_agg = geo.add_regions_to_table(
-        tb[["country", "year", "population_requiring_pc_for_lf", "reported_number_of_people_treated"]],
-        regions=REGIONS,
-        ds_regions=ds_regions,
-        min_num_values_per_year=1,
+    tb = add_regions_to_selected_vars(
+        tb, cols=["country", "year", "reported_number_of_people_treated"], ds_regions=ds_regions
     )
-    tb_agg = tb_agg[tb_agg["country"].isin(REGIONS)]
-    tb = pr.concat([tb, tb_agg], axis=0, ignore_index=True)
     # Format the tables
     tb = tb.format(["country", "year", "type_of_mda"])
     tb_nat = tb_nat.format(["country", "year"])
@@ -59,3 +62,20 @@ def run(dest_dir: str) -> None:
 
     # Save changes in the new garden dataset.
     ds_garden.save()
+
+
+def add_regions_to_selected_vars(tb: Table, cols: List[str], ds_regions: Dataset) -> Table:
+    """
+    Adding regions to selected variables in the table and then combining the table with the original table
+    """
+
+    tb_agg = geo.add_regions_to_table(
+        tb[cols],
+        regions=REGIONS,
+        ds_regions=ds_regions,
+        min_num_values_per_year=1,
+    )
+    tb_agg = tb_agg[tb_agg["country"].isin(REGIONS)]
+    tb = pr.concat([tb, tb_agg], axis=0, ignore_index=True)
+
+    return tb
