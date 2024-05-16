e6416f29ac293bffe5f990d875f8c43e0c3b34c5
Edouard Mathieu <edmat@pm.me>
2024-05-16 13:50:28 +0200

    us_consumer_prices: update & add all-items CPI

---
+++ etl/steps/data/garden/bls/2024-05-16/us_consumer_prices.py
@@ -0,0 +1,75 @@
+"""Load a meadow dataset and create a garden dataset."""
+
+import datetime
+
+from etl.helpers import PathFinder, create_dataset
+from owid.catalog import Table
+
+# Get paths and naming conventions for current step.
+paths = PathFinder(__file__)
+
+VARIABLE_NAMES = {
+    "CUUR0000SEEB01": "college_tuition_fees",
+    "CUUR0000SAE1": "education",
+    "CUUR0000SEEB": "tuition_fees_childcare",
+    "CUUR0000SAM": "medical_care",
+    "CUUR0000SAH21": "household_energy",
+    "CUUR0000SAH": "housing",
+    "CUUR0000SAF": "food_beverages",
+    "CUUR0000SETG": "public_transport",
+    "CUUS0000SS45011": "new_cars",
+    "CUUR0000SAA": "clothing",
+    "CUUR0000SEEE02": "software",
+    "CUUR0000SERE01": "toys",
+    "CUUR0000SERA01": "televisions",
+    "CUUR0000SA0": "all_items",
+}
+
+
+def run(dest_dir: str) -> None:
+    #
+    # Load inputs.
+    #
+    # Load meadow dataset.
+    ds_meadow = paths.load_dataset("us_consumer_prices")
+
+    # Read table from meadow dataset.
+    tb = ds_meadow["us_consumer_prices"].reset_index()
+
+    #
+    # Process data.
+    #
+
+    # Check that each series_id/year combination doesn't have more than 12 values (1 per month)
+    values_per_year = tb.groupby(["series_id", "year"], as_index=False).size()
+    assert values_per_year["size"].le(12).all()
+
+    # Cut the time series at the end of current_year-1
+    tb = tb[tb.year < datetime.date.today().year]
+
+    # Calculate the average value per series_id & year
+    tb = tb[["series_id", "year", "value"]].groupby(["series_id", "year"], as_index=False).mean()
+
+    # Translate variables to human-readable names
+    tb.series_id = tb.series_id.replace(VARIABLE_NAMES)
+
+    # Pivot to wide format and add country
+    tb = tb.pivot(columns="series_id", values="value", index="year").assign(country="United States").reset_index()
+
+    # Create a new table with the processed data.
+    tb_garden = Table(tb, short_name="us_consumer_prices").set_index(["country", "year"], verify_integrity=True)
+
+    # Ensure metadata is correctly associated.
+    for column in tb_garden.columns:
+        tb_garden[column].metadata.origins = tb["college_tuition_fees"].metadata.origins
+
+    #
+    # Save outputs.
+    #
+    # Create a new garden dataset with the same metadata as the meadow dataset.
+    ds_garden = create_dataset(
+        dest_dir, tables=[tb_garden], check_variables_metadata=True, default_metadata=ds_meadow.metadata
+    )
+
+    # Save changes in the new garden dataset.
+    ds_garden.save()
---
+++ snapshots/bls/2024-05-16/us_consumer_prices.py
@@ -0,0 +1,67 @@
+"""Script to create a snapshot of dataset."""
+
+from pathlib import Path
+import datetime
+import io
+import requests
+
+import click
+import pandas as pd
+
+from etl.snapshot import Snapshot
+from owid.datautils.io import df_to_file
+
+# Version for current snapshot dataset.
+SNAPSHOT_VERSION = Path(__file__).parent.name
+
+SERIES_IDS = [
+    "CUUR0000SEEB01",  # College tuition fees
+    "CUUR0000SAE1",  # Tuition, other school fees, and childcare
+    "CUUR0000SEEB",  # Childcare
+    "CUUR0000SAM",  # Medical care
+    "CUUR0000SAH21",  # Household energy
+    "CUUR0000SAH",  # Housing
+    "CUUR0000SAF",  # Food & Beverages
+    "CUUR0000SETG",  # Public transport
+    "CUUS0000SS45011",  # New cars
+    "CUUR0000SAA",  # Clothing
+    "CUUR0000SEEE02",  # Computer software and accessories
+    "CUUR0000SERE01",  # Toys
+    "CUUR0000SERA01",  # TVs
+    "CUUR0000SA0",  # All items
+]
+
+
+def fetch_one(series_id: str) -> pd.DataFrame:
+    print(series_id)
+    response = requests.post(
+        "https://beta.bls.gov/dataViewer/csv",
+        data={
+            "selectedSeriesIds": series_id,
+            "startYear": 1900,
+            "endYear": datetime.date.today().year,
+        },
+    )
+    return pd.read_csv(io.BytesIO(response.content))
+
+
+def fetch_many(series_ids):
+    return pd.concat([fetch_one(id) for id in series_ids])
+
+
+@click.command()
+@click.option("--upload/--skip-upload", default=True, type=bool, help="Upload dataset to Snapshot")
+def main(upload: bool) -> None:
+    # Create a new snapshot.
+    snap = Snapshot(f"bls/{SNAPSHOT_VERSION}/us_consumer_prices.csv")
+
+    # Download data from source, add file to DVC and upload to S3.
+    df = fetch_many(series_ids=SERIES_IDS)
+
+    df_to_file(df, file_path=snap.path)
+
+    snap.dvc_add(upload=upload)
+
+
+if __name__ == "__main__":
+    main()
