88a5eb301bb64256816f07ab942fb204dcfdacab
Edouard Mathieu <edmat@pm.me>
2024-05-16 16:34:18 +0200

    📊 computer_memory_storage: adjust for inflation

--- etl/steps/data/garden/technology/2024-05-13/computer_memory_storage.py
+++ etl/steps/data/garden/technology/2024-05-13/computer_memory_storage.py
@@ -1,6 +1,7 @@
 """Load a meadow dataset and create a garden dataset."""
 
 from etl.helpers import PathFinder, create_dataset
+from owid import catalog
 
 # Get paths and naming conventions for current step.
 paths = PathFinder(__file__)
@@ -19,6 +20,22 @@ def run(dest_dir: str) -> None:
     #
     # Process data.
     #
+    us_cpi = catalog.find_latest(dataset="us_consumer_prices").reset_index()[["year", "all_items"]]
+
+    # Left-merge and validate 1:1
+    tb = tb.merge(us_cpi, on="year", how="left", validate="1:1")
+
+    # If there are any missing values, raise an error.
+    if tb["all_items"].isnull().any():
+        raise ValueError("Missing values in the CPI table.")
+
+    # Adjust for inflation, using 2020 as the base year.
+    cpi_2020 = us_cpi[us_cpi["year"] == 2020]["all_items"].values[0]
+    for col in ["ddrives", "flash", "memory", "ssd"]:
+        tb[col] = tb[col] * cpi_2020 / tb["all_items"]
+    # Drop the CPI column.
+    tb = tb.drop(columns=["all_items"])
+
     tb = tb.format(["country", "year"])
 
     #
