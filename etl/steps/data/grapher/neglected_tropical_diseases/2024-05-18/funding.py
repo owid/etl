ed7d5db29f0a748d8cc2103753581275379ab427
spoonerf <spoonerfiona@gmail.com>
2024-05-18 17:48:00 +0100

    adding grapher step

--- etl/steps/data/grapher/neglected_tropical_diseases/2024-05-18/funding.py
+++ etl/steps/data/grapher/neglected_tropical_diseases/2024-05-18/funding.py
@@ -14,18 +14,18 @@ def run(dest_dir: str) -> None:
     ds_garden = paths.load_dataset("funding")
 
     # Read table from garden dataset.
-    tb = ds_garden["funding"]
+    tb_disease = ds_garden["funding_disease"]
+    tb_product = ds_garden["funding_product"]
+    tb_disease_product = ds_garden["funding_disease_product"]
 
-    #
-    # Process data.
-    #
-
-    #
     # Save outputs.
     #
     # Create a new grapher dataset with the same metadata as the garden dataset.
     ds_grapher = create_dataset(
-        dest_dir, tables=[tb], check_variables_metadata=True, default_metadata=ds_garden.metadata
+        dest_dir,
+        tables=[tb_disease, tb_product, tb_disease_product],
+        check_variables_metadata=True,
+        default_metadata=ds_garden.metadata,
     )
 
     # Save changes in the new grapher dataset.
