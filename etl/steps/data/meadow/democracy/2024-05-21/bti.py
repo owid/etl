56c9b126c612604261a06ff6ce0cde736e8d0a9a
lucasrodes <lucasrodes@users.noreply.github.com>
2024-05-21 12:25:21 +0200

    meadow (wip)

--- etl/steps/data/meadow/democracy/2024-05-21/bti.py
+++ etl/steps/data/meadow/democracy/2024-05-21/bti.py
@@ -1,9 +1,36 @@
 """Load a snapshot and create a meadow dataset."""
 
+from owid.catalog.tables import Table, concat
+
 from etl.helpers import PathFinder, create_dataset
+from etl.snapshot import Snapshot
 
 # Get paths and naming conventions for current step.
 paths = PathFinder(__file__)
+# YEAR COVERAGE
+YEAR_MIN = 2006
+YEAR_MAX = 2024
+
+# COLUMNS (relevant, renamings)
+COLUMNS = {
+    "Regions:\n1 | East-Central and Southeast Europe\n2 | Latin America and the Caribbean\n3 | West and Central Africa\n4 | Middle East and North Africa\n5 | Southern and Eastern Africa\n6 | Eastern Europe, Caucasus and Central Asia\n7 | Asia and Oceania": "country",
+    "  SI | Democracy Status": "democracy_bti",
+    "  Q1 | Stateness": "state_bti",
+    "  Q2 | Political Participation": "political_participation_bti",
+    "  Q3 | Rule of Law": "rule_of_law_bti",
+    "  Q4 | Stability of Democratic Institutions": "stability_dem_inst_bti",
+    "  Q5 | Political and Social Integration": "pol_soc_integr_bti",
+    # "": "regime_bti",
+    "  Q2_1 | Free and fair elections": "electfreefair_bti",
+    "  Q2_2 | Effective power to govern": "effective_power_bti",
+    "  Q2_3 | Association / assembly rights": "freeassoc_bti",
+    "  Q2_4 | Freedom of expression": "freeexpr_bti",
+    "  Q3_1 | Separation of powers": "sep_power_bti",
+    "  Q3_4 | Civil rights": "civ_rights_bti",
+    "  Failed State": "state_basic_bti",
+    "  Total": "pol_sys",
+}
+COLUMNS_INDEX = ["country", "year"]
 
 
 def run(dest_dir: str) -> None:
@@ -14,7 +41,7 @@ def run(dest_dir: str) -> None:
     snap = paths.load_snapshot("bti.xlsx")
 
     # Load data from snapshot.
-    tb = snap.read()
+    tb = load_data(snap)
 
     #
     # Process data.
@@ -30,3 +57,32 @@ def run(dest_dir: str) -> None:
 
     # Save changes in the new meadow dataset.
     ds_meadow.save()
+
+
+def load_data(snap: Snapshot) -> Table:
+    """Load data from snapshot and return a Table."""
+    tbs = []
+    for year in range(2006, YEAR_MAX + 1, 2):
+        # Read
+        tb_ = snap.read(sheet_name=f"BTI {year}")
+        # Column check
+        columns_missing = set(COLUMNS) - set(tb_.columns)
+        if columns_missing:
+            raise ValueError(f"Columns missing in snapshot: {columns_missing}")
+        # Column selection & renaming
+        tb_ = tb_.rename(columns=COLUMNS)[COLUMNS.values()]
+        # Add year (year of observation = year of report - 1)
+        tb_["year"] = year - 1
+        tbs.append(tb_)
+
+    # Concatenate
+    tb = concat(tbs)
+
+    # Replace '-' -> NA
+    columns = [col for col in COLUMNS.values() if col not in COLUMNS_INDEX]
+    tb[columns] = tb[columns].replace("-", float("nan"))
+
+    # Set dtypes
+    tb[columns] = tb[columns].astype(float)
+
+    return tb
