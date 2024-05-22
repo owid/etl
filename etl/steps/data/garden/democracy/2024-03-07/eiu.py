509852ae07f32e46b5fa0cd2c53992633c82f8f7
lucasrodes <lucasrodes@users.noreply.github.com>
2024-05-22 16:54:14 +0200

    wip

---
+++ etl/steps/data/garden/democracy/2024-03-07/eiu.py
@@ -0,0 +1,69 @@
+"""Load a meadow dataset and create a garden dataset."""
+
+import pandas as pd
+from owid.catalog import Table
+
+from etl.data_helpers import geo
+from etl.helpers import PathFinder, create_dataset
+
+# Get paths and naming conventions for current step.
+paths = PathFinder(__file__)
+
+
+def run(dest_dir: str) -> None:
+    #
+    # Load inputs.
+    #
+    # Load meadow dataset.
+    ds_meadow = paths.load_dataset("eiu")
+
+    # Read table from meadow dataset.
+    tb = ds_meadow["eiu"].reset_index()
+
+    #
+    # Process data.
+    #
+    tb = geo.harmonize_countries(
+        df=tb,
+        countries_file=paths.country_mapping_path,
+    )
+
+    # Remove years with interpolated data (2007 and 2009 are interpolated by Gapminder)
+    tb = tb[~tb["year"].isin([2007, 2009])]
+
+    # Drop rank column
+    tb = tb.drop(columns=["rank_eiu"])
+
+    # Format
+    tb = tb.format(["country", "year"])
+
+    #
+    # Save outputs.
+    #
+    # Create a new garden dataset with the same metadata as the meadow dataset.
+    ds_garden = create_dataset(
+        dest_dir, tables=[tb], check_variables_metadata=True, default_metadata=ds_meadow.metadata
+    )
+
+    # Save changes in the new garden dataset.
+    ds_garden.save()
+
+
+def add_regime_identifier(tb: Table) -> Table:
+    bins = [
+        -0.01,
+        4,
+        6,
+        8,
+        10,
+    ]
+    labels = [
+        0,
+        1,
+        2,
+        3,
+    ]
+    # 1. Create variable for age group of electoral demcoracies:
+    tb["regime_eiu"] = pd.cut(tb["democracy_eiu"], bins=bins, labels=labels)
+
+    return tb
--- etl/steps/data/meadow/democracy/2024-05-22/eiu.py
+++ etl/steps/data/meadow/democracy/2024-05-22/eiu.py
@@ -1,6 +1,7 @@
 """Load a snapshot and create a meadow dataset."""
 
-from owid.catalog.tables import concat
+import pandas as pd
+from owid.catalog.tables import Table, concat
 
 from etl.helpers import PathFinder, create_dataset
 
@@ -12,9 +13,14 @@ def run(dest_dir: str) -> None:
     #
     # Load inputs.
     #
-    # Retrieve snapshots
+
+    # Retieve data from Gapminder
+    snap = paths.load_snapshot("eiu_gapminder.csv")
+    tb_gm = snap.read()
+
+    # Retrieve data from EIU (single year reports)
     shortnames = [
-        "eiu_gapminder",
+        # "eiu_gapminder",
         "eiu_2021",
         "eiu_2022",
         "eiu_2023",
@@ -25,7 +31,23 @@ def run(dest_dir: str) -> None:
         tb = snap.read()
         tbs.append(tb)
 
+    # Correct data by Gapminder
+    ## Gapminder multiplies all values by ten.
+    cols = [
+        "democracy_eiu",
+        "elect_freefair_eiu",
+        "funct_gov_eiu",
+        "pol_part_eiu",
+        "dem_culture_eiu",
+        "civlib_eiu",
+    ]
+    tb_gm[cols] = tb_gm[cols] / 10
+
+    ## Add missing data
+    tb_gm = add_datapoints(tb_gm)
+
     # Concatenate all tables.
+    tbs.append(tb_gm)
     tb = concat(tbs, ignore_index=True, short_name="eiu")
 
     #
@@ -51,3 +73,52 @@ def run(dest_dir: str) -> None:
 
     # Save changes in the new meadow dataset.
     ds_meadow.save()
+
+
+def add_datapoints(tb: Table) -> Table:
+    """Add missing datapoints in Gapminder data."""
+    # Define records
+    records = [
+        {
+            "country_name": "Algeria",
+            "democracy_eiu": 3.77,
+            "elect_freefair_eiu": 3.08,
+            "funct_gov_eiu": 2.5,
+            "pol_part_eiu": 4.44,
+            "dem_culture_eiu": 5,
+            "civlib_eiu": 3.82,
+        },
+        {
+            "country_name": "Iran",
+            "democracy_eiu": 2.2,
+            "elect_freefair_eiu": 0,
+            "funct_gov_eiu": 2.5,
+            "pol_part_eiu": 3.89,
+            "dem_culture_eiu": 3.13,
+            "civlib_eiu": 1.47,
+        },
+        {
+            "country_name": "Lithuania",
+            "democracy_eiu": 7.13,
+            "elect_freefair_eiu": 9.58,
+            "funct_gov_eiu": 6.07,
+            "pol_part_eiu": 5.56,
+            "dem_culture_eiu": 5.63,
+            "civlib_eiu": 8.82,
+        },
+        {
+            "country_name": "Ukraine",
+            "democracy_eiu": 5.81,
+            "elect_freefair_eiu": 8.25,
+            "funct_gov_eiu": 2.71,
+            "pol_part_eiu": 7.22,
+            "dem_culture_eiu": 5,
+            "civlib_eiu": 5.88,
+        },
+    ]
+    tb_ext = Table.from_records(records).assign(year=2020)
+
+    # Add to main table
+    tb = concat([tb, tb_ext], ignore_index=True, short_name=tb.m.short_name)
+
+    return tb
