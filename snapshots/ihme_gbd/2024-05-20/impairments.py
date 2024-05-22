2737a16ffc48dccb5df0a8f3f7388bffff803380
spoonerf <spoonerfiona@gmail.com>
2024-05-22 14:43:15 +0100

    updating in line with Mojmir's suggestions

--- snapshots/ihme_gbd/2024-05-20/impairments.py
+++ snapshots/ihme_gbd/2024-05-20/impairments.py
@@ -20,80 +20,43 @@ The data will then be requested and a download link will be sent to you with a n
 We will download and combine the files in the following script.
 """
 
-import os
-import tempfile
-import time
-import zipfile
+
 from pathlib import Path
 
 import click
 import pandas as pd
-import requests
-from owid.datautils.io import df_to_file
+from owid.datautils.dataframes import concatenate
+from owid.repack import repack_frame
+from shared import download_data
+from structlog import get_logger
 
 from etl.snapshot import Snapshot
 
+log = get_logger()
 # Version for current snapshot dataset.
 SNAPSHOT_VERSION = Path(__file__).parent.name
 BASE_URL = "https://dl.healthdata.org:443/gbd-api-2021-public/a086e74384319dfcf408e10b4fdcdcd8_files/IHME-GBD_2021_DATA-a086e743-"
 NUMBER_OF_FILES = 21
 
 
-def download_data(file_number: int) -> pd.DataFrame:
-    # Unique URL for each file
-    url_to_download = f"{BASE_URL}{file_number}.zip"
-    # Retry logic
-    max_retries = 5
-    backoff_factor = 1  # Factor for exponential backoff
-
-    for attempt in range(max_retries):
-        try:
-            response = requests.get(url_to_download)
-            response.raise_for_status()
-            break  # If request is successful, exit the loop
-        except requests.RequestException as e:
-            if attempt < max_retries - 1:
-                wait_time = backoff_factor * (2**attempt)  # Exponential backoff
-                print(f"Attempt {attempt + 1} failed: {e}. Retrying in {wait_time} seconds...")
-                time.sleep(wait_time)
-            else:
-                print(f"Failed to download the file after {max_retries} attempts. Error: {e}")
-                raise
-    # Download data from source, open the csv within and return that.
-    response = requests.get(url_to_download)
-    with tempfile.TemporaryDirectory() as tmpdirname:
-        zip_file_name = f"{BASE_URL.split('/')[-1]}{file_number}.zip"
-        zip_file_path = os.path.join(tmpdirname, zip_file_name)
-        with open(zip_file_path, "wb") as f:
-            f.write(response.content)
-        # Extract the zip file
-        with zipfile.ZipFile(zip_file_path, "r") as zip_ref:
-            zip_ref.extractall(tmpdirname)
-        # Construct the CSV file name and path
-        csv_file_name = f"{BASE_URL.split('/')[-1]}{file_number}.csv"
-        csv_file_path = os.path.join(tmpdirname, csv_file_name)
-        # Read the CSV file
-        df = pd.read_csv(csv_file_path)
-        return df
-
-
 @click.command()
 @click.option("--upload/--skip-upload", default=True, type=bool, help="Upload dataset to Snapshot")
 def main(upload: bool) -> None:
     # Create a new snapshot.
-    snap = Snapshot(f"ihme_gbd/{SNAPSHOT_VERSION}/impairments.csv")
+    snap = Snapshot(f"ihme_gbd/{SNAPSHOT_VERSION}/gbd_cause.csv")
     # Download data from source.
-    all_dfs = []
+    dfs: list[pd.DataFrame] = []
     for file_number in range(1, NUMBER_OF_FILES + 1):
-        df = download_data(file_number)
-        all_dfs.append(df)
+        log.info(f"Downloading file {file_number} of {NUMBER_OF_FILES}")
+        df = download_data(file_number, base_url=BASE_URL)
+        log.info(f"Download of file {file_number} finished", size=f"{df.memory_usage(deep=True).sum()/1e6:.2f} MB")
+        dfs.append(df)
 
-    combined_df = pd.concat(all_dfs, ignore_index=True)
-    # Download data from source.
-    df_to_file(combined_df, file_path=snap.path)
+    # Concatenate the dataframes while keeping categorical columns to reduce memory usage.
+    df = repack_frame(concatenate(dfs))
 
-    # Add file to DVC and upload to S3.
-    snap.dvc_add(upload=upload)
+    log.info("Uploading final file", size=f"{df.memory_usage(deep=True).sum()/1e6:.2f} MB")
+    snap.create_snapshot(upload=upload, data=df)
 
 
 if __name__ == "__main__":
