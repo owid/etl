acfaf6285263b8764fb9a4f6f0bf0314df2bbf4f
spoonerf <spoonerfiona@gmail.com>
2024-05-23 15:59:26 +0100

    sorting out the rates

--- etl/steps/data/garden/ihme_gbd/2024-05-20/impairments.py
+++ etl/steps/data/garden/ihme_gbd/2024-05-20/impairments.py
@@ -6,11 +6,20 @@ from owid.catalog import Dataset, Table
 from owid.catalog import processing as pr
 
 from etl.data_helpers import geo
+from etl.data_helpers.population import add_population
 from etl.helpers import PathFinder, create_dataset
 
 # Get paths and naming conventions for current step.
 paths = PathFinder(__file__)
 REGIONS = ["North America", "South America", "Europe", "Africa", "Asia", "Oceania"]
+AGE_GROUPS_RANGES = {
+    "All ages": [0, None],
+    "<5 years": [0, 4],
+    "5-14 years": [5, 14],
+    "15-49 years": [15, 49],
+    "50-69 years": [50, 69],
+    "70+ years": [70, None],
+}
 
 
 def run(dest_dir: str) -> None:
@@ -61,7 +70,9 @@ def add_regional_aggregates(tb: Table, ds_regions: Dataset, ds_population: Datas
     Adding the regional aggregated data for the OWID continent regions
     """
     # Add population data
-    tb = geo.add_population_to_table(tb=tb, ds_population=ds_population)
+    tb = add_population(
+        df=tb, country_col="country", year_col="year", age_col="age", age_group_mapping=AGE_GROUPS_RANGES
+    )
     tb_number = tb[tb["metric"] == "Number"].copy()
     tb_rate = tb[tb["metric"] == "Rate"].copy()
     # Add region aggregates.
