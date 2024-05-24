212459ae13ec23a5305d149621fa11e76c33de32
Pablo Rosado <pabloarosado@gmail.com>
2024-05-24 18:00:31 +0200

    Improve titles

--- etl/steps/data/grapher/emdat/2024-04-11/natural_disasters.py
+++ etl/steps/data/grapher/emdat/2024-04-11/natural_disasters.py
@@ -5,25 +5,42 @@ from etl.helpers import PathFinder, create_dataset
 paths = PathFinder(__file__)
 
 # Map of variable titles to their corresponding public titles.
-TITLE_IMPACT = {
-    "Natural disasters": "Annual number of natural disasters",
-    "Natural disasters per 100,000 people": "Annual rate of natural disasters",
-    "Affected": "Annual number of people requiring immediate assistance",
-    "Affected per 100,000 people": "Annual rate of people requiring immediate assistance",
-    "Deaths": "Annual number of deaths",
-    "Deaths per 100,000 people": "Annual rate of deaths",
-    "Homeless": "Annual number of people left homeless",
-    "Homeless per 100,000 people": "Annual rate of people left homeless",
-    "Injured": "Annual number of people injured",
-    "Injured per 100,000 people": "Annual rate of people injured",
-    "Total affected": "Annual number of people affected",
-    "Total affected per 100,000 people": "Annual rate of people affected",
-    "Insured damages": "Annual insured damages",
-    "Insured damages as a share of GDP": "Annual insured damages as a share of GDP",
-    "Reconstruction costs": "Annual reconstruction costs",
-    "Reconstruction costs as a share of GDP": "Annual reconstruction costs as a share of GDP",
-    "Total economic damages": "Annual economic damages",
-    "Total economic damages as a share of GDP": "Annual economic damages as a share of GDP",
+TITLE_PUBLIC_START = {
+    "Natural disasters": "Annual number of ",
+    "Natural disasters per 100,000 people": "Annual rate of ",
+    "Affected": "Annual number of people requiring immediate assistance from ",
+    "Affected per 100,000 people": "Annual rate of people requiring immediate assistance from ",
+    "Deaths": "Annual number of deaths from ",
+    "Deaths per 100,000 people": "Annual rate of deaths from ",
+    "Homeless": "Annual number of people left homeless from ",
+    "Homeless per 100,000 people": "Annual rate of people left homeless from ",
+    "Injured": "Annual number of people injured from ",
+    "Injured per 100,000 people": "Annual rate of people injured from ",
+    "Total affected": "Annual number of people affected from ",
+    "Total affected per 100,000 people": "Annual rate of people affected from ",
+    "Insured damages": "Annual insured damages from ",
+    "Insured damages as a share of GDP": "Annual insured damages as a share of GDP from ",
+    "Reconstruction costs": "Annual reconstruction costs from ",
+    "Reconstruction costs as a share of GDP": "Annual reconstruction costs as a share of GDP from ",
+    "Total economic damages": "Annual economic damages from ",
+    "Total economic damages as a share of GDP": "Annual economic damages as a share of GDP from ",
+}
+# Map disaster type to their name at the end of the public title (plural).
+TITLE_PUBLIC_END = {
+    "earthquake": "earthquakes",
+    "extreme_weather": "extreme weather events",
+    "volcanic_activity": "volcanic activity events",
+    "flood": "floods",
+    "all_disasters": "all disasters",
+    "glacial_lake_outburst_flood": "glacial lake outburst floods",
+    "all_disasters_excluding_earthquakes": "all disasters excluding earthquakes",
+    "wildfire": "wildfires",
+    "wet_mass_movement": "wet mass movements",
+    "dry_mass_movement": "dry mass movements",
+    "fog": "fogs",
+    "drought": "droughts",
+    "extreme_temperature": "extreme temperatures",
+    "all_disasters_excluding_extreme_temperature": "all disasters excluding extreme temperatures",
 }
 # Map disaster type to their name in the title (singular).
 TITLE_DISASTER = {
@@ -41,23 +58,6 @@ TITLE_DISASTER = {
     "drought": "Drought",
     "extreme_temperature": "Extreme temperature",
     "all_disasters_excluding_extreme_temperature": "All disasters excluding extreme temperature",
-}
-# Map disaster type to their name in the title (plural).
-TITLE_DISASTER_PLURAL = {
-    "earthquake": "earthquakes",
-    "extreme_weather": "extreme weather events",
-    "volcanic_activity": "volcanic activity",
-    "flood": "floods",
-    "all_disasters": "all disasters",
-    "glacial_lake_outburst_flood": "glacial lake outburst floods",
-    "all_disasters_excluding_earthquakes": "all disasters excluding earthquakes",
-    "wildfire": "wildfires",
-    "wet_mass_movement": "wet mass movements",
-    "dry_mass_movement": "dry mass movements",
-    "fog": "fogs",
-    "drought": "droughts",
-    "extreme_temperature": "extreme temperatures",
-    "all_disasters_excluding_extreme_temperature": "all disasters excluding extreme temperatures",
 }
 
 
@@ -81,9 +81,7 @@ def create_wide_tables(table: Table, is_decade: bool) -> Table:
         old_title = table_wide[column].metadata.title
         _, disaster = column.split("-")
         new_title = old_title + " - " + TITLE_DISASTER[disaster] + variable_title_suffix
-        new_title_public = (
-            f"{variable_title_public_prefix}{TITLE_IMPACT[old_title]} from {TITLE_DISASTER_PLURAL[disaster]}"
-        )
+        new_title_public = f"{variable_title_public_prefix}{TITLE_PUBLIC_START[old_title]}{TITLE_PUBLIC_END[disaster]}"
         table_wide[column].metadata.title = new_title
         table_wide[column].metadata.display = {"name": TITLE_DISASTER[disaster]}
         table_wide[column].metadata.presentation.title_public = new_title_public
