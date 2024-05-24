470ae96fdac717db3855cea49cb7cc5f19e9de15
spoonerf <spoonerfiona@gmail.com>
2024-05-24 12:18:16 +0100

    mental health garden

--- etl/steps/data/garden/ihme_gbd/2024-05-20/gbd_mental_health.py
+++ etl/steps/data/garden/ihme_gbd/2024-05-20/gbd_mental_health.py
@@ -21,26 +21,16 @@ def run(dest_dir: str) -> None:
     # Process data.
     #
     tb = geo.harmonize_countries(df=tb, countries_file=paths.country_mapping_path)
-    # Split into two tables: one for deaths, one for DALYs
-    tb_deaths = tb[tb["measure"] == "Deaths"].copy()
-    tb_dalys = tb[tb["measure"] == "DALYs (Disability-Adjusted Life Years)"].copy()
-    # Shorten the metric name for DALYs
-    tb_dalys["measure"] = "DALYs"
-
-    # Drop the measure column
-    tb_deaths = tb_deaths.drop(columns="measure")
-    tb_dalys = tb_dalys.drop(columns="measure")
 
     # Format the tables
-    tb_deaths = tb_deaths.format(["country", "year", "metric", "age", "cause"], short_name="gbd_cause_deaths")
-    tb_dalys = tb_dalys.format(["country", "year", "metric", "age", "cause"], short_name="gbd_cause_dalys")
+    tb = tb.format(["country", "year", "cause", "metric", "sex", "age"])
 
     #
     # Save outputs.
     #
     # Create a new garden dataset with the same metadata as the meadow dataset.
     ds_garden = create_dataset(
-        dest_dir, tables=[tb_deaths, tb_dalys], check_variables_metadata=True, default_metadata=ds_meadow.metadata
+        dest_dir, tables=[tb], check_variables_metadata=True, default_metadata=ds_meadow.metadata
     )
 
     # Save changes in the new garden dataset.
