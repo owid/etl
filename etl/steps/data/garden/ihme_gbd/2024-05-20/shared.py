f51b0774913098a7ed3d736c51f48718a520c85e
spoonerf <spoonerfiona@gmail.com>
2024-05-28 09:22:12 +0100

    comments

--- etl/steps/data/garden/ihme_gbd/2024-05-20/shared.py
+++ etl/steps/data/garden/ihme_gbd/2024-05-20/shared.py
@@ -18,14 +18,17 @@ def add_regional_aggregates(
 
     For Rate we need to calculate it for each region by dividing the sum of the 'Number' values by the sum of the population.
     """
+    # Split the table into Number, Percent and Rate
     tb_number = tb[tb["metric"].isin(["Number", "Percent"])].copy()
+    tb_rate = tb[tb["metric"] == "Rate"].copy()
+    tb_percent = tb[tb["metric"] == "Percent"].copy()
+    # Add population data
     tb_number = add_population(
         df=tb_number, country_col="country", year_col="year", age_col="age", age_group_mapping=age_group_mapping
     )
-    tb_rate = tb[tb["metric"] == "Rate"].copy()
-    tb_percent = tb[tb["metric"] == "Percent"].copy()
+    # Combine Number and Percent tables
     tb_number_percent = pr.concat([tb_number, tb_percent], ignore_index=True)
-    # Add region aggregates - for Number
+    # Add region aggregates - for Number and Percent (if present)
     tb_number_percent = geo.add_regions_to_table(
         tb_number_percent,
         index_columns=index_cols,
@@ -33,13 +36,15 @@ def add_regional_aggregates(
         ds_regions=ds_regions,
         min_num_values_per_year=1,
     )
-    # Add region aggregates - for Rate - only need population here?
-
+    # Calculate region aggregates - for Rate
     tb_rate_regions = tb_number_percent[
         (tb_number_percent["country"].isin(regions)) & (tb_number_percent["metric"] == "Number")
     ].copy()
+
+    # Calculate rates per 100,000 for regions
     tb_rate_regions["value"] = tb_number["value"] / tb_number["population"] * 100_000
     tb_rate_regions["metric"] = "Rate"
+
     tb_rate = pr.concat([tb_rate, tb_rate_regions], ignore_index=True)
     tb_rate = tb_rate.drop(columns="population")
 
