f277655b12091619975710466318da006a37f7c5
Tuna Acisu <tuna.acisu@ourworldindata.com>
2024-06-03 16:46:53 +0100

    adding germanies together in grapher step

--- etl/steps/data/grapher/smoking/2024-05-30/cigarette_sales.py
+++ etl/steps/data/grapher/smoking/2024-05-30/cigarette_sales.py
@@ -1,9 +1,22 @@
 """Load a garden dataset and create a grapher dataset."""
 
+import owid.catalog.processing as pr
+
 from etl.helpers import PathFinder, create_dataset
 
 # Get paths and naming conventions for current step.
 paths = PathFinder(__file__)
+
+COLS_WITH_DATA = [
+    "manufactured_cigarettes_millions",
+    "manufactured_cigarettes_per_adult_per_day",
+    "handrolled_cigarettes_millions",
+    "handrolled_cigarettes_per_adult_per_day",
+    "total_cigarettes_millions",
+    "total_cigarettes_per_adult_per_day",
+    "all_tobacco_products_tonnes",
+    "all_tobacco_products_grams_per_adult_per_day",
+]
 
 
 def run(dest_dir: str) -> None:
@@ -14,11 +27,15 @@ def run(dest_dir: str) -> None:
     ds_garden = paths.load_dataset("cigarette_sales")
 
     # Read table from garden dataset.
-    tb = ds_garden["cigarette_sales"]
+    tb = ds_garden["cigarette_sales"].reset_index()
 
     #
     # Process data.
-    #
+
+    # Add East and West Germany together to get values for Germany 1945-1990
+    e_ger = tb[tb["country"] == "East Germany"]
+    w_ger = tb[tb["country"] == "West Germany"]
+    germanies_update = pr.concat([e_ger, w_ger])[COLS_WITH_DATA + ["year"]].groupby("year").sum()
 
     #
     # Save outputs.
