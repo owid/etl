9469593d98dc90710eecd0acc96b1670cfa51f31
Pablo Rosado <pabloarosado@gmail.com>
2024-06-06 12:31:33 +0200

    Refactor some definitions

--- apps/step_update/cli.py
+++ apps/step_update/cli.py
@@ -1,7 +1,6 @@
 import difflib
 import sys
 from datetime import datetime
-from enum import Enum
 from pathlib import Path
 from typing import Any, Dict, List, Optional, Tuple, Union
 
@@ -19,90 +18,12 @@ from etl.helpers import (
 from etl.paths import BASE_DIR, DAG_ARCHIVE_FILE, DAG_DIR, DAG_TEMP_FILE, SNAPSHOTS_DIR, STEP_DIR
 from etl.snapshot import SnapshotMeta
 from etl.steps import to_dependency_order
-from etl.version_tracker import DAG_TEMP_STEP, VersionTracker
+from etl.version_tracker import DAG_TEMP_STEP, UpdateState, VersionTracker
 
 log = structlog.get_logger()
 
 # If a new version is not specified, assume current date.
 STEP_VERSION_NEW = datetime.now().strftime("%Y-%m-%d")
-
-
-# Define labels for update states.
-class UpdateState(Enum):
-    UNKNOWN = "Unknown"
-    UP_TO_DATE = "No updates known"
-    OUTDATED = "Outdated"
-    MINOR_UPDATE = "Minor update possible"
-    MAJOR_UPDATE = "Major update possible"
-    ARCHIVABLE = "Archivable"
-
-
-# List of identifiers of steps that should be considered as non-updateable.
-# NOTE: The identifier is the step name without the version (and without the "data://").
-NON_UPDATEABLE_IDENTIFIERS = [
-    # All population-related datasets.
-    "garden/demography/population",
-    "garden/gapminder/population",
-    "garden/hyde/baseline",
-    "garden/un/un_wpp",
-    "meadow/gapminder/population",
-    "meadow/hyde/baseline",
-    "meadow/hyde/general_files",
-    "meadow/un/un_wpp",
-    "open_numbers/open_numbers/gapminder__systema_globalis",
-    "open-numbers/ddf--gapminder--systema_globalis",
-    "snapshot/hyde/general_files.zip",
-    "snapshot/hyde/baseline.zip",
-    "snapshot/gapminder/population.xlsx",
-    "snapshot/un/un_wpp.zip",
-    # Regions dataset.
-    "garden/regions/regions",
-    # Old WB income groups.
-    "garden/wb/wb_income",
-    "meadow/wb/wb_income",
-    "walden/wb/wb_income",
-    # New WB income groups.
-    "garden/wb/income_groups",
-    "meadow/wb/income_groups",
-    "snapshot/wb/income_groups.xlsx",
-    # World Bank country shapes.
-    "snapshot/countries/world_bank.zip",
-    # World Bank WDI.
-    "snapshot/worldbank_wdi/wdi.zip",
-    "meadow/worldbank_wdi/wdi",
-    "garden/worldbank_wdi/wdi",
-    # Other steps we don't want to update (because the underlying data does not get updated).
-    # TODO: We need a better way to achieve this, for example adding update_period_days to all steps and snapshots.
-    #  A simpler alternative would be to move these steps to a separate file in a meaningful place.
-    #  Another option is to have "playlists", e.g. "climate_change_explorer" with the identifiers of steps to update.
-    "meadow/epa/ocean_heat_content",
-    "snapshot/epa/ocean_heat_content_annual_world_700m.csv",
-    "snapshot/epa/ocean_heat_content_annual_world_2000m.csv",
-    "garden/epa/ocean_heat_content",
-    "meadow/epa/ocean_heat_content",
-    "meadow/epa/ice_sheet_mass_balance",
-    "snapshot/epa/ice_sheet_mass_balance.csv",
-    "garden/epa/ice_sheet_mass_balance",
-    "meadow/epa/ice_sheet_mass_balance",
-    "meadow/epa/ghg_concentration",
-    "snapshot/epa/co2_concentration.csv",
-    "snapshot/epa/ch4_concentration.csv",
-    "snapshot/epa/n2o_concentration.csv",
-    "garden/epa/ghg_concentration",
-    "meadow/epa/ghg_concentration",
-    "meadow/epa/mass_balance_us_glaciers",
-    "snapshot/epa/mass_balance_us_glaciers.csv",
-    "garden/epa/mass_balance_us_glaciers",
-    "meadow/epa/mass_balance_us_glaciers",
-    "meadow/climate/antarctic_ice_core_co2_concentration",
-    "snapshot/climate/antarctic_ice_core_co2_concentration.xls",
-    "garden/climate/antarctic_ice_core_co2_concentration",
-    "meadow/climate/antarctic_ice_core_co2_concentration",
-    "meadow/climate/global_sea_level",
-    "snapshot/climate/global_sea_level.csv",
-    "garden/climate/global_sea_level",
-    "meadow/climate/global_sea_level",
-]
 
 
 class StepUpdater:
--- apps/wizard/app_pages/dashboard.py
+++ apps/wizard/app_pages/dashboard.py
@@ -10,7 +10,7 @@ from st_aggrid import AgGrid, GridUpdateMode, JsCode
 from st_aggrid.grid_options_builder import GridOptionsBuilder
 from structlog import get_logger
 
-from apps.step_update.cli import NON_UPDATEABLE_IDENTIFIERS, StepUpdater, UpdateState
+from apps.step_update.cli import StepUpdater, UpdateState
 from etl.config import ADMIN_HOST, ENV
 from etl.db import can_connect
 
@@ -30,6 +30,73 @@ st.set_page_config(
 # Current date.
 # This is used as the default version of new steps to be created.
 TODAY = datetime.now().strftime("%Y-%m-%d")
+
+# List of identifiers of steps that should be considered as non-updateable.
+# NOTE: The identifier is the step name without the version (and without the "data://").
+NON_UPDATEABLE_IDENTIFIERS = [
+    # All population-related datasets.
+    "garden/demography/population",
+    "garden/gapminder/population",
+    "garden/hyde/baseline",
+    "garden/un/un_wpp",
+    "meadow/gapminder/population",
+    "meadow/hyde/baseline",
+    "meadow/hyde/general_files",
+    "meadow/un/un_wpp",
+    "open_numbers/open_numbers/gapminder__systema_globalis",
+    "open-numbers/ddf--gapminder--systema_globalis",
+    "snapshot/hyde/general_files.zip",
+    "snapshot/hyde/baseline.zip",
+    "snapshot/gapminder/population.xlsx",
+    "snapshot/un/un_wpp.zip",
+    # Regions dataset.
+    "garden/regions/regions",
+    # Old WB income groups.
+    "garden/wb/wb_income",
+    "meadow/wb/wb_income",
+    "walden/wb/wb_income",
+    # New WB income groups.
+    "garden/wb/income_groups",
+    "meadow/wb/income_groups",
+    "snapshot/wb/income_groups.xlsx",
+    # World Bank country shapes.
+    "snapshot/countries/world_bank.zip",
+    # World Bank WDI.
+    "snapshot/worldbank_wdi/wdi.zip",
+    "meadow/worldbank_wdi/wdi",
+    "garden/worldbank_wdi/wdi",
+    # Other steps we don't want to update (because the underlying data does not get updated).
+    # TODO: We need a better way to achieve this, for example adding update_period_days to all steps and snapshots.
+    #  A simpler alternative would be to move these steps to a separate file in a meaningful place.
+    #  Another option is to have "playlists", e.g. "climate_change_explorer" with the identifiers of steps to update.
+    "meadow/epa/ocean_heat_content",
+    "snapshot/epa/ocean_heat_content_annual_world_700m.csv",
+    "snapshot/epa/ocean_heat_content_annual_world_2000m.csv",
+    "garden/epa/ocean_heat_content",
+    "meadow/epa/ocean_heat_content",
+    "meadow/epa/ice_sheet_mass_balance",
+    "snapshot/epa/ice_sheet_mass_balance.csv",
+    "garden/epa/ice_sheet_mass_balance",
+    "meadow/epa/ice_sheet_mass_balance",
+    "meadow/epa/ghg_concentration",
+    "snapshot/epa/co2_concentration.csv",
+    "snapshot/epa/ch4_concentration.csv",
+    "snapshot/epa/n2o_concentration.csv",
+    "garden/epa/ghg_concentration",
+    "meadow/epa/ghg_concentration",
+    "meadow/epa/mass_balance_us_glaciers",
+    "snapshot/epa/mass_balance_us_glaciers.csv",
+    "garden/epa/mass_balance_us_glaciers",
+    "meadow/epa/mass_balance_us_glaciers",
+    "meadow/climate/antarctic_ice_core_co2_concentration",
+    "snapshot/climate/antarctic_ice_core_co2_concentration.xls",
+    "garden/climate/antarctic_ice_core_co2_concentration",
+    "meadow/climate/antarctic_ice_core_co2_concentration",
+    "meadow/climate/global_sea_level",
+    "snapshot/climate/global_sea_level.csv",
+    "garden/climate/global_sea_level",
+    "meadow/climate/global_sea_level",
+]
 
 # Define the base URL for the grapher datasets (which will be different depending on the environment).
 GRAPHER_DATASET_BASE_URL = f"{ADMIN_HOST}/admin/datasets/"
