f327f0c3f5ec52275808cdc854f44ad88c39a395
Pablo Rosado <pabloarosado@gmail.com>
2024-06-06 18:12:04 +0200

    Improve documentation

--- apps/utils/start_server.py
+++ apps/utils/start_server.py
@@ -1,14 +1,19 @@
-"""This script creates a git branch, makes an empty commit, and creates a draft pull request.
+"""This script creates a new git branch, makes an empty commit, and creates a draft pull request.
+
+Usually, this script will be executed from the master branch on your local ETL repos, to create a new branch from there.
+But you can also run it from any other existing branch, to create a sub-branch.
 
 The resulting draft PR will be new_branch_name -> base_branch.
 * The new_branch_name is the name of the new branch to be created (that must be given as an input).
-* The base branch will be the current branch, where your ETL repos currently is.
+* The base branch (if not given as an input) will be the current branch, where your ETL repos currently is.
 
 To create a Github token, go to github settings. On the left, at the bottom, choose Developer settings.
 Then, choose "Personal access tokens" and "Tokens (classic)". Click on "Generate new token (classic)".
 Give it a name (e.g. "etl-work"), set an expiration time, and select the scope "repo".
 Then, click on Generate token. Copy the token and save it in your .env file as GITHUB_TOKEN.
 """
+
+from typing import Optional
 
 import click
 import requests
@@ -28,23 +33,26 @@ GITHUB_API_URL = "https://api.github.com/repos/owid/etl/pulls"
 
 @click.command(name="start-server", cls=RichCommand, help=__doc__)
 @click.argument("new-branch-name", type=str)
-def cli(new_branch_name: str) -> None:
+@click.option(
+    "--base-branch", type=str, default=None, help="Name of base branch (if not given, current branch will be used)."
+)
+def cli(new_branch_name: str, base_branch: Optional[str] = None) -> None:
     if not GITHUB_TOKEN:
-        log.error("Define GITHUB_TOKEN in your ETL .env file. " "Then run this tool again.")
+        log.error("Define GITHUB_TOKEN in your ETL .env file. Then run this tool again.")
         return
 
     # Initialize a repos object at the root folder of the etl repos.
     repo = Repo(BASE_DIR)
 
-    # Assume that the current branch will be the the base branch.
-    # The resulting draft PR will be branch_name -> base_branch.
-    base_branch = repo.active_branch.name
+    if base_branch is None:
+        # Assume that the current branch will be the the base branch.
+        # The resulting draft PR will be branch_name -> base_branch.
+        base_branch = repo.active_branch.name
 
     # Ensure the base branch exists in remote.
     origin = repo.remote(name="origin")
     # Update the list of remote branches.
     origin.fetch()
-    # Ensure the base branch exists.
     # NOTE: For some reason, remote_branches includes branches that don't seem to exist anymore in github.
     remote_branches = [ref.name.split("/")[-1] for ref in origin.refs if ref.remote_head != "HEAD"]
     if base_branch not in remote_branches:
@@ -57,9 +65,7 @@ def cli(new_branch_name: str) -> None:
 
     # Ensure the new branch does not already exist.
     if new_branch_name in [branch.name for branch in repo.branches]:
-        log.error(
-            f"Branch {new_branch_name} already exists. " "Choose a different name for the new branch to be created."
-        )
+        log.error(f"Branch {new_branch_name} already exists. Choose a different name for the new branch to be created.")
         return
 
     # Create new branch and switch to it.
