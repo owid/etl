9a7e6fe069e37ae60a9872d43ac97ac74faf12cc
Tuna Acisu <tuna.acisu@ourworldindata.com>
2024-06-10 17:32:35 +0200

    garden step

---
+++ etl/steps/data/garden/happiness/2024-06-09/happiness.py
@@ -0,0 +1,54 @@
+"""Load a meadow dataset and create a garden dataset."""
+
+import owid.catalog.processing as pr
+
+from etl.data_helpers import geo
+from etl.helpers import PathFinder, create_dataset
+
+# Get paths and naming conventions for current step.
+paths = PathFinder(__file__)
+
+
+def run(dest_dir: str) -> None:
+    #
+    # Load inputs.
+    #
+    # Load meadow dataset, previous years and population data.
+    ds_meadow = paths.load_dataset("happiness", version="2024-06-09")
+    ds_prev_years = paths.load_dataset("happiness", channel="garden", version="2023-03-20")
+    ds_population = paths.load_dataset("population", channel="garden")
+
+    # Read table datasets.
+    tb_this_year = ds_meadow["happiness"].reset_index()
+    tb_prev_years = ds_prev_years["happiness"]
+    tb_population = ds_population["population"]
+
+    # combine meadow data with previous years
+    tb_this_year["cantril_ladder_score"] = tb_this_year["ladder_score"]
+    cols_overlap = ["country", "cantril_ladder_score", "year"]
+    tb = pr.concat([tb_this_year[cols_overlap], tb_prev_years], ignore_index=True)
+
+    # Harmonize country names
+    tb = geo.harmonize_countries(
+        df=tb, countries_file=paths.country_mapping_path, excluded_countries_file=paths.excluded_countries_path
+    )
+
+    # Process data.
+    #
+
+    # add population weighted averages
+    tb = geo.add_population_to_dataframe(tb, tb_population)
+    tb = geo.add_regions_to_table(tb, paths.region_mapping_path)
+
+    tb = tb.format(["country", "year"])
+
+    #
+    # Save outputs.
+    #
+    # Create a new garden dataset with the same metadata as the meadow dataset.
+    ds_garden = create_dataset(
+        dest_dir, tables=[tb], check_variables_metadata=True, default_metadata=ds_meadow.metadata
+    )
+
+    # Save changes in the new garden dataset.
+    ds_garden.save()
