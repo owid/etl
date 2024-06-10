738eb63d62b0cb5522401dc98812f0b6969c3b63
veronikasamborska1994 <32176660+veronikasamborska1994@users.noreply.github.com>
2024-06-10 17:31:56 +0200

    paths.load_dependency fix

--- etl/steps/data/grapher/wb/2024-06-10/gender_statistics.py
+++ etl/steps/data/grapher/wb/2024-06-10/gender_statistics.py
@@ -1,9 +1,5 @@
 """Load a garden dataset and create a grapher dataset."""
 
-from typing import cast
-
-from owid.catalog import Dataset
-
 from etl.helpers import PathFinder, create_dataset, grapher_checks
 
 # Get paths and naming conventions for current step.
@@ -15,7 +11,7 @@ def run(dest_dir: str) -> None:
     # Load inputs.
     #
     # Load garden dataset.
-    ds_garden = cast(Dataset, paths.load_dependency("gender_statistics"))
+    ds_garden = paths.load_dependency("gender_statistics")
 
     # Read table from garden dataset.
     tb = ds_garden["gender_statistics"]
