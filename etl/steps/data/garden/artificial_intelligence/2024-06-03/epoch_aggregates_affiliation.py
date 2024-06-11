e8848960692dd7ccd6f6658bcb96b233538e6ed2
veronikasamborska1994 <32176660+veronikasamborska1994@users.noreply.github.com>
2024-06-11 20:06:01 +0100

    AI Epoch - June substantial update 📊 (#2742)

---
+++ etl/steps/data/garden/artificial_intelligence/2024-06-03/epoch_aggregates_affiliation.py
@@ -0,0 +1,66 @@
+""" Generate aggregated table for total yearly and cumulative number of notable AI systems in each category of researcher affiliation."""
+
+from etl.helpers import PathFinder, create_dataset
+
+# Get paths and naming conventions for current step.
+paths = PathFinder(__file__)
+
+
+def run(dest_dir: str) -> None:
+    paths.log.info("epoch_aggregates_affiliation.start")
+
+    #
+    # Load inputs.
+    #
+    # Load the the garden dataset without aggregations.
+    ds_garden = paths.load_dataset("epoch")
+
+    # Read table from meadow dataset.
+    tb = ds_garden["epoch"]
+    tb = tb.reset_index()
+
+    # Define the columns that are not needed
+    unused_columns = [
+        "days_since_1949",
+        "parameters",
+        "training_dataset_size__datapoints",
+        "domain",
+        "training_computation_petaflop",
+    ]
+    # Store the origins metadata for later use
+    origins = tb["organization_categorization"].metadata.origins
+
+    # Drop the unused columns
+    tb = tb.drop(unused_columns, axis=1)
+
+    # Ensure 'publication_date' column type is datetime64
+    assert tb["publication_date"].dtype == "datetime64[ns]", "publication_date column is not of type datetime64"
+    tb["year"] = tb["publication_date"].dt.year
+
+    # Store the origins metadata for later use
+    origins = tb["organization_categorization"].metadata.origins
+
+    # Group by year and country and count the number of systems
+    tb_agg = tb.groupby(["year", "organization_categorization"], observed=False).size().reset_index(name="yearly_count")
+
+    # Add the origins metadata to the 'number_of_systems' column
+    tb_agg["yearly_count"].metadata.origins = origins
+
+    # Calculate the cumulative count
+    tb_agg["cumulative_count"] = tb_agg.groupby("organization_categorization", observed=False)["yearly_count"].cumsum()
+
+    # Add the origins metadata to the columns
+    for col in ["yearly_count", "cumulative_count"]:
+        tb_agg[col].metadata.origins = origins
+
+    # Set the short_name metadata of the table
+    tb_agg.metadata.short_name = paths.short_name
+
+    # Set the index to year and country
+    tb_agg = tb_agg.format(["year", "organization_categorization"])
+
+    # Save outputs.
+    ds_garden = create_dataset(dest_dir, tables=[tb_agg])
+    ds_garden.save()
+
+    paths.log.info("epoch_aggregates_affiliation.end")
--- etl/steps/data/grapher/artificial_intelligence/2024-06-03/epoch_aggregates_countries.py
+++ etl/steps/data/grapher/artificial_intelligence/2024-06-03/epoch_aggregates_countries.py
@@ -1,9 +1,5 @@
 """Load a garden dataset and create a grapher dataset."""
 
-from typing import cast
-
-from owid.catalog import Dataset
-
 from etl.helpers import PathFinder, create_dataset, grapher_checks
 
 # Get paths and naming conventions for current step.
@@ -15,23 +11,16 @@ def run(dest_dir: str) -> None:
     # Load inputs.
     #
     # Load garden dataset.
-    ds_garden = cast(Dataset, paths.load_dependency("epoch_aggregates"))
+    ds_garden = paths.load_dataset("epoch_aggregates_countries")
 
     # Read table from garden dataset.
-    tb = ds_garden["epoch"]
-    tb = tb.reset_index()
-    #
-    # Process data.
-    #
-    # Rename for plotting model name as country in grapher
-    tb["country"] = "Total"
-    tb = tb.set_index(["country", "year"])
-
+    tb_garden = ds_garden["epoch_aggregates_countries"]
+    print(tb_garden)
     #
     # Save outputs.
     #
     # Create a new grapher dataset with the same metadata as the garden dataset.
-    ds_grapher = create_dataset(dest_dir, tables=[tb], default_metadata=ds_garden.metadata)
+    ds_grapher = create_dataset(dest_dir, tables=[tb_garden], default_metadata=ds_garden.metadata)
 
     #
     # Checks.
