94fb6f67413cf738a39102667fcacba1714be6c4
veronikasamborska1994 <32176660+veronikasamborska1994@users.noreply.github.com>
2024-06-17 17:14:34 +0200

    intermidiate work on metadata

--- etl/steps/data/garden/unesco/2024-06-16/education_opri.py
+++ etl/steps/data/garden/unesco/2024-06-16/education_opri.py
@@ -1,5 +1,9 @@
 """Load a meadow dataset and create a garden dataset."""
 
+import re
+
+from tqdm import tqdm
+
 from etl.data_helpers import geo
 from etl.helpers import PathFinder, create_dataset
 
@@ -23,20 +27,147 @@ def run(dest_dir: str) -> None:
     tb = geo.harmonize_countries(
         df=tb, countries_file=paths.country_mapping_path, excluded_countries_file=paths.excluded_countries_path
     )
-    tb = tb.pivot(index=["country", "year"], columns="indicator_label_en", values="value")
-
-    tb = tb.reset_index()
-    tb = tb.format(["country", "year"])
+    tb_pivoted = tb.pivot(index=["country", "year"], columns="indicator_label_en", values="value")
 
     # TO DO: Add metadata columns to the table
+    summary_dict = create_metadata_dictionary(tb)
+    tb_pivoted = add_metadata_description(tb_pivoted, summary_dict)
+    # Add metadata columns to the table
+    tb_pivoted = tb_pivoted.reset_index()
+    tb_pivoted = tb_pivoted.format(["country", "year"])
 
     #
     # Save outputs.
     #
     # Create a new garden dataset with the same metadata as the meadow dataset.
     ds_garden = create_dataset(
-        dest_dir, tables=[tb], check_variables_metadata=True, default_metadata=ds_meadow.metadata
+        dest_dir, tables=[tb_pivoted], check_variables_metadata=True, default_metadata=ds_meadow.metadata
     )
 
     # Save changes in the new garden dataset.
     ds_garden.save()
+
+
+def create_metadata_dictionary(tb):
+    # Define the mapping for the 'qualifier' column
+    qualifier_mapping = {"NAT_EST": "National", "UIS_EST": "UIS"}
+
+    # Replace the entries in the 'qualifier' column
+    tb["qualifier"] = tb["qualifier"].astype(str).replace(qualifier_mapping)
+    tb = tb.drop("magnitude", axis=1)
+
+    # Generate the summary string for metadata columns for each indicator
+    metadata_columns = [
+        "qualifier",
+        "change__data_reporting",
+        "source__data_sources",
+        "under_coverage__students_or_individuals",
+    ]
+    summary_dict = {}
+
+    for indicator in tqdm(tb["indicator_label_en"].unique(), desc="Processing indicators"):
+        tb_indicator = tb[tb["indicator_label_en"] == indicator]
+        summary_strings = {}
+        for col in metadata_columns:
+            if col == "qualifier":
+                meta = "National or UIS estimate:"
+            elif col == "source__data_sources":
+                meta = "Data sources:"
+            elif col == "change__data_reporting":
+                meta = "Change in data reporting:"
+            elif col == "under_coverage__students_or_individuals":
+                meta = "Under coverage:"
+            unique_values = tb_indicator[col].astype(str).dropna().unique()
+            if not all(value == "nan" for value in unique_values):
+                summary_per_unique_value = {}
+                for value in unique_values:
+                    if value != "nan":
+                        countries_years = (
+                            tb_indicator[tb_indicator[col] == value].groupby(["country", "year"], observed=False).size()
+                        )
+                        unique_countries = (
+                            countries_years[countries_years.values == 1].index.get_level_values(0).unique().tolist()
+                        )
+                        summary_per_unique_country = {}
+                        for country in unique_countries:
+                            years = countries_years.loc[country]
+                            years_list = years[years.values == 1].index.get_level_values(0).tolist()
+                            summary_per_unique_country[country] = years_list
+                        summary_per_unique_value[value] = summary_per_unique_country
+                if summary_per_unique_value:
+                    summary_strings[meta] = summary_per_unique_value
+        summary_dict[indicator] = summary_strings
+    return summary_dict
+
+
+def add_metadata_description(tb, summary_dict):
+    for column in tb.columns:
+        meta = tb[column].metadata
+
+        # Use the function
+        md_table = dict_to_string(summary_dict[column])
+        meta.description_from_producer = md_table
+        meta.title = column
+        meta.processing = "minor"
+        meta.display = {}
+
+        # Convert the 'name' variable to lowercase to make text matching easier
+        name_lower = tb[column].title.lower()
+
+        #
+        # Update metadata units, short_units and number of decimal places to display depending on what keywords the variable name contains
+        #
+        # Fill out units and decimal places
+        if "%" in name_lower:
+            update_metadata(meta, display_decimals=1, unit="%", short_unit="%")
+        elif "(days)" in name_lower:
+            update_metadata(meta, display_decimals=1, unit="days", short_unit="")
+        elif "index" in name_lower:
+            update_metadata(meta, display_decimals=1, unit="index", short_unit="")
+        elif "index" in name_lower:
+            update_metadata(meta, display_decimals=1, unit="index", short_unit="")
+        elif "(current us$)" in name_lower:
+            update_metadata(meta, display_decimals=1, unit="current US$", short_unit="current $")
+        elif "number" in name_lower:
+            update_metadata(meta, display_decimals=0, unit="number", short_unit="")
+
+        else:
+            # Default metadata update when no other conditions are met.
+            update_metadata(meta, 0, " ", " ")
+    return tb
+
+
+def update_metadata(meta, display_decimals, unit, short_unit):
+    """
+    Update metadata attributes of a specified column in the given table.
+
+    Args:
+    meta (obj): Metadata object of the column to update.
+    display_decimals (int): Number of decimal places to display.
+    unit (str): The full name of the unit of measurement for the column data.
+    short_unit (str, optional): The abbreviated form of the unit. Defaults to an empty space.
+
+    Returns:
+    None: The function updates the table in-place.
+    """
+    meta.display["numDecimalPlaces"] = display_decimals
+    meta.unit = unit
+    meta.short_unit = short_unit
+
+
+def dict_to_string(d):
+    if not d:
+        return "No metadata."
+    else:
+        paragraphs = []
+        for key, nested_dict in d.items():
+            paragraph = f"**{key}**\n"
+            paragraph += "\n".join(
+                f"**{nested_key}** - {nested_value}+\n" for nested_key, nested_value in nested_dict.items()
+            )
+            paragraph = paragraph.replace("(", "").replace(")", "") + "\n\n"
+            paragraph = re.sub(r"[\[\]']+", "", paragraph)
+            paragraph = paragraph.replace("{", "").replace("}", "")
+
+            paragraphs.append(paragraph)
+        return "\n\n".join(paragraphs)
