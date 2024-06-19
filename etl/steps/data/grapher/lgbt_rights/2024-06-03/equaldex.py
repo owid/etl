769881f7a88295596da0c53af96fe97a06896357
lucasrodes <lucasrodes@users.noreply.github.com>
2024-06-20 00:40:56 +0200

    fix step

--- etl/steps/data/grapher/lgbt_rights/2024-06-03/equaldex.py
+++ etl/steps/data/grapher/lgbt_rights/2024-06-03/equaldex.py
@@ -1,6 +1,5 @@
 """Load a garden dataset and create a grapher dataset."""
 
-from etl.data_helpers import geo
 from etl.helpers import PathFinder, create_dataset, grapher_checks
 
 # Get paths and naming conventions for current step.
@@ -13,19 +12,10 @@ def run(dest_dir: str) -> None:
     #
     # Load garden dataset.
     ds_garden = paths.load_dataset("equaldex")
-    ds_regions = paths.load_dataset("regions")
-    ds_population = paths.load_dataset("population")
 
     # Read table from garden dataset.
     tb_garden = ds_garden["equaldex"].reset_index()
 
-    countries_europe = geo.list_members_of_region("Europe", ds_regions)
-
-    tb_garden["country"] = tb_garden["country"].astype("string")
-    tb_garden = geo.add_population_to_table(tb_garden, ds_population)
-
-    tb_garden = tb_garden.loc[tb_garden.year == 2024, ["country", "year", "ei_legal", "population"]]
-    tb_garden = tb_garden[tb_garden.country.isin(countries_europe)]
     #
     # Save outputs.
     #
