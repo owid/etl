808cfdca67df4a4e36c0c6cd30d9fe742837b123
lucasrodes <lucasrodes@users.noreply.github.com>
2024-06-20 14:32:08 +0200

    fix

--- etl/steps/data/meadow/neglected_tropical_diseases/2024-05-18/funding.py
+++ etl/steps/data/meadow/neglected_tropical_diseases/2024-05-18/funding.py
@@ -29,7 +29,7 @@ def run(dest_dir: str) -> None:
     # Drop 'FY'from the year column
     tb["Year"] = tb["Year"].str.replace("FY ", "").astype(int)
     # Set index as disease and year, there are plenty of duplicates but we will aggregate in garden.
-    tb = tb.format(["disease", "Year"])
+    tb = tb.format(["disease", "year"])
 
     #
     # Save outputs.
