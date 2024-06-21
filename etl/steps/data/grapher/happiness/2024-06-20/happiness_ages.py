bffd82df086879ce825207be65ab31c569438e4e
Tuna Acisu <tuna.acisu@ourworldindata.com>
2024-06-21 15:59:08 +0200

    pivot table for grapher & add origins

--- etl/steps/data/grapher/happiness/2024-06-20/happiness_ages.py
+++ etl/steps/data/grapher/happiness/2024-06-20/happiness_ages.py
@@ -1,9 +1,42 @@
 """Load a garden dataset and create a grapher dataset."""
 
-from etl.helpers import PathFinder, create_dataset
+import pandas as pd
+
+from etl.helpers import PathFinder, Table, create_dataset
 
 # Get paths and naming conventions for current step.
 paths = PathFinder(__file__)
+
+
+def pivot_age_groups(tb):
+    new_tb_rows = []
+    for cty in tb["country"].unique():
+        cty_tb = tb[tb["country"] == cty]
+        for year in cty_tb["year"].unique():
+            new_row_dict = {"country": cty, "year": year}
+            row_tb = cty_tb[cty_tb["year"] == year]
+            new_row_dict["happiness_below_30"] = row_tb[row_tb["age_group"] == "below 30"]["happiness_score"].values[0]
+            new_row_dict["happiness_30_to_44"] = row_tb[row_tb["age_group"] == "30-44"]["happiness_score"].values[0]
+            new_row_dict["happiness_45_to_59"] = row_tb[row_tb["age_group"] == "45-59"]["happiness_score"].values[0]
+            new_row_dict["happiness_60_and_above"] = row_tb[row_tb["age_group"] == "60 and above"][
+                "happiness_score"
+            ].values[0]
+            new_tb_rows.append(new_row_dict)
+    tb_pivot = Table(
+        pd.DataFrame(
+            new_tb_rows,
+            columns=[
+                "country",
+                "year",
+                "happiness_below_30",
+                "happiness_30_to_44",
+                "happiness_45_to_59",
+                "happiness_60_and_above",
+            ],
+        )
+    )
+    tb_pivot = tb_pivot.copy_metadata(tb)
+    return tb_pivot
 
 
 def run(dest_dir: str) -> None:
@@ -14,11 +47,16 @@ def run(dest_dir: str) -> None:
     ds_garden = paths.load_dataset("happiness_ages")
 
     # Read table from garden dataset.
-    tb = ds_garden["happiness_ages"]
+    tb = ds_garden["happiness_ages"].reset_index()
 
     #
     # Process data.
     #
+    tb = tb.drop(columns=["population"])
+
+    tb = pivot_age_groups(tb)
+
+    tb = tb.format(["country", "year"])
 
     #
     # Save outputs.
