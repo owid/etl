9b1d5a5ba9b34f7f84042da95f0fa03ab2c6cd3e
veronikasamborska1994 <32176660+veronikasamborska1994@users.noreply.github.com>
2024-06-22 15:30:24 +0200

    added types

--- etl/steps/data/garden/unesco/2024-06-16/education_opri.py
+++ etl/steps/data/garden/unesco/2024-06-16/education_opri.py
@@ -1,5 +1,5 @@
 """Load a meadow dataset and create a garden dataset."""
-from typing import Any, Dict
+from owid.catalog import VariableMeta
 
 from etl.data_helpers import geo
 from etl.helpers import PathFinder, create_dataset
@@ -46,29 +46,29 @@ def run(dest_dir: str) -> None:
     # Pivot the table to have the indicators as columns to add descriptions from producer
     tb_pivoted = tb.pivot(index=["country", "year"], columns="indicator_label_en", values="value")
     for column in tb_pivoted.columns:
-        metadata = tb_pivoted[column].metadata
-        metadata.display = {}
+        meta = tb_pivoted[column].metadata
+        meta.display = {}
         long_definition = tb["long_description"].loc[tb["indicator_label_en"] == column].iloc[0]
-        metadata.description_from_producer = long_definition
-        metadata.title = column
+        meta.description_from_producer = long_definition
+        meta.title = column
         #
         # Update metadata units, short_units and number of decimal places to display depending on what keywords the variable name contains
         #
         # Fill out units and decimal places
         if "%" in column.lower():
-            update_metadata(metadata, display_decimals=1, unit="%", short_unit="%")
+            update_metadata(meta, display_decimals=1, unit="%", short_unit="%")
         elif "(days)" in column.lower():
-            update_metadata(metadata, display_decimals=1, unit="days", short_unit="")
+            update_metadata(meta, display_decimals=1, unit="days", short_unit="")
         elif "index" in column.lower():
-            update_metadata(metadata, display_decimals=1, unit="index", short_unit="")
+            update_metadata(meta, display_decimals=1, unit="index", short_unit="")
         elif "(current us$)" in column.lower():
-            update_metadata(metadata, display_decimals=1, unit="current US$", short_unit="current $")
+            update_metadata(meta, display_decimals=1, unit="current US$", short_unit="current $")
         elif "(number)" in column.lower():
-            update_metadata(metadata, display_decimals=0, unit="number", short_unit="")
+            update_metadata(meta, display_decimals=0, unit="number", short_unit="")
 
         else:
             # Default metadata update when no other conditions are met.
-            update_metadata(metadata, 0, " ", " ")
+            update_metadata(meta, 0, " ", " ")
 
     tb_pivoted = tb_pivoted.reset_index()
     tb_pivoted = tb_pivoted.format(["country", "year"])
@@ -85,12 +85,12 @@ def run(dest_dir: str) -> None:
     ds_garden.save()
 
 
-def update_metadata(meta: Dict[str, Any], display_decimals: int, unit: str, short_unit: str) -> None:
+def update_metadata(meta: VariableMeta, display_decimals: int, unit: str, short_unit: str) -> None:
     """
     Update metadata attributes of a specified column in the given table.
 
     Args:
-    meta (Dict[str, Any]): Metadata object of the column to update.
+    meta (obj): Metadata object of the column to update.
     display_decimals (int): Number of decimal places to display.
     unit (str): The full name of the unit of measurement for the column data.
     short_unit (str, optional): The abbreviated form of the unit. Defaults to an empty space.
@@ -98,6 +98,6 @@ def update_metadata(meta: Dict[str, Any], display_decimals: int, unit: str, shor
     Returns:
     None: The function updates the table in-place.
     """
-    meta["display"]["numDecimalPlaces"] = display_decimals
-    meta["unit"] = unit
-    meta["short_unit"] = short_unit
+    meta.display["numDecimalPlaces"] = display_decimals
+    meta.unit = unit
+    meta.short_unit = short_unit
