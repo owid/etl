8318521590d8c2a405fc8bd00024a4a720b6e998
spoonerf <spoonerfiona@gmail.com>
2024-06-26 12:32:39 +0100

    adding extra age-groups

--- etl/steps/data/garden/ihme_gbd/2024-05-20/gbd_child_mortality.py
+++ etl/steps/data/garden/ihme_gbd/2024-05-20/gbd_child_mortality.py
@@ -1,6 +1,7 @@
 """Load a meadow dataset and create a garden dataset."""
 
 
+from owid.catalog import Table
 from owid.catalog import processing as pr
 
 from etl.data_helpers import geo
@@ -38,6 +39,7 @@ def run(dest_dir: str) -> None:
     tb = pr.concat([tb_number, tb_other])
     # Split into two tables: one for deaths, one for DALYs
     tb_deaths = tb[tb["measure"] == "Deaths"].copy()
+    tb_deaths = add_additional_age_groups(tb_deaths)
     tb_dalys = tb[tb["measure"] == "DALYs (Disability-Adjusted Life Years)"].copy()
     # Shorten the metric name for DALYs
     tb_dalys["measure"] = "DALYs"
@@ -64,3 +66,34 @@ def run(dest_dir: str) -> None:
 
     # Save changes in the new garden dataset.
     ds_garden.save()
+
+
+def add_additional_age_groups(tb: Table) -> Table:
+    """
+    Adding additional age groups to the table, 28-264 days and 1-4 years. These age-groups were previously supplied but IHME, but no longer are.
+    """
+    tb_number = tb[tb["metric"] == "Number"]
+    # calculating 28-364 days
+    tb_neonatal = tb_number[tb_number["age"] == "<28 days"]
+    tb_infant = tb_number[tb_number["age"] == "<1 year"]
+
+    tb_diff = tb_neonatal.merge(
+        tb_infant, on=["country", "year", "sex", "cause", "measure", "metric"], suffixes=("_neonatal", "_infant")
+    )
+    tb_diff["age"] = "28-364 days"
+    tb_diff["value"] = tb_diff["value_infant"] - tb_diff["value_neonatal"]
+
+    # Calculating 1-4 years
+    tb_child = tb_number[tb_number["age"] == "<5 years"]
+
+    tb_1_4 = tb_child.merge(
+        tb_infant, on=["country", "year", "sex", "cause", "measure", "metric"], suffixes=("_child", "_infant")
+    )
+    tb_1_4["age"] = "1-4 years"
+    tb_1_4["value"] = tb_1_4["value_child"] - tb_1_4["value_infant"]
+
+    tb = pr.concat([tb, tb_diff, tb_1_4])
+    # remove columns ending in _neonatal, _child and _infant
+    tb = tb.drop(columns=tb.filter(like="_").columns)
+
+    return tb
