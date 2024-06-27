221fd436fdddecff8b745a97ef66301a4e7eb077
spoonerf <spoonerfiona@gmail.com>
2024-06-27 13:27:21 +0100

    adding infant mort to grapher

--- etl/steps/data/grapher/ihme_gbd/2024-05-20/gbd_child_mortality.py
+++ etl/steps/data/grapher/ihme_gbd/2024-05-20/gbd_child_mortality.py
@@ -17,13 +17,13 @@ def run(dest_dir: str) -> None:
     tb_dalys = ds_garden["gbd_child_mortality_dalys"]
     tb_deaths_slope = ds_garden["gbd_child_mortality_slope"]
     tb_india = ds_garden["gbd_child_mortality_india"]
-
-    # Save outputs.
+    tb_infant = ds_garden["gbd_child_mortality_infant"]
+    # Save outputs .
     #
     # Create a new grapher dataset with the same metadata as the garden dataset.
     ds_grapher = create_dataset(
         dest_dir,
-        tables=[tb_deaths, tb_dalys, tb_deaths_slope, tb_india],
+        tables=[tb_deaths, tb_dalys, tb_deaths_slope, tb_india, tb_infant],
         check_variables_metadata=True,
         default_metadata=ds_garden.metadata,
     )
