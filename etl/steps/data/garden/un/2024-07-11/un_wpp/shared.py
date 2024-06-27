28c877d8730b25c3310e4c120ad481cc829dd52c
lucasrodes <lucasrodes@users.noreply.github.com>
2024-06-27 18:27:24 +0200

    enable non-strict mode

--- etl/steps/data/garden/un/2024-07-11/un_wpp/shared.py
+++ etl/steps/data/garden/un/2024-07-11/un_wpp/shared.py
@@ -3,15 +3,16 @@ from typing import Dict
 from owid.catalog import Table
 
 
-def harmonize_dimension(tb: Table, column_name: str, mapping: Dict[str, str]) -> Table:
+def harmonize_dimension(tb: Table, column_name: str, mapping: Dict[str, str], strict: bool = True) -> Table:
     """Harmonize a dimension in a table using a mapping.
 
     tb: Table to harmonize.
     column_name: Column name to harmonize.
     mapping: Mapping to harmonize the column.
     """
-    # Assert column_name does not contain any other column but those in mapping
-    assert set(tb[column_name].unique()) == set(mapping.keys())
+    if strict:
+        # Assert column_name does not contain any other column but those in mapping
+        assert set(tb[column_name].unique()) == set(mapping.keys())
 
     # Replace values in column_name
     tb[column_name] = tb[column_name].replace(mapping)
