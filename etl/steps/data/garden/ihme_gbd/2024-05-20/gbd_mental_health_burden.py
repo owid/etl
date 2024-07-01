0bfca2acd13aa76fc8fe396e8015d6286f8414b0
spoonerf <spoonerfiona@gmail.com>
2024-07-01 16:34:24 +0100

    sorting metadata

--- etl/steps/data/garden/ihme_gbd/2024-05-20/gbd_mental_health_burden.py
+++ etl/steps/data/garden/ihme_gbd/2024-05-20/gbd_mental_health_burden.py
@@ -29,7 +29,11 @@ def run(dest_dir: str) -> None:
 
     tb_entities = diseases_as_entities(tb)
     tb_entities = tb_entities.format(["country", "year"], short_name="gbd_mental_health_burden_entities")
-    #
+    # fixing the metadata which has got lost somewhere
+
+    for col in tb_entities.columns:
+        print(col)
+        tb_entities[col] = tb_entities[col].copy_metadata(tb["value"])
     # Save outputs.
     #
     # Create a new garden dataset with the same metadata as the meadow dataset.
@@ -50,12 +54,12 @@ def diseases_as_entities(tb: Table) -> Table:
     """
     Save the diseases as entities, and measure as variable, for use in this chart: https://ourworldindata.org/grapher/estimated-prevalence-vs-burden-mental-illnesses
     """
-    tb_ent = tb[(tb["country"] == "World") & (tb["age"] == "All ages")]
+    tb_ent = tb[(tb["country"] == "World") & (tb["age"] == "All ages")].copy()
 
     tb_ent = tb_ent.pivot_table(index=["cause", "year"], columns=["measure", "metric"], values="value").reset_index()
     tb_ent.columns = ["_".join(filter(None, col)).strip() for col in tb_ent.columns.values]
     tb_ent = tb_ent.rename(columns={"cause": "country"})
     tb_ent = tb_ent.drop(columns="DALYs (Disability-Adjusted Life Years)_Share")
 
-    tb_ent = tb_ent.copy_metadata(tb)
+    # tb_ent = tb_ent.copy_metadata(tb)
     return tb_ent
