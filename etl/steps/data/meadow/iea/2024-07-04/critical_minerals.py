e49ca60fdde9f33c9cfc8a5c49e2b55bdc201c92
Pablo Rosado <pabloarosado@gmail.com>
2024-07-04 16:26:34 +0200

    Parse more sheets in the meadow step

--- etl/steps/data/meadow/iea/2024-07-04/critical_minerals.py
+++ etl/steps/data/meadow/iea/2024-07-04/critical_minerals.py
@@ -19,23 +19,24 @@ def drop_all_rows_of_notes(tb: Table) -> Table:
     return tb
 
 
-def separate_minerals_and_technology(tb: Table) -> Table:
+def transform_header_rows_into_new_column(tb: Table, header_row_name: str, normal_row_name: str) -> Table:
     tb = tb.copy()
     # Now, the first column contains the name of the mineral (with no data in other columns) followed by the technology where the mineral is relevant.
     # Create a new column for the mineral.
-    tb["mineral"] = None
+    tb[header_row_name] = None
     # Identify the rows for each mineral (which are the ones for which all other columns are empty).
-    mineral_mask = (tb["technology"].notnull()) & (tb[tb.columns[1]].isnull())
-    tb.loc[mineral_mask, "mineral"] = tb[mineral_mask]["technology"]
-    tb["mineral"] = tb["mineral"].ffill()
+    mineral_mask = (tb[normal_row_name].notnull()) & (tb[tb.columns[1]].isnull())
+    tb.loc[mineral_mask, header_row_name] = tb[mineral_mask][normal_row_name]
+    tb[header_row_name] = tb[header_row_name].ffill()
     # Remove the rows where the minerals were.
     tb = tb[~mineral_mask].reset_index(drop=True)
 
     return tb
 
 
-def process_demand_for_key_minerals(data: pr.ExcelFile) -> Table:
-    sheet_name = "1 Total demand for key minerals"
+def process_demand_for_key_minerals(
+    data: pr.ExcelFile, sheet_name: str, header_row_name: str, normal_row_name: str, tb_short_name: str
+) -> Table:
     tb = data.parse(sheet_name)
 
     scenarios = {
@@ -53,7 +54,7 @@ def process_demand_for_key_minerals(data: pr.ExcelFile) -> Table:
         + "__"
         + tb.iloc[3].astype("Int64").astype(object).fillna("").astype(str).values
     )
-    new_columns[0] = "technology"
+    new_columns[0] = normal_row_name
     new_columns[1] = f"current{new_columns[1]}"
     tb = tb.rename(columns={tb.columns[i]: column for i, column in enumerate(new_columns)}, errors="raise")
 
@@ -70,21 +71,21 @@ def process_demand_for_key_minerals(data: pr.ExcelFile) -> Table:
     tb = drop_all_rows_of_notes(tb=tb)
 
     # Separate minerals and technology in different columns.
-    tb = separate_minerals_and_technology(tb=tb)
+    tb = transform_header_rows_into_new_column(tb=tb, header_row_name=header_row_name, normal_row_name=normal_row_name)
 
     # Transform table to end up with another table that has a "year" and "scenario" columns.
     scenarios_names = ["current"] + [scenario for scenario in scenarios.values() if scenario != ""]
     tables = []
     for scenario in scenarios_names:
-        _tb = tb[["mineral", "technology"] + [column for column in tb.columns if column.startswith(scenario)]]
+        _tb = tb[[header_row_name, normal_row_name] + [column for column in tb.columns if column.startswith(scenario)]]
         _tb = _tb.rename(columns={column: column.replace(f"{scenario}__", "") for column in _tb.columns})
-        _tb = _tb.melt(id_vars=["mineral", "technology"], value_name="demand", var_name="year")
+        _tb = _tb.melt(id_vars=[header_row_name, normal_row_name], value_name="demand", var_name="year")
         _tb = _tb.assign(**{"scenario": scenario})
         tables.append(_tb)
-    tb = pr.concat(tables, short_name="demand_for_key_minerals")
+    tb = pr.concat(tables, short_name=tb_short_name)
 
     # Ensure all columns are snake-case, set an appropriate index, and sort conveniently.
-    tb = tb.format(["mineral", "technology", "year", "scenario"])
+    tb = tb.format([header_row_name, normal_row_name, "year", "scenario"])
 
     return tb
 
@@ -101,7 +102,7 @@ def process_supply_for_key_minerals(data: pr.ExcelFile) -> Table:
     def _prepare_sub_table(_tb: Table) -> Table:
         _tb = _tb.copy()
         new_columns = _tb.iloc[2].astype("Int64").astype(object).fillna("").astype(str).values
-        new_columns[0] = "technology"
+        new_columns[0] = "country"
         _tb = _tb.rename(columns={_tb.columns[i]: column for i, column in enumerate(new_columns)}, errors="raise")
         _tb = _tb.drop([0, 1, 2, 3]).reset_index(drop=True)
         _tb = drop_all_rows_of_notes(tb=_tb)
@@ -116,16 +117,16 @@ def process_supply_for_key_minerals(data: pr.ExcelFile) -> Table:
     tb = pr.concat([_tb_1, _tb_2])
 
     # All predictions seems to be of the "Base case" scenario.
-    tb["scenario"] = "base_case"
+    tb["case"] = "base_case"
 
     # Separate minerals and technology in different columns.
-    tb = separate_minerals_and_technology(tb=tb)
+    tb = transform_header_rows_into_new_column(tb=tb, header_row_name="mineral_process", normal_row_name="country")
 
     # Reformat table to have "year" and "supply" columns.
-    tb = tb.melt(id_vars=["mineral", "technology", "scenario"], value_name="supply", var_name="year")
+    tb = tb.melt(id_vars=["mineral_process", "country", "case"], value_name="supply", var_name="year")
 
     # Ensure all columns are snake-case, set an appropriate index, and sort conveniently.
-    tb = tb.format(["mineral", "technology", "year", "scenario"])
+    tb = tb.format(["country", "year", "mineral_process", "case"])
 
     return tb
 
@@ -144,8 +145,59 @@ def run(dest_dir: str) -> None:
     # Process data.
     #
     # Process each table.
-    tb_demand_for_key_minerals = process_demand_for_key_minerals(data=data)
+    tb_demand_for_key_minerals = process_demand_for_key_minerals(
+        data=data,
+        sheet_name="1 Total demand for key minerals",
+        header_row_name="mineral",
+        normal_row_name="technology",
+        tb_short_name="demand_for_key_minerals",
+    )
     tb_supply = process_supply_for_key_minerals(data=data)
+    tb_demand_for_clean_energy_technologies = process_demand_for_key_minerals(
+        data=data,
+        sheet_name="3.1 Cleantech demand by tech",
+        header_row_name="mineral",
+        normal_row_name="technology",
+        tb_short_name="demand_for_clean_energy_technologies",
+    )
+    tb_demand_for_solar_pv = process_demand_for_key_minerals(
+        data=data,
+        sheet_name="4.1 Solar PV",
+        header_row_name="case",
+        normal_row_name="mineral",
+        tb_short_name="demand_for_solar_pv",
+    )
+    tb_demand_for_wind = process_demand_for_key_minerals(
+        data=data,
+        sheet_name="4.2 Wind",
+        header_row_name="case",
+        normal_row_name="mineral",
+        tb_short_name="demand_for_wind",
+    )
+    tb_demand_for_ev = process_demand_for_key_minerals(
+        data=data, sheet_name="4.3 EV", header_row_name="case", normal_row_name="mineral", tb_short_name="demand_for_ev"
+    )
+    tb_demand_for_battery_storage = process_demand_for_key_minerals(
+        data=data,
+        sheet_name="4.4 Battery storage",
+        header_row_name="case",
+        normal_row_name="mineral",
+        tb_short_name="demand_for_battery_storage",
+    )
+    tb_demand_for_electricity_networks = process_demand_for_key_minerals(
+        data=data,
+        sheet_name="4.5 Electricity networks",
+        header_row_name="case",
+        normal_row_name="mineral",
+        tb_short_name="demand_for_electricity_networks",
+    )
+    tb_demand_for_hydrogen = process_demand_for_key_minerals(
+        data=data,
+        sheet_name="4.6 Hydrogen",
+        header_row_name="case",
+        normal_row_name="mineral",
+        tb_short_name="demand_for_hydrogen",
+    )
 
     #
     # Save outputs.
@@ -153,7 +205,17 @@ def run(dest_dir: str) -> None:
     # Create a new meadow dataset with the same metadata as the snapshot.
     ds_meadow = create_dataset(
         dest_dir,
-        tables=[tb_demand_for_key_minerals, tb_supply],
+        tables=[
+            tb_demand_for_key_minerals,
+            tb_supply,
+            tb_demand_for_clean_energy_technologies,
+            tb_demand_for_solar_pv,
+            tb_demand_for_wind,
+            tb_demand_for_ev,
+            tb_demand_for_battery_storage,
+            tb_demand_for_electricity_networks,
+            tb_demand_for_hydrogen,
+        ],
         check_variables_metadata=True,
         default_metadata=snap.metadata,
     )
