34b76d3998edefc99e1f17bf8076376182f5998a
Tuna Acisu <tuna.acisu@ourworldindata.com>
2024-07-09 16:01:33 +0100

    :sparkles: fix metadata propagation and update metadata for long run data set

--- etl/steps/data/garden/maternal_mortality/2024-07-08/maternal_mortality.py
+++ etl/steps/data/garden/maternal_mortality/2024-07-08/maternal_mortality.py
@@ -1,5 +1,7 @@
 """Load a meadow dataset and create a garden dataset."""
 
+from owid.datautils.dataframes import combine_two_overlapping_dataframes
+
 from etl.data_helpers import geo
 from etl.helpers import PathFinder, create_dataset
 
@@ -23,7 +25,7 @@ def run(dest_dir: str) -> None:
     tb_gm = ds_gm["maternal_mortality"].reset_index()
     tb_un = ds_un["maternal_mortality"].reset_index()
 
-    aggr = {"maternal_deaths": sum, "births": sum}
+    aggr = {"maternal_deaths": "sum", "births": "sum"}
 
     # regional aggregates (before joining - since Gapminder only includes 14 countries)
     tb_un = geo.add_regions_to_table(
@@ -39,20 +41,11 @@ def run(dest_dir: str) -> None:
     tb_un["mmr"] = tb_un.apply(lambda x: calc_mmr(x), axis=1)
 
     # join the two tables
-    tb = tb_un.merge(tb_gm, how="outer", on=["country", "year"], suffixes=("_un", "_gm"))
+    # first - rename columns so they have the same names
+    tb_un = tb_un.rename(columns={"births": "live_births"})
 
-    # combine rows, using UN data where available
-    tb["maternal_deaths"] = tb["maternal_deaths_un"].combine_first(tb["maternal_deaths_gm"])
-    tb["mmr"] = tb["mmr_un"].combine_first(tb["mmr_gm"])
-    tb["live_births"] = tb["live_births"].combine_first(tb["births"])
-    tb["mm_rate"] = tb["mm_rate_un"].combine_first(tb["mm_rate_gm"])
-
-    # TODO - figure out how to join metadata here - currently all joined columns only have metadata from the GM dataset
-
-    print(tb["maternal_deaths"].metadata)
-    print(tb["mmr"].metadata)
-    print(tb["live_births"].metadata)
-    print(tb["mm_rate"].metadata)
+    # combine both dataframes
+    tb = combine_two_overlapping_dataframes(tb_un, tb_gm, index_columns=["country", "year"])
 
     # since this is the long run dataset, drop all columns not in both datasets
     cols_to_keep = ["country", "year", "maternal_deaths", "mmr", "live_births", "mm_rate"]
@@ -65,7 +58,7 @@ def run(dest_dir: str) -> None:
     # Save outputs.
     #
     # Create a new garden dataset.
-    ds_garden = create_dataset(dest_dir, tables=[tb], check_variables_metadata=True, default_metadata=ds_gm.metadata)
+    ds_garden = create_dataset(dest_dir, tables=[tb], check_variables_metadata=True)
 
     # Save changes in the new garden dataset.
     ds_garden.save()
