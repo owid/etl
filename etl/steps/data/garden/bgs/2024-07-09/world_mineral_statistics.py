5bfd98b40278f043e21afc1a75156b7de08ff7c4
Pablo Rosado <pabloarosado@gmail.com>
2024-07-10 19:38:10 +0200

    Add garden step

---
+++ etl/steps/data/garden/bgs/2024-07-09/world_mineral_statistics.py
@@ -0,0 +1,38 @@
+"""Load a meadow dataset and create a garden dataset."""
+
+from etl.data_helpers import geo
+from etl.helpers import PathFinder, create_dataset
+
+# Get paths and naming conventions for current step.
+paths = PathFinder(__file__)
+
+
+def run(dest_dir: str) -> None:
+    #
+    # Load inputs.
+    #
+    # Load meadow dataset.
+    ds_meadow = paths.load_dataset("world_mineral_statistics")
+
+    # Read table from meadow dataset.
+    tb = ds_meadow.read_table("world_mineral_statistics")
+
+    #
+    # Process data.
+    #
+    # Harmonize country names.
+    tb = geo.harmonize_countries(
+        df=tb, countries_file=paths.country_mapping_path, excluded_countries_file=paths.excluded_countries_path
+    )
+
+    # Format table conveniently.
+    tb = tb.format(["country", "year", "category", "commodity", "sub_commodity"])
+
+    #
+    # Save outputs.
+    #
+    # Create a new garden dataset with the same metadata as the meadow dataset.
+    ds_garden = create_dataset(dest_dir, tables=[tb], check_variables_metadata=True)
+
+    # Save changes in the new garden dataset.
+    ds_garden.save()
