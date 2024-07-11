1466bfdc4480af2d28ebdc097607c34f5ca5b055
Tuna Acisu <tuna.acisu@ourworldindata.com>
2024-07-11 11:49:15 +0100

    :bar_chart: calculate MMR and MM-rate from WHO mortality database and UN WPP

--- etl/steps/data/garden/maternal_mortality/2024-07-08/maternal_mortality.py
+++ etl/steps/data/garden/maternal_mortality/2024-07-08/maternal_mortality.py
@@ -12,6 +12,17 @@ REGIONS = [reg for reg in geo.REGIONS.keys() if reg != "European Union (27)"] + 
 
 
 def run(dest_dir: str) -> None:
+    """Creates long running data set on maternal mortality combining following sources (in brackets - timeframes available for each source):
+    - Gapminder maternal mortality data (1751 - 2008)
+    - WHO mortality database (1950 - 2020) & UN WPP (1950 - 2020)
+    - UN MMEIG maternal mortality data (1985 - 2020)
+    We combine them following the hierarchy below, where the most recent data is used when available:
+    - UN MMEIG >> WHO/UN >> Gapminder
+
+    For the timeframe ~1950-1984 we calculate maternal mortality ratio and rate out of WHO mortality database and UN WPP by using:
+    - death from maternal conditions (all sexes, all ages) from WHO mortality database
+    - births and female population aged 14-49 from UN WPP"""
+
     #
     # Load inputs.
     #
@@ -20,14 +31,48 @@ def run(dest_dir: str) -> None:
     ds_un = paths.load_dataset("maternal_mortality", namespace="un")
     ds_regions = paths.load_dataset("regions")
     ds_income = paths.load_dataset("income_groups")
+    ds_who_mortality = paths.load_dataset("mortality_database")
+    ds_wpp = paths.load_dataset("un_wpp")
 
     # Read table from meadow dataset.
     tb_gm = ds_gm["maternal_mortality"].reset_index()
     tb_un = ds_un["maternal_mortality"].reset_index()
+    tb_who_mortality = ds_who_mortality["maternal_conditions__both_sexes__all_ages"].reset_index()
+    tb_wpp = ds_wpp["un_wpp"].reset_index()
+
+    # calculate maternal mortality ratio/ rate out of WHO mortality database and UN WPP
+    tb_who_mortality = tb_who_mortality.rename(
+        columns={"total_deaths_that_are_from_maternal_conditions__in_both_sexes_aged_all_ages": "maternal_deaths"}
+    )[["country", "year", "maternal_deaths"]]
+    tb_who_mortality = tb_who_mortality[tb_who_mortality["year"] < 1985]
+
+    # we need births from WPP but only until 1985 (when UN MMEIG data starts)
+    tb_wpp_births = tb_wpp[(tb_wpp["metric"] == "births") & (tb_wpp["year"] < 1985) & (tb_wpp["age"] == "all")]
+    tb_wpp_births = tb_wpp_births.rename(columns={"location": "country", "value": "live_births"})[
+        ["country", "year", "live_births"]
+    ]
+    # we need female population of reproductive age from WPP until 1985
+    reproductive_ages = ["15-19", "20-24", "25-29", "30-34", "35-39", "40-44", "45-49"]
+    tb_wpp_fm_pop = tb_wpp[
+        (tb_wpp["metric"] == "population")
+        & (tb_wpp["year"] < 1985)
+        & (tb_wpp["age"].isin(reproductive_ages))
+        & (tb_wpp["sex"] == "female")
+    ]
+    tb_wpp_fm_pop = tb_wpp_fm_pop.rename(columns={"location": "country", "value": "female_population"})
+    tb_wpp_fm_pop = (
+        tb_wpp_fm_pop[["country", "year", "female_population"]].groupby(by=["country", "year"]).sum().reset_index()
+    )
+
+    # calculate maternal mortality indicators
+    tb_calc_mm = tb_who_mortality.merge(tb_wpp_births, on=["country", "year"], how="left")
+    tb_calc_mm = tb_calc_mm.merge(tb_wpp_fm_pop, on=["country", "year"], how="left")
+    tb_calc_mm["mmr"] = (tb_calc_mm["maternal_deaths"] / tb_calc_mm["live_births"]) * 100_000
+    tb_calc_mm["mm_rate"] = (tb_calc_mm["maternal_deaths"] / tb_calc_mm["female_population"]) * 100_000
 
     aggr = {"maternal_deaths": "sum", "births": "sum"}
 
-    # regional aggregates (before joining - since Gapminder only includes 14 countries)
+    # regional aggregates (before joining - since Gapminder/ WHO only include subset of countries)
     tb_un = geo.add_regions_to_table(
         tb=tb_un,
         regions=REGIONS,
@@ -44,8 +89,9 @@ def run(dest_dir: str) -> None:
     # first - rename columns so they have the same names
     tb_un = tb_un.rename(columns={"births": "live_births"})
 
-    # combine both dataframes
-    tb = combine_two_overlapping_dataframes(tb_un, tb_gm, index_columns=["country", "year"])
+    # combine dataframes - first UN MMEIG with WHO/UN, then Gapminder with the result
+    tb = combine_two_overlapping_dataframes(tb_un, tb_calc_mm, index_columns=["country", "year"])
+    tb = combine_two_overlapping_dataframes(tb, tb_gm, index_columns=["country", "year"])
 
     # since this is the long run dataset, drop all columns not in both datasets
     cols_to_keep = ["country", "year", "maternal_deaths", "mmr", "live_births", "mm_rate"]
