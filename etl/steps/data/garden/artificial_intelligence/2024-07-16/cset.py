0e1ac35729035ea1c4a63eef0c01764c86d3e453
veronikasamborska1994 <32176660+veronikasamborska1994@users.noreply.github.com>
2024-07-16 18:25:49 +0100

    clean up

--- etl/steps/data/garden/artificial_intelligence/2024-07-16/cset.py
+++ etl/steps/data/garden/artificial_intelligence/2024-07-16/cset.py
@@ -14,28 +14,16 @@ def add_world(tb: Table, ds_regions: Dataset) -> Table:
     # Create a deep copy of the input table to avoid modifying the original data
     tb_with_regions = tb.copy()
 
-    # List of members representing different regions CSET
+    # List of members representing different regions CSET and use these for World aggregation
     members = ["North America", "Europe", "Asia Pacific", "Africa", "Latin America and the Caribbean", "Oceania"]
 
-    # Filter the table to only include rows corresponding to the countries and not regions
-    df_regions = tb_with_regions[tb_with_regions["country"].isin(members)]
-
-    # Reset the index of the filtered data
-    df_regions = df_regions.reset_index(drop=True)
-
-    # Define aggregation rules for each column excluding "country", "year", and "field"
+    df_regions = tb_with_regions[tb_with_regions["country"].isin(members)].reset_index(drop=True)
     numeric_cols = [col for col in df_regions.columns if col not in ["country", "year", "field"]]
 
-    # Group the filtered data by "year" and "field" and aggregate the data based on the defined rules
     result = df_regions.groupby(["year", "field"], observed=False)[numeric_cols].agg(sum_with_nan).reset_index()
-
-    # Assign the aggregated data to a new country named "World"
     result["country"] = "World"
 
-    # Concatenate the aggregated 'World' data with the original table
     tb = pr.concat([tb_with_regions, result])
-
-    # Reset the index of the concatenated table
     tb = tb.reset_index(drop=True)
 
     return tb
@@ -62,6 +50,7 @@ def run(dest_dir: str) -> None:
     tb = geo.harmonize_countries(df=tb, countries_file=paths.country_mapping_path)
     tb = add_world(tb=tb, ds_regions=ds_regions)
 
+    # Adjust investment columns for inflation using the US Consumer Price Index (CPI)
     _investment_cols = [col for col in tb.columns if "investment" in col]
     tb[_investment_cols] = tb[_investment_cols].astype("float64")
     tb.loc[:, _investment_cols] *= 1e6
@@ -76,6 +65,7 @@ def run(dest_dir: str) -> None:
 
     tb_cpi_inv = tb_cpi_inv.drop("cpi_adj_2021", axis=1)
 
+    # Calculate the number of patent applications, patents granted, and articles per million people
     tb = pr.merge(
         tb_cpi_inv,
         tb_population[["country", "year", "population"]].astype({"population": "float64"}),
@@ -88,8 +78,10 @@ def run(dest_dir: str) -> None:
 
     tb = tb.drop("population", axis=1)
 
+    # Calculate the number of citations per article
     tb["citations_per_article"] = (tb["num_citations_summary"] / tb["num_articles_summary"]).astype(float)
 
+    # Exclude rows where the number of articles is less than 1000 from the calculation of citations per article to avoid outliers.
     condition = (tb["num_articles_summary"] < 1000) & pd.notna(tb["num_articles_summary"])
     tb.loc[condition, "citations_per_article"] = pd.NA
 
