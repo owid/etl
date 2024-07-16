f61b692fe3e8955cd4b331ffb0915ea3b4571236
Pablo Rosado <pabloarosado@gmail.com>
2024-07-16 15:00:23 +0200

    Adjust unit value for inflation

--- etl/steps/data/garden/usgs/2024-07-15/historical_statistics_for_mineral_and_material_commodities.py
+++ etl/steps/data/garden/usgs/2024-07-15/historical_statistics_for_mineral_and_material_commodities.py
@@ -1,12 +1,40 @@
 """Load a meadow dataset and create a garden dataset."""
 
 import owid.catalog.processing as pr
+from owid.catalog import Table
 from owid.datautils.dataframes import map_series
 
 from etl.helpers import PathFinder, create_dataset
 
 # Get paths and naming conventions for current step.
 paths = PathFinder(__file__)
+
+# Reference year for inflation adjustment.
+REFERENCE_YEAR = 2022
+
+
+def correct_unit_value_for_inflation(tb_unit_value: Table, tb_cpi: Table) -> Table:
+    tb_unit_value = tb_unit_value.copy()
+    # Correct for inflation, using a reference year (the latest year in the data).
+    assert tb_unit_value["year"].max() == REFERENCE_YEAR
+    # Adjust CPI values so that 2021 is the reference year (2021 = 100)
+    tb_cpi = tb_cpi[["year", "all_items"]].reset_index(drop=True)
+
+    reference_value = tb_cpi.loc[tb_cpi["year"] == REFERENCE_YEAR, "all_items"].values[0]
+    # Adjust value so that it is 1 at the reference year.
+    tb_cpi["all_items"] = tb_cpi["all_items"] / reference_value
+    # The US consumer prices dataset has data only since 1913.
+    # We need to correct for inflation since 1900.
+    # TODO: For now, fill backwards, but check if there is better strategy.
+    tb_unit_value = tb_unit_value.merge(tb_cpi.rename(columns={"all_items": "cpi"}), on="year", how="left")
+    tb_unit_value = tb_unit_value.sort_values(["country", "year"]).reset_index(drop=True)
+    tb_unit_value["cpi"] = tb_unit_value["cpi"].bfill()
+    # Adjust unit value for inflation.
+    tb_unit_value["unit_value"] = tb_unit_value["unit_value_current"] / tb_unit_value["cpi"]
+    # Remove column for CPI.
+    tb_unit_value = tb_unit_value.drop(columns=["cpi"])
+
+    return tb_unit_value
 
 
 def run(dest_dir: str) -> None:
@@ -19,7 +47,11 @@ def run(dest_dir: str) -> None:
     # Read table from meadow dataset.
     # NOTE: Since the data has spurious footnotes, like "W", some columns were stored as strings.
     # Later on we will remove these footnotes and store data as floats.
-    tb = ds_meadow["historical_statistics_for_mineral_and_material_commodities"].reset_index()
+    tb = ds_meadow.read_table("historical_statistics_for_mineral_and_material_commodities")
+
+    # Load US consumer prices dataset (to use CPI to adjust for inflation).
+    ds_cpi = paths.load_dataset("us_consumer_prices")
+    tb_cpi = ds_cpi.read_table("us_consumer_prices")
 
     #
     # Process data.
@@ -73,12 +105,15 @@ def run(dest_dir: str) -> None:
     tb_unit_value = (
         tb[["commodity", "year", "unit_value_dollar_t"]]
         .assign(**{"country": "World"})
-        .rename(columns={"unit_value_dollar_t": "unit_value"}, errors="raise")
+        .rename(columns={"unit_value_dollar_t": "unit_value_current"}, errors="raise")
     )
     # Remove spurious footnotes like "W" (and "nan", caused by saving the column as string).
-    tb_unit_value["unit_value"] = tb_unit_value["unit_value"].astype(str).replace("W", None).replace("nan", None)
+    tb_unit_value["unit_value_current"] = (
+        tb_unit_value["unit_value_current"].astype(str).replace("W", None).replace("nan", None).astype(float)
+    )
 
-    # TODO: Correct for inflation.
+    # Correct unit value for inflation.
+    tb_unit_value = correct_unit_value_for_inflation(tb_unit_value=tb_unit_value, tb_cpi=tb_cpi)
 
     # Combine tables.
     tb_combined = pr.concat([tb_us_production, tb_world_production], ignore_index=True)
