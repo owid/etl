87107b7d14f1f53be961d40e8e23bba209a5169c
veronikasamborska1994 <32176660+veronikasamborska1994@users.noreply.github.com>
2024-07-16 14:45:16 +0100

    unused imports

--- etl/steps/data/garden/artificial_intelligence/2024-07-16/cset.py
+++ etl/steps/data/garden/artificial_intelligence/2024-07-16/cset.py
@@ -1,4 +1,3 @@
-import numpy as np
 import owid.catalog.processing as pr
 import pandas as pd
 from owid.catalog import Dataset, Table
--- etl/steps/data/grapher/artificial_intelligence/2024-07-16/cset.py
+++ etl/steps/data/grapher/artificial_intelligence/2024-07-16/cset.py
@@ -1,6 +1,5 @@
 """Load a garden dataset and create a grapher dataset."""
 
-from etl import grapher_helpers as gh
 from etl.helpers import PathFinder, create_dataset
 
 # Get paths and naming conventions for current step.
