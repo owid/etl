72a73d607256968ae84b3b28cb309b5ee015f2d4
spoonerf <spoonerfiona@gmail.com>
2024-07-17 22:18:40 +0100

    adding some early language steps

---
+++ etl/steps/data/garden/language/2024-07-17/ethnologue.py
@@ -0,0 +1,92 @@
+"""Load a meadow dataset and create a garden dataset."""
+
+from owid.catalog import Table
+
+from etl.data_helpers import geo
+from etl.helpers import PathFinder, create_dataset
+
+# Get paths and naming conventions for current step.
+paths = PathFinder(__file__)
+
+
+def run(dest_dir: str) -> None:
+    #
+    # Load inputs.
+    #
+    # Load meadow dataset.
+    ds_meadow = paths.load_dataset("ethnologue")
+
+    # Read table from meadow dataset.
+    tb_country_codes = ds_meadow["country_codes"].reset_index()
+    tb_language_codes = ds_meadow["language_codes"].reset_index()
+    tb_language_index = ds_meadow["language_index"].reset_index()
+
+    #
+    tb_country_codes = tb_country_codes.drop(columns="area")
+    # Process data.
+    #
+    tb = geo.harmonize_countries(
+        df=tb_country_codes,
+        countries_file=paths.country_mapping_path,
+        excluded_countries_file=paths.excluded_countries_path,
+    )
+
+    # Calculate the total number of languages per country
+    tb_languages_per_country = languages_per_country(tb_language_index, tb_country_codes)
+
+    # tb = tb.format(["country", "year"])
+
+    #
+    # Save outputs.
+    #
+    # Create a new garden dataset with the same metadata as the meadow dataset.
+    ds_garden = create_dataset(
+        dest_dir, tables=[tb], check_variables_metadata=True, default_metadata=ds_meadow.metadata
+    )
+
+    # Save changes in the new garden dataset.
+    ds_garden.save()
+
+
+def countries_per_language(tb_language_index: Table, tb_country_codes: Table, tb_language_codes: Table) -> Table:
+    tb_countries_per_language = (
+        tb_country_codes.merge(tb_language_index, on="countryid", how="inner")
+        .loc[:, ["countryid", "langid"]]
+        .drop_duplicates()
+        .groupby("langid")
+        .size()
+        .reset_index(name="number_of_countries")
+        .sort_values(by="number_of_countries", ascending=False)
+        .merge(tb_language_codes, on="langid", how="inner")
+    )
+
+    tb_countries_per_language = tb_countries_per_language[["name", "number_of_countries"]]
+    return tb_countries_per_language
+
+
+def languages_per_country(tb_language_index: Table, tb_country_codes: Table) -> Table:
+    tb_languages_per_country = (
+        tb_language_index.groupby("countryid")["langid"].nunique().reset_index(name="n")
+    ).reset_index(drop=True)
+
+    tb_languages_per_country = tb_languages_per_country.merge(tb_country_codes, on="countryid", how="left")
+
+    return tb_languages_per_country
+
+
+def extinct_and_living_languages_per_country(
+    tb_language_index: Table, tb_language_codes: Table, tb_country_codes: Table
+) -> Table:
+    tb_extinct_living_languages = (
+        tb_language_index.merge(tb_language_codes, on=["langid", "countryid"], how="outer")
+        .groupby(["countryid", "langstatus"])
+        .agg({"langid": "nunique"})
+        .reset_index()
+        .rename(columns={"langid": "number_of_languages"})
+        .replace({"langstatus": {"L": "living", "X": "extinct"}})
+        .merge(tb_country_codes, on="countryid", how="left")
+        .pivot(index="country", columns="langstatus", values="number_of_languages")
+        .fillna(0)
+        .reset_index()
+    )
+    return tb_extinct_living_languages
---
+++ etl/steps/data/meadow/language/2024-07-17/ethnologue.py
@@ -0,0 +1,52 @@
+"""Load a snapshot and create a meadow dataset."""
+import zipfile
+
+from owid.catalog import processing as pr
+
+from etl.helpers import PathFinder, create_dataset
+
+# Get paths and naming conventions for current step.
+paths = PathFinder(__file__)
+
+
+def run(dest_dir: str) -> None:
+    #
+    # Load inputs.
+    #
+    # Retrieve snapshot.
+    snap = paths.load_snapshot("ethnologue.zip")
+
+    file_names = ["CountryCodes.tab", "LanguageCodes.tab", "LanguageIndex.tab"]
+    # Each table has a different index, so we need to specify them here
+    indexes = ["countryid", "langid", ["langid", "countryid", "nametype", "name"]]
+    short_names = ["country_codes", "language_codes", "language_index"]
+    # Open the ZIP file and read the .tab file into a pandas DataFrame
+    tables = []
+    for file_name, index, short_name in zip(file_names, indexes, short_names):
+        with zipfile.ZipFile(snap.path, "r") as zip_ref:
+            with zip_ref.open(f"Language_Code_Data_20240221/{file_name}") as file:
+                tb = pr.read_csv(file, sep="\t")
+                if file_name == "LanguageIndex.tab":
+                    # There is one erroneous duplicate in the LanguageIndex.tab file - let's remove it here
+                    duplicates = tb[tb.duplicated()]
+                    assert duplicates.shape[0] == 1
+                    tb = tb.drop_duplicates()
+                if file_name == "CountryCodes.tab":
+                    tb = tb.rename(columns={"Name": "country"}, errors="raise")
+                tb = tb.format(index, short_name=short_name)
+                tables.append(tb)
+    # Give the variables the same origins as the snapshot
+    for table in tables:
+        for col in table.columns:
+            table[col].metadata.origins = [snap.metadata.origin]
+    #
+    # Ensure all columns are snake-case, set an appropriate index, and sort conveniently.
+
+    #
+    # Save outputs.
+    #
+    # Create a new meadow dataset with the same metadata as the snapshot.
+    ds_meadow = create_dataset(dest_dir, tables=tables, check_variables_metadata=True, default_metadata=snap.metadata)
+
+    # Save changes in the new meadow dataset.
+    ds_meadow.save()
