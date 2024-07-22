4734e8b083702add7a7355b0445d7975641eb8e9
lucasrodes <lucasrodes@users.noreply.github.com>
2024-07-22 18:31:17 +0200

    interpolate country population for region agg

--- etl/steps/data/garden/demography/2024-07-15/population.py
+++ etl/steps/data/garden/demography/2024-07-15/population.py
@@ -379,17 +379,21 @@ def add_regions(tb: Table, ds_regions: Dataset, ds_income_groups: Dataset) -> Ta
     sources = tb.loc[:, ["country", "year", "source"]].copy()
     tb = tb.drop(columns=["source"])
 
-    # Split historical vs current countries
+    # Build table specifically for estimating regions: (1) no historical regions, (2) interpolation of country values
+
+    ## (1) remove historical regions
     ## This is because it looks like historical countries are being considered when estimating values for regions.
     tb_regions = ds_regions["regions"]
     historical_regions = set(tb_regions.loc[tb_regions["is_historical"], "name"])
-    tb_historical = tb.loc[tb["country"].isin(historical_regions)]
-    tb = tb.loc[~tb["country"].isin(historical_regions)]
+    tb_aggregates = tb.loc[~tb["country"].isin(historical_regions)].copy()
+
+    ## (2) interpolate population for countries
+    tb_aggregates = geo.interpolate_table(tb_aggregates, "country", "year")
 
     # re-estimate region aggregates
     aggregations = {"population": "sum"}
-    tb = geo.add_regions_to_table(
-        tb=tb,
+    tb_aggregates = geo.add_regions_to_table(
+        tb=tb_aggregates,
         regions=regions,
         aggregations=aggregations,
         ds_regions=ds_regions,
@@ -440,9 +444,10 @@ def add_regions(tb: Table, ds_regions: Dataset, ds_income_groups: Dataset) -> Ta
             ],
         },
     )
+    tb_aggregates = tb_aggregates.loc[tb_aggregates["country"].isin(regions)]
 
     # Add historical countries back
-    tb = pr.concat([tb, tb_historical], ignore_index=True)
+    tb = pr.concat([tb, tb_aggregates], ignore_index=True)
 
     # tb = tb.loc[
     #     (
