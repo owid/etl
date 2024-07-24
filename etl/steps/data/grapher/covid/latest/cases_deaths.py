39b161b8c4c04f8704f4b13b342d8f0c45535fb8
lucasrodes <lucasrodes@users.noreply.github.com>
2024-07-24 18:05:26 +0200

    wip

--- etl/steps/data/grapher/covid/latest/cases_deaths.py
+++ etl/steps/data/grapher/covid/latest/cases_deaths.py
@@ -25,6 +25,7 @@ def run(dest_dir: str) -> None:
     # Save outputs.
     #
     # Create a new grapher dataset with the same metadata as the garden dataset.
+    print(tb.head())
     ds_grapher = create_dataset(
         dest_dir, tables=[tb], check_variables_metadata=True, default_metadata=ds_garden.metadata
     )
