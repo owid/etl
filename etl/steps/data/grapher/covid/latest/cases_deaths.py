06ce1c9d64c91b7d21b349671308f2274079528b
lucasrodes <lucasrodes@users.noreply.github.com>
2024-07-24 18:10:12 +0200

    fix grapher step

--- etl/steps/data/grapher/covid/latest/cases_deaths.py
+++ etl/steps/data/grapher/covid/latest/cases_deaths.py
@@ -1,5 +1,7 @@
 """Load a garden dataset and create a grapher dataset."""
 
+import pandas as pd
+
 from etl.helpers import PathFinder, create_dataset
 
 # Get paths and naming conventions for current step.
@@ -14,20 +16,23 @@ def run(dest_dir: str) -> None:
     ds_garden = paths.load_dataset("cases_deaths")
 
     # Read table from garden dataset.
-    tb = ds_garden["cases_deaths"]
+    tb = ds_garden["cases_deaths"].reset_index()
 
     #
     # Process data.
     #
-    tb = tb.rename_index_names({"date": "year"})
+    tb["year"] = pd.to_datetime(tb["date"].astype(str), format="%Y-%m-%d") - pd.to_datetime("2020-01-21")
+    tb = tb.format(["country", "year"])
 
     #
     # Save outputs.
     #
     # Create a new grapher dataset with the same metadata as the garden dataset.
-    print(tb.head())
     ds_grapher = create_dataset(
-        dest_dir, tables=[tb], check_variables_metadata=True, default_metadata=ds_garden.metadata
+        dest_dir,
+        tables=[tb],
+        check_variables_metadata=True,
+        default_metadata=ds_garden.metadata,
     )
 
     # Save changes in the new grapher dataset.
