7d40d813c600e196fb26eae1ecf99b7a8b6ee1e4
lucasrodes <lucasrodes@users.noreply.github.com>
2024-07-25 16:11:26 +0200

    set snapshot version to latest

--- snapshots/covid/latest/cases_deaths.py
+++ snapshots/covid/latest/cases_deaths.py
@@ -1,5 +1,6 @@
 """Script to create a snapshot of dataset."""
 
+from datetime import date
 from pathlib import Path
 
 import click
@@ -16,8 +17,22 @@ def main(upload: bool) -> None:
     # Create a new snapshot.
     snap = Snapshot(f"covid/{SNAPSHOT_VERSION}/cases_deaths.csv")
 
+    # Update metadata
+    snap = modify_metadata(snap)
+
     # Download data from source, add file to DVC and upload to S3.
     snap.create_snapshot(upload=upload)
+
+
+def modify_metadata(snap: Snapshot) -> Snapshot:
+    """Modify metadata"""
+    # Get access date
+    snap.metadata.origin.date_accessed = date.today()  # type: ignore
+    # Set publication date
+    snap.metadata.origin.publication_date = date.today()  # type: ignore
+    # Save
+    snap.metadata.save()
+    return snap
 
 
 if __name__ == "__main__":
