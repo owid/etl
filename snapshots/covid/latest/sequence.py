54e56e217c8ad72b2da6e13e47d7e77df976c68f
lucasrodes <lucasrodes@users.noreply.github.com>
2024-07-29 18:25:16 +0200

    snapshot: modify date

--- snapshots/covid/latest/sequence.py
+++ snapshots/covid/latest/sequence.py
@@ -1,8 +1,10 @@
 """Script to create a snapshot of dataset."""
 
+from datetime import date, datetime
 from pathlib import Path
 
 import click
+import requests
 
 from etl.snapshot import Snapshot
 
@@ -18,6 +20,27 @@ def main(upload: bool) -> None:
 
     # Download data from source, add file to DVC and upload to S3.
     snap.create_snapshot(upload=upload)
+
+
+def _get_latest_update():
+    url = "https://github.com/hodcroftlab/covariants/raw/master/web/public/data/update.json"
+    field_name = "lastUpdated"
+    date_json = requests.get(url).json()
+    if field_name in date_json:
+        date_raw = date_json[field_name]
+        return datetime.fromisoformat(date_raw).date()
+    raise ValueError(f"{field_name} field not found!")
+
+
+def modify_metadata(snap: Snapshot) -> Snapshot:
+    """Modify metadata"""
+    # Get access date
+    snap.metadata.origin.date_accessed = date.today()  # type: ignore
+    # Set publication date
+    snap.metadata.origin.publication_date = _get_latest_update()  # type: ignore
+    # Save
+    snap.metadata.save()
+    return snap
 
 
 if __name__ == "__main__":
