08b5f4518c468c087ac160a5e1e84074f79ca3d5
lucasrodes <lucasrodes@users.noreply.github.com>
2024-07-30 00:11:38 +0200

    grapher: wip

--- etl/steps/data/grapher/covid/latest/sequence.py
+++ etl/steps/data/grapher/covid/latest/sequence.py
@@ -1,5 +1,7 @@
 """Load a garden dataset and create a grapher dataset."""
 
+from shared import to_grapher_date
+
 from etl.helpers import PathFinder, create_dataset
 
 # Get paths and naming conventions for current step.
@@ -14,7 +16,7 @@ def run(dest_dir: str) -> None:
     ds_garden = paths.load_dataset("sequence")
 
     # Read table from garden dataset.
-    tb = ds_garden["sequence"]
+    tables = [to_grapher_date(tb) for tb in list(ds_garden)]
 
     #
     # Process data.
@@ -25,7 +27,7 @@ def run(dest_dir: str) -> None:
     #
     # Create a new grapher dataset with the same metadata as the garden dataset.
     ds_grapher = create_dataset(
-        dest_dir, tables=[tb], check_variables_metadata=True, default_metadata=ds_garden.metadata
+        dest_dir, tables=tables, check_variables_metadata=True, default_metadata=ds_garden.metadata
     )
 
     # Save changes in the new grapher dataset.
