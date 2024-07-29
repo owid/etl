4a13afaed22a331f7d8a24d2d448f376ca0bfaa6
veronikasamborska1994 <32176660+veronikasamborska1994@users.noreply.github.com>
2024-07-29 13:33:33 +0100

    ai: maximum compute, parameters and data test

--- etl/steps/data/grapher/artificial_intelligence/2024-07-10/epoch.py
+++ etl/steps/data/grapher/artificial_intelligence/2024-07-10/epoch.py
@@ -14,17 +14,51 @@ def run(dest_dir: str) -> None:
     ds_garden = paths.load_dataset("epoch")
 
     # Read table from garden dataset.
-    tb = ds_garden["epoch"]
+    tb = ds_garden["epoch"].reset_index()
     #
     # Process data.
     #
+    # Extract year from 'publication_date' and create a new 'year' column
+    tb["year"] = tb["publication_date"].dt.year
+    # Find the index of the maximum 'training_computation_petaflop', 'data', and 'parameters' in each year
+    # Fill NaN values with 0 before finding the index of the maximum
+    idx_compute = (
+        tb[["training_computation_petaflop", "year"]]
+        .fillna(0)
+        .groupby("year")["training_computation_petaflop"]
+        .idxmax()
+    )
+    idx_data = (
+        tb[["training_dataset_size__datapoints", "year"]]
+        .fillna(0)
+        .groupby("year")["training_dataset_size__datapoints"]
+        .idxmax()
+    )
+    idx_parameters = tb[["parameters", "year"]].fillna(0).groupby("year")["parameters"].idxmax()
+
+    # Create indicator columns
+    tb["largest_compute"] = 0
+    tb["largest_data"] = 0
+    tb["largest_parameters"] = 0
+
+    tb.loc[idx_compute, "largest_compute"] = 1
+    tb.loc[idx_data, "largest_data"] = 1
+    tb.loc[idx_parameters, "largest_parameters"] = 1
+    tb = tb.drop("year", axis=1)
+    for col in ["largest_compute", "largest_data", "largest_parameters"]:
+        tb[col].metadata.origins = tb["domain"].metadata.origins
+
+    tb = tb.rename(columns={"system": "country", "days_since_1949": "year"})
     # Rename for plotting model name as country in grapher
-    tb = tb.rename_index_names({"system": "country", "days_since_1949": "year"})
+    tb = tb.format(["country", "year"])
+
     #
     # Save outputs.
     #
     # Create a new grapher dataset with the same metadata as the garden dataset.
-    ds_grapher = create_dataset(dest_dir, tables=[tb], default_metadata=ds_garden.metadata)
+    ds_grapher = create_dataset(
+        dest_dir, tables=[tb], default_metadata=ds_garden.metadata, check_variables_metadata=True
+    )
 
     #
     # Checks.
