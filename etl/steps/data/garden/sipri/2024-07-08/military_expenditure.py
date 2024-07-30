8f9fe1abc5dcd9d0cf4d0d4914e729ace6d24a44
Pablo Arriagada <63430031+paarriagadap@users.noreply.github.com>
2024-07-30 13:00:52 +0100

    :bar_chart: conflict: Three military expenditure datasets (#2942)
    * Update military expenditure data
    
    * empty

---
+++ etl/steps/data/garden/sipri/2024-07-08/military_expenditure.py
@@ -0,0 +1,79 @@
+"""Load a meadow dataset and create a garden dataset."""
+
+import owid.catalog.processing as pr
+from owid.catalog import Table
+
+from etl.data_helpers import geo
+from etl.helpers import PathFinder, create_dataset
+
+# Get paths and naming conventions for current step.
+paths = PathFinder(__file__)
+
+
+def run(dest_dir: str) -> None:
+    #
+    # Load inputs.
+    #
+    # Load meadow dataset.
+    ds_meadow = paths.load_dataset("military_expenditure")
+
+    # Read table from meadow dataset.
+    tb_constant_usd = ds_meadow["constant_usd"].reset_index()
+    tb_constant_usd_regions = ds_meadow["constant_usd_regions"].reset_index()
+    tb_share_gdp = ds_meadow["share_gdp"].reset_index()
+    tb_per_capita = ds_meadow["per_capita"].reset_index()
+    tb_share_govt_spending = ds_meadow["share_govt_spending"].reset_index()
+
+    # Merge all these tables
+    tb = pr.multi_merge(
+        tables=[tb_constant_usd, tb_constant_usd_regions, tb_share_gdp, tb_per_capita, tb_share_govt_spending],
+        on=["country", "year"],
+        how="outer",
+        short_name=paths.short_name,
+    )
+
+    tb = adjust_units(tb)
+    tb = move_regional_data_to_constant_usd(tb)
+
+    #
+    # Process data.
+    #
+    tb = geo.harmonize_countries(
+        df=tb,
+        countries_file=paths.country_mapping_path,
+    )
+    tb = tb.format(["country", "year"])
+
+    #
+    # Save outputs.
+    #
+    # Create a new garden dataset with the same metadata as the meadow dataset.
+    ds_garden = create_dataset(
+        dest_dir, tables=[tb], check_variables_metadata=True, default_metadata=ds_meadow.metadata
+    )
+
+    # Save changes in the new garden dataset.
+    ds_garden.save()
+
+
+def adjust_units(tb: Table) -> Table:
+    """
+    Adjust units of the table. Multiply percentages by 100, monetary values by 1e6 in the case of constant_usd and bt 1e9 in the case of constant_usd_regions.
+    """
+    tb["share_gdp"] *= 100
+    tb["share_govt_spending"] *= 100
+    tb["constant_usd"] *= 1e6
+    tb["constant_usd_regions"] *= 1e9
+
+    return tb
+
+
+def move_regional_data_to_constant_usd(tb: Table) -> Table:
+    """
+    Move regional data (constant_usd_regions) to the constant_usd column.
+    """
+
+    tb["constant_usd"] = tb["constant_usd"].combine_first(tb["constant_usd_regions"])
+    tb = tb.drop(columns=["constant_usd_regions"])
+
+    return tb
