67d153ab46f9531c59fa4fa19d427ffc22edc024
Marigold <mojmir.vinkler@gmail.com>
2024-07-30 08:28:56 +0200

    wip

--- lib/catalog/owid/catalog/tables.py
+++ lib/catalog/owid/catalog/tables.py
@@ -976,9 +976,11 @@ class Table(pd.DataFrame):
     def sort_index(self, *args, **kwargs) -> "Table":
         return super().sort_index(*args, **kwargs)  # type: ignore
 
-    def groupby(self, *args, **kwargs) -> "TableGroupBy":
+    def groupby(self, *args, observed=True, **kwargs) -> "TableGroupBy":
         """Groupby that preserves metadata."""
-        return TableGroupBy(pd.DataFrame.groupby(self.copy(deep=False), *args, **kwargs), self.metadata, self._fields)
+        return TableGroupBy(
+            pd.DataFrame.groupby(self.copy(deep=False), *args, observed=observed, **kwargs), self.metadata, self._fields
+        )
 
     def rolling(self, *args, **kwargs) -> "TableRolling":
         """Rolling operation that preserves metadata."""
