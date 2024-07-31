c23a0839e9f061ab3ed644bbb3039885e6f8d91a
spoonerf <spoonerfiona@gmail.com>
2024-07-31 12:39:13 +0100

    updating grapher step

--- etl/steps/data/garden/homicide/2024-07-30/homicide_long_run_omm.py
+++ etl/steps/data/garden/homicide/2024-07-30/homicide_long_run_omm.py
@@ -1,10 +1,10 @@
-import pandas as pd
-from owid import catalog
+from owid.catalog import Table
+from owid.catalog import processing as pr
 from owid.catalog.utils import underscore
 from owid.datautils import dataframes
 from structlog import get_logger
 
-from etl.helpers import PathFinder
+from etl.helpers import PathFinder, create_dataset
 
 log = get_logger()
 
@@ -13,10 +13,9 @@ paths = PathFinder(__file__)
 
 def run(dest_dir: str) -> None:
     # Load data from fast track
-    ds_eisner = paths.load_dependency("long_term_homicide_rates_in_europe")
+    ds_eisner = paths.load_dataset("long_term_homicide_rates_in_europe")
     # Get the required table
     tb_eisner = ds_eisner["long_term_homicide_rates_in_europe"]
-    # Create a convenient dataframe
     tb_eisner = tb_eisner.rename(
         columns={"homicide_rate_in_europe_over_long_term__per_100_000": "death_rate_per_100_000_population"}
     )
@@ -24,70 +23,71 @@ def run(dest_dir: str) -> None:
 
     eisner_entities = tb_eisner["country"].drop_duplicates()
     # Get both the WHO and UNODC datasets from garden
-    df_who = get_who_mortality_db()
-    df_unodc = get_unodc()
+    tb_who = get_who_mortality_db()
+    tb_unodc = get_unodc()
 
     # Combine both datasets with Eisner
-    df_who_long = combine_datasets(tb_eisner, df_who)
-    df_unodc_long = combine_datasets(tb_eisner, df_unodc)
+    tb_who_long = combine_datasets(tb_eisner, tb_who)
+    tb_unodc_long = combine_datasets(tb_eisner, tb_unodc)
 
-    df_eisner = df_eisner.rename(
+    tb_eisner = tb_eisner.rename(
         columns={"death_rate_per_100_000_population": "death_rate_per_100_000_population_eisner"}
     ).drop(columns="source")
-    df_combined = dataframes.multi_merge([df_eisner, df_who_long, df_unodc_long], on=["country", "year"], how="outer")
+    tb_combined = dataframes.multi_merge([tb_eisner, tb_who_long, tb_unodc_long], on=["country", "year"], how="outer")
 
     # We only want entities for which long-run data is available in grapher - so let's drop all the others
 
-    df_combined = df_combined[df_combined["country"].isin(eisner_entities)]
+    tb_combined = tb_combined[tb_combined["country"].isin(eisner_entities)]
 
-    # Create Table and add short_name
-    df_combined = catalog.Table(df_combined.reset_index(drop=True), short_name="homicide_long_run_omm")
-
+    tb = tb_combined.format(["country", "year"], short_name="homicide_long_run_omm")
     # Save outputs
-    ds_garden = catalog.Dataset.create_empty(dest_dir)
+    ds_garden = create_dataset(
+        dest_dir,
+        tables=[tb],
+        check_variables_metadata=True,
+        default_metadata=ds_eisner.metadata,
+    )
 
-    ds_garden.add(df_combined)
-
-    ds_garden.update_metadata(paths.metadata_path, if_source_exists="append")
-
+    # Save changes in the new garden dataset.
     ds_garden.save()
 
 
-def get_who_mortality_db() -> pd.DataFrame:
+def get_who_mortality_db() -> Table:
     """
     Get the homicide rate from the WHO Mortality Database Dataset
     """
 
     ds_who_db = paths.load_dataset("who_mort_db")
-    df_who = ds_who_db["who_mort_db"].reset_index()
-    df_who = pd.DataFrame(df_who[["country", "year", "death_rate_per_100_000_population_both_sexes_all_ages"]])
-    df_who = df_who.dropna(subset="death_rate_per_100_000_population_both_sexes_all_ages")
-    df_who = df_who.rename(
-        columns={"death_rate_per_100_000_population_both_sexes_all_ages": "death_rate_per_100_000_population"}
-    )
-    df_who["source"] = "WHO"
-    return df_who
+    tb_who = ds_who_db["who_mort_db"].reset_index()
+    # Just pull out the rows for all ages and both sexes
+    tb_who = tb_who[(tb_who["age_group"] == "All ages") & (tb_who["sex"] == "Both Sexes")]
+    assert tb_who.set_index(["country", "year"]).index.is_unique and tb_who.shape[0] > 0
+    # Grab the columns we need
+    tb_who = tb_who[["country", "year", "death_rate_per_100_000_population"]]
+    tb_who = tb_who.dropna(subset="death_rate_per_100_000_population")
+    tb_who["source"] = "WHO"
+    return tb_who
 
 
-def get_unodc() -> pd.DataFrame:
+def get_unodc() -> Table:
     """
     Get the homicide rate from the UNODC
     """
 
-    ds_unodc: catalog.Dataset = paths.load_dependency("unodc")
-    df_unodc = ds_unodc["total"]
-    df_unodc = pd.DataFrame(df_unodc["rate_per_100_000_population_both_sexes_all_ages"]).reset_index()
-    df_unodc = df_unodc.dropna(subset="rate_per_100_000_population_both_sexes_all_ages")
-    df_unodc = df_unodc.rename(
+    ds_unodc = paths.load_dataset("unodc")
+    tb_unodc = ds_unodc["total"].reset_index()
+    tb_unodc = tb_unodc[["country", "year", "rate_per_100_000_population_both_sexes_all_ages"]]
+    tb_unodc = tb_unodc.dropna(subset="rate_per_100_000_population_both_sexes_all_ages")
+    tb_unodc = tb_unodc.rename(
         columns={"rate_per_100_000_population_both_sexes_all_ages": "death_rate_per_100_000_population"}
     )
-    df_unodc["source"] = "UNODC"
-    return df_unodc
+    tb_unodc["source"] = "UNODC"
+    return tb_unodc
 
 
-def combine_datasets(eisner: pd.DataFrame, recent_df: pd.DataFrame) -> pd.DataFrame:
+def combine_datasets(eisner: Table, recent_df: Table) -> Table:
     # Combine the Eisner dataset with a more recent dataset
-    df_combined = pd.merge(
+    df_combined = pr.merge(
         eisner, recent_df, how="outer", on=["country", "year", "death_rate_per_100_000_population", "source"]
     )
 
@@ -98,7 +98,7 @@ def combine_datasets(eisner: pd.DataFrame, recent_df: pd.DataFrame) -> pd.DataFr
     # Dropping values which are duplicated for country and year and where Eisner is the source.
     df_combined = df_combined[
         ~((df_combined.duplicated(subset=["country", "year"], keep=False)) & (df_combined["source"] == "Eisner"))
-    ]
+    ].reset_index(drop=True)
 
     colname = "death_rate_per_100_000_population_" + underscore(
         "_".join(df_combined["source"].drop_duplicates().to_list())
--- etl/steps/data/grapher/homicide/2024-07-30/homicide_long_run_omm.py
+++ etl/steps/data/grapher/homicide/2024-07-30/homicide_long_run_omm.py
@@ -1,17 +1,16 @@
-from owid import catalog
+from etl.helpers import PathFinder, create_dataset
 
-from etl.helpers import PathFinder
-
-N = PathFinder(__file__)
+paths = PathFinder(__file__)
 
 
 def run(dest_dir: str) -> None:
-    dataset = catalog.Dataset.create_empty(dest_dir, N.garden_dataset.metadata)
+    ds_garden = paths.load_dataset("homicide_long_run_omm")
 
-    dataset.metadata.version = "2023-01-03"
+    tb = ds_garden["homicide_long_run_omm"]
+    # Create a new grapher dataset with the same metadata as the garden dataset.
+    ds_grapher = create_dataset(
+        dest_dir, tables=[tb], check_variables_metadata=True, default_metadata=ds_garden.metadata
+    )
 
-    table = N.garden_dataset["homicide_long_run_omm"]
-
-    dataset.add(table)
-
-    dataset.save()
+    # Save changes in the new grapher dataset.
+    ds_grapher.save()
