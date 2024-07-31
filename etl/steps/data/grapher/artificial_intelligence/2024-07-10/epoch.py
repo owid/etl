e50717bd5ae9c7d8b0d64838b45c712928653c94
veronikasamborska1994 <32176660+veronikasamborska1994@users.noreply.github.com>
2024-07-31 18:02:06 +0100

    Update epoch.py

--- etl/steps/data/grapher/artificial_intelligence/2024-07-10/epoch.py
+++ etl/steps/data/grapher/artificial_intelligence/2024-07-10/epoch.py
@@ -37,14 +37,14 @@ def run(dest_dir: str) -> None:
     idx_parameters = tb[["parameters", "year"]].fillna(0).groupby("year")["parameters"].idxmax()
 
     # Create indicator columns
-    tb["largest_compute"] = 1
-    tb["largest_data"] = 1
-    tb["largest_parameters"] = 1
+    tb["largest_compute"] = 0.1
+    tb["largest_data"] = 0.1
+    tb["largest_parameters"] = 0.1
 
-    tb.loc[idx_compute, "largest_compute"] = 2
+    tb.loc[idx_compute, "largest_compute"] = 1
     tb.loc[idx_data, "system"] = "Maximum compute"
-    tb.loc[idx_data, "largest_data"] = 2
-    tb.loc[idx_parameters, "largest_parameters"] = 2
+    tb.loc[idx_data, "largest_data"] = 1
+    tb.loc[idx_parameters, "largest_parameters"] = 1
     tb = tb.drop("year", axis=1)
 
     for col in ["largest_compute", "largest_data", "largest_parameters"]:
