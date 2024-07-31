c998d9794950c611b575591ee318197a851c669b
lucasrodes <lucasrodes@users.noreply.github.com>
2024-07-31 21:24:52 +0200

    wip

--- etl/steps/data/garden/covid/latest/google_mobility.py
+++ etl/steps/data/garden/covid/latest/google_mobility.py
@@ -1,5 +1,7 @@
 """Load a meadow dataset and create a garden dataset."""
 
+from owid.catalog import Table
+
 from etl.data_helpers import geo
 from etl.helpers import PathFinder, create_dataset
 
@@ -21,9 +23,16 @@ def run(dest_dir: str) -> None:
     # Process data.
     #
     tb = geo.harmonize_countries(
-        df=tb, countries_file=paths.country_mapping_path, excluded_countries_file=paths.excluded_countries_path
+        df=tb,
+        countries_file=paths.country_mapping_path,
     )
-    tb = tb.format(["country", "year"])
+
+    # Remove subnational data, keeping only country figures
+    tb = keep_national(tb)
+    tb = rename_columns(tb)
+    tb = smooth_indicators(tb)
+
+    tb = tb.format(["country", "date"])
 
     #
     # Save outputs.
@@ -35,3 +44,53 @@ def run(dest_dir: str) -> None:
 
     # Save changes in the new garden dataset.
     ds_garden.save()
+
+
+def keep_national(tb: Table) -> Table:
+    filter_cols = [
+        "sub_region_1",
+        "sub_region_2",
+        "metro_area",
+        "iso_3166_2_code",
+        "census_fips_code",
+    ]
+    tb = tb[tb[filter_cols].isna().all(1)]
+
+    tb = tb.drop(columns=filter_cols)
+    return tb
+
+
+def rename_columns(tb: Table) -> Table:
+    rename_dict = {
+        "retail_and_recreation_percent_change_from_baseline": "retail_and_recreation",
+        "grocery_and_pharmacy_percent_change_from_baseline": "grocery_and_pharmacy",
+        "parks_percent_change_from_baseline": "parks",
+        "transit_stations_percent_change_from_baseline": "transit_stations",
+        "workplaces_percent_change_from_baseline": "workplaces",
+        "residential_percent_change_from_baseline": "residential",
+    }
+
+    # Rename columns
+    tb = tb.rename(columns=rename_dict)
+    return tb
+
+
+def smooth_indicators(tb: Table) -> Table:
+    tb = tb.sort_values(by=["country", "date"]).reset_index(drop=True)
+    smoothed_cols = [
+        "retail_and_recreation",
+        "grocery_and_pharmacy",
+        "parks",
+        "transit_stations",
+        "workplaces",
+        "residential",
+    ]
+    tb[smoothed_cols] = (
+        tb.groupby("country", observed=True)[smoothed_cols]
+        .rolling(window=7, min_periods=3, center=False)
+        .mean()
+        .reset_index(level=0, drop=True)
+    )
+    tb[smoothed_cols] = tb[smoothed_cols].round(3)
+
+    return tb
--- etl/steps/data/meadow/covid/latest/google_mobility.py
+++ etl/steps/data/meadow/covid/latest/google_mobility.py
@@ -1,9 +1,28 @@
 """Load a snapshot and create a meadow dataset."""
 
+import pandas as pd
+from owid.catalog import Table
+
 from etl.helpers import PathFinder, create_dataset
 
 # Get paths and naming conventions for current step.
 paths = PathFinder(__file__)
+
+cols = [
+    "retail_and_recreation_percent_change_from_baseline",
+    "grocery_and_pharmacy_percent_change_from_baseline",
+    "parks_percent_change_from_baseline",
+    "transit_stations_percent_change_from_baseline",
+    "workplaces_percent_change_from_baseline",
+    "residential_percent_change_from_baseline",
+    "census_fips_code",
+    "country_region",
+    "sub_region_1",
+    "sub_region_2",
+    "metro_area",
+    "iso_3166_2_code",
+    "date",
+]
 
 
 def run(dest_dir: str) -> None:
@@ -14,13 +33,16 @@ def run(dest_dir: str) -> None:
     snap = paths.load_snapshot("google_mobility.csv")
 
     # Load data from snapshot.
-    tb = snap.read()
+    tb = snap.read(usecols=cols)
 
     #
     # Process data.
     #
+    tb = tb.rename(columns={"country_region": "country"})
+    tb = set_dtypes(tb)
+
     # Ensure all columns are snake-case, set an appropriate index, and sort conveniently.
-    tb = tb.format(["country", "year"])
+    tb = tb.format(["country", "date", "sub_region_1", "sub_region_2", "metro_area"])
 
     #
     # Save outputs.
@@ -30,3 +52,16 @@ def run(dest_dir: str) -> None:
 
     # Save changes in the new meadow dataset.
     ds_meadow.save()
+
+
+def set_dtypes(tb: Table) -> Table:
+    tb["date"] = pd.to_datetime(tb["date"])
+    col_str = [
+        "country",
+        "sub_region_1",
+        "sub_region_2",
+        "metro_area",
+        "iso_3166_2_code",
+    ]
+    tb[col_str] = tb[col_str].astype("string")
+    return tb
