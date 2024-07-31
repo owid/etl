d8e3b37983f8933866a17559e36d4c076d393bba
lucasrodes <lucasrodes@users.noreply.github.com>
2024-07-31 17:18:15 +0200

    improve function

--- etl/steps/data/meadow/covid/latest/shared.py
+++ etl/steps/data/meadow/covid/latest/shared.py
@@ -1,10 +1,14 @@
+from typing import Optional
+
 import pandas as pd
 from owid.catalog import Table
 
 ZERO_DAY = "2020-01-21"
 
 
-def year_to_date(tb: Table, col_date: str = "date", col_year: str = "Year") -> Table:
-    tb[col_date] = pd.Timestamp(ZERO_DAY) + pd.to_timedelta(tb[col_year], unit="days")  # type: ignore
+def year_to_date(tb: Table, col_date: str = "date", col_year: str = "Year", zero_day: Optional[str] = None) -> Table:
+    if zero_day is None:
+        zero_day = ZERO_DAY
+    tb[col_date] = pd.Timestamp(zero_day) + pd.to_timedelta(tb[col_year], unit="days")  # type: ignore
     tb = tb.drop(columns=[col_year])
     return tb
