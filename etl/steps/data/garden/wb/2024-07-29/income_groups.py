2bd33d06ceb45fb1b869aab19eaa91997573e942
Pablo Arriagada <63430031+paarriagadap@users.noreply.github.com>
2024-08-01 11:23:52 +0100

    :sparkles: add country count and population aggregate for World Bank income groups (#3055)
    * empty
    
    * :sparkles: add count and population
    
    * :bug: modify display name
    
    * :bug: add title_public
    
    * :lipstick: add description_processing
    
    * :bug: fix typo in description_processing
    
    * :bug: fix error in regions script

--- etl/steps/data/garden/wb/2024-07-29/income_groups.py
+++ etl/steps/data/garden/wb/2024-07-29/income_groups.py
@@ -1,7 +1,10 @@
 """Load a meadow dataset and create a garden dataset."""
 
+from typing import List
+
 import numpy as np
-from owid.catalog import Table
+import owid.catalog.processing as pr
+from owid.catalog import Dataset, Table
 
 from etl.data_helpers import geo
 from etl.helpers import PathFinder, create_dataset
@@ -19,6 +22,12 @@ EXPECTED_MISSING_COUNTRIES_IN_LATEST_RELEASE = {
     "Yugoslavia",
 }
 
+# Define regions to aggregate
+REGIONS = ["Europe", "Asia", "North America", "South America", "Africa", "Oceania", "World"]
+
+# Define fraction of allowed NaNs per year
+FRAC_ALLOWED_NANS_PER_YEAR = 0.2
+
 
 def run(dest_dir: str) -> None:
     #
@@ -26,6 +35,8 @@ def run(dest_dir: str) -> None:
     #
     # Load meadow dataset and read its main table.
     ds_meadow = paths.load_dataset("income_groups")
+    ds_regions = paths.load_dataset("regions")
+    ds_population = paths.load_dataset("population")
     tb = ds_meadow["income_groups"].reset_index()
 
     #
@@ -43,9 +54,6 @@ def run(dest_dir: str) -> None:
     # Drop unnecessary columns.
     tb = tb.drop(columns=["country_code"], errors="raise")
 
-    # Set an appropriate index and sort conveniently.
-    tb = tb.set_index(["country", "year"], verify_integrity=True).sort_index()
-
     # Create an additional table for the classification of the latest year available.
     tb_latest = tb.reset_index().drop_duplicates(subset=["country"], keep="last")
 
@@ -61,8 +69,20 @@ def run(dest_dir: str) -> None:
     # Extract data only for latest release (and remove column year).
     tb_latest = tb_latest[tb_latest["year"] == tb_latest["year"].max()].drop(columns=["year"])
 
+    tb = add_country_counts_and_population_by_status(
+        tb=tb,
+        columns=["classification"],
+        ds_regions=ds_regions,
+        ds_population=ds_population,
+        regions=REGIONS,
+        missing_data_on_columns=False,
+    )
+
     # Set an appropriate index and sort conveniently.
-    tb_latest = tb_latest.set_index(["country"], verify_integrity=True).sort_index()
+    tb = tb.format(["country", "year"])
+
+    # Set an appropriate index and sort conveniently.
+    tb_latest = tb_latest.format(["country"])
 
     #
     # Save outputs.
@@ -111,5 +131,96 @@ def harmonize_income_group_labels(tb: Table) -> Table:
 
     # Drop years with no country classification
     tb = tb.dropna(subset="classification").reset_index(drop=True)
+
+    return tb
+
+
+def add_country_counts_and_population_by_status(
+    tb: Table,
+    columns: List[str],
+    ds_regions: Dataset,
+    ds_population: Dataset,
+    regions: List[str],
+    missing_data_on_columns: bool = False,
+) -> Table:
+    """
+    Add country counts and population by status for the columns in the list
+    """
+
+    tb_regions = tb.copy()
+
+    tb_regions = geo.add_population_to_table(
+        tb=tb_regions, ds_population=ds_population, warn_on_missing_countries=False
+    )
+
+    # Define empty dictionaries for each of the columns
+    columns_count_dict = {columns[i]: [] for i in range(len(columns))}
+    columns_pop_dict = {columns[i]: [] for i in range(len(columns))}
+    for col in columns:
+        if missing_data_on_columns:
+            # Fill nan values with "missing"
+            tb_regions[col] = tb_regions[col].fillna("missing")
+        # Get the unique values in the column
+        status_list = list(tb_regions[col].unique())
+        for status in status_list:
+            # Calculate count and population for each status in the column
+            tb_regions[f"{col}_{status}_count"] = tb_regions[col].apply(lambda x: 1 if x == status else 0)
+            tb_regions[f"{col}_{status}_pop"] = tb_regions[f"{col}_{status}_count"] * tb_regions["population"]
+
+            # Add the new columns to the list
+            columns_count_dict[col].append(f"{col}_{status}_count")
+            columns_pop_dict[col].append(f"{col}_{status}_pop")
+
+    # Create a new list with all the count columns and population columns
+    columns_count = [item for sublist in columns_count_dict.values() for item in sublist]
+    columns_pop = [item for sublist in columns_pop_dict.values() for item in sublist]
+
+    aggregations = dict.fromkeys(
+        columns_count + columns_pop + ["population"],
+        "sum",
+    )
+
+    tb_regions = geo.add_regions_to_table(
+        tb=tb_regions,
+        ds_regions=ds_regions,
+        regions=regions,
+        aggregations=aggregations,
+        frac_allowed_nans_per_year=FRAC_ALLOWED_NANS_PER_YEAR,
+    )
+
+    # Remove population column
+    tb_regions = tb_regions.drop(columns=["population"])
+
+    # Add population again
+    tb_regions = geo.add_population_to_table(
+        tb=tb_regions, ds_population=ds_population, warn_on_missing_countries=False
+    )
+
+    # Calculate the missing population for each region
+    for col in columns:
+        # Calculate the missing population for each column, by subtracting the population of the countries with data from the total population
+        tb_regions[f"{col}_missing_pop_other_countries"] = tb_regions["population"] - tb_regions[
+            columns_pop_dict[col]
+        ].sum(axis=1)
+        if missing_data_on_columns:
+            tb_regions[f"{col}_missing_pop"] = (
+                tb_regions[f"{col}_missing_pop"] + tb_regions[f"{col}_missing_pop_other_countries"]
+            )
+
+        else:
+            # Rename column
+            tb_regions = tb_regions.rename(columns={f"{col}_missing_pop_other_countries": f"{col}_missing_pop"})
+
+            # Append this missing population column to the list of population columns
+            columns_pop.append(f"{col}_missing_pop")
+
+    # Keep only the regions in the country column
+    tb_regions = tb_regions[tb_regions["country"].isin(REGIONS)].copy().reset_index(drop=True)
+
+    # Keep only the columns I need
+    tb_regions = tb_regions[["country", "year"] + columns_count + columns_pop]
+
+    # Merge the two tables
+    tb = pr.merge(tb, tb_regions, on=["country", "year"], how="outer")
 
     return tb
