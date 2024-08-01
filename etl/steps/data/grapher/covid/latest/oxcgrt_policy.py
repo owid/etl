8570b790e679ee8276150e2d2cdf0f069cf77d29
lucasrodes <lucasrodes@users.noreply.github.com>
2024-08-01 16:52:55 +0200

    grapher

---
+++ etl/steps/data/grapher/covid/latest/oxcgrt_policy.py
@@ -0,0 +1,35 @@
+"""Load a garden dataset and create a grapher dataset."""
+
+from etl.helpers import PathFinder, create_dataset
+
+from .shared import to_grapher_date
+
+# Get paths and naming conventions for current step.
+paths = PathFinder(__file__)
+
+
+def run(dest_dir: str) -> None:
+    #
+    # Load inputs.
+    #
+    # Load garden dataset.
+    ds_garden = paths.load_dataset("oxcgrt_policy")
+
+    # Read table from garden dataset.
+    tb = ds_garden["oxcgrt_policy"]
+
+    #
+    # Process data.
+    #
+    tb = to_grapher_date(tb, "2020-01-01")
+
+    #
+    # Save outputs.
+    #
+    # Create a new grapher dataset with the same metadata as the garden dataset.
+    ds_grapher = create_dataset(
+        dest_dir, tables=[tb], check_variables_metadata=True, default_metadata=ds_garden.metadata
+    )
+
+    # Save changes in the new grapher dataset.
+    ds_grapher.save()
