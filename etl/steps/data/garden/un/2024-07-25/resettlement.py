4e30da05635d4751d8f14f76107f057dbabe7e04
Tuna <50630133+antea04@users.noreply.github.com>
2024-08-05 14:33:01 +0100

    :bar_chart: Adding UN HCR & Unicef data on migration (#3026)
    * :bar_chart: include unhcr data
    
    * :bar_chart: add metadata and resettlement data
    
    * Start a new staging server for branch 'migration-expl-unhcr'
    
    * :bar_chart: add per capita measures and metadata
    
    * :bar_chart: add resettlement data and snapshot unicef
    
    * :bar_chart: add unicef migration data
    
    * :sparkles: add/ improve metadata
    
    * :sparkles: add/ improve metadata
    
    * :bug: re-add diarrhea excluded countries file
    
    * :bar_chart: include 5y rolling average calculation and add metadata for that
    
    * :sparkles: review comments as per Pablos suggestions
    
    * :bug: fix unit scaling
    
    * :sparkles: add numDecimalPlaces to metadata file
    
    ---------
    
    Co-authored-by: Tuna Acisu <tuna.acisu@ourworldindata.com>

---
+++ etl/steps/data/garden/un/2024-07-25/resettlement.py
@@ -0,0 +1,71 @@
+"""Load a meadow dataset and create a garden dataset."""
+from owid.catalog import processing as pr
+
+from etl.data_helpers import geo
+from etl.helpers import PathFinder, create_dataset
+
+# Get paths and naming conventions for current step.
+paths = PathFinder(__file__)
+
+
+def run(dest_dir: str) -> None:
+    #
+    # Load inputs.
+    #
+    # Load meadow dataset.
+    ds_meadow = paths.load_dataset("resettlement")
+
+    # Read table from meadow dataset.
+    tb = ds_meadow.read_table("resettlement")
+
+    # group table by country and year
+    tb_origin = (
+        tb.drop(columns=["country_of_asylum"]).groupby(["country_of_origin", "year"], observed=True).sum().reset_index()
+    )
+    tb_asylum = (
+        tb.drop(columns=["country_of_origin"]).groupby(["country_of_asylum", "year"], observed=True).sum().reset_index()
+    )
+
+    # harmonize countries
+    tb_origin = geo.harmonize_countries(
+        df=tb_origin,
+        country_col="country_of_origin",
+        countries_file=paths.country_mapping_path,
+        warn_on_unused_countries=False,
+    )
+    tb_asylum = geo.harmonize_countries(
+        df=tb_asylum,
+        country_col="country_of_asylum",
+        countries_file=paths.country_mapping_path,
+        warn_on_unused_countries=False,
+    )
+
+    # merge tables
+    tb = pr.merge(
+        tb_origin,
+        tb_asylum,
+        left_on=["country_of_origin", "year"],
+        right_on=["country_of_asylum", "year"],
+        how="outer",
+        suffixes=("_origin", "_dest"),
+    )
+
+    # merge country column (data is split between origin and asylum in columns)
+    tb["country_of_origin"] = tb["country_of_origin"].fillna(tb["country_of_asylum"])
+    tb = tb.rename(columns={"country_of_origin": "country"}, errors="raise")
+
+    # drop country of asylum and idps destination (since it is identical to idps origin) column
+    tb = tb.drop(columns=["country_of_asylum", "returned_idpss_dest"], errors="raise")
+
+    tb = tb.format(["country", "year"])
+
+    #
+    # Save outputs.
+    #
+    # Create a new garden dataset with the same metadata as the meadow dataset.
+    ds_garden = create_dataset(
+        dest_dir, tables=[tb], check_variables_metadata=True, default_metadata=ds_meadow.metadata
+    )
+
+    # Save changes in the new garden dataset.
+    ds_garden.save()
