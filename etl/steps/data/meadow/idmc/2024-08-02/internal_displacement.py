08ea0ec65d424d48e4f45e59fb580c44abd5155a
Tuna Acisu <tuna.acisu@ourworldindata.com>
2024-08-05 16:20:32 +0100

    :bar_chart: add population shares

--- etl/steps/data/meadow/idmc/2024-08-02/internal_displacement.py
+++ etl/steps/data/meadow/idmc/2024-08-02/internal_displacement.py
@@ -1,4 +1,4 @@
-"""Load a snapshot and create a meadow dataset."""
+"""Load a snapshot and create meadow dataset. This is meadow and garden step in one, since data manipulation is minimal."""
 
 from etl.data_helpers import geo
 from etl.helpers import PathFinder, create_dataset
@@ -14,6 +14,15 @@ COLUMNS_TO_DROP = [
     "Disaster Stock Displacement (Raw)",
 ]
 
+COL_RENAMES = {
+    "Name": "country",
+    "Year": "year",
+    "Conflict Stock Displacement": "conflict_stock_displacement",
+    "Conflict Internal Displacements": "conflict_internal_displacements",
+    "Disaster Internal Displacements": "disaster_internal_displacements",
+    "Disaster Stock Displacement": "disaster_stock_displacement",
+}
+
 
 def run(dest_dir: str) -> None:
     #
@@ -21,17 +30,35 @@ def run(dest_dir: str) -> None:
     #
     # Retrieve snapshot.
     snap = paths.load_snapshot("internal_displacement.xlsx")
+    ds_pop = paths.load_dataset("population")
 
     # Load data from snapshot.
     tb = snap.read()
 
     # rename and drop columns
-    tb = tb.rename(columns={"Name": "country", "Year": "year"})
-    tb = tb.drop(columns=COLUMNS_TO_DROP)
+    tb = tb.drop(columns=COLUMNS_TO_DROP, errors="raise")
+    tb = tb.rename(columns=COL_RENAMES, errors="raise")
 
+    # Harmonize country names.
     tb = geo.harmonize_countries(
         df=tb, countries_file=paths.country_mapping_path, warn_on_missing_countries=True, warn_on_unused_countries=True
     )
+
+    # calculate population averages
+    tb = geo.add_population_to_table(tb, ds_pop)
+
+    tb["total_stock_displacement"] = tb["conflict_stock_displacement"] + tb["disaster_stock_displacement"]
+    tb["total_internal_displacements"] = tb["conflict_internal_displacements"] + tb["disaster_internal_displacements"]
+
+    tb["share_of_internally_displaced_pop"] = tb["total_stock_displacement"] / tb["population"] * 100
+    tb["share_of_conflict_displaced_pop"] = tb["conflict_stock_displacement"] / tb["population"] * 100
+    tb["share_of_disaster_displaced_pop"] = tb["disaster_stock_displacement"] / tb["population"] * 100
+
+    tb["displacements_per_100_people"] = tb["total_internal_displacements"] / tb["population"] * 100
+    tb["conflict_displacements_per_100_people"] = tb["conflict_internal_displacements"] / tb["population"] * 100
+    tb["disaster_displacements_per_100_people"] = tb["disaster_internal_displacements"] / tb["population"] * 100
+
+    tb = tb.drop(columns=["population"], errors="raise")
 
     # Ensure all columns are snake-case, set an appropriate index, and sort conveniently.
     tb = tb.format(["country", "year"])
