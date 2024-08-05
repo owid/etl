3ab5b07332328b4bf6f768a347c480a2a5d09ea8
Pablo Arriagada <paarriagadap@gmail.com>
2024-08-05 16:53:28 +0100

    :sparkles: meadow

--- etl/steps/data/meadow/chartbook/2024-08-05/atkinson_2008_australia.py
+++ etl/steps/data/meadow/chartbook/2024-08-05/atkinson_2008_australia.py
@@ -1,5 +1,7 @@
 """Load a snapshot and create a meadow dataset."""
 
+from owid.catalog import Table
+
 from etl.helpers import PathFinder, create_dataset
 
 # Get paths and naming conventions for current step.
@@ -15,29 +17,38 @@ def run(dest_dir: str) -> None:
 
     # Load data from snapshot.
     tb_oecd_lms = snap.read(sheet_name="Table A.3 (OECD LMS)", usecols="C:I", skiprows=2)
+    tb_eeh = snap.read(sheet_name="Table A.5 (EEH)", usecols="C:I", skiprows=3)
 
     #
     # Process data.
     #
     # Rename Unnamed:2 to year
-    tb_oecd_lms = tb_oecd_lms.rename(columns={"Unnamed: 2": "year"})
-
-    # Add country column
-    tb_oecd_lms["country"] = "Australia"
-
-    # Remove missing year columns
-    tb_oecd_lms = tb_oecd_lms.dropna(subset=["year"])
-
-    # Ensure all columns are snake-case, set an appropriate index, and sort conveniently.
-    tb_oecd_lms = tb_oecd_lms.format(["country", "year"], short_name="oecd_lms")
+    tb_oecd_lms = rename_year_and_add_country(tb=tb_oecd_lms, short_name="oecd_lms")
+    tb_eeh = rename_year_and_add_country(tb=tb_eeh, short_name="eeh")
 
     #
     # Save outputs.
     #
     # Create a new meadow dataset with the same metadata as the snapshot.
     ds_meadow = create_dataset(
-        dest_dir, tables=[tb_oecd_lms], check_variables_metadata=True, default_metadata=snap.metadata
+        dest_dir, tables=[tb_oecd_lms, tb_eeh], check_variables_metadata=True, default_metadata=snap.metadata
     )
 
     # Save changes in the new meadow dataset.
     ds_meadow.save()
+
+
+def rename_year_and_add_country(tb: Table, short_name: str) -> Table:
+    """
+    Rename the unnamed column to year and add a country column with the value Australia
+    Also, remove missing year columns, create indices and add short_name
+    """
+
+    tb = tb.rename(columns={"Unnamed: 2": "year"})
+    tb["country"] = "Australia"
+    tb = tb.dropna(subset=["year"])
+
+    # Ensure all columns are snake-case, set an appropriate index, and sort conveniently.
+    tb = tb.format(["country", "year"], short_name=short_name)
+
+    return tb
