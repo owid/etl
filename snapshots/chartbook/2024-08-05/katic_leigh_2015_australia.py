74935fd051e35dd6dc7cbbfbd7fc1ee4b063448e
Pablo Arriagada <63430031+paarriagadap@users.noreply.github.com>
2024-08-06 12:02:26 +0100

    :bar_chart: chartbook: Upload Australian data from Katic and Leigh (2015) (#3073)
    * empty
    
    * :sparkles: snapshot
    
    * :sparkles: meadow
    
    * :sparkles: garden
    
    * :lipstick: reorder chartbook dag

--- snapshots/chartbook/2024-08-05/katic_leigh_2015_australia.py
+++ snapshots/chartbook/2024-08-05/katic_leigh_2015_australia.py
@@ -1,10 +1,10 @@
 """
 Script to create a snapshot of dataset.
 
-The file comes from Appendix A in the original book, available to buy here: https://academic.oup.com/book/38708.
-I use a xls file from the data extracted in the past by the Chartbook team. See https://docs.google.com/spreadsheets/d/1_WIBAjDLO7ufWuBFhRFr-GMl6dLgLz47-uF5EvC48ZY/edit?gid=0#gid=0
+The file comes from the original paper, available here: https://onlinelibrary.wiley.com/doi/abs/10.1111/roiw.12177.
+I use a xlsx file from the data extracted in the past by the Chartbook team. See https://docs.google.com/spreadsheets/d/1_WIBAjDLO7ufWuBFhRFr-GMl6dLgLz47-uF5EvC48ZY/edit?gid=0#gid=0
 After creating the file, run
-    python snapshots/chartbook/2024-08-05/atkinson_2008_australia.py --path-to-file <path-to-file>
+    python snapshots/chartbook/2024-08-05/katic_leigh_2015_australia.py --path-to-file <path-to-file>
 """
 
 from pathlib import Path
@@ -22,7 +22,7 @@ SNAPSHOT_VERSION = Path(__file__).parent.name
 @click.option("--path-to-file", prompt=True, type=str, help="Path to local data file.")
 def main(path_to_file: str, upload: bool) -> None:
     # Create a new snapshot.
-    snap = Snapshot(f"chartbook/{SNAPSHOT_VERSION}/atkinson_2008_australia.xls")
+    snap = Snapshot(f"chartbook/{SNAPSHOT_VERSION}/katic_leigh_2015_australia.xlsx")
 
     # Copy local data file to snapshots data folder, add file to DVC and upload to S3.
     snap.create_snapshot(filename=path_to_file, upload=upload)
