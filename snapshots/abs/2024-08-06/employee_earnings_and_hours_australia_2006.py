2d43f356b88b5774149b16f229b5af0a94b5ed23
Pablo Arriagada <paarriagadap@gmail.com>
2024-08-07 17:26:22 +0100

    :sparkles: earnings meadow

--- snapshots/abs/2024-08-06/employee_earnings_and_hours_australia_2006.py
+++ snapshots/abs/2024-08-06/employee_earnings_and_hours_australia_2006.py
@@ -5,8 +5,8 @@ The data for this snapshot is created manually in a csv file by copying the data
 This data is available in page 17 (all occupations) here https://www.ausstats.abs.gov.au/ausstats/subscriber.nsf/0/284EA51F2E7BD8F9CA25728F000D10AC/$File/63060_May%202006.pdf.
 
 I save the file in the format:
-country,year,indicator,All occupations
-Australia,2006,50th percentile (median),755.0
+country,year,indicator,value
+Australia,2006,50th percentile (median) (2nd quartile),755.0
 Australia,2006,90th percentile,1521.0
 
 The datasets for 2010+ are available as Excel files, so there is no need to manually create the data.
