1d61b6e4af2c263442a3b38a04290da1ec9b073c
Pablo Arriagada <paarriagadap@gmail.com>
2024-08-07 10:16:17 +0100

    :construction: meadow 17-18

--- etl/steps/data/meadow/abs/2024-08-06/household_income_and_wealth_australia_2017_2018.py
+++ etl/steps/data/meadow/abs/2024-08-06/household_income_and_wealth_australia_2017_2018.py
@@ -1,5 +1,7 @@
 """Load a snapshot and create a meadow dataset."""
 
+from owid.catalog import Table
+
 from etl.helpers import PathFinder, create_dataset
 
 # Get paths and naming conventions for current step.
@@ -11,11 +13,35 @@ def run(dest_dir: str) -> None:
     # Load inputs.
     #
     # Retrieve snapshot.
-    snap = paths.load_snapshot("household_income_australia_2017_2018.xlsx")
+    snap_income = paths.load_snapshot("household_income_australia_2017_2018.xlsx")
+    snap_wealth = paths.load_snapshot("household_wealth_australia_2017_2018.xlsx")
 
     # Load data from snapshot.
-    tb = snap.read(sheet_name="Table 1.1", skiprows=4, nrows=43)
+    tb_income = snap_income.read(sheet_name="Table 1.1", skiprows=4, nrows=43)
+    tb_wealth = snap_wealth.read(sheet_name="Table 2.2 ", skiprows=4, nrows=41)
 
+    #
+    # Process data.
+    #
+    tb_income = reformat_tables(tb=tb_income, short_name="income")
+    tb_wealth = reformat_tables(tb=tb_wealth, short_name="wealth")
+
+    #
+    # Save outputs.
+    #
+    # Create a new meadow dataset with the same metadata as the snapshot.
+    ds_meadow = create_dataset(
+        dest_dir, tables=[tb_income, tb_wealth], check_variables_metadata=True, default_metadata=snap_income.metadata
+    )
+
+    # Save changes in the new meadow dataset.
+    ds_meadow.save()
+
+
+def reformat_tables(tb: Table, short_name: str) -> Table:
+    """
+    Rename columns, and make tables wide for further processing
+    """
     # Rename unnamed columns.
     tb = tb.rename(columns={"Unnamed: 0": "indicator", "Unnamed: 1": "unit"})
 
@@ -40,17 +66,7 @@ def run(dest_dir: str) -> None:
     # Add country
     tb["country"] = "Australia"
 
-    #
-    # Process data.
-    #
     # Ensure all columns are snake-case, set an appropriate index, and sort conveniently.
-    tb = tb.format(["country", "year"])
+    tb = tb.format(["country", "year"], short_name=short_name)
 
-    #
-    # Save outputs.
-    #
-    # Create a new meadow dataset with the same metadata as the snapshot.
-    ds_meadow = create_dataset(dest_dir, tables=[tb], check_variables_metadata=True, default_metadata=snap.metadata)
-
-    # Save changes in the new meadow dataset.
-    ds_meadow.save()
+    return tb
