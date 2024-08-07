447a91b080409854b3c5f5df857dc72de155c6bd
spoonerf <spoonerfiona@gmail.com>
2024-08-07 13:25:07 +0100

    minor changes

--- etl/steps/data/garden/forests/2024-07-10/tree_cover_loss_by_driver.py
+++ etl/steps/data/garden/forests/2024-07-10/tree_cover_loss_by_driver.py
@@ -42,7 +42,7 @@ def run(dest_dir: str) -> None:
         df=tb, countries_file=paths.country_mapping_path, excluded_countries_file=paths.excluded_countries_path
     )
     # Some regions are broken down into smaller regions in the dataset, so we need to aggregate them here e.g. Alaska and Hawaii are recorded separately in the dataset, but the geo.harmonize_countries function renames them as United States
-    tb = tb.groupby(["country", "year", "category"]).sum().reset_index()
+    tb = tb.groupby(["country", "year", "category"], observed=True).sum().reset_index()
     tb = tb.pivot(index=["country", "year"], columns="category", values="area").reset_index()
     tb = geo.add_regions_to_table(
         tb, ds_regions=ds_regions, ds_income_groups=ds_income_groups, regions=REGIONS, min_num_values_per_year=1
@@ -59,16 +59,3 @@ def run(dest_dir: str) -> None:
 
     # Save changes in the new garden dataset.
     ds_garden.save()
-
-
-def convert_codes_to_drivers(tb: Table) -> Table:
-    """ """
-    code_dict = {
-        1: "commodity_driven_deforestation",
-        2: "shifting_agriculture",
-        3: "forestry",
-        4: "wildfire",
-        5: "urbanization",
-    }
-    tb["category"] = tb["category"].replace(code_dict)
-    return tb
