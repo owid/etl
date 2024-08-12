5ea506919099b45ea77a229e371f6bcb81f5563a
spoonerf <spoonerfiona@gmail.com>
2024-08-12 10:56:02 +0100

    adding in regional and global totals

--- etl/steps/data/garden/language/2024-07-17/ethnologue.py
+++ etl/steps/data/garden/language/2024-07-17/ethnologue.py
@@ -1,6 +1,7 @@
 """Load a meadow dataset and create a garden dataset."""
 
-from owid.catalog import Table
+from owid.catalog import Dataset, Table
+from owid.catalog import processing as pr
 
 from etl.data_helpers import geo
 from etl.helpers import PathFinder, create_dataset
@@ -8,7 +9,7 @@ from etl.helpers import PathFinder, create_dataset
 # Get paths and naming conventions for current step.
 paths = PathFinder(__file__)
 
-REGIONS = ["Africa", "Asia", "Europe", "North America", "Oceania", "South America", "World"]
+REGIONS = ["Africa", "Asia", "Europe", "North America", "Oceania", "South America"]
 
 
 def run(dest_dir: str) -> None:
@@ -34,6 +35,7 @@ def run(dest_dir: str) -> None:
         countries_file=paths.country_mapping_path,
         excluded_countries_file=paths.excluded_countries_path,
     )
+    tb_country_codes = add_region_to_table(ds_regions, tb_country_codes)
 
     # countries by status living and extinct
     tb_lang_by_status = extinct_and_living_languages_per_country(tb_language_index, tb_language_codes, tb_country_codes)
@@ -60,25 +62,54 @@ def run(dest_dir: str) -> None:
     ds_garden.save()
 
 
-def countries_per_region():
-    return
+def add_region_to_table(ds_regions: Dataset, tb_country_codes: Table):
+    for region in REGIONS:
+        countries_in_region = geo.list_members_of_region(ds_regions=ds_regions, region=region)
+        tb_country_codes.loc[tb_country_codes["country"].isin(countries_in_region), "region"] = region
+    return tb_country_codes
+
+
+def languages_per_region(tb_language_index: Table, tb_language_codes: Table, tb_country_codes: Table):
+    tb_languages_per_region = (
+        tb_language_index.merge(tb_language_codes, on=["langid"], how="outer", suffixes=("", "_lang"))
+        .merge(tb_country_codes, on=["countryid"])
+        .drop(columns=["countryid", "nametype", "name", "countryid_lang", "name_lang", "country"])
+        .drop_duplicates()
+        .groupby(["region", "langstatus"], observed=True)["langid"]
+        .nunique()
+        .unstack(fill_value=0)
+        .rename(columns={"L": "living", "X": "extinct"})
+        .reset_index()
+        .rename(columns={"region": "country"})
+    )
+    return tb_languages_per_region
 
 
 def extinct_and_living_languages_per_country(
     tb_language_index: Table, tb_language_codes: Table, tb_country_codes: Table
 ) -> Table:
     tb_extinct_living_languages = (
-        tb_language_index.merge(tb_language_codes, on=["langid"], how="outer", suffixes=("", "_lang"))
+        tb_language_index.merge(tb_language_codes, on="langid", how="outer", suffixes=("", "_lang"))
         .drop(columns=["nametype", "name", "name_lang"])
         .drop_duplicates()
-        .groupby(["countryid", "langstatus"], observed=True)
-        .agg({"langid": "nunique"})
+        .groupby(["countryid", "langstatus"], observed=True)["langid"]
+        .nunique()
+        .unstack(fill_value=0)
+        .rename(columns={"L": "living", "X": "extinct"})
         .reset_index()
-        .rename(columns={"langid": "number_of_languages"})
-        .replace({"langstatus": {"L": "living", "X": "extinct"}})
         .merge(tb_country_codes, on="countryid", how="left")
-        .pivot(index="country", columns="langstatus", values="number_of_languages")
-        .fillna(0)
-        .reset_index()
+        .drop(columns=["countryid", "region"])
     )
-    return tb_extinct_living_languages
+
+    tb_region = languages_per_region(tb_language_index, tb_language_codes, tb_country_codes)
+
+    tb_global = tb_language_codes.groupby(["langstatus"], observed=True)["langid"].nunique().reset_index()
+    tb_global["country"] = "World"
+    tb_global = (
+        tb_global.pivot(index="country", columns=["langstatus"], values="langid")
+        .reset_index()
+        .rename(columns={"L": "living", "X": "extinct"})
+    )
+
+    tb_combined = pr.concat([tb_extinct_living_languages, tb_region, tb_global], ignore_index=True)
+    return tb_combined
