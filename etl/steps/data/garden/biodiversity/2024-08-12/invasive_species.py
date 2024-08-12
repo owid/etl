e0cbe1c9b1077f40ffae7e4d6cf328b13b257b4b
spoonerf <spoonerfiona@gmail.com>
2024-08-12 16:18:20 +0100

    doing cumulative sum properly

--- etl/steps/data/garden/biodiversity/2024-08-12/invasive_species.py
+++ etl/steps/data/garden/biodiversity/2024-08-12/invasive_species.py
@@ -1,5 +1,7 @@
 """Load a meadow dataset and create a garden dataset."""
 
+from owid.catalog import processing as pr
+
 from etl.helpers import PathFinder, create_dataset
 
 # Get paths and naming conventions for current step.
@@ -17,11 +19,20 @@ def run(dest_dir: str) -> None:
     tb = ds_meadow["invasive_species"].reset_index()
 
     #
-    # Add cumulative
+    # Add cumulative sum for each species
+    tables = []
     cols = tb.columns.drop(["continent", "year"]).tolist()
+    regions = tb["continent"].unique().tolist()
     for col in cols:
-        tb[f"{col}_cumulative"] = tb.groupby("continent")[col].apply(lambda x: x.fillna(0).cumsum())
+        for region in regions:
+            tb_slice = tb.loc[tb["continent"] == region].copy()  # Using .copy() to avoid SettingWithCopyWarning
 
+            # Compute cumulative sum and assign it to a new column
+            tb_slice.loc[:, f"{col}_cumulative"] = tb_slice[col].fillna(0).cumsum()
+
+            # Append the modified slice to the tables list
+            tables.append(tb_slice)
+    tb = pr.concat(tables, ignore_index=True)
     # Process data.
     #
     tb = tb.format(["continent", "year"])
