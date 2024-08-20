902f63e7599bf5216ff72840ab8e409ecab6196e
Pablo Arriagada <paarriagadap@gmail.com>
2024-08-20 12:19:26 +0100

    :bug: fix snapshot names

--- snapshots/chartbook/2024-08-20/soepmonitor_1984_2013_germany_relative_poverty.py
+++ snapshots/chartbook/2024-08-20/soepmonitor_1984_2013_germany_relative_poverty.py
@@ -4,7 +4,7 @@ Script to create a snapshot of dataset.
 The file comes this pdf (page 91): https://drive.google.com/file/d/1sTz9-eXCLYFhH7nVQ0hWSvSQXFEHIxsX/view.
 I use a csv file from the data extracted in the past by the Chartbook team. See https://docs.google.com/spreadsheets/d/1g8gGUHRye1L7hEu3HqYgM56RvjHrc3LD85kBwRZcYj4/edit?gid=1888715824#gid=1888715824
 After creating the file, run
-    python snapshots/chartbook/2024-08-20/soepmonitor_1984_1983_germany_relative_poverty.py --path-to-file <path-to-file>
+    python snapshots/chartbook/2024-08-20/soepmonitor_1984_2013_germany_relative_poverty.py --path-to-file <path-to-file>
 """
 
 from pathlib import Path
@@ -22,7 +22,7 @@ SNAPSHOT_VERSION = Path(__file__).parent.name
 @click.option("--path-to-file", prompt=True, type=str, help="Path to local data file.")
 def main(path_to_file: str, upload: bool) -> None:
     # Create a new snapshot.
-    snap = Snapshot(f"chartbook/{SNAPSHOT_VERSION}/soepmonitor_1984_1983_germany_relative_poverty.csv")
+    snap = Snapshot(f"chartbook/{SNAPSHOT_VERSION}/soepmonitor_1984_2013_germany_relative_poverty.csv")
 
     # Copy local data file to snapshots data folder, add file to DVC and upload to S3.
     snap.create_snapshot(filename=path_to_file, upload=upload)
