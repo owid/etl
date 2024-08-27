aa829842278c866938f3f2da57f158564da9aef7
Pablo Rosado <pabloarosado@gmail.com>
2024-08-27 16:46:49 +0200

    Create minerals supply and demand prospects (WIP)

--- etl/steps/data/grapher/iea/2024-07-04/critical_minerals.py
+++ etl/steps/data/grapher/iea/2024-07-04/critical_minerals.py
@@ -1,5 +1,8 @@
 """Load a garden dataset and create a grapher dataset."""
 
+import owid.catalog.processing as pr
+from owid.catalog import Table
+
 from etl.helpers import PathFinder, create_dataset
 
 # Get paths and naming conventions for current step.
@@ -18,35 +21,146 @@ def run(dest_dir: str) -> None:
     #
     # Process data.
     #
-    def create_demand_of_copper_as_share_of_supply(tb_supply, tb_demand):
-        tb_supply = tb_supply[
+    # TODO: We will create two tables:
+    #  * Demand by technology. Explorer will have mineral, case, scenario, and metric.
+    #  * Supply by country.
+    #
+    #  We will then create a "Mineral supply and demand prospects" explorer, with the following elements:
+    #  * A radio button for "Demand by tech" (demand table) and "Supply by country" (supply table).
+    #  * A "Mineral" dropdown.
+    #  * A "Case" dropdown (which will be only "Base case" for the supply table).
+    #  * A "Scenario" dropdown (which will be only "All scenarios" (or empty?) for the supply table).
+    #  * A "Metric" dropdown, with options "Total", "Share of global demand", and "Share of global supply".
+
+    def create_demand_by_technology_table(tb_demand: Table, tb_supply: Table) -> Table:
+        tb_demand = tb_demand.copy()
+        tb_supply = tb_supply.copy()
+
+        # Remove unnecessary country column.
+        tb_demand = tb_demand.drop(columns=["country"], errors="raise")
+        # Drop rows with no demand data.
+        tb_demand = tb_demand.dropna(subset="demand").reset_index(drop=True)
+        # We want the current year to appear in all scenarios (and therefore drop "Current" scenario).
+        tb_demand_current = tb_demand[tb_demand["scenario"] == "Current"].reset_index(drop=True)
+        tb_demand = tb_demand[tb_demand["scenario"] != "Current"].reset_index(drop=True)
+        for scenario in sorted(set(tb_demand[tb_demand["scenario"] != "Current"]["scenario"])):
+            tb_demand = pr.concat([tb_demand, tb_demand_current.assign(**{"scenario": scenario})], ignore_index=True)
+        # After doing this, some combinations for which there was no data for a given scenario will have only data for 2023.
+        # Drop these cases, where there is only one row (one year) for a given case-mineral-scenario-technology combination.
+        tb_demand = tb_demand[
+            tb_demand.groupby(["case", "mineral", "scenario", "technology"], observed=True, as_index=False)[
+                "year"
+            ].transform("nunique")
+            > 1
+        ].reset_index(drop=True)
+
+        # For each case-scenario-mineral-year, we need the global demand of all technologies.
+        # NOTE: Global mineral demand, including uses outside clean tech, is only given for "Base case" for a few minerals (for which "Other uses" is explicitly given).
+        minerals_with_total_demand = tb_demand[tb_demand["technology"] == "Other uses"]["mineral"].unique().tolist()
+        tb_demand_global = (
+            tb_demand[(tb_demand["case"] == "Base case") & (tb_demand["mineral"].isin(minerals_with_total_demand))]
+            .groupby(["case", "scenario", "mineral", "year"], observed=True, as_index=False)
+            .agg({"demand": "sum"})
+            .rename(columns={"demand": "global_demand"})
+        )
+
+        # From the original supply table, we need the global supply of each mineral-year (given only for the "Base case").
+        # Supply data for each mineral is divided in "mining" and "refining".
+        # TODO: For consistency with the current minerals explorer, we should rename them "Mine" and "Refinery".
+        # When combining with demand data, we can simply take "refining" values, as they are the ones actually demanded to produce the technology.
+        # But there are two special cases:
+        # * For lithium, supply data is divided in "mining" and "chemicals". For consistency, we can rename them "mining" and "refining" and add a footnote.
+        # * For graphite, supply data is divided in "mining (natural)" and "battery grade". For consistency, we can rename them "mining" and "refining" and add a footnote.
+        # TODO: Add those footnotes.
+        tb_supply_global = tb_supply.astype({"process": "string"}).copy()
+        tb_supply_global.loc[
+            (tb_supply["mineral"] == "Lithium") & (tb_supply["process"] == "Chemicals"), "process"
+        ] = "Refining"
+        tb_supply_global.loc[
+            (tb_supply["mineral"] == "Graphite") & (tb_supply["process"] == "Battery grade"), "process"
+        ] = "Refining"
+        tb_supply_global = tb_supply_global[tb_supply_global["process"] == "Refining"].reset_index(drop=True)
+        tb_supply_global = tb_supply_global[tb_supply_global["country"] == "World"][
+            ["case", "year", "mineral", "supply"]
+        ].rename(columns={"supply": "global_supply"})
+
+        # Add total demand to the demand table.
+        # NOTE:
+        tb_demand = tb_demand.merge(tb_demand_global, on=["case", "scenario", "mineral", "year"], how="left")
+        # TODO: For "Graphite (all grades: natural and synthetic)", "Other uses" has all the demand. There may be a bug.
+        #  t[t["demand"]==t["global_demand"]]
+        # TODO: After fixing that, consider renaming to "Graphite" and add a subtitle or footnote.
+
+        # Add total supply to the demand table.
+        tb_demand = tb_demand.merge(tb_supply_global, on=["case", "year", "mineral"], how="left")
+
+        # Add "share" columns.
+        tb_demand["demand_as_share_of_global_demand"] = 100 * tb_demand["demand"] / tb_demand["global_demand"]
+        tb_demand["demand_as_share_of_global_supply"] = 100 * tb_demand["demand"] / tb_demand["global_supply"]
+
+        return tb_demand
+
+    # Prepare "Demand by technology" table.
+    tb_demand_by_technology = create_demand_by_technology_table(tb_demand=tb_demand, tb_supply=tb_supply)
+
+    # Create a wide-format table.
+    tb_demand_by_technology_flat = tb_demand_by_technology.pivot(
+        index=["technology", "year"],
+        columns=["mineral", "case", "scenario"],
+        values=["demand", "demand_as_share_of_global_demand", "demand_as_share_of_global_supply"],
+        join_column_levels_with="|",
+    )
+    # Adapt table to grapher.
+    tb_demand_by_technology_flat = tb_demand_by_technology_flat.rename(columns={"technology": "country"})
+
+    # Improve metadata.
+    # TODO: Do this properly.
+    for column in tb_demand_by_technology_flat.drop(columns=["country", "year"]).columns:
+        tb_demand_by_technology_flat[column].m.title = column
+        if "_share_of_" in column:
+            tb_demand_by_technology_flat[column].m.unit = "%"
+            tb_demand_by_technology_flat[column].m.short_unit = "%"
+        else:
+            tb_demand_by_technology_flat[column].m.unit = "tonnes"
+            tb_demand_by_technology_flat[column].m.short_unit = "t"
+
+    # Remove empty columns.
+    tb_demand_by_technology_flat = tb_demand_by_technology_flat.dropna(axis=1, how="all")
+
+    # Improve its format.
+    tb_demand_by_technology_flat = tb_demand_by_technology_flat.format(short_name="demand_by_technology")
+
+    def create_mineral_demand_by_technology(tb_supply, tb_demand):
+        _tb_supply = tb_supply[
             (tb_supply["case"] == "Base case")
             & (tb_supply["country"] == "World")
             & (tb_supply["mineral"] == "Copper")
             & (tb_supply["process"] == "Refining")
         ].reset_index(drop=True)[["year", "supply"]]
-        tb_demand = tb_demand[
+        _tb_demand = tb_demand[
             (tb_demand["case"] == "Base case")
             & (tb_demand["country"] == "World")
             & (tb_demand["mineral"] == "Copper")
             & (tb_demand["scenario"].isin(["Current", "Net zero by 2050"]))
         ][["technology", "year", "demand"]]
-        tb_combined = tb_demand.merge(tb_supply, on=["year"], how="inner")
+        tb_combined = _tb_demand.merge(_tb_supply, on=["year"], how="inner")
         tb_combined["demand_as_share_of_supply"] = 100 * tb_combined["demand"] / tb_combined["supply"]
-        tb_combined = tb_combined[["technology", "year", "demand_as_share_of_supply"]].rename(
+        tb_combined = tb_combined[["technology", "year", "demand", "demand_as_share_of_supply"]].rename(
             columns={"technology": "country"}
         )
-        tb_combined["demand_as_share_of_supply"].m.title = "Demand of copper as share of supply"
+        tb_combined["demand"].m.title = "Demand of copper by technology"
+        tb_combined["demand"].m.unit = "tonnes"
+        tb_combined["demand"].m.short_unit = "t"
+        tb_combined["demand_as_share_of_supply"].m.title = "Demand of copper by technology as share of global supply"
         tb_combined["demand_as_share_of_supply"].m.unit = "%"
         tb_combined["demand_as_share_of_supply"].m.short_unit = "%"
-        tb_combined = tb_combined.sort_values(["year", "demand_as_share_of_supply"]).reset_index(drop=True)
         tb_combined = tb_combined.format(sort_rows=False)
 
         return tb_combined
 
-    tb_demand_of_copper_as_share_of_supply = create_demand_of_copper_as_share_of_supply(tb_supply, tb_demand)
+    # tb_demand_of_copper_by_technology = create_mineral_demand_by_technology(tb_supply, tb_demand)
 
-    def create_supply_of_copper_by_country(tb_supply, tb_demand):
+    def create_mineral_supply_by_country(tb_supply, tb_demand):
         tb_supply_global = tb_supply[
             (tb_supply["case"] == "Base case")
             & (tb_supply["country"] == "World")
@@ -100,24 +214,9 @@ def run(dest_dir: str) -> None:
 
         return tb_supply_share
 
-    tb_supply_share = create_supply_of_copper_by_country(tb_supply, tb_demand)
+    # tb_supply_share = create_mineral_supply_by_country(tb_supply, tb_demand)
 
-    # TODO: Supply data for each mineral is divided in "mining" and "refining". When combining with demand data, we can simply take "refining" values, as they are the ones actually demanded to produce the technology.
-    #  In the case of lithium, supply data is divided in "mining" and "chemicals". For consistency, we can rename "chemicals" -> "refining" and add a footnote.
-    #  In the case of graphite, supply data is divided in "mining (natural)" and "battery grade". For consistency, we can rename "battery grade" -> "refining" and add a footnote.
-    # tb_supply = tb_supply.astype({"process": "string"})
-    # tb_supply.loc[
-    #     (tb_supply["mineral"] == "Lithium") & (tb_supply["process"] == "Chemicals"), "process"
-    # ] = "Refining"
-    # tb_supply.loc[
-    #     (tb_supply["mineral"] == "Graphite") & (tb_supply["process"] == "Battery grade"), "process"
-    # ] = "Refining"
-    # tb_supply_refined = tb_supply[tb_supply["process"] == "Refining"].reset_index(drop=True)
-    # tb_supply_refined_global = tb_supply_refined[tb_supply_refined["country"] == "World"][
-    #     ["case", "year", "mineral", "supply"]
-    # ].rename(columns={"supply": "supply_global"})
-
-    def create_demand_by_technology(tb_demand):
+    def create_technology_demand_by_mineral(tb_demand):
         # TODO: Another explorer (view) could have a checkbox for technology. It shows a stacked are chart of minerals demanded as a share of supply, for the technologies selected.
         # NOTE: Here "global" should probably be "total", since it refers to the sum of all technologies (and all demand data is global), but for simplicity call it "global".
         tb_demand_pv = tb_demand[
@@ -136,15 +235,11 @@ def run(dest_dir: str) -> None:
         return tb_demand_pv
 
     # NOTE: Global total demand is sometimes zero. This introduces nans in the shares.
-    tb_demand_pv = create_demand_by_technology(tb_demand)
+    # tb_demand_pv = create_technology_demand_by_mineral(tb_demand)
 
     #
     # Save outputs.
     #
     # Create a new grapher dataset.
-    ds_grapher = create_dataset(
-        dest_dir,
-        tables=[tb_demand_of_copper_as_share_of_supply, tb_supply_share, tb_demand_pv],
-        check_variables_metadata=True,
-    )
+    ds_grapher = create_dataset(dest_dir, tables=[tb_demand_by_technology_flat], check_variables_metadata=True)
     ds_grapher.save()
---
+++ etl/steps/export/explorers/minerals/latest/minerals_supply_and_demand_prospects.py
@@ -0,0 +1,103 @@
+"""Load a grapher dataset and create an explorer dataset with its tsv file."""
+
+import pandas as pd
+from structlog import get_logger
+
+from etl.helpers import PathFinder, create_explorer
+
+# Initialize log.
+log = get_logger()
+
+# Get paths and naming conventions for current step.
+paths = PathFinder(__file__)
+
+# Prefix used for "share" columns.
+# NOTE: This must coincide with the same variable as defined in the garden minerals step.
+SHARE_OF_GLOBAL_PREFIX = "share of global "
+
+
+def run(dest_dir: str) -> None:
+    #
+    # Load inputs.
+    #
+    # Load minerals grapher dataset and read its main table.
+    ds = paths.load_dataset("critical_minerals")
+    tb = ds.read_table("demand_by_technology")
+
+    #
+    # Process data.
+    #
+    # Prepare graphers table of explorer.
+    variable_ids = []
+    metric_dropdown = []
+    mineral_dropdown = []
+    case_dropdown = []
+    scenario_dropdown = []
+    map_tab = []
+    for column in tb.drop(columns=["country", "year"]).columns:
+        metric, mineral, case, scenario = tb[column].metadata.title.split("|")
+        metric = metric.replace("_", " ").capitalize()
+
+        # Append extracted values.
+        variable_ids.append([f"{ds.metadata.uri}/{tb.metadata.short_name}#{column}"])
+        metric_dropdown.append(metric)
+        mineral_dropdown.append(mineral)
+        case_dropdown.append(case)
+        scenario_dropdown.append(scenario)
+        map_tab.append(False)
+
+    df_graphers = pd.DataFrame()
+    df_graphers["yVariableIds"] = variable_ids
+    df_graphers["Mineral Dropdown"] = mineral_dropdown
+    df_graphers["Case Dropdown"] = case_dropdown
+    df_graphers["Scenario Dropdown"] = scenario_dropdown
+    df_graphers["Metric Dropdown"] = metric_dropdown
+    df_graphers["hasMapTab"] = map_tab
+
+    # Impose that all line charts start at zero.
+    df_graphers["yAxisMin"] = 0
+
+    # Make all views stacked area charts.
+    df_graphers["type"] = "StackedArea"
+
+    # Sanity check.
+    error = "Duplicated rows in explorer."
+    assert df_graphers[
+        df_graphers.duplicated(
+            subset=["Case Dropdown", "Scenario Dropdown", "Mineral Dropdown", "Metric Dropdown"], keep=False
+        )
+    ].empty, error
+
+    # Sort rows conveniently.
+    # df_graphers["Metric Dropdown"] = pd.Categorical(
+    #     df_graphers["Metric Dropdown"],
+    #     categories=["Production", "Reserves", "Unit value"],
+    #     ordered=True,
+    # )
+    # df_graphers = df_graphers.sort_values(["Mineral Dropdown", "Metric Dropdown", "Type Dropdown"]).reset_index(
+    #     drop=True
+    # )
+
+    # Choose which indicator to show by default when opening the explorer.
+    # df_graphers["defaultView"] = False
+    # df_graphers.loc[
+    #     (df_graphers["Mineral Dropdown"] == "Copper")
+    #     & (df_graphers["Type Dropdown"] == "Mine")
+    #     & (df_graphers["Metric Dropdown"] == "Production")
+    #     & (~df_graphers["Share of global Checkbox"]),
+    #     "defaultView",
+    # ] = True
+
+    # Prepare explorer metadata.
+    config = {
+        "explorerTitle": "Minerals supply and demand prospects",
+        "explorerSubtitle": "",
+        # "selection": ["World", "Australia", "Chile", "China", "United States"],
+    }
+
+    #
+    # Save outputs.
+    #
+    # Create a new explorers dataset and tsv file.
+    ds_explorer = create_explorer(dest_dir=dest_dir, config=config, df_graphers=df_graphers)
+    ds_explorer.save()
