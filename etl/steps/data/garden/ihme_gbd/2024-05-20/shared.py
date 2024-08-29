64e0c1d3ce7365a44159ca8ac9b06d57eb1bc2ed
spoonerf <spoonerfiona@gmail.com>
2024-08-29 12:40:34 +0100

    wip

--- etl/steps/data/garden/ihme_gbd/2024-05-20/shared.py
+++ etl/steps/data/garden/ihme_gbd/2024-05-20/shared.py
@@ -37,7 +37,7 @@ def add_regional_aggregates(
     if "rei" not in index_cols:
         # Check there aren't any values above 100
         tb_percent = pr.concat([tb_percent, tb_percent_regions], ignore_index=True)
-        assert tb_percent["value"].max() <= 100
+        assert tb_percent["value"].max() <= 100 or tb_percent.shape[0] == 0
     # Combine all the metrics back together
     tb_out = pr.concat([tb_number, tb_rate, tb_percent], ignore_index=True)
     for col in ("age", "cause", "metric", "measure", "country"):
