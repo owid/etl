9fde2e5e8f0fbc22b3914bb2d86e8e953e8670f6
lucasrodes <lucasrodes@users.noreply.github.com>
2024-08-30 12:53:36 +0200

    wip: explorer editor

---
+++ apps/wizard/app_pages/explorer_edit.py
@@ -0,0 +1,11 @@
+"""Helper tool to create map brackets for all indicators in an indicator-based explorer.
+
+"""
+import streamlit as st
+
+# PAGE CONFIG
+st.set_page_config(
+    page_title="Wizard: Explorer editor",
+    page_icon="🪄",
+)
+st.title(":material/explore: Explorer Editor")
