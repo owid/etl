822c8427305616faca3a242b1cffe5a58d97498e
lucasrodes <lucasrodes@users.noreply.github.com>
2024-08-30 12:58:49 +0200

    wip

--- apps/wizard/app_pages/explorer_edit.py
+++ apps/wizard/app_pages/explorer_edit.py
@@ -9,3 +9,13 @@ st.set_page_config(
     page_icon="🪄",
 )
 st.title(":material/explore: Explorer Editor")
+
+
+with st.container(border=True):
+    st.subheader("IDs to Paths")
+    st.markdown("Migrate all references to indicator IDs for their corresponding indicator paths.")
+
+    uploaded_files = st.file_uploader(
+        label="Upload Explorer config file",
+        type="tsv",
+    )
