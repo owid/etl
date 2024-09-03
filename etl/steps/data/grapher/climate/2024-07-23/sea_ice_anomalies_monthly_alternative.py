fa3430566362b86564eb8237b10c534a0548957d
Pablo Rosado <pabloarosado@gmail.com>
2024-09-03 13:47:00 +0200

    Create a new alternative grapher dataset that combines yearly and decadal data

--- etl/steps/data/grapher/climate/2024-07-23/sea_ice_anomalies_monthly_alternative.py
+++ etl/steps/data/grapher/climate/2024-07-23/sea_ice_anomalies_monthly_alternative.py
@@ -2,6 +2,7 @@
 
 import re
 
+import owid.catalog.processing as pr
 from owid.catalog import Table
 
 from etl.helpers import PathFinder, create_dataset
@@ -13,54 +14,39 @@ paths = PathFinder(__file__)
 # This is chosen because the minimum year informed is 1978 (with only 2 months informed).
 # NOTE: We could include 1979. But, for consistency between yearly and decadal data, we ignore this year.
 YEAR_MIN = 1980
+# TODO: Describe.
+USE_DECADAL_DATA_PRIOR_TO_YEAR = 2020
 
 
 def create_yearly_table(tb: Table) -> Table:
-    # Ignore the very first years (1978 and 1979), as explained above (where YEAR_MIN is defined).
-    _tb = tb[tb["year"] >= YEAR_MIN].reset_index(drop=True)
-    # Create a table with a column for each year.
-    tb_yearly = _tb.pivot(
-        index=["location", "month"], columns=["year"], values="sea_ice_extent", join_column_levels_with=""
-    )
+    tb_yearly = tb.copy()
 
-    # To adjust to grapher, rename location column.
-    tb_yearly = tb_yearly.rename(columns={"location": "country", "month": "year"}, errors="raise")
+    tb_yearly = tb_yearly[tb_yearly["year"] >= USE_DECADAL_DATA_PRIOR_TO_YEAR].reset_index(drop=True)
 
-    # Ensure all column names are strings.
-    tb_yearly = tb_yearly.rename(
-        columns={
-            column: f"sea_ice_extent_in_{column}" for column in tb_yearly.drop(columns=["country", "year"]).columns
-        },
-        errors="raise",
+    # tb_yearly["country"] = tb_yearly["location"] + " " + tb_yearly["year"].astype("string")
+    tb_yearly["country"] = tb_yearly["location"].str.lower() + tb_yearly["year"].astype("string")
+    tb_yearly = tb_yearly.drop(columns=["decade", "year", "location"], errors="raise").rename(
+        columns={"month": "year"}, errors="raise"
     )
 
     return tb_yearly
 
 
 def create_decadal_table(tb: Table) -> Table:
-    # Ignore the very first decade (1970s), since it contains only 2 years.
-    _tb = tb[tb["year"] >= YEAR_MIN].reset_index(drop=True)
+    tb_decadal = tb.copy()
+
+    tb_decadal = tb_decadal[tb_decadal["year"] < USE_DECADAL_DATA_PRIOR_TO_YEAR].reset_index(drop=True)
 
     # Calculate the sea ice extent of each month, averaged over the same 10 months of each decade.
     # For example, January 1990 will be the average sea ice extent of the 10 months of January between 1990 and 1999.
-    tb_grouped = _tb.groupby(["location", "month", "decade"], observed=True, as_index=False).agg(
+    tb_decadal = tb_decadal.groupby(["location", "month", "decade"], observed=True, as_index=False).agg(
         {"sea_ice_extent": "mean"}
     )
 
-    # Pivot the table to get decades as columns.
-    tb_decadal = tb_grouped.pivot(
-        index=["location", "month"], columns="decade", values="sea_ice_extent", join_column_levels_with=""
-    )
-
-    # To adjust to grapher, rename location column.
-    tb_decadal = tb_decadal.rename(columns={"location": "country", "month": "year"}, errors="raise")
-
-    # Rename columns conveniently.
-    tb_decadal = tb_decadal.rename(
-        columns={
-            column: f"sea_ice_extent_in_the_{int(column)}s"
-            for column in tb_decadal.drop(columns=["country", "year"]).columns
-        }
+    # tb_decadal["country"] = tb_decadal["location"] + " " + tb_decadal["decade"].astype("string") + "s"
+    tb_decadal["country"] = tb_decadal["location"].str.lower() + tb_decadal["decade"].astype("string") + "s"
+    tb_decadal = tb_decadal.drop(columns=["decade", "location"], errors="raise").rename(
+        columns={"month": "year"}, errors="raise"
     )
 
     return tb_decadal
@@ -69,14 +55,14 @@ def create_decadal_table(tb: Table) -> Table:
 def improve_metadata(tb: Table) -> Table:
     tb = tb.copy()
 
-    # Table title (which will also become the dataset title).
-    tb.metadata.title = "Monthly sea ice extent"
-
+    # Main title (which will be used for the indicator, the table, and the dataset).
+    title = "Monthly sea ice extent"
     description_short_yearly = "Mean sea ice extent for each month. For example, June 2010 represents the sea ice extent averaged over all days in June 2010."
-    description_short_decadal = "Average of monthly means over the decade. For example, June 2010 represents the mean sea ice extent for June — calculated as the average over all days in the month — averaged across all Junes from 2010 to 2019."
-    for column in tb.drop(columns=["country", "year"]).columns:
+
+    colors = {}
+    columns = sorted(set(tb["country"]), reverse=False)
+    for column in columns:
         year = int(re.findall(r"\d{4}", column)[0])
-        title = column.replace("_", " ").capitalize()
         if 1980 <= year < 1990:
             # Very light blue.
             color = "#E9F2FF"
@@ -86,19 +72,30 @@ def improve_metadata(tb: Table) -> Table:
         elif 2000 <= year < 2010:
             # Medium blue.
             color = "#99C2FF"
-        elif 2010 <= year < 2020:
+        elif 2010 <= year < USE_DECADAL_DATA_PRIOR_TO_YEAR:
             # Darker blue.
             color = "#66A3FF"
+        elif year == int(tb["country"].max()[-4:]):
+            # Black:
+            color = "#000000"
         else:
             # Dark red.
             color = "#8E0F0F"
-        tb[column].metadata.title = title
-        if f"{year}s" in column:
-            tb[column].metadata.description_short = description_short_decadal
-        else:
-            tb[column].metadata.description_short = description_short_yearly
-        tb[column].metadata.display = {"name": str(year), "color": color}
-        tb[column].metadata.presentation.title_public = title
+        colors[column] = color
+
+    # Rename table.
+    tb.metadata.title = title
+
+    # Name of data column (there is only one).
+    column = "sea_ice_extent"
+    tb[column].metadata.title = title
+    tb[column].metadata.description_short = description_short_yearly
+    tb[column].metadata.presentation.title_public = title
+    # Set color for each entity.
+    tb[column].metadata.presentation.grapher_config = {
+        "selectedEntityNames": columns,
+        "selectedEntityColors": colors,
+    }
 
     return tb
 
@@ -157,9 +154,9 @@ def run(dest_dir: str) -> None:
     #
     # Rename locations conveniently.
     tb = tb.astype({"location": "string"})
-    tb.loc[tb["location"] == "Northern Hemisphere", "location"] = "Arctic Ocean"
-    tb.loc[tb["location"] == "Southern Hemisphere", "location"] = "Antarctica"
-    assert set(tb["location"]) == {"Antarctica", "Arctic Ocean"}, "Unexpected locations."
+    tb.loc[tb["location"] == "Northern Hemisphere", "location"] = "Arctic"
+    tb.loc[tb["location"] == "Southern Hemisphere", "location"] = "Antarctic"
+    assert set(tb["location"]) == {"Antarctic", "Arctic"}, "Unexpected locations."
 
     # Create columns for month, year, and decade.
     tb["year"] = tb["date"].dt.year
@@ -169,59 +166,32 @@ def run(dest_dir: str) -> None:
     # Sanity checks.
     sanity_check_inputs(tb=tb)
 
-    # Select only Arctic Ocean and years after a certain minimum (see explanation above, where YEAR_MIN is defined).
+    # Select years after a certain minimum (see explanation above, where YEAR_MIN is defined).
     tb = (
-        tb[(tb["location"] == "Arctic Ocean") & (tb["year"] >= YEAR_MIN)]
+        tb[(tb["year"] >= YEAR_MIN)]
         .sort_values(["year", "month"], ascending=(False, True))
+        .drop(columns=["date"], errors="raise")
         .reset_index(drop=True)
-        .drop(columns=["location", "date"], errors="raise")
     )
 
     # Create yearly table, adapted to grapher.
-    tb_yearly = tb.drop(columns=["decade"], errors="raise").rename(
-        columns={"year": "country", "month": "year"}, errors="raise"
-    )
+    tb_yearly = create_yearly_table(tb=tb)
 
-    # Create decadal table.
-    # tb_decadal = tb.drop(columns=["year"], errors="raise").rename(columns={"decade": "country"}, errors="raise")
+    # Create decadal table, adapted to grapher.
+    tb_decadal = create_decadal_table(tb=tb)
 
-    # Combine both tables.
-    # tb_combined = tb_yearly.merge(tb_decadal, on=["country", "year"], how="outer")
-
-    # Create a dictionary of colors.
-    years = sorted(set(tb_yearly["country"]), reverse=True)
-    colors = {}
-    for year in years:
-        if 1980 <= year < 1990:
-            # Very light blue.
-            color = "#E9F2FF"
-        elif 1990 <= year < 2000:
-            # Light blue.
-            color = "#CCE0FF"
-        elif 2000 <= year < 2010:
-            # Medium blue.
-            color = "#99C2FF"
-        elif 2010 <= year < 2020:
-            # Darker blue.
-            color = "#66A3FF"
-        else:
-            # Dark red.
-            color = "#8E0F0F"
-        colors[str(year)] = color
+    # Combine both tables (take decadal data prior to 2020, and individual years from 2020 on).
+    tb_combined = pr.concat([tb_decadal, tb_yearly], ignore_index=True)
 
     # Improve metadata.
-    tb_yearly.metadata.title = "Monthly sea ice extent in the Arctic Ocean"
-    tb_yearly["sea_ice_extent"].metadata.presentation.grapher_config = {
-        "selectedEntityNames": [str(year) for year in years],
-        "selectedEntityColors": colors,
-    }
+    tb_combined = improve_metadata(tb=tb_combined)
 
     # Improve format.
-    tb_yearly = tb_yearly.format(sort_rows=False)
+    tb_combined = tb_combined.format(sort_rows=False)
 
     #
     # Save outputs.
     #
     # Create a new grapher dataset.
-    ds_grapher = create_dataset(dest_dir, tables=[tb_yearly], check_variables_metadata=True)
+    ds_grapher = create_dataset(dest_dir, tables=[tb_combined], check_variables_metadata=True)
     ds_grapher.save()
