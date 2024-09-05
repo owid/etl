f025b0a4ffe3ab097e6c3c0c93f08204b52ef37c
veronikasamborska1994 <32176660+veronikasamborska1994@users.noreply.github.com>
2024-09-05 12:01:37 +0200

    clean up cancer names

--- etl/steps/data/garden/who/2024-08-06/mortality_database_cancer_most_common.py
+++ etl/steps/data/garden/who/2024-08-06/mortality_database_cancer_most_common.py
@@ -1,5 +1,7 @@
 """Load a meadow dataset and create a garden dataset."""
 
+import re
+
 from etl.helpers import PathFinder, create_dataset
 
 # Get paths and naming conventions for current step.
@@ -17,19 +19,24 @@ def run(dest_dir: str) -> None:
     #
     # Process data.
     #
-    tb_cleaned = tb.dropna(subset=["age_standardized_death_rate_per_100_000_standard_population"])
+    # Remove rows with missing values in 'age_standardized_death_rate_per_100_000_standard_population'
+    tb = tb.dropna(subset=["age_standardized_death_rate_per_100_000_standard_population"])
 
     # Group by 'country', 'year', 'sex', and 'age_group' and find the cause with the maximum death rate
-    most_deadly_causes = tb_cleaned.loc[
-        tb_cleaned.groupby(["country", "year", "sex", "age_group"])[
+    tb = tb.loc[
+        tb.groupby(["country", "year", "sex", "age_group"])[
             "age_standardized_death_rate_per_100_000_standard_population"
         ].idxmax()
     ]
 
+    # Replace "cancer" or "cancers" with an empty string, and replace commas with "and"
+    tb["cause"] = (
+        tb["cause"].str.replace(r"\bcancers?\b", "", case=False, regex=True).str.replace(",", " and").str.strip()
+    )
     # Keep only the 'cause' column
-    most_deadly_causes = most_deadly_causes[["country", "year", "sex", "cause"]]
+    tb = tb[["country", "year", "sex", "cause"]]
 
-    tb = most_deadly_causes.format(["country", "year", "sex"])
+    tb = tb.format(["country", "year", "sex"])
     ds_garden = create_dataset(
         dest_dir, tables=[tb], check_variables_metadata=False, default_metadata=ds_meadow.metadata
     )
