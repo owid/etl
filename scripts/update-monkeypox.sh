2957843664ded8470cd31b83b54e4c0d13256d1f
Fiona Spooner <fiona@ourworldindata.org>
2024-09-09 13:15:32 +0200

    📊  Adding global health data for mpox (#3229)
    * adding global health data
    
    * using global health for explorer
    
    * update explorer
    
    * auto-update script
    
    * combine mpox scripts
    
    * adding metadata
    
    * update snapshot

--- scripts/update-monkeypox.sh
+++ scripts/update-monkeypox.sh
@@ -7,6 +7,7 @@ start_time=$(date +%s)
 echo '--- Update Monkeypox'
 cd /home/owid/etl
 poetry run python snapshots/who/latest/monkeypox.py
+poetry run python snapshots/health/latest/global_health_mpox.py
 
 # commit to master will trigger ETL which is gonna run the step
 echo '--- Commit and push changes'
