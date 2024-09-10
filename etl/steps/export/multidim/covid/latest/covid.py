4583d5fd2a7cf379d2bd79744f00d05d7892af0e
lucasrodes <lucasrodes@users.noreply.github.com>
2024-09-10 16:34:45 +0200

    wip

--- etl/steps/export/multidim/covid/latest/covid.py
+++ etl/steps/export/multidim/covid/latest/covid.py
@@ -14,19 +14,24 @@ def run(dest_dir: str) -> None:
         "covid.deaths.yml",
         "covid.hospital.yml",
         "covid.boosters.yml",
+        "covid.vax_breakdowns.yml",
     ]
     # Load YAML file
     for fname in filenames:
         config = paths.load_mdim_config(fname)
-        slug = f"mdd-{fname.replace('.yml', '').replace('.', '-')}"
+        slug = fname_to_slug(fname)
         multidim.upsert_multidim_data_page(slug, config, engine)
 
     # Automatic ones (they have dimensions in the tables)
     fname = "covid.mobility.yml"
     config = paths.load_mdim_config(fname)
-    slug = f"mdd-{fname.replace('.yml', '').replace('.', '-')}"
+    slug = fname_to_slug(fname)
     table = "grapher/covid/latest/google_mobility/google_mobility"
     config["views"] += multidim.expand_views(config, {"place": "*"}, table, engine)
     multidim.upsert_multidim_data_page(slug, config, engine)
 
     print(1)
+
+
+def fname_to_slug(fname: str) -> str:
+    return f"mdd-{fname.replace('.yml', '').replace('.', '-').replace('_', '-')}"
