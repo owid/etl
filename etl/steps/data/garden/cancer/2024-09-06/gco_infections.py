79e1a537954f2b06caccb6053cad7ddf4931bb6c
veronikasamborska1994 <32176660+veronikasamborska1994@users.noreply.github.com>
2024-09-11 16:49:48 +0200

    🎉 cancer: GCO - add World

--- etl/steps/data/garden/cancer/2024-09-06/gco_infections.py
+++ etl/steps/data/garden/cancer/2024-09-06/gco_infections.py
@@ -5,6 +5,8 @@ from etl.helpers import PathFinder, create_dataset
 
 # Get paths and naming conventions for current step.
 paths = PathFinder(__file__)
+# Define regions to aggregate
+REGIONS = ["Europe", "Asia", "North America", "South America", "Africa", "Oceania"]
 
 
 def run(dest_dir: str) -> None:
@@ -13,7 +15,8 @@ def run(dest_dir: str) -> None:
     #
     # Load meadow dataset.
     ds_meadow = paths.load_dataset("gco_infections")
-
+    # Load regions dataset.
+    ds_regions = paths.load_dataset("regions")
     # Read table from meadow dataset.
     tb = ds_meadow["gco_infections"].reset_index()
 
@@ -22,6 +25,15 @@ def run(dest_dir: str) -> None:
     #
     tb = geo.harmonize_countries(df=tb, countries_file=paths.country_mapping_path)
 
+    aggregates = {"cases": "sum", "attr_cases": "sum"}
+    tb = geo.add_regions_to_table(
+        tb,
+        index_columns=["country", "year", "sex", "agent", "cancer"],
+        regions=REGIONS,
+        aggregations=aggregates,
+        ds_regions=ds_regions,
+        min_num_values_per_year=1,
+    )
     tb["attr_cases_share"] = (tb["attr_cases"] / tb["cases"]) * 100
 
     tb = tb.format(["country", "year", "sex", "agent", "cancer"])
