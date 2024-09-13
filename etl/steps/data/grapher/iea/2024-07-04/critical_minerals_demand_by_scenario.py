cafb99f0fb998f47424a6ea52f0757257bdca08c
Pablo Rosado <pabloarosado@gmail.com>
2024-09-13 13:50:15 +0200

    📊 minerals: Visualize IEA critical minerals (#3195)
    * 📊 Visualize IEA critical minerals
    
    * Create grapher step for IEA critical minerals (WIP)
    
    * Create minerals supply and demand prospects (WIP)
    
    * Create table of supply by country (WIP)
    
    * Include process as another dropdown in the explorer, and some other improvements
    
    * Combine supply and demand in explorer
    
    * Various small improvements
    
    * Use indicator and metric radio buttons instead of dropdown
    
    * Move processing to garden step (WIP)
    
    * Move processing to garden (WIP)
    
    * Improve garden step
    
    * Improve garden step
    
    * Improve titles
    
    * Sort rows and add default view to explorer
    
    * Minor explorer improvements
    
    * Small fixes
    
    * Create two grapher steps instead of one
    
    * Sort explorer differently, and remove redundant scenarios for supply data
    
    * Improve garden step
    
    * Minor refactor
    
    * Add footnotes
    
    * Add short descriptions
    
    * Improve metadata
    
    * Improve metadata
    
    * Add display name
    
    * Improve display name
    
    * Create tables for different technologies
    
    * Simplify code
    
    * Improve code
    
    * Rename table
    
    * Fix wrong DoD
    
    * Implement improvements suggested by Pablo A
    
    * Add subtitle to minerals supply and demand explorer
    
    * wip
    
    ---------
    
    Co-authored-by: Marigold <mojmir.vinkler@gmail.com>

---
+++ etl/steps/data/grapher/iea/2024-07-04/critical_minerals_demand_by_scenario.py
@@ -0,0 +1,32 @@
+"""Load a garden dataset and create a grapher dataset."""
+
+
+from etl.helpers import PathFinder, create_dataset
+
+# Get paths and naming conventions for current step.
+paths = PathFinder(__file__)
+
+
+def run(dest_dir: str) -> None:
+    #
+    # Load inputs.
+    #
+    # Load garden dataset and read its main table.
+    ds_garden = paths.load_dataset("critical_minerals")
+    tb_demand_by_scenario_flat = ds_garden.read_table("demand_by_scenario")
+
+    #
+    # Process data.
+    #
+    # Adapt to grapher.
+    tb_demand_by_scenario_flat = tb_demand_by_scenario_flat.rename(columns={"scenario": "country"}, errors="raise")
+
+    # Improve format.
+    tb_demand_by_scenario_flat = tb_demand_by_scenario_flat.format()
+
+    #
+    # Save outputs.
+    #
+    # Create a new grapher dataset.
+    ds_grapher = create_dataset(dest_dir, tables=[tb_demand_by_scenario_flat], check_variables_metadata=True)
+    ds_grapher.save()
---
+++ etl/steps/data/grapher/iea/2024-07-04/critical_minerals_supply_by_country.py
@@ -0,0 +1,37 @@
+"""Load a garden dataset and create a grapher dataset."""
+
+
+from etl.helpers import PathFinder, create_dataset
+
+# Get paths and naming conventions for current step.
+paths = PathFinder(__file__)
+
+
+def run(dest_dir: str) -> None:
+    #
+    # Load inputs.
+    #
+    # Load garden dataset and read its main table.
+    ds_garden = paths.load_dataset("critical_minerals")
+    tb_supply_by_country_flat = ds_garden.read_table("supply_by_country")
+
+    #
+    # Process data.
+    #
+    # Remove "World" from supply data, since charts will be stacked area
+    # (and, by construction, all countries should always add up to World).
+    tb_supply_by_country_flat = tb_supply_by_country_flat[tb_supply_by_country_flat["country"] != "World"].reset_index(
+        drop=True
+    )
+
+    #
+    # Improve format.
+    #
+    tb_supply_by_country_flat = tb_supply_by_country_flat.format()
+
+    #
+    # Save outputs.
+    #
+    # Create a new grapher dataset.
+    ds_grapher = create_dataset(dest_dir, tables=[tb_supply_by_country_flat], check_variables_metadata=True)
+    ds_grapher.save()
