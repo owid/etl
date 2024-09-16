36392a2386e2b5b762709161355404f9ce54c6d9
Lucas Rodés-Guirao <lucasrodes@users.noreply.github.com>
2024-09-16 16:23:02 +0200

    📊 covid: models (#3287)
    * 📊 covid: models
    
    * snapshot
    
    * meadow
    
    * wip
    
    * country std
    
    * change short_name youyang -> yyg
    
    * meadow
    
    * garden
    
    * add units, fix display name
    
    * fix estimate name
    
    * fix metadata
    
    * format

---
+++ snapshots/covid/latest/infections_model.py
@@ -0,0 +1,38 @@
+"""Script to create a snapshot of dataset."""
+
+from pathlib import Path
+
+import click
+
+from etl.snapshot import Snapshot
+
+# Version for current snapshot dataset.
+SNAPSHOT_VERSION = Path(__file__).parent.name
+
+
+@click.command()
+@click.option("--upload/--skip-upload", default=True, type=bool, help="Upload dataset to Snapshot")
+@click.option("--icl", default=None, type=str, help="Path to ICL local data file.")
+@click.option("--ihme", default=None, type=str, help="Path to IHME local data file.")
+@click.option("--lshtm", default=None, type=str, help="Path to LSHTM local data file.")
+@click.option("--yyg", default=None, type=str, help="Path to Youyang Gu local data file.")
+def main(icl: str, ihme: str, lshtm: str, yyg: str, upload: bool) -> None:
+    estimates = [
+        ("icl", icl),
+        ("ihme", ihme),
+        ("lshtm", lshtm),
+        ("yyg", yyg),
+    ]
+    # Create a new snapshots.
+    for estimate in estimates:
+        name = estimate[0]
+        filename = estimate[1]
+
+        if filename is not None:
+            snap = Snapshot(f"covid/{SNAPSHOT_VERSION}/infections_model_{name}.csv")
+            # Copy local data file to snapshots data folder, add file to DVC and upload to S3.
+            snap.create_snapshot(filename=filename, upload=upload)
+
+
+if __name__ == "__main__":
+    main()
