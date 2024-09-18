04b33ce6b8fdcc4958f0fc8a40f4017f7f99d9b5
Lars Yencken <lars@yencken.org>
2024-09-18 17:10:03 +0100

    :tada: Add data explaining capability to Insighter

--- apps/wizard/app_pages/datainsight_robot.py
+++ apps/wizard/app_pages/datainsight_robot.py
@@ -5,8 +5,14 @@ from urllib import parse
 
 import requests
 import streamlit as st
+from owid.catalog.charts import Chart, list_charts
 
 from apps.utils.gpt import OpenAIWrapper
+
+
+class DataError(Exception):
+    pass
+
 
 MODEL = "gpt-4o"
 
@@ -37,8 +43,27 @@ def get_grapher_thumbnail(grapher_url: str) -> str:
     return f"data:image/png;base64,{base64.b64encode(data).decode('utf8')}"
 
 
-# PROMPT
-default_prompt = """This is a chart from Our World In Data.
+def get_trajectory_prompt(base_prompt: str, slug: str) -> str:
+    chart = Chart(slug)
+    df = chart.get_data()
+    st.warning(f"Chart has {len(df)} rows and {len(df.columns)} columns")
+    if len(df.columns) > 3:
+        raise DataError("This chart has more than 3 columns, which is not supported.")
+
+    (value_col,) = df.columns.difference(["entities", "years"])
+    df_s = df.round(1).query("years >= 2000").pivot(index="entities", columns="years", values=value_col).to_csv()
+
+    title = chart.config["title"]
+    subtitle = chart.config["subtitle"]
+
+    return f"{base_prompt}\n\n---\n\n## {title}\n\n{subtitle}\n\n{df_s}"
+
+
+(tab1, tab2) = st.tabs(["Insight from chart", "Explain raw data"])
+
+with tab1:
+    # PROMPT
+    default_prompt = """This is a chart from Our World In Data.
 
 I'd like you to write a data insight for me. Data insights are a short format that explains the main point of a chart. They are usually a title sentence and then three concise paragraphs that are written in a way that is easy to understand for a general audience.
 
@@ -67,64 +92,117 @@ Fortunately, the world has made continuous progress, and such tragic deaths have
 --
 
 Please write a data insight for the given chart. Use simple language and short paragraphs. Provide the title as a markdown header-2.
-"""
-# Ask user for URL & commit
-grapher_url = st.text_input(
-    label="Grapher URL",
-    placeholder="https://ourworldindata.org/grapher/life-expectancy?country=~CHN",
-    help="Introduce the URL to a Grapher URL. Query parameters work!",
-    key="url",
-)
-if grapher_url != "" and not grapher_url.startswith("https://ourworldindata.org/grapher/"):
-    st.warning("Please introduce a valid Grapher URL")
-
-with st.expander("Edit the prompt"):
-    prompt = st.text_area(
-        label="Prompt",
-        value=default_prompt,
-        key="prompt",
+    """
+    # Ask user for URL & commit
+    grapher_url = st.text_input(
+        label="Grapher URL",
+        placeholder="https://ourworldindata.org/grapher/life-expectancy?country=~CHN",
+        help="Introduce the URL to a Grapher URL. Query parameters work!",
+        key="url",
     )
-confirmed = st.button(
-    "Generate insight", disabled=grapher_url == "" or not grapher_url.startswith("https://ourworldindata.org/grapher/")
-)
+    if grapher_url != "" and not grapher_url.startswith("https://ourworldindata.org/grapher/"):
+        st.warning("Please introduce a valid Grapher URL")
 
-# Action if user clicks on 'generate'
-if confirmed:
-    # Opena AI (do first to catch possible errors in ENV)
-    api = OpenAIWrapper()
-
-    # Get thumbnail for chart
-    thumb_url = get_thumbnail_url(grapher_url)
-    hex = get_grapher_thumbnail(grapher_url)
-
-    # Show image
-    st.image(thumb_url)
-
-    # Prepare messages for Insighter
-    messages = [
-        {
-            "role": "system",
-            "content": [
-                {"type": "text", "text": prompt},
-            ],
-        },
-        {
-            "role": "user",
-            "content": [
-                {
-                    "type": "image_url",
-                    "image_url": {"url": hex},
-                }
-            ],
-        },
-    ]
-
-    with st.chat_message("assistant"):
-        # Ask GPT (stream)
-        stream = api.chat.completions.create(
-            model=MODEL,
-            messages=messages,  # type: ignore
-            max_tokens=3000,
-            stream=True,
+    with st.expander("Edit the prompt"):
+        prompt = st.text_area(
+            label="Prompt",
+            value=default_prompt,
+            key="prompt",
         )
-        response = cast(str, st.write_stream(stream))
+    confirmed = st.button(
+        "Generate insight",
+        disabled=grapher_url == "" or not grapher_url.startswith("https://ourworldindata.org/grapher/"),
+    )
+
+    # Action if user clicks on 'generate'
+    if confirmed:
+        # Opena AI (do first to catch possible errors in ENV)
+        api = OpenAIWrapper()
+
+        # Get thumbnail for chart
+        thumb_url = get_thumbnail_url(grapher_url)
+        hex = get_grapher_thumbnail(grapher_url)
+
+        # Show image
+        st.image(thumb_url)
+
+        # Prepare messages for Insighter
+        messages = [
+            {
+                "role": "system",
+                "content": [
+                    {"type": "text", "text": prompt},
+                ],
+            },
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "image_url",
+                        "image_url": {"url": hex},
+                    }
+                ],
+            },
+        ]
+
+        with st.chat_message("assistant"):
+            # Ask GPT (stream)
+            stream = api.chat.completions.create(
+                model=MODEL,
+                messages=messages,  # type: ignore
+                max_tokens=3000,
+                stream=True,
+            )
+            response = cast(str, st.write_stream(stream))
+
+with tab2:
+    default_prompt = """This is an indicator published by Our World In Data.
+
+Explain the core insights present in this data, in plain, educational language.
+"""
+    all_charts = list_charts()
+    slug = st.multiselect(
+        label="Grapher slug",
+        options=all_charts,
+        help="Introduce the URL to a Grapher URL. Query parameters work!",
+        key="tab2_url",
+    )
+
+    with st.expander("Edit the prompt"):
+        prompt = st.text_area(
+            label="Prompt",
+            value=default_prompt,
+            key="tab2_prompt",
+        )
+    confirmed = st.button(
+        "Generate insight",
+        disabled=slug is None,
+        key="tab2_button",
+    )
+
+    # Action if user clicks on 'generate'
+    if confirmed and slug is not None:
+        # Opena AI (do first to catch possible errors in ENV)
+        api = OpenAIWrapper()
+
+        prompt_with_data = get_trajectory_prompt(prompt, slug)  # type: ignore
+
+        # Prepare messages for Insighter
+        messages = [
+            {
+                "role": "user",
+                "content": [
+                    {"type": "text", "text": prompt_with_data},
+                ],
+            },
+        ]
+
+        with st.chat_message("assistant"):
+            # Ask GPT (stream)
+            stream = api.chat.completions.create(
+                model=MODEL,
+                messages=messages,  # type: ignore
+                max_tokens=3000,
+                stream=True,
+            )
+            response = cast(str, st.write_stream(stream))
