df0d7297d2462c77ae637e9bf6e402246b39fbac
Pablo Rosado <pabloarosado@gmail.com>
2024-09-22 17:23:43 +0200

    📊 energy: Fix missing energy data for Saudi Arabia (#3322)
    * 📊 energy: Fix missing energy data for Saudi Arabia
    
    * Remove only wind data, where spurious points are
    
    * Fill with zeros spurious missing values for Arabias hydropower
    
    * Hold also wind consumption in Arabia for review

--- etl/steps/data/garden/energy_institute/2024-06-20/statistical_review_of_world_energy.py
+++ etl/steps/data/garden/energy_institute/2024-06-20/statistical_review_of_world_energy.py
@@ -613,7 +613,25 @@ def run(dest_dir: str) -> None:
     tb = fix_missing_nuclear_energy_data(tb=tb)
 
     ####################################################################################################################
-    # Wind generation for Saudi Arabia in 2022 and 2023 is possibly wrong.
+    # Similarly to missing nuclear data, hydropower is missing from year 2000 onwards, at least for Saudi Arabia.
+    # However, in the excel data file, those values are zero.
+    # Fill those missing points with zeros.
+    error = "Expected missing data for Arabia's hydropower from 2000 on. It may be fixed, so, remove this code."
+    assert (
+        tb.loc[
+            (tb["country"] == "Saudi Arabia") & (tb["year"] >= 2000),
+            ["hydro_consumption_equivalent_ej", "hydro_electricity_generation_twh"],
+        ]
+        .isnull()
+        .all()
+        .all()
+    ), error
+    tb.loc[
+        (tb["country"] == "Saudi Arabia") & (tb["year"] >= 2000),
+        ["hydro_consumption_equivalent_ej", "hydro_electricity_generation_twh"],
+    ] = 0
+
+    # Wind generation (and consumption) for Saudi Arabia in 2022 and 2023 is possibly wrong.
     # It goes from 0.005678 TWh in 2021 to 1.45 TWh in 2022 and 2023.
     # According to IRENA, Saudi Arabia's wind capacity was 3MW in 2022:
     # https://www.irena.org/Publications/2023/Jul/Renewable-energy-statistics-2023
@@ -626,9 +644,16 @@ def run(dest_dir: str) -> None:
     assert (
         tb[(tb["country"] == "Saudi Arabia") & (tb["year"] == 2022)]["wind_electricity_generation_twh"].item() > 1.45
     ), error
-    tb = tb.drop(tb[(tb["country"] == "Saudi Arabia") & (tb["year"].isin([2022, 2023]))].index.tolist()).reset_index(
-        drop=True
-    )
+    assert (
+        tb[(tb["country"] == "Saudi Arabia") & (tb["year"] == 2021)]["wind_consumption_equivalent_ej"].item() < 0.00006
+    ), error
+    assert (
+        tb[(tb["country"] == "Saudi Arabia") & (tb["year"] == 2022)]["wind_consumption_equivalent_ej"].item() > 0.01
+    ), error
+    tb.loc[
+        (tb["country"] == "Saudi Arabia") & (tb["year"].isin([2022, 2023])),
+        ["wind_consumption_equivalent_ej", "wind_electricity_generation_twh"],
+    ] = None
     ####################################################################################################################
 
     # Create additional variables, like primary energy consumption in TWh (both direct and in input-equivalents).
