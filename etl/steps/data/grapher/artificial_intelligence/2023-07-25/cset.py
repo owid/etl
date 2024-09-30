5b5ccb3aca22830e5a941c02179577eb3439be48
Marigold <mojmir.vinkler@gmail.com>
2024-09-30 11:31:56 +0200

    :honeybee: update pandas to 2.2.2

--- etl/steps/data/grapher/artificial_intelligence/2023-07-25/cset.py
+++ etl/steps/data/grapher/artificial_intelligence/2023-07-25/cset.py
@@ -4,7 +4,6 @@ from typing import cast
 
 from owid.catalog import Dataset
 
-from etl import grapher_helpers as gh
 from etl.helpers import PathFinder, create_dataset, grapher_checks
 
 # Get paths and naming conventions for current step.
@@ -23,20 +22,11 @@ def run(dest_dir: str) -> None:
     tb = tb.reset_index()
     tb.set_index(["country", "year", "field"], inplace=True)
 
-    # Expand dimensions into columns.
-    expanded_tb = gh.expand_dimensions(tb)
-
-    # Set display name to its `field` for each column
-    for col in expanded_tb.columns:
-        dim_filters = expanded_tb[col].metadata.additional_info["dimensions"]["filters"]
-        assert len(dim_filters) == 1
-        expanded_tb[col].metadata.display["name"] = dim_filters[0]["value"]
-
     #
     # Save outputs.
     #
     # Create a new grapher dataset with the same metadata as the garden dataset
-    ds_grapher = create_dataset(dest_dir, tables=[expanded_tb], default_metadata=ds_garden.metadata)
+    ds_grapher = create_dataset(dest_dir, tables=[tb], default_metadata=ds_garden.metadata)
 
     #
     # Checks.
