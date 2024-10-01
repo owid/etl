ecea5aee6872033f0e989f632b1927fa2fc57c8a
spoonerf <spoonerfiona@gmail.com>
2024-10-01 09:21:33 +0100

    updates

--- etl/steps/data/meadow/biodiversity/2024-09-30/living_planet_index.py
+++ etl/steps/data/meadow/biodiversity/2024-09-30/living_planet_index.py
@@ -1,8 +1,5 @@
 """Load a snapshot and create a meadow dataset."""
 
-from owid.catalog import Table
-from owid.catalog import processing as pr
-
 from etl.helpers import PathFinder, create_dataset
 
 # Get paths and naming conventions for current step.
@@ -15,43 +12,21 @@ def run(dest_dir: str) -> None:
     #
     # Retrieve snapshot.
     snap = paths.load_snapshot("living_planet_index.xlsx")
-    sheet_names = [
-        "Global",
-        "Freshwater",
-        "Africa",
-        "AsiaPacific",
-        "NorthAmerica",
-        "LatinAmerica&Carib",
-        "EuropeCentralAsia",
-    ]
-    all_tbs = Table()
+
     # Load data from snapshot.
-    for sheet_name in sheet_names:
-        tb = snap.read(sheet_name=sheet_name)
-        tb = tb[
-            [
-                "Unnamed: 0",
-                "LPI_final",
-                "CI_low",
-                "CI_high",
-            ]
-        ]
-        tb["country"] = sheet_name
-        tb = tb.rename(columns={"Unnamed: 0": "year"})
-        all_tbs = pr.concat([all_tbs, tb])
+    tb = snap.read()
+
     #
     # Process data.
     #
     # Ensure all columns are snake-case, set an appropriate index, and sort conveniently.
-    all_tbs = all_tbs.format(["country", "year"], short_name="living_planet_index")
+    tb = tb.format(["country", "year"])
 
     #
     # Save outputs.
     #
     # Create a new meadow dataset with the same metadata as the snapshot.
-    ds_meadow = create_dataset(
-        dest_dir, tables=[all_tbs], check_variables_metadata=True, default_metadata=snap.metadata
-    )
+    ds_meadow = create_dataset(dest_dir, tables=[tb], check_variables_metadata=True, default_metadata=snap.metadata)
 
     # Save changes in the new meadow dataset.
     ds_meadow.save()
