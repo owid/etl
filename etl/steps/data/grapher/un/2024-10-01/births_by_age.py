0043ee723c249f46e38f2c29915011f49385dad8
Fiona Spooner <fiona@ourworldindata.org>
2024-10-01 13:55:51 +0100

    📊 Data on share of births by decadal age of mother (#3350)
    * adding data
    
    * births by decade grapher step
    
    * clean up
    
    * rename grapher step
    
    * add short name
    
    * fix
    
    * fix
    
    * udpating display names
    
    * update dp
    
    * update age

---
+++ etl/steps/data/grapher/un/2024-10-01/births_by_age.py
@@ -0,0 +1,29 @@
+from etl.helpers import PathFinder, create_dataset
+
+# Get paths and naming conventions for current step.
+paths = PathFinder(__file__)
+
+
+def run(dest_dir: str) -> None:
+    #
+    # Load inputs.
+    #
+    # Load garden dataset.
+    ds_garden = paths.load_dataset("births_by_age")
+    #
+    # Process data.
+    #
+    tb = ds_garden["births_by_age"]
+    #
+    # Save outputs.
+    #
+    # Create grapher dataset
+    ds_grapher = create_dataset(
+        dest_dir,
+        tables=[tb],
+        check_variables_metadata=True,
+        default_metadata=ds_garden.metadata,
+    )
+
+    # Save changes in the new grapher dataset.
+    ds_grapher.save()
