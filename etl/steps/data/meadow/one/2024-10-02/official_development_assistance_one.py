e7c6c19e8625c14941d4264f5a375079b6b23073
Pablo Arriagada <paarriagadap@gmail.com>
2024-10-02 18:15:00 +0100

    :construction: meadow

--- etl/steps/data/meadow/one/2024-10-02/official_development_assistance_one.py
+++ etl/steps/data/meadow/one/2024-10-02/official_development_assistance_one.py
@@ -5,6 +5,20 @@ from etl.helpers import PathFinder, create_dataset
 # Get paths and naming conventions for current step.
 paths = PathFinder(__file__)
 
+# Define columns to keep
+COLUMNS_TO_KEEP = [
+    "year",
+    "donor_name",
+    "recipient_name",
+    "sector_name",
+    "purpose_name",
+    "channel_name",
+    "value",
+]
+
+# Define index columns (COLUMS_TO_KEEP minus value)
+INDEX_COLUMNS = [col for col in COLUMNS_TO_KEEP if col != "value"]
+
 
 def run(dest_dir: str) -> None:
     #
@@ -16,11 +30,13 @@ def run(dest_dir: str) -> None:
     # Load data from snapshot.
     tb = snap.read()
 
+    print(tb)
+
     #
     # Process data.
     #
     # Ensure all columns are snake-case, set an appropriate index, and sort conveniently.
-    tb = tb.format(["country", "year"])
+    tb = tb.format(INDEX_COLUMNS)
 
     #
     # Save outputs.
