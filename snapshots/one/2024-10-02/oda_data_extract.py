43e3da5761c130af661d5b904f0bc9522efc239c
Pablo Arriagada <paarriagadap@gmail.com>
2024-10-02 17:13:42 +0100

    :construction: meadow

--- snapshots/one/2024-10-02/oda_data_extract.py
+++ snapshots/one/2024-10-02/oda_data_extract.py
@@ -16,6 +16,9 @@ Steps to extract the data:
         - pandas
     2. Run this code to extract the data.
         python snapshots/one/{version}/oda_data_extract.py
+    3. Use the file oda_by_sectors_one.feather as the snapshot, by running the following code:
+        python snapshots/one/{version}/official_development_assistance_one.py --path-to-file snapshots/one/{version}/oda_by_sectors_one.feather
+    4. Delete all the files that are _not_ oda_by_sectors_one.feather or oda_data_extract.py.
 
 
 How to run the snapshot on the staging server:
@@ -34,9 +37,13 @@ from typing import Dict, List
 import pandas as pd
 from oda_data import ODAData, set_data_path
 from oda_data.tools.groupings import donor_groupings, recipient_groupings
+from structlog import get_logger
 
 # Set path of this script
 PARENT_DIR = Path(__file__).parent
+
+# Initialize logger.
+log = get_logger()
 
 # Set path where ODA data is downloaded
 set_data_path(path=PARENT_DIR)
@@ -81,18 +88,19 @@ COLUMNS_TO_REMAP = {
     "recipient": ["recipient_code", "recipient_name"],
     "sector": ["sector_code", "sector_name"],
     "purpose": ["purpose_code", "purpose_name"],
-    "channel": ["channel_code", "channel_name", "parent_channel_code", "channel_reported_name"],
+    "channel": ["channel_code", "channel_name"],
 }
 
 # Define columns to extract in the csv
 FINAL_COLUMNS = [
     "year",
+    "donor_code",
     "donor_name",
+    "recipient_code",
     "recipient_name",
     "sector_name",
     "purpose_name",
     "channel_name",
-    "channel_reported_name",
     "value",
 ]
 
@@ -105,10 +113,24 @@ def main() -> None:
     df = aggregate_data(df=df, columns_to_keep=FINAL_COLUMNS)
 
     # Extract data as csv
-    df.to_csv(f"{PARENT_DIR}/oda_by_sectors_oda.csv", index=False)
+    df.to_feather(f"{PARENT_DIR}/oda_by_sectors_one.feather")
+
+    log.info("Data extracted successfully")
 
 
 def get_the_data(columns_to_keep: List[str]) -> pd.DataFrame:
+    """
+    Get data from ONE wrapper and group it by columns_to_keep
+    """
+
+    # Define file name for the raw data
+    raw_data_file = f"{PARENT_DIR}/oda_by_sectors_one_raw.feather"
+
+    if Path(raw_data_file).is_file():
+        log.info("Raw data already exists. Loading it...")
+        return pd.read_feather(raw_data_file)
+
+    log.info("Loading data...")
     # Instantiate the `ODAData` class and store it in a variable called 'oda'
     oda = ODAData(years=YEARS, prices=PRICES, base_year=BASE_YEAR, include_names=INCLUDE_NAMES)
 
@@ -117,10 +139,12 @@ def get_the_data(columns_to_keep: List[str]) -> pd.DataFrame:
 
     # Add all the indicators in our `indicators` list
     for indicator in INDICATORS:
+        log.info(f"Loading indicator {indicator}...")
         oda.load_indicator(indicator)
 
     # Get a DataFrame with all the data.
     df = oda.get_data("all")
+    log.info("Raw data loaded to a dataframe")
 
     # Finally, group the DataFrame rows by year, currency, prices and indicator
     df = (
@@ -132,6 +156,31 @@ def get_the_data(columns_to_keep: List[str]) -> pd.DataFrame:
         .sum(numeric_only=True)
         .reset_index(drop=False)
     )
+    log.info(f"Data grouped by {[col for col in columns_to_keep if col != 'value']}")
+
+    # Export grouped data to csv
+    df.to_feather(raw_data_file)
+
+    return df
+
+
+def rename_categories(df: pd.DataFrame, columns_to_remap: Dict[str, List], columns_to_keep: List[str]) -> pd.DataFrame:
+    """
+    Rename all the categories from codes to names by using crs_codes.json
+    """
+
+    # Open the CRS file to obtain the codes and names
+    df_crs_full = pd.read_parquet(f"{PARENT_DIR}/fullCRS.parquet")
+
+    for concept, columns in columns_to_remap.items():
+        log.info(f"Remapping {concept}...")
+        # Keep only the columns we need and drop duplicates
+        df_crs_extract = df_crs_full[columns].drop_duplicates(ignore_index=True).copy()
+
+        # Merge the dataframes
+        df = pd.merge(df, df_crs_extract, how="left", on=columns[0])
+
+    log.info("Done remapping")
 
     return df
 
@@ -140,6 +189,7 @@ def aggregate_data(df: pd.DataFrame, columns_to_keep: List[str]) -> pd.DataFrame
     """
     Create aggregations from definitions of donor and recipient aggregations.
     """
+    log.info("Aggregating data...")
     df_donors_list = aggregate_donors(df=df, columns_to_keep=columns_to_keep)
     df_recipients_list = aggregate_recipients(df=df, columns_to_keep=columns_to_keep)
 
@@ -160,11 +210,12 @@ def aggregate_donors(df: pd.DataFrame, columns_to_keep: List[str]) -> List[pd.Da
     df_donors_list = []
 
     for donor_group, donor_composition in DONOR_GROUPINGS.items():
+        log.info(f"Aggregating {donor_group} donors...")
         df_donors_by_group = df_donors[df_donors["donor_code"].isin(donor_composition.keys())].copy()
 
         df_donors_by_group = (
             df_donors_by_group.groupby(
-                [col for col in columns_to_keep if col not in ["donor_name", "value"]],
+                [col for col in columns_to_keep if col not in ["donor_code", "donor_name", "value"]],
                 observed=True,
                 dropna=False,
             )["value"]
@@ -177,13 +228,18 @@ def aggregate_donors(df: pd.DataFrame, columns_to_keep: List[str]) -> List[pd.Da
         df_donors_list.append(df_donors_by_group)
 
         for recipient_group, recipient_composition in RECIPIENT_GROUPINGS.items():
+            log.info(f"Aggregating {donor_group} donors to {recipient_group} recipients...")
             df_donors_by_recipient_group = df_donors_by_group[
                 df_donors_by_group["recipient_code"].isin(recipient_composition.keys())
             ].copy()
 
             df_donors_by_recipient_group = (
                 df_donors_by_recipient_group.groupby(
-                    [col for col in columns_to_keep if col not in ["recipient_name", "value"]],
+                    [
+                        col
+                        for col in columns_to_keep
+                        if col not in ["donor_code", "recipient_code", "recipient_name", "value"]
+                    ],
                     observed=True,
                     dropna=False,
                 )["value"]
@@ -208,13 +264,14 @@ def aggregate_recipients(df: pd.DataFrame, columns_to_keep: List[str]) -> List[p
     df_recipients_list = []
 
     for recipient_group, recipient_composition in RECIPIENT_GROUPINGS.items():
+        log.info(f"Aggregating {recipient_group} recipients...")
         df_recipients_by_group = df_recipients[
             df_recipients["recipient_code"].isin(recipient_composition.keys())
         ].copy()
 
         df_recipients_by_group = (
             df_recipients_by_group.groupby(
-                [col for col in columns_to_keep if col not in ["recipient_name", "value"]],
+                [col for col in columns_to_keep if col not in ["recipient_code", "recipient_name", "value"]],
                 observed=True,
                 dropna=False,
             )["value"]
@@ -227,24 +284,6 @@ def aggregate_recipients(df: pd.DataFrame, columns_to_keep: List[str]) -> List[p
         df_recipients_list.append(df_recipients_by_group)
 
     return df_recipients_list
-
-
-def rename_categories(df: pd.DataFrame, columns_to_remap: Dict[str, List], columns_to_keep: List[str]) -> pd.DataFrame:
-    """
-    Rename all the categories from codes to names by using crs_codes.json
-    """
-
-    # Open the CRS file to obtain the codes and names
-    df_crs_full = pd.read_parquet(f"{PARENT_DIR}/fullCRS.parquet")
-
-    for concept, columns in columns_to_remap.items():
-        # Keep only the columns we need and drop duplicates
-        df_crs_extract = df_crs_full[columns].drop_duplicates().copy()
-
-        # Merge the dataframes
-        df = pd.merge(df, df_crs_extract, how="left", on=columns[0])
-
-    return df
 
 
 if __name__ == "__main__":
