4373b06dde8f7b6c7f0168d241327dbc6ec653ae
Daniel Bachler <daniel@danielbachler.de>
2024-10-03 14:33:07 +0200

    ✨ first draft for an immigration mdim

---
+++ etl/steps/export/multidim/migration/latest/immigration.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+
+from owid.catalog import utils
+
+from etl import multidim
+from etl.db import get_engine
+from etl.helpers import PathFinder
+
+# Get paths and naming conventions for current step.
+paths = PathFinder(__file__)
+
+CURRENT_DIR = Path(__file__).parent
+
+
+def run(dest_dir: str) -> None:
+    engine = get_engine()
+
+    # Load YAML file
+    config = utils.dynamic_yaml_to_dict(utils.dynamic_yaml_load(CURRENT_DIR / "immigration.yml"))
+
+    multidim.upsert_multidim_data_page("mdd-immigration", config, engine)
