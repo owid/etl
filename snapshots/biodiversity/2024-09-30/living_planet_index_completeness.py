746c6562c3182c3053a5dc9ba59acfe8cca06090
spoonerf <spoonerfiona@gmail.com>
2024-10-08 13:12:33 +0100

    add completeness

--- snapshots/biodiversity/2024-09-30/living_planet_index_completeness.py
+++ snapshots/biodiversity/2024-09-30/living_planet_index_completeness.py
@@ -19,11 +19,11 @@ def main(upload: bool) -> None:
         {
             "taxon": ["mammals", "amphibians", "birds", "reptiles", "fish"],
             "year": [2022, 2022, 2022, 2022, 2022],
-            "species_in_lpi": [43.3, 44.9, 44.6, 41.5, 43.3],
+            "species_in_lpi": [724, 307, 1496, 238, 2078],
             "species_in_taxon": [5171, 7682, 9350, 11913, 34633],
         }
     )
-    snap = Snapshot(f"biodiversity/{SNAPSHOT_VERSION}/living_planet_index_share.csv")
+    snap = Snapshot(f"biodiversity/{SNAPSHOT_VERSION}/living_planet_index_completeness.csv")
 
     # Copy local data file to snapshots data folder, add file to DVC and upload to S3.
     snap.create_snapshot(data=df, upload=upload)
