51ab63c01ffbc1482e78fa6337c868f46e6963e5
Pablo Arriagada <paarriagadap@gmail.com>
2024-10-14 17:46:49 +0100

    :lipstick: add instruction to kill process

--- snapshots/wb/2024-10-07/pip_api.py
+++ snapshots/wb/2024-10-07/pip_api.py
@@ -27,6 +27,8 @@ To run this code from scratch,
     - Run the code. You have two options to see the output, in the terminal or in the background:
         python snapshots/wb/{version}/pip_api.py
         nohup uv run python snapshots/wb/{version}/pip_api.py > output.log 2>&1 &
+    - You can kill the process with:
+        pkill -f pip_api
 
 When the code finishes, you will have the following files in the cache folder:
     - world_bank_pip.csv: file with the results of the queries for key indicators (8 for countries and 8 for regions), plus some additional indicators (thresholds, relative poverty).
