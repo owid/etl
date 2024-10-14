065030d81e5fa10520e05ea0d74d4a6d7aeff193
veronikasamborska1994 <32176660+veronikasamborska1994@users.noreply.github.com>
2024-10-14 17:21:23 +0100

    urbanization: GHSL degree of urbanization update

---
+++ etl/steps/data/garden/urbanization/2024-10-14/ghsl_degree_of_urbanisation.py
@@ -0,0 +1,118 @@
+"""Load a meadow dataset and create a garden dataset."""
+
+import owid.catalog.processing as pr
+
+from etl.data_helpers import geo
+from etl.helpers import PathFinder, create_dataset
+
+# Get paths and naming conventions for current step.
+paths = PathFinder(__file__)
+
+# Regions for which aggregates will be created.
+REGIONS = [
+    "North America",
+    "South America",
+    "Europe",
+    "Africa",
+    "Asia",
+    "Oceania",
+    "Low-income countries",
+    "Upper-middle-income countries",
+    "Lower-middle-income countries",
+    "High-income countries",
+]
+
+
+def run(dest_dir: str) -> None:
+    #
+    # Load inputs.
+    #
+    # Load meadow dataset.
+    ds_meadow = paths.load_dataset("ghsl_degree_of_urbanisation")
+    # Load regions dataset.
+    ds_regions = paths.load_dataset("regions")
+
+    # Load income groups dataset.
+    ds_income_groups = paths.load_dataset("income_groups")
+    # Read table from meadow dataset.
+    tb = ds_meadow["ghsl_degree_of_urbanisation"].reset_index()
+
+    #
+    # Process data.
+    #
+    tb = geo.harmonize_countries(
+        df=tb, countries_file=paths.country_mapping_path, excluded_countries_file=paths.excluded_countries_path
+    )
+
+    # Pivot the table to each indicator as a column.
+    tb = tb.pivot(index=["country", "year"], columns="indicator", values="value").reset_index()
+    columns_to_aggregate = [
+        "rural_population",
+        "urban_centre_population",
+        "urban_cluster_population",
+        "rural_area",
+        "urban_centre_area",
+        "urban_cluster_area",
+    ]
+
+    aggr_dict = {col: "sum" for col in columns_to_aggregate}
+    # Add region aggregates.
+    tb = geo.add_regions_to_table(
+        tb,
+        aggregations=aggr_dict,
+        regions=REGIONS,
+        ds_regions=ds_regions,
+        ds_income_groups=ds_income_groups,
+        min_num_values_per_year=1,
+    )
+
+    # Ensure the column names are correctly specified
+    tb["total_population"] = tb[["rural_population", "urban_centre_population", "urban_cluster_population"]].sum(axis=1)
+    tb["total_area"] = tb[["rural_area", "urban_centre_area", "urban_cluster_area"]].sum(axis=1)
+
+    tb["share_of_urban_population"] = (
+        (tb["urban_centre_population"] + tb["urban_cluster_population"]) / tb["total_population"]
+    ) * 100
+
+    tb["share_of_urban_area"] = (tb["urban_centre_area"] + tb["urban_cluster_area"]) / tb["total_area"] * 100
+
+    tb["share_of_rural_area"] = tb["rural_area"] / tb["total_area"] * 100
+    tb["share_of_rural_population"] = tb["rural_population"] / tb["total_population"] * 100
+
+    tb["share_of_urban_cluster_population"] = tb["urban_cluster_population"] / tb["total_population"] * 100
+    tb["share_of_urban_centre_population"] = tb["urban_centre_population"] / tb["total_population"] * 100
+
+    tb["share_of_urban_cluster_area"] = tb["urban_cluster_area"] / tb["total_area"] * 100
+    tb["share_of_urban_centre_area"] = tb["urban_centre_area"] / tb["total_area"] * 100
+
+    tb = tb.drop(columns=["total_population", "total_area"])
+
+    # Create two new dataframes to separate data into estimates and projections (pre-2025 and post-2025 (five year intervals)))
+    past_estimates = tb[tb["year"] < 2025].copy()
+    future_projections = tb[tb["year"] >= 2025].copy()
+
+    # Now, for each column in the original dataframe, split it into two (projections and estimates)
+    for col in tb.columns:
+        if col not in ["country", "year"]:
+            past_estimates[f"{col}_estimates"] = tb.loc[tb["year"] < 2025, col]
+            future_projections[f"{col}_projections"] = tb.loc[tb["year"] >= 2025, col]
+            past_estimates = past_estimates.drop(columns=[col])
+            future_projections = future_projections.drop(columns=[col])
+
+    tb = pr.merge(past_estimates, future_projections, on=["country", "year"], how="outer")
+
+    # Melt the DataFrame to make the metadata easier to generate
+    tb = tb.melt(id_vars=["country", "year"], var_name="indicator", value_name="value")
+
+    tb = tb.format(["country", "year", "indicator"])
+
+    #
+    # Save outputs.
+    #
+    # Create a new garden dataset with the same metadata as the meadow dataset.
+    ds_garden = create_dataset(
+        dest_dir, tables=[tb], check_variables_metadata=True, default_metadata=ds_meadow.metadata
+    )
+
+    # Save changes in the new garden dataset.
+    ds_garden.save()
---
+++ etl/steps/data/meadow/urbanization/2024-10-14/ghsl_degree_of_urbanisation.py
@@ -0,0 +1,72 @@
+"""Load a snapshot and create a meadow dataset."""
+
+import owid.catalog.processing as pr
+
+from etl.helpers import PathFinder, create_dataset
+
+# Get paths and naming conventions for current step.
+paths = PathFinder(__file__)
+
+
+def run(dest_dir: str) -> None:
+    #
+    # Load inputs.
+    #
+    # Retrieve snapshot.
+    snap = paths.load_snapshot("ghsl_degree_of_urbanisation.xlsx")
+
+    # Load data from snapshot.
+    tb_area = snap.read(sheet_name="AREA_km2_L1")
+    tb_population = snap.read(sheet_name="POP_L1")
+    #
+    # Process data.
+    #
+    # Remove rows where all values are NaNs
+    tb_area = tb_area.dropna(how="all")
+    tb_population = tb_population.dropna(how="all")
+
+    columns_to_drop = ["IDs", "GADM_ISO", "sdg_region", "WB_income_2022"]
+    tb_area = tb_area.drop(columns=columns_to_drop)
+    tb_population = tb_population.drop(columns=columns_to_drop)
+
+    # Melt the dataframe to get 'Area' and 'Year' in a single column, keeping the 'country' column intact
+    tb_area = tb_area.melt(id_vars=["GADM_Name"], var_name="area_year", value_name="value")
+    # Extract 'Area' and 'Year' from the combined 'Area_Year' column
+    tb_area["indicator"] = tb_area["area_year"].str.extract(r"([A-Za-z_]+)")
+    tb_area["year"] = tb_area["area_year"].str.extract(r"(\d{4})")
+    tb_area = tb_area.drop(columns=["area_year"])
+
+    # Melt the dataframe to get 'Population' and 'Year' in a single column, keeping the 'country' column intact
+    tb_population = tb_population.melt(id_vars=["GADM_Name"], var_name="population_year", value_name="value")
+    # Extract 'Population' and 'Year' from the combined 'Population_Year' column
+    tb_population["indicator"] = tb_population["population_year"].str.extract(r"([A-Za-z_]+)")
+    tb_population["year"] = tb_population["population_year"].str.extract(r"(\d{4})")
+    tb_population = tb_population.drop(columns=["population_year"])
+
+    tb = pr.concat([tb_area, tb_population], axis=0, ignore_index=True)
+    # Define the renaming dictionary
+    renaming_dict = {
+        "UC_AREA_": "urban_centre_area",
+        "UCL_AREA_": "urban_cluster_area",
+        "RUR_AREA_": "rural_area",
+        "UC_POP_": "urban_centre_population",
+        "UCL_POP_": "urban_cluster_population",
+        "RUR_POP_": "rural_population",
+    }
+
+    # Apply the renaming by replacing the entries in the 'indicator' column
+    tb["indicator"] = tb["indicator"].replace(renaming_dict, regex=True)
+
+    tb = tb.rename(columns={"GADM_Name": "country"})
+
+    # Ensure all columns are snake-case, set an appropriate index, and sort conveniently.
+    tb = tb.format(["country", "year", "indicator"])
+
+    #
+    # Save outputs.
+    #
+    # Create a new meadow dataset with the same metadata as the snapshot.
+    ds_meadow = create_dataset(dest_dir, tables=[tb], check_variables_metadata=True, default_metadata=snap.metadata)
+
+    # Save changes in the new meadow dataset.
+    ds_meadow.save()
