11688b5a6bc85f8c0309c082703d499850789068
Marigold <mojmir.vinkler@gmail.com>
2024-10-15 14:14:12 +0200

    :bug: fix tests

--- etl/helpers.py
+++ etl/helpers.py
@@ -1197,7 +1197,7 @@ def map_indicator_path_to_id(catalog_path: str) -> str | int:
     # Get ID, assuming given path is a catalog path
     engine = get_engine()
     with Session(engine) as session:
-        db_indicator = gm.Variable.load_from_catalog_path(session, catalog_path)
+        db_indicator = gm.Variable.from_id_or_path(session, catalog_path)
         assert db_indicator.id is not None
         return db_indicator.id
 
