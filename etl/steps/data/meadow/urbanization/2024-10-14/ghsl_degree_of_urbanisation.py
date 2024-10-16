a62afc52c678f3196ea3697d8fa4e55a854fbf66
veronikasamborska1994 <32176660+veronikasamborska1994@users.noreply.github.com>
2024-10-16 16:13:35 +0100

    split the meadow function for readability as per Pablo's suggestion

--- etl/steps/data/meadow/urbanization/2024-10-14/ghsl_degree_of_urbanisation.py
+++ etl/steps/data/meadow/urbanization/2024-10-14/ghsl_degree_of_urbanisation.py
@@ -1,6 +1,7 @@
 """Load a snapshot and create a meadow dataset."""
 
 import owid.catalog.processing as pr
+from owid.catalog import Table
 
 from etl.helpers import PathFinder, create_dataset
 
@@ -9,42 +10,20 @@ paths = PathFinder(__file__)
 
 
 def run(dest_dir: str) -> None:
-    #
     # Load inputs.
-    #
-    # Retrieve snapshot.
     snap = paths.load_snapshot("ghsl_degree_of_urbanisation.xlsx")
 
-    # Load data from snapshot.
-    tb_area = snap.read(sheet_name="AREA_km2_L1")
-    tb_population = snap.read(sheet_name="POP_L1")
-    #
-    # Process data.
-    #
-    # Remove rows where all values are NaNs
-    tb_area = tb_area.dropna(how="all")
-    tb_population = tb_population.dropna(how="all")
-
+    # Define columns to drop.
     columns_to_drop = ["IDs", "GADM_ISO", "sdg_region", "WB_income_2022"]
-    tb_area = tb_area.drop(columns=columns_to_drop)
-    tb_population = tb_population.drop(columns=columns_to_drop)
 
-    # Melt to get 'area' and 'year' in a single column, keeping the 'country' column intact
-    tb_area = tb_area.melt(id_vars=["GADM_Name"], var_name="area_year", value_name="value")
-    # Extract 'area' and 'year' from the combined 'area_year' column
-    tb_area["indicator"] = tb_area["area_year"].str.extract(r"([A-Za-z_]+)")
-    tb_area["year"] = tb_area["area_year"].str.extract(r"(\d{4})")
-    tb_area = tb_area.drop(columns=["area_year"])
+    # Load and process data from sheets.
+    tb_area = load_and_process_sheet(snap, "AREA_km2_L1", columns_to_drop)
+    tb_population = load_and_process_sheet(snap, "POP_L1", columns_to_drop)
 
-    # Melt to get 'population' and 'year' in a single column, keeping the GADM_Name (country) column intact
-    tb_population = tb_population.melt(id_vars=["GADM_Name"], var_name="population_year", value_name="value")
-    # Extract 'population' and 'yar' from the combined 'population_year' column
-    tb_population["indicator"] = tb_population["population_year"].str.extract(r"([A-Za-z_]+)")
-    tb_population["year"] = tb_population["population_year"].str.extract(r"(\d{4})")
-    tb_population = tb_population.drop(columns=["population_year"])
-
+    # Concatenate area and population data.
     tb = pr.concat([tb_area, tb_population], axis=0, ignore_index=True)
-    # Define the renaming dictionary
+
+    # Define the renaming dictionary.
     renaming_dict = {
         "UC_AREA_": "urban_centre_area",
         "UCL_AREA_": "urban_cluster_area",
@@ -54,19 +33,36 @@ def run(dest_dir: str) -> None:
         "RUR_POP_": "rural_total_population",
     }
 
-    # Apply the renaming by replacing the entries in the 'indicator' column to be more interpertable
+    # Apply the renaming by replacing the entries in the 'indicator' column to be more interpretable.
     tb["indicator"] = tb["indicator"].replace(renaming_dict, regex=True)
 
+    # Rename columns.
     tb = tb.rename(columns={"GADM_Name": "country"})
 
     # Ensure all columns are snake-case, set an appropriate index, and sort conveniently.
     tb = tb.format(["country", "year", "indicator"])
 
-    #
     # Save outputs.
-    #
-    # Create a new meadow dataset with the same metadata as the snapshot.
     ds_meadow = create_dataset(dest_dir, tables=[tb], check_variables_metadata=True, default_metadata=snap.metadata)
-
-    # Save changes in the new meadow dataset.
     ds_meadow.save()
+
+
+def load_and_process_sheet(snap, sheet_name: str, columns_to_drop: list) -> Table:
+    # Load data from snapshot.
+    tb = snap.read(sheet_name=sheet_name)
+
+    # Remove rows where all values are NaNs.
+    tb = tb.dropna(how="all")
+
+    # Drop unnecessary columns.
+    tb = tb.drop(columns=columns_to_drop)
+
+    # Melt to get 'value' and 'year' in a single column, keeping the 'country' column intact.
+    tb = tb.melt(id_vars=["GADM_Name"], var_name="indicator_year", value_name="value")
+
+    # Extract 'indicator' and 'year' from the combined 'indicator_year' column.
+    tb["indicator"] = tb["indicator_year"].str.extract(r"([A-Za-z_]+)")
+    tb["year"] = tb["indicator_year"].str.extract(r"(\d{4})")
+    tb = tb.drop(columns=["indicator_year"])
+
+    return tb
