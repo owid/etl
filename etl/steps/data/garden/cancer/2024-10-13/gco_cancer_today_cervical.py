a495273b69c486628948ac48b6cd29537f26dfe5
veronikasamborska1994 <32176660+veronikasamborska1994@users.noreply.github.com>
2024-10-17 10:17:30 +0100

    📊 cancer: split the two GCO datasets on cervical cancer incidence (#3419)

--- etl/steps/data/garden/cancer/2024-10-13/gco_cancer_today_cervical.py
+++ etl/steps/data/garden/cancer/2024-10-13/gco_cancer_today_cervical.py
@@ -1,7 +1,5 @@
 """Load a meadow dataset and create a garden dataset."""
 
-import owid.catalog.processing as pr
-
 from etl.data_helpers import geo
 from etl.helpers import PathFinder, create_dataset
 
@@ -13,17 +11,15 @@ def run(dest_dir: str) -> None:
     #
     # Load inputs.
     #
-    # Load meadow datasets for Cancer Today and Cancer Over Time datasets.
-    ds_meadow_over_time = paths.load_dataset("gco_cancer_over_time_cervical")
-    ds_meadow_today = paths.load_dataset("gco_cancer_today_cervical")
+    # Load meadow dataset.
+    ds_meadow = paths.load_dataset("gco_cancer_today_cervical")
 
-    # Read tables from meadow datasets.
-    tb_over_time = ds_meadow_over_time["gco_cancer_over_time_cervical"].reset_index()
-    tb_today = ds_meadow_today["gco_cancer_today_cervical"].reset_index()
+    # Read table.
+
+    tb = ds_meadow["gco_cancer_today_cervical"].reset_index()
     #
     # Process data.
     #
-    tb = pr.merge(tb_today, tb_over_time, on=["country", "year", "asr"], how="outer")
 
     tb = geo.harmonize_countries(df=tb, countries_file=paths.country_mapping_path)
     tb = tb.format(["country", "year"])
@@ -33,7 +29,7 @@ def run(dest_dir: str) -> None:
     #
     # Create a new garden dataset with the same metadata as the meadow dataset.
     ds_garden = create_dataset(
-        dest_dir, tables=[tb], check_variables_metadata=True, default_metadata=ds_meadow_over_time.metadata
+        dest_dir, tables=[tb], check_variables_metadata=True, default_metadata=ds_meadow.metadata
     )
 
     # Save changes in the new garden dataset.
