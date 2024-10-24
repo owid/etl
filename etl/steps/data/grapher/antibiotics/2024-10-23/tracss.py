7092d9405f8b60d9ff301cfc943ec13f1391489a
spoonerf <spoonerfiona@gmail.com>
2024-10-24 13:47:01 +0100

    update metadata

--- etl/steps/data/grapher/antibiotics/2024-10-23/tracss.py
+++ etl/steps/data/grapher/antibiotics/2024-10-23/tracss.py
@@ -17,10 +17,6 @@ def run(dest_dir: str) -> None:
     tb = ds_garden["tracss"]
 
     #
-    # Process data.
-    #
-
-    #
     # Save outputs.
     #
     # Create a new grapher dataset with the same metadata as the garden dataset.
