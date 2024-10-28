38a4461134ecc6369a16ac1ab9bd285eda1ac101
spoonerf <spoonerfiona@gmail.com>
2024-10-28 07:42:56 +0000

    add meadow and dag

---
+++ etl/steps/data/meadow/antibiotics/2024-10-25/esvac_sales_corrected.py
@@ -0,0 +1,59 @@
+"""Load a snapshot and create a meadow dataset."""
+
+from owid.catalog import Table
+from owid.catalog import processing as pr
+
+from etl.helpers import PathFinder, create_dataset
+
+# Get paths and naming conventions for current step.
+paths = PathFinder(__file__)
+
+
+def run(dest_dir: str) -> None:
+    #
+    # Load inputs.
+    #
+    # Retrieve snapshot.
+    snap = paths.load_snapshot("esvac_sales_corrected.zip")
+
+    tables = Table()
+    for year in range(2010, 2023):
+        # Load data from snapshot.
+        tb = snap.read_in_archive(filename=f"esvac/esvac_{year}.xlsx", sheet_name="Overall sales", skiprows=5).dropna()
+        tb["year"] = year
+        cols = [
+            "country",
+            "tablets_tonnes",
+            "tablets_share_of_sales",
+            "all_other_forms_tonnes",
+            "all_other_forms_share_of_sales",
+            "total_tonnes",
+            "total_share_of_sales",
+            "year",
+        ]
+        tb.columns = cols
+        tb = tb.dropna(
+            subset=[
+                "tablets_tonnes",
+                "tablets_share_of_sales",
+                "all_other_forms_tonnes",
+                "all_other_forms_share_of_sales",
+                "total_tonnes",
+                "total_share_of_sales",
+            ]
+        )
+        tables = pr.concat([tables, tb])
+    #
+    # Process data.
+    #
+    # Ensure all columns are snake-case, set an appropriate index, and sort conveniently.
+    tb = tables.format(["country", "year"], short_name="esvac_sales")
+
+    #
+    # Save outputs.
+    #
+    # Create a new meadow dataset with the same metadata as the snapshot.
+    ds_meadow = create_dataset(dest_dir, tables=[tb], check_variables_metadata=True, default_metadata=snap.metadata)
+
+    # Save changes in the new meadow dataset.
+    ds_meadow.save()
