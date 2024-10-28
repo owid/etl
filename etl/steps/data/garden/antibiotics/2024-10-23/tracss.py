9af584f566cde8d8fbde9dd548f317ddb5cdadc5
spoonerf <spoonerfiona@gmail.com>
2024-10-28 14:30:49 +0000

    Pablo's suggestions :)

--- etl/steps/data/garden/antibiotics/2024-10-23/tracss.py
+++ etl/steps/data/garden/antibiotics/2024-10-23/tracss.py
@@ -11,6 +11,51 @@ from etl.helpers import PathFinder, create_dataset
 # Get paths and naming conventions for current step.
 paths = PathFinder(__file__)
 
+RESPONSES = {
+    "monitoring_consumption_human_3_2": {
+        "A - No national plan or system for monitoring use of antimicrobials. ": "No monitoring system",
+        "B - System designed for surveillance of antimicrobial use, that includes monitoring national level sales† or use  of antibiotics in health services. ": "National monitoring of antimicrobial sales and antibiotic use in health services",
+        "C - Total sales of antimicrobials are monitored at national level and/or some monitoring of antibiotic use at sub-national level. ": "National monitoring of antimicrobial sales and some monitoring of antibiotic use at sub-national level",
+        "D - Prescribing practices and appropriate antibiotic use are monitored in a national sample of healthcare settings. ": "Prescribing practices and appropriate antibiotic use monitored in a sample of healthcare settings",
+        "E - On a regular basis (every year/two years) data is collected and reported on: a) and b) - refer to questionnaire for complete text": "Regular data collection and reporting",
+    },
+    "amr_surveillance_human_3_3": {
+        "A - No capacity for generating data (antibiotic susceptibility testing and accompanying clinical and epidemiological data) and reporting on antibiotic resistance.  ": "No capacity for generating data on antibiotic resistance",
+        "B - AMR data is collated locally for common† bacterial infections in hospitalized and community patients‡, but data collection may not use a standardized approach and lacks national coordination and/or quality management. ": "Local collation of AMR data for common bacterial infections",
+        "C - AMR data are collated nationally for common bacterial infections in hospitalized and community patients, but national coordination and standardization are lacking. ": "National collation of AMR data for common bacterial infections",
+        "D - There is a standardized national AMR surveillance system collecting data on common bacterial infections in hospitalized and community patients, with established network of surveillance sites, designated national reference laboratory for AMR, and a national coordinating centre producing reports on AMR. ": "Standardized national AMR surveillance system",
+        "E - The national AMR surveillance system links AMR surveillance with antimicrobial consumption and/or use data for human health§. ": "National AMR surveillance system linked with antimicrobial consumption data",
+    },
+    "woah_reporting_options_4_6": {
+        "A - WOAH Reporting Option: Baseline information: On a regular basis, only baseline information is reported to the WOAH ": "Baseline information only",
+        "B - WOAH Reporting option 1:  On a regular basis, data is collected and reported to the WOAH on the overall amount sold for use/used in animals by antimicrobial class, with the possibility to separate by type of use.   ": "Information on amount sold for use/used in animals by antimicrobial class and type of use",
+        "C - WOAH Reporting option 2:  On a regular basis, data is collected and reported to the WOAH on the overall amount sold for use/used in animals by antimicrobial class, with the possibility to separate by type of use and animal group.   ": "Information on amount sold for use/used in animals by antimicrobial class, type of use and animal group",
+        "D - WOAH Reporting option 3:  On a regular basis, data is collected and reported to the WOAH on the overall amount sold for use/used in animals by antimicrobial class, with the possibility to separate by type of use, animal group and route of administration.   ": "Information on amount sold for use/used in animals by antimicrobial class, type of use, animal group and route of administration",
+        "E - Data on antimicrobials used under veterinary supervision in animals are available for individual animal species. ": "Antimicrobials used under veterinary supervision in animals are available for individual animal species",
+    },
+    "surveillance_amr_terrestrial_4_7": {
+        "A - There are no local or national strategies/plans for generating AMR surveillance data from animals for an AMR surveillance system.  ": "No national plan for AMR surveillance system",
+        "B - National plan for AMR surveillance in place but laboratory and epidemiology capacities for generating, analysing and reporting data are lacking.  ": "National plan for AMR surveillance but lacking capacities",
+        "C - Some AMR data is collected at local levels but a nationally standardized approach is not used. National coordination and/or quality management is lacking.  ": "AMR data collected at local levels but no standardized approach",
+        "D - Priority pathogenic/ commensal bacterial species have been identified for surveillance. Data systematically collected and reported on levels of resistance in at least one of those bacterial species, involving a laboratory that follows quality management processes e.g. proficiency testing. ": "Priority bacterial species identified for surveillance",
+        "E - National system of AMR surveillance established for priority animal pathogens, zoonotic and commensal bacterial isolates which follows quality assurance processes in line with intergovernmental standards. Laboratories that report for AMR surveillance follow quality assurance processes. ": "National system of AMR surveillance established for priority animal pathogens, high quality assurance processes",
+    },
+    "surveillance_amr_aquatic_4_8": {
+        "A - There are no local or national strategies/plans for generating AMR surveillance data from aquatic  animals for an AMR surveillance system.  ": "No national plan for AMR surveillance system",
+        "B - National plan for AMR surveillance in place but laboratory and epidemiology capacities for generating, analysing and reporting data are lacking.  ": "National plan for AMR surveillance but lacking capacities",
+        "C - Some AMR data is collected at local levels but a nationally standardized approach is not used. National coordination and/or quality management is lacking.  ": "AMR data collected at local levels but no standardized approach",
+        "D - Priority pathogenic/ commensal bacterial species have been identified for surveillance. Data systematically collected and reported on levels of resistance in at least one of those bacterial species, involving a laboratory that follows quality management processes e.g. proficiency testing. ": "Priority bacterial species identified for surveillance",
+        "E - National system of AMR surveillance established for priority animal pathogens, zoonotic and commensal bacterial isolates which follows quality assurance processes in line with intergovernmental standards. Laboratories that report for AMR surveillance follow quality assurance processes. ": "National system of AMR surveillance established for priority animal pathogens, high quality assurance processes",
+    },
+    "surveillance_amr_food_5_3": {
+        "A - No national plan for an AMR surveillance system.   ": "No national plan for AMR surveillance system",
+        "B - National plan for AMR surveillance in place but capacity (including laboratory and reporting) is lacking. ": "National plan for AMR surveillance but lacking capacities",
+        "C - Some AMR data is collected - but a standardized approach is not used. National coordination and/or quality management is lacking.  ": "AMR data collected but no standardized approach",
+        "D - Priority food borne pathogenic/ indicator bacterial species have been identified for surveillance. Data systematically collected and reported on levels of resistance in at least one of those bacterial species, involving a laboratory that follows quality management processes e.g. proficiency testing. ": "Priority foodborne pathogens identified for surveillance",
+        "E - National system of AMR surveillance established for priority foodborne pathogens and/or relevant indicator bacteria which follows quality assurance processes in line with intergovernmental standards. Laboratories that report for AMR surveillance follow quality assurance processes. ": "National system of AMR surveillance established for priority foodborne pathogens, high quality assurance processes",
+    },
+}
+
 
 def run(dest_dir: str) -> None:
     #
@@ -21,12 +66,13 @@ def run(dest_dir: str) -> None:
 
     # Read table from meadow dataset.
     tb = ds_meadow["tracss"].reset_index()
+    # Save origins for re-adding later
     origins = tb["laws_antimicrobials_terrestrial_2_8_2"].metadata.origins
     #
     # Process data.
     #
     tb = geo.harmonize_countries(df=tb, countries_file=paths.country_mapping_path)
-    tb = shorten_survey_responses(tb)
+    tb = shorten_survey_responses(tb, RESPONSES)
     tb = remove_spaces(tb)
     tb = tb.format(["country", "year"])
     # Adding origins back in to the columns
@@ -45,78 +91,13 @@ def run(dest_dir: str) -> None:
     ds_garden.save()
 
 
-def shorten_survey_responses(tb: Table) -> Table:
-    monitoring_consumption_human_3_2_responses = {
-        "A - No national plan or system for monitoring use of antimicrobials. ": "No monitoring system",
-        "B - System designed for surveillance of antimicrobial use, that includes monitoring national level sales† or use  of antibiotics in health services. ": "National monitoring of antimicrobial sales and antibiotic use in health services",
-        "C - Total sales of antimicrobials are monitored at national level and/or some monitoring of antibiotic use at sub-national level. ": "National monitoring of antimicrobial sales and some monitoring of antibiotic use at sub-national level",
-        "D - Prescribing practices and appropriate antibiotic use are monitored in a national sample of healthcare settings. ": "Prescribing practices and appropriate antibiotic use monitored in a sample of healthcare settings",
-        "E - On a regular basis (every year/two years) data is collected and reported on: a) and b) - refer to questionnaire for complete text": "Regular data collection and reporting",
-    }
-    check_keys_exist(tb, monitoring_consumption_human_3_2_responses, "monitoring_consumption_human_3_2")
-    tb["monitoring_consumption_human_3_2"] = tb["monitoring_consumption_human_3_2"].cat.rename_categories(
-        monitoring_consumption_human_3_2_responses
-    )
-
-    amr_surveillance_human_3_3_responses = {
-        "A - No capacity for generating data (antibiotic susceptibility testing and accompanying clinical and epidemiological data) and reporting on antibiotic resistance.  ": "No capacity for generating data on antibiotic resistance",
-        "B - AMR data is collated locally for common† bacterial infections in hospitalized and community patients‡, but data collection may not use a standardized approach and lacks national coordination and/or quality management. ": "Local collation of AMR data for common bacterial infections",
-        "C - AMR data are collated nationally for common bacterial infections in hospitalized and community patients, but national coordination and standardization are lacking. ": "National collation of AMR data for common bacterial infections",
-        "D - There is a standardized national AMR surveillance system collecting data on common bacterial infections in hospitalized and community patients, with established network of surveillance sites, designated national reference laboratory for AMR, and a national coordinating centre producing reports on AMR. ": "Standardized national AMR surveillance system",
-        "E - The national AMR surveillance system links AMR surveillance with antimicrobial consumption and/or use data for human health§. ": "National AMR surveillance system linked with antimicrobial consumption data",
-    }
-    check_keys_exist(tb, amr_surveillance_human_3_3_responses, "amr_surveillance_human_3_3")
-    tb["amr_surveillance_human_3_3"] = tb["amr_surveillance_human_3_3"].cat.rename_categories(
-        amr_surveillance_human_3_3_responses
-    )
-
-    woah_reporting_options_4_6_responses = {
-        "A - WOAH Reporting Option: Baseline information: On a regular basis, only baseline information is reported to the WOAH ": "Baseline information only",
-        "B - WOAH Reporting option 1:  On a regular basis, data is collected and reported to the WOAH on the overall amount sold for use/used in animals by antimicrobial class, with the possibility to separate by type of use.   ": "Information on amount sold for use/used in animals by antimicrobial class and type of use",
-        "C - WOAH Reporting option 2:  On a regular basis, data is collected and reported to the WOAH on the overall amount sold for use/used in animals by antimicrobial class, with the possibility to separate by type of use and animal group.   ": "Information on amount sold for use/used in animals by antimicrobial class, type of use and animal group",
-        "D - WOAH Reporting option 3:  On a regular basis, data is collected and reported to the WOAH on the overall amount sold for use/used in animals by antimicrobial class, with the possibility to separate by type of use, animal group and route of administration.   ": "Information on amount sold for use/used in animals by antimicrobial class, type of use, animal group and route of administration",
-        "E - Data on antimicrobials used under veterinary supervision in animals are available for individual animal species. ": "Antimicrobials used under veterinary supervision in animals are available for individual animal species",
-    }
-    check_keys_exist(tb, woah_reporting_options_4_6_responses, "woah_reporting_options_4_6")
-    tb["woah_reporting_options_4_6"] = tb["woah_reporting_options_4_6"].cat.rename_categories(
-        woah_reporting_options_4_6_responses
-    )
-
-    surveillance_amr_terrestrial_4_7_responses = {
-        "A - There are no local or national strategies/plans for generating AMR surveillance data from animals for an AMR surveillance system.  ": "No national plan for AMR surveillance system",
-        "B - National plan for AMR surveillance in place but laboratory and epidemiology capacities for generating, analysing and reporting data are lacking.  ": "National plan for AMR surveillance but lacking capacities",
-        "C - Some AMR data is collected at local levels but a nationally standardized approach is not used. National coordination and/or quality management is lacking.  ": "AMR data collected at local levels but no standardized approach",
-        "D - Priority pathogenic/ commensal bacterial species have been identified for surveillance. Data systematically collected and reported on levels of resistance in at least one of those bacterial species, involving a laboratory that follows quality management processes e.g. proficiency testing. ": "Priority bacterial species identified for surveillance",
-        "E - National system of AMR surveillance established for priority animal pathogens, zoonotic and commensal bacterial isolates which follows quality assurance processes in line with intergovernmental standards. Laboratories that report for AMR surveillance follow quality assurance processes. ": "National system of AMR surveillance established for priority animal pathogens, high quality assurance processes",
-    }
-    check_keys_exist(tb, surveillance_amr_terrestrial_4_7_responses, "surveillance_amr_terrestrial_4_7")
-    tb["surveillance_amr_terrestrial_4_7"] = tb["surveillance_amr_terrestrial_4_7"].cat.rename_categories(
-        surveillance_amr_terrestrial_4_7_responses
-    )
-
-    surveillance_amr_aquatic_4_8_responses = {
-        "A - There are no local or national strategies/plans for generating AMR surveillance data from aquatic  animals for an AMR surveillance system.  ": "No national plan for AMR surveillance system",
-        "B - National plan for AMR surveillance in place but laboratory and epidemiology capacities for generating, analysing and reporting data are lacking.  ": "National plan for AMR surveillance but lacking capacities",
-        "C - Some AMR data is collected at local levels but a nationally standardized approach is not used. National coordination and/or quality management is lacking.  ": "AMR data collected at local levels but no standardized approach",
-        "D - Priority pathogenic/ commensal bacterial species have been identified for surveillance. Data systematically collected and reported on levels of resistance in at least one of those bacterial species, involving a laboratory that follows quality management processes e.g. proficiency testing. ": "Priority bacterial species identified for surveillance",
-        "E - National system of AMR surveillance established for priority animal pathogens, zoonotic and commensal bacterial isolates which follows quality assurance processes in line with intergovernmental standards. Laboratories that report for AMR surveillance follow quality assurance processes. ": "National system of AMR surveillance established for priority animal pathogens, high quality assurance processes",
-    }
-    check_keys_exist(tb, surveillance_amr_aquatic_4_8_responses, "surveillance_amr_aquatic_4_8")
-    tb["surveillance_amr_aquatic_4_8"] = tb["surveillance_amr_aquatic_4_8"].cat.rename_categories(
-        surveillance_amr_aquatic_4_8_responses
-    )
-
-    surveillance_amr_food_5_3_responses = {
-        "A - No national plan for an AMR surveillance system.   ": "No national plan for AMR surveillance system",
-        "B - National plan for AMR surveillance in place but capacity (including laboratory and reporting) is lacking. ": "National plan for AMR surveillance but lacking capacities",
-        "C - Some AMR data is collected - but a standardized approach is not used. National coordination and/or quality management is lacking.  ": "AMR data collected but no standardized approach",
-        "D - Priority food borne pathogenic/ indicator bacterial species have been identified for surveillance. Data systematically collected and reported on levels of resistance in at least one of those bacterial species, involving a laboratory that follows quality management processes e.g. proficiency testing. ": "Priority foodborne pathogens identified for surveillance",
-        "E - National system of AMR surveillance established for priority foodborne pathogens and/or relevant indicator bacteria which follows quality assurance processes in line with intergovernmental standards. Laboratories that report for AMR surveillance follow quality assurance processes. ": "National system of AMR surveillance established for priority foodborne pathogens, high quality assurance processes",
-    }
-    check_keys_exist(tb, surveillance_amr_food_5_3_responses, "surveillance_amr_food_5_3")
-    tb["surveillance_amr_food_5_3"] = tb["surveillance_amr_food_5_3"].cat.rename_categories(
-        surveillance_amr_food_5_3_responses
-    )
+def shorten_survey_responses(tb: Table, responses_dict: Dict) -> Table:
+    """
+    Shortening the survey responses to make them more readable.
+    """
+    for response in responses_dict:
+        check_keys_exist(tb, responses_dict[response], response)
+        tb[response] = tb[response].cat.rename_categories(responses_dict[response])
 
     return tb
 
--- etl/steps/data/meadow/antibiotics/2024-10-23/tracss.py
+++ etl/steps/data/meadow/antibiotics/2024-10-23/tracss.py
@@ -5,6 +5,21 @@ from etl.helpers import PathFinder, create_dataset
 # Get paths and naming conventions for current step.
 paths = PathFinder(__file__)
 YEAR = paths.version[:4]
+COLS_TO_KEEP = {
+    "Country": "country",
+    "2.8.2 Country has laws or regulations on prescription and sale of antimicrobials for terrestrial animal use.": "laws_antimicrobials_terrestrial_2_8_2",
+    "2.8.3 Country has laws or regulations on prescription and sale of antimicrobials for aquatic animals.": "law_antimicrobials_aquatic_2_8_3",
+    "2.8.5 Country has laws or regulations that prohibits the use of antibiotics for growth promotion in terrestrial animals in the absence of risk analysis.": "law_antimicrobials_terrestrial_growth_promotion_2_8_5",
+    "3.2 National monitoring system for consumption and rational use of antimicrobials in human health": "monitoring_consumption_human_3_2",
+    "3.3 National surveillance system for antimicrobial resistance (AMR) in humans": "amr_surveillance_human_3_3",
+    "4.5 a Do you have a national plan or system in place for monitoring sales/use of antimicrobials in animals?": "monitoring_sales_use_animals_4_5_a",
+    "4.5 b  Do you submit AMU data to the WOAH Database on Antimicrobial agents intended for use in animals?": "amu_data_submission_whoah_4_5_b",
+    "4.6 WOAH Reporting Options for the antimicrobial use database": "woah_reporting_options_4_6",
+    "4.7 National surveillance system for antimicrobial resistance (AMR) in live terrestrial animals": "surveillance_amr_terrestrial_4_7",
+    "4.8 National surveillance system for antimicrobial resistance (AMR) in live aquatic animals": "surveillance_amr_aquatic_4_8",
+    "5.3 National surveillance system for antimicrobial resistance (AMR) in food (terrestrial and aquatic animal and plant origin)": "surveillance_amr_food_5_3",
+    "6.3 Is there a system for regular monitoring (passive surveillance) of antimicrobial compounds and their metabolites (or residues) and resistant bacteria or antimicrobial resistance genes (ARGs) in water quality?": "monitoring_amr_water_6_3",
+}
 
 
 def run(dest_dir: str) -> None:
@@ -20,25 +35,10 @@ def run(dest_dir: str) -> None:
     #
     # Process data.
     tb["year"] = YEAR
-    cols = {
-        "Country": "country",
-        "2.8.2 Country has laws or regulations on prescription and sale of antimicrobials for terrestrial animal use.": "laws_antimicrobials_terrestrial_2_8_2",
-        "2.8.3 Country has laws or regulations on prescription and sale of antimicrobials for aquatic animals.": "law_antimicrobials_aquatic_2_8_3",
-        "2.8.5 Country has laws or regulations that prohibits the use of antibiotics for growth promotion in terrestrial animals in the absence of risk analysis.": "law_antimicrobials_terrestrial_growth_promotion_2_8_5",
-        "3.2 National monitoring system for consumption and rational use of antimicrobials in human health": "monitoring_consumption_human_3_2",
-        "3.3 National surveillance system for antimicrobial resistance (AMR) in humans": "amr_surveillance_human_3_3",
-        "4.5 a Do you have a national plan or system in place for monitoring sales/use of antimicrobials in animals?": "monitoring_sales_use_animals_4_5_a",
-        "4.5 b  Do you submit AMU data to the WOAH Database on Antimicrobial agents intended for use in animals?": "amu_data_submission_whoah_4_5_b",
-        "4.6 WOAH Reporting Options for the antimicrobial use database": "woah_reporting_options_4_6",
-        "4.7 National surveillance system for antimicrobial resistance (AMR) in live terrestrial animals": "surveillance_amr_terrestrial_4_7",
-        "4.8 National surveillance system for antimicrobial resistance (AMR) in live aquatic animals": "surveillance_amr_aquatic_4_8",
-        "5.3 National surveillance system for antimicrobial resistance (AMR) in food (terrestrial and aquatic animal and plant origin)": "surveillance_amr_food_5_3",
-        "6.3 Is there a system for regular monitoring (passive surveillance) of antimicrobial compounds and their metabolites (or residues) and resistant bacteria or antimicrobial resistance genes (ARGs) in water quality?": "monitoring_amr_water_6_3",
-    }
-    tb = tb.rename(columns=cols, errors="raise")
+
+    tb = tb.rename(columns=COLS_TO_KEEP, errors="raise")
     # Lets keep just the columns we need, the questions listed above and 'year'
-    cols_to_keep = list(cols.values())
-    cols_to_keep.append("year")
+    cols_to_keep = list(COLS_TO_KEEP.values()) + ["year"]
     # Ensure all columns are snake-case, set an appropriate index, and sort conveniently.
     tb = tb[cols_to_keep]
     tb = tb.format(["country", "year"])
