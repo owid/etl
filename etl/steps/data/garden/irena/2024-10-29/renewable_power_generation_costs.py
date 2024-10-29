c059b3959a62f0d50842849d63fcd563b49bb983
Pablo Rosado <pabloarosado@gmail.com>
2024-10-29 18:08:41 +0100

    Duplicate old steps

---
+++ etl/steps/data/garden/irena/2024-10-29/renewable_power_generation_costs.py
@@ -0,0 +1,35 @@
+"""Load a meadow dataset and create a garden dataset."""
+
+from etl.data_helpers import geo
+from etl.helpers import PathFinder, create_dataset
+
+# Get paths and naming conventions for current step.
+paths = PathFinder(__file__)
+
+
+def run(dest_dir: str) -> None:
+    #
+    # Load inputs.
+    #
+    # Load meadow dataset and read its tables.
+    ds_meadow = paths.load_dataset("renewable_power_generation_costs")
+    tb = ds_meadow["renewable_power_generation_costs"].reset_index()
+    tb_solar_pv = ds_meadow["solar_photovoltaic_module_prices"]
+
+    #
+    # Process data.
+    #
+    # Harmonize country names.
+    tb = geo.harmonize_countries(df=tb, countries_file=paths.country_mapping_path)
+
+    # Set an appropriate index and sort conveniently.
+    tb = tb.set_index(["country", "year"], verify_integrity=True).sort_index()
+
+    #
+    # Save outputs.
+    #
+    # Create a new garden dataset with the same metadata as the meadow dataset.
+    ds_garden = create_dataset(
+        dest_dir, tables=[tb, tb_solar_pv], default_metadata=ds_meadow.metadata, check_variables_metadata=True
+    )
+    ds_garden.save()
