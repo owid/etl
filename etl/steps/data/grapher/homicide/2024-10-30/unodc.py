d076ddba8545dcc6892aaaaa539282f01b29ef1d
spoonerf <spoonerfiona@gmail.com>
2024-11-01 14:25:07 +0000

    adding metadata

--- etl/steps/data/grapher/homicide/2024-10-30/unodc.py
+++ etl/steps/data/grapher/homicide/2024-10-30/unodc.py
@@ -1,5 +1,4 @@
 """Load a garden dataset and create a grapher dataset."""
-
 from etl.helpers import PathFinder, create_dataset
 
 # Get paths and naming conventions for current step.
@@ -8,15 +7,18 @@ paths = PathFinder(__file__)
 
 def run(dest_dir: str) -> None:
     #
-    # Load inputs.
-    #
     # Load garden dataset.
     ds_garden = paths.load_dataset("unodc")
 
     table_names = ds_garden.table_names
     # if your data is in long format, you can use `grapher_helpers.long_to_wide_tables`
     # to get into wide format
+    tables = []
     for table_name in table_names:
         table = paths.garden_dataset[table_name]
-        ds_garden.add(table)
-        ds_garden.save()
+        tables.append(table)
+
+    ds_grapher = create_dataset(dest_dir, tables=tables, default_metadata=ds_garden.metadata)
+
+    # Save changes in the new grapher dataset.
+    ds_grapher.save()
