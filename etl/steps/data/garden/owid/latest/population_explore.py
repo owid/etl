1b6c18e12e182d4df0a0cf25ddca94b519788939
lucasrodes <lucasrodes@users.noreply.github.com>
2024-11-06 12:50:12 +0100

    wip

--- etl/steps/data/garden/owid/latest/population_explore.py
+++ etl/steps/data/garden/owid/latest/population_explore.py
@@ -1,7 +1,5 @@
 """Load a meadow dataset and create a garden dataset."""
 
-import owid.catalog.processing as pr
-
 from etl.data_helpers import geo
 from etl.helpers import PathFinder, create_dataset
 
@@ -15,9 +13,11 @@ def run(dest_dir: str) -> None:
     #
     # Load meadow dataset.
     ds_meadow = paths.load_dataset("population_explore")
+    ds_omm = paths.load_dataset("population")
 
     # Read table from meadow dataset.
     tb = ds_meadow["population_explore"].reset_index()
+    tb_omm = ds_omm["population"].reset_index()
 
     #
     # Process data.
@@ -27,6 +27,17 @@ def run(dest_dir: str) -> None:
         countries_file=paths.country_mapping_path,
     )
 
+    # Format OMM column
+    tb_omm = tb_omm[["country", "year", "population"]]
+    tb_omm = tb_omm[tb_omm["country"].isin(tb["country"].unique())]
+    tb_omm = tb_omm[(tb_omm["year"] >= 1800) & (tb_omm["year"] <= 1951)]
+    tb_omm["population"] = tb_omm["population"].astype("Int64")
+
+    # Merge
+    tb = tb.merge(tb_omm, on=["country", "year"], suffixes=("_explore", "_omm"), how="outer")
+    tb["diff"] = tb["population_explore"] - tb["population_omm"]
+
+    # Format
     tb = tb.format(["country", "year"])
 
     #
