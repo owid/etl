289f6cc9d7df2c55855340c2edf495eec3194921
Pablo Rosado <pabloarosado@gmail.com>
2024-11-18 15:52:51 +0100

    Remove archived steps (WIP)

--- etl/steps/archive/garden/faostat/2022-05-17/faostat_fo.py
+++
@@ -1,2 +0,0 @@
-"""FAOSTAT garden step for faostat_fo dataset."""
-from .shared import run  # noqa:F401
--- etl/steps/archive/garden/faostat/2023-02-22/faostat_ek.py
+++
@@ -1,2 +0,0 @@
-"""FAOSTAT garden step for faostat_ek dataset."""
-from .shared import run  # noqa:F401
