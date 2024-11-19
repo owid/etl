a0b2d9435312cc7e7ee5282e747d48ced6d67697
lucasrodes <lucasrodes@users.noreply.github.com>
2024-11-19 18:15:31 +0100

    actually import from local file

--- snapshots/hmd/2024-11-19/hfd.py
+++ snapshots/hmd/2024-11-19/hfd.py
@@ -12,12 +12,13 @@ SNAPSHOT_VERSION = Path(__file__).parent.name
 
 @click.command()
 @click.option("--upload/--skip-upload", default=True, type=bool, help="Upload dataset to Snapshot")
-def main(upload: bool) -> None:
+@click.option("--path-to-file", prompt=True, type=str, help="Path to local data file.")
+def main(path_to_file: str, upload: bool) -> None:
     # Create a new snapshot.
     snap = Snapshot(f"hmd/{SNAPSHOT_VERSION}/hfd.zip")
 
     # Copy local data file to snapshots data folder, add file to DVC and upload to S3.
-    snap.create_snapshot(upload=upload)
+    snap.create_snapshot(filename=path_to_file, upload=upload)
 
 
 if __name__ == "__main__":
