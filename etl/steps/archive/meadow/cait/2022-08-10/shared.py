fb77323558ae20ce76249d6ad2938951f1fd745a
Pablo Rosado <pabloarosado@gmail.com>
2024-11-19 08:44:37 +0100

    🧹 Remove unnecessary code from archive folders (#3555)
    * 🧹 Remove unnecessary code from archive folders
    
    * Remove snapshots archive and adjust archive dag
    
    * Remove archived steps (WIP)
    
    * Remove archived steps (WIP)
    
    * Remove archived steps (WIP)
    
    * Remove archived steps (WIP)
    
    * Remove archived steps (WIP)
    
    * Remove archived steps (WIP)
    
    * Remove archived steps (WIP)
    
    * Remove archived steps (WIP)
    
    * Remove archived steps (WIP)
    
    * Remove archived steps (WIP)

--- etl/steps/archive/meadow/cait/2022-08-10/shared.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-from structlog import get_logger
-
-log = get_logger()
-
-NAMESPACE = "cait"
-CURRENT_DIR = Path(__file__).parent
-VERSION = CURRENT_DIR.name
--- etl/steps/archive/meadow/faostat/2022-05-17/faostat_qi.py
+++
@@ -1,2 +0,0 @@
-"""FAOSTAT meadow step for faostat_qi dataset."""
-from .shared import run  # noqa:F401
