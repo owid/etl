e02e0030093c61448117d3d897ff705092dd839e
spoonerf <spoonerfiona@gmail.com>
2024-11-20 15:19:08 +0000

    add grapher step

--- etl/steps/data/grapher/antibiotics/2024-11-20/microbe.py
+++ etl/steps/data/grapher/antibiotics/2024-11-20/microbe.py
@@ -14,7 +14,7 @@ def run(dest_dir: str) -> None:
     ds_garden = paths.load_dataset("microbe")
 
     # Read table from garden dataset.
-    tb = ds_garden.read("microbe")
+    tb = ds_garden.read("microbe", reset_index=False)
     #
     # Save outputs.
     #
