d06202d06db1a6e7c41bf667ce6b757e8a0aa3aa
veronikasamborska1994 <32176660+veronikasamborska1994@users.noreply.github.com>
2024-11-20 16:43:12 +0100

    remove 2024 as incomplete

--- etl/steps/data/grapher/climate/2024-11-19/total_precipitation_annual.py
+++ etl/steps/data/grapher/climate/2024-11-19/total_precipitation_annual.py
@@ -26,26 +26,24 @@ def run(dest_dir: str) -> None:
     tb["year"] = tb["time"].astype(str).str[0:4]
 
     # Group by year and calculate the mean of the specified columns
-    tb_annual_average = (
+    tb = (
         tb.groupby(["year", "country"])
         .agg({"total_precipitation": "sum", "precipitation_anomaly": "sum"})
         .reset_index()
     )
 
-    # Convert the 'year' column to integer type
-    tb_annual_average["year"] = tb_annual_average["year"].astype(int)
-
     # Remove rows where the year is 2024 as it's incomplete
-    tb_annual_average = tb_annual_average[tb["year"] != INCOMPLETE_YEAR]
+    tb["year"] = tb["year"].astype(int)
+    tb = tb[tb["year"] != INCOMPLETE_YEAR]
 
     # Set the index to 'year' and 'country'
-    tb_annual_average = tb_annual_average.set_index(["year", "country"], verify_integrity=True)
+    tb = tb.set_index(["year", "country"], verify_integrity=True)
 
     # Save outputs.
     #
     # Create a new grapher dataset with the same metadata as the garden dataset.
     ds_grapher = create_dataset(
-        dest_dir, tables=[tb_annual_average], default_metadata=ds_garden.metadata, check_variables_metadata=True
+        dest_dir, tables=[tb], default_metadata=ds_garden.metadata, check_variables_metadata=True
     )
 
     ds_grapher.save()
