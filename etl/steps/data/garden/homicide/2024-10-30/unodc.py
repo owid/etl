3ea30e1b123238cd4ab5a2f3d284fecd910488e7
spoonerf <spoonerfiona@gmail.com>
2024-11-21 21:47:44 +0000

    fix uk

--- etl/steps/data/garden/homicide/2024-10-30/unodc.py
+++ etl/steps/data/garden/homicide/2024-10-30/unodc.py
@@ -136,22 +136,24 @@ def calculate_united_kingdom(tb: Table, ds_population: Dataset) -> Table:
     Calculate data for the UK as it is reported by the constituent countries
     """
 
-    countries = ["Wales", "Scotland", "England", "Northern Ireland"]
+    countries = ["England and Wales", "Scotland", "Northern Ireland"]
     tb_uk = tb[(tb["country"].isin(countries)) & (tb["unit_of_measurement"] == "Counts")]
 
     tb_uk = (
-        tb_uk.groupby(["year", "indicator", "dimension", "category", "sex", "age", "unit_of_measurement"])["value"]
-        .sum()
+        tb_uk.groupby(["year", "indicator", "dimension", "category", "sex", "age", "unit_of_measurement"])
+        .agg(value_sum=("value", "sum"), count=("value", "size"))
         .reset_index()
     )
+    # Use only rows where all three entites are in the data
+    tb_uk = tb_uk[tb_uk["count"] == 2]
     tb_uk["country"] = "United Kingdom"
 
     # Add in UK population to calculate rates
     tb_uk_rate = tb_uk.copy()
     tb_uk_rate = add_population_to_table(tb_uk_rate, ds_population)
-    tb_uk_rate["value"] = tb_uk_rate["value"] / tb_uk_rate["population"] * 100000
+    tb_uk_rate["value"] = tb_uk_rate["value_sum"] / tb_uk_rate["population"] * 100000
     tb_uk_rate["unit_of_measurement"] = "Rate per 100,000 population"
-    tb_uk_rate = tb_uk_rate.drop(columns=["population"])
+    tb_uk_rate = tb_uk_rate.drop(columns=["population", "value_sum", "count"])
 
     tb = pr.concat([tb, tb_uk, tb_uk_rate])
     return tb
