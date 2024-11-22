fc2ba8e8d402578eea84428fa8919a6b934398a0
lucasrodes <lucasrodes@users.noreply.github.com>
2024-11-22 13:09:14 +0100

    fix meadow

--- etl/steps/data/meadow/war/2024-11-22/ucdp_ced.py
+++ etl/steps/data/meadow/war/2024-11-22/ucdp_ced.py
@@ -1,5 +1,7 @@
 """Load a snapshot and create a meadow dataset."""
 
+import owid.catalog.processing as pr
+
 from etl.helpers import PathFinder, create_dataset
 
 # Get paths and naming conventions for current step.
@@ -20,15 +22,38 @@ def run(dest_dir: str) -> None:
     tb_10 = snap_10.read_csv()
     tb_q3 = snap_q3.read_csv()
 
-    # Check shapes
+    # Remove spurious columns, sanity checks
+    if "#" in tb_10.columns:
+        tb_10 = tb_10.drop(columns=["#"])
 
-    tb = tb_10.format("id")
+    assert (tb_10.columns == tb_q3.columns).all(), "Columns do not match between monthly and quarterly snapshots!"
+
+    # Combine tables
+    tb = pr.concat([tb_q3, tb_10], ignore_index=True)
+    tb = tb.drop_duplicates()
+
+    # Monthly data may have events that were already reported in the quarterly release.
+    # Idea: Check that this is the case, and safely remove duplicates from the quarterly release, since the monthly release is more up-to-date.
+
+    ## Ensure that all duplicate IDs are indeed because of duplicates between monthly-quarterly
+    value_counts = tb["id"].value_counts()
+    assert set(value_counts.unique()) == {1, 2}, "IDs should appear once or twice, not more!"
+    ids_duplicated = list(value_counts[value_counts > 1].index)
+    assert len(ids_duplicated) == tb_10[tb_10["id"].isin(ids_duplicated)].shape[0], "All duplicated ID"
+    tb = tb.drop_duplicates(subset="id", keep="last")
+
+    # Format table
+    tb = tb.format("id")
 
     #
     # Save outputs.
     #
     # Create a new meadow dataset with the same metadata as the snapshot.
-    ds_meadow = create_dataset(dest_dir, tables=[tb], default_metadata=snap.metadata, check_variables_metadata=True)  # type: ignore
+    ds_meadow = create_dataset(
+        dest_dir,
+        tables=[tb],
+        check_variables_metadata=True,
+    )  # type: ignore
 
     # Save changes in the new garden dataset.
     ds_meadow.save()
