c80974d28b14c9697f344ac8dad8ec57b82b9c3a
veronikasamborska1994 <32176660+veronikasamborska1994@users.noreply.github.com>
2024-11-22 12:28:20 +0100

    clean up

--- etl/steps/data/garden/unesco/2024-11-21/enrolment_rates.py
+++ etl/steps/data/garden/unesco/2024-11-21/enrolment_rates.py
@@ -43,31 +43,21 @@ def run(dest_dir: str) -> None:
     tb_pivoted = tb.pivot(index=["country", "year"], columns="indicator", values="value")
     for column in tb_pivoted.columns:
         meta = tb_pivoted[column].metadata
-        meta.display = {}
         long_definition = tb["long_description"].loc[tb["indicator"] == column].iloc[0]
         meta.description_from_producer = long_definition
         meta.title = column
-        #
-        # Update metadata units, short_units and number of decimal places to display depending on what keywords the variable name contains
-        #
-        # Fill out units and decimal places
-        if "%" in column.lower():
-            update_metadata(meta, display_decimals=1, unit="%", short_unit="%")
-        elif "(days)" in column.lower():
-            update_metadata(meta, display_decimals=1, unit="days", short_unit="")
-        elif "index" in column.lower():
-            update_metadata(meta, display_decimals=1, unit="index", short_unit="")
-        elif "(current us$)" in column.lower():
-            update_metadata(meta, display_decimals=1, unit="current US$", short_unit="current $")
-        elif "(number)" in column.lower():
-            update_metadata(meta, display_decimals=0, unit="number", short_unit="")
-
-        else:
-            # Default metadata update when no other conditions are met.
-            update_metadata(meta, 0, " ", " ")
 
     tb_pivoted = tb_pivoted.reset_index()
     tb_pivoted = tb_pivoted.format(["country", "year"])
+
+    # Drop columns that are not needed
+    tb_pivoted = tb_pivoted.drop(
+        columns=[
+            "total_net_enrolment_rate__lower_secondary__adjusted_gender_parity_index__gpia",
+            "total_net_enrolment_rate__primary__adjusted_gender_parity_index__gpia",
+            "total_net_enrolment_rate__upper_secondary__adjusted_gender_parity_index__gpia",
+        ]
+    )
 
     #
     # Save outputs.
@@ -79,21 +69,3 @@ def run(dest_dir: str) -> None:
 
     # Save changes in the new garden dataset.
     ds_garden.save()
-
-
-def update_metadata(meta: VariableMeta, display_decimals: int, unit: str, short_unit: str) -> None:
-    """
-    Update metadata attributes of a specified column in the given table.
-
-    Args:
-    meta (obj): Metadata object of the column to update.
-    display_decimals (int): Number of decimal places to display.
-    unit (str): The full name of the unit of measurement for the column data.
-    short_unit (str, optional): The abbreviated form of the unit. Defaults to an empty space.
-
-    Returns:
-    None: The function updates the table in-place.
-    """
-    meta.display["numDecimalPlaces"] = display_decimals
-    meta.unit = unit
-    meta.short_unit = short_unit
