fa6537733dbb93d8385f63d25f942eb8c6b2eed3
spoonerf <spoonerfiona@gmail.com>
2024-11-24 10:21:15 +0000

    remove per million calculation

--- etl/steps/data/garden/antibiotics/2024-10-18/who_glass.py
+++ etl/steps/data/garden/antibiotics/2024-10-18/who_glass.py
@@ -15,7 +15,6 @@ def run(dest_dir: str) -> None:
     # Load meadow dataset.
     ds_meadow = paths.load_dataset("who_glass")
     # Add population dataset.
-    ds_population = paths.load_dataset("population")
     # Read table from meadow dataset.
     tb = ds_meadow["who_glass"].reset_index()
 
@@ -23,11 +22,6 @@ def run(dest_dir: str) -> None:
     # Process data.
     #
     tb = geo.harmonize_countries(df=tb, countries_file=paths.country_mapping_path)
-    # Caclulate tests with ast per million
-    tb = add_population_to_table(tb, ds_population)
-    tb["total_specimen_isolates_with_ast_per_million"] = (
-        tb["total_specimen_isolates_with_ast"] / tb["population"] * 1_000_000
-    )
     tb = tb.format(["country", "year", "syndrome"])
 
     #
