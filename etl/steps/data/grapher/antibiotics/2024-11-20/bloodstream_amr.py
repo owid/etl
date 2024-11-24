b7feafdf7302b35b4ffe05772dc8ffbe7d2adb4d
spoonerf <spoonerfiona@gmail.com>
2024-11-24 12:51:14 +0000

    adding table to grapher

--- etl/steps/data/grapher/antibiotics/2024-11-20/bloodstream_amr.py
+++ etl/steps/data/grapher/antibiotics/2024-11-20/bloodstream_amr.py
@@ -15,13 +15,14 @@ def run(dest_dir: str) -> None:
 
     # Read table from garden dataset.
     tb = ds_garden.read("bloodstream_amr", reset_index=False)
+    tb_amr = ds_garden.read("amr_entity", reset_index=False)
 
     #
     # Save outputs.
     #
     # Create a new grapher dataset with the same metadata as the garden dataset.
     ds_grapher = create_dataset(
-        dest_dir, tables=[tb], check_variables_metadata=True, default_metadata=ds_garden.metadata
+        dest_dir, tables=[tb, tb_amr], check_variables_metadata=True, default_metadata=ds_garden.metadata
     )
 
     # Save changes in the new grapher dataset.
