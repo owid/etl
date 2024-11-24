b162b3e0746e9cd6631314805c47cb585a55ca9d
spoonerf <spoonerfiona@gmail.com>
2024-11-24 10:15:44 +0000

    adding per million for tests with ast result

--- etl/steps/data/garden/antibiotics/2024-10-18/who_glass.py
+++ etl/steps/data/garden/antibiotics/2024-10-18/who_glass.py
@@ -1,6 +1,7 @@
 """Load a meadow dataset and create a garden dataset."""
 
 from etl.data_helpers import geo
+from etl.data_helpers.geo import add_population_to_table
 from etl.helpers import PathFinder, create_dataset
 
 # Get paths and naming conventions for current step.
@@ -13,7 +14,8 @@ def run(dest_dir: str) -> None:
     #
     # Load meadow dataset.
     ds_meadow = paths.load_dataset("who_glass")
-
+    # Add population dataset.
+    ds_population = paths.load_dataset("population")
     # Read table from meadow dataset.
     tb = ds_meadow["who_glass"].reset_index()
 
@@ -21,6 +23,11 @@ def run(dest_dir: str) -> None:
     # Process data.
     #
     tb = geo.harmonize_countries(df=tb, countries_file=paths.country_mapping_path)
+    # Caclulate tests with ast per million
+    tb = add_population_to_table(tb, ds_population)
+    tb["total_specimen_isolates_with_ast_per_million"] = (
+        tb["total_specimen_isolates_with_ast"] / tb["population"] * 1_000_000
+    )
     tb = tb.format(["country", "year", "syndrome"])
 
     #
