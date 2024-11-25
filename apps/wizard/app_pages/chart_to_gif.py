7ec58238c44020b0a700959c37249efa9fe74172
Pablo Rosado <pabloarosado@gmail.com>
2024-11-25 12:27:15 +0100

    Rename session status items

--- apps/wizard/app_pages/chart_to_gif.py
+++ apps/wizard/app_pages/chart_to_gif.py
@@ -26,19 +26,13 @@ st.set_page_config(
 st.title(":material/animated_images: Chart animation")
 
 # Initialize session state for generated files.
-if "images_folder" not in st.session_state:
-    st.session_state.images_folder = DOWNLOADS_DIR
-if "image_paths" not in st.session_state:
-    st.session_state.image_paths = None
-if "images_exist" not in st.session_state:
-    st.session_state.images_exist = False
-if "gif_file" not in st.session_state:
-    st.session_state.gif_file = None
-if "iframe_html" not in st.session_state:
-    st.session_state.iframe_html = None
-if "years" not in st.session_state:
-    # NOTE: The range of years will be loaded automatically from the chart's metadata. We just define this range here to avoid typing issues.
-    st.session_state.years = range(2000, 2022)
+st.session_state.chart_animation_images_folder = st.session_state.get("chart_animation_images_folder", DOWNLOADS_DIR)
+st.session_state.chart_animation_image_paths = st.session_state.get("chart_animation_image_paths", None)
+st.session_state.chart_animation_images_exist = st.session_state.get("images_exist", False)
+st.session_state.chart_animation_gif_file = st.session_state.get("gif_file", None)
+st.session_state.chart_animation_iframe_html = st.session_state.get("iframe_html", None)
+# NOTE: The range of years will be loaded automatically from the chart's metadata. We just define this range here to avoid typing issues.
+st.session_state.chart_animation_years = st.session_state.get("years", range(2000, 2022))
 
 # Step 1: Input chart URL and get years.
 st.markdown("### Step 1: Chart URL and timeline")
@@ -52,10 +46,10 @@ chart_url = st.text_input(
 slug = get_chart_slug(chart_url=chart_url)
 
 # Set images folder and output file.
-st.session_state.images_folder = DOWNLOADS_DIR / slug
-st.session_state.gif_file = DOWNLOADS_DIR / f"{slug}.gif"
-st.session_state.images_exist = (
-    len([image for image in st.session_state.images_folder.iterdir() if image.suffix == ".png"]) > 0
+st.session_state.chart_animation_images_folder = DOWNLOADS_DIR / slug
+st.session_state.chart_animation_gif_file = DOWNLOADS_DIR / f"{slug}.gif"
+st.session_state.chart_animation_images_exist = (
+    len([image for image in st.session_state.chart_animation_images_folder.iterdir() if image.suffix == ".png"]) > 0
 )
 
 
@@ -64,27 +58,27 @@ if st.button("Get chart"):
         st.error("Please enter a valid chart URL.")
         st.stop()
     # Embed the iframe in the app.
-    st.session_state.iframe_html = f"""
+    st.session_state.chart_animation_iframe_html = f"""
     <iframe src="{chart_url}" loading="lazy"
             style="width: 100%; height: 600px; border: 0px none;"
             allow="web-share; clipboard-write"></iframe>
     """
-    st.session_state.years = get_years_in_chart(chart_url)
+    st.session_state.chart_animation_years = get_years_in_chart(chart_url)
     st.session_state.show_image_settings = True
 
 # Display iframe if it was fetched.
-if st.session_state.iframe_html:
+if st.session_state.chart_animation_iframe_html:
     st.info("Modify chart as you wish, click on share -> copy link, and paste it in the box above.")
-    st.components.v1.html(st.session_state.iframe_html, height=600)  # type: ignore
+    st.components.v1.html(st.session_state.chart_animation_iframe_html, height=600)  # type: ignore
 
 if st.session_state.get("show_image_settings"):
     st.markdown("### Step 2: Image generation settings")
     # Slider for year range.
     year_min, year_max = st.slider(
         "Select year range",
-        min_value=min(st.session_state.years),
-        max_value=max(st.session_state.years),
-        value=(min(st.session_state.years), max(st.session_state.years)),
+        min_value=min(st.session_state.chart_animation_years),
+        max_value=max(st.session_state.chart_animation_years),
+        value=(min(st.session_state.chart_animation_years), max(st.session_state.chart_animation_years)),
     )
 
     # Tab and year range settings.
@@ -96,23 +90,25 @@ if st.session_state.get("show_image_settings"):
     )
 
     # Button to generate images.
-    if st.session_state.images_exist:
+    if st.session_state.chart_animation_images_exist:
         st.warning(
-            f"Images already exist in the folder: {st.session_state.images_folder}\nYears for which there is already a file will be skipped.\nEither delete them all or continue to get images for remaining years (if any is missing)."
+            f"Images already exist in the folder: {st.session_state.chart_animation_images_folder}\nYears for which there is already a file will be skipped.\nEither delete them all or continue to get images for remaining years (if any is missing)."
         )
         # Create a button to delete the folder.
         if st.button("Delete images"):
-            for image in [image for image in st.session_state.images_folder.iterdir() if image.suffix == ".png"]:
+            for image in [
+                image for image in st.session_state.chart_animation_images_folder.iterdir() if image.suffix == ".png"
+            ]:
                 image.unlink()
             # Update session state to reflect that images are deleted.
-            st.session_state.images_exist = False
+            st.session_state.chart_animation_images_exist = False
             st.info("Images deleted.")
 
     if st.button("Get images"):
         years = range(year_min, year_max + 1)
-        st.session_state.image_paths = get_images_from_chart_url(
+        st.session_state.chart_animation_image_paths = get_images_from_chart_url(
             chart_url=chart_url,
-            png_folder=st.session_state.images_folder,
+            png_folder=st.session_state.chart_animation_images_folder,
             tab=tab,
             years=years,
             year_range_open=year_range_open,
@@ -130,7 +126,9 @@ if st.session_state.get("show_gif_settings"):
     output_type = st.radio("Output Type", ["GIF", "Video"])
     if output_type == "Video":
         st.error("Video output is not yet supported. Please select GIF.")
-    st.session_state.gif_file = st.session_state.gif_file.with_suffix(".gif" if output_type == "GIF" else ".mp4")
+    st.session_state.chart_animation_gif_file = st.session_state.chart_animation_gif_file.with_suffix(
+        ".gif" if output_type == "GIF" else ".mp4"
+    )
     remove_duplicates = st.checkbox("Remove Duplicate Frames?", value=True)
     repetitions_last_frame = st.number_input("Repetitions of Last Frame", value=0, step=1)
     duration = st.number_input("Duration (ms)", value=200, step=10)
@@ -139,9 +137,9 @@ if st.session_state.get("show_gif_settings"):
 
     # Regenerate GIF button.
     if st.button("Generate animation"):
-        st.session_state.gif_file = create_gif_from_images(
-            image_paths=st.session_state.image_paths,
-            output_gif=st.session_state.gif_file,
+        st.session_state.chart_animation_gif_file = create_gif_from_images(
+            image_paths=st.session_state.chart_animation_image_paths,
+            output_gif=st.session_state.chart_animation_gif_file,
             duration=duration,
             loops=loop_count,
             remove_duplicate_frames=remove_duplicates,
@@ -150,6 +148,6 @@ if st.session_state.get("show_gif_settings"):
         )
 
     # GIF preview.
-    if st.session_state.gif_file.exists():
+    if st.session_state.chart_animation_gif_file.exists():
         st.info('Animation preview. Right click and "Save Image As..." to download it.')
-        st.image(str(st.session_state.gif_file), use_container_width=True)
+        st.image(str(st.session_state.chart_animation_gif_file), use_container_width=True)
