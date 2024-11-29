36b8f8189ea05a49edaa3baea50778ae09379a0e
lucasrodes <lucasrodes@users.noreply.github.com>
2024-11-29 18:35:29 +0100

    wip

--- etl/steps/data/garden/hmd/2024-11-27/hmd.py
+++ etl/steps/data/garden/hmd/2024-11-27/hmd.py
@@ -15,11 +15,33 @@ def run(dest_dir: str) -> None:
     ds_meadow = paths.load_dataset("hmd")
 
     # Read table from meadow dataset.
-    tb = ds_meadow.read("hmd")
+    paths.log.info("reading tables")
+    tb_lt = ds_meadow.read("life_tables")
+    tb_exp = ds_meadow.read("exposures")
+    tb_mort = ds_meadow.read("deaths")
+    tb_pop = ds_meadow.read("population")
+    tb_births = ds_meadow.read("births")
 
     #
     # Process data.
     #
+
+    # Life tables
+    tb_lt = standardize_sex_cat_names(tb_lt)
+    tb_lt = tb_lt.sort_values("format").drop_duplicates(
+        subset=[col for col in tb_lt.columns if col != "format"], keep="first"
+    )
+
+    ## Check
+    summary = tb_lt.groupby(["country", "year", "sex", "type", "age"], as_index=False).size().sort_values("size")
+    num_dups = summary.loc[summary["size"] != 1].shape[0]
+    assert num_dups <= 19
+    ## Final drops
+    tb_lt = tb_lt.loc[~(tb_lt["format"] == "5x1") & (tb_lt["age"] == "110+")]
+
+    # Exposures
+    tb_exp = standardize_sex_cat_names(tb_exp, {"female", "male", "total"})
+
     tb = geo.harmonize_countries(
         df=tb, countries_file=paths.country_mapping_path, excluded_countries_file=paths.excluded_countries_path
     )
@@ -35,3 +57,22 @@ def run(dest_dir: str) -> None:
 
     # Save changes in the new garden dataset.
     ds_garden.save()
+
+
+def standardize_sex_cat_names(tb, sex_expected=None):
+    # Define expected sex categories
+    if sex_expected is None:
+        sex_expected = {"females", "males", "total"}
+    else:
+        sex = {s.lower() for s in sex_expected}
+
+    # Set sex categories to lowercase
+    tb["sex"] = tb["sex"].str.lower()
+
+    # Sanity check categories
+    sex_found = set(tb["sex"].unique())
+    assert sex_found == sex_expected, f"Unexpected sex categories! Found {sex_found} but expected {sex_expected}"
+
+    # Rename
+    tb["sex"] = tb["sex"].replace({"females": "female", "males": "male"})
+    return tb
