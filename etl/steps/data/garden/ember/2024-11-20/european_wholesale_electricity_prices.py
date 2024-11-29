6642d7af9dc3e41d45fd657c3775af7582705c21
Pablo Rosado <pabloarosado@gmail.com>
2024-11-29 14:22:06 +0100

    Complete to-do

--- etl/steps/data/garden/ember/2024-11-20/european_wholesale_electricity_prices.py
+++ etl/steps/data/garden/ember/2024-11-20/european_wholesale_electricity_prices.py
@@ -37,8 +37,11 @@ def run(dest_dir: str) -> None:
     # But we also need to create an annual table of average wholesale electricity prices.
     tb_annual = tb_monthly.copy()
     tb_annual["year"] = tb_annual["date"].str[:4].astype("Int64")
-    # TODO: Consider removing incomplete years.
-    tb_annual = tb_annual.groupby(["country", "year"], observed=True, as_index=False).agg({"price": "mean"})
+    # NOTE: We will include only complete years. This means that the latest year will not be included. But also, we will disregard country-years like Ireland 2022, which only has data for a few months, for some reason.
+    n_months = tb_annual.groupby(["country", "year"], observed=True, as_index=False)["date"].transform("count")
+    tb_annual = (
+        tb_annual[n_months == 12].groupby(["country", "year"], observed=True, as_index=False).agg({"price": "mean"})
+    )
 
     # Improve table formats.
     tb_monthly = tb_monthly.format(["country", "date"], short_name="european_wholesale_electricity_prices_monthly")
