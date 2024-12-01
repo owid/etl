6a1f5f200f7ac524d3b4a88c6369de6b9fa9b960
veronikasamborska1994 <32176660+veronikasamborska1994@users.noreply.github.com>
2024-12-01 10:41:42 +0100

    Update gender_statistics.py

--- etl/steps/data/garden/wb/2024-06-10/gender_statistics.py
+++ etl/steps/data/garden/wb/2024-06-10/gender_statistics.py
@@ -73,7 +73,7 @@ def run(dest_dir: str) -> None:
     # Pivot the dataframe so that each indicator is a separate column
     tb = tb.pivot(index=["country", "year"], columns="wb_seriescode", values="value").reset_index()
 
-    # Apply the update
+    # Slovakia - should be the same as Czechoslovakia in the period 1970-1993 for the indicator SG.LAW.EQRM.WK as confirmed by the source (but not yet updated on their database)
     tb.loc[(tb["country"] == "Slovakia") & (tb["year"].between(1970, 1993)), "SG.LAW.EQRM.WK"] = 1
     # Add metadata by finding the descriptions and sources using indicator codes
     tb = add_metadata(tb, metadata_tb)
