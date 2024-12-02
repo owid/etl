a27b8ae61b1c3ec34dd509fde635756a11a80181
veronikasamborska1994 <32176660+veronikasamborska1994@users.noreply.github.com>
2024-12-02 13:56:32 +0100

    projections and estimates

--- etl/steps/data/garden/urbanization/2024-12-02/ghsl_urban_centers.py
+++ etl/steps/data/garden/urbanization/2024-12-02/ghsl_urban_centers.py
@@ -1,10 +1,13 @@
 """Load a meadow dataset and create a garden dataset."""
+import owid.catalog.processing as pr
 
 from etl.data_helpers import geo
 from etl.helpers import PathFinder, create_dataset
 
 # Get paths and naming conventions for current step.
 paths = PathFinder(__file__)
+
+START_OF_PROJECTIONS = 2025
 
 
 def run(dest_dir: str) -> None:
@@ -21,6 +24,22 @@ def run(dest_dir: str) -> None:
     # Process data.
     #
     tb = geo.harmonize_countries(df=tb, countries_file=paths.country_mapping_path)
+    tb = tb.drop(columns=["urban_center_name"])
+
+    # Split data into estimates and projections.
+    past_estimates = tb[tb["year"] < START_OF_PROJECTIONS].copy()
+    future_projections = tb[tb["year"] >= START_OF_PROJECTIONS - 5].copy()
+
+    # Now, for each column, split it into two (projections and estimates).
+    for col in ["urban_pop", "urban_area", "urban_density"]:
+        if col not in ["country", "year"]:
+            past_estimates[f"{col}_estimates"] = tb.loc[tb["year"] < START_OF_PROJECTIONS, col]
+            future_projections[f"{col}_projections"] = tb.loc[tb["year"] >= START_OF_PROJECTIONS - 5, col]
+            past_estimates = past_estimates.drop(columns=[col])
+            future_projections = future_projections.drop(columns=[col])
+
+    # Merge past estimates and future projections
+    tb = pr.merge(past_estimates, future_projections, on=["country", "year"], how="outer")
 
     tb = tb.format(["country", "year"])
 
