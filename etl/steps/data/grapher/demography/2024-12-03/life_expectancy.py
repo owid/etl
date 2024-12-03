813aa1caf9861146b761bd719da24b4d727ec71a
lucasrodes <lucasrodes@users.noreply.github.com>
2024-12-03 13:28:17 +0100

    wip

---
+++ etl/steps/data/grapher/demography/2024-12-03/life_expectancy.py
@@ -0,0 +1,35 @@
+"""Load a garden dataset and create a grapher dataset."""
+
+from etl.helpers import PathFinder, create_dataset
+
+# Get paths and naming conventions for current step.
+paths = PathFinder(__file__)
+
+
+def run(dest_dir: str) -> None:
+    #
+    # Load inputs.
+    #
+    # Load garden dataset.
+    ds_garden = paths.load_dataset("life_expectancy")
+
+    # Read table from garden dataset.
+    tables = [
+        ds_garden["life_expectancy"],
+        ds_garden["life_expectancy_with_proj"],
+        ds_garden["life_expectancy_only_proj"],
+    ]
+    #
+    # Process data.
+    #
+
+    #
+    # Save outputs.
+    #
+    # Create a new grapher dataset with the same metadata as the garden dataset.
+    ds_grapher = create_dataset(
+        dest_dir, tables=tables, check_variables_metadata=True, default_metadata=ds_garden.metadata
+    )
+
+    # Save changes in the new grapher dataset.
+    ds_grapher.save()
