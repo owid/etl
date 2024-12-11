8caa5515d26ebae9e3229de6b8798b4bd62f876f
spoonerf <spoonerfiona@gmail.com>
2024-12-11 11:17:08 +0000

    adding some garden steps

--- etl/steps/data/garden/who/2024-12-10/vaccination_coverage.py
+++ etl/steps/data/garden/who/2024-12-10/vaccination_coverage.py
@@ -1,5 +1,7 @@
 """Load a meadow dataset and create a garden dataset."""
 
+from owid.catalog import Table
+
 from etl.data_helpers import geo
 from etl.helpers import PathFinder, create_dataset
 
@@ -23,7 +25,15 @@ def run(dest_dir: str) -> None:
     tb = geo.harmonize_countries(
         df=tb, countries_file=paths.country_mapping_path, excluded_countries_file=paths.excluded_countries_path
     )
-    tb = tb.format(["country", "year"])
+    tb = use_only_wuenic_data(tb)
+
+    antigen_dict = (
+        tb[["antigen", "antigen_description"]].drop_duplicates().set_index("antigen")["antigen_description"].to_dict()
+    )
+
+    tb = clean_data(tb)
+
+    tb = tb.format(["country", "year", "antigen_description"])
 
     #
     # Save outputs.
@@ -35,3 +45,26 @@ def run(dest_dir: str) -> None:
 
     # Save changes in the new garden dataset.
     ds_garden.save()
+
+
+def use_only_wuenic_data(tb: Table) -> Table:
+    """
+    Keep only data that is from WUENIC - estimated by World Health Organization and UNICEF.
+    """
+    assert "WUENIC" in tb["coverage_category"].unique(), "No data from WUENIC in the table."
+    tb = tb[tb["coverage_category"] == "WUENIC"]
+    tb = tb.drop(columns=["coverage_category"])
+    return tb
+
+
+def clean_data(tb: Table) -> Table:
+    """
+    Clean up the data:
+    - Remove rows where coverage is NA
+    - Remove unneeded columns
+    """
+
+    tb = tb[tb["coverage"].notna()]
+    tb = tb.drop(columns=["group", "code", "coverage_category_description", "target_number", "doses"])
+
+    return tb
