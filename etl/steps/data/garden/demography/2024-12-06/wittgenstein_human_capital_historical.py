c19a638d8539539d113ad286dadfffffaedd4f42
lucasrodes <lucasrodes@users.noreply.github.com>
2024-12-11 01:32:27 +0100

    wip

---
+++ etl/steps/data/garden/demography/2024-12-06/wittgenstein_human_capital_historical.py
@@ -0,0 +1,150 @@
+"""Load a meadow dataset and create a garden dataset."""
+
+import owid.catalog.processing as pr
+
+from etl.helpers import PathFinder, create_dataset
+
+from .shared import make_table
+
+# Get paths and naming conventions for current step.
+paths = PathFinder(__file__)
+# Columns index
+COLUMNS_INDEX = [
+    "country",
+    "year",
+    "scenario",
+    "sex",
+    "age",
+    "education",
+]
+
+
+def run(dest_dir: str) -> None:
+    #
+    # Load inputs.
+    #
+    # Load meadow dataset.
+    ds_meadow = paths.load_dataset("wittgenstein_human_capital_historical")
+
+    # Read table from meadow dataset.
+    paths.log.info("reading tables...")
+    tb = ds_meadow.read("main").reset_index(drop=True)
+    tb_age = ds_meadow.read("by_age").reset_index(drop=True)
+    tb_sex = ds_meadow.read("by_sex").reset_index(drop=True)
+    tb_edu = ds_meadow.read("by_edu").reset_index(
+        drop=True
+    )  # TODO: add metadata field for table explaining the different education levels
+    tb_sex_age = ds_meadow.read("by_sex_age").reset_index(drop=True)
+    tb_age_edu = ds_meadow.read("by_age_edu").reset_index(drop=True)
+    tb_sex_age_edu = ds_meadow.read("by_sex_age_edu").reset_index(drop=True)
+
+    #
+    # Process data.
+    #
+
+    # 1/ MAKE MAIN TABLE
+    paths.log.info("baking main table...")
+    tb = make_table(
+        tb,
+        country_mapping_path=paths.country_mapping_path,
+        cols_single=["tdr", "ggapmys25", "mage", "ydr", "ggapmys15", "odr"],
+        cols_range=["growth", "imm", "emi", "cbr", "nirate", "cdr"],
+        per_100=["tdr", "odr", "ydr"],
+        per_1000=["emi", "imm"],
+    )
+
+    # 2.1/ MAKE BY AGE TABLE (sexratio)
+    paths.log.info("baking tables by age...")
+    tb_age = make_table(
+        tb_age,
+        country_mapping_path=paths.country_mapping_path,
+        all_single=True,
+        per_100=["sexratio"],
+    )
+
+    # 2.2/ BY SEX
+    paths.log.info("baking tables by sex...")
+    tb_sex = make_table(
+        tb_sex,
+        country_mapping_path=paths.country_mapping_path,
+        all_range=True,
+    )
+
+    # 2.3/ BY EDU
+    paths.log.info("baking tables by education...")
+    tb_edu = make_table(
+        tb_edu,
+        country_mapping_path=paths.country_mapping_path,
+        cols_single=["ggapedu15", "ggapedu25"],
+        cols_range=["macb", "tfr", "net"],
+        per_1000=["net"],
+    )
+
+    # 3.1/ BY SEX+AGE
+    paths.log.info("baking tables by sex+age...")
+    tb_sex_age = make_table(
+        tb_sex_age,
+        country_mapping_path=paths.country_mapping_path,
+        all_single=True,
+    )
+
+    # 3.2/ BY AGE+EDU
+    paths.log.info("baking tables by age+education...")
+    assert "total" not in set(tb_age_edu["age"].unique()), "Unexpected age category: 'total'"
+    tb_age_edu = make_table(
+        tb_age_edu,
+        country_mapping_path=paths.country_mapping_path,
+        all_range=True,
+    )
+
+    # 4.1/ BY SEX+AGE+EDU
+    paths.log.info("baking tables by sex+age+education...")
+    tb_sex_age_edu = make_table(
+        tb_sex_age_edu,
+        country_mapping_path=paths.country_mapping_path,
+        dtypes={
+            "sex": "category",
+            "age": "category",
+            "education": "category",
+        },
+        cols_single=["pop", "prop"],
+        cols_range=["assr"],
+        per_1000=["pop"],
+        per_100=["assr"],
+    )
+
+    # Add education="some_education" (only for sex=total and age=total, and indicator 'pop')
+    cols_index = ["country", "year", "age", "sex", "scenario"]
+    tb_tmp = tb_sex_age_edu.loc[
+        tb_sex_age_edu["education"].isin(["total", "no_education"]), cols_index + ["education", "pop"]
+    ]
+    tb_tmp = tb_tmp.pivot(index=cols_index, columns="education", values="pop").reset_index().dropna()
+    tb_tmp["some_education"] = tb_tmp["total"] - tb_tmp["no_education"]
+    assert (tb_tmp["some_education"] >= 0).all()
+    tb_tmp = tb_tmp.melt(id_vars=cols_index, value_vars="some_education", var_name="education", value_name="pop")
+
+    tb_sex_age_edu = pr.concat([tb_sex_age_edu, tb_tmp], ignore_index=True)
+
+    #
+    # Save outputs.
+    #
+    # Format
+    tables = [
+        tb.format(["country", "year", "scenario"], short_name="main"),
+        tb_age.format(["country", "scenario", "age", "year"], short_name="by_age"),
+        tb_sex.format(["country", "scenario", "sex", "year"], short_name="by_sex"),
+        tb_edu.format(["country", "scenario", "education", "year"], short_name="by_edu"),
+        tb_sex_age.format(["country", "scenario", "sex", "age", "year"], short_name="by_sex_age"),
+        tb_age_edu.format(["country", "scenario", "age", "education", "year"], short_name="by_age_edu"),
+        tb_sex_age_edu.format(["country", "scenario", "sex", "age", "education", "year"], short_name="by_sex_age_edu"),
+    ]
+    # Create a new garden dataset with the same metadata as the meadow dataset.
+    ds_garden = create_dataset(
+        dest_dir,
+        tables=tables,
+        check_variables_metadata=True,
+        default_metadata=ds_meadow.metadata,
+    )
+
+    # Save changes in the new garden dataset.
+    ds_garden.save()
