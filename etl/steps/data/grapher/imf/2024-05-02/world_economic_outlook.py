c2bae9fbdc89a11d795f28459005f7b1b950a9d4
lucasrodes <lucasrodes@users.noreply.github.com>
2024-12-24 15:12:10 +0100

    may

--- etl/steps/data/grapher/imf/2024-05-02/world_economic_outlook.py
+++ etl/steps/data/grapher/imf/2024-05-02/world_economic_outlook.py
@@ -37,7 +37,7 @@ def run(dest_dir: str) -> None:
         tb = tb.drop(columns="last_obs")
 
     # Reinstate the index
-    tb = tb.set_index(["country", "year"], verify_integrity=True)
+    tb = tb.format(["country",)
 
     #
     # Save outputs.
