558e912b724a2c006ba945af5a3b094b522d0b6f
lucasrodes <lucasrodes@users.noreply.github.com>
2024-12-27 18:03:55 +0100

    mvp

--- apps/wizard/app_pages/dataset_search.py
+++ apps/wizard/app_pages/dataset_search.py
@@ -12,12 +12,28 @@ import streamlit as st
 
 from apps.wizard.utils.components import grapher_chart, st_horizontal
 from etl.config import OWID_ENV
+from etl.db import read_sql
 from etl.grapher.io import load_variables_in_dataset
+from etl.indicator_upgrade.indicator_update import find_charts_from_variable_ids
 
 ICONS_DIMENSIONS = {
     "age": ":material/cake:",
     "sex": ":material/wc:",
 }
+
+
+# TODO: move it elsewhere
+@st.cache_data
+def get_charts_view():
+    query = """SELECT SUBSTRING_INDEX(url, '/', -1) AS slug, views_7d, views_365d FROM analytics_pageviews WHERE url LIKE %s;"""
+    param = ("%ourworldindata.org/grapher/%",)
+
+    df = read_sql(
+        query,
+        OWID_ENV.engine,
+        params=param,
+    )
+    return df.set_index("slug").to_dict(orient="index")
 
 
 class IndicatorArray:
@@ -106,6 +122,33 @@ class IndicatorWithDimensions(IndicatorArray):
         return self.df.loc[dim_values, "variable"]
 
 
+class IndicatorsInCharts:
+    def __init__(self, indicator_ids):
+        # Get affected charts
+        charts = find_charts_from_variable_ids(indicator_ids)
+        # Map chart ID to chart object (1:1)
+        self.chart_id_to_chart = {c.id: c for c in charts}
+        # Map indicator ID to chart IDs (1:m)
+        self.indicator_id_to_chart_ids = self._get_charts_by_indicator_id(charts)
+
+    @classmethod
+    def from_indicators(cls, indicators):
+        indicator_ids = {v.id for v in indicators}
+        return IndicatorsInCharts(indicator_ids)
+
+    def _get_charts_by_indicator_id(self, charts):
+        indicators_to_chart_ids = defaultdict(list)
+        for c in charts:
+            for dim in c.config["dimensions"]:
+                indicators_to_chart_ids[dim["variableId"]].append(c.id)
+        return indicators_to_chart_ids
+
+    def get_charts(self, indicator_id):
+        chart_ids = self.indicator_id_to_chart_ids[indicator_id]
+        chart = [self.chart_id_to_chart[c] for c in chart_ids]
+        return chart
+
+
 def parse_indicators(indicators_raw):
     indicators = []
 
@@ -138,8 +181,22 @@ def filter_sort_indicators(indicators):
     return indicators
 
 
+def _get_average_daily_views(chart_slug):
+    num_views = CHART_VIEWS.get(chart_slug)
+
+    if num_views is not None:
+        num_views = num_views["views_365d"]
+        return round(num_views / 365, 2)
+
+
+def _get_title_chart(chart):
+    if chart.publishedAt is None:
+        return f"(DRAFT) {chart.config['title']}"
+    return chart.config["title"]
+
+
 @st.fragment
-def st_show_indicator(indicator):
+def st_show_indicator(indicator, indicator_charts):
     with st.container(border=False):
         # Allocate space for indicator title / URI
         st_header = st.container()
@@ -176,8 +233,41 @@ def st_show_indicator(indicator):
                 # Get indicator-dimensions combination
                 var = indicator.get_dimension(dim_values)
             else:
-                st.markdown("No dimensions")
+                # st.markdown("No dimensions")
                 var = indicator.indicators[0]
+
+            # Charts
+            charts = indicator_charts.get_charts(var.id)
+            df_charts = (
+                pd.DataFrame(
+                    {
+                        # "thumbnail": [OWID_ENV.thumb_url(chart.slug) for chart in charts],  # type: ignore
+                        "Id": [chart.id for chart in charts],  # type: ignore
+                        "Chart": [_get_title_chart(chart) for chart in charts],  # type: ignore
+                        # "Views (last 7 days)": [CHART_VIEWS.get(chart.slug)["views_7d"] for chart in charts],  # type: ignore
+                        "Daily views (year-average)": [_get_average_daily_views(chart.slug) for chart in charts],  # type: ignore
+                        "Edit": [OWID_ENV.chart_admin_site(chart.id) for chart in charts],  # type: ignore
+                    }
+                )
+                .set_index("Id")
+                .sort_values("Daily views (year-average)", ascending=False)
+            )
+            if not df_charts.empty:
+                st.dataframe(
+                    df_charts,
+                    column_config={
+                        "Edit": st.column_config.LinkColumn(
+                            label="Chart",
+                            help="Link to chart edit page",
+                            display_text=r"Edit",
+                            width="small",
+                        ),
+                    },
+                    use_container_width=True,
+                )
+
+            # l = [chart.config["title"] for chart in charts]
+            # st.table({"Charts": l})
 
         # Show indicator title and URI
         name = var.name
@@ -201,6 +291,9 @@ st.set_page_config(
 )
 
 
+CHART_VIEWS = get_charts_view()
+
+
 # with st.sidebar:
 default = st.query_params.get("datasetId")
 
@@ -214,20 +307,57 @@ with st_horizontal():
 
 # DATASET_ID = 6869, 6813
 if DATASET_ID is not None:
-    # Load variables for given dataset
-    indicators_raw = load_variables_in_dataset(dataset_id=[int(DATASET_ID)])
+    tab_indicators, tab_charts = st.tabs(["Indicators", "Charts"])
 
-    # Parse indicators
-    indicators = parse_indicators(indicators_raw)
+    with tab_indicators:
+        # Load variables for given dataset
+        indicators_raw = load_variables_in_dataset(dataset_id=[int(DATASET_ID)])
 
-    # Apply filters / sorting
-    indicators = filter_sort_indicators(indicators)
+        # Chart info
+        indicator_charts = IndicatorsInCharts.from_indicators(indicators_raw)
 
-    ########################################
-    # RENDER
-    # TODO: show title based on selection
-    ########################################
-    st.divider()
-    for indicator in indicators:
-        st_show_indicator(indicator)
+        # Parse indicators
+        indicators = parse_indicators(indicators_raw)
+
+        # Apply filters / sorting
+        indicators = filter_sort_indicators(indicators)
+
+        # ########################################
+        # # RENDER
+        # # TODO: show title based on selection
+        # ########################################
         st.divider()
+        for indicator in indicators:
+            st_show_indicator(indicator, indicator_charts)
+            st.divider()
+
+    with tab_charts:
+        charts = indicator_charts.chart_id_to_chart.values()
+        df_charts = (
+            pd.DataFrame(
+                {
+                    # "thumbnail": [OWID_ENV.thumb_url(chart.slug) for chart in charts],  # type: ignore
+                    "Id": [chart.id for chart in charts],  # type: ignore
+                    "Chart": [_get_title_chart(chart) for chart in charts],  # type: ignore
+                    # "Views (last 7 days)": [CHART_VIEWS.get(chart.slug)["views_7d"] for chart in charts],  # type: ignore
+                    "Daily views (year-average)": [_get_average_daily_views(chart.slug) for chart in charts],  # type: ignore
+                    "Edit": [OWID_ENV.chart_admin_site(chart.id) for chart in charts],  # type: ignore
+                }
+            )
+            .set_index("Id")
+            .sort_values("Daily views (year-average)", ascending=False)
+        )
+        if not df_charts.empty:
+            st.dataframe(
+                df_charts,
+                column_config={
+                    "Edit": st.column_config.LinkColumn(
+                        label="Chart",
+                        help="Link to chart edit page",
+                        display_text=r"Edit",
+                        width="small",
+                    ),
+                },
+                use_container_width=True,
+            )
+        st.text(f"{len(charts)} charts")
