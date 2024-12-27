5cecb70bb9b19bfd61c84301fc1e3a4f65d8cebb
lucasrodes <lucasrodes@users.noreply.github.com>
2024-12-27 15:54:12 +0100

    🎉 Add indicator handling with dimensions in dataset search

--- apps/wizard/app_pages/dataset_search.py
+++ apps/wizard/app_pages/dataset_search.py
@@ -10,19 +10,69 @@ from collections import defaultdict
 import pandas as pd
 import streamlit as st
 
+from apps.wizard.utils.components import grapher_chart, st_horizontal
 from etl.config import OWID_ENV
 from etl.grapher.io import load_variables_in_dataset
 
+ICONS_DIMENSIONS = {
+    "age": ":material/cake:",
+    "sex": ":material/wc:",
+}
 
-class IndicatorWithDimensions:
+
+class IndicatorArray:
+    def __init__(self, indicators, key):
+        self.indicators = indicators
+        self.key = key
+        self.dimensions = None
+
+    @property
+    def is_mdim(self):
+        return self.dimensions is not None
+
+
+class IndicatorSingleDimension(IndicatorArray):
+    def __init__(self, indicator):
+        """Object with a single indicator."""
+        super().__init__([indicator], indicator.catalogPath)
+
+    def get_dimension(self):
+        return self.indicators[0]
+
+
+class IndicatorWithDimensions(IndicatorArray):
     def __init__(self, indicators):
         """Object with all indicator-dimensions.
 
         indicators: List of Variable objects. They should all belong to the same indicator.
         """
-        self.indicators = indicators
+        super().__init__(indicators, self.check_and_extract_key(indicators))
         self.df = self.create_df()
         self.dimensions = self.get_dimensions()
+
+    def check_and_extract_key(self, indicators):
+        short_name = None
+        table = None
+        for indicator in indicators:
+            # Extract short_name
+            short_name_ = indicator.dimensions["originalShortName"]
+            assert isinstance(indicator.catalogPath, str), f"`catalogPath` is empty for variable {indicator.id}"
+            # Extract table URI
+            table_ = indicator.catalogPath.split("#")[0]
+
+            if short_name is None:
+                short_name = short_name_
+            if table is None:
+                table = table_
+
+            # Checks
+            assert short_name == short_name_, f"Short name mismatch: {short_name} != {short_name_}"
+            assert table == table_, f"Table mismatch: {table} != {table_}"
+
+        assert isinstance(table, str), "Table is empty"
+
+        key = f"{table.replace('grapher/', '')}#{short_name}"
+        return key
 
     def create_df(self):
         data = []
@@ -47,67 +97,137 @@ class IndicatorWithDimensions:
         return df
 
     def get_dimensions(self):
-        dimensions = {level: self.df.index.get_level_values(level).unique().tolist() for level in self.df.index.names}
+        dimensions = {
+            level: sorted(self.df.index.get_level_values(level).unique().tolist()) for level in self.df.index.names
+        }
         return dimensions
 
-
-# DATASET_ID = 6869
-DATASET_ID = 6813
-
-# Load variables for given dataset
-indicators = load_variables_in_dataset(dataset_id=[DATASET_ID])
-
-# Prepare data
-data_with_dim = defaultdict(list)
-data_no_dim = []
-for indicator in indicators:
-    # Add dimensions, if any
-    if indicator.dimensions is not None:
-        short_name = indicator.dimensions["originalShortName"]
-        table = indicator.catalogPath
-        assert isinstance(indicator.catalogPath, str), f"`catalogPath` is empty for variable {indicator.id}"
-        table = indicator.catalogPath.split("#")[0]
-        key = f"{table}#{short_name}"
-        data_with_dim[key].append(indicator)
-    # Does not have dimensions
-    else:
-        assert isinstance(indicator.catalogPath, str), f"`catalogPath` is empty for variable {indicator.id}"
-        key = indicator.catalogPath
-        data_no_dim.append(indicator)
-
-# Prepare objects
-data = {}
-for key, vars in data_with_dim.items():
-    data[key] = IndicatorWithDimensions(vars)
+    def get_dimension(self, dim_values):
+        return self.df.loc[dim_values, "variable"]
 
 
-########################################
-# RENDER
-# TODO: show title based on selection
-########################################
-# st.write(st.session_state)
-for key, indicator in data.items():
-    with st.container(border=False):
-        s = st.container()
+def parse_indicators(indicators_raw):
+    indicators = []
 
-        dim_value = None
-        for dim, options in indicator.dimensions.items():
-            key_pills = f"dataset_pills_{key}_{dim}"
-            st.pills(
-                dim,
-                options,
-                key=key_pills,
-            )
-
-            dim_value = st.session_state.get(key_pills)
-
-        if dim_value is not None:
-            var = indicator.df.loc[dim_value].item()
-            name = var.name
-            iid = var.id
-            s.markdown(f"[**{name}**]({OWID_ENV.indicator_admin_site(iid)})")
-            s.caption(var.catalogPath)
+    # Group indicators with dimensions by short_name (add them to indicators_with_dim)
+    # and those without dimensions (add them to indicators_no_dim)
+    indicators_with_dim = defaultdict(list)
+    for indicator in indicators_raw:
+        # Add dimensions, if any
+        if indicator.dimensions is not None:
+            short_name = indicator.dimensions["originalShortName"]
+            assert isinstance(indicator.catalogPath, str), f"`catalogPath` is empty for variable {indicator.id}"
+            table = indicator.catalogPath.split("#")[0]
+            key = f"{table}#{short_name}"
+            indicators_with_dim[key].append(indicator)
+        # Does not have dimensions
         else:
-            s.markdown(f"**{key}**")
-            s.caption(f"{key}")
+            assert isinstance(indicator.catalogPath, str), f"`catalogPath` is empty for variable {indicator.id}"
+            key = indicator.catalogPath
+            indicators.append(IndicatorSingleDimension(indicator))
+
+    # Prepare objects with indicator-collection
+    for key, vars in indicators_with_dim.items():
+        indicators.append(IndicatorWithDimensions(vars))
+
+    return indicators
+
+
+def filter_sort_indicators(indicators):
+    indicators.sort(key=lambda x: (x.key is None, x.key))
+    return indicators
+
+
+@st.fragment
+def st_show_indicator(indicator):
+    with st.container(border=False):
+        # Allocate space for indicator title / URI
+        st_header = st.container()
+        st_metadata_left, st_metadata_right = st.columns(2)
+
+        # Dimension selection
+        # st.write(indicator.is_mdim)
+        # st.write(indicator.dimensions)
+        with st_metadata_right:
+            # Show dimensions as pills -- TODO: add icons for recognized dimensions
+            if indicator.is_mdim:
+                # Dimensions
+                with st.container(border=True):
+                    st.markdown("**Dimensions**")
+                    # with st_horizontal():
+                    #     with st.container(border=True):
+                    dim_values = []
+                    for dim, options in indicator.dimensions.items():
+                        key_pills = f"dataset_pills_{indicator.key}_{dim}"
+                        st.pills(
+                            dim,
+                            options,
+                            key=key_pills,
+                            default=options[0],
+                        )
+
+                        dim_value_ = st.session_state.get(key_pills)
+                        dim_values.append(dim_value_)
+                    dim_values = tuple(dim_values)
+
+                # Sanity check on dimensions
+                assert all(value is not None for value in dim_values)
+
+                # Get indicator-dimensions combination
+                var = indicator.get_dimension(dim_values)
+            else:
+                st.markdown("No dimensions")
+                var = indicator.indicators[0]
+
+        # Show indicator title and URI
+        name = var.name
+        iid = var.id
+        with st_header:
+            with st_horizontal(vertical_alignment="center"):
+                st.markdown(f"[**{name}**]({OWID_ENV.indicator_admin_site(iid)})")
+                st.caption(var.catalogPath.replace("grapher/", ""))
+
+        # Show chart (contains description, and other metadata fields)
+        with st_metadata_left:
+            grapher_chart(variable_id=iid, tab="map")  # type: ignore
+
+
+# CONFIG
+st.set_page_config(
+    # page_title="Wizard: Dataset Explorer",
+    layout="wide",
+    page_icon="🪄",
+    # initial_sidebar_state="collapsed",
+)
+
+
+# with st.sidebar:
+default = st.query_params.get("datasetId")
+
+# default = 6813
+with st_horizontal():
+    DATASET_ID = st.text_input(
+        "Dataset id",
+        placeholder="6813",
+        value=default,
+    )
+
+# DATASET_ID = 6869, 6813
+if DATASET_ID is not None:
+    # Load variables for given dataset
+    indicators_raw = load_variables_in_dataset(dataset_id=[int(DATASET_ID)])
+
+    # Parse indicators
+    indicators = parse_indicators(indicators_raw)
+
+    # Apply filters / sorting
+    indicators = filter_sort_indicators(indicators)
+
+    ########################################
+    # RENDER
+    # TODO: show title based on selection
+    ########################################
     st.divider()
+    for indicator in indicators:
+        st_show_indicator(indicator)
+        st.divider()
