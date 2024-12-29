f4b52ed08fc45e5101d0175e22bac2e61d6d4e39
lucasrodes <lucasrodes@users.noreply.github.com>
2024-12-29 20:02:43 +0100

    use xlsx instead

--- snapshots/health/2024-12-29/isaps_plastic_surgery.py
+++ snapshots/health/2024-12-29/isaps_plastic_surgery.py
@@ -15,7 +15,7 @@ SNAPSHOT_VERSION = Path(__file__).parent.name
 @click.option("--path-to-file", prompt=True, type=str, help="Path to local data file.")
 def main(path_to_file: str, upload: bool) -> None:
     # Create a new snapshot.
-    snap = Snapshot(f"health/{SNAPSHOT_VERSION}/isaps_plastic_surgery.csv")
+    snap = Snapshot(f"health/{SNAPSHOT_VERSION}/isaps_plastic_surgery.xlsx")
 
     # Copy local data file to snapshots data folder, add file to DVC and upload to S3.
     snap.create_snapshot(filename=path_to_file, upload=upload)
