c0f8f42eba1ae2b91b87eb7f7f92779e87ff192f
Veronika Samborska <32176660+veronikasamborska1994@users.noreply.github.com>
2024-12-31 11:18:56 +0000

    📊 Marriages and Divorces: OECD Family Database (#3773)

---
+++ etl/steps/data/meadow/oecd/2024-12-30/family_database.py
@@ -0,0 +1,36 @@
+"""Load a snapshot and create a meadow dataset."""
+
+from etl.helpers import PathFinder, create_dataset
+
+# Get paths and naming conventions for current step.
+paths = PathFinder(__file__)
+
+
+def run(dest_dir: str) -> None:
+    #
+    # Load inputs.
+    #
+    # Retrieve snapshot.
+    snap = paths.load_snapshot("family_database.csv")
+
+    # Load data from snapshot.
+    tb = snap.read(safe_types=False)
+    columns_to_use = ["Country", "Indicator", "TIME_PERIOD", "OBS_VALUE"]
+
+    tb = tb[columns_to_use]
+    tb = tb.rename(columns={"TIME_PERIOD": "year", "OBS_VALUE": "value"})
+
+    #
+    # Process data.
+    #
+    # Ensure all columns are snake-case, set an appropriate index, and sort conveniently.
+    tb = tb.format(["country", "year", "indicator"])
+
+    #
+    # Save outputs.
+    #
+    # Create a new meadow dataset with the same metadata as the snapshot.
+    ds_meadow = create_dataset(dest_dir, tables=[tb], check_variables_metadata=True, default_metadata=snap.metadata)
+
+    # Save changes in the new meadow dataset.
+    ds_meadow.save()
