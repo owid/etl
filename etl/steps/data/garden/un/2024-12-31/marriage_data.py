5a25e9a1810c0c26aca328982ce12bb6fc892ada
veronikasamborska1994 <32176660+veronikasamborska1994@users.noreply.github.com>
2025-01-01 14:29:07 +0000

    add World Marriage Data 2019

---
+++ etl/steps/data/garden/un/2024-12-31/marriage_data.py
@@ -0,0 +1,40 @@
+"""Load a meadow dataset and create a garden dataset."""
+
+import numpy as np
+
+from etl.data_helpers import geo
+from etl.helpers import PathFinder, create_dataset
+
+# Get paths and naming conventions for current step.
+paths = PathFinder(__file__)
+
+
+def run(dest_dir: str) -> None:
+    #
+    # Load inputs.
+    #
+    # Load meadow dataset.
+    ds_meadow = paths.load_dataset("marriage_data")
+
+    # Read table from meadow dataset.
+    tb = ds_meadow.read("marriage_data")
+
+    #
+    # Process data.
+    #
+    tb = geo.harmonize_countries(df=tb, countries_file=paths.country_mapping_path)
+
+    tb["never_married"] = 100 - tb["ever_married"]
+
+    tb = tb.format(["country", "year", "age", "sex"])
+
+    #
+    # Save outputs.
+    #
+    # Create a new garden dataset with the same metadata as the meadow dataset.
+    ds_garden = create_dataset(
+        dest_dir, tables=[tb], check_variables_metadata=True, default_metadata=ds_meadow.metadata
+    )
+
+    # Save changes in the new garden dataset.
+    ds_garden.save()
