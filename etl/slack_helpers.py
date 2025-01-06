f0b36e35f7c841c845e16334489490c5d43f1fdc
lucasrodes <lucasrodes@users.noreply.github.com>
2025-01-06 20:51:03 +0100

    utils to get slack channels

--- etl/slack_helpers.py
+++ etl/slack_helpers.py
@@ -9,10 +9,10 @@ from etl import config
 slack_client = WebClient(token=config.SLACK_API_TOKEN)
 
 
-def send_slack_message(channel: str, message: str) -> None:
+def send_slack_message(channel: str, message: str, **kwargs) -> None:
     """Send `message` to Slack channel `channel`."""
     if config.SLACK_API_TOKEN:
-        slack_client.chat_postMessage(channel=channel, text=message)
+        slack_client.chat_postMessage(channel=channel, text=message, **kwargs)
 
 
 def format_slack_message(method, url, status_code, req_body, res_body):
@@ -39,3 +39,17 @@ def format_slack_message(method, url, status_code, req_body, res_body):
     message += f"Response\n```\n{res_body}\n```\n"
 
     return message
+
+
+def get_channels():
+    response = slack_client.conversations_list()
+    channels = response["channels"]
+    return channels
+
+
+def channels_mapping():
+    channels = get_channels()
+    mapping = {}
+    for channel in channels:
+        mapping[channel["name"]] = channel["id"]
+    return mapping
