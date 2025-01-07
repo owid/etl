fec4ead417c63436d28b9b52862126c724ffb59c
lucasrodes <lucasrodes@users.noreply.github.com>
2025-01-07 01:48:09 +0100

    reflect new names

--- apps/housekeeper/utils.py
+++ apps/housekeeper/utils.py
@@ -6,12 +6,12 @@ from etl.grapher import model as gm
 
 def get_reviews_id(object_type: str):
     with Session(OWID_ENV.engine) as session:
-        return gm.HousekeepingSuggestedReview.load_reviews_object_id(session, object_type=object_type)
+        return gm.HousekeeperReview.load_reviews_object_id(session, object_type=object_type)
 
 
 def add_reviews(object_type: str, object_id: int):
     with Session(OWID_ENV.engine) as session:
-        gm.HousekeepingSuggestedReview.add_review(
+        gm.HousekeeperReview.add_review(
             session=session,
             object_type=object_type,
             object_id=object_id,
