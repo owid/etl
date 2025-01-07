2bf9dfe53358c94e9fb64e9caf4ce3b2f8ce7331
lucasrodes <lucasrodes@users.noreply.github.com>
2025-01-07 02:47:51 +0100

    wip

--- apps/housekeeper/cli.py
+++ apps/housekeeper/cli.py
@@ -1,5 +1,7 @@
 """Keep things in OWID catalog clean by regularly checking and reviewing content."""
 
+from typing import Optional
+
 import click
 from rich_click import RichCommand
 
@@ -18,12 +20,24 @@ ICON_EMOJI = "sus-blue"
 
 
 @click.command("housekeeper", cls=RichCommand, help=__doc__)
-@click.option("--review-type", "-t", type=click.Choice(REVIEW_TYPES, case_sensitive=False))
-def main(review_type: str):
+@click.option(
+    "--review-type",
+    "-t",
+    type=click.Choice(REVIEW_TYPES, case_sensitive=False),
+    help="Type of the review",
+)
+@click.option(
+    "--channel",
+    "-c",
+    type=str,
+    help=f"Name of the slack channel to send the message two. If None, {CHANNEL_NAME} will be used",
+)
+def main(review_type: str, channel: Optional[str] = None):
+    channel_name = CHANNEL_NAME if channel is None else channel
     # Review charts
     if review_type == "chart":
         send_slack_chart_review(
-            channel_name=CHANNEL_NAME,
+            channel_name=channel_name,
             slack_username=SLACK_USERNAME,
             icon_emoji=ICON_EMOJI,
         )
