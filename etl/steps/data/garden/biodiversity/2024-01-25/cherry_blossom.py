1054fa9f908e9d5167942d44a485b54e3dbf5d50
Marigold <mojmir.vinkler@gmail.com>
2025-01-13 10:31:14 +0100

    wip

--- etl/steps/data/garden/biodiversity/2024-01-25/cherry_blossom.py
+++ etl/steps/data/garden/biodiversity/2024-01-25/cherry_blossom.py
@@ -19,6 +19,8 @@ def run(dest_dir: str) -> None:
     # Calculate a 20,40 and 50 year average
     tb = calculate_multiple_year_average(tb)
 
+    tb["full_flowering_date"] += 10
+
     #
     # Save outputs.
     #
