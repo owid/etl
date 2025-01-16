8d697ec1190bb3bb356898c6b3aa12de3ac9e596
lucasrodes <lucasrodes@users.noreply.github.com>
2025-01-16 22:01:03 +0100

    custom spinner message

--- apps/wizard/app_pages/producer_analytics/producer.py
+++ apps/wizard/app_pages/producer_analytics/producer.py
@@ -5,6 +5,7 @@ import streamlit as st
 from st_aggrid import AgGrid, GridUpdateMode
 
 from apps.wizard.app_pages.producer_analytics.utils import columns_producer, make_grid
+from apps.wizard.utils.components import st_cache_data
 
 
 class UIProducerAnalytics:
@@ -59,31 +60,29 @@ class UIProducerAnalytics:
         return df["producer"].tolist()
 
 
-@st.cache_data(show_spinner=False)
+@st_cache_data(
+    custom_text="Loading producer data from various sources (Big Query, MySQL, etc.) This can take few seconds..."
+)
 def _process_df(df):
-    def _process(df):
-        # Group by producer and get the full list of chart slugs for each producer.
-        df = df.groupby("producer", observed=True, as_index=False).agg(
-            {
-                "chart_url": lambda x: [item for item in x if pd.notna(item)],  # Filter out NaN values
-                "views_365d": "sum",
-                "views_30d": "sum",
-                "views_custom": "sum",
-            }
-        )
-        df["n_charts"] = df["chart_url"].apply(len)
+    # Group by producer and get the full list of chart slugs for each producer.
+    df = df.groupby("producer", observed=True, as_index=False).agg(
+        {
+            "chart_url": lambda x: [item for item in x if pd.notna(item)],  # Filter out NaN values
+            "views_365d": "sum",
+            "views_30d": "sum",
+            "views_custom": "sum",
+        }
+    )
+    df["n_charts"] = df["chart_url"].apply(len)
 
-        # Check if lists are unique. If not, make them unique in the previous line.
-        error = "Duplicated chart slugs found for a given producer."
-        assert df["chart_url"].apply(lambda x: len(x) == len(set(x))).all(), error
+    # Check if lists are unique. If not, make them unique in the previous line.
+    error = "Duplicated chart slugs found for a given producer."
+    assert df["chart_url"].apply(lambda x: len(x) == len(set(x))).all(), error
 
-        # Drop unnecessary columns.
-        df = df.drop(columns=["chart_url"])
+    # Drop unnecessary columns.
+    df = df.drop(columns=["chart_url"])
 
-        # Sort conveniently.
-        df = df.sort_values(["views_custom"], ascending=False).reset_index(drop=True)
+    # Sort conveniently.
+    df = df.sort_values(["views_custom"], ascending=False).reset_index(drop=True)
 
-        return df
-
-    with st.spinner("Loading producer data from various sources (Big Query, MySQL, etc.) This can take few seconds..."):
-        return _process(df)
+    return df
