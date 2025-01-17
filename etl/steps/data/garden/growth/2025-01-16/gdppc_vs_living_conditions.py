ecd0ced5a411627262f33985b47d9e5511e9fdb7
Pablo Arriagada <paarriagadap@gmail.com>
2025-01-17 12:30:47 +0000

    :construction: filter the right indicators

--- etl/steps/data/garden/growth/2025-01-16/gdppc_vs_living_conditions.py
+++ etl/steps/data/garden/growth/2025-01-16/gdppc_vs_living_conditions.py
@@ -6,33 +6,6 @@ from etl.helpers import PathFinder, create_dataset
 # Get paths and naming conventions for current step.
 paths = PathFinder(__file__)
 
-# Define dependencies, tables and indicators to use
-DEPENDENCIES_TABLES_INDICATORS = {
-    "wdi": {"table": ["wdi"], "indicators": ["ny_gdp_pcap_pp_kd", "sh_med_phys_zs", "eg_elc_accs_zs"]},
-    "un_wpp": {"table": ["life_expectancy"], "indicators": ["life_expectancy__sex_all__age_0__variant_estimates"]},
-    "igme": {
-        "table": ["igme"],
-        "indicators": [
-            "obs_value__indicator_under_five_mortality_rate__sex_total__wealth_quintile_total__unit_of_measure_deaths_per_100_live_births"
-        ],
-    },
-    "mortality_database": {
-        "table": ["mortality_database"],
-        "indicators": [
-            "age_standardized_death_rate_per_100_000_standard_population__sex_both_sexes__age_group_all_ages__cause_maternal_conditions__icd10_codes_o00_o99"
-        ],
-    },
-    "who": {"table": ["who"], "indicators": ["wat_imp__residence_total"]},
-    "unwto": {"table": ["unwto"], "indicators": ["out_tour_departures_ovn_vis_tourists_per_1000"]},
-    "penn_world_table": {"table": ["penn_world_table"], "indicators": ["avh"]},
-    "edstats": {"table": ["edstats"], "indicators": ["learning_adjusted_years_of_school", "harmonized_test_scores"]},
-    "education_sdgs": {
-        "table": ["education_sdgs"],
-        "indicators": ["adult_literacy_rate__population_15plus_years__both_sexes__pct__lr_ag15t99"],
-    },
-    "happiness": {"table": ["happiness"], "indicators": ["cantril_ladder_score"]},
-}
-
 
 def run(dest_dir: str) -> None:
     #
@@ -66,32 +39,68 @@ def run(dest_dir: str) -> None:
     #
     # Process data.
     #
-    # Select only the necessary columns from the tables.
-    tb_wdi = tb_wdi[["country", "year", "ny_gdp_pcap_pp_kd", "sh_med_phys_zs", "eg_elc_accs_zs"]]
-    print(tb_un_wpp)
-    tb_un_wpp = tb_un_wpp[["country", "year", "life_expectancy__sex_all__age_0__variant_estimates"]]
+    # Select only the necessary columns and dimensions from the tables.
+    # WDI
+    tb_wdi = tb_wdi[["country", "year", "ny_gdp_pcap_pp_kd", "sh_med_phys_zs", "eg_elc_accs_zs"]].rename(
+        columns={
+            "ny_gdp_pcap_pp_kd": "gdp_per_capita",
+            "sh_med_phys_zs": "physicians_per_1000_people",
+            "eg_elc_accs_zs": "access_to_electricity",
+        }
+    )
+
+    # UN WPP
+    tb_un_wpp = tb_un_wpp[
+        (tb_un_wpp["sex"] == "all") & (tb_un_wpp["age"] == "0") & (tb_un_wpp["variant"] == "estimates")
+    ].reset_index(drop=True)
+    tb_un_wpp = tb_un_wpp[["country", "year", "life_expectancy"]]
+
+    # IGME
     tb_igme = tb_igme[
-        [
-            "country",
-            "year",
-            "obs_value__indicator_under_five_mortality_rate__sex_total__wealth_quintile_total__unit_of_measure_deaths_per_100_live_births",
-        ]
-    ]
+        (tb_igme["obs_value"] == "Under five mortality rate")
+        & (tb_igme["sex"] == "Total")
+        & (tb_igme["wealth_quintile"] == "Total")
+        & (tb_igme["unit_of_measure"] == "Deaths per 100 live births")
+    ].reset_index(drop=True)
+    tb_igme = tb_igme[["country", "year", "obs_value"]].rename(columns={"obs_value": "child_mortality_rate"})
+
+    # Mortality Database
     tb_mortality = tb_mortality[
-        [
-            "country",
-            "year",
-            "age_standardized_death_rate_per_100_000_standard_population__sex_both_sexes__age_group_all_ages__cause_maternal_conditions__icd10_codes_o00_o99",
-        ]
-    ]
-    tb_wash = tb_wash[["country", "year", "wat_imp__residence_total"]]
-    tb_unwto = tb_unwto[["country", "year", "out_tour_departures_ovn_vis_tourists_per_1000"]]
-    tb_pwt = tb_pwt[["country", "year", "avh"]]
+        (tb_mortality["sex"] == "Both sexes")
+        & (tb_mortality["age_group"] == "all ages")
+        & (tb_mortality["cause"] == "Maternal conditions")
+        & (tb_mortality["icd10_codes"] == "O00-O99")
+    ].reset_index(drop=True)
+    tb_mortality = tb_mortality[
+        ["country", "year", "age_standardized_death_rate_per_100_000_standard_population"]
+    ].rename(columns={"age_standardized_death_rate_per_100_000_standard_population": "maternal_death_rate"})
+
+    # WHO
+    tb_wash = tb_wash[tb_wash["residence"] == "Total"].reset_index(drop=True)
+    tb_wash = tb_wash[["country", "year", "wat_imp"]].rename(columns={"wat_imp": "access_to_improve_drinking_water"})
+
+    # UNWTO
+    tb_unwto = tb_unwto[["country", "year", "out_tour_departures_ovn_vis_tourists_per_1000"]].rename(
+        columns={"out_tour_departures_ovn_vis_tourists_per_1000": "tourist_departures_per_1000_people"}
+    )
+
+    # Penn World Table
+    tb_pwt = tb_pwt[["country", "year", "avh"]].rename(columns={"avh": "average_working_hours"})
+
+    # Edstats
     tb_edstats = tb_edstats[["country", "year", "learning_adjusted_years_of_school", "harmonized_test_scores"]]
+
+    # UNESCO
     tb_unesco = tb_unesco[
         ["country", "year", "adult_literacy_rate__population_15plus_years__both_sexes__pct__lr_ag15t99"]
-    ]
-    tb_happiness = tb_happiness[["country", "year", "cantril_ladder_score"]]
+    ].rename(
+        columns={"adult_literacy_rate__population_15plus_years__both_sexes__pct__lr_ag15t99": "adult_literacy_rate"}
+    )
+
+    # Happiness
+    tb_happiness = tb_happiness[["country", "year", "cantril_ladder_score"]].rename(
+        columns={"cantril_ladder_score": "happiness_score"}
+    )
 
     tb = geo.harmonize_countries(
         df=tb, countries_file=paths.country_mapping_path, excluded_countries_file=paths.excluded_countries_path
