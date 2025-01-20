bdb9edd5be3da00c65d5fe9a5530d916f1ed802c
lucasrodes <lucasrodes@users.noreply.github.com>
2025-01-20 15:47:59 +0100

    ✨ Enhance instruction rendering for ETL steps

--- apps/wizard/etl_steps/instructions.py
+++ apps/wizard/etl_steps/instructions.py
@@ -4,103 +4,119 @@ from apps.wizard.app_pages.harmonizer.utils import render as render_harmonizer
 from apps.wizard.etl_steps.utils import STEP_NAME_PRESENT
 
 
-@st.dialog("Harmonize country names", width="large")
-def render_harm(form):
-    meadow_step = form.step_uri("meadow")
-    render_harmonizer(meadow_step)
+def render_instructions(form=None, key: str = ""):
+    Instructions(form, key).render()
 
 
-def render_instructions(form):
-    for channel in ["meadow", "garden", "grapher"]:
-        if channel in form.steps_to_create:
-            with st.container(border=True):
-                st.markdown(f"##### **{STEP_NAME_PRESENT.get(channel, channel)}**")
-                render_instructions_step(channel, form)
+class Instructions:
+    def __init__(self, form=None, key=""):
+        self.form = form
+        self.key = key
 
+    def render(self):
+        for channel in ["meadow", "garden", "grapher"]:
+            if (self.form is None) or (channel in self.form.steps_to_create):
+                with st.container(border=True):
+                    st.markdown(f"##### **{STEP_NAME_PRESENT.get(channel, channel)}**")
+                    if channel == "meadow":
+                        self.render_instructions_meadow()
+                    elif channel == "garden":
+                        self.render_instructions_garden()
+                    elif channel == "grapher":
+                        self.render_instructions_grapher()
 
-def render_instructions_step(channel, form=None):
-    if channel == "meadow":
-        render_instructions_meadow(form)
-    elif channel == "garden":
-        render_instructions_garden(form)
-    elif channel == "grapher":
-        render_instructions_grapher(form)
+    def render_instructions_meadow(self):
+        ## Run step
+        st.markdown("**1) Run Meadow step**")
+        if self.form is None:
+            st.code(
+                "uv run etl run data://meadow/namespace/version/short_name",
+                language="shellSession",
+                wrap_lines=True,
+                line_numbers=True,
+            )
+            st.markdown("Use `--private` if the dataset is private.")
+        else:
+            st.code(
+                f"uv run etl run {self.form.meadow_step_uri} {'--private' if self.form.is_private else ''}",
+                language="shellSession",
+                wrap_lines=True,
+                line_numbers=True,
+            )
 
+    def render_instructions_garden(self):
+        ## 1/ Run etl step
+        st.markdown("**1) Harmonize country names**")
+        self._render_harmonizer_btn()
 
-def render_instructions_meadow(form=None):
-    ## Run step
-    st.markdown("**1) Run Meadow step**")
-    if form is None:
-        st.code(
-            "uv run etl run data://meadow/namespace/version/short_name",
-            language="shellSession",
-            wrap_lines=True,
-            line_numbers=True,
+        st.markdown("You can also run it in your terminal:")
+        if self.form is None:
+            st.code(
+                "uv run etl harmonize data/meadow/version/short_name/table_name.feather country etl/steps/data/garden/version/short_name.countries.json",
+                "shellSession",
+                wrap_lines=True,
+                line_numbers=True,
+            )
+        else:
+            st.code(
+                f"uv run etl harmonize data/meadow/{self.form.base_step_name}/{self.form.short_name}.feather country etl/steps/data/garden/{self.form.base_step_name}.countries.json",
+                "shellSession",
+                wrap_lines=True,
+                line_numbers=True,
+            )
+        st.markdown("**2) Run Garden step**")
+        st.markdown("After editing the code of your Garden step, run the following command:")
+        if self.form is None:
+            st.code(
+                "uv run etl run data://garden/namespace/version/short_name",
+                "shellSession",
+                wrap_lines=True,
+                line_numbers=True,
+            )
+            st.markdown("Use `--private` if the dataset is private.")
+        else:
+            st.code(
+                f"uv run etl run {self.form.garden_step_uri} {'--private' if self.form.is_private else ''}",
+                "shellSession",
+                wrap_lines=True,
+                line_numbers=True,
+            )
+
+    def render_instructions_grapher(self):
+        st.markdown("**1) Run Grapher step**")
+        if self.form is None:
+            st.code(
+                "uv run etl run data://meadow/namespace/version/short_name",
+                language="shellSession",
+                wrap_lines=True,
+                line_numbers=True,
+            )
+            st.markdown("Use `--private` if the dataset is private.")
+        else:
+            st.code(
+                f"uv run etl run {self.form.grapher_step_uri} {'--private' if self.form.is_private else ''}",
+                language="shellSession",
+                wrap_lines=True,
+                line_numbers=True,
+            )
+        st.markdown("**2) Pull request**")
+        st.markdown("Create a pull request in [ETL](https://github.com/owid/etl), get it reviewed and merged.")
+
+    @st.fragment
+    def _render_harmonizer_btn(self):
+        key = "data_step_harmonize"
+        if self.key != "":
+            key = f"{key}_{self.key}"
+        st.button(
+            "Harmonize",
+            on_click=self._render_harmonizer_dialog,
+            key=key,
         )
-        st.markdown("Use `--private` if the dataset is private.")
-    else:
-        st.code(
-            f"uv run etl run {form.meadow_step_uri} {'--private' if form.is_private else ''}",
-            language="shellSession",
-            wrap_lines=True,
-            line_numbers=True,
-        )
 
-
-def render_instructions_garden(form=None):
-    ## 1/ Run etl step
-    st.markdown("**1) Harmonize country names**")
-    st.button("Harmonize", on_click=lambda form=form: render_harm(form))
-    st.markdown("You can also run it in your terminal:")
-    if form is None:
-        st.code(
-            "uv run etl harmonize data/meadow/version/short_name/table_name.feather country etl/steps/data/garden/version/short_name.countries.json",
-            "shellSession",
-            wrap_lines=True,
-            line_numbers=True,
-        )
-    else:
-        st.code(
-            f"uv run etl harmonize data/meadow/{form.base_step_name}/{form.short_name}.feather country etl/steps/data/garden/{form.base_step_name}.countries.json",
-            "shellSession",
-            wrap_lines=True,
-            line_numbers=True,
-        )
-    st.markdown("**2) Run Garden step**")
-    st.markdown("After editing the code of your Garden step, run the following command:")
-    if form is None:
-        st.code(
-            "uv run etl run data://garden/namespace/version/short_name",
-            "shellSession",
-            wrap_lines=True,
-            line_numbers=True,
-        )
-        st.markdown("Use `--private` if the dataset is private.")
-    else:
-        st.code(
-            f"uv run etl run {form.garden_step_uri} {'--private' if form.is_private else ''}",
-            "shellSession",
-            wrap_lines=True,
-            line_numbers=True,
-        )
-
-
-def render_instructions_grapher(form=None):
-    st.markdown("**1) Run Grapher step**")
-    if form is None:
-        st.code(
-            "uv run etl run data://meadow/namespace/version/short_name",
-            language="shellSession",
-            wrap_lines=True,
-            line_numbers=True,
-        )
-        st.markdown("Use `--private` if the dataset is private.")
-    else:
-        st.code(
-            f"uv run etl run {form.grapher_step_uri} {'--private' if form.is_private else ''}",
-            language="shellSession",
-            wrap_lines=True,
-            line_numbers=True,
-        )
-    st.markdown("**2) Pull request**")
-    st.markdown("Create a pull request in [ETL](https://github.com/owid/etl), get it reviewed and merged.")
+    @st.dialog("Harmonize country names", width="large")
+    def _render_harmonizer_dialog(self):
+        if self.form is None:
+            meadow_step = None
+        else:
+            meadow_step = self.form.step_uri("meadow")
+        render_harmonizer(None)
