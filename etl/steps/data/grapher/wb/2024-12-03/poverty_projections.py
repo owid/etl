66a27165f5243fbcb49bcbb094fa7c18fd63cb1e
Pablo Arriagada <paarriagadap@gmail.com>
2025-01-20 10:04:03 +0000

    :bug: update grapher step in old version

--- etl/steps/data/grapher/wb/2024-12-03/poverty_projections.py
+++ etl/steps/data/grapher/wb/2024-12-03/poverty_projections.py
@@ -5,6 +5,9 @@ from etl.helpers import PathFinder, create_dataset
 # Get paths and naming conventions for current step.
 paths = PathFinder(__file__)
 
+# Define index columns
+INDEX_COLUMNS = ["country", "year", "povertyline", "scenario"]
+
 
 def run(dest_dir: str) -> None:
     #
@@ -14,12 +17,17 @@ def run(dest_dir: str) -> None:
     ds_garden = paths.load_dataset("poverty_projections")
 
     # Read table from garden dataset.
-    tb = ds_garden.read("poverty_projections", reset_index=False)
+    tb = ds_garden.read("poverty_projections")
+
+    # Round povertyline to 2 decimal places
+    tb["povertyline"] = tb["povertyline"].round(2)
+
+    tb = tb.format(INDEX_COLUMNS)
 
     #
     # Save outputs.
     #
-    # Create a new grapher dataset with the same metadata as the garden dataset.
+    # Create a new grapher dataset with the same metadata as the garden dataset..
     ds_grapher = create_dataset(
         dest_dir, tables=[tb], check_variables_metadata=True, default_metadata=ds_garden.metadata
     )
