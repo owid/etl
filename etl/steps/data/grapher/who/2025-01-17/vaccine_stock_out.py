b3e1802773011e9cc66ca1860865f243b2f3dc02
spoonerf <spoonerfiona@gmail.com>
2025-01-20 14:39:41 +0000

    add origins in

--- etl/steps/data/grapher/who/2025-01-17/vaccine_stock_out.py
+++ etl/steps/data/grapher/who/2025-01-17/vaccine_stock_out.py
@@ -17,13 +17,17 @@ def run(dest_dir: str) -> None:
     # tb = ds_garden.read("vaccine_stock_out", reset_index=False)
     tb_agg = ds_garden.read("derived_metrics", reset_index=False)
     tb_cause = ds_garden.read("reason_for_stockout", reset_index=False)
+    tb_global = ds_garden.read("global_stockout", reset_index=False)
 
     #
     # Save outputs.
     #
     # Create a new grapher dataset with the same metadata as the garden dataset.
     ds_grapher = create_dataset(
-        dest_dir, tables=[tb_agg, tb_cause], check_variables_metadata=True, default_metadata=ds_garden.metadata
+        dest_dir,
+        tables=[tb_agg, tb_cause, tb_global],
+        check_variables_metadata=True,
+        default_metadata=ds_garden.metadata,
     )
 
     # Save changes in the new grapher dataset.
