e8bb13b341cc3082a169ae0ad5bd3c17c7e4725e
spoonerf <spoonerfiona@gmail.com>
2025-01-20 12:24:27 +0000

    add district level stock out

--- etl/steps/data/garden/who/2025-01-17/vaccine_stock_out.py
+++ etl/steps/data/garden/who/2025-01-17/vaccine_stock_out.py
@@ -27,6 +27,7 @@ def run(dest_dir: str) -> None:
     tb = tb.drop(columns=["iso_3_code", "who_region", "indcode", "indcatcode", "indcat_description", "indsort"])
     tb = tb.replace({"value": {"ND": pd.NA, "NR": pd.NA}})
     tb = national_stockout_for_any_vaccine(tb)
+    tb = district_level_stockout_for_any_vaccine(tb)
     tb = tb.format(["country", "year", "description"])
     #
     # Save outputs.
@@ -59,6 +60,30 @@ def national_stockout_for_any_vaccine(tb: Table) -> Table:
         .groupby(["country", "year"], as_index=False)["is_yes"]
         .any()
         .assign(any_national_vaccine_stockout=lambda df: df["is_yes"].map({True: "Yes", False: "No"}))
+        .drop(columns=["is_yes"])
+    )
+
+    tb = tb.merge(tb_agg, on=["country", "year"], how="left")
+
+    return tb
+
+
+def district_level_stockout_for_any_vaccine(tb: Table) -> Table:
+    """
+    Was there a vaccine stockout at the district-level of any vaccine in the country in the given year?
+    """
+    # Get the rows relevant to district level vaccine stockouts (i.e. not national, or stockouts of syringes)
+
+    tb_agg = tb[
+        tb["description"].str.contains("Was there a vaccine stock-out at the district level")
+        & (tb["description"].str.contains("vaccine"))
+    ]
+    tb_agg = tb_agg.dropna(subset=["value"])
+    tb_agg = (
+        tb_agg.assign(is_yes=tb_agg["value"].eq("Yes"))
+        .groupby(["country", "year"], as_index=False)["is_yes"]
+        .any()
+        .assign(any_district_level_vaccine_stockout=lambda df: df["is_yes"].map({True: "Yes", False: "No"}))
         .drop(columns=["is_yes"])
     )
 
