5875448368aa06ab4c0ad7083936d035fd79b32a
spoonerf <spoonerfiona@gmail.com>
2025-01-21 17:42:51 +0000

    global cause

--- etl/steps/data/grapher/who/2025-01-17/vaccine_stock_out.py
+++ etl/steps/data/grapher/who/2025-01-17/vaccine_stock_out.py
@@ -18,6 +18,7 @@ def run(dest_dir: str) -> None:
     tb_agg = ds_garden.read("derived_metrics", reset_index=False)
     tb_cause = ds_garden.read("reason_for_stockout", reset_index=False)
     tb_global = ds_garden.read("global_stockout", reset_index=False)
+    tb_global_cause = ds_garden.read("global_cause", reset_index=False)
 
     #
     # Save outputs.
@@ -25,7 +26,7 @@ def run(dest_dir: str) -> None:
     # Create a new grapher dataset with the same metadata as the garden dataset.
     ds_grapher = create_dataset(
         dest_dir,
-        tables=[tb, tb_agg, tb_cause, tb_global],
+        tables=[tb, tb_agg, tb_cause, tb_global, tb_global_cause],
         check_variables_metadata=True,
         default_metadata=ds_garden.metadata,
     )
