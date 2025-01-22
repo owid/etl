ffaee982e02584c7e623a70c3b1c5dc0ee814e75
veronikasamborska1994 <32176660+veronikasamborska1994@users.noreply.github.com>
2025-01-22 10:50:25 +0000

    marriages&divorces: historical US rates

---
+++ etl/steps/data/garden/us_census_bureau/2025-01-22/marriages_divorces.py
@@ -0,0 +1,30 @@
+"""Load a meadow dataset and create a garden dataset."""
+
+from etl.helpers import PathFinder, create_dataset
+
+# Get paths and naming conventions for current step.
+paths = PathFinder(__file__)
+
+
+def run(dest_dir: str) -> None:
+    #
+    # Load inputs.
+    #
+    # Load meadow dataset.
+    ds_meadow = paths.load_dataset("marriages_divorces")
+
+    # Read table from meadow dataset.
+    tb = ds_meadow.read("marriages_divorces")
+
+    tb = tb.format(["country", "year"])
+
+    #
+    # Save outputs.
+    #
+    # Create a new garden dataset with the same metadata as the meadow dataset.
+    ds_garden = create_dataset(
+        dest_dir, tables=[tb], check_variables_metadata=True, default_metadata=ds_meadow.metadata
+    )
+
+    # Save changes in the new garden dataset.
+    ds_garden.save()
