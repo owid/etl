4b403b73c161338ecf503f08874f2276db369214
lucasrodes <lucasrodes@users.noreply.github.com>
2025-01-23 00:31:45 +0100

    📊 Update UNAIDS data files for 2024 report

--- snapshots/health/2025-01-22/unaids.py
+++ snapshots/health/2025-01-22/unaids.py
@@ -5,12 +5,30 @@ In particular, we follow these steps:
 - Get the file with all API links: "UNAIDS_API_links.csv"
     - This file was obtained by directly reaching the UNAIDS via mail.
     - This file contains the list of all UNAIDS indicators, along with their respective API links to access their values.
-- Select those indicators of interest
-    - Together with a researcher, we selected the indicators of interest.
-- Access the data, combine them into a CSV and store it.
-    - For that, we can use the function `get_all_data_from_api` from this module.
+- Access each indicator and save the data.
+
+NOTE:
+
+1) There are four main categories in the data:
+    EPI -- Epidemic indicators (https://aidsinfo.unaids.org/):
+    These indicators provide data on the status and trends of the HIV epidemic within populations. They include metrics such as HIV prevalence, incidence rates, and AIDS-related mortality. Epidemiological indicators are essential for understanding the scope and dynamics of the epidemic, informing public health strategies, and allocating resources effectively.
+
+    GAM -- Global AIDS Monitoring:
+    This framework encompasses a comprehensive set of indicators designed to assess progress towards achieving global HIV targets, as outlined in political declarations and strategic plans. The GAM indicators cover various aspects of the HIV response, including prevention, treatment, and support services. Countries report on these indicators annually to UNAIDS, facilitating global monitoring and accountability.
+
+
+    KPA -- Key Population Atlas:
+    This category focuses on indicators related to key populations that are at higher risk of HIV infection, such as sex workers, men who have sex with men, people who inject drugs, transgender individuals, and prisoners. The Key Population Atlas provides data on the size, prevalence, and service coverage for these groups, supporting targeted interventions and resource allocation to address their specific needs.
+
+    NCPI -- National Commitments and Policy Instrument:
+    The NCPI is a tool used to evaluate the policy, legal, and programmatic environment of national HIV responses. It includes questions related to the implementation of laws, policies, and strategies that affect the effectiveness of HIV interventions. The NCPI helps identify structural barriers and enablers within countries, guiding improvements in the national response to HIV.
+
+2) We import all data, we will filter in later stages.
+
+3) Importing all indicators, in parallel, takes ~3 minutes on M1 MAX PRO.
 """
 
+from concurrent.futures import ThreadPoolExecutor
 from pathlib import Path
 
 import click
@@ -25,33 +43,6 @@ SNAPSHOT_VERSION = Path(__file__).parent.name
 # Logger
 log = get_logger()
 
-# Relevant indicators in UNAIDS data
-## Key values correspond to sheet tab names in the API links file. Values correspond to indicator names within each sheet.
-FIELDS_RELEVANT = {
-    "EPI": [
-        "HIV Prevalence",
-        "Deaths averted due to ART",
-        "AIDS-related deaths",
-        "HIV Incidence per 1000 population",
-        "New HIV Infections averted due to PMTCT",
-        "New HIV Infections",
-        "AIDS Orphans",
-        "Gap to reaching the target percent of people receiving ART",
-    ],
-    "GAM": [
-        "Country-reported HIV expenditure by funding source",
-        "Condom use at last high-risk sex",
-        "Condom use among men who have sex with men",
-        "Discriminatory attitudes towards people living with HIV",
-        "Knowledge about HIV prevention in young people",
-        "Estimated TB-related deaths among people living with HIV",
-        "TB patients living with HIV receiving ART",
-        "TB patients tested positive for HIV",
-        "Estimated HIV resource availability for low- and middle-income countries in constant 2019 USD",
-        "Estimated resource needs",
-    ],
-}
-
 
 @click.command()
 @click.option(
@@ -60,47 +51,70 @@ FIELDS_RELEVANT = {
     type=bool,
     help="Upload dataset to Snapshot",
 )
-@click.option("--path-to-file", prompt=True, type=str, help="Path to local file with the API links.")
+@click.option("--path-to-file", "-f", prompt=True, type=str, help="Path to local file with the API links.")
 def main(path_to_file: str, upload: bool) -> None:
-    # Create a new snapshot.
-    snap = Snapshot(f"health/{SNAPSHOT_VERSION}/unaids.csv")
+    categories = [
+        "epi",
+        "gam",
+        "kpa",
+        "ncpi",
+    ]
+    for category in categories:
+        # log.info(" =================================================================================== ")
+        # log.info(f" ======================== health.unaids: {category.upper()} ======================== ")
+        # log.info(" =================================================================================== ")
 
-    # Ensure destination folder exists.
-    snap.path.parent.mkdir(exist_ok=True, parents=True)
+        # Create a new snapshot.
+        snap = Snapshot(f"health/{SNAPSHOT_VERSION}/unaids_{category}.csv")
 
-    # Get data
-    df = get_all_data_from_api(path_to_file)
+        # Get data
+        df = get_all_data_from_api(path_to_file, sheet_name=category.upper())
 
-    # Manually upload df to S3
-    snap.create_snapshot(data=df, upload=upload)
+        # Manually upload df to S3
+        snap.create_snapshot(data=df, upload=upload)
 
 
-def get_all_data_from_api(path: str) -> pd.DataFrame:
+def get_all_data_from_api(path: str, sheet_name: str) -> pd.DataFrame:
     """Download relevant data from UNAIDS API."""
 
     # Load all api links
-    df_links = pd.read_excel(path, sheet_name=None)
+    df_links = pd.read_excel(path, sheet_name=sheet_name)
 
-    # Load relevant fields, and concatenate
-    dfs = []
-    for tab_name, fields in FIELDS_RELEVANT.items():
-        df_tab = df_links[tab_name]
-        df_tab = df_tab[df_tab["Indicator_Name"].isin(fields)]
-        dfs.append(df_tab)
-    df_links = pd.concat(dfs, ignore_index=True)
-
-    # Get data
+    # Get data from all links
+    links = df_links["API"].values
     data = []
-    for url in df_links["API"].values:
-        log.info(f"health.unaids: downloading from {url}")
-        data_ = requests.get(url).json()
-        # df_ = pd.DataFrame.from_records(data["Data"][0]["Observation"])
-        data.extend(data_["Data"][0]["Observation"])
 
+    with ThreadPoolExecutor() as executor:
+        results = list(executor.map(api_query, links))
+
+    for result in results:
+        if result is not None:
+            data.extend(result)
+
+    # Combine all records into single dataframe
     df = pd.DataFrame.from_records(data)
-    # df = pd.concat(dfs, ignore_index=True)  # type: ignore
 
     return df
+
+
+def api_query(link):
+    """Query UNAIDS API and return the data."""
+    log.info(f"health.unaids: downloading from {link}")
+
+    # Query API
+    response = requests.get(link)
+
+    # Sanity check and return data if available, otherwise return None
+    if response.status_code == 200:
+        data_ = response.json()
+        if "Data" in data_ and len(data_["Data"]) > 0 and "Observation" in data_["Data"][0]:
+            return data_["Data"][0]["Observation"]
+        else:
+            log.error(f"health.unaids: unexpected data format in response from {link}")
+    else:
+        log.error(f"health.unaids: failed to download from {link}, status code: {response.status_code}")
+
+    return
 
 
 def safe_request():
@@ -127,22 +141,3 @@ def safe_request():
 
 if __name__ == "__main__":
     main()
-
-# df_ = pd.concat(objs=[d for d in df_links.values()])
-# link = df_.loc[df_.Indicator_GId == "MSM_CONDOM_USE", "API"].values
-# data = requests.get(link[0]).json()
-# df = pd.DataFrame.from_records(data["Data"][0]["Observation"])
-
-
-# df_ = pd.concat(objs=[d for d in df_links.values()])
-# links = df_.loc[df_.Indicator_GId == "MSM_CONDOM_USE", "API"].values
-# subgroups = df_.loc[df_.Indicator_GId == "MSM_CONDOM_USE", "Subgroup_Val_GId"].values
-# for link, subgroup in zip(links, subgroups):
-#     data = requests.get(link).json()
-#     df = pd.DataFrame.from_records(data["Data"][0]["Observation"])
-#     print(subgroup)
-#     if df.empty:
-#         print("!! ERROR !!")
-#     else:
-#         print(df.TIME_PERIOD.min())
-#     print("------------")
