bf8bb03eb73923d65e17b02b5a8704476fb8ee17
lucasrodes <lucasrodes@users.noreply.github.com>
2025-01-23 20:27:30 +0100

    wip

--- etl/steps/data/grapher/health/2025-01-22/unaids.py
+++ etl/steps/data/grapher/health/2025-01-22/unaids.py
@@ -1,9 +1,5 @@
 """Load a garden dataset and create a grapher dataset."""
 
-from typing import cast
-
-from owid.catalog import Dataset
-
 from etl.helpers import PathFinder, create_dataset, grapher_checks
 
 # Get paths and naming conventions for current step.
@@ -15,20 +11,22 @@ def run(dest_dir: str) -> None:
     # Load inputs.
     #
     # Load garden dataset.
-    ds_garden = cast(Dataset, paths.load_dependency("unaids"))
-
-    # Read table from garden dataset.
-    tb = ds_garden["unaids"]
+    ds_garden = paths.load_dataset("unaids")
 
     #
     # Process data.
     #
+    tables = list(ds_garden)
 
     #
     # Save outputs.
     #
     # Create a new grapher dataset with the same metadata as the garden dataset.
-    ds_grapher = create_dataset(dest_dir, tables=[tb], default_metadata=ds_garden.metadata)
+    ds_grapher = create_dataset(
+        dest_dir,
+        tables=tables,
+        default_metadata=ds_garden.metadata,
+    )
 
     #
     # Checks.
