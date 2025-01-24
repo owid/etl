0e97a530fd70621daeb688560b7e4a7e6d7f0e4e
spoonerf <spoonerfiona@gmail.com>
2025-01-24 11:39:13 +0000

    adding in national values table

--- etl/steps/data/grapher/health/2025-01-22/measles.py
+++ etl/steps/data/grapher/health/2025-01-22/measles.py
@@ -15,13 +15,14 @@ def run(dest_dir: str) -> None:
 
     # Read table from garden dataset.
     tb = ds_garden.read("measles", reset_index=False)
+    tb_usa = ds_garden.read("national_measles", reset_index=False)
 
     #
     # Save outputs.
     #
     # Create a new grapher dataset with the same metadata as the garden dataset.
     ds_grapher = create_dataset(
-        dest_dir, tables=[tb], check_variables_metadata=True, default_metadata=ds_garden.metadata
+        dest_dir, tables=[tb, tb_usa], check_variables_metadata=True, default_metadata=ds_garden.metadata
     )
 
     # Save changes in the new grapher dataset.
