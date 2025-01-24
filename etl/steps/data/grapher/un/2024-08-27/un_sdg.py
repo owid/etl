8b470d18ba997995a917e8342dba7141d94e19c4
Tuna Acisu <tuna.acisu@ourworldindata.com>
2025-01-24 17:50:47 +0000

    :bug: fix excluded countries

--- etl/steps/data/grapher/un/2024-08-27/un_sdg.py
+++ etl/steps/data/grapher/un/2024-08-27/un_sdg.py
@@ -25,7 +25,7 @@ pd.options.mode.chained_assignment = None
 # only include tables containing INCLUDE string, this is useful for debugging
 # but should be None before merging to master!!
 # TODO: set this to None before merging to master
-# INCLUDE = "_6_1_1|_6_2_1"
+#INCLUDE = "_6_1_1|_6_2_1|_16|_2_4"
 INCLUDE = None
 
 
