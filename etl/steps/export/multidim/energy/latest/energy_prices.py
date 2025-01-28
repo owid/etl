192ec028e63692120529a14c7be2677ba625cd77
Pablo Rosado <pabloarosado@gmail.com>
2025-01-28 10:58:22 +0100

    Add stacked bar view to energy prices mdim explorer, and other minor improvements

--- etl/steps/export/multidim/energy/latest/energy_prices.py
+++ etl/steps/export/multidim/energy/latest/energy_prices.py
@@ -41,6 +41,58 @@ def run(dest_dir: str) -> None:
         },
     )
 
+    # Create special view for the stacked area chart of total consumer price by components.
+    table_name = paths.get_dependency_step_name("energy_prices").replace("data://", "")
+    for source in ["electricity", "gas"]:
+        price_components = [
+            # The total price is (to a very good approximation) equivalent to the combination of "Energy and supply", "Network costs", and "Taxes, fees, levies, and charges".
+            # We can either show only those three main components, or split the latter into its subcomponents, namely:
+            # "Capacity taxes", "Environmental taxes", "Nuclear taxes", "Renewable taxes", "Value added tax (VAT)", "Other".
+            # NOTE: For more details, see analysis in the garden gas_and_electricity_prices step).
+            # Three main components:
+            "energy_and_supply",
+            "network_costs",
+            # "taxes_fees_levies_and_charges",
+            # Subcomponents of "taxes_fees_levies_and_charges":
+            "capacity_taxes",
+            "environmental_taxes",
+            # Nuclear taxes is a component of electricity prices only (not gas), so it will be appended only for electricity.
+            # "nuclear_taxes",
+            "renewable_taxes",
+            "value_added_tax_vat",
+            "other",
+        ]
+        if source == "electricity":
+            price_components.append("nuclear_taxes")
+        for consumer in ["household", "non_household"]:
+            for unit in ["euro", "pps"]:
+                subtitle = "Prices are given in euros per [megawatt-hour](#dod:watt-hours)."
+                if unit == "euro":
+                    subtitle += " They are not adjusted for inflation or differences in living costs between countries."
+                config["views"].append(
+                    {
+                        "dimensions": {
+                            "frequency": "annual",
+                            "source": source,
+                            "price_component": "consumer_price_components",
+                            "consumer": consumer,
+                            "unit": unit,
+                        },
+                        "indicators": {
+                            "y": [
+                                f"{table_name}/energy_prices_annual#annual_{source}_{consumer}_{component}_{unit}"
+                                for component in price_components
+                            ],
+                        },
+                        "config": {
+                            "chartTypes": ["StackedBar"],
+                            "tab": "chart",
+                            "title": f"Components of {source} price for {consumer.replace('_', '-')} consumers",
+                            "subtitle": subtitle,
+                        },
+                    },
+                )
+
     #
     # Save outputs.
     #
