29054c797f390c2dde3a1eafa2841c7a90778a28
veronikasamborska1994 <32176660+veronikasamborska1994@users.noreply.github.com>
2025-01-28 11:23:13 +0000

    keep country column for bastian

--- etl/steps/data/grapher/wpf/2025-01-17/famines_by_regime_gdp.py
+++ etl/steps/data/grapher/wpf/2025-01-17/famines_by_regime_gdp.py
@@ -24,8 +24,6 @@ def run(dest_dir: str) -> None:
     famine_names_to_drop = ["China 1958", "China 1959", "China 1960", "China 1961", "China 1962"]
     tb = tb[~tb["famine_name"].isin(famine_names_to_drop)]
 
-    tb = tb.drop(columns={"country"})
-
     tb = tb.rename({"famine_name": "country", "country": "country_name"}, axis=1)
 
     # Keep the first year for each famine (country)
