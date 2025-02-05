a04fd4c88109bbef5867cd40636a419fcfecfa8e
lucasrodes <lucasrodes@users.noreply.github.com>
2025-02-05 19:24:49 +0100

    wip

--- etl/steps/data/grapher/health/2025-01-22/unaids.py
+++ etl/steps/data/grapher/health/2025-01-22/unaids.py
@@ -34,10 +34,15 @@ def run(dest_dir: str) -> None:
     #
     # DEBUG: The following lines provide insights on which indicators are most complete.
     #
+    # import owid.catalog.processing as pr
+    # from etl.config import OWID_ENV
+    # import pandas as pd
+
+    # # Get completeness by indicator-dimension
     # tbs = []
     # for tb in tables:
     #     print(tb.m.short_name)
-    #     uri = tb.m.short_name
+    #     uri =  str(tb.m.short_name)
     #     index_cols = tb.index.names
 
     #     tbx = tb.reset_index()
@@ -49,17 +54,51 @@ def run(dest_dir: str) -> None:
     #     tbx["indicator"] = str(uri) + "#" + tbx["indicator"]
 
     #     tbs.append(tbx)
-    # import owid.catalog.processing as pr
     # tbx = pr.concat(tbs)
     # cols_last = ["value", "country"]
     # tbx = tbx[[col for col in tbx.columns if col not in cols_last] + cols_last]
     # tbx = tbx.sort_values(["value", "country"], ascending=False)
+    # tbx["uri_complete"] = (ds_garden.m.uri + "/" + tbx["indicator"]).str.replace("garden", "grapher")
 
+    # # Get indicators in use
+    # query = """
+    # WITH t1 AS (
+    #     SELECT
+    #         *
+    #     FROM variables
+    #     WHERE catalogPath IS NOT NULL AND variables.datasetId = 6905
+    # ),
+    # t AS (
+    # SELECT
+    #     id,
+    #     CASE
+    #         WHEN catalogPath LIKE '%%__%%' THEN SUBSTRING_INDEX(catalogPath, '__', 1)
+    #         ELSE catalogPath
+    #     END AS uri_complete
+    # FROM t1
+    # )
+    # SELECT
+    #     t.uri_complete,
+    #     count(DISTINCT cd.chartId) num_charts,
+    #     GROUP_CONCAT(DISTINCT cd.chartId ORDER BY cd.chartId ASC) AS chart_ids
+    # FROM chart_dimensions cd
+    #     JOIN t
+    #     ON t.id = cd.variableId
+    #     GROUP BY uri_complete
+    #     ORDER BY num_charts desc;
+    # """
+    # df = OWID_ENV.read_sql(query)
+    # tbx = pd.DataFrame(tbx).merge(df, on="uri_complete", how="left")
+    # tbx["num_charts"] = tbx["num_charts"].fillna(0).astype(int)
+
+    # # Summary by indicator (aggregate over dimensions)
     # tbg = tbx.groupby("indicator", as_index=False).agg({
     #     "value": ["mean", "size"],
     #     "country": "mean",
+    #     "num_charts": "max",
+    #     "chart_ids": "unique",
     # })
-    # tbg.columns = ["indicator", "num_values", "num_dimensions", "num_countries"]
+    # tbg.columns = ["indicator", "num_values", "num_dimensions", "num_countries", "num_charts", "charts"]
     # tbg = tbg.sort_values(["num_values", "num_countries"], ascending=False)
     ##################################################################################################
 
