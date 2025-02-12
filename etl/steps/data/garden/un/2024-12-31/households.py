1224af6b6026bfec5bc3c96be0c22db0d0df344f
Veronika Samborska <veronika@ourworldindata.org>
2025-02-12 09:18:38 +0000

    assign to "unknown" category if values don't add up to 100

--- etl/steps/data/garden/un/2024-12-31/households.py
+++ etl/steps/data/garden/un/2024-12-31/households.py
@@ -27,22 +27,13 @@ def run(dest_dir: str) -> None:
     tb = geo.harmonize_countries(df=tb, countries_file=paths.country_mapping_path)
 
     columns_to_keep = [
-        "average_household_size__number_of_members",
-        "female_head_of_household__percentage_of_households",
         "one_person",
         "couple_only",
         "couple_with_children",
         "single_parent_with_children",
-        "single_mother_with_children",
-        "single_father_with_children",
         "extended_family",
         "non_relatives",
         "unknown",
-        "nuclear",
-        "multi_generation",
-        "three_generation",
-        "skip_generation",
-        "other",
     ]
 
     # Replace ".." with NaN
@@ -76,9 +67,7 @@ def create_other_category(tb: Table) -> Table:
     Returns:
     Table: The updated table with the 'other' category added and cleaned data.
     """
-    # Make s
     # Make sure these columns add up to around 100 for each country and year
-    # Some years don't have all the data so we want to make those NaNs to exclude them.
     columns_to_sum = [
         "unknown",
         "non_relatives",
@@ -89,7 +78,7 @@ def create_other_category(tb: Table) -> Table:
         "one_person",
     ]
 
-    # Convert columns to numeric, forcing errors to NaN
+    # Convert columns to numeric
     tb[columns_to_sum] = tb[columns_to_sum].apply(pd.to_numeric, errors="coerce")
 
     # Calculate the sum of the specified columns
@@ -98,20 +87,22 @@ def create_other_category(tb: Table) -> Table:
     # Check which rows don't add up to around 100 (using a tolerance, e.g., ±0.1)
     tolerance = 0.5
     rows_not_around_100 = tb[(tb["sum_households"] < 100 - tolerance) | (tb["sum_households"] > 100 + tolerance)]
-    # Make the values NaN for the columns in the rows that don't add up to around 100
-    tb.loc[rows_not_around_100.index, columns_to_sum] = np.nan
+
+    # Calculate the difference from 100 for the rows that don't add up to around 100 and set it to the "unknown" column
+    tb.loc[rows_not_around_100.index, "unknown"] = tb.loc[rows_not_around_100.index, "unknown"].fillna(0) + (
+        100 - tb.loc[rows_not_around_100.index, "sum_households"]
+    )
+    # Ensure if the "unknown" column row is 100, then others are 0
+    tb.loc[tb["unknown"] == 100, columns_to_sum] = 0
+    tb.loc[tb["unknown"] == 100, "unknown"] = 100
 
     # Drop the sum_households column if needed
     tb = tb.drop(columns=["sum_households"])
-    # Some years don't have all the data so we want to make those NaNs to exclude them.
-    columns_for_other = [
-        "extended_family",
-        "non_relatives",
-    ]
-    tb["other"] = tb[columns_for_other].sum(axis=1)
-    # Remove rows where "other" == 0 because of the summing of the columns
-    tb = tb[tb["other"] != 0]
-    # Remove rows where "other" < 0
-    tb = tb[tb["other"] >= 0]
+
+    # Ensure other columns are not NaN for rows where "unknown" is not NaN
+    tb.loc[tb["unknown"].notna(), columns_to_sum] = tb.loc[tb["unknown"].notna(), columns_to_sum].fillna(0)
+
+    # If all values are zero for each of the columns in columns_to_sum, set them to NaN (avoid plotting just 0s)
+    tb.loc[(tb[columns_to_sum] == 0).all(axis=1), columns_to_sum] = np.nan
 
     return tb
