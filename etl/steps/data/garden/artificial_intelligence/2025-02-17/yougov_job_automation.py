253bf9585572e080039db79ff4baba98ed84ba0f
Veronika Samborska <veronika@ourworldindata.org>
2025-02-17 10:17:05 +0000

    ai: update surveys and papers with code datasets

---
+++ etl/steps/data/garden/artificial_intelligence/2025-02-17/yougov_job_automation.py
@@ -0,0 +1,41 @@
+"""Load a meadow dataset and create a garden dataset."""
+
+import shared as sh
+from structlog import get_logger
+
+from etl.helpers import PathFinder, create_dataset
+
+log = get_logger()
+
+# Get paths and naming conventions for current step.
+paths = PathFinder(__file__)
+
+
+def run(dest_dir: str) -> None:
+    #
+    # Load inputs.
+    #
+    # Load meadow dataset.
+    ds_meadow = paths.load_dataset("yougov_job_automation")
+
+    # Read table from meadow dataset.
+    tb = ds_meadow.read("yougov_job_automation")
+    #
+    # Process data.
+    #
+    tb = sh.preprocess_data(
+        tb,
+        index_columns=["group", "date"],
+        pivot_column="how_worried__if_it_all__are_you_that_your_type_of_work_could_be_automated_within_your_lifetime",
+        value_column="value",
+    )
+    tb = tb.format(["group", "date"])
+    #
+    # Save outputs.
+    #
+    # Create a new garden dataset with the same metadata as the meadow dataset.
+    ds_garden = create_dataset(
+        dest_dir, tables=[tb], check_variables_metadata=True, default_metadata=ds_meadow.metadata
+    )
+    # Save changes in the new garden dataset.
+    ds_garden.save()
---
+++ etl/steps/data/grapher/artificial_intelligence/2025-02-17/yougov_job_automation.py
@@ -0,0 +1,39 @@
+"""Load a garden dataset and create a grapher dataset."""
+
+from etl.helpers import PathFinder, create_dataset, grapher_checks
+
+# Get paths and naming conventions for current step.
+paths = PathFinder(__file__)
+
+
+def run(dest_dir: str) -> None:
+    #
+    # Load inputs.
+    #
+    # Load garden dataset.
+    ds_garden = paths.load_dataset("yougov_job_automation")
+
+    # Read table from garden dataset.
+    tb = ds_garden.read("yougov_job_automation", reset_index=False)
+
+    #
+    # Process data.
+    #
+    # Rename the 'question' to 'country' for visualising in the grapher
+    tb = tb.rename_index_names({"group": "country"})
+
+    #
+    # Save outputs.
+    #
+    # Create a new grapher dataset with the same metadata as the garden dataset.
+    ds_grapher = create_dataset(
+        dest_dir, tables=[tb], default_metadata=ds_garden.metadata, check_variables_metadata=True
+    )
+
+    #
+    # Checks.
+    #
+    grapher_checks(ds_grapher)
+
+    # Save changes in the new grapher dataset.
+    ds_grapher.save()
