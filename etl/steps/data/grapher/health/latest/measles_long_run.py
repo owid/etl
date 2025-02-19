865290903e571e3db8a35a8d19db802be6ed480e
spoonerf <spoonerfiona@gmail.com>
2025-02-19 15:17:43 +0000

    add auto update desc short

--- etl/steps/data/grapher/health/latest/measles_long_run.py
+++ etl/steps/data/grapher/health/latest/measles_long_run.py
@@ -1,5 +1,8 @@
 """Load a garden dataset and create a grapher dataset."""
 
+import datetime as dt
+from datetime import datetime
+
 from etl.helpers import PathFinder, create_dataset
 
 # Get paths and naming conventions for current step.
@@ -15,7 +18,10 @@ def run(dest_dir: str) -> None:
 
     # Read table from garden dataset.
     tb = ds_garden.read("measles_long_run", reset_index=False)
-    #
+    assert tb["cases"].metadata.origins[1].title == "CDC Yearly measles cases (1985-present)"
+    publication_date = tb["cases"].metadata.origins[1].date_published
+    desc_short = format_description_short(publication_date)
+    tb["cases"].metadata.description_short = desc_short
     # Save outputs.
     #
     # Create a new grapher dataset with the same metadata as the garden dataset.
@@ -25,3 +31,10 @@ def run(dest_dir: str) -> None:
 
     # Save changes in the new grapher dataset.
     ds_grapher.save()
+
+
+def format_description_short(publication_date: str) -> str:
+    date_obj = datetime.strptime(publication_date, "%Y-%m-%d")
+    formatted_date = date_obj.strftime("%d %B %Y")
+
+    return f"Reported measles cases. The date for {datetime.now().year} is incomplete and was last updated {formatted_date}"
