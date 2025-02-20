bea10951280cc3cb13cedfaf41d9e13e5ac814ae
spoonerf <spoonerfiona@gmail.com>
2025-02-20 15:57:28 +0000

    adding death rates

--- etl/steps/data/garden/health/2025-02-20/measles_deaths_long_run.py
+++ etl/steps/data/garden/health/2025-02-20/measles_deaths_long_run.py
@@ -13,9 +13,13 @@ def run(dest_dir: str) -> None:
     # Load inputs.
     #
     # Load meadow dataset.
+    # Historical data from early 20th C to 1949
     ds_meadow_hist = paths.load_dataset("measles_deaths_historical")
+    # Data from WHO mortality database for 1950-present
     ds_who_mort = paths.load_dataset("mortality_database_vaccine_preventable")
-
+    # Load population data to calculate death rates.
+    ds_population = paths.load_dataset("population")
+    tb_pop = ds_population.read("population")
     # Read table from meadow dataset.
     tb_hist = ds_meadow_hist.read("measles_deaths_historical")
     tb_pres = ds_who_mort.read("mortality_database_vaccine_preventable", reset_metadata="keep_origins")
@@ -27,8 +31,17 @@ def run(dest_dir: str) -> None:
     ]
     tb_pres = tb_pres[["country", "year", "number"]]
     tb_pres = tb_pres.rename(columns={"number": "deaths"})
-
+    # Combine the historical and present data.
     tb = pr.concat([tb_hist, tb_pres], short_name="measles_deaths_long_run", ignore_index=True)
+
+    tb = pr.merge(
+        tb,
+        tb_pop,
+        on=["country", "year"],
+        how="left",
+    )
+    tb["death_rate"] = tb["deaths"] / tb["population"] * 100000
+    tb = tb.drop(columns=["population", "source", "world_pop_share"])
     tb = tb.format(["country", "year"])
 
     #
