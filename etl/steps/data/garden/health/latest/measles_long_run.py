f2d0baa76e89bcad9fd76eb953ac38f4242817d2
Fiona Spooner <fiona@ourworldindata.org>
2025-02-20 17:11:57 +0000

    📊 add measles case rate (#4010)
    * 📊 add measles case rate
    
    * adding measles case rate
    
    * add population to dag
    
    * add dataset title

--- etl/steps/data/garden/health/latest/measles_long_run.py
+++ etl/steps/data/garden/health/latest/measles_long_run.py
@@ -15,7 +15,7 @@ def run(dest_dir: str) -> None:
     # Load meadow dataset.
     ds_meadow_historical = paths.load_dataset("measles_historical")
     ds_meadow_current = paths.load_dataset("measles_cases")
-
+    ds_population = paths.load_dataset("population")
     # Read table from meadow dataset.
     tb_hist = ds_meadow_historical.read("measles_historical")
     tb_curr = ds_meadow_current.read("measles_cases")
@@ -24,6 +24,14 @@ def run(dest_dir: str) -> None:
     tb = pr.concat([tb_hist, tb_curr], short_name="measles_long_run")
     # Save outputs.
 
+    tb = pr.merge(
+        tb,
+        ds_population.read("population"),
+        on=["country", "year"],
+        how="left",
+    )
+    tb["case_rate"] = tb["cases"] / tb["population"] * 100000
+    tb = tb.drop(columns=["population", "source", "world_pop_share"])
     tb = tb.format(["country", "year"])
     # Create a new garden dataset with the same metadata as the meadow dataset.
     ds_garden = create_dataset(dest_dir, tables=[tb], check_variables_metadata=True)
