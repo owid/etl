a58d738a56645a80fd01387cf04a5931ca681688
Veronika Samborska <veronika@ourworldindata.org>
2025-02-25 11:06:26 +0000

    Update near_surface_temperature.py

--- etl/steps/data/garden/met_office_hadley_centre/2025-01-21/near_surface_temperature.py
+++ etl/steps/data/garden/met_office_hadley_centre/2025-01-21/near_surface_temperature.py
@@ -16,7 +16,7 @@ def run(dest_dir: str) -> None:
     # Calculate the adjustment factors based only on temperature_anomaly
     adjustment_factors = (
         tb_meadow[tb_meadow["year"].between(1961, 1990)].groupby("region")["temperature_anomaly"].mean()
-        - tb_meadow[tb_meadow["year"].between(1850, 1900)].groupby("region")["temperature_anomaly"].mean()
+        - tb_meadow[tb_meadow["year"].between(1861, 1890)].groupby("region")["temperature_anomaly"].mean()
     )
 
     columns_to_adjust = ["temperature_anomaly", "lower_limit", "upper_limit"]  # Add any other columns as needed
@@ -24,7 +24,7 @@ def run(dest_dir: str) -> None:
     # Apply the temperature_anomaly adjustment factor to all specified columns
     for region in adjustment_factors.index:
         for column in columns_to_adjust:
-            tb_meadow.loc[tb_meadow["region"] == region, column] -= adjustment_factors[region]
+            tb_meadow.loc[tb_meadow["region"] == region, column] += adjustment_factors[region]
 
     tb_meadow = tb_meadow.format(["region", "year"])
     #
