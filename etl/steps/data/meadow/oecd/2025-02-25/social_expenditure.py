a097420ac98c742416f516d71b507cd9eafa15f2
Pablo Arriagada <paarriagadap@gmail.com>
2025-02-25 18:36:00 +0000

    :construction: meadow

--- etl/steps/data/meadow/oecd/2025-02-25/social_expenditure.py
+++ etl/steps/data/meadow/oecd/2025-02-25/social_expenditure.py
@@ -5,6 +5,18 @@ from etl.helpers import PathFinder, create_dataset
 # Get paths and naming conventions for current step.
 paths = PathFinder(__file__)
 
+# Define columns to keep and their new names
+COLUMNS_TO_KEEP = {
+    "Reference area": "country",
+    "TIME_PERIOD": "year",
+    "Unit of measure": "indicator",
+    "Expenditure source": "expenditure_source",
+    "Spending type": "spending_type",
+    "Programme type": "programme_type",
+    "OBS_VALUE": "value",
+    "Observation status": "status",
+}
+
 
 def run(dest_dir: str) -> None:
     #
@@ -19,6 +31,12 @@ def run(dest_dir: str) -> None:
     #
     # Process data.
     #
+    # Keep only the columns of interest.
+    tb = tb[COLUMNS_TO_KEEP.keys()]
+
+    # Rename columns.
+    tb = tb.rename(columns=COLUMNS_TO_KEEP)
+
     # Ensure all columns are snake-case, set an appropriate index, and sort conveniently.
     tables = [tb.format(["country", "year"])]
 
