bd3d3be4a54669f34eb04c13d41c5bca0be4a02f
Fiona Spooner <fiona@ourworldindata.org>
2025-02-26 14:17:50 +0000

    📊 measles cases: Project Tycho (#3885)
    * 📊 measles cases tycho
    
    * adding tycho measles cases and fred population
    
    * adding jinja
    
    * rename country
    
    * rename country
    
    * merge
    
    * adding in national values table
    
    * adding in national values table
    
    * adding year to format
    
    * add cdc snapshot
    
    * adding garden and meadow steps
    
    * combining all sources
    
    * fix metadata
    
    * remove local snapshots
    
    * fix metadata
    
    * deduping variable shortnames
    
    * fix the new york and dc issues
    
    * fix up project tycho
    
    * fix coutnries
    
    * fix new york function
    
    * fix year in attribution
    
    * rename
    
    * get steps working with name change
    
    * add state level to dataset title
    
    * remove double dataset

---
+++ etl/steps/data/garden/demography/2025-01-23/us_state_population.py
@@ -0,0 +1,85 @@
+"""Load a meadow dataset and create a garden dataset."""
+
+from etl.helpers import PathFinder, create_dataset
+
+# Get paths and naming conventions for current step.
+paths = PathFinder(__file__)
+
+STATE_DICT = {
+    "AL": "Alabama",
+    "AK": "Alaska",
+    "AZ": "Arizona",
+    "AR": "Arkansas",
+    "CA": "California",
+    "CO": "Colorado",
+    "CT": "Connecticut",
+    "DE": "Delaware",
+    "FL": "Florida",
+    "GA": "Georgia",
+    "HI": "Hawaii",
+    "ID": "Idaho",
+    "IL": "Illinois",
+    "IN": "Indiana",
+    "IA": "Iowa",
+    "KS": "Kansas",
+    "KY": "Kentucky",
+    "LA": "Louisiana",
+    "ME": "Maine",
+    "MD": "Maryland",
+    "MA": "Massachusetts",
+    "MI": "Michigan",
+    "MN": "Minnesota",
+    "MS": "Mississippi",
+    "MO": "Missouri",
+    "MT": "Montana",
+    "NE": "Nebraska",
+    "NV": "Nevada",
+    "NH": "New Hampshire",
+    "NJ": "New Jersey",
+    "NM": "New Mexico",
+    "NY": "New York",
+    "NC": "North Carolina",
+    "ND": "North Dakota",
+    "OH": "Ohio",
+    "OK": "Oklahoma",
+    "OR": "Oregon",
+    "PA": "Pennsylvania",
+    "RI": "Rhode Island",
+    "SC": "South Carolina",
+    "SD": "South Dakota",
+    "TN": "Tennessee",
+    "TX": "Texas",
+    "UT": "Utah",
+    "VT": "Vermont",
+    "VA": "Virginia",
+    "WA": "Washington",
+    "WV": "West Virginia",
+    "WI": "Wisconsin",
+    "WY": "Wyoming",
+}
+
+
+def run(dest_dir: str) -> None:
+    #
+    # Load inputs.
+    #
+    # Load meadow dataset.
+    ds_meadow = paths.load_dataset("us_state_population")
+
+    # Read table from meadow dataset.
+    tb = ds_meadow.read("us_state_population")
+
+    tb = tb.replace({"state": STATE_DICT})
+    tb = tb.rename(columns={"value": "population"})
+    # Process data.
+    tb = tb.format(["state", "year"])
+
+    # Save outputs.
+    #
+    # Create a new garden dataset with the same metadata as the meadow dataset.
+    ds_garden = create_dataset(
+        dest_dir, tables=[tb], check_variables_metadata=True, default_metadata=ds_meadow.metadata
+    )
+
+    # Save changes in the new garden dataset.
+    ds_garden.save()
