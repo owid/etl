3310e664ddeb2a6d3573c303cc9467406187b6ae
Veronika Samborska <veronika@ourworldindata.org>
2025-02-27 20:35:14 +0000

    epoch:test

---
+++ etl/steps/data/garden/artificial_intelligence/2025-02-27/epoch_price_reduction.py
@@ -0,0 +1,47 @@
+"""Load a meadow dataset and create a garden dataset."""
+
+import pandas as pd
+
+from etl.helpers import PathFinder, create_dataset
+
+# Get paths and naming conventions for current step.
+paths = PathFinder(__file__)
+
+
+def run(dest_dir: str) -> None:
+    #
+    # Load inputs.
+    #
+    # Load meadow dataset.
+    ds_meadow = paths.load_dataset("epoch_price_reduction")
+
+    # Read table from meadow dataset.
+    tb = ds_meadow.read("epoch_price_reduction")
+
+    #
+    # Process data.
+    #
+
+    # Pivot the table so that each column represents a benchmark
+    tb_pivot = tb.pivot_table(
+        index=["threshold_model", "year"], columns="bench", values="price_reduction_factor_per_year"
+    ).reset_index()
+
+    # Flatten the columns
+    tb_pivot.columns.name = None
+    tb_pivot.columns = [str(col) for col in tb_pivot.columns]
+
+    tb_pivot = tb_pivot.rename(columns={"threshold_model": "country"})
+    print(tb_pivot)
+
+    tb_pivot = tb_pivot.format(["country", "year"])
+
+    # Save outputs.
+    #
+    # Create a new garden dataset with the same metadata as the meadow dataset.
+    ds_garden = create_dataset(dest_dir, tables=[tb_pivot], default_metadata=ds_meadow.metadata)
+
+    # Save changes in the new garden dataset.
+    ds_garden.save()
+
+    paths.log.info("epoch.end")
---
+++ etl/steps/data/meadow/artificial_intelligence/2025-02-27/epoch_price_reduction.py
@@ -0,0 +1,42 @@
+"""Load a snapshot and create a meadow dataset."""
+
+import pandas as pd
+
+from etl.helpers import PathFinder, create_dataset
+
+# Get paths and naming conventions for current step.
+paths = PathFinder(__file__)
+
+
+def run(dest_dir: str) -> None:
+    #
+    # Load inputs.
+    #
+    # Retrieve snapshot.
+    snap = paths.load_snapshot("epoch_price_reduction.csv")
+
+    # Read snapshot
+    tb = snap.read()
+
+    #
+    # Process data.
+    #
+    #
+    # Create a new table.
+    #
+    tb = tb[["bench", "threshold_model", "end_date", "price_reduction_factor_per_year"]]
+    tb["year"] = pd.to_datetime(tb["end_date"]).dt.year
+    tb["price_reduction_factor_per_year"] = tb["price_reduction_factor_per_year"].astype(float)
+    tb = tb.drop(columns=["end_date"])
+    tb = tb.format(["bench", "threshold_model", "year"])
+
+    #
+    # Save outputs.
+    #
+    # Create a new meadow dataset with the same metadata as the snapshot.
+    ds_meadow = create_dataset(dest_dir, tables=[tb], default_metadata=snap.metadata)
+
+    # Save changes in the new garden dataset.
+    ds_meadow.save()
+
+    paths.log.info("epoch.end")
