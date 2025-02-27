d56112751a11d555a9232835b3b749f221dc4709
Pablo Arriagada <63430031+paarriagadap@users.noreply.github.com>
2025-02-27 19:05:58 +0000

    📊 Update OECD Social Expenditure Database (SOCX) (#4033)
    * 📊 Update OECD Social Expenditure Database (SOCX)
    
    * :sparkles: snapshot
    
    * :construction: all steps
    
    * :construction: meadow
    
    * :construction: index
    
    * :sparkles: add programme types dict
    
    * :sparkles: countries mapping
    
    * :sparkles: garden
    
    * :lipstick: reduce text for social expenditure
    
    * :lipstick: in-kind and in-cash
    
    * :bug: fix description short whitespace issue
    
    * :bug: remove unused library
    
    * :bug: adjust metadata
    
    * :bug: wrong reference
    
    * :bug: fix line break
    
    * :lipstick: make programs in italics
    
    * :lipstick: improve key and short descriptions to avoid repeating text
    
    * :sparkles: add reference to OECD guide
    
    * :sparkles: add attribution_short and close parenthesis in desc
    
    * :lipstick: labour > labor
    
    * :bug: update broken link
    
    * :sparkles: add redistribution.yml
    
    * :sparkles: create redistribution yaml
    
    * :sparkles: add military expenditure to redistribution
    
    * :sparkles: comments from Lucas
    
    * :sparkles: add int-$ FAQ
    
    * :sparkles: improve title_variant
    
    * :sparkles: changes in title_variant and program type
    
    * :lipstick: improve program type handling
    
    * revert to default in grapher
    
    * :lisptick: improve handling of program type in short desc

---
+++ etl/steps/data/garden/oecd/2025-02-25/social_expenditure.py
@@ -0,0 +1,76 @@
+"""Load a meadow dataset and create a garden dataset."""
+
+from etl.data_helpers import geo
+from etl.helpers import PathFinder, create_dataset
+
+# Get paths and naming conventions for current step.
+paths = PathFinder(__file__)
+
+# Define indicator columns and their new names.
+INDICATOR_COLUMNS = {
+    "Percentage of GDP": "share_of_gdp",
+    "Percentage of general government expenditure": "share_of_gov_expenditure",
+    "US dollars per person, PPP converted": "usd_per_person_ppp",
+}
+
+
+def run(dest_dir: str) -> None:
+    #
+    # Load inputs.
+    #
+    # Load meadow dataset.
+    ds_meadow = paths.load_dataset("social_expenditure")
+
+    # Read table from meadow dataset.
+    tb = ds_meadow.read("social_expenditure")
+
+    #
+    # Process data.
+    #
+    tb = geo.harmonize_countries(
+        df=tb,
+        countries_file=paths.country_mapping_path,
+    )
+
+    # Pivot table to have indicator as columns.
+    tb = tb.pivot(
+        index=["country", "year", "expenditure_source", "spending_type", "programme_type_category", "programme_type"],
+        columns="indicator",
+        values="value",
+        join_column_levels_with="_",
+    )
+
+    # Rename columns
+    tb = tb.rename(columns=INDICATOR_COLUMNS)
+
+    # Change the categories of the total rows in the table.
+    tb = change_total_categories(tb)
+
+    tb = tb.format(
+        ["country", "year", "expenditure_source", "spending_type", "programme_type_category", "programme_type"]
+    )
+
+    #
+    # Save outputs.
+    #
+    # Create a new garden dataset with the same metadata as the meadow dataset.
+    ds_garden = create_dataset(
+        dest_dir, tables=[tb], check_variables_metadata=True, default_metadata=ds_meadow.metadata
+    )
+
+    # Save changes in the new garden dataset.
+    ds_garden.save()
+
+
+def change_total_categories(tb):
+    """
+    Change the categories of the total rows in the table.
+    This allows for easier reading of data pages.
+    """
+    tb.loc[tb["spending_type"] == "Total", "spending_type"] = "In-cash and in-kind spending"
+    tb.loc[tb["spending_type"] == "In cash spending", "spending_type"] = "In-cash spending"
+    tb.loc[tb["spending_type"] == "In kind spending", "spending_type"] = "In-kind spending"
+
+    tb.loc[tb["programme_type_category"] == "Total", "programme_type_category"] = "All"
+
+    return tb
