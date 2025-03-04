9fe2d12e2f3fa7f5ed041c64c1759d804f66f259
Pablo Arriagada <paarriagadap@gmail.com>
2025-03-04 18:27:40 +0000

    :construction: omm

--- etl/steps/data/garden/health_expenditure/2025-03-04/health_expenditure_omm.py
+++ etl/steps/data/garden/health_expenditure/2025-03-04/health_expenditure_omm.py
@@ -1,6 +1,7 @@
 """Load a meadow dataset and create a garden dataset."""
 
 import owid.catalog.processing as pr
+from owid.catalog import Table, warnings
 
 from etl.helpers import PathFinder, create_dataset
 
@@ -61,4 +62,50 @@ def run(dest_dir: str) -> None:
 
     # Save changes in the new garden dataset.
     ds_garden.save()
-    ds_garden.save()
+
+
+def create_estimations_from_growth(
+    tb: Table, reference_year: int, reference_var_suffix: str, to_adjust_var_suffix: str
+) -> Table:
+    """
+    Adjust estimations of variables according to the growth of a reference variable.
+
+    Parameters
+    ----------
+    tb : Table
+        Table that contains both the reference variable (the one the growth is extracted from) and the variable to be adjusted (the one the growth is applied to).
+    reference_year : int
+        Reference year from which the growth will be applied retroactively.
+    reference_var_suffix : str
+        Suffix of the reference variable (the one the growth is extracted from). In this project, "_mpd" or "_md".
+    to_adjust_var_suffix : str
+        Suffix of the variable to be adjusted (the one the growth is applied to). In this project, "_wdi" or "".
+
+    Returns
+    -------
+    tb : Table
+        Table with the adjusted variables.
+    """
+    with warnings.ignore_warnings([warnings.DifferentValuesWarning]):
+        # Get value from the reference variable in the reference year
+        reference_value = tb.loc[tb["year"] == reference_year, f"{var}{reference_var_suffix}"].iloc[0]
+
+        # The scalar is the previous value divided by the reference variable. This is the growth that will be applied retroactively to the variable to be adjusted.
+        tb[f"{var}_scalar"] = tb[f"{var}{reference_var_suffix}"] / reference_value
+
+        # Get value to be adjusted in the reference year
+        to_adjust_value = tb.loc[tb["year"] == reference_year, f"{var}{to_adjust_var_suffix}"].iloc[0]
+
+        # The estimated values are the division between the reference value and the scalars. This is the variable to be adjusted effectively adjusted by the growth of the reference variable.
+        tb[f"{var}_estimated"] = to_adjust_value * tb[f"{var}_scalar"]
+
+        # Rename the estimated variables without the suffix
+        tb[f"{var}"] = tb[f"{var}{to_adjust_var_suffix}"].astype("Float64").fillna(tb[f"{var}_estimated"])
+
+    # Specify "World" entity for each row
+    tb["country"] = "World"
+
+    # Keep only new variables
+    tb = tb[["country", "year"] + var_list]
+
+    return tb
