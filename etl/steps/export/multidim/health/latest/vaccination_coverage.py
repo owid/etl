7be59f4eb2fb75f7ed90930ddd3aba7e35241ac9
Lucas Rodés-Guirao <lucasrodes@users.noreply.github.com>
2025-03-06 19:19:56 +0100

    🎉  MDIMs: expand more than one indicator (#4078)
    * 🔨 debug mdim
    
    * wip
    
    * support expanding multiple indicators from single table
    
    * minor bug
    
    * adapt existing mdims

--- etl/steps/export/multidim/health/latest/vaccination_coverage.py
+++ etl/steps/export/multidim/health/latest/vaccination_coverage.py
@@ -9,8 +9,6 @@ paths = PathFinder(__file__)
 
 # etlr multidim
 def run(dest_dir: str) -> None:
-    # engine = get_engine()
-    # Load configuration from adjacent yaml file.
     config = paths.load_mdim_config()
 
     # Add views for all dimensions
@@ -19,12 +17,19 @@ def run(dest_dir: str) -> None:
     tb = ds.read("vaccination_coverage", load_data=False)
 
     # 2: Bake config automatically from table
-    config_new = multidim.expand_config(tb, indicator_name="coverage", dimensions=["antigen"])
+    config_new = multidim.expand_config(
+        tb,
+        indicator_names=["coverage", "unvaccinated_one_year_olds"],
+        dimensions=["antigen"],
+    )
+
     # 3: Combine both sources (basically dimensions and views)
+    # config["dimensions"] = config_coverage["dimensions"]
     config["dimensions"] = multidim.combine_config_dimensions(
         config_dimensions=config_new["dimensions"],
-        config_dimensions_yaml=config.get("dimensions", {}),
+        config_dimensions_yaml=config["dimensions"],
     )
+
     config["views"] = config_new["views"]
 
     # 4: Upsert to DB
@@ -33,24 +38,3 @@ def run(dest_dir: str) -> None:
         config=config,
         paths=paths,
     )
-
-
-#    multidim.upsert_multidim_data_page(
-#        config=config,
-#        paths=paths,
-#    )
-# config["views"] = multidim.generate_views_for_dimensions(
-#        dimensions=config["dimensions"],
-#        tables=[tb],
-#        dimensions_order_in_slug=("metric", "antigen"),
-#        warn_on_missing_combinations=False,
-#       additional_config={
-#            "$schema": "https://files.ourworldindata.org/schemas/grapher-schema.005.json",
-#            "chartTypes": ["LineChart"],
-#            "hasMapTab": True,
-#            "tab": "map",
-#            "map": {
-#                "colorScale": {"baseColorScheme": "YlGbBu"},
-#            },
-#        },
-#    )
