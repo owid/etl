a69500884e42cbe4914c88479a54093ce8f32093
Pablo Rosado <pabloarosado@gmail.com>
2025-03-06 09:50:53 +0100

    Add all steps for CSIS data (WIP)

---
+++ etl/steps/data/garden/space/2025-03-04/cost_of_space_launches.py
@@ -0,0 +1,38 @@
+"""Load a meadow dataset and create a garden dataset."""
+
+from etl.data_helpers import geo
+from etl.helpers import PathFinder
+
+# Get paths and naming conventions for current step.
+paths = PathFinder(__file__)
+
+
+def run() -> None:
+    #
+    # Load inputs.
+    #
+    # Load meadow dataset.
+    ds_meadow = paths.load_dataset("international_astronaut_database")
+
+    # Read table from meadow dataset.
+    tb = ds_meadow.read("international_astronaut_database")
+
+    #
+    # Process data.
+    #
+    # Harmonize country names.
+    tb = geo.harmonize_countries(
+        df=tb, countries_file=paths.country_mapping_path, excluded_countries_file=paths.excluded_countries_path
+    )
+
+    # Improve table format.
+    tb = tb.format(["country", "year"])
+
+    #
+    # Save outputs.
+    #
+    # Initialize a new garden dataset.
+    ds_garden = paths.create_dataset(tables=[tb], default_metadata=ds_meadow.metadata)
+
+    # Save garden dataset.
+    ds_garden.save()
