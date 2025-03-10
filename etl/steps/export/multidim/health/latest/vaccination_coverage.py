49473eafef1b2a2f75b7cda89175476eebf718de
Fiona Spooner <fiona@ourworldindata.org>
2025-03-10 16:41:34 +0000

    🎉 vaccination-coverage-mdim (#4073)
    * 🎉 vaccination-coverage-mdim
    
    * adding mdim skeleton and simplying grapher table
    
    * filling out config
    
    * adding indicator views
    
    * remove unused module
    
    * pivot to using un wpp example
    
    * wip
    
    * wip
    
    * wip
    
    * change load data to false
    
    * experiment removing manual views
    
    * adding back in  manual views
    
    * add unvaccinated numbers
    
    * remove unvaccinated yellow fever as it's empty
    
    * wip
    
    * remove metric dimensio
    
    * 🎉  MDIMs: expand more than one indicator (#4078)
    
    * 🔨 debug mdim
    
    * wip
    
    * support expanding multiple indicators from single table
    
    * minor bug
    
    * adapt existing mdims
    
    * tidy up
    
    * improvements to metadata
    
    * adding map brackets
    
    * update
    
    ---------
    
    Co-authored-by: lucasrodes <lucasrodes@users.noreply.github.com>

--- etl/steps/export/multidim/health/latest/vaccination_coverage.py
+++ etl/steps/export/multidim/health/latest/vaccination_coverage.py
@@ -16,10 +16,15 @@ def run(dest_dir: str) -> None:
     # Add views for all dimensions
     # NOTE: using load_data=False which only loads metadata significantly speeds this up
     ds = paths.load_dataset("vaccination_coverage")
-    tb = ds.read("vaccination_coverage", load_data=False)
+    tb = ds.read("vaccination_coverage", load_data=True)
 
     # 2: Bake config automatically from table
-    config_new = multidim.expand_config(tb, indicator_names=["coverage"], dimensions=["antigen"])
+    config_new = multidim.expand_config(
+        tb,
+        indicator_names=["coverage", "unvaccinated_one_year_olds"],
+        dimensions=["antigen"],
+        indicators_slug="metric",
+    )
     # 3: Combine both sources (basically dimensions and views)
     config["dimensions"] = multidim.combine_config_dimensions(
         config_dimensions=config_new["dimensions"],
@@ -33,24 +38,3 @@ def run(dest_dir: str) -> None:
         config=config,
         paths=paths,
     )
-
-
-#    multidim.upsert_multidim_data_page(
-#        config=config,
-#        paths=paths,
-#    )
-# config["views"] = multidim.generate_views_for_dimensions(
-#        dimensions=config["dimensions"],
-#        tables=[tb],
-#        dimensions_order_in_slug=("metric", "antigen"),
-#        warn_on_missing_combinations=False,
-#       additional_config={
-#            "$schema": "https://files.ourworldindata.org/schemas/grapher-schema.005.json",
-#            "chartTypes": ["LineChart"],
-#            "hasMapTab": True,
-#            "tab": "map",
-#            "map": {
-#                "colorScale": {"baseColorScheme": "YlGbBu"},
-#            },
-#        },
-#    )
