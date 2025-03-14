4e5cf776a3e3e8bf4b9ef75afe247636b4475899
Mojmir Vinkler <mojmir.vinkler@gmail.com>
2025-03-14 15:51:39 +0100

    🔨 Migrate Walden to Snapshots (#4119)

---
+++ snapshots/ggdc/2021-06-18/penn_world_table.py
@@ -0,0 +1,17 @@
+import click
+
+from etl.snapshot import Snapshot
+
+
+@click.command()
+@click.option("--upload/--skip-upload", default=True, type=bool, help="Upload dataset to Snapshot")
+def run(upload: bool) -> None:
+    # Initialize a new snapshot.
+    snap = Snapshot("ggdc/2021-06-18/penn_world_table.xlsx")
+
+    # Save snapshot.
+    snap.create_snapshot(upload=upload)
+
+
+if __name__ == "__main__":
+    run()
