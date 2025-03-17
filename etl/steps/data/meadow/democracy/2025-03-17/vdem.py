efe216582d940c726e6638ddc223b61ccd3b0c1e
lucasrodes <lucasrodes@users.noreply.github.com>
2025-03-17 20:40:32 +0100

    meadow

--- etl/steps/data/meadow/democracy/2025-03-17/vdem.py
+++ etl/steps/data/meadow/democracy/2025-03-17/vdem.py
@@ -1,6 +1,6 @@
 """Load a snapshot and create a meadow dataset."""
 
-from etl.helpers import PathFinder, create_dataset
+from etl.helpers import PathFinder
 
 # Get paths and naming conventions for current step.
 paths = PathFinder(__file__)
@@ -222,7 +222,7 @@ COLUMNS_KEEP = [
 ]
 
 
-def run(dest_dir: str) -> None:
+def run() -> None:
     #
     # Load inputs.
     #
@@ -248,7 +248,7 @@ def run(dest_dir: str) -> None:
     # Save outputs.
     #
     # Create a new meadow dataset with the same metadata as the snapshot.
-    ds_meadow = create_dataset(dest_dir, tables=[tb], check_variables_metadata=True, default_metadata=snap.metadata)
+    ds_meadow = paths.create_dataset(tables=[tb], check_variables_metadata=True, default_metadata=snap.metadata)
 
     # Save changes in the new meadow dataset.
     ds_meadow.save()
