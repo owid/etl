cc359efa0ffcca33c802249ac3349648f264bf6c
lucasrodes <lucasrodes@users.noreply.github.com>
2025-03-17 21:16:06 +0100

    remove unused imports

--- etl/steps/data/grapher/democracy/2025-03-17/vdem.py
+++ etl/steps/data/grapher/democracy/2025-03-17/vdem.py
@@ -2,7 +2,7 @@
 
 from owid.catalog import Table
 
-from etl.helpers import PathFinder, create_dataset
+from etl.helpers import PathFinder
 
 # Get paths and naming conventions for current step.
 paths = PathFinder(__file__)
