3a3ff3c10354accfd2eabbb044b42b95755a01bb
spoonerf <spoonerfiona@gmail.com>
2025-03-20 09:48:53 +0000

    use garden flunet

--- etl/steps/data/garden/who/2025-03-19/flu_yamagata.py
+++ etl/steps/data/garden/who/2025-03-19/flu_yamagata.py
@@ -1,6 +1,5 @@
 """Load a meadow dataset and create a garden dataset."""
 
-from etl.data_helpers import geo
 from etl.helpers import PathFinder
 
 # Get paths and naming conventions for current step.
@@ -17,19 +16,10 @@ def run() -> None:
     # Read table from meadow dataset.
     tb = ds_meadow.read("flunet")
 
-    #
-    # Process data.
-    #
-    # Harmonize country names.
-    tb = geo.harmonize_countries(
-        df=tb, countries_file=paths.country_mapping_path, excluded_countries_file=paths.excluded_countries_path
-    )
-
-    tb = tb[["country", "iso_weekstartdate", "iso_year", "byam"]]
+    tb = tb[["country", "date", "byamcombined"]]
     # tb_agg = tb.groupby(["country", "iso_weekstartdate", "iso_year"]).sum().reset_index()
-    tb_world = tb.groupby(["iso_weekstartdate"])["byam"].sum().reset_index()
+    tb_world = tb.groupby(["date"])["byamcombined"].sum().reset_index()
     tb_world["country"] = "World"
-    tb_world.rename(columns={"iso_weekstartdate": "date"}, inplace=True)
 
     # Improve table format.
     tb_world = tb_world.format(["country", "date"], short_name="flu_yamagata")
