81372f4532751b9a229478310c2e69400162738b
lucasrodes <lucasrodes@users.noreply.github.com>
2025-03-20 02:44:36 +0100

    cicd

--- etl/collections/explorer.py
+++ etl/collections/explorer.py
@@ -524,7 +524,7 @@ def hack_metadata_propagation(explorer, tbs, indicator_slug=None):
             dimension_idx = [str(dimensions[col]) for col in cols_index]
 
             try:
-                meta = df.loc[*dimension_idx].metadata
+                meta = df.loc[dimension_idx, "metadata"]
             except KeyError:
                 continue
 
