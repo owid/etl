aa81a3901f7400250dd429e9d6bd41e9519a11c8
spoonerf <spoonerfiona@gmail.com>
2025-03-26 09:56:01 +0000

    start update - indicators codes need figuring out

---
+++ etl/steps/data/meadow/un/2025-03-25/igme.py
@@ -0,0 +1,48 @@
+"""Load a snapshot and create a meadow dataset."""
+
+from etl.helpers import PathFinder, create_dataset
+
+# Get paths and naming conventions for current step.
+paths = PathFinder(__file__)
+
+
+def run(dest_dir: str) -> None:
+    #
+    # Load inputs.
+    #
+    # Retrieve snapshot.
+    snap = paths.load_snapshot("igme.zip")
+
+    # Load data from snapshot.
+    tb = snap.read_in_archive("UN IGME 2024.csv", low_memory=False, safe_types=False)
+    #
+    # Process data.
+    #
+    # Ensure all columns are snake-case, set an appropriate index, and sort conveniently.
+    tb = tb.rename(columns={"REF_AREA": "country", "REF_DATE": "year"}, errors="raise")
+    # There are some duplicated values in the UNICEF regions - so let's remove those. I wrote to UN IGME to report this 2024-09-12.
+    tb = tb[tb["REGIONAL_GROUP"] != "UNICEF"]
+    # Only grab the UN IGME estimates (not the input raw data)
+    tb = tb[tb["SERIES_NAME"] == "UN_IGME"]
+    tb = tb.format(
+        [
+            "country",
+            "year",
+            "indicator",
+            "sex",
+            "wealth_quintile",
+            "series_name",
+            "regional_group",
+            "unit_measure",
+        ]
+    )
+    #
+    # Save outputs.
+    #
+    # Create a new meadow dataset with the same metadata as the snapshot.
+    ds_meadow = create_dataset(
+        dest_dir, tables=[tb], check_variables_metadata=True, default_metadata=snap.metadata, repack=False
+    )
+
+    # Save changes in the new meadow dataset.
+    ds_meadow.save()
