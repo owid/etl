d618517b6c58119d6fadfaddad56114ec08feb28
Veronika Samborska <veronika@ourworldindata.org>
2025-03-31 10:07:27 +0100

    metadata

--- etl/steps/data/garden/ihme_gbd/2025-03-31/gbd_child_mortality_leading_causes.py
+++ etl/steps/data/garden/ihme_gbd/2025-03-31/gbd_child_mortality_leading_causes.py
@@ -20,7 +20,7 @@ def run(dest_dir: str) -> None:
     tb = tb[tb["metric"] != "Number"]
 
     # Replace with more descriptive values
-    tb["sex"] = tb["sex"].replace({"All": "children", "Male": "boys", "Female": "girls"})
+    tb["sex"] = tb["sex"].replace({"All": "all children", "Male": "boys", "Female": "girls"})
     # Group by 'country', 'year', 'sex', and 'age_group' and find the cause with the maximum death rate
     tb = tb.loc[tb.groupby(["country", "year", "sex", "age"])["value"].idxmax()]
     tb = tb.drop(columns=["value", "metric"])
