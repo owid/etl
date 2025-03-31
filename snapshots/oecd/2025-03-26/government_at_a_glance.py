2679e231cf3c10458566ad12ced5420467cfbda5
Pablo Arriagada <paarriagadap@gmail.com>
2025-03-31 13:05:26 +0100

    :lipstick: keep one script for snapshot

--- snapshots/oecd/2025-03-26/government_at_a_glance.py
+++ snapshots/oecd/2025-03-26/government_at_a_glance.py
@@ -9,15 +9,23 @@ from etl.snapshot import Snapshot
 # Version for current snapshot dataset.
 SNAPSHOT_VERSION = Path(__file__).parent.name
 
+# Define snapshot names
+SNAPSHOT_NAMES = [
+    "govt_glance_public_finance",
+    "govt_glance_size_public_procurement",
+    "govt_glance_public_finance_economic_transaction",
+]
+
 
 @click.command()
 @click.option("--upload/--skip-upload", default=True, type=bool, help="Upload dataset to Snapshot")
 def run(upload: bool) -> None:
-    # Initialize a new snapshot.
-    snap = Snapshot(f"oecd/{SNAPSHOT_VERSION}/govt_glance_public_finance.csv")
+    for snapshot_name in SNAPSHOT_NAMES:
+        # Initialize a new snapshot.
+        snap = Snapshot(f"oecd/{SNAPSHOT_VERSION}/{snapshot_name}.csv")
 
-    # Save snapshot.
-    snap.create_snapshot(upload=upload)
+        # Save snapshot.
+        snap.create_snapshot(upload=upload)
 
 
 if __name__ == "__main__":
