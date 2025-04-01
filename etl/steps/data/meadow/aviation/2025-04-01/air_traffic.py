576581d40949bd408f0b59db1891fbbed41a533a
Veronika Samborska <veronika@ourworldindata.org>
2025-04-01 09:04:33 +0100

    transport: update World Airlines Traffic and Capacity

---
+++ etl/steps/data/meadow/aviation/2025-04-01/air_traffic.py
@@ -0,0 +1,33 @@
+"""Load a snapshot and create a meadow dataset."""
+
+from etl.helpers import PathFinder
+
+# Get paths and naming conventions for current step.
+paths = PathFinder(__file__)
+
+
+def run() -> None:
+    #
+    # Load inputs.
+    #
+    # Retrieve snapshot.
+    snap = paths.load_snapshot("air_traffic.csv")
+
+    # Load data from snapshot.
+    tb = snap.read(encoding="utf-16", sep="\t")
+
+    #
+    # Process data.
+    #
+    tb["country"] = "World"
+    # Improve tables format.
+    tables = [tb.format(["country", "year"])]
+
+    #
+    # Save outputs.
+    #
+    # Initialize a new meadow dataset.
+    ds_meadow = paths.create_dataset(tables=tables, default_metadata=snap.metadata)
+
+    # Save meadow dataset.
+    ds_meadow.save()
