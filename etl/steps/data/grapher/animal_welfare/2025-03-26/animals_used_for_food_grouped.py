594e4f2c2afb87171fa5bfc0e3b8bf01d55bf8fc
Pablo Rosado <pabloarosado@gmail.com>
2025-04-02 11:31:54 +0200

    Adapt grapher step

--- etl/steps/data/grapher/animal_welfare/2025-03-26/animals_used_for_food_grouped.py
+++ etl/steps/data/grapher/animal_welfare/2025-03-26/animals_used_for_food_grouped.py
@@ -21,6 +21,11 @@ MAIN_ANIMALS_KILLED = [
     "cattle",
 ]
 
+# Label for meat total and for mid-point estimate.
+# NOTE: These labels should coincide with the ones defined in the garden step of animals_used_for_food (otherwise, an assertion will fail below).
+MEAT_TOTAL_LABEL = "all land animals"
+ESTIMATE_MIDPOINT_LABEL = "mid-point"
+
 # Label for all other animals.
 OTHER_ANIMALS_KILLED_LABEL = "other animals"
 
@@ -36,20 +41,26 @@ def run() -> None:
     #
     # Process data.
     #
-    # Keep only non-per capita rows.
-    tb = tb[~tb["per_capita"]].reset_index(drop=True)
+    # Sanity checks.
+    error = "Label for mid-point estimate may have changed. Update it in the code."
+    assert ESTIMATE_MIDPOINT_LABEL in set(tb["estimate"]), error
+    error = "Label for all animals (item assigned to total meat) may have changed. Update it in the code."
+    assert MEAT_TOTAL_LABEL in set(tb["animal"]), error
+
+    # Keep only non-per capita, mid-point estimate rows.
+    tb = tb[(~tb["per_capita"]) & (tb["estimate"] == ESTIMATE_MIDPOINT_LABEL)].reset_index(drop=True)
 
     # Group less frequently slaughtered animals into an "other" category.
     tb_other = (
-        tb[~tb["animal"].isin(MAIN_ANIMALS_KILLED)]
-        .groupby(["country", "year", "per_capita"], as_index=False)
+        tb[~tb["animal"].isin(MAIN_ANIMALS_KILLED + [MEAT_TOTAL_LABEL])]
+        .groupby(["country", "year", "per_capita", "estimate"], as_index=False)
         .agg({"n_animals_killed": "sum", "n_animals_alive": "sum"})
         .assign(**{"animal": OTHER_ANIMALS_KILLED_LABEL})
     )
     tb = pr.concat([tb, tb_other], ignore_index=True)
 
     # Improve table format.
-    tb = tb.format(["country", "year", "animal", "per_capita"])
+    tb = tb.format(["country", "year", "animal", "per_capita", "estimate"])
 
     #
     # Save outputs.
