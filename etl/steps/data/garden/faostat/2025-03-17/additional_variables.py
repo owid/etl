6a406e60969e43bce36c8803520ccb9cf8fc731e
Pablo Rosado <pabloarosado@gmail.com>
2025-04-08 11:59:33 +0200

    Fix wrong attribution in additional_variables due to a bug in propagation of presentation.attribution

--- etl/steps/data/garden/faostat/2025-03-17/additional_variables.py
+++ etl/steps/data/garden/faostat/2025-03-17/additional_variables.py
@@ -1002,6 +1002,19 @@ def generate_hypothetical_meat_consumption(tb_qcl: Table) -> Table:
         ["country", "year"], sort_columns=True, short_name="hypothetical_meat_consumption"
     )
 
+    ####################################################################################################################
+    # TODO: I noticed a bug (https://github.com/owid/etl/issues/4241) in the way presentation.attribution is propagated.
+    #  It causes that these specific variables show the attribution of the population dataset instead of FAOSTAT.
+    #  So, assert that the issue is happening, and if so, remove that attribution.
+    for column in tb_hypothetical_meat_consumption.columns:
+        if tb_hypothetical_meat_consumption[column].metadata.presentation is not None:
+            assert (
+                tb_hypothetical_meat_consumption[column].metadata.presentation.attribution
+                == "HYDE (2023); Gapminder (2022); UN WPP (2024)"
+            )
+            tb_hypothetical_meat_consumption[column].metadata.presentation.attribution = None
+    ####################################################################################################################
+
     return tb_hypothetical_meat_consumption
 
 
