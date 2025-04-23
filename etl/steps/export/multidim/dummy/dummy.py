b6ce8ba702fc363a1c9a71129799719355e73fc2
lucasrodes <lucasrodes@users.noreply.github.com>
2025-04-23 17:25:04 +0200

    re-structure dummy mdim example

--- etl/steps/export/multidim/dummy/dummy.py
+++ etl/steps/export/multidim/dummy/dummy.py
@@ -1,3 +1,13 @@
+"""This is an example on how you can read another MDIM and create a new one based on it.
+
+TODO:
+
+    - Transform MDIM into Explorer easily
+    - Combine with YML metadata
+    - Ease setting of catalog_path
+    - Combine multiple MDIMs
+"""
+
 from etl.helpers import PathFinder
 
 # Get paths and naming conventions for current step.
@@ -10,6 +20,7 @@ def run() -> None:
     mdims = paths.load_mdims("covid")
     mdim = mdims.read("covid_cases")
 
+    mdim.title["title"] = "This is a duplicate"
     mdim.catalog_path = f"{paths.namespace}/{paths.version}/{paths.short_name}#{paths.short_name}"
 
     # Save & upload
