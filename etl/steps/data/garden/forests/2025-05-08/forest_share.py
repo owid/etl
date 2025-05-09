c5436833a02dbb25a4fbc9ae889295e6a0c2d388
spoonerf <spoonerfiona@gmail.com>
2025-05-09 12:55:31 +0100

    processing garden step

--- etl/steps/data/garden/forests/2025-05-08/forest_share.py
+++ etl/steps/data/garden/forests/2025-05-08/forest_share.py
@@ -1,8 +1,10 @@
 """Load a meadow dataset and create a garden dataset."""
 
+from typing import List
+
+from owid.catalog import Table
 from owid.catalog import processing as pr
 
-from etl.data_helpers import geo
 from etl.helpers import PathFinder
 
 # Get paths and naming conventions for current step.
@@ -15,49 +17,60 @@ def run() -> None:
     #
     # Load meadow dataset.
     # Defra data for England and Scotland.
-    ds_meadow_defra = paths.load_snapshot("forest_share", namespace="defra")
+    snap_meadow_defra = paths.load_snapshot("forest_share", namespace="defra")
     # FAO data for England and Scotland.
-    ds_meadow_fao = paths.load_snapshot("forest_share", namespace="fao")
+    snap_meadow_fao = paths.load_snapshot("forest_share", namespace="fao")
     # Forest cover data for  Scotland.
-    ds_meadow_forest_research = paths.load_snapshot("forest_share", namespace="forest_research")
+    snap_meadow_forest_research = paths.load_snapshot("forest_share", namespace="forest_research")
     # Forest cover data for France
-    ds_meadow_france = paths.load_snapshot("france_forest_share", namespace="papers")
+    snap_meadow_france = paths.load_snapshot("france_forest_share", namespace="papers")
     # Forest cover data for Japan.
-    ds_meadow_japan = paths.load_snapshot("japan_forest_share", namespace="papers")
+    snap_meadow_japan = paths.load_snapshot("japan_forest_share", namespace="papers")
     # Forest cover data for Taiwan.
-    ds_meadow_taiwan = paths.load_snapshot("taiwan_forest_share", namespace="papers")
+    snap_meadow_taiwan = paths.load_snapshot("taiwan_forest_share", namespace="papers")
     # Forest cover data for the Scotland
-    ds_meadow_scotland = paths.load_snapshot("mather_2004", namespace="papers")
+    snap_meadow_scotland = paths.load_snapshot("mather_2004", namespace="papers")
     # Forest research data for South Korea
-    ds_meadow_south_korea = paths.load_snapshot("soo_bae_et_al_2012", namespace="papers")
+    snap_meadow_south_korea = paths.load_snapshot("soo_bae_et_al_2012", namespace="papers")
     # Forest research data for USA
-    ds_meadow_usa = paths.load_snapshot("forest_share", namespace="usda_fs")
+    snap_meadow_usa = paths.load_snapshot("forest_share", namespace="usda_fs")
     # FAO RL
     ds_meadow_fao_rl = paths.load_dataset("faostat_rl")
 
     # Read table from meadow dataset.
-    tb_defra = ds_meadow_defra.read("forest_share")
-    tb_fao = ds_meadow_fao.read("forest_share")
-    tb_forest_research = ds_meadow_forest_research.read("forest_share")
-    tb_france = ds_meadow_france.read("france_forest_share")
-    tb_japan = ds_meadow_japan.read("japan_forest_share")
-    tb_taiwan = ds_meadow_taiwan.read("taiwan_forest_share")
-    tb_scotland = ds_meadow_scotland.read("mather_2004")
-    tb_south_korea = ds_meadow_south_korea.read("soo_bae_et_al_2012")
-    tb_usa = ds_meadow_usa.read("forest_share")
-
+    tb_defra = snap_meadow_defra.read()
+    tb_fao = snap_meadow_fao.read()
+    tb_forest_research = snap_meadow_forest_research.read()
+    tb_france = snap_meadow_france.read()
+    tb_japan = snap_meadow_japan.read()
+    tb_taiwan = snap_meadow_taiwan.read()
+    tb_scotland = snap_meadow_scotland.read()
+    tb_south_korea = snap_meadow_south_korea.read()
+    tb_usa = snap_meadow_usa.read()
+    tb_fao_rl = ds_meadow_fao_rl["faostat_rl_flat"].reset_index()
+    tb_fao_rl = tb_fao_rl[["country", "year", "forest_land__00006646__share_in_land_area__007209__percent"]]
+    tb_fao_rl = tb_fao_rl.rename(
+        columns={"forest_land__00006646__share_in_land_area__007209__percent": "forest_share"}
+    ).dropna(subset=["forest_share"])
+    tb_fao_rl["source"] = "fao_rl"
     # Concatenate tables.
     tb = pr.concat(
-        [tb_defra, tb_fao, tb_forest_research, tb_france, tb_japan, tb_taiwan, tb_scotland, tb_south_korea, tb_usa]
+        [
+            tb_defra,
+            tb_fao,
+            tb_forest_research,
+            tb_france,
+            tb_japan,
+            tb_taiwan,
+            tb_scotland,
+            tb_south_korea,
+            tb_usa,
+        ]
     )
 
+    tb_com = combine_datasets(tb_a=tb, tb_b=tb_fao_rl, table_name="forest_share", preferred_source="fao_rl")
+    tb_com = tb_com.drop(columns=["source"])
     #
-    # Process data.
-    #
-    # Harmonize country names.
-    tb = geo.harmonize_countries(
-        df=tb, countries_file=paths.country_mapping_path, excluded_countries_file=paths.excluded_countries_path
-    )
 
     # Improve table format.
     tb = tb.format(["country", "year"])
@@ -70,3 +83,42 @@ def run() -> None:
 
     # Save garden dataset.
     ds_garden.save()
+
+
+def combine_datasets(tb_a: Table, tb_b: Table, table_name: str, preferred_source: str) -> Table:
+    """
+    Combine two tables with a preference for one source of data.
+    """
+    tb_combined = pr.concat([tb_a, tb_b], short_name=table_name).sort_values(
+        ["country", "year", "source"], ignore_index=True
+    )
+    assert any(tb_combined["source"] == preferred_source), "Preferred source not in table!"
+    tb_combined = remove_duplicates(
+        tb_combined,
+        preferred_source=preferred_source,
+        dimensions=["country", "year"],
+    )
+
+    return tb_combined
+
+
+def remove_duplicates(tb: Table, preferred_source: str, dimensions: List[str]) -> Table:
+    """
+    Removing rows where there are overlapping years with a preference for IGME data.
+
+    """
+    assert any(tb["source"] == preferred_source)
+    tb = tb.copy(deep=True)
+    duplicate_rows = tb.duplicated(subset=dimensions, keep=False)
+
+    tb_no_duplicates = tb[~duplicate_rows]
+
+    tb_duplicates = tb[duplicate_rows]
+
+    tb_duplicates_removed = tb_duplicates[tb_duplicates["source"] == preferred_source]
+
+    tb = pr.concat([tb_no_duplicates, tb_duplicates_removed], ignore_index=True)
+
+    assert len(tb[tb.duplicated(subset=dimensions, keep=False)]) == 0, "Duplicates still in table!"
+
+    return tb
