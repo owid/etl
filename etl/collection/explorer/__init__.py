3f13765118dca25f4cd4679e3811d112e4674eeb
lucasrodes <lucasrodes@users.noreply.github.com>
2025-05-09 12:16:21 +0200

    add minor docs

--- etl/collection/explorer/__init__.py
+++ etl/collection/explorer/__init__.py
@@ -1,3 +1,7 @@
+"""This submodule contains utils specific to Explorers.
+
+Explorers are a particular type of ETL collections."""
+
 from etl.collection.explorer.core import Explorer, combine_config_dimensions, create_explorer, expand_config
 from etl.collection.explorer.legacy import ExplorerLegacy, create_explorer_legacy
 
