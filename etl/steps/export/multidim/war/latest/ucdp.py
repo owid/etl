1b7e204ecaf16c6b7125fec23c962420af1d423e
lucasrodes <lucasrodes@users.noreply.github.com>
2025-05-14 14:00:56 +0200

    wip

--- etl/steps/export/multidim/war/latest/ucdp.py
+++ etl/steps/export/multidim/war/latest/ucdp.py
@@ -1,4 +1,5 @@
-# from etl.db import get_engine
+"""Load a meadow dataset and create a garden dataset."""
+
 from etl.helpers import PathFinder
 
 # Get paths and naming conventions for current step.
@@ -6,18 +7,125 @@ paths = PathFinder(__file__)
 
 
 def run() -> None:
-    # Load configuration from adjacent yaml file.
+    #
+    # Load inputs.
+    #
+    # Default collection config
     config = paths.load_collection_config()
 
-    # load table using load_data=False which only loads metadata significantly speeds this up
-    # ds = paths.load_dataset("ucdp")
-    # tb = ds.read("migrant_stock_dest_origin", load_data=False)
+    # Load grapher dataset.
+    ds = paths.load_dataset("ucdp")
+    tb = ds.read("ucdp", load_data=False)
 
-    # Create collection
+    # Filter unnecessary columns
+    tb = tb.filter(regex="^country|^year|^number_deaths_ongoing|^number_ongoing_conflicts__")
+
+    #
+    # (optional) Adjust dimensions if needed
+    #
+    tb = adjust_dimensions(tb)
+
+    #
+    # Create collection object
+    #
     c = paths.create_collection(
         config=config,
-        short_name="migration-flows",
+        short_name="ucdp",
+        tb=tb,
+        indicator_names=[
+            "deaths",
+            "death_rate",
+            "num_conflicts",
+        ],
+        # dimensions={},
     )
 
-    # Save & upload
+    #
+    # (optional) Edit views
+    #
+    for view in c.views:
+        # if view.dimension["sex"] == "male":
+        #     view.config["title"] = "Something else"
+        pass
+
+    #
+    # Save garden dataset.
+    #
     c.save()
+
+
+def adjust_dimensions(tb):
+    """Add dimensions to table columns.
+
+    It adds fields:
+    - `estimate`
+    - `people`
+
+    And tweaks original_short_name to make this consistent.
+    """
+
+    # Mapping columns to indicators
+    indicator_mapping_ = {
+        # Deaths
+        "^number_deaths_ongoing_conflicts(_combatants|_civilians|_high|_low|_unknown)?__": "deaths",
+        "^number_deaths_ongoing_conflicts(_high|_low)?_per_capita__": "death_rate",
+        "^number_ongoing_conflicts__": "num_conflicts",
+    }
+
+    # 1. Adjust indicators dictionary reference (maps full column name to actual indicator)
+    indicator_mapping = {}
+    for prefix, indicator_name in indicator_mapping_.items():
+        columns = list(tb.filter(regex=prefix).columns)
+        indicator_mapping = {**indicator_mapping, **{c: indicator_name for c in columns}}
+
+    # 2. Iterate over columns and adjust dimensions
+    columns = [col for col in tb.columns if col not in {"year", "country"}]
+    for col in columns:
+        # Overwrite original_short_name to actual indicator name
+        if col not in indicator_mapping:
+            raise Exception(f"Column {col} not in indicator mapping")
+        tb[col].metadata.original_short_name = indicator_mapping[col]
+
+        # Add estimate
+        if indicator_mapping[col] not in {"deaths", "death_rate"}:
+            tb[col].metadata.dimensions["estimate"] = "na"
+        else:
+            if "_high_" in col:
+                tb[col].metadata.dimensions["estimate"] = "high"
+            # Add low estimate dimension
+            elif "_low_" in col:
+                tb[col].metadata.dimensions["estimate"] = "low"
+            # Add 'Best'
+            else:
+                tb[col].metadata.dimensions["estimate"] = "best"
+
+        # Add people dimension
+        if indicator_mapping[col] == "deaths":
+            if "_combatants_" in col:
+                tb[col].metadata.dimensions["people"] = "combatants"
+            # Add low estimate dimension
+            elif "_civilians_" in col:
+                tb[col].metadata.dimensions["people"] = "civilians"
+            # Add 'Best'
+            elif "_unknown_" in col:
+                tb[col].metadata.dimensions["people"] = "unknown"
+            else:
+                tb[col].metadata.dimensions["people"] = "all"
+        else:
+            tb[col].metadata.dimensions["people"] = "na"
+
+    # 3. Adjust table-level dimension metadata
+    if isinstance(tb.metadata.dimensions, list):
+        tb.metadata.dimensions.extend(
+            [
+                {
+                    "name": "estimate",
+                    "slug": "estimate",
+                },
+                {
+                    "name": "people",
+                    "slug": "people",
+                },
+            ]
+        )
+    return tb
