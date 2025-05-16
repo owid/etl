27c81e5a75287a93e06069a57a30a1e858f6fe78
Veronika Samborska <32176660+veronikasamborska1994@users.noreply.github.com>
2025-05-16 15:35:11 +0100

    📊 corruption: Transparency International Corruption Barometer (#4382)

---
+++ etl/steps/data/garden/corruption/2025-05-12/corruption_barometer.py
@@ -0,0 +1,54 @@
+"""Load a meadow dataset and create a garden dataset."""
+
+from etl.data_helpers import geo
+from etl.helpers import PathFinder
+
+# Get paths and naming conventions for current step.
+paths = PathFinder(__file__)
+
+
+def run() -> None:
+    #
+    # Load inputs.
+    #
+    # Load meadow dataset.
+    ds_meadow = paths.load_dataset("corruption_barometer")
+
+    # Read table from meadow dataset.
+    tb = ds_meadow.read("corruption_barometer")
+
+    #
+    # Process data.
+    #
+    # Harmonize country names.
+    tb = geo.harmonize_countries(df=tb, countries_file=paths.country_mapping_path)
+    # Map specific terms in the 'answer' column to desired values
+    answer_mapping = {
+        "AGREE": "Agree or strongly agree",
+        "DISAGREE": "Disagree or strongly disagree",
+        "INCREASED": "Increased somewhat or a lot",
+        "DECREASED": "Decreased somewhat or a lot",
+        "NET NONE/ SOME": "Some or none of them",
+        "NET MOST/ALL": "Most or all of them",
+        "NET BADLY": "Fairly or very badly",
+        "NET WELL": "Fairly or very well",
+        "None": "None of them",
+        "Don't know / Refused": "Don't know or refused",
+        "TOTAL Bribery Rate, Excluding no contact": "Bribery rate among those who had contact with a public official in the past 12 months",
+        "TOTAL Bribery Rate, Total population": "Bribery rate among all respondents irrespective of contact with a public official in the past 12 months",
+        "TOTAL Contact Rate": "Contact rate with a public official in the past 12 months",
+    }
+
+    tb["answer"] = tb["answer"].replace(answer_mapping).str.lower().str.capitalize()
+    tb["value"] = tb["value"] * 100  # Convert to percentage
+    # Improve table format.
+    tb = tb.format(["country", "year", "question", "answer", "institution"])
+
+    #
+    # Save outputs.
+    #
+    # Initialize a new garden dataset.
+    ds_garden = paths.create_dataset(tables=[tb], default_metadata=ds_meadow.metadata)
+
+    # Save garden dataset.
+    ds_garden.save()
