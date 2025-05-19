26cfc8850f8206b8970abaca9134c6e4e306cee1
Veronika Samborska <veronika@ourworldindata.org>
2025-05-19 13:18:47 +0200

    test subselection

--- etl/steps/export/multidim/corruption/latest/corruption_barometer.py
+++ etl/steps/export/multidim/corruption/latest/corruption_barometer.py
@@ -1,5 +1,6 @@
 """Load a meadow dataset and create a garden dataset."""
 
+from etl.collection import create_collection, expand_config
 from etl.helpers import PathFinder
 
 # Get paths and naming conventions for current step.
@@ -10,31 +11,55 @@ def run() -> None:
     #
     # Load inputs.
     #
-    # Default collection config
-    config = paths.load_collection_config()
 
     # Load grapher dataset.
+    # Load configuration from adjacent yaml file.
+    config = paths.load_config()
+
     ds = paths.load_dataset("corruption_barometer")
-    tb = ds.read("corruption_barometer", load_data=False)
-    config = paths.load_collection_config()
-    #
-    # (optional) Adjust dimensions if needed
-    #
-    # config["views"] = multidim.expand_config(
-    #    tb,
-    #    dimensions=["question", "answer", "institution"],
-    #    additional_config={"chartTypes": ["LineChart"], "hasMapTab": True, "tab": "map"},
-    # )
+    tb = ds.read("corruption_barometer")
+
+    # Create views.
+    config_new = expand_config(
+        tb,
+        dimensions={
+            "question": [
+                "How many of the following people do you think are involved in corruption, or haven’t you heard enough about them to say? "
+            ],
+            "institution": [
+                "Business executives",
+                "Government officials",
+                "Judges and magistrates",
+                "Local government councilors",
+                "MPs or senators",
+                "Police",
+                "Prime Minister / President",
+                "Religious leaders",
+                "Tax officials",
+            ],
+            "answer": [
+                "All of them",
+                "Don't know / haven't heard",
+                "Most of them",
+                "Most or all of them",
+                "None of them",
+                "Some of them",
+                "Some or none of them",
+            ],
+        },
+    )
+
+    config["dimensions"] = config_new["dimensions"]
+    config["views"] = config_new["views"]
+
+    c = paths.create_collection(
+        config=config,
+        tb=tb,
+    )
 
     #
     # Create collection object
     #
-    c = paths.create_collection(
-        config=config,
-        short_name="corruption_barometer",
-        tb=tb,
-    )
-    print(c)
 
     #
     # Save garden dataset.
