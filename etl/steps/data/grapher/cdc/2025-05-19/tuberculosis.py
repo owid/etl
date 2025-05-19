86410e5798eaee6ba737fa87a1efed9ac2e15ae6
spoonerf <spoonerfiona@gmail.com>
2025-05-19 15:31:26 +0100

    table names

--- etl/steps/data/grapher/cdc/2025-05-19/tuberculosis.py
+++ etl/steps/data/grapher/cdc/2025-05-19/tuberculosis.py
@@ -16,15 +16,15 @@ def run() -> None:
     # Read table from garden dataset.
     tb_cases_pre_1975 = ds_garden.read("cases_before_1975", reset_index=False)
     tb_cases_post_1975 = ds_garden.read("cases_after_1975", reset_index=False)
-    tb_deaths_pre_1980 = ds_garden.read("deaths_before_1980", reset_index=False)
-    tb_deaths_post_1980 = ds_garden.read("deaths_after_1980", reset_index=False)
+    tb_deaths_pre_1979 = ds_garden.read("deaths_before_1979", reset_index=False)
+    tb_deaths_post_1979 = ds_garden.read("deaths_after_1979", reset_index=False)
 
     #
     # Save outputs.
     #
     # Initialize a new grapher dataset.
     ds_grapher = paths.create_dataset(
-        tables=[tb_cases_pre_1975, tb_cases_post_1975, tb_deaths_pre_1980, tb_deaths_post_1980],
+        tables=[tb_cases_pre_1975, tb_cases_post_1975, tb_deaths_pre_1979, tb_deaths_post_1979],
         default_metadata=ds_garden.metadata,
     )
 
