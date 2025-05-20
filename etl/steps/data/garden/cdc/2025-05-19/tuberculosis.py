46b5d8dd6d5dd0a208f0282083e502a0b912e800
Fiona Spooner <fiona@ourworldindata.org>
2025-05-20 20:50:43 +0100

    🐛 cdc tb fix (#4421)
    * 🐛 cdc tb fix
    
    * fix the deaths
    
    * copilot improvements
    
    * update table name

--- etl/steps/data/garden/cdc/2025-05-19/tuberculosis.py
+++ etl/steps/data/garden/cdc/2025-05-19/tuberculosis.py
@@ -27,6 +27,10 @@ def run() -> None:
         ]
     )
     tb = tb.replace("--", pd.NA)  # Replace '--' with NaN for better handling of missing values.
+    tb["tb_cases_no"] = (
+        tb["tb_cases_no"].str.replace(",", "", regex=False).astype("Int64")
+    )  # Remove commas from numbers for better handling of numeric values.
+    tb["tb_deaths_no"] = tb["tb_deaths_no"].str.replace(",", "", regex=False).astype("Int64")
     # Case data after 1974 are not comparable to prior years due to changes in the surveillance case definition that became effective in 1975.
     # So we need to split the table into two parts.
     tb_cases_before_1975 = tb[["country", "year", "tb_cases_no", "tb_cases_rate"]][tb["year"] <= 1974].copy()
@@ -41,14 +45,14 @@ def run() -> None:
     # Improve table format.
     tb_cases_before_1975 = tb_cases_before_1975.format(["country", "year"], short_name="cases_before_1975")
     tb_cases_after_1975 = tb_cases_after_1975.format(["country", "year"], short_name="cases_after_1975")
-    tb_deaths_before_1980 = tb_deaths_before_1979.format(["country", "year"], short_name="deaths_before_1979")
-    tb_deaths_after_1980 = tb_deaths_after_1979.format(["country", "year"], short_name="deaths_after_1979")
+    tb_deaths_before_1979 = tb_deaths_before_1979.format(["country", "year"], short_name="deaths_before_1979")
+    tb_deaths_after_1979 = tb_deaths_after_1979.format(["country", "year"], short_name="deaths_after_1979")
     #
     # Save outputs.
     #
     # Initialize a new garden dataset.
     ds_garden = paths.create_dataset(
-        tables=[tb_cases_before_1975, tb_cases_after_1975, tb_deaths_before_1980, tb_deaths_after_1980],
+        tables=[tb_cases_before_1975, tb_cases_after_1975, tb_deaths_before_1979, tb_deaths_after_1979],
         default_metadata=ds_meadow.metadata,
     )
 
