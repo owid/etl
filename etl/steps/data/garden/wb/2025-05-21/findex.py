0fb40fbc8382716dc0e2be34a5abed8e2027113d
Veronika Samborska <veronika@ourworldindata.org>
2025-05-21 12:39:55 +0200

    test metadata

--- etl/steps/data/garden/wb/2025-05-21/findex.py
+++ etl/steps/data/garden/wb/2025-05-21/findex.py
@@ -1,5 +1,7 @@
 """Load a meadow dataset and create a garden dataset."""
 
+from owid.catalog import Table
+
 from etl.data_helpers import geo
 from etl.helpers import PathFinder
 
@@ -26,8 +28,11 @@ def run() -> None:
     )
     # Convert to %
     tb["value"] = tb["value"] * 100
+
+    # Add metadata to the table.
+    tb = add_metadata(tb)
     # Improve table format.
-    tb = tb.format(["country", "year", "indicator_name", "short_definition", "long_definition"])
+    tb = tb.format(["country", "year"])
 
     #
     # Save outputs.
@@ -37,3 +42,24 @@ def run() -> None:
 
     # Save garden dataset.
     ds_garden.save()
+
+
+def add_metadata(tb: Table) -> Table:
+    """
+    Add metadata to the table.
+    """
+    # Pivot the table to have the indicators as columns to add descriptions from producer and description_short.
+    tb_pivoted = tb.pivot(index=["country", "year"], columns="indicator_name", values="value")
+
+    for column in tb_pivoted.columns:
+        meta = tb_pivoted[column].metadata
+        meta.display = {}
+        long_definition = tb["long_definition"].loc[tb["indicator_name"] == column]
+        short_definition = tb["short_definition"].loc[tb["indicator_name"] == column]
+        meta.description_from_producer = long_definition.iloc[0]
+        meta.description_short = short_definition.iloc[0]
+        meta.title = column
+        meta.unit = "%"
+        meta.short_unit = "%"
+    tb_pivoted = tb_pivoted.reset_index()
+    return tb_pivoted
