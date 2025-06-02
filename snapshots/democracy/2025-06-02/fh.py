1b23f3ac6b5fc72117077c080e3aaab32a818960
lucasrodes <lucasrodes@users.noreply.github.com>
2025-06-02 17:40:43 +0200

    snapshot add data

--- snapshots/democracy/2025-06-02/fh.py
+++ snapshots/democracy/2025-06-02/fh.py
@@ -14,14 +14,15 @@ SNAPSHOT_VERSION = Path(__file__).parent.name
 @click.option("--upload/--skip-upload", default=True, type=bool, help="Upload dataset to Snapshot")
 def main(upload: bool) -> None:
     # Create a new snapshot.
-    snaps = [
-        # Snapshot(f"democracy/{SNAPSHOT_VERSION}/fh_ratings.xlsx"),
-        Snapshot(f"democracy/{SNAPSHOT_VERSION}/fh_scores.xlsx"),
+    paths = [
+        f"democracy/{SNAPSHOT_VERSION}/fh_ratings.xlsx",
+        # f"democracy/{SNAPSHOT_VERSION}/fh_scores.xlsx",
+        f"democracy/{SNAPSHOT_VERSION}/fh.xlsx",
     ]
 
     # Download data from source, add file to DVC and upload to S3.
-    for snap in snaps:
-        snap.create_snapshot(upload=upload)
+    for path in paths:
+        Snapshot(path).create_snapshot(upload=upload)
 
 
 if __name__ == "__main__":
