ee5e58020dc1e6c8e7435e2899f02e47ee83cc21
spoonerf <spoonerfiona@gmail.com>
2025-06-10 15:04:40 +0100

    add group view

--- etl/steps/export/multidim/health/latest/vaccination_coverage.py
+++ etl/steps/export/multidim/health/latest/vaccination_coverage.py
@@ -21,4 +21,21 @@ def run() -> None:
         dimensions=["antigen"],
         indicators_slug="metric",
     )
+
+    c.group_views(
+        groups={
+            "dimension": "antigen",
+            "choice_new_slug": "comparison",
+            "view_config": {
+                "hasMapTab": False,
+                "addCountryMode": "change-country",
+                "tab": "chart",
+                "selectedFacetStrategy": "entity",
+                "title": "Vaccination coverage",
+                "subtitle": "Share of one-year-olds who have been immunized against a disease or a pathogen.",
+                "note": "This includes diphtheria, pertussis and tetanus (3rd dose), measles (1st dose), hepatitis B (3rd dose), polio (3rd dose), Haemophilus influenzae b (3rd dose), rubella (1st dose), rotavirus (final dose), yellow fever (1st dose), and inactivated polio (first dose).",
+            },
+        }
+    )
+
     c.save()
