146fd5e845387d4e7cacfbcb5426826fada46243
spoonerf <spoonerfiona@gmail.com>
2025-06-10 09:42:22 +0100

    remove unused function

--- etl/steps/data/garden/tuberculosis/2025-06-06/shared.py
+++ etl/steps/data/garden/tuberculosis/2025-06-06/shared.py
@@ -13,18 +13,3 @@ def add_variable_description_from_producer(tb: Table, dd: Table) -> Table:
         else:
             tb[col].metadata.description_from_producer = description_from_producer
     return tb
-
-
-def removing_old_variables(tb: Table, dd: Table, dataset_name: str) -> Table:
-    """
-    There are several variables in this dataset that are recorded as no longer being used after 2011.
-
-    We will remove these as they will be of limited use to us.
-    """
-    dd = dd[dd["dataset"] == dataset_name]
-    cols_to_drop = dd["variable_name"][dd["definition"].str.contains("not used after ")].to_list()
-    # Removing any columns that are in data dictionary but not in the dataset
-    cols_to_drop = [col for col in cols_to_drop if col in tb.columns]
-    tb = tb.drop(columns=cols_to_drop)
-
-    return tb
