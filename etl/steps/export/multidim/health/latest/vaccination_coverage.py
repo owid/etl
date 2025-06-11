096a3c0dc8cb70317cb4e5a46664efb8e6e0c82a
spoonerf <spoonerfiona@gmail.com>
2025-06-11 15:44:39 +0100

    fix

--- etl/steps/export/multidim/health/latest/vaccination_coverage.py
+++ etl/steps/export/multidim/health/latest/vaccination_coverage.py
@@ -23,19 +23,22 @@ def run() -> None:
     )
 
     c.group_views(
-        groups={
-            "dimension": "antigen",
-            "choice_new_slug": "comparison",
-            "view_config": {
-                "hasMapTab": False,
-                "addCountryMode": "change-country",
-                "tab": "chart",
-                "selectedFacetStrategy": "entity",
-                "title": "Vaccination coverage",
-                "subtitle": "Share of one-year-olds who have been immunized against a disease or a pathogen.",
-                "note": "This includes diphtheria, pertussis and tetanus (3rd dose), measles (1st dose), hepatitis B (3rd dose), polio (3rd dose), Haemophilus influenzae b (3rd dose), rubella (1st dose), rotavirus (final dose), yellow fever (1st dose), and inactivated polio (first dose).",
-            },
-        }
+        groups=[
+            {
+                "dimension": "antigen",
+                "choice_new_slug": "comparison",
+                "choices": ["MCV1", "DTP3", "YF1", "IPV1"],
+                "view_config": {
+                    "hasMapTab": False,
+                    "addCountryMode": "change-country",
+                    "tab": "chart",
+                    "selectedFacetStrategy": "entity",
+                    "title": "Vaccination coverage",
+                    "subtitle": "Share of one-year-olds who have been immunized against a disease or a pathogen.",
+                    "note": "This includes diphtheria, pertussis and tetanus (3rd dose), measles (1st dose), hepatitis B (3rd dose), polio (3rd dose), Haemophilus influenzae b (3rd dose), rubella (1st dose), rotavirus (final dose), yellow fever (1st dose), and inactivated polio (first dose).",
+                },
+            }
+        ]
     )
 
     c.save()
