9fd3bea3f226326954af5ff041bc9f023bcc6d9f
Pablo Arriagada <paarriagadap@gmail.com>
2025-06-18 11:03:16 +0100

    :bug: fix snapshots

--- snapshots/wb/2025-06-18/poverty_aggregates.py
+++ snapshots/wb/2025-06-18/poverty_aggregates.py
@@ -6,7 +6,7 @@ HOW TO OBTAIN THE FILE:
     2. Find the file `pip_2021_aggregate_202505.dta` in the data/02_processed/ folder.
 
     Copy that file to this folder and run the script:
-    python snapshots/wb/{version}/poverty_projections.py --path-to-file snapshots/wb/{version}/pip_2021_aggregate_202505.dta
+    python snapshots/wb/{version}/poverty_aggregates.py --path-to-file snapshots/wb/{version}/pip_2021_aggregate_202505.dta
 
 """
 
@@ -25,7 +25,7 @@ SNAPSHOT_VERSION = Path(__file__).parent.name
 @click.option("--path-to-file", "-f", prompt=True, type=str, help="Path to local data file.")
 def run(path_to_file: str, upload: bool) -> None:
     # Initialize a new snapshot.
-    snap = Snapshot(f"wb/{SNAPSHOT_VERSION}/poverty_projections.dta")
+    snap = Snapshot(f"wb/{SNAPSHOT_VERSION}/poverty_aggregates.dta")
 
     # Save snapshots.
     snap.create_snapshot(filename=path_to_file, upload=upload)
