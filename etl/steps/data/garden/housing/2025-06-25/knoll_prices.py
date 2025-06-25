d8f96d98650222de1b020ee1ab1765123d308fbc
Tuna Acisu <tuna.acisu@ourworldindata.com>
2025-06-25 17:29:33 +0100

    :bar_chart: add data

---
+++ etl/steps/data/garden/housing/2025-06-25/knoll_prices.py
@@ -0,0 +1,41 @@
+"""Load a meadow dataset and create a garden dataset."""
+
+from etl.data_helpers import geo
+from etl.helpers import PathFinder
+
+# Get paths and naming conventions for current step.
+paths = PathFinder(__file__)
+
+
+def run() -> None:
+    #
+    # Load inputs.
+    #
+    # Load meadow dataset.
+    ds_meadow = paths.load_dataset("knoll_prices")
+
+    # Read table from meadow dataset.
+    tb = ds_meadow.read("knoll_prices")
+
+    # Calculate real house prices from nominal house prices and CPI (analogue to Knoll)
+    tb["hpreal"] = tb["hpnom"] / tb["cpi"] * 100
+
+    tb = tb[["country", "year", "hpreal"]]
+
+    #
+    # Process data.
+    #
+    # Harmonize country names.
+    tb = geo.harmonize_countries(df=tb, countries_file=paths.country_mapping_path)
+
+    # Improve table format.
+    tb = tb.format(["country", "year"])
+
+    #
+    # Save outputs.
+    #
+    # Initialize a new garden dataset.
+    ds_garden = paths.create_dataset(tables=[tb], default_metadata=ds_meadow.metadata)
+
+    # Save garden dataset.
+    ds_garden.save()
