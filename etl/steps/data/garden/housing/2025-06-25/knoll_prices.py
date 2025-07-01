c429a7cffaaae51dbcfaa0cadbd4efa0ef8ab180
Tuna Acisu <50630133+antea04@users.noreply.github.com>
2025-07-01 09:31:57 +0100

    📊 house prices per GDP (#4665)
    * 📊 house prices per GDP
    
    * :bar_chart: Add house prices per GDP per capita
    
    * :bug: fix origins
    
    * :sparkles: pablo review comments
    
    ---------
    
    Co-authored-by: Tuna Acisu <tuna.acisu@ourworldindata.com>

--- etl/steps/data/garden/housing/2025-06-25/knoll_prices.py
+++ etl/steps/data/garden/housing/2025-06-25/knoll_prices.py
@@ -13,9 +13,12 @@ def run() -> None:
     #
     # Load meadow dataset.
     ds_meadow = paths.load_dataset("knoll_prices")
+    ds_gdp = paths.load_dataset("maddison_project_database")
 
     # Read table from meadow dataset.
     tb = ds_meadow.read("knoll_prices")
+    tb_gdp = ds_gdp.read("maddison_project_database")
+    tb_gdp = tb_gdp[["country", "year", "gdp_per_capita"]]
 
     # Calculate real house prices from nominal house prices and CPI
     # This calculation is identical to the calculation used in the original paper which can be found here: https://www.aeaweb.org/articles?id=10.1257/aer.20150501
@@ -27,7 +30,27 @@ def run() -> None:
     # Process data.
     #
     # Harmonize country names.
-    tb = geo.harmonize_countries(df=tb, countries_file=paths.country_mapping_path)
+    tb = geo.harmonize_countries(df=tb, countries_file=paths.country_mapping_path, warn_on_unused_countries=False)
+
+    # calculate real house prices per gdp per capita
+    tb = tb.merge(tb_gdp, on=["country", "year"], how="left")
+
+    tb["hp_per_gdp"] = tb["hpreal"] / tb["gdp_per_capita"] * 1000
+
+    # index time series to 1990 value for each country
+    for country in tb["country"].unique():
+        tb_c = tb.loc[tb["country"] == country]
+        # check that the country has data for 1990
+        if 1990 not in tb_c["year"].values:
+            raise ValueError(f"Country {country} does not have data for 1990.")
+        # normalize to 1990 value
+        norm = tb_c.loc[tb_c["year"] == 1990, "hp_per_gdp"].iloc[0]
+        tb.loc[tb["country"] == country, "hp_per_gdp_nom"] = (tb_c["hp_per_gdp"] / norm) * 100
+
+    tb["hp_per_gdp"] = tb["hp_per_gdp_nom"].round(3)
+    tb["hp_per_gdp"].m.origins = [tb["hpreal"].m.origins[0], tb["gdp_per_capita"].m.origins[0]]
+
+    tb = tb[["country", "year", "hpreal", "hp_per_gdp"]]
 
     # Improve table format.
     tb = tb.format(["country", "year"])
