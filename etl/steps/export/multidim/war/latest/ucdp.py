fc784f8d6382c9472adf5e855cdc65fa61987f12
Edouard Mathieu <edmat@pm.me>
2025-07-18 17:44:23 +0200

    🐛  Update string quotes in description key assertions
    Changed single quotes to double quotes in assertion strings for description keys in ucdp.py and ucdp_prio.py to ensure consistency with the expected format.

--- etl/steps/export/multidim/war/latest/ucdp.py
+++ etl/steps/export/multidim/war/latest/ucdp.py
@@ -283,7 +283,7 @@ def _set_description_key(view, tb):
     keys = tb[column].m.description_key
 
     if (view.d.estimate == "best_ci") or (view.d.indicator == "num_conflicts"):
-        assert keys[-1].startswith("We show here the 'best' death")
+        assert keys[-1].startswith('We show here the "best" death')
         keys = keys[:-1] + [None]
 
     return keys
