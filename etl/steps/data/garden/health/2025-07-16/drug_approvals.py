e5d6180f90448179519af2ced80b62bbe7333a1e
Tuna Acisu <tuna.acisu@ourworldindata.com>
2025-07-25 16:32:53 +0100

    :sparkles: some improvements to metadata

--- etl/steps/data/garden/health/2025-07-16/drug_approvals.py
+++ etl/steps/data/garden/health/2025-07-16/drug_approvals.py
@@ -2,7 +2,6 @@
 
 from datetime import datetime
 
-from matplotlib import pyplot as plt
 from owid.catalog import processing as pr
 
 from etl.helpers import PathFinder
@@ -11,15 +10,110 @@ from etl.helpers import PathFinder
 paths = PathFinder(__file__)
 
 
+def run() -> None:
+    # Load inputs.
+    #
+    # Load meadow dataset.
+    ds_cder = paths.load_dataset("cder_approvals")
+    ds_purple_book = paths.load_dataset("purple_book")
+
+    # Read table from meadow dataset.
+    tb_cder = ds_cder.read("cder_approvals")
+    tb_purple_book = ds_purple_book.read("purple_book")
+
+    # Normalize relevant column names.
+    tb_cder = tb_cder.rename(
+        columns={
+            "application_number__1": "application_number",
+            "proprietary__name": "proprietary_name",
+            "approval_date": "approval_date",
+            "nda_bla": "application_type",
+        }
+    )
+    tb_cder = tb_cder.replace({"BLA": "New biologics", "NDA": "New chemical drugs"})
+    tb_purple_book = tb_purple_book.rename(
+        columns={
+            "bla_number": "application_number",
+            "proprietary_name": "proprietary_name",
+            "approval_date": "approval_date",
+            "bla_type": "application_type",
+        }
+    )
+    # add approval year to purple book
+    tb_purple_book["approval_year"] = tb_purple_book["approval_date"].apply(
+        lambda x: datetime.strptime(str(x), "%Y-%m-%d %H:%M:%S").year
+    )
+
+    # restrict purple book to a) CBER approvals and b) 351(a) applications
+    tb_purple_book = tb_purple_book[tb_purple_book["center"] == "CBER"].copy()
+    tb_purple_book = tb_purple_book[tb_purple_book["application_type"] == "351(a)"]
+
+    # get approvals per year (NMEs, biologics, vaccines)
+    tb = get_approvals_per_year(tb_cder, tb_purple_book)
+    tb_total = tb[tb["application_type"] == "All new drug approvals"].copy()
+
+    # get approvals per designation
+    tb_designations = get_approvals_per_designation(tb_cder)
+
+    # add total drug approvals to designations
+    tb_designations = pr.concat(
+        [
+            tb_designations,
+            tb_total[["approval_year", "application_type", "approvals"]].rename(
+                columns={"application_type": "designation"}
+            ),
+        ],  # type: ignore
+        ignore_index=True,
+    )
+
+    # Format tables
+    tb = tb.rename(
+        columns={
+            "approval_year": "year",
+            "approvals": "total_approvals",
+        }
+    )
+
+    tb_designations = tb_designations.rename(
+        columns={
+            "approval_year": "year",
+        }
+    )
+
+    # get cumulative approvals (in case we want to plot cumulative approvals later)
+    # tb['cumulative_approvals'] = tb.sort_values(['application_type', 'year'])
+    # .groupby('application_type')['total_approvals']
+    # .cumsum()
+
+    # designations['cumulative_approvals'] = designations.sort_values(['designation', 'approval_year'])
+    #  .groupby('designation')['approvals']
+    #  .cumsum()
+
+    # Improve table format.
+    tb = tb.format(["year", "application_type"], short_name="total_drug_approvals")
+    tb_designations = tb_designations.format(["year", "designation"], short_name="drug_approvals_designations")
+
+    #
+    # Save outputs.
+    #
+    # Initialize a new garden dataset.
+    ds_garden = paths.create_dataset(tables=[tb, tb_designations], default_metadata=ds_cder.metadata)
+
+    # Save garden dataset.
+    ds_garden.save()
+
+
 def get_approvals_per_year(tb_cder, tb_purple_book):
     # copy table to avoid modifying original data
-    tb_cber = tb_purple_book.copy()
+    tb_cber_biologics = tb_purple_book.copy()
     tb_cder = tb_cder.copy()
 
-    tb_cber["vaccine"] = tb_cber["proper_name"].str.contains("vaccine", case=False, na=False)
-    tb_cber["vaccine"] = tb_cber["vaccine"].apply(lambda x: "New vaccines" if x else "Other new biologics")
-    tb_cber["application_type"] = tb_cber["vaccine"]
-
+    # get vaccine approvals from CBER
+    tb_cber_biologics["vaccine"] = tb_cber_biologics["proper_name"].str.contains("vaccine", case=False, na=False)
+    tb_cber_biologics["vaccine"] = tb_cber_biologics["vaccine"].apply(
+        lambda x: "New vaccines" if x else "Other new biologics"
+    )
+    tb_cber_biologics["application_type"] = tb_cber_biologics["vaccine"]
     tb_cder = tb_cder.replace(
         {
             "New biologics": "Other new biologics",
@@ -29,7 +123,7 @@ def get_approvals_per_year(tb_cder, tb_purple_book):
     tb = pr.concat(
         [
             tb_cder[["approval_year", "application_type"]],
-            tb_cber[["approval_year", "application_type"]],
+            tb_cber_biologics[["approval_year", "application_type"]],
         ],
         ignore_index=True,
     )
@@ -38,8 +132,9 @@ def get_approvals_per_year(tb_cder, tb_purple_book):
 
     tb_gb["approvals"].m.origins = tb_gb["approval_year"].m.origins.copy()
 
+    # get total approvals per year
     tb_cum = tb_gb.groupby("approval_year").sum().reset_index()
-    tb_cum["application_type"] = "Total drug approvals"
+    tb_cum["application_type"] = "All new drug approvals"
 
     tb = pr.concat([tb_gb, tb_cum], ignore_index=True)
 
@@ -91,115 +186,12 @@ def get_approvals_per_designation(tb_cder):
     # rename designation values
     designations = designations.replace(
         {
-            "orphan_drug_designation": "Orphan Drug",
-            "accelerated_approval": "Accelerated Approval",
-            "breakthrough_therapy_designation": "Breakthrough Therapy",
-            "fast_track_designation": "Fast Track",
-            "qualified_infectious_disease_product": "Qualified Infectious Disease Product",
+            "orphan_drug_designation": "Orphan drug approvals",
+            "accelerated_approval": "Accelerated approvals",
+            "breakthrough_therapy_designation": "Breakthrough therapy approvals",
+            "fast_track_designation": "Fast track approvals",
+            "qualified_infectious_disease_product": "Qualified infectious disease product approvals",
         }
     )
 
     return designations
-
-
-def run() -> None:
-    # Load inputs.
-    #
-    # Load meadow dataset.
-    ds_cder = paths.load_dataset("cder_approvals")
-    ds_purple_book = paths.load_dataset("purple_book")
-
-    # Read table from meadow dataset.
-    tb_cder = ds_cder.read("cder_approvals")
-    tb_purple_book = ds_purple_book.read("purple_book")
-
-    # Normalize relevant column names.
-    tb_cder = tb_cder.rename(
-        columns={
-            "application_number__1": "application_number",
-            "proprietary__name": "proprietary_name",
-            "approval_date": "approval_date",
-            "nda_bla": "application_type",
-        }
-    )
-    tb_cder = tb_cder.replace({"BLA": "New biologics", "NDA": "New chemical drugs"})
-    tb_purple_book = tb_purple_book.rename(
-        columns={
-            "bla_number": "application_number",
-            "proprietary_name": "proprietary_name",
-            "approval_date": "approval_date",
-            "bla_type": "application_type",
-        }
-    )
-    # add approval year to purple book
-    tb_purple_book["approval_year"] = tb_purple_book["approval_date"].apply(
-        lambda x: datetime.strptime(str(x), "%Y-%m-%d %H:%M:%S").year
-    )
-
-    # restrict purple book to a) CBER approvals and b) 351(a) applications
-    tb_purple_book = tb_purple_book[tb_purple_book["center"] == "CBER"].copy()
-    tb_purple_book = tb_purple_book[tb_purple_book["application_type"] == "351(a)"]
-
-    # get approvals per year (NDAs, BLAs)
-    tb = get_approvals_per_year(tb_cder, tb_purple_book)
-
-    # get approvals per designation
-    tb_designations = get_approvals_per_designation(tb_cder)
-
-    # Format tables
-    tb = tb.rename(
-        columns={
-            "approval_year": "year",
-            "approvals": "total_approvals",
-        }
-    )
-
-    tb_designations = tb_designations.rename(
-        columns={
-            "approval_year": "year",
-        }
-    )
-
-    # get cumulative approvals (in case we want to plot cumulative approvals later)
-    # tb['cumulative_approvals'] = tb.sort_values(['application_type', 'year'])
-    # .groupby('application_type')['total_approvals']
-    # .cumsum()
-
-    # designations['cumulative_approvals'] = designations.sort_values(['designation', 'approval_year'])
-    #  .groupby('designation')['approvals']
-    #  .cumsum()
-
-    # Improve table format.
-    tb = tb.format(["year", "application_type"], short_name="total_drug_approvals")
-    tb_designations = tb_designations.format(["year", "designation"], short_name="drug_approvals_designations")
-
-    #
-    # Save outputs.
-    #
-    # Initialize a new garden dataset.
-    ds_garden = paths.create_dataset(tables=[tb, tb_designations], default_metadata=ds_cder.metadata)
-
-    # Save garden dataset.
-    ds_garden.save()
-
-
-def plot_per_source(tb, source, by="application_type", color=None, title=None):
-    tb_source = tb[tb["source"] == source][["approval_year", "approvals", by]].copy()
-    tb_source = tb_source.groupby(["approval_year", by]).sum().reset_index()
-
-    tb_pv = tb_source.pivot(index="approval_year", columns=by, values="approvals")
-
-    if color:
-        ordered_series = list(color.keys())
-        tb_pv = tb_pv[ordered_series]
-
-    tb_pv.plot(kind="bar", figsize=(8, 5), stacked=True, color=color)
-    plt.xlabel("Approval Year")
-    plt.ylabel("Approvals")
-    if title:
-        plt.title(f"{title}")
-    else:
-        plt.title(f"Drug Approvals per Year ({source})")
-    plt.legend(title=by)
-    plt.tight_layout()
-    plt.show()
--- etl/steps/data/grapher/health/2025-07-16/drug_approvals.py
+++ etl/steps/data/grapher/health/2025-07-16/drug_approvals.py
@@ -1,7 +1,5 @@
 """Load a garden dataset and create a grapher dataset."""
 
-from owid.catalog import processing as pr
-
 from etl.helpers import PathFinder
 
 # Get paths and naming conventions for current step.
@@ -24,57 +22,19 @@ def run() -> None:
         errors="raise",
     )
 
-    tb = tb.replace({"BLA": "Biologics License Application", "NDA": "New Molecular Entity"})
-
     tb_designations = ds_garden.read("drug_approvals_designations")
-    tb_desig_original = tb_designations.copy()
-
-    tb_designations = pr.melt(
-        tb_designations,
-        id_vars=["year"],
-        value_vars=[
-            "orphan_drug_approvals",
-            "accelerated_approvals",
-            "breakthrough_therapy_approvals",
-            "fast_track_approvals",
-            "qualified_infectious_disease_approvals",
-            "vaccine_approvals",
-        ],
-        var_name="country",
-        value_name="approvals",
+    tb_designations = tb_designations.rename(
+        columns={
+            "designation": "country",
+        },
+        errors="raise",
     )
-
-    tb_designations = tb_designations.replace(
-        {
-            "orphan_drug_approvals": "Orphan Drug",
-            "accelerated_approvals": "Accelerated Approval",
-            "breakthrough_therapy_approvals": "Breakthrough Therapy",
-            "fast_track_approvals": "Fast Track",
-            "qualified_infectious_disease_approvals": "Qualified Infectious Disease Product",
-            "vaccine_approvals": "Vaccine",
-        }
-    )
-
-    tb_designations["approvals"].m.title = "Number of approvals"
-    tb_designations["approvals"].m.description = "The number of approvals for each designation in a given year."
-    tb_designations["approvals"].m.unit = "approvals"
-    tb_designations["approvals"].m.short_unit = ""
-    tb_designations = tb_designations.format(["year", "country"], short_name="drug_approvals_designations_melted")
-    tb = tb.format(
-        ["year", "country"],
-        short_name="total_drug_approvals",
-    )
-
-    tb_desig_original["country"] = "United States"
-    tb_desig_original = tb_desig_original.format(["year", "country"])
 
     #
     # Save outputs.
     #
     # Initialize a new grapher dataset.
-    ds_grapher = paths.create_dataset(
-        tables=[tb, tb_designations, tb_desig_original], default_metadata=ds_garden.metadata
-    )
+    ds_grapher = paths.create_dataset(tables=[tb, tb_designations], default_metadata=ds_garden.metadata)
 
     # Save grapher dataset.
     ds_grapher.save()
