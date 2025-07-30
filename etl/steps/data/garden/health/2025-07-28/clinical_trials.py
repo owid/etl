fdf3f12cd2e123e1c47e0eea702e85181897a39d
Tuna Acisu <tuna.acisu@ourworldindata.com>
2025-07-30 21:05:01 +0100

    :bar_chart: do all the analysis

--- etl/steps/data/garden/health/2025-07-28/clinical_trials.py
+++ etl/steps/data/garden/health/2025-07-28/clinical_trials.py
@@ -1,33 +1,33 @@
 """Load a meadow dataset and create a garden dataset."""
+# find out more about the data fields at https://clinicaltrials.gov/data-api/about-api/study-data-structure
 
-import logging
+import time
 from collections import Counter
 
 import pandas as pd
+from structlog import get_logger
 
 from etl.data_helpers import geo
 from etl.helpers import PathFinder
 
 # import logger
-logger = logging.getLogger(__name__)
+logger = get_logger()
 
 # Get paths and naming conventions for current step.
 paths = PathFinder(__file__)
 
 INTERVENTIONS = [
-    "PREVENTION",
     "DEVICE",
-    "PROCEDURE",
+    "PROCEDURE",  # e.g. surgery
     "RADIATION",
     "OTHER",
     "DIETARY_SUPPLEMENT",
     "DIAGNOSTIC_TEST",
     "BEHAVIORAL",
-    "BIOLOGICAL",
+    "BIOLOGICAL",  # e.g. vaccine
     "DRUG",
-    "BONE VOID FILLER",
-    "GENETIC",
-    "COMBINATION_PRODUCT",
+    "GENETIC",  # e.g. gene therapy
+    "COMBINATION_PRODUCT",  # e.g. drug + device
 ]
 
 
@@ -121,15 +121,22 @@ def date_col_to_year(df: pd.DataFrame, col_name: str) -> pd.Series:
 
 
 def get_most_common(lst):
-    data = Counter(lst)
-    return data.most_common(1)[0][0]
+    """
+    Get the most common element in a list.
+    If there are multiple elements with the same frequency, it returns the first one.
+    """
+    if not lst:
+        return "Location not specified"
+    count = Counter(lst)
+    most_common = count.most_common(1)[0][0].strip()
+    return most_common
 
 
 def extract_primary_location(loc_string: str) -> str:
     """
     Extract the primary location from a location string.
 
-    If multiple locations are provided, it returns the most common one.
+    If multiple locations are provided, it returns the most common one. If multiple locations appear equally often, it returns the first one.
     If no location is specified, it returns "Location not specified".
     """
     if pd.isna(loc_string):
@@ -147,12 +154,13 @@ def run() -> None:
     # Load inputs.
     #
     # Load meadow dataset.
+    time_stamp = time.time()
     snap = paths.load_snapshot("clinical_trials.csv")
 
     # Load data from snapshot.
     tb = snap.read()
 
-    logger.info(f"Loaded snapshot with {len(tb)} rows.")
+    logger.info(f"Loaded snapshot with {len(tb)} rows in {time.time() - time_stamp:.2f} seconds.")
 
     tb = tb.drop(
         columns=[
@@ -172,6 +180,12 @@ def run() -> None:
     tb["primary_completion_year"] = date_col_to_year(tb, "Primary Completion Date")
     tb["first_posted_year"] = date_col_to_year(tb, "First Posted")
     tb["results_first_posted_year"] = date_col_to_year(tb, "Results First Posted")
+
+    # get length of study in days
+    tb["study_length_days"] = (
+        pd.to_datetime(tb["Completion Date"], errors="coerce", format="mixed", yearfirst=True)
+        - pd.to_datetime(tb["Start Date"], errors="coerce", format="mixed", yearfirst=True)
+    ).dt.days
 
     # change appriopriate columns to categorical (to save memory & speed up processing)
     tb["Study Status"] = tb["Study Status"].astype("category")
@@ -220,36 +234,117 @@ def run() -> None:
     )
 
     # get all studies by completion year and country
-    tb_trials_per_year = (
-        tb.groupby(["primary_location", "completion_year"], observed=True).size().reset_index(name="n_studies_country")
-    )
+    tb_trials = (
+        tb.groupby(["primary_location", "completion_year"]).size().reset_index(name="n_studies_country")
+    ).copy_metadata(tb)
+    tb_trials["n_studies_country"].m.origins = tb["Phases"].m.origins
 
     # studies by completion year and sponsor type
-    tb_sponsor_per_year = (
-        tb.groupby(["Funder Type", "completion_year"], observed=True).size().reset_index(name="n_studies_sponsor")
-    )
+    tb_sponsor = (
+        tb.groupby(["Funder Type", "completion_year"]).size().reset_index(name="n_studies_sponsor")
+    ).copy_metadata(tb)
+    tb_sponsor["n_studies_sponsor"].m.origins = tb["Funder Type"].m.origins
 
     # get sum of studies by completion year by intervention type
-    tb_intervention_per_year = tb.copy()
-    tb_intervention_per_year[INTERVENTIONS] = tb_intervention_per_year[INTERVENTIONS].astype("Int64")
+    tb_interventions = tb.copy()
+    tb_interventions[INTERVENTIONS] = tb_interventions[INTERVENTIONS].astype("Int64")
     intvt_cols = ["completion_year"] + INTERVENTIONS
-    tb_intervention_per_year = (
-        tb_intervention_per_year[intvt_cols].groupby("completion_year").sum().reset_index()
-    )
+    tb_interventions = tb_interventions[intvt_cols].groupby("completion_year").sum().reset_index().copy_metadata(tb)
+    tb_interventions[INTERVENTIONS].m.origins = tb["Phases"].m.origins
+
+    # get sum of studies by completion year by study type
+    tb_study_type = (
+        tb.groupby(["Study Type", "completion_year"]).size().reset_index(name="n_studies_type")
+    ).copy_metadata(tb)
+    tb_study_type["n_studies_type"].m.origins = tb["Phases"].m.origins
+
+    # get sum of studies by completion year by primary purpose
+    tb_purpose = (
+        tb.groupby(["primary_purpose", "completion_year"]).size().reset_index(name="n_studies_purpose")
+    ).copy_metadata(tb)
+    tb_purpose["n_studies_purpose"].m.origins = tb["Phases"].m.origins
+
+    # get sum of studies by completion year by status
+    tb_status = tb[tb["Study Type"] != "Expanded Access"]
+    tb_status = (
+        tb_status.groupby(["Study Status", "start_year"]).size().reset_index(name="n_studies_status")
+    ).copy_metadata(tb)
+    tb_status["n_studies_status"].m.origins = tb["Phases"].m.origins
+
+    # get sum of studies by completion year and whether they have results
+    tb_results = (
+        tb.groupby(["Study Results", "start_year"]).size().reset_index(name="n_studies_results")
+    ).copy_metadata(tb)
+    tb_results["n_studies_results"].m.origins = tb["Phases"].m.origins
+
+    # get average study length by phase and completion year
+    tb_length = (
+        tb.groupby(["completion_year", "Phases"])["study_length_days"].mean().reset_index(name="avg_study_length_days")
+    ).copy_metadata(tb)
+    tb_length["avg_study_length_days"].m.origins = tb["Phases"].m.origins
 
     # Improve table format.
-    tb = tb.format(["country", "year"])
+    tb_trials, tb_sponsor, tb_interventions, tb_study_type, tb_purpose, tb_status, tb_results, tb_length = (
+        format_tables(
+            tb_trials, tb_sponsor, tb_interventions, tb_study_type, tb_purpose, tb_status, tb_results, tb_length
+        )
+    )
 
-    tables = [
-        tb,
-        tb_trials_per_year,
+    # TODO: add replacements for entities
+    # {'EARLY_PHASE1', 'PHASE1|PHASE2', 'PHASE4', 'PHASE2', 'PHASE1', 'PHASE3', 'PHASE2|PHASE3'}
+    # {'INDIV', 'NIH', 'OTHER', 'FED', 'AMBIG', 'INDUSTRY', 'OTHER_GOV', 'UNKNOWN', 'NETWORK'}
+    # {'ENROLLING_BY_INVITATION', 'RECRUITING', 'TERMINATED', 'NOT_YET_RECRUITING', 'COMPLETED', 'WITHDRAWN', 'ACTIVE_NOT_RECRUITING', 'AVAILABLE', 'NO_LONGER_AVAILABLE', 'SUSPENDED', 'APPROVED_FOR_MARKETING', 'TEMPORARILY_NOT_AVAILABLE'}
+    # {'OBSERVATIONAL', 'INTERVENTIONAL', 'EXPANDED_ACCESS'}
+
+    tables_ls = [
+        tb_trials,
+        tb_sponsor,
+        tb_interventions,
+        tb_study_type,
+        tb_purpose,
+        tb_status,
+        tb_results,
+        tb_length,
     ]
 
     #
     # Save outputs.
     #
     # Initialize a new garden dataset.
-    ds_garden = paths.create_dataset(tables=[tb], default_metadata=snap.metadata)
+    ds_garden = paths.create_dataset(tables=tables_ls, default_metadata=snap.metadata)
 
     # Save garden dataset.
     ds_garden.save()
+
+
+def format_tables(tb_trials, tb_sponsor, tb_interventions, tb_study_type, tb_purpose, tb_status, tb_results, tb_length):
+    # rename the columns in each table to "year" and "country" so they can be used in grapher
+    tb_trials = tb_trials.rename(columns={"completion_year": "year", "primary_location": "country"}, errors="raise")
+    tb_sponsor = tb_sponsor.rename(columns={"completion_year": "year", "Funder Type": "country"}, errors="raise")
+    tb_interventions = tb_interventions.rename(columns={"completion_year": "year"}, errors="raise")
+    tb_study_type = tb_study_type.rename(columns={"completion_year": "year", "Study Type": "country"}, errors="raise")
+    tb_purpose = tb_purpose.rename(columns={"completion_year": "year", "primary_purpose": "country"}, errors="raise")
+    tb_status = tb_status.rename(columns={"start_year": "year", "Study Status": "country"}, errors="raise")
+    tb_results = tb_results.rename(columns={"start_year": "year", "Study Results": "country"}, errors="raise")
+    tb_length = tb_length.rename(columns={"completion_year": "year", "Phases": "country"}, errors="raise")
+
+    # set the index to year and country for each table
+    tb_trials = tb_trials.format(["year", "country"], short_name="trials_per_year")
+    tb_sponsor = tb_sponsor.format(["year", "country"], short_name="sponsor_per_year")
+    tb_interventions = tb_interventions.format(["year"], short_name="interventions_per_year")
+    tb_study_type = tb_study_type.format(["year", "country"], short_name="study_type_per_year")
+    tb_purpose = tb_purpose.format(["year", "country"], short_name="purpose_per_year")
+    tb_status = tb_status.format(["year", "country"], short_name="status_per_year")
+    tb_results = tb_results.format(["year", "country"], short_name="results_per_year")
+    tb_length = tb_length.format(["year", "country"], short_name="length_per_year")
+
+    return (
+        tb_trials,
+        tb_sponsor,
+        tb_interventions,
+        tb_study_type,
+        tb_purpose,
+        tb_status,
+        tb_results,
+        tb_length,
+    )
--- etl/steps/data/grapher/health/2025-07-28/clinical_trials.py
+++ etl/steps/data/grapher/health/2025-07-28/clinical_trials.py
@@ -13,14 +13,34 @@ def run() -> None:
     # Load garden dataset.
     ds_garden = paths.load_dataset("clinical_trials")
 
-    # Read table from garden dataset.
-    tb = ds_garden.read("clinical_trials", reset_index=False)
+    # Read tables from garden dataset.
 
+    tb_trials_per_year = ds_garden.read("trials_per_year", reset_index=False)
+    tb_sponsor_per_year = ds_garden.read("sponsor_per_year", reset_index=False)
+    tb_study_type_per_year = ds_garden.read("study_type_per_year", reset_index=False)
+    tb_purpose_per_year = ds_garden.read("purpose_per_year", reset_index=False)
+    tb_status_per_year = ds_garden.read("status_per_year", reset_index=False)
+    tb_length_per_year = ds_garden.read("length_per_year", reset_index=False)
+    tb_intervention_per_year = ds_garden.read("interventions_per_year")
+
+    tb_intervention_per_year["country"] = "World"
+
+    tb_intervention_per_year = tb_intervention_per_year.format(["year", "country"])
+
+    tables_ls = [
+        tb_trials_per_year,
+        tb_sponsor_per_year,
+        tb_intervention_per_year,
+        tb_study_type_per_year,
+        tb_purpose_per_year,
+        tb_status_per_year,
+        tb_length_per_year,
+    ]
     #
     # Save outputs.
     #
     # Initialize a new grapher dataset.
-    ds_grapher = paths.create_dataset(tables=[tb], default_metadata=ds_garden.metadata)
+    ds_grapher = paths.create_dataset(tables=tables_ls, default_metadata=ds_garden.metadata)
 
     # Save grapher dataset.
     ds_grapher.save()
