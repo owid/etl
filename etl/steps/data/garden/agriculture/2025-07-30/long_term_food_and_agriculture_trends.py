efa0631b4ff992647995957d594695d9398d8864
Pablo Rosado <pabloarosado@gmail.com>
2025-07-31 16:14:10 +0200

    Fix population units and add sanity checks

--- etl/steps/data/garden/agriculture/2025-07-30/long_term_food_and_agriculture_trends.py
+++ etl/steps/data/garden/agriculture/2025-07-30/long_term_food_and_agriculture_trends.py
@@ -190,14 +190,100 @@ def create_corrected_lists_of_region_members(tb, tb_regions):
     return regions
 
 
+def additional_debugging_checks():
+    # This function loads the original meadow steps for FBSH and FBS, and combines them doing minimal processing.
+    # This function is only used to ensure that the results do not depend on any possibly additional processing that happens in the FAOSTAT garden step.
+    # NOTE: For this function to work, you will need to add some dependencies to the current step in the DAG, namely:
+    # - data://meadow/faostat/2025-03-17/faostat_fbs
+    # - data://meadow/faostat/2025-03-17/faostat_fbsh
+    # - data://garden/demography/2024-07-15/population
+    # - data://garden/wb/2025-07-01/income_groups
+    from owid.datautils.dataframes import combine_two_overlapping_dataframes
+
+    from etl.data_helpers import geo
+    from etl.paths import STEP_DIR
+
+    # Load FBSH and FBS datasets directly from meadow.
+    ds_fbs = paths.load_dataset("faostat_fbs")
+    tb_fbs = ds_fbs.read("faostat_fbs")
+    ds_fbsh = paths.load_dataset("faostat_fbsh")
+    tb_fbsh = ds_fbsh.read("faostat_fbsh")
+    tb_fbs = tb_fbs[(tb_fbs["element_code"].isin([511, 664])) & (tb_fbs["item_code"].isin([2501, 2901]))].rename(
+        columns={"area": "country"}
+    )
+    tb_fbs["col"] = tb_fbs["item"] + "-" + tb_fbs["element"]
+    tb_fbs = (
+        tb_fbs[["country", "year", "col", "value"]]
+        .pivot(index=["country", "year"], columns="col", join_column_levels_with="_")
+        .rename(
+            columns={
+                "value_Grand Total-Food supply (kcal/capita/day)": "food_supply",
+                "value_Population-Total Population - Both sexes": "population",
+            }
+        )
+    )
+    tb_fbsh = tb_fbsh[(tb_fbsh["element_code"].isin([511, 664])) & (tb_fbsh["item_code"].isin([2501, 2901]))].rename(
+        columns={"area": "country"}
+    )
+    tb_fbsh["col"] = tb_fbsh["item"] + "-" + tb_fbsh["element"]
+    tb_fbsh = (
+        tb_fbsh[["country", "year", "col", "value"]]
+        .pivot(index=["country", "year"], columns="col", join_column_levels_with="_")
+        .rename(
+            columns={
+                "value_Grand Total-Food supply (kcal/capita/day)": "food_supply",
+                "value_Population-Total Population - Both sexes": "population",
+            }
+        )
+    )
+    countries_file = STEP_DIR / f"data/garden/faostat/{ds_fbs.version}/faostat.countries.json"
+    excluded_countries_file = STEP_DIR / f"data/garden/faostat/{ds_fbs.version}/faostat.excluded_countries.json"
+    tb_fbs = geo.harmonize_countries(
+        tb_fbs,
+        countries_file=countries_file,
+        excluded_countries_file=excluded_countries_file,
+        warn_on_unknown_excluded_countries=False,
+        warn_on_unused_countries=False,
+        warn_on_missing_countries=True,
+    )
+    tb_fbsh = geo.harmonize_countries(
+        tb_fbsh,
+        countries_file=countries_file,
+        excluded_countries_file=excluded_countries_file,
+        warn_on_unknown_excluded_countries=False,
+        warn_on_unused_countries=False,
+        warn_on_missing_countries=True,
+    )
+    tb_fbsc = combine_two_overlapping_dataframes(tb_fbs, tb_fbsh, index_columns=["country", "year"])
+    tb_population = (
+        paths.load_dataset("population")
+        .read("population")[["country", "year", "population"]]
+        .rename(columns={"population": "owid_population"})
+    )
+    tb_population["owid_population"] /= 1000
+
+    # An additional check is to use OWID population instead of FAO population, to see if things change.
+    # tb_fbsc = tb_fbsc.merge(tb_population, on=["country", "year"], how="left")
+    # tb_fbsc["population"] = tb_fbsc["owid_population"].astype(float).fillna(tb_fbsc["population"])
+    # Also, visually inspect the difference between FAO and OWID population for USSR.
+    # check = tb_fbsc[tb_fbsc["country"].isin(["USSR"] + regions["USSR"])].groupby("year", as_index=False).agg({"population": "sum", "owid_population": "sum"})[["year", "population", "owid_population"]].melt(id_vars=["year"])
+    # px.line(check, x="year", y="value", color="variable", markers=True)
+    tb_fbsc["food_supply"] *= tb_fbsc["population"]
+    ds_regions = paths.load_dataset("regions")
+    ds_income_groups = paths.load_dataset("income_groups")
+    tb_fbsc = geo.add_regions_to_table(tb=tb_fbsc, ds_regions=ds_regions, ds_income_groups=ds_income_groups)
+
+    return tb_fbsc
+
+
 def plot_corrected_data(tb):
     import plotly.express as px
 
-    for region in REGIONS:
+    for region in sorted(REGIONS):
         regions_to_plot = [f"{region} (FAO)", f"{region} (corrected)"]
         # Uncomment to plot the OWID region (it shouldn't be significantly different to FAO's region).
         # regions_to_plot = [region, f"{region} (FAO)", f"{region} (corrected)"]
-        for column in ["food_supply", "population", "agricultural_land"]:
+        for column in ["agricultural_land", "food_supply", "population"]:
             unit = {"food_supply": "daily kilocalories", "population": "people", "agricultural_land": "hectares"}[
                 column
             ]
@@ -222,7 +308,7 @@ def plot_corrected_data(tb):
 
                 # Uncomment to export as png (after doing "uv add kaleido").
                 # from pathlib import Path
-                # fig.write_image(Path.home() / "Downloads" / f"{region}_{column}.png", scale=0.6)
+                # fig.write_image(Path.home() / "Downloads" / "plots_corrected" / f"{region}_{column}.png", scale=2)
 
 
 def run() -> None:
@@ -247,6 +333,12 @@ def run() -> None:
     #
     # Select and rename columns in food balances data.
     tb_fbsc = tb_fbsc[list(COLUMNS_FBSC)].rename(columns=COLUMNS_FBSC, errors="raise")
+
+    # Uncomment the following line to replace the loaded FBSC dataset from garden, and instead load the original FBSH and FBS datasets from meadow. This ensures that the results are not affected by any further processing in the FAOSTAT garden step.
+    # tb_fbsc = additional_debugging_checks()
+
+    # Correct units of population data.
+    tb_fbsc["population"] *= 1000
 
     # Select and rename columns in land use data.
     tb_rl = tb_rl[list(COLUMNS_RL)].rename(columns=COLUMNS_RL, errors="raise")
