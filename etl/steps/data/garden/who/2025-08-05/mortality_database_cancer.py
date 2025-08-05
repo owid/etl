66c665adaf28d092627fdb2f6464641e374e5246
spoonerf <spoonerfiona@gmail.com>
2025-08-05 16:00:51 +0100

    update demographic attributes check

--- etl/steps/data/garden/who/2025-08-05/mortality_database_cancer.py
+++ etl/steps/data/garden/who/2025-08-05/mortality_database_cancer.py
@@ -177,34 +177,33 @@ def _validate_cancer_mortality_values(tb: Table) -> None:
 
 def _validate_demographic_dimensions(tb: Table) -> None:
     """Validate sex and age group categories."""
-    expected_sex_categories = {"Both sexes", "Females", "Males", "Unknown sex"}
+    expected_sex_categories = {"All", "Female", "Male", "Unknown"}
     actual_sex = set(tb["sex"].unique())
     unexpected_sex = actual_sex - expected_sex_categories
-    if unexpected_sex:
-        log.warning("Unexpected sex categories", categories=list(unexpected_sex))
+    assert not unexpected_sex, f"Unexpected sex categories: {unexpected_sex}"
 
     expected_age_groups = {
-        "all ages",
-        "less than 1 year",
-        "1-4 years",
-        "5-9 years",
-        "10-14 years",
-        "15-19 years",
-        "20-24 years",
-        "25-29 years",
-        "30-34 years",
-        "35-39 years",
-        "40-44 years",
-        "45-49 years",
-        "50-54 years",
-        "55-59 years",
-        "60-64 years",
-        "65-69 years",
-        "70-74 years",
-        "75-79 years",
-        "80-84 years",
-        "over 85 years",
-        "Unknown age",
+        "[All]",
+        "[0]",
+        "[1-4]",
+        "[5-9]",
+        "[10-14]",
+        "[15-19]",
+        "[20-24]",
+        "[25-29]",
+        "[30-34]",
+        "[35-39]",
+        "[40-44]",
+        "[45-49]",
+        "[50-54]",
+        "[55-59]",
+        "[60-64]",
+        "[65-69]",
+        "[70-74]",
+        "[75-79]",
+        "[80-84]",
+        "[85+]",
+        "[Unknown]",
     }
     actual_ages = set(tb["age_group"].unique())
     unexpected_ages = actual_ages - expected_age_groups
