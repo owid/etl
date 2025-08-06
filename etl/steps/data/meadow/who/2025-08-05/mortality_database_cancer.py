b2bd0691b8e1079e311a6630094d3a6d2a1d3de1
Fiona Spooner <fiona@ourworldindata.org>
2025-08-06 11:07:33 +0100

    📊 WHO Mortality Database Cancer (#4806)
    * 📊 who mortality db cancer
    
    * add new steps
    
    * fix replace
    
    * wip
    
    * organize imports
    
    * organize imports
    
    * update demographic attributes check
    
    * Update etl/steps/data/garden/who/2025-08-05/mortality_database_cancer.py
    
    Co-authored-by: graphite-app[bot] <96075541+graphite-app[bot]@users.noreply.github.com>
    
    * fillna
    
    * fillna
    
    * update year
    
    * update icd codes dataset too
    
    * archive and update
    
    * archive and update
    
    * update dependency
    
    ---------
    
    Co-authored-by: graphite-app[bot] <96075541+graphite-app[bot]@users.noreply.github.com>

---
+++ etl/steps/data/meadow/who/2025-08-05/mortality_database_cancer.py
@@ -0,0 +1,34 @@
+"""Load a snapshot and create a meadow dataset."""
+
+from etl.helpers import PathFinder
+
+# Get paths and naming conventions for current step.
+paths = PathFinder(__file__)
+
+
+def run() -> None:
+    #
+    # Load inputs.
+    #
+    # Retrieve snapshot.
+    snap = paths.load_snapshot("mortality_database_cancer.csv")
+
+    # Load data from snapshot.
+    tb = snap.read(safe_types=False)
+
+    #
+    # Process data.
+    #
+    # Create a new table and ensure all columns are snake-case.
+    tb = tb.drop(columns=["Region Code", "Country Code", "Age group code", "Region Name"])
+    tb = tb.rename(columns={"Country Name": "country"}, errors="raise")
+    tb = tb.format(["country", "year", "sex", "age_group", "cause"])
+
+    #
+    # Save outputs.
+    #
+    # Create a new meadow dataset with the same metadata as the snapshot.
+    ds_meadow = paths.create_dataset(tables=[tb], default_metadata=snap.metadata, repack=False)
+
+    # Save changes in the new garden dataset.
+    ds_meadow.save()
