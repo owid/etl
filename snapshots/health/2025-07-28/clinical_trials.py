c4e1cd8e4d588698bf6d9addf15da1b8b4c56c50
Tuna Acisu <tuna.acisu@ourworldindata.com>
2025-08-14 20:27:24 +0200

    :sparkles: review comments

--- snapshots/health/2025-07-28/clinical_trials.py
+++ snapshots/health/2025-07-28/clinical_trials.py
@@ -8,11 +8,16 @@ from pathlib import Path
 import click
 import pandas as pd
 import requests
+from structlog import get_logger
 from tqdm.auto import tqdm
 
 from etl.snapshot import Snapshot
 
+logger = get_logger()
+
 CT_BASE_URL = "https://beta-ut.clinicaltrials.gov/api/v2/studies"
+
+CT_SIZE_URL = "https://beta-ut.clinicaltrials.gov/api/v2/stats/size"
 
 PAGE_SIZE = 1000
 FORMAT = "csv"
@@ -23,8 +28,10 @@ SNAPSHOT_VERSION = Path(__file__).parent.name
 
 def fetch_full_clinical_trials_data(page_size: int) -> list:
     chunks = []
+    total_size = fetch_clinical_trials_size()
+    logger.info("Total clinical trials", total_size=total_size)
     next_page_token = None
-    pbar = tqdm(total=546474, desc="Fetching data", unit="entries")
+    pbar = tqdm(total=total_size, desc="Fetching data", unit="entries")
     while True:
         data_txt, next_page_token = fetch_next_page_ct(page_size, next_page_token=next_page_token)
         chunks.append(data_txt)
@@ -34,6 +41,16 @@ def fetch_full_clinical_trials_data(page_size: int) -> list:
         time.sleep(0.1)  # To avoid hitting rate limits
     pbar.close()
     return chunks
+
+
+def fetch_clinical_trials_size() -> int:
+    response = requests.get(CT_SIZE_URL)
+
+    if response.status_code == 200:
+        size_data = response.json()
+        return size_data.get("totalStudies", None)
+    else:
+        raise Exception(f"Error fetching size data: {response.status_code} - {response.text}")
 
 
 def fetch_next_page_ct(page_size, next_page_token=None):
@@ -53,7 +70,7 @@ def run(upload: bool) -> None:
     # Initialize a new snapshot.
     snap = Snapshot(f"health/{SNAPSHOT_VERSION}/clinical_trials.csv")
 
-    print(f"Fetching clinical trials data from {CT_BASE_URL}...")
+    logger.info("Fetching data from ClinicalTrials...", API_URL=CT_BASE_URL)
     chunks = fetch_full_clinical_trials_data(PAGE_SIZE)
 
     full_tb = pd.read_csv(StringIO("".join(chunks)))
