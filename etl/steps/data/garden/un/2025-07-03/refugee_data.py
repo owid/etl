f07caae59898876956a10d406eceb8d4f9f2f40b
Tuna Acisu <tuna.acisu@ourworldindata.com>
2025-08-21 14:04:21 +0100

    :lipstick: update data, improve code and improve metadata

--- etl/steps/data/garden/un/2025-07-03/refugee_data.py
+++ etl/steps/data/garden/un/2025-07-03/refugee_data.py
@@ -8,6 +8,8 @@ from etl.helpers import PathFinder
 # Get paths and naming conventions for current step.
 paths = PathFinder(__file__)
 
+VERBOSE = False
+
 
 def run() -> None:
     #
@@ -30,14 +32,29 @@ def run() -> None:
     tb["ooc"] = tb["ooc"].where(tb["year"] >= 2018, pd.NA)
     tb["returned_idps"] = tb["returned_idps"].where(tb["year"] >= 1997, pd.NA)
 
-    # TODO: check for refugees in the same country of origin and asylum
-    # TODO: check that idps are the same for asylum and origin
+    if VERBOSE:
+        # there are 85 rows with refugees who have the same country of origin and asylum
+        r_same = tb[(tb["country_of_origin"] == tb["country_of_asylum"]) & (tb["refugees"] > 0)]
+        print(len(r_same), "rows with refugees who have the same country of origin and asylum")
+        # if country of origin and asylum are different idps are always 0 or nan
+        idp = tb[(tb["country_of_origin"] != tb["country_of_asylum"]) & (tb["idps"] > 0)]
+        print(len(idp), "rows with idps who have different country of origin and asylum")
 
-    tb_asylum = tb.copy()
-    tb_asylum = tb_asylum.rename(columns={"country_of_asylum": "country"})
-
-    tb_origin = tb.copy()
+    tb_origin = (
+        tb.drop(columns=["country_of_asylum"])
+        .groupby(["country_of_origin", "year"], observed=True)
+        .sum(min_count=1)  # type: ignore
+        .reset_index()
+    )
     tb_origin = tb_origin.rename(columns={"country_of_origin": "country"})
+
+    tb_asylum = (
+        tb.drop(columns=["country_of_origin"])
+        .groupby(["country_of_asylum", "year"], observed=True)
+        .sum(min_count=1)  # type: ignore
+        .reset_index()
+    )
+    tb_asylum = tb_asylum.rename(columns={"country_of_asylum": "country"})
 
     # harmonize countries
     tb_asylum = geo.harmonize_countries(
@@ -61,31 +78,28 @@ def run() -> None:
     tb_asylum["asylum_seekers_per_100k"] = tb_asylum["asylum_seekers"] / tb_asylum["population"] * 100_000
     tb_asylum["refugees_per_100k"] = tb_asylum["refugees"] / tb_asylum["population"] * 100_000
     tb_asylum["refugees_per_1000"] = tb_asylum["refugees"] / tb_asylum["population"] * 1000
-    tb_asylum["returned_refugees_per_100k"] = tb_asylum["returned_refugees"] / tb_asylum["population"] * 100_000
+    # stateless people always have "stateless" as country of origin
     tb_asylum["stateless_per_100k"] = tb_asylum["stateless"] / tb_asylum["population"] * 100_000
 
     # origin table
     tb_origin["asylum_seekers_per_100k"] = tb_origin["asylum_seekers"] / tb_origin["population"] * 100_000
     tb_origin["refugees_per_100k"] = (tb_origin["refugees"] / tb_origin["population"]) * 100_000
     tb_origin["refugees_per_1000"] = (tb_origin["refugees"] / tb_origin["population"]) * 1000
-    tb_origin["returned_refugees_per_100k"] = tb_origin["returned_refugees"] / tb_origin["population"] * 100_000
-    tb_origin["stateless_per_100k"] = tb_origin["stateless"] / tb_origin["population"] * 100_000
-    tb_origin["returned_idps_per_1000"] = tb_origin["returned_idps"] / tb_origin["population"] * 1000
+    # (returned) idps always have same origin and destination, so we only include them in origin table
+    tb_origin["idps_per_100k"] = tb_origin["idps"] / tb_origin["population"] * 100_000
+    tb_origin["returned_idps_per_100k"] = tb_origin["returned_idps"] / tb_origin["population"] * 100_000
 
-    # drop idps from asylum table as we have them in the origin table + drop population
+    # drop idps from asylum table as we have them in the origin table
     tb_asylum = tb_asylum.drop(columns=["idps", "returned_idps", "population"])
     tb_origin = tb_origin.drop(columns=["population"])
 
     # Replace unknown countries with either "Unknown Origin" or "Unknown Destination"
     tb_asylum["country"] = tb_asylum["country"].replace("Unknown", "Unknown Destination")
-    tb_asylum["country_of_orign"] = tb_asylum["country_of_origin"].replace("Unknown", "Unknown Origin")
-
     tb_origin["country"] = tb_origin["country"].replace("Unknown", "Unknown Origin")
-    tb_origin["country_of_asylum"] = tb_origin["country_of_asylum"].replace("Unknown", "Unknown Destination")
 
     # Improve table format.
-    tb_asylum = tb_asylum.format(["country", "country_of_origin", "year"], short_name="refugee_data_asylum")
-    tb_origin = tb_origin.format(["country", "country_of_asylum", "year"], short_name="refugee_data_origin")
+    tb_asylum = tb_asylum.format(["country", "year"], short_name="refugee_data_asylum")
+    tb_origin = tb_origin.format(["country", "year"], short_name="refugee_data_origin")
 
     #
     # Save outputs.
--- etl/steps/data/grapher/un/2025-07-03/refugee_data.py
+++ etl/steps/data/grapher/un/2025-07-03/refugee_data.py
@@ -14,13 +14,14 @@ def run() -> None:
     ds_garden = paths.load_dataset("refugee_data")
 
     # Read table from garden dataset.
-    tb = ds_garden.read("refugee_data", reset_index=False)
+    tb_asylum = ds_garden.read("refugee_data_asylum", reset_index=False)
+    tb_origin = ds_garden.read("refugee_data_origin", reset_index=False)
 
     #
     # Save outputs.
     #
     # Initialize a new grapher dataset.
-    ds_grapher = paths.create_dataset(tables=[tb], default_metadata=ds_garden.metadata)
+    ds_grapher = paths.create_dataset(tables=[tb_asylum, tb_origin], default_metadata=ds_garden.metadata)
 
     # Save grapher dataset.
     ds_grapher.save()
