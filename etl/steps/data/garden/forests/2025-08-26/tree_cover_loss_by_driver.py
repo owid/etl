2f53a7d9b42b5876bb09b124a5c7635c144c1d5b
spoonerf <spoonerfiona@gmail.com>
2025-08-27 11:54:36 +0100

    add garden meadow

---
+++ etl/steps/data/garden/forests/2025-08-26/tree_cover_loss_by_driver.py
@@ -0,0 +1,62 @@
+"""Load a meadow dataset and create a garden dataset."""
+
+from owid.catalog import processing as pr
+from etl.data_helpers import geo
+from etl.helpers import PathFinder
+
+# Get paths and naming conventions for current step.
+paths = PathFinder(__file__)
+# List of regions to be used in the dataset - continents, world and income groups
+REGIONS = [
+    "Africa",
+    "Asia",
+    "Europe",
+    "North America",
+    "Oceania",
+    "South America",
+    "Low-income countries",
+    "Upper-middle-income countries",
+    "Lower-middle-income countries",
+    "High-income countries",
+    "World",
+]
+
+
+def run() -> None:
+    #
+    # Load inputs.
+    #
+    # Load meadow dataset.
+    ds_meadow = paths.load_dataset("tree_cover_loss_by_driver")
+    # Load regions dataset.
+    ds_regions = paths.load_dataset("regions")
+    # Read table from meadow dataset.
+    ds_income_groups = paths.load_dataset("income_groups")
+    tb = ds_meadow["tree_cover_loss_by_driver"].reset_index()
+
+    #
+    # Process data.
+    #
+    tb = geo.harmonize_countries(
+        df=tb, countries_file=paths.country_mapping_path, excluded_countries_file=paths.excluded_countries_path
+    )
+    tb_total = tb.groupby(["country","year"]).sum(numeric_only=True).reset_index()
+    tb_total["category"] = "Total"
+
+    tb = pr.concat([tb, tb_total], ignore_index=True)
+
+    tb = geo.add_regions_to_table(
+        tb, ds_regions=ds_regions, ds_income_groups=ds_income_groups, regions=REGIONS, min_num_values_per_year=1
+    )
+    tb = tb.format(["country", "year", "category"])
+
+    #
+    # Save outputs.
+    #
+    # Create a new garden dataset with the same metadata as the meadow dataset.
+    ds_garden = paths.create_dataset(
+        tables=[tb], check_variables_metadata=True, default_metadata=ds_meadow.metadata
+    )
+
+    # Save changes in the new garden dataset.
+    ds_garden.save()
