657c33d74df4fe2ac4313ebf822234cd1d29057b
spoonerf <spoonerfiona@gmail.com>
2025-09-02 17:12:33 +0100

    format

--- etl/steps/data/grapher/maternal_mortality/2025-09-02/maternal_mortality.py
+++ etl/steps/data/grapher/maternal_mortality/2025-09-02/maternal_mortality.py
@@ -15,18 +15,11 @@ def run() -> None:
 
     # Read table from garden dataset.
     tb = ds_garden["maternal_mortality"]
-
-    #
-    # Process data.
-    #
-
     #
     # Save outputs.
     #
     # Create a new grapher dataset with the same metadata as the garden dataset.
-    ds_grapher = paths.create_dataset(
-        tables=[tb], check_variables_metadata=True, default_metadata=ds_garden.metadata
-    )
+    ds_grapher = paths.create_dataset(tables=[tb], check_variables_metadata=True, default_metadata=ds_garden.metadata)
 
     # Save changes in the new grapher dataset.
     ds_grapher.save()
