62eb062709db807e0116428ac852456f2bcd88ee
Pablo Arriagada <paarriagadap@gmail.com>
2025-09-05 16:33:12 +0100

    :sparkles: add regions in grapher

--- etl/steps/data/grapher/un/2025-08-12/ilostat.py
+++ etl/steps/data/grapher/un/2025-08-12/ilostat.py
@@ -15,12 +15,13 @@ def run() -> None:
 
     # Read table from garden dataset.
     tb = ds_garden.read("ilostat", reset_index=False)
+    tb_regions = ds_garden.read("regions", reset_index=False)
 
     #
     # Save outputs.
     #
     # Initialize a new grapher dataset.
-    ds_grapher = paths.create_dataset(tables=[tb], default_metadata=ds_garden.metadata)
+    ds_grapher = paths.create_dataset(tables=[tb, tb_regions], default_metadata=ds_garden.metadata)
 
     # Save grapher dataset.
     ds_grapher.save()
