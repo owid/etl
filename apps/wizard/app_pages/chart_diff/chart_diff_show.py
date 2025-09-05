7baad1759c4482b9aa1727970333821c88fbf40c
Marigold <mojmir.vinkler@gmail.com>
2025-09-05 07:20:49 +0200

    wip

--- apps/wizard/app_pages/chart_diff/chart_diff_show.py
+++ apps/wizard/app_pages/chart_diff/chart_diff_show.py
@@ -449,7 +449,7 @@ class ChartDiffShow:
                     model=MODEL_DEFAULT,
                     messages=messages,  # type: ignore
                     temperature=0.15,
-                    max_tokens=1000,
+                    max_completion_tokens=1000,
                     stream=True,
                 )
                 response = cast(str, st.write_stream(stream))
