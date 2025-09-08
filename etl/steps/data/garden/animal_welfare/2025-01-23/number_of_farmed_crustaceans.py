f19026e420ee512514b65365539a68589155e8ff
Pablo Rosado <pabloarosado@gmail.com>
2025-09-08 17:40:00 +0200

    Undo changes to farmed crustaceans data (rethink changes in a separate PR)

--- etl/steps/data/garden/animal_welfare/2025-01-23/number_of_farmed_crustaceans.py
+++ etl/steps/data/garden/animal_welfare/2025-01-23/number_of_farmed_crustaceans.py
@@ -1,7 +1,8 @@
 """Load a meadow dataset and create a garden dataset."""
 
-from owid.catalog import Table
+from owid.catalog import Dataset, Table
 
+from etl.data_helpers import geo
 from etl.helpers import PathFinder
 
 # Get paths and naming conventions for current step.
@@ -32,6 +33,17 @@ REGIONS_TO_ADD = [
 ]
 
 
+def add_per_capita_variables(tb: Table, ds_population: Dataset) -> Table:
+    tb = geo.add_population_to_table(tb, ds_population=ds_population, warn_on_missing_countries=False)
+    tb["n_farmed_crustaceans_low_per_capita"] = tb["n_farmed_crustaceans_low"] / tb["population"]
+    tb["n_farmed_crustaceans_per_capita"] = tb["n_farmed_crustaceans"] / tb["population"]
+    tb["n_farmed_crustaceans_high_per_capita"] = tb["n_farmed_crustaceans_high"] / tb["population"]
+    # Drop population column.
+    tb = tb.drop(columns=["population"], errors="raise")
+
+    return tb
+
+
 def sanity_check_outputs(tb: Table) -> None:
     # Check that the total agrees (within a few percent) with the sum of aggregates from each continent, for non per capita columns.
     tb = tb[[column for column in tb.columns if "per_capita" not in column]].copy()
@@ -52,19 +64,23 @@ def run() -> None:
     ds_meadow = paths.load_dataset("number_of_farmed_crustaceans")
     tb = ds_meadow.read("number_of_farmed_crustaceans")
 
+    # Load regions dataset.
+    ds_regions = paths.load_dataset("regions")
+
+    # Load income groups dataset.
+    ds_income_groups = paths.load_dataset("income_groups")
+
+    # Load population dataset.
+    ds_population = paths.load_dataset("population")
+
     #
     # Process data.
     #
     # Select and rename columns.
     tb = tb[list(COLUMNS)].rename(columns=COLUMNS, errors="raise")
 
-    ####################################################################################################################
-    # TODO: Remove, temporary code. Uncomment for testing. Here we introduce some arbitrary nans to check how region aggregates change.
-    # tb.loc[(tb["country"] == "China") & (tb["year"] == 2020), "n_farmed_crustaceans_low"] = None
-    ####################################################################################################################
-
     # Harmonize country names.
-    tb = paths.regions.harmonize_names(tb=tb)
+    tb = geo.harmonize_countries(df=tb, countries_file=paths.country_mapping_path)
 
     # There are cases of "<1" in the data. Replace those with the average value in that range, namely 0.5.
     # Convert to float and change from million to units.
@@ -72,17 +88,16 @@ def run() -> None:
         tb[column] = tb[column].replace("< 1", "0.5").astype("Float64") * 1e6
 
     # Add region aggregates.
-    tb_agg = paths.region_aggregator(regions=REGIONS_TO_ADD)
-    tb = tb_agg.add_aggregates(tb=tb, min_num_values_per_year=1)
+    tb = geo.add_regions_to_table(
+        tb=tb,
+        ds_regions=ds_regions,
+        ds_income_groups=ds_income_groups,
+        regions=REGIONS_TO_ADD,
+        min_num_values_per_year=1,
+    )
 
     # Add per capita number of farmed decapod crustaceans.
-    tb = tb_agg.add_per_capita(
-        tb=tb,
-        columns=["n_farmed_crustaceans_low", "n_farmed_crustaceans", "n_farmed_crustaceans_high"],
-        warn_on_missing_countries=False,
-        # TODO: Remove, temporary code. Uncomment to add columns of *_informed_population.
-        # drop_population=False,
-    )
+    tb = add_per_capita_variables(tb=tb, ds_population=ds_population)
 
     # Run sanity checks on outputs.
     sanity_check_outputs(tb=tb)
