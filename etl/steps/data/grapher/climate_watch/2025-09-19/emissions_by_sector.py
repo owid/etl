35d317fd98bae3bc27df17a0c674ea4842ce5692
Pablo Rosado <pabloarosado@gmail.com>
2025-09-19 12:33:19 +0200

    Update Climate Watch data

---
+++ etl/steps/data/grapher/climate_watch/2025-09-19/emissions_by_sector.py
@@ -0,0 +1,49 @@
+"""Load a garden dataset and create a grapher dataset."""
+
+from etl.helpers import PathFinder
+
+# Get paths and naming conventions for current step.
+paths = PathFinder(__file__)
+
+# Convert million tonnes to tonnes.
+MT_TO_T = 1e6
+
+
+def run() -> None:
+    #
+    # Load inputs.
+    #
+    # Load garden dataset.
+    ds_garden = paths.load_dataset("emissions_by_sector")
+
+    #
+    # Process data.
+    #
+    # Process each table in the dataset.
+    tables = []
+    for table_name in ds_garden.table_names:
+        tb = ds_garden[table_name].copy()
+
+        # Drop unnecessary columns.
+        tb = tb.drop(columns=["population"], errors="raise")
+
+        # For convenience, change units from "million tonnes" to "tonnes" and multiply all variables by a million.
+        # Doing this, grapher will know when to use the word "million" and when to use "billion".
+        for column in tb.columns:
+            if tb[column].metadata.unit == "million tonnes":
+                tb[column].metadata.unit = "tonnes"
+                tb[column].metadata.short_unit = "t"
+                tb[column] *= MT_TO_T
+                tb[column].metadata.description_short = tb[column].metadata.description_short.replace(
+                    "million tonnes", "tonnes"
+                )
+
+        # Add current table to the list.
+        tables.append(tb)
+
+    #
+    # Save outputs.
+    #
+    # Create a new grapher dataset with the same metadata as the garden dataset.
+    ds_grapher = paths.create_dataset(default_metadata=ds_garden.metadata, tables=tables)
+    ds_grapher.save()
