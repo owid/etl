47d89eb3f18ed34cce3276f112f6f7a23bdb8b1c
Veronika Samborska <veronika@ourworldindata.org>
2025-09-22 17:00:47 +0200

    trade: add World exports since 1800s (in constant prices)

---
+++ etl/steps/data/garden/trade/2025-09-22/historic_exports.py
@@ -0,0 +1,33 @@
+"""Load a meadow dataset and create a garden dataset."""
+
+from etl.helpers import PathFinder
+
+# Get paths and naming conventions for current step.
+paths = PathFinder(__file__)
+
+
+def run() -> None:
+    #
+    # Load inputs.
+    #
+    # Load meadow dataset.
+    ds_meadow = paths.load_dataset("historic_exports")
+
+    # Read table from meadow dataset.
+    tb = ds_meadow.read("historic_exports")
+
+    #
+    # Process data.
+    #
+
+    # Improve table format.
+    tb = tb.format(["country", "year"])
+
+    #
+    # Save outputs.
+    #
+    # Initialize a new garden dataset.
+    ds_garden = paths.create_dataset(tables=[tb], default_metadata=ds_meadow.metadata)
+
+    # Save garden dataset.
+    ds_garden.save()
---
+++ etl/steps/data/meadow/trade/2025-09-22/historic_exports.py
@@ -0,0 +1,42 @@
+"""Load a snapshot and create a meadow dataset."""
+
+from etl.helpers import PathFinder
+
+# Get paths and naming conventions for current step.
+paths = PathFinder(__file__)
+
+
+def run() -> None:
+    #
+    # Load inputs.
+    #
+    # Retrieve snapshot.
+    snap = paths.load_snapshot("historic_exports.xlsx")
+
+    # Load data from snapshot.
+    tb = snap.read(sheet_name="volume")
+
+    #
+    # Process data.
+    #
+    tb = tb[["Unnamed: 0", "Trade (1913=100)", "Extrapolation 1815-1913"]]
+    tb["country"] = "World"
+
+    tb = tb.rename(
+        columns={
+            "Unnamed: 0": "year",
+            "Extrapolation 1815-1913": "historic_trade",
+        }
+    )
+
+    # Improve tables format.
+    tables = [tb.format(["country", "year"])]
+
+    #
+    # Save outputs.
+    #
+    # Initialize a new meadow dataset.
+    ds_meadow = paths.create_dataset(tables=tables, default_metadata=snap.metadata)
+
+    # Save meadow dataset.
+    ds_meadow.save()
