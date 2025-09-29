52417f45942577c929e7f462c2c353905aff2dfb
Fiona Spooner <fiona@ourworldindata.org>
2025-09-29 11:05:25 +0100

    📊 UN WPP data for off site (#5071)
    * 📊 more cologne nu wpp
    
    * get both jan and july pop data in
    
    * add mediun
    
    * adding medium
    
    * Update etl/steps/data/external/demography/latest/un_wpp_subset.py
    
    Co-authored-by: graphite-app[bot] <96075541+graphite-app[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: graphite-app[bot] <96075541+graphite-app[bot]@users.noreply.github.com>

--- snapshots/un/2024-07-12/un_wpp.py
+++ snapshots/un/2024-07-12/un_wpp.py
@@ -31,7 +31,8 @@ def main(upload: bool) -> None:
         # "un_wpp_population_medium.csv",
         # "un_wpp_population_high.csv",
         # "un_wpp_population_constant_fertility.csv",
-        "un_wpp_population_jan_estimates.csv",
+        # "un_wpp_population_jan_estimates.csv",
+        "un_wpp_population_jan_medium.csv",
         # Fertility
         # "un_wpp_fertility_single_age.csv",
         # "un_wpp_fertility.csv",
