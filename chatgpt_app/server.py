297ed8993ba4c29e300c74a73428b82de736f4e8
Marigold <mojmir.vinkler@gmail.com>
2025-10-20 18:50:39 +0200

    wip

--- chatgpt_app/server.py
+++ chatgpt_app/server.py
@@ -359,4 +359,4 @@ app.add_middleware(
 
 
 if __name__ == "__main__":
-    uvicorn.run("chatgpt_app.server:app", host="0.0.0.0", port=8001)
+    uvicorn.run("chatgpt_app.server:app", host="0.0.0.0", port=8081)
