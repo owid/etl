714ea21b1276e5c49703ddf90eb36c73f5827ea7
lucasrodes <lucasrodes@users.noreply.github.com>
2025-10-22 18:49:33 +0200

    remove dest_dir

--- etl/steps/data/grapher/hmd/2025-10-22/hmd.py
+++ etl/steps/data/grapher/hmd/2025-10-22/hmd.py
@@ -1,6 +1,6 @@
 """Load a garden dataset and create a grapher dataset."""
 
-from etl.helpers import PathFinder, create_dataset
+from etl.helpers import PathFinder
 
 # Get paths and naming conventions for current step.
 paths = PathFinder(__file__)
@@ -12,7 +12,7 @@ INDICATORS_RELEVANT_LT = [
 ]
 
 
-def run(dest_dir: str) -> None:
+def run() -> None:
     #
     # Load inputs.
     #
@@ -49,8 +49,10 @@ def run(dest_dir: str) -> None:
         tb_deaths_agg.format(["country", "year", "sex"]),
     ]
     # Create a new grapher dataset with the same metadata as the garden dataset.
-    ds_grapher = create_dataset(
-        dest_dir, tables=tables, check_variables_metadata=True, default_metadata=ds_garden.metadata
+    ds_grapher = paths.create_dataset(
+        tables=tables,
+        check_variables_metadata=True,
+        default_metadata=ds_garden.metadata,
     )
 
     # Save changes in the new grapher dataset.
