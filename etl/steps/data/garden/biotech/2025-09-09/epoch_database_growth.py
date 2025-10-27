6dc9f7b9ea956d91ea690d5e9155d2fa85ee5ba5
Tuna Acisu <tuna.acisu@ourworldindata.com>
2025-10-27 13:48:27 +0000

    :bar_chart: include key datasets

--- etl/steps/data/garden/biotech/2025-09-09/epoch_database_growth.py
+++ etl/steps/data/garden/biotech/2025-09-09/epoch_database_growth.py
@@ -1,22 +1,9 @@
-import datetime as dt
-
-import pandas as pd
+from owid.catalog import processing as pr
 
 from etl.helpers import PathFinder
 
 # Get paths and naming conventions for current step.
 paths = PathFinder(__file__)
-
-
-def get_maximum_per_year(year: int, df):
-    """Get the maximum value for a given year."""
-    start_year = dt.datetime(year, 1, 1)
-    end_year = dt.datetime(year, 12, 31)
-    df_year = df[(df["Date"] >= start_year) & (df["Date"] <= end_year)]
-    if not df_year.empty:
-        return df_year["Value"].max()
-    else:
-        return pd.NA
 
 
 def run() -> None:
@@ -38,32 +25,153 @@ def run() -> None:
     tb_ddbj = ds_meadow["ddbj"]
     tb_refseq = ds_meadow["refseq"]
 
-    tb_uni_prot["date"] = pd.to_datetime(tb_uni_prot["date"])
+    # data formats
 
-    tb_mgnify["date"] = pd.to_datetime(tb_mgnify["release"])
-
-    tb_alpha_fold["date"] = pd.to_datetime(tb_alpha_fold["release_time"])
-
-    tb_esm_atlas["date"] = pd.to_datetime(tb_esm_atlas["release"])
+    tb_uni_prot["year"] = tb_uni_prot["date"].dt.year
+    tb_mgnify["year"] = tb_mgnify["release"].dt.year
+    tb_pdb["year"] = tb_pdb["year"]  # YYYY
+    tb_alpha_fold["year"] = tb_alpha_fold["release_time"].dt.year
+    tb_esm_atlas["year"] = tb_esm_atlas["release_time"].dt.year
+    tb_ena["year"] = tb_ena["month"].dt.year
+    tb_gb_all["year"] = tb_gb_all["date"].dt.year
+    tb_gb_traditional["year"] = tb_gb_traditional["date"].dt.year
+    tb_ddbj["year"] = tb_ddbj["date"].dt.year
+    tb_refseq["year"] = tb_refseq["date"].dt.year
 
     tables = [
-        tb_uni_prot,
-        tb_mgnify,
-        tb_pdb,
-        tb_alpha_fold,
-        tb_esm_atlas,
-        tb_ena,
-        tb_gb_all,
-        tb_gb_traditional,
-        tb_ddbj,
-        tb_refseq,
+        {
+            "name": "uniprot",
+            "table": tb_uni_prot,
+            "value_cols": [
+                "uniprotkb",
+                "uniprotkb_trembl",
+                "uniprotkb_swiss_prot",
+                "uniref100",
+                "uniref90",
+                "uniref50",
+                "uniparc",
+            ],
+        },
+        {
+            "name": "mgnify",
+            "table": tb_mgnify,
+            "value_cols": [
+                "total_seq",
+                "total_clust",
+                "full_seq",
+                "full_clust",
+                "partial_seq",
+                "partial_clust",
+                "swiss_prot_seq",
+                "swiss_prot_clust",
+                "trembl_seq",
+                "trembl_clust",
+            ],
+        },
+        {
+            "name": "pdb",
+            "table": tb_pdb,
+            "value_cols": [
+                "total_number_of_entries_available",
+                "number_of_structures_released_annually",
+                "growth_rate__pct_per_year",
+            ],
+        },
+        {
+            "name": "alpha_fold",
+            "table": tb_alpha_fold,
+            "value_cols": [
+                "number_of_predicted_structures",
+            ],
+        },
+        {
+            "name": "esm_atlas",
+            "table": tb_esm_atlas,
+            "value_cols": [
+                "number_of_predicted_structures",
+            ],
+        },
+        {
+            "name": "ena",
+            "table": tb_ena,
+            "value_cols": [
+                "entries",
+                "nucleotides",
+            ],
+        },
+        {
+            "name": "gb_all",
+            "table": tb_gb_all,
+            "value_cols": [
+                "reported_sequences",
+                "bases",
+            ],
+        },
+        {
+            "name": "gb_traditional",
+            "table": tb_gb_traditional,
+            "value_cols": [
+                "reported_sequences",
+                "bases",
+            ],
+        },
+        {
+            "name": "ddbj",
+            "table": tb_ddbj,
+            "value_cols": [
+                "entries",
+                "bases",
+            ],
+        },
+        {
+            "name": "refseq",
+            "table": tb_refseq,
+            "value_cols": [
+                "taxons",
+                "nucleotides",
+                "amino_acids",
+                "records",
+            ],
+        },
     ]
+
+    # get maximum per year
+    harmonized_tables = []
+    for table_dict in tables:
+        # give value_cols prefix with table name
+        tb = table_dict["table"]
+        tb = tb.rename(columns={col: f"{table_dict['name']}_{col}" for col in table_dict["value_cols"]})
+        table_dict["value_cols"] = [f"{table_dict['name']}_{col}" for col in table_dict["value_cols"]]
+
+        harm_tb = tb.groupby("year")[table_dict["value_cols"]].max().reset_index()
+        harmonized_tables.append(harm_tb)
+
+    tb_databases = pr.multi_merge(
+        harmonized_tables,
+        on="year",
+        how="outer",
+    ).sort_values("year")
+
+    # filter on most important columns for first version
+    important_cols = [
+        "year",
+        "gb_all_reported_sequences",
+        "pdb_total_number_of_entries_available",
+        "uniprot_uniprotkb_swiss_prot",
+        "alpha_fold_number_of_predicted_structures",
+        "esm_atlas_number_of_predicted_structures",
+        "refseq_records",
+    ]
+    tb_databases = tb_databases[important_cols]
+
+    tb_databases = tb_databases.format(["year"], short_name="epoch_database_growth")
+
     #
     # Save outputs.
     #
     # Create a new garden dataset with the same metadata as the meadow dataset.
     ds_garden = paths.create_dataset(
-        tables=tables,
+        tables=[tb_databases],
         check_variables_metadata=True,
         default_metadata=ds_meadow.metadata,
     )
