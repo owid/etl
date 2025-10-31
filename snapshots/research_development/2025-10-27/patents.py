fde20181c33d1d8127cfd2b814b8fb23b0dee151
Tuna Acisu <50630133+antea04@users.noreply.github.com>
2025-10-31 11:57:24 +0000

    📊 Medicine Biotech patents (#4966)
    * 📊 Medicine Biotech patents
    
    * :bar_chart: add patent data
    
    * :bar_chart: add meadow
    
    * :bar_chart: harmonization
    
    * :sparkles: clean up metadata
    
    * :bug: fix formatting
    
    * :sparkles: metadata tweaking
    
    ---------
    
    Co-authored-by: Tuna Acisu <tuna.acisu@ourworldindata.com>

---
+++ snapshots/research_development/2025-10-27/patents.py
@@ -0,0 +1,18 @@
+"""Script to create a snapshot of dataset."""
+
+from etl.helpers import PathFinder
+
+paths = PathFinder(__file__)
+
+
+def run(upload: bool = True, path_to_file: str = None) -> None:
+    """Create a new snapshot.
+
+    Args:
+        upload: Whether to upload the snapshot to S3.
+        path_to_file: Path to local data file.
+    """
+    # Init Snapshot object
+    snap = paths.init_snapshot()
+    # Save snapshot from local file.
+    snap.create_snapshot(filename=path_to_file, upload=upload)
