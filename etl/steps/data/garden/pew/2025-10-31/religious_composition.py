a6fe6b45f9193049c0a3e12810c16520bc367df9
lucasrodes <lucasrodes@users.noreply.github.com>
2025-10-31 15:52:38 +0100

    garden

---
+++ etl/steps/data/garden/pew/2025-10-31/religious_composition.py
@@ -0,0 +1,38 @@
+"""Load a meadow dataset and create a garden dataset."""
+
+from etl.helpers import PathFinder
+
+# Get paths and naming conventions for current step.
+paths = PathFinder(__file__)
+
+
+def run() -> None:
+    #
+    # Load inputs.
+    #
+    # Load meadow dataset.
+    ds_meadow = paths.load_dataset("religious_composition")
+
+    # Read table from meadow dataset.
+    tb = ds_meadow.read("religious_composition")
+
+    #
+    # Process data.
+    #
+    # Harmonize country names.
+    tb = paths.regions.harmonize_names(tb=tb)
+
+    # Lower case religion names
+    tb["religion"] = tb["religion"].str.lower()
+
+    # Improve table format.
+    tb = tb.format(["country", "year", "religion"])
+
+    #
+    # Save outputs.
+    #
+    # Initialize a new garden dataset.
+    ds_garden = paths.create_dataset(tables=[tb], default_metadata=ds_meadow.metadata)
+
+    # Save garden dataset.
+    ds_garden.save()
