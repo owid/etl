1731fbecb540ca8a5d6a1ae1f2ef620b2c394a4c
Pablo Arriagada <paarriagadap@gmail.com>
2025-11-04 17:29:46 +0000

    :sparkles: delete unnecesary code

--- etl/steps/export/multidim/worldbank_wdi/latest/wdi.py
+++ etl/steps/export/multidim/worldbank_wdi/latest/wdi.py
@@ -13,25 +13,12 @@ def run() -> None:
     # Default collection config
     config = paths.load_collection_config()
 
-    # # Load grapher dataset.
-    # ds = paths.load_dataset("wdi")
-    # tb = ds.read("wdi", load_data=False)
-
     #
     # Create collection object
     #
     c = paths.create_collection(
         config=config,
         short_name="ilostat_national_vs_modeled",
-        # tb=tb,
-        # indicator_names=[
-        #     "sl_uem_totl_ne_zs",
-        #     "sl_uem_totl_zs",
-        #     "sl_tlf_cact_ne_zs",
-        #     "sl_tlf_cact_zs",
-        #     "sl_emp_totl_sp_ne_zs",
-        #     "sl_emp_totl_sp_zs",
-        # ],
     )
 
     #
