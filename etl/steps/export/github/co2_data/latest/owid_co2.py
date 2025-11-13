ecb7407a3fbbc975b9d7a1f4d4b6a8132b3b73ec
Pablo Rosado <pabloarosado@gmail.com>
2025-11-13 11:51:34 +0100

    Fix broken links and improve README

--- etl/steps/export/github/co2_data/latest/owid_co2.py
+++ etl/steps/export/github/co2_data/latest/owid_co2.py
@@ -103,7 +103,7 @@ Additionally, to construct indicators per capita, per GDP, and per unit energy, 
 - Regions (Our World in Data).
   - [Processing code](https://github.com/owid/etl/blob/master/etl/steps/data/garden/regions/{regions_version}/regions.py)
 - Population (Our World in Data based on [a number of different sources](https://ourworldindata.org/population-sources)).
-  - [Processing code](https://github.com/owid/etl/blob/master/etl/steps/data/garden/demography/{population_version}/population/__init__.py)
+  - [Processing code](https://github.com/owid/etl/blob/master/etl/steps/data/garden/demography/{population_version}/population.py)
 - Income groups (World Bank).
   - [Processing code](https://github.com/owid/etl/blob/master/etl/steps/data/garden/wb/{income_groups_version}/income_groups.py)
 - GDP (University of Groningen GGDC's Maddison Project Database, Bolt and van Zanden).
@@ -178,7 +178,7 @@ Additionally, to construct indicators per capita, per GDP, and per unit energy, 
 
 - **We standardize names of countries and regions.** Since the names of countries and regions are different in different data sources, we standardize all names in order to minimize data loss during data merges.
 - **We recalculate carbon emissions to CO2.** The primary data sources on CO2 emissions—the Global Carbon Project, for example—typically report emissions in tonnes of carbon. We have recalculated these figures as tonnes of CO2 using a conversion factor of 3.664.
-- **We calculate per capita figures.** All of our per capita figures are calculated from our metric `Population`, which is included in the complete dataset. These population figures are sourced from [Gapminder](http://gapminder.org) and the [UN World Population Prospects (UNWPP)](https://population.un.org/wpp/).
+- **We calculate per capita figures.** All of our per capita figures are calculated from our metric `Population`, which is included in the complete dataset.
 
 ## License
 
@@ -188,7 +188,7 @@ The data produced by third parties and made available by _Our World in Data_ is 
 
 ## Authors
 
-This data has been collected, aggregated, and documented by Hannah Ritchie, Max Roser, Edouard Mathieu, Bobbie Macdonald and Pablo Rosado.
+This data has been collected, aggregated, and documented by Pablo Rosado, Hannah Ritchie, Max Roser, Edouard Mathieu, and Bobbie Macdonald.
 
 The mission of *Our World in Data* is to make data and research on the world's largest problems understandable and accessible. [Read more about our mission](https://ourworldindata.org/about).
 
