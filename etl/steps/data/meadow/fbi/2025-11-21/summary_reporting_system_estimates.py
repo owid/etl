d9b1bb18065fd2b22c3d7be33d874113f10d5071
spoonerf <spoonerfiona@gmail.com>
2025-11-25 10:55:30 +0000

    sorting out data for 2017-2020

--- etl/steps/data/meadow/fbi/2025-11-21/summary_reporting_system_estimates.py
+++ etl/steps/data/meadow/fbi/2025-11-21/summary_reporting_system_estimates.py
@@ -1,5 +1,7 @@
 """Load a snapshot and create a meadow dataset."""
 
+from owid.catalog import processing as pr
+
 from etl.helpers import PathFinder
 
 # Get paths and naming conventions for current step.
@@ -17,10 +19,37 @@ def run() -> None:
     tb = snap.read()
     tb.loc[tb["state_abbr"].isna(), "state_abbr"] = "US"
     tb.loc[tb["state_abbr"] == "US", "state_name"] = "United States"
+    # Some commas in numeric columns
+    tb = tb.replace(",", "", regex=True)
+    # Filter to US only
+    tb_us = tb[tb["state_name"] == "United States"]
+    tb_us = tb_us.rename(columns={"state_name": "country"})
+    # Data for 2017-2020 is missing national values so we need to calculate them
+    assert (~tb_us["year"].isin([2017, 2018, 2019, 2020])).all()
 
-    tb = tb[tb["state_name"] == "United States"]
-    tb = tb.rename(columns={"state_name": "country"})
-    #
+    tb_missing_years = tb[tb["year"].isin([2017, 2018, 2019, 2020])]
+    cols = [
+        "population",
+        "violent_crime",
+        "homicide",
+        "rape_legacy",
+        "rape_revised",
+        "robbery",
+        "aggravated_assault",
+        "property_crime",
+        "burglary",
+        "larceny",
+        "motor_vehicle_theft",
+    ]
+
+    tb_missing_years = tb_missing_years.copy()  # avoid SettingWithCopyWarning
+
+    tb_missing_years.loc[:, cols] = tb_missing_years.loc[:, cols].apply(pr.to_numeric, errors="coerce")
+    tb_missing_years = tb_missing_years.groupby("year").sum(numeric_only=True).reset_index()
+    tb_missing_years["country"] = "United States"
+    tb_missing_years["state_abbr"] = "US"
+    tb = pr.concat([tb_us, tb_missing_years], ignore_index=True)
+
     # Process data.
     #
     # Improve tables format.
