f0dc6e152325211bbf9894f97254bf412e1dec57
Pablo Arriagada <paarriagadap@gmail.com>
2025-12-15 18:03:11 +0000

    :construction: poverty MDim

---
+++ etl/steps/export/multidim/lis/latest/lis_poverty.py
@@ -0,0 +1,69 @@
+"""Load a meadow dataset and create a garden dataset."""
+
+from etl.helpers import PathFinder
+
+# Get paths and naming conventions for current step.
+paths = PathFinder(__file__)
+
+# Define indicators to use
+INDICATOR_NAMES = [
+    "headcount_ratio",
+    "headcount",
+    "total_shortfall",
+    "avg_shortfall",
+    "income_gap_ratio",
+    "poverty_gap_index",
+]
+
+DIMENSIONS_CONFIG = {
+    "poverty_line": [
+        "1",
+        "2",
+        "5",
+        "10",
+        "20",
+        "30",
+        "40",
+        "40% of the median",
+        "50% of the median",
+        "60% of the median",
+    ],
+    "welfare_type": ["dhi", "mi"],
+    "equivalence_scale": ["per capita", "square root"],
+}
+
+
+def run() -> None:
+    #
+    # Load inputs.
+    #
+    # Default collection config
+    config = paths.load_collection_config()
+
+    # Load grapher dataset.
+    ds = paths.load_dataset("luxembourg_income_study")
+    tb = ds.read("poverty", load_data=False)
+
+    #
+    # Create collection object
+    #
+    c = paths.create_collection(
+        config=config,
+        short_name="lis_poverty",
+        tb=tb,
+        indicator_names=INDICATOR_NAMES,
+        # dimensions=DIMENSIONS_CONFIG,
+    )
+
+    #
+    # (optional) Edit views
+    #
+    for view in c.views:
+        # if view.dimension["sex"] == "male":
+        #     view.config["title"] = "Something else"
+        pass
+
+    #
+    # Save garden dataset.
+    #
+    c.save()
