3897d2eebc1407e8ab8e4c7a5492f9de025d8d52
lucasrodes <lucasrodes@users.noreply.github.com>
2025-12-23 12:31:49 +0100

    use explicit dependency in etl

--- etl/steps/data/garden/technology/2024-05-13/computer_memory_storage.py
+++ etl/steps/data/garden/technology/2024-05-13/computer_memory_storage.py
@@ -14,6 +14,7 @@ def run(dest_dir: str) -> None:
     #
     # Load meadow dataset.
     ds_meadow = paths.load_dataset("computer_memory_storage")
+    ds_cpi = paths.load_dataset("us_consumer_prices")
 
     # Read table from meadow dataset.
     tb = ds_meadow["computer_memory_storage"].reset_index()
@@ -21,7 +22,7 @@ def run(dest_dir: str) -> None:
     #
     # Process data.
     #
-    us_cpi = ETLCatalog().find_latest(dataset="us_consumer_prices").reset_index()[["year", "all_items"]]
+    us_cpi = ds_cpi.read("us_consumer_prices")[["year", "all_items"]]
 
     # Left-merge and validate 1:1
     tb = tb.merge(us_cpi, on="year", how="left", validate="1:1")
