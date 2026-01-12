845eaae4288ec2b5afa797010d781f1f485b79cc
Tuna Acisu <tuna.acisu@ourworldindata.com>
2026-01-12 18:00:22 +0000

    :sparkles: add indicator titles & descriptions, improve map brackets

--- etl/steps/export/explorers/wash/latest/water_and_sanitation.py
+++ etl/steps/export/explorers/wash/latest/water_and_sanitation.py
@@ -10,6 +10,10 @@ def run() -> None:
     #
     # Load inputs.
     #
+    # Load dataset without including data
+    ds = paths.load_dataset("household")
+    tb = ds.read("household", load_data=False)
+
     # Load grapher config from YAML
     config = paths.load_collection_config()
 
@@ -20,14 +24,41 @@ def run() -> None:
         explorer=True,
     )
 
+    map_bins = {
+        "regular_perc": "10,,;20,,;30,,;40,,;50,,;60,,;70,,;80,,;90,,;100",
+        "focus_on_lower_perc": "5,,;10,,;15,,;20,,;25,,;30,,;35,,;40,,;45,,;0",
+    }
+
     # Edit display
     for view in c.views:
         assert view.indicators.y is not None
+        if len(view.indicators.y) == 1:
+            col_name = view.indicators.y[0].catalogPath.split("#")[1]
+            view.config["title"] = tb[col_name].metadata.title
+            view.config["subtitle"] = tb[col_name].metadata.description_short
         for y in view.indicators.y:
-            y.update_display(
-                {
-                    "colorScaleNumericMinValue": 0,
-                }
-            )
-
+            set_map_bins(y, view, map_bins)
     c.save()
+
+
+def set_map_bins(y, view, map_bins):
+    y.update_display(
+        {
+            "colorScaleNumericMinValue": 0,
+        }
+    )
+    if y.display.get("unit") == "%":
+        if check_lower_perc_columns(view):
+            y.update_display({"colorScaleNumericBins": map_bins["focus_on_lower_perc"]})
+        else:
+            y.update_display({"colorScaleNumericBins": map_bins["regular_perc"]})
+
+
+def check_lower_perc_columns(view):
+    dim = view.dimensions
+    resources = ["drinking_water", "sanitation"]
+    levels = ["limited", "unimproved", "no_facilities"]
+    if dim.get("resource") in resources and dim.get("level_of_use_access") in levels:
+        return True
+    else:
+        return False
