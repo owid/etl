80301c30fc2a2f0c1fcc95e316701362cd1fc71c
Pablo Arriagada <paarriagadap@gmail.com>
2026-01-15 18:42:32 +0000

    :construction: wip

--- etl/steps/data/grapher/wid/2026-01-12/world_inequality_database.py
+++ etl/steps/data/grapher/wid/2026-01-12/world_inequality_database.py
@@ -14,13 +14,17 @@ def run() -> None:
     ds_garden = paths.load_dataset("world_inequality_database")
 
     # Read table from garden dataset.
-    tb = ds_garden.read("world_inequality_database", reset_index=False)
+    tb_inequality = ds_garden.read("inequality", reset_index=False)
+    tb_incomes = ds_garden.read("incomes", reset_index=False)
+    tb_relative_poverty = ds_garden.read("relative_poverty", reset_index=False)
 
     #
     # Save outputs.
     #
     # Initialize a new grapher dataset.
-    ds_grapher = paths.create_dataset(tables=[tb], default_metadata=ds_garden.metadata)
+    ds_grapher = paths.create_dataset(
+        tables=[tb_inequality, tb_incomes, tb_relative_poverty], default_metadata=ds_garden.metadata
+    )
 
     # Save grapher dataset.
     ds_grapher.save()
