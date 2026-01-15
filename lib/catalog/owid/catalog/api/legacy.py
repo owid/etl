2dee03d348185cd74259bc86588f69b451434718
lucasrodes <lucasrodes@users.noreply.github.com>
2026-01-16 00:21:17 +0100

    bug fix

--- lib/catalog/owid/catalog/api/legacy.py
+++ lib/catalog/owid/catalog/api/legacy.py
@@ -445,7 +445,7 @@ class ETLCatalog(CatalogMixin):
         self.channels = channels
         self.timeout = timeout
         self.metadata = self._read_metadata(self.uri + "catalog.meta.json", timeout=timeout)
-        if self.metadata["format_version"] > 5:
+        if self.metadata["format_version"] > OWID_CATALOG_VERSION:
             raise PackageUpdateRequired(
                 f"library supports api version {OWID_CATALOG_VERSION}, "
                 f"but the remote catalog has version {self.metadata['format_version']} "
