8e8af5ac2bed46eba9848e90c4fd3b76feb9e816
lucasrodes <lucasrodes@users.noreply.github.com>
2026-01-19 22:01:52 +0100

    drop owid.catalog.catalogs

--- lib/catalog/owid/catalog/__init__.py
+++ lib/catalog/owid/catalog/__init__.py
@@ -2,8 +2,7 @@ __version__ = "1.0.0rc2"
 
 from owid.catalog import api, processing, utils
 from owid.catalog.api import Client, fetch, search
-from owid.catalog.catalogs import CHANNEL
-from owid.catalog.core.datasets import Dataset
+from owid.catalog.core.datasets import CHANNEL, Dataset
 from owid.catalog.core.indicators import Indicator, Variable
 from owid.catalog.core.meta import (
     DatasetMeta,
