5ef688dc3f18ff6f7696208aff466598aa6011e9
lucasrodes <lucasrodes@users.noreply.github.com>
2026-01-19 21:03:32 +0100

    deprecate owid.catalog.processing_log

--- lib/catalog/owid/catalog/processing_log.py
+++
@@ -1,5 +0,0 @@
-# Stub file for backwards compatibility - re-exports from core/processing_log.py
-# New code should import from owid.catalog.core.processing_log
-from owid.catalog.core.processing_log import wrap
-
-__all__ = ["wrap"]
