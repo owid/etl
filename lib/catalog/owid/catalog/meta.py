57ff47a8aaf0164fb827f65edde3508b627e2e94
lucasrodes <lucasrodes@users.noreply.github.com>
2026-01-19 22:08:01 +0100

    simpler re-imports, DeprecationWarning message

--- lib/catalog/owid/catalog/meta.py
+++ lib/catalog/owid/catalog/meta.py
@@ -1,31 +1,9 @@
-# Stub file for backwards compatibility - re-exports from core/meta.py
-# New code should import from owid.catalog.core.meta
-from owid.catalog.core.meta import (
-    DatasetMeta,
-    License,
-    MetaBase,
-    Origin,
-    Source,
-    T,
-    TableDimension,
-    TableMeta,
-    VariableMeta,
-    to_html,
+# Stub - re-exports from core.meta for backwards compatibility
+import warnings
+
+warnings.warn(
+    "Importing from 'owid.catalog.meta' is deprecated. Use 'owid.catalog.core.meta' instead.",
+    DeprecationWarning,
+    stacklevel=2,
 )
-
-# pruned_json is re-exported from utils for backwards compatibility
-from owid.catalog.core.utils import pruned_json
-
-__all__ = [
-    "T",
-    "MetaBase",
-    "License",
-    "Source",
-    "Origin",
-    "VariableMeta",
-    "DatasetMeta",
-    "TableDimension",
-    "TableMeta",
-    "to_html",
-    "pruned_json",
-]
+from owid.catalog.core.meta import *  # noqa: E402,F401,F403
--- lib/catalog/owid/catalog/utils.py
+++ lib/catalog/owid/catalog/utils.py
@@ -1,33 +1,9 @@
-# Stub file for backwards compatibility - re-exports from core/utils.py
-# New code should import from owid.catalog.core.utils or owid.catalog directly
-from owid.catalog.core.utils import (
-    T,
-    dataclass_from_dict,
-    dynamic_yaml_load,
-    dynamic_yaml_to_dict,
-    hash_any,
-    log,
-    parse_numeric_list,
-    prune_dict,
-    pruned_json,
-    remove_details_on_demand,
-    underscore,
-    underscore_table,
-    validate_underscore,
-)
+# Stub - re-exports from core.utils for backwards compatibility
+import warnings
 
-__all__ = [
-    "T",
-    "log",
-    "prune_dict",
-    "pruned_json",
-    "underscore",
-    "underscore_table",
-    "validate_underscore",
-    "dynamic_yaml_load",
-    "dynamic_yaml_to_dict",
-    "hash_any",
-    "dataclass_from_dict",
-    "remove_details_on_demand",
-    "parse_numeric_list",
-]
+warnings.warn(
+    "Importing from 'owid.catalog.utils' is deprecated. Use 'owid.catalog.core.utils' instead.",
+    DeprecationWarning,
+    stacklevel=2,
+)
+from owid.catalog.core.utils import *  # noqa: E402,F401,F403
