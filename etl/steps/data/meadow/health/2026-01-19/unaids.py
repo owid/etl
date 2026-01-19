17692e4f60519c25482646d856e8d6506ae56a60
lucasrodes <lucasrodes@users.noreply.github.com>
2026-01-19 11:43:29 +0100

    meadow

---
+++ etl/steps/data/meadow/health/2026-01-19/unaids.py
@@ -0,0 +1,83 @@
+"""Load a snapshot and create a meadow dataset."""
+
+from etl.helpers import PathFinder
+
+# Get paths and naming conventions for current step.
+paths = PathFinder(__file__)
+
+column_index = ["country", "year", "indicator", "dimension"]
+
+
+def run() -> None:
+    #
+    # Load inputs.
+    #
+    short_names = [
+        "epi",
+        "gam",
+        "kpa",
+        "ncpi",
+    ]
+    tables = []
+    for name in short_names:
+        # Retrieve snapshot.
+        short_name = f"unaids_{name}.zip"
+        snap = paths.load_snapshot(short_name)
+
+        # Load data from snapshot.
+        tb = snap.read_csv()
+
+        #
+        # Process data.
+        #
+        tb = clean_table(tb)
+        # Format table
+        tb = tb.format(column_index, short_name=name)
+
+        # Append current table to list of tables.
+        tables.append(tb)
+
+    #
+    # Save outputs.
+    #
+    # Initialize a new meadow dataset.
+    ds_meadow = paths.create_dataset(tables=tables)
+
+    # Save meadow dataset.
+    ds_meadow.save()
+
+
+def clean_table(tb):
+    """Minor table cleaning."""
+    paths.log.info(f"Formatting table {tb.m.short_name}")
+
+    # Rename columns, only keep relevant
+    columns = {
+        "Indicator": "indicator",
+        "Indicator_GId": "indicator_id",
+        "Unit": "unit",
+        "Subgroup": "dimension",
+        "Subgroup_Val_GId": "dimension_id",
+        "Area": "country",
+        # "Area ID" : "",
+        "Time Period": "year",
+        "Source": "source",
+        "Data value": "value",
+        "Formatted": "value_rounded",
+        "Data_Denominator": "data_denominator",
+        "Footnote": "footnote",
+    }
+    tb = tb.rename(columns=columns)[columns.values()]
+
+    # Sanity check: only one code per indicator
+    assert tb.groupby('indicator')['indicator_id'].nunique().max() == 1
+    assert tb.groupby('indicator_id')['indicator'].nunique().max() == 1
+
+    # Drop duplicates
+    tb = tb.drop_duplicates(subset=["country", "year", "indicator", "dimension"], keep="first")
+
+    # Handle NaNs
+    # tb.loc[:, "value"] = tb["value"].replace("...", np.nan)
+    tb = tb.dropna(subset=["value"])
+
+    return tb
