87d4894a581e905bdf6e16105c5869befdb03d9c
lucasrodes <lucasrodes@users.noreply.github.com>
2026-01-22 19:45:00 +0100

    fix meadow step

--- etl/steps/data/meadow/health/2026-01-19/unaids.py
+++ etl/steps/data/meadow/health/2026-01-19/unaids.py
@@ -1,11 +1,69 @@
 """Load a snapshot and create a meadow dataset."""
 
+import numpy as np
+
 from etl.helpers import PathFinder
 
 # Get paths and naming conventions for current step.
 paths = PathFinder(__file__)
 
 column_index = ["country", "year", "indicator", "dimension"]
+
+
+VALUES_ROUNDED_EXPECTED_NAN_VALUES = {
+    "epi": ["..."],
+    "gam": [
+        "Death penalty",
+        "Laws penalizing same-sex sexual acts have been decriminalized or never existed",
+        "Imprisonment or no penalty specified",
+        "Any criminalization or punitive regulation of sex work",
+        "Sex work is not subject to punitive regulations or is not criminalized",
+        "Issue is determined/differs at subnational level",
+        "Possession of drugs for personal use or drug use or consumption are not punished by laws or regulations",
+        "Possession of drugs for personal use or drug use and/or consumption are specified as criminal offences",
+        "Possession of drugs for personal use or drug use and/or consumption are specified as non-criminal offences",
+        "Compulsory detention for drug offences",
+        "4-High-income",
+        "1-Low-income",
+        "2-Lower-middle-income",
+        "3-Upper-middle-income",
+        "No",
+        "Yes",
+        "No but prosecutions exist based on general criminal laws",
+        "Yes for adolescents younger than 18 years",
+        "Yes for adolescents younger than 16 years",
+        "Yes for adolescents younger than 12 years",
+        "Deport prohibit short- and/or long-stay and require HIV testing or disclosure for some permits",
+        "Require HIV testing or disclosure for some permits",
+        "Prohibit short- and/or long-stay and require HIV testing or disclosure for some permits",
+        "Asia and Pacific",
+        "East and Southern Africa",
+        "Eastern Europe and Central Asia",
+        "Western and Central Europe and North America",
+        "Middle East and North Africa",
+        "Latin America",
+        "Caribbean",
+        "West and Central Africa",
+        "Latin America and the Caribbean",
+    ],
+    "kpa": [
+        "No",
+        "Yes",
+        "Yes imprisonment (up to 14 years)",
+        "Laws penalizing same-sex sexual acts have been decriminalized or never existed",
+        "Yes death penalty",
+        "Yes imprisonment (14 years - life)",
+        "No specific legislation",
+        "Yes no penalty specified",
+        "Any criminalization or punitive regulation of sex work",
+        "Sex work is not subject to punitive regulations or is not criminalized",
+        "No but prosecutions exist based on general criminal laws",
+        "No restrictions",
+        "Deport prohibit short- and/or long-stay and require HIV testing or disclosure for some permits",
+        "Require HIV testing or disclosure for some permits",
+        "Prohibit short- and/or long-stay and require HIV testing or disclosure for some permits",
+    ],
+}
 
 
 def run() -> None:
@@ -16,10 +74,11 @@ def run() -> None:
         "epi",
         "gam",
         "kpa",
-        "ncpi",
+        # "ncpi",
     ]
     tables = []
     for name in short_names:
+        paths.log.info(name)
         # Retrieve snapshot.
         short_name = f"unaids_{name}.zip"
         snap = paths.load_snapshot(short_name)
@@ -30,7 +89,7 @@ def run() -> None:
         #
         # Process data.
         #
-        tb = clean_table(tb)
+        tb = clean_table(tb, name)
         # Format table
         tb = tb.format(column_index, short_name=name)
 
@@ -47,7 +106,7 @@ def run() -> None:
     ds_meadow.save()
 
 
-def clean_table(tb):
+def clean_table(tb, name):
     """Minor table cleaning."""
     paths.log.info(f"Formatting table {tb.m.short_name}")
 
@@ -76,10 +135,13 @@ def clean_table(tb):
     # Drop duplicates
     tb = tb.drop_duplicates(subset=["country", "year", "indicator", "dimension"], keep="first")
 
-    # Handle NaNs
-    assert tb[tb["value"].isna()]["value_rounded"].unique() == [
-        "..."
-    ], "Unexpected non-numeric missing value indicators"
+    # Check NaN values: Check that only expected indicators have NaNs
+    # We do this check because we are dropping all NaN values.
+    # Note: Some non-NaN values are categorical indicators, whose values are captured in value_rounded.
+    # Note 2: For some reason, there are some countries as indicators. Unclear why.
+    assert (
+        tb[tb["value"].isna()]["value_rounded"].dropna().unique() == VALUES_ROUNDED_EXPECTED_NAN_VALUES[name]
+    ).all(), "Unexpected indicators with NaN values"
     # tb.loc[:, "value"] = tb["value"].replace("...", np.nan)
     tb = tb.dropna(subset=["value"])
 
