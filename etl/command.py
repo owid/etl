86ccea7d3bd5b08ad8a73583919cc83447f0761d
lucasrodes <lucasrodes@users.noreply.github.com>
2026-01-26 02:09:23 +0100

    deprecate autocomplete

--- etl/command.py
+++ etl/command.py
@@ -20,7 +20,6 @@ from pathlib import Path
 from typing import TYPE_CHECKING, Any, Callable, Dict, Iterator, List, Optional, Set
 
 import rich_click as click
-from click.shell_completion import CompletionItem
 
 from etl import paths
 from etl.dag_helpers import load_dag
@@ -40,30 +39,6 @@ DAG = Dict[str, Set[str]]
 
 # if the number of open files allowed is less than this, increase it
 LIMIT_NOFILE = 4096
-
-
-def complete_steps(ctx: click.Context, param: click.Argument, incomplete: str) -> List[CompletionItem]:
-    """Complete step names from DAG with fuzzy matching.
-
-    Used for shell tab-completion (bash, zsh, fish).
-    """
-    try:
-        from etl.browser import filter_steps, get_all_steps
-
-        # Load DAG and get all steps
-        dag = load_dag(paths.DEFAULT_DAG_FILE)
-        private = ctx.params.get("private", False)
-        all_steps = get_all_steps(dag, private=private)
-
-        # Filter using existing fuzzy logic
-        if incomplete:
-            matches = filter_steps(incomplete, all_steps)
-        else:
-            matches = all_steps[:50]  # Show top 50 if no input
-
-        return [CompletionItem(m) for m in matches[:50]]
-    except Exception:
-        return []  # Graceful degradation
 
 
 @click.command(name="run")
@@ -179,7 +154,6 @@ def complete_steps(ctx: click.Context, param: click.Argument, incomplete: str) -
     "steps",
     nargs=-1,
     type=str,
-    shell_complete=complete_steps,
 )
 def main_cli(
     steps: List[str],
