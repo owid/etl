206f0c2c7f6d62268d2f8c611ca880c9d7317343
Pablo Arriagada <paarriagadap@gmail.com>
2026-01-26 17:06:38 +0000

    :sparkles: modify title

--- etl/steps/export/multidim/worldbank_wdi/latest/labor_force_participation_rate.py
+++ etl/steps/export/multidim/worldbank_wdi/latest/labor_force_participation_rate.py
@@ -13,6 +13,10 @@ def run() -> None:
     # Default collection config
     config = paths.load_collection_config()
 
+    # Load grapher dataset.
+    ds = paths.load_dataset("wdi")
+    tb = ds.read("wdi", load_data=False)
+
     #
     # Create collection object
     #
@@ -21,13 +25,50 @@ def run() -> None:
         short_name="labor_force_participation_rate",
     )
 
+    # Group sex views
+    c.group_views(
+        groups=[
+            {
+                "dimension": "sex",
+                "choices": ["male", "female"],
+                "choice_new_slug": "male_vs_female",
+                "view_config": {
+                    "hideRelativeToggle": "false",
+                    "selectedFacetStrategy": "entity",
+                    "hasMapTab": False,
+                    "chartTypes": ["LineChart"],
+                },
+            },
+        ]
+    )
+
     #
-    # (optional) Edit views
+    # Edit views
     #
     for view in c.views:
-        # if view.dimension["sex"] == "male":
-        #     view.config["title"] = "Something else"
-        pass
+        # Initialize config and metadata if they're None
+        if view.config is None:
+            view.config = {}
+        if view.metadata is None:
+            view.metadata = {}
+
+        if view.dimensions["sex"] == "male_vs_female":
+            # Get the catalog path of the first indicator
+            if view.indicators.y:
+                first_indicator_path = view.indicators.y[0].catalogPath
+                # Extract the column name from the catalog path
+                indicator_col = first_indicator_path.split("#")[-1]
+
+            # Remove "Male " from beginning of title. Also capitalize first letter. We need to extract this from the table's metadata (Grapher step)
+            # It is only male because of how we grouped the views
+            view.config["title"] = (
+                tb[indicator_col].metadata.presentation.grapher_config["title"].replace("Male ", "").capitalize()
+                + ": males vs. females"
+            )
+            view.config["subtitle"] = (
+                tb[indicator_col].metadata.presentation.grapher_config["subtitle"].replace("male ", "")
+            )
+            view.metadata["description_short"] = tb[indicator_col].metadata.description_short.replace("male ", "")
 
     #
     # Save garden dataset.
