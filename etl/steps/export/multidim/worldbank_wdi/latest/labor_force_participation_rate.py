e4a97b749b601e2088aa46663dd269ef5133e176
Pablo Arriagada <paarriagadap@gmail.com>
2026-01-26 16:06:35 +0000

    :sparkles: new MDim

---
+++ etl/steps/export/multidim/worldbank_wdi/latest/labor_force_participation_rate.py
@@ -0,0 +1,35 @@
+"""Load a meadow dataset and create a garden dataset."""
+
+from etl.helpers import PathFinder
+
+# Get paths and naming conventions for current step.
+paths = PathFinder(__file__)
+
+
+def run() -> None:
+    #
+    # Load inputs.
+    #
+    # Default collection config
+    config = paths.load_collection_config()
+
+    #
+    # Create collection object
+    #
+    c = paths.create_collection(
+        config=config,
+        short_name="labor_force_participation_rate",
+    )
+
+    #
+    # (optional) Edit views
+    #
+    for view in c.views:
+        # if view.dimension["sex"] == "male":
+        #     view.config["title"] = "Something else"
+        pass
+
+    #
+    # Save garden dataset.
+    #
+    c.save()
