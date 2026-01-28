32565580fc0f863c74e6c5c90582e2bbc92859b3
lucasrodes <lucasrodes@users.noreply.github.com>
2026-01-28 12:15:07 +0100

    keep just one list

--- etl/steps/data/external/owid_grapher/latest/neighbours.py
+++ etl/steps/data/external/owid_grapher/latest/neighbours.py
@@ -19,21 +19,23 @@ def run() -> None:
     for col in list_columns:
         tb[col] = tb[col].apply(lambda x: x.split(";") if x else [])
 
-    # Rename columns to value_* format.
+    # Pick one neighbour list
+    COLUMN = "value_neighbours_balanced"
+    # "value_neighbours_border",  # Neighbors by border share
+    # "value_neighbours_balanced",  # Neighbors by balanced score (1:1 border/distance)
+    # "value_neighbours_distance",  # Neighbors by distance-weighted score (~2:1 distance/border)
+    # "value_neighbours_population",  # Neighbors by population
+    # "value_nearest_borders",  # Nearest countries by distance
+
     tb = tb.rename(
         columns={
             "country": "entity",
-            "neighbours_1": "value_neighbours_border",  # Neighbors by border share
-            "neighbours_2": "value_neighbours_balanced",  # Neighbors by balanced score (1:1 border/distance)
-            "neighbours_3": "value_neighbours_distance",  # Neighbors by distance-weighted score (~2:1 distance/border)
-            "neighbours_4": "value_neighbours_population",  # Neighbors by population
-            "neighbours_10": "value_nearest_borders",  # Nearest countries by distance
+            COLUMN: "value",
         }
     )
 
     # Set index and name.
-    tb = tb.set_index(["entity"], verify_integrity=True)
-    tb.metadata.short_name = "neighbours"
+    tb = tb.format(["entity"], short_name="neighbours")
 
     # Save as JSON.
     ds = paths.create_dataset(tables=[tb], formats=["json"])
