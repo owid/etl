e654105d7cbe825d85067554f7e31f06dff0e0ea
lucasrodes <lucasrodes@users.noreply.github.com>
2026-02-05 01:11:13 +0100

    edit country

--- etl/steps/data/meadow/demography/2026-02-04/census_us_idb.py
+++ etl/steps/data/meadow/demography/2026-02-04/census_us_idb.py
@@ -26,9 +26,12 @@ def run() -> None:
         }
     )
 
+    # Keep and process countries
+    tb = filter_and_parse_country_codes(tb)
+
     # Improve tables format.
     tables = [
-        tb.format(["geo_id", "year"]),
+        tb.format(["country", "year"]),
     ]
 
     #
@@ -42,3 +45,22 @@ def run() -> None:
 
     # Save meadow dataset.
     ds_meadow.save()
+
+
+def filter_and_parse_country_codes(tb):
+    """As per US Census site, codes starting with W14 correspond to countries
+    - Go to https://www.census.gov/data/developers/data-sets/international-database.html
+    - Click on "Geography" under "Time Series...": https://api.census.gov/data/timeseries/idb/5year/geography.html
+    """
+    # Keep only countries
+    tb = tb[tb["GEO_ID"].str.startswith("W14")]
+    # Sanity check 1: geo_id has the expected format
+    # Verify geo_id format is 'W140000WOXX' where XX is the country code
+    assert (tb["GEO_ID"].str.len().unique() == [11]).all()
+    assert tb["GEO_ID"].str.match(r"^W140000WO[A-Z]{2}$").all(), "Unexpected geo_id format"
+
+    # Extract country codes from geo_id (last 2 characters)
+    tb.loc[:, "GEO_ID"] = tb["GEO_ID"].str[-2:]
+    tb = tb.rename(columns={"GEO_ID": "country"})
+
+    return tb
