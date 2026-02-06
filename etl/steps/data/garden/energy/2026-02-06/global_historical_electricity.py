763f86fcb82d7d40a635e1cdfce6acd2152c5fe5
Pablo Rosado <pabloarosado@gmail.com>
2026-02-06 15:10:06 +0100

    Adapt meadow and garden steps

--- etl/steps/data/garden/energy/2026-02-06/global_historical_electricity.py
+++ etl/steps/data/garden/energy/2026-02-06/global_historical_electricity.py
@@ -13,17 +13,28 @@ def run() -> None:
     # Load meadow dataset.
     ds_meadow = paths.load_dataset("global_historical_electricity")
 
-    # Read table from meadow dataset.
-    tb = ds_meadow.read("global_historical_electricity")
+    # Read table on the total electricity production (and consumption).
+    tb_total = ds_meadow.read("electricity_production_and_consumption")
+    # Read table on the share of electricity production by source.
+    tb_share = ds_meadow.read("electricity_production_share_by_source")
 
     #
     # Process data.
     #
-    # Harmonize country names.
-    tb = paths.regions.harmonize_names(tb=tb)
+    # Create a combined table with total electricity production, and the share of each source.
+    tb = tb_total.drop(columns=["electricity_consumption"]).merge(tb_share, on=["year"], how="outer")
+
+    for column in [column for column in tb.columns if column not in ["year", "electricity_production"]]:
+        # Add a column with the electricity produced by each source.
+        tb[f"{column}_production"] = tb["electricity_production"] * tb[column]
+        # Rename share column conveniently.
+        tb = tb.rename(columns={column: f"{column.replace('__', '_')}_share"}, errors="raise")
+
+    # Add a country column.
+    tb["country"] = "World"
 
     # Improve table format.
-    tb = tb.format(["country", "year"])
+    tb = tb.format(["country", "year"], short_name=paths.short_name)
 
     #
     # Save outputs.
