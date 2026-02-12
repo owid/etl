b9abfcd6303e6ce4b87610ec4b3b94fef21d28b4
Fiona Spooner <fiona@ourworldindata.org>
2026-02-12 14:18:56 +0000

    📊 IHME GBD Risk Factors (#5656)
    * 📊 ihme risk factors
    
    * adding risk factor deaths
    
    * adding risk factor deaths steps
    
    * fix table name
    
    * table name
    
    * drop population group name
    
    * fix order of description short

---
+++ etl/steps/data/garden/ihme_gbd/2026-02-07/gbd_risk_deaths.py
@@ -0,0 +1,55 @@
+"""Load a meadow dataset and create a garden dataset."""
+
+from shared import add_regional_aggregates
+
+from etl.helpers import PathFinder
+
+# Get paths and naming conventions for current step.
+paths = PathFinder(__file__)
+REGIONS = ["North America", "South America", "Europe", "Africa", "Asia", "Oceania"]
+AGE_GROUPS_RANGES = {
+    "All ages": [0, None],
+    "<5 years": [0, 4],
+    "5-14 years": [5, 14],
+    "15-49 years": [15, 49],
+    "50-69 years": [50, 69],
+    "70+ years": [70, None],
+}
+
+
+def run() -> None:
+    #
+    # Load inputs.
+    #
+    # Load meadow dataset.
+    ds_meadow = paths.load_dataset("gbd_risk_deaths")
+
+    # Read table from meadow dataset.
+    tb = ds_meadow.read("gbd_risk_deaths", reset_index=True)
+    tb = tb.drop(columns=["population_group_name"])
+    ds_regions = paths.load_dataset("regions")
+    #
+    # Process data.
+    #
+    tb = paths.regions.harmonize_names(tb)
+    # Add regional aggregates
+    tb = add_regional_aggregates(
+        tb=tb,
+        ds_regions=ds_regions,
+        index_cols=["country", "year", "metric", "measure", "rei", "cause", "age"],
+        regions=REGIONS,
+        age_group_mapping=AGE_GROUPS_RANGES,
+        run_percent=False,
+    )
+
+    # Format the tables
+    tb = tb.format(["country", "year", "metric", "measure", "rei", "age", "cause"])
+
+    #
+    # Save outputs.
+    #
+    # Create a new garden dataset with the same metadata as the meadow dataset.
+    ds_garden = paths.create_dataset(tables=[tb], check_variables_metadata=True, default_metadata=ds_meadow.metadata)
+
+    # Save changes in the new garden dataset.
+    ds_garden.save()
