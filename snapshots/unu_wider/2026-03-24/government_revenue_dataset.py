df94832535fafb5b421da77ccbd48c85cf0f000c
Pablo Arriagada <paarriagadap@gmail.com>
2026-03-24 15:35:08 +0000

    :sparkles: update snapshot

--- snapshots/unu_wider/2026-03-24/government_revenue_dataset.py
+++ snapshots/unu_wider/2026-03-24/government_revenue_dataset.py
@@ -4,9 +4,9 @@ Steps to obtain the data:
 - Go to https://www.wider.unu.edu/project/grd-government-revenue-dataset
 - Click on `Access full and additional datasets here`
 - Fill the form and submit
-- Download the Full dataset (Stata format). The name of the file should be similar to UNUWIDERGRD_{year}.dta_.zip
+- Download the Core dataset (Stata format). The name of the file should be similar to UNUWIDERGRD_{year}.dta_.zip
 - Unzip the file and upload the Stata file (UNUWIDERGRD_{year}.dta) by running:
-    python *this code relative path* --path-to-file *path to Stata file*
+    etls unu_wider/{version}/government_revenue_dataset --path-to-file *path to Stata file*
 
 
 """
@@ -30,7 +30,3 @@ def main(path_to_file: str, upload: bool) -> None:
 
     # Copy local data file to snapshots data folder, add file to DVC and upload to S3.
     snap.create_snapshot(filename=path_to_file, upload=upload)
-
-
-if __name__ == "__main__":
-    main()
