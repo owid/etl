0140a2d3df67b69109e8aab5e5a2d852eb3c727f
Marigold <mojmir.vinkler@gmail.com>
2026-03-26 10:49:46 +0100

    🐛 Fix catalog.meta.Source import error in grapher/to_db.py

--- etl/grapher/to_db.py
+++ etl/grapher/to_db.py
@@ -21,7 +21,7 @@ import pandas as pd
 import structlog
 from owid import catalog
 from owid.catalog import Table, Variable, VariableMeta, utils
-from owid.catalog.core.meta import update_variable_metadata
+from owid.catalog.core.meta import Source, update_variable_metadata
 from owid.catalog.core.paths import CatalogPath
 from owid.catalog.core.utils import hash_any
 from sqlalchemy import select, text, update
@@ -53,7 +53,7 @@ class DatasetUpsertResult:
 
 
 def upsert_dataset(
-    engine: Engine, dataset: catalog.Dataset, namespace: str, sources: List[catalog.meta.Source]
+    engine: Engine, dataset: catalog.Dataset, namespace: str, sources: List[Source]
 ) -> DatasetUpsertResult:
     assert dataset.metadata.short_name, "Dataset must have a short_name"
     assert dataset.metadata.version, "Dataset must have a version"
