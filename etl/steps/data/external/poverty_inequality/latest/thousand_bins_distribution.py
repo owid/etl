1107af2961cf0ef96f66954e18489e2d2a8c3ef1
Pablo Arriagada <paarriagadap@gmail.com>
2026-04-07 11:57:58 +0100

    :sparkles: external step for thousand bins

---
+++ etl/steps/data/external/poverty_inequality/latest/thousand_bins_distribution.py
@@ -0,0 +1,23 @@
+"""Thousand-bins global income distribution from World Bank PIP.
+
+Published as CSV and JSON for use by Grapher interactive charts and static charts.
+"""
+
+from etl.helpers import PathFinder
+
+# Get paths and naming conventions for current step.
+paths = PathFinder(__file__)
+
+
+def run() -> None:
+    # Load garden dataset.
+    ds_garden = paths.load_dataset("thousand_bins_distribution")
+
+    # Read table from garden dataset.
+    tb = ds_garden.read("thousand_bins_distribution", reset_index=False)
+
+    # Initialize a new external dataset.
+    ds = paths.create_dataset(tables=[tb], formats=["csv", "json"])
+
+    # Save external dataset.
+    ds.save()
