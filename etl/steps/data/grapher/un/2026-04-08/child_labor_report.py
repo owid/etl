27319fa89f54a0045f40e0f0cc7ff4214724830d
Pablo Arriagada <paarriagadap@gmail.com>
2026-04-09 17:44:22 +0100

    :construction: wip

--- etl/steps/data/grapher/un/2026-04-08/child_labor_report.py
+++ etl/steps/data/grapher/un/2026-04-08/child_labor_report.py
@@ -13,14 +13,15 @@ def run() -> None:
     # Load garden dataset.
     ds_garden = paths.load_dataset("child_labor_report")
 
-    # Read table from garden dataset.
-    tb = ds_garden.read("child_labor_report", reset_index=False)
+    # Read tables from garden dataset.
+    tb = ds_garden.read("child_labor", reset_index=False)
+    tb_sector = ds_garden.read("sector", reset_index=False)
 
     #
     # Save outputs.
     #
     # Initialize a new grapher dataset.
-    ds_grapher = paths.create_dataset(tables=[tb], default_metadata=ds_garden.metadata)
+    ds_grapher = paths.create_dataset(tables=[tb, tb_sector], default_metadata=ds_garden.metadata)
 
     # Save grapher dataset.
     ds_grapher.save()
