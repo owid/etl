9a57ad95e9a0fe88d87fe3faf5368acde8ecac62
Tuna Acisu <tuna.acisu@ourworldindata.com>
2026-06-02 14:38:53 +0200

    🐋 whaling data 2/2

---
+++ snapshots/biodiversity/2014/historic_whaling_rocha_et_al.py
@@ -0,0 +1,31 @@
+"""Script to create a snapshot of dataset.
+
+Steps to download the data manually:
+  1. Go to https://spo.nmfs.noaa.gov/content/emptying-oceans-summary-industrial-whaling-catches-20th-century
+  2. Download the PDF file and save it locally.
+  3. Run: python snapshots/biodiversity/2014/historic_whaling_rocha_et_al.py --path-to-file <path>
+"""
+
+import click
+
+from etl.helpers import PathFinder
+
+paths = PathFinder(__file__)
+
+
+@click.command()
+@click.option("--upload/--skip-upload", default=True, type=bool, help="Upload dataset to Snapshot")
+@click.option("--path-to-file", prompt=True, type=str, help="Path to local data file.")
+def run(upload: bool = True, path_to_file: str | None = None) -> None:
+    """Create a new snapshot.
+
+    Args:
+        upload: Whether to upload the snapshot to S3.
+        path_to_file: Path to local data file.
+    """
+    snap = paths.init_snapshot()
+    snap.create_snapshot(filename=path_to_file, upload=upload)
+
+
+if __name__ == "__main__":
+    run()
