b3ca520e9c5f546e2a656a78c49ea79a1c64a3bf
Tuna Acisu <tuna.acisu@ourworldindata.com>
2026-07-01 12:55:00 +0100

    :bar_chart

---
+++ snapshots/antibiotics/2026-06-30/glass_enrolment.py
@@ -0,0 +1,25 @@
+"""Script to create a snapshot of dataset.
+
+The data should be available here: https://www.who.int/initiatives/glass/country-participation
+
+But if it is out of date (e.g not in sync with the image on the page above), then contact glass@who.int to access the latest data.
+"""
+
+from pathlib import Path
+
+import click
+
+from etl.snapshot import Snapshot
+
+# Version for current snapshot dataset.
+SNAPSHOT_VERSION = Path(__file__).parent.name
+
+
+@click.command()
+@click.option("--upload/--skip-upload", default=True, type=bool, help="Upload dataset to Snapshot")
+def main(upload: bool) -> None:
+    # Create a new snapshot.
+    snap = Snapshot(f"antibiotics/{SNAPSHOT_VERSION}/glass_enrolment.xlsx")
+
+    # Copy local data file to snapshots data folder, add file to DVC and upload to S3.
+    snap.create_snapshot(upload=upload)
